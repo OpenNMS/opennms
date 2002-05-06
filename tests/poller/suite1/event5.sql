c3c9ff21575fee4c3452c86104225e9c4e965308
Unknown Author <opennms-devel@lists.sourceforge.net>
2002-05-06 15:13:20 +0000

    This commit was manufactured by cvs2svn to create branch 'DocBook-1_65_1'.

--- tests/poller/suite1/event5.sql
+++
@@ -1,8 +0,0 @@
-DELETE FROM ifServices WHERE nodeID = 5;
-DELETE FROM ipInterface WHERE nodeID = 5;
-DELETE FROM node WHERE nodeID = 5;
-
-INSERT INTO node (nodeID, dpName, nodeCreateTime, nodeType) VALUES (5, 'localhost', '30-Aug-2001 12:45:00', 'A');
-INSERT INTO ipInterface (nodeID, ipAddr, ipHostname, isManaged) VALUES (5, '192.168.0.195', '192.168.0.195', 'A');
-INSERT INTO ifServices (nodeID, ipAddr, serviceID, status, source, notify) VALUES (5, '192.168.0.195', 1, 'A', 'P', 'N');
-
