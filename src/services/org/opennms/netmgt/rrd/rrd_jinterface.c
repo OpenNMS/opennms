5abaab2fdf984800d433c20350870a24255ad15d
Benjamin Reed <rangerrick@users.sourceforge.net>
2003-02-06 03:40:59 +0000

    whups, had debug defined when I checked in

--- src/services/org/opennms/netmgt/rrd/rrd_jinterface.c
+++ src/services/org/opennms/netmgt/rrd/rrd_jinterface.c
@@ -47,7 +47,7 @@
 #define FALSE 0
 #define TRUE 1
 
-#define DEBUG /* Enable/Disable debugging */
+#undef DEBUG /* Enable/Disable debugging */
 
 /*---------------------------------------------------------------------------
   *
