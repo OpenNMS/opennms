3935df59a55f52c6850fbfa2fb020019223c062f
Tarus Balog <tarus@users.sourceforge.net>
2003-02-17 20:24:49 +0000

    Corrected a small mistake in install.pl and added some env variables to platform_sun.sh

--- tools/infrastructure/platform_sun.sh
+++ tools/infrastructure/platform_sun.sh
@@ -5,7 +5,9 @@ PACKAGES="$PACKAGES PLATFORM_SUN"
 
 if [ `uname` = 'SunOS' ]; then
 	PATH="/usr/local/bin:/usr/xpg4/bin:/usr/ccs/bin:$PATH" export PATH
-	LD_LIBRARY_PATH="/usr/local/pgsql/lib:/usr/local/rrdtool-1.0.37/lib:$LD_LIBRARY_PATH" export LD_LIBRARY_PATH
+        LD_LIBRARY_PATH="/usr/local/lib:/usr/local/ssl/lib/:/usr/local/pgsql/lib:/usr/local/opennms/lib:/usr/local/pgsql/include/server/lib:/usr/local/pgsql/include/internal/lib:$LD_LIBRARY_PATH" export LD_LIBRARY_PATH
+	POSTGRES_LIB="/usr/local/pgsql/lib" export POSTGRES_LIB
+        PERL5LIB="/usr/local/lib/perl5" export PERL5LIB
 	PLATFORM=sun export PLATFORM
 	# compile.platform = the arch-specific path for headers in the JDK
 	DEFINES="$DEFINES -Dcompile.otherlibs=/lib/libc.a -Dcompile.platform=solaris -Dcompile.soext=so"
