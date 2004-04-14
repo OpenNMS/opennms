f4c285174b0d8123ec780e675c4a45f8b8e6d96e
Edwin Buck <ebuck_@users.sourceforge.net>
2004-04-14 17:46:54 +0000

    Wednesday, April 14 2004 - Edwin Buck   Reworked build.sh and build.xml   -- build.sh now calls ant 1.6 located in devlib   -- target usage on build.xml now redirects to defined ant tasks, not      static text.

--- build.sh
+++ build.sh
@@ -50,10 +50,10 @@ fi
 # see tools/infrastructure/build_classpath.sh for syntax
 CLASSPATH=`build_classpath cp:$CLASSPATH_OVERRIDE dir:$TOOL_CLASSES \
 	dir:$BUILD_CLASSES dir:$SABLECC_CLASSES \
-	dir:$PREFIX/lib jar:ant141 jar:ant141-optional \
-	jar:xerces201 jar:xalan231 jar:regexp120 jar:jdhcp jar:castor-0.9.3.9 \
-	jar:castor-0.9.3.9-xml jar:jcifs jar:log4j jar:postgresql \
-	jar:jdbc2_0-stdext jar:sablecc-2.17.2 jar:sablecc-anttask-1.1.0 \
+	dir:$PREFIX/lib \
+	jar:xerces201 jar:xalan231 jar:regexp120 jar:jdhcp \
+	jar:jcifs jar:log4j jar:postgresql \
+	jar:jdbc2_0-stdext \
 	jar:servlet jar:catalina jar:fop0203 jar:bsf220 jar:batik100 \
 	jar:jimi100 jar:ldap jar:batik jar:avalon-framework-4.0 jar:logkit-1.0 \
 	jar:jasper-compiler jar:mx4j jar:mx4j-tools jar:xmlrpc-1.2-b1 "cp:$CLASSPATH"` \
@@ -113,18 +113,18 @@ RETVAL=$?
 if [ "$RETVAL" -gt "0" ]; then
 	if [ "$VERBOSE" = "1" ]; then
 		echo "*** $RETVAL new tool files -- rebuilding ***"
-		echo $JAVA_HOME/bin/java -Droot.source="$PREFIX" -Djava.home="$JAVA_HOME" -cp "$CLASSPATH" \
-			-mx256m org.apache.tools.ant.Main $DEFINES -buildfile tools/build.xml $TOOL_ARGS compile.tools
+		echo $JAVA_HOME/bin/java -Droot.source="$PREFIX" -Dant.home=devlib -Djava.home="$JAVA_HOME" -cp "devlib/ant-launcher.jar:$CLASSPATH" \
+			-mx256m org.apache.tools.ant.launch.Launcher $DEFINES -buildfile tools/build.xml $TOOL_ARGS compile.tools
 	fi
-	$JAVA_HOME/bin/java $DEFINES -Droot.source="$PREFIX" -Djava.home="$JAVA_HOME" -cp "$CLASSPATH" \
-		-mx256m org.apache.tools.ant.Main -buildfile tools/build.xml $TOOL_ARGS compile.tools
+	$JAVA_HOME/bin/java $DEFINES -Droot.source="$PREFIX" -Dant.home=devlib -Djava.home="$JAVA_HOME" -cp "devlib/ant-launcher.jar:$CLASSPATH" \
+		-mx256m org.apache.tools.ant.launch.Launcher -buildfile tools/build.xml $TOOL_ARGS compile.tools
 fi
 
 if [ "$VERBOSE" = "1" ]; then
 	DEFINES="$DEFINES -verbose"
-	echo $JAVA_HOME/bin/java $DEFINES -Droot.source="$PREFIX" -Djava.home="$JAVA_HOME" -cp "$CLASSPATH" \
-		-mx256m org.apache.tools.ant.Main "$@"
+	echo $JAVA_HOME/bin/java $DEFINES -Dant.home=devlib -Droot.source="$PREFIX" -Djava.home="$JAVA_HOME" -cp "devlib/ant-launcher.jar:$CLASSPATH" \
+		-mx256m org.apache.tools.ant.launch.Launcher "$@"
 fi
 
-$JAVA_HOME/bin/java $DEFINES -Droot.source="$PREFIX" -Djava.home="$JAVA_HOME" -cp "$CLASSPATH" \
-	-mx256m org.apache.tools.ant.Main "$@"
+$JAVA_HOME/bin/java $DEFINES -Dand.home=devlib -Droot.source="$PREFIX" -Djava.home="$JAVA_HOME" -cp "devlib/ant-launcher.jar:$CLASSPATH" \
+	-mx256m org.apache.tools.ant.launch.Launcher "$@"
