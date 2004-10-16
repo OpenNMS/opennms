869a785fecb445c463cd2e519682153a16c18865
Matt Brozowski <brozow@users.sourceforge.net>
2004-10-16 00:32:33 +0000

    Fix unused import

--- src/services/org/opennms/netmgt/capsd/AbstractTcpPlugin.java
+++ src/services/org/opennms/netmgt/capsd/AbstractTcpPlugin.java
@@ -46,7 +46,6 @@ import java.net.ConnectException;
 import java.net.InetAddress;
 import java.net.NoRouteToHostException;
 import java.net.Socket;
-import java.security.KeyManagementException;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
