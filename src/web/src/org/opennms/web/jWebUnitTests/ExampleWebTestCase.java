76ca02cc22d3e5d7a141f6f951b7a0ec39b2deac
Mike Huot <mhuot@users.sourceforge.net>
2005-03-27 17:28:32 +0000

    Preliminary jwebunit stuff.  Does nto do anything at this time.  Hoping someone else can take a shot at it.

---
+++ src/web/src/org/opennms/web/jWebUnitTests/ExampleWebTestCase.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package org.opennms.web.jWebUnitTests;
+
+/**
+ * @author mhuot
+ *
+ */
+
+import java.io.FileInputStream;
+import net.sourceforge.jwebunit.WebTestCase;
+import com.meterware.servletunit.ServletRunner;
+import com.meterware.servletunit.ServletUnitClient;
+
+public class ExampleWebTestCase extends WebTestCase {
+    
+    public ExampleWebTestCase(String name) {
+        super(name);
+    }
+    
+    public void setUp() throws Exception {
+        ServletRunner sr = new ServletRunner(new FileInputStream("src/web/etc/web.xml"));
+     
+        ServletUnitClient sc = sr.newClient();
+        getTestContext().setWebClient(sc);
+        getTestContext().setAuthorization("admin","admin");
+        getTestContext().setBaseUrl("http://localhost:8080/opennms");
+    }
+    
+    public void testAddInterface() {
+        
+        beginAt("/index.jsp");
+        assertTitleEquals("OpenNMS Web Console");
+        assertLinkPresentWithText("Admin");
+        clickLinkWithText("Admin");
+        assertTitleEquals("Admin | OpenNMS Web Console");
+        clickLinkWithText("Add Interface");
+        assertTitleEquals("Admin | OpenNMS Web Console");
+        assertTextPresent("Please enter a new IP address below.");
+        setFormElement("ipAddress", "10.10.10.10");
+        submit();
+        clickLinkWithText("Search");
+        setFormElement("iplike", "10.10.10.10");        
+        submit();
+    }
+
+    public void _testAdminPage() {
+        
+        beginAt("/index.jsp");
+        assertTitleEquals("OpenNMS Web Console");
+        assertLinkPresentWithText("Admin");
+        clickLinkWithText("Admin");
+        assertTitleEquals("Admin | OpenNMS Web Console");
+    }
+    
+}
