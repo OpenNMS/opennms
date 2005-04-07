382044ccb3fb3d5154144c36e16ddbb051037e8e
David Hustace <dhustace@users.sourceforge.net>
2005-04-07 04:28:40 +0000

    -Added v3 attributes to snmp-config.xml-cleaned up some imports-David

--- src/services/org/opennms/netmgt/capsd/SnmpV3Plugin.java
+++ src/services/org/opennms/netmgt/capsd/SnmpV3Plugin.java
@@ -44,10 +44,7 @@ import java.util.Map;
 import org.opennms.netmgt.utils.ParameterMap;
 import org.opennms.netmgt.utils.SnmpHelpers;
 import org.snmp4j.PDU;
-import org.snmp4j.ScopedPDU;
 import org.snmp4j.Snmp;
-import org.snmp4j.Target;
-import org.snmp4j.TransportMapping;
 import org.snmp4j.UserTarget;
 import org.snmp4j.event.ResponseEvent;
 import org.snmp4j.mp.MPv3;
@@ -55,16 +52,11 @@ import org.snmp4j.mp.SnmpConstants;
 import org.snmp4j.security.AuthMD5;
 import org.snmp4j.security.PrivDES;
 import org.snmp4j.security.SecurityLevel;
-import org.snmp4j.security.SecurityModels;
-import org.snmp4j.security.SecurityProtocols;
-import org.snmp4j.security.USM;
-import org.snmp4j.security.UsmUser;
 import org.snmp4j.smi.Address;
 import org.snmp4j.smi.OID;
 import org.snmp4j.smi.OctetString;
 import org.snmp4j.smi.UdpAddress;
 import org.snmp4j.smi.VariableBinding;
-import org.snmp4j.transport.DefaultUdpTransportMapping;
 
 /**
  * This class is used by capsd to detect if an agent supports SNMPv3.
