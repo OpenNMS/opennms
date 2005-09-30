a13aad3e0ee31714347c364395f6afd046aa1366
Matt Brozowski <brozow@users.sourceforge.net>
2005-09-30 15:39:47 +0000

    Fix javadoc for the new parameters

--- src/services/org/opennms/netmgt/xmlrpcd/Provisioner.java
+++ src/services/org/opennms/netmgt/xmlrpcd/Provisioner.java
@@ -51,7 +51,8 @@ public interface Provisioner {
      * @param interval <code>int</code> the standard polling interval in milliseconds 
      * @param downTimeInterval <code>int</code> the downtime polling interval in milliseconds(downtime model)
      * @param downTimeDuration <code>int</> the amount of time in milliseconds the downtime polling interval is in effect
-     * @return Always returns true.  Check for XML-RPC exception.
+     * @return Always returns true.
+     * @throws IllegalArgumentException when arguments are outside of ranges
      */
     boolean addServiceICMP(String serviceId, int retries, int timeout, int interval, int downTimeInterval, int downTimeDuration);
 
@@ -69,7 +70,10 @@ public interface Provisioner {
      * @param interval <code>int</code> the standard polling interval in milliseconds 
      * @param downTimeInterval <code>int</code> the downtime polling interval in milliseconds(downtime model)
      * @param downTimeDuration <code>int</> the amount of time in milliseconds the downtime polling interval is in effect
-     * @return Always returns true.  Check for XML-RPC exception.
+     * @param port <code>int</code> the port to poll for DNS service
+     * @param hostname <code>String</code> the hostname to resolve with the DNS service 
+     * @return Always returns true.  
+     * @throws IllegalArgumentException when arguments are outside of ranges
      */
     boolean addServiceDNS(String serviceId, int retries, int timeout, int interval, int downTimeInterval, int downTimeDuration, int port, String hostname);
 
@@ -87,7 +91,10 @@ public interface Provisioner {
      * @param interval <code>int</code> the standard polling interval in milliseconds 
      * @param downTimeInterval <code>int</code> the downtime polling interval in milliseconds(downtime model)
      * @param downTimeDuration <code>int</> the amount of time in milliseconds the downtime polling interval is in effect
-     * @return Always returns true.  Check for XML-RPC exception.
+     * @param port <code>int</code> the port to attempt to connect to determine if the service is accepting connections
+     * @param contentCheck <code>String</code> a string to check for in the returned banner of the TCP service or "" if no check is desired.
+     * @return Always returns true.  
+     * @throws IllegalArgumentException when arguments are outside of ranges
      */
     boolean addServiceTCP(String serviceId, int retries, int timeout, int interval, int downTimeInterval, int downTimeDuration, int port, String contentCheck);
 
@@ -105,7 +112,19 @@ public interface Provisioner {
      * @param interval <code>int</code> the standard polling interval in milliseconds 
      * @param downTimeInterval <code>int</code> the downtime polling interval in milliseconds(downtime model)
      * @param downTimeDuration <code>int</> the amount of time in milliseconds the downtime polling interval is in effect
-     * @return Always returns true.  Check for XML-RPC exception.
+     * @param port <code>int</code> the port to attempt to connect to determine if the service is accepting connections
+     * @param responseCode <code>String</code> a set of responseCodes that are considered valid.  
+     *        This string should consist of one or more ranges ( [range],[range],...) where a range is either a single number or
+     *        a pair of number separated by a - ( 200,202-300 ).  If the default range check is desire this can be ""
+     *        The default range check is defined by the HttpMonitor plugin and is currently 100-499 for / or 100-399 for other URLs
+     * @param contentCheck <code>String</code> a string to check for in the returned web page.  This string is either an exact string 
+     *        to search for in the page or a regular expression.  To indicate a regular expression prefix the string with a '~' character
+     * @param url <code>String</code> the path portion of the URL to check on the server
+     * @param user <code>String</code> the user to use with basic authentication or "" if no authentication required
+     * @param passwd <code>String</code> the password to use with the user parameter with doing authentication
+     * @param agent <code>String</code> the String to use for the User agent field when communicating with the server
+     * @return Always returns true. 
+     * @throws IllegalArgumentException when arguments are outside of ranges
      */
     boolean addServiceHTTP(String serviceId, int retries, int timeout, int interval, int downTimeInterval, int downTimeDuration, int port, String responseCode, String contentCheck, String url, String user, String passwd, String agent) throws MalformedURLException;
 
@@ -123,7 +142,19 @@ public interface Provisioner {
      * @param interval <code>int</code> the standard polling interval in milliseconds 
      * @param downTimeInterval <code>int</code> the downtime polling interval in milliseconds(downtime model)
      * @param downTimeDuration <code>int</> the amount of time in milliseconds the downtime polling interval is in effect
-     * @return Always returns true.  Check for XML-RPC exception.
+     * @param port <code>int</code> the port to attempt to connect to determine if the service is accepting connections
+     * @param responseCode <code>String</code> a set of responseCodes that are considered valid.  
+     *        This string should consist of one or more ranges ( [range],[range],...) where a range is either a single number or
+     *        a pair of number separated by a - ( 200,202-300 ).  If the default range check is desire this can be ""
+     *        The default range check is defined by the HttpMonitor plugin and is currently 100-499 for / or 100-399 for other URLs
+     * @param contentCheck <code>String</code> a string to check for in the returned web page.  This string is either an exact string 
+     *        to search for in the page or a regular expression.  To indicate a regular expression prefix the string with a '~' character
+     * @param url <code>String</code> the path portion of the URL to check on the server
+     * @param user <code>String</code> the user to use with basic authentication or "" if no authentication required
+     * @param passwd <code>String</code> the password to use with the user parameter with doing authentication
+     * @param agent <code>String</code> the String to use for the User agent field when communicating with the server
+     * @return Always returns true. 
+     * @throws IllegalArgumentException when arguments are outside of ranges
      */
     boolean addServiceHTTPS(String serviceId, int retries, int timeout, int interval, int downTimeInterval, int downTimeDuration, int port, String responseCode, String contentCheck, String url, String user, String passwd, String agent) throws MalformedURLException;
 
@@ -140,8 +171,13 @@ public interface Provisioner {
      * @param timeout <code>int</code> time in milliseconds to wait during each <code>retries</code>
      * @param interval <code>int</code> the standard polling interval in milliseconds 
      * @param downTimeInterval <code>int</code> the downtime polling interval in milliseconds(downtime model)
-     * @param downTimeDuration <code>int</> the amount of time in milliseconds the downtime polling interval is in effect
-     * @return Always returns true.  Check for XML-RPC exception.
+     * @param downTimeDuration <code>int</code> the amount of time in milliseconds the downtime polling interval is in effect
+     * @param username <code>String</code> the user to pass to the JDBC connection
+     * @param password <code>String</code> the password to pass to the JDBC connection
+     * @param driver <code>String</code> the fully qualified name of the JDBC Driver class
+     * @param url <code>String</code> the url used to make a JDBC connection to the database
+     * @return Always returns true. 
+     * @throws IllegalArgumentException when arguments are outside of ranges
      */
     boolean addServiceDatabase(String serviceId, int retries, int timeout, int interval, int downTimeInterval, int downTimeDuration, String username, String password, String driver, String url);
 
@@ -153,7 +189,8 @@ public interface Provisioner {
      * 
      * @param pkName Name of the polling package i.e. "default" or custom package "FastHTTP"
      * @param serviceId Name of the service, i.e. "ICMP" or a custom service "FastHTTP"
-     * @return Always returns true.  Check for XML-RPC exception.
+     * @return Always returns true. 
+     * @throws IllegalArgumentException when arguments are outside of ranges
      */
     Map getServiceConfiguration(String pkName, String serviceId);    
 
