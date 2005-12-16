36f72534ce1e5a7f14cb28c3b7c22b0868d40f81
Matt Brozowski <brozow@users.sourceforge.net>
2005-12-16 22:02:27 +0000

    Add tests to services modules.  It requires you to set MAVEN_OPTS=-Xmx512m before running mvn.  (A smaller number my be ok but this one works)Try running this way (from the mavenize directory):	./mavenize.sh reorg; cd tmp; mvn -Dmaven.test.failure.ignore=true surefire-report:reportThen browse services/target/site/surefire-reports.html

--- src/services/org/opennms/netmgt/trapd/TrapdTest.java
+++ src/services/org/opennms/netmgt/trapd/TrapdTest.java
@@ -6,30 +6,21 @@ package org.opennms.netmgt.trapd;
 
 import java.io.Reader;
 import java.io.StringReader;
+import java.net.InetAddress;
 
 import org.opennms.netmgt.config.DatabaseConnectionFactory;
-import org.opennms.netmgt.config.TrapdConfig;
 import org.opennms.netmgt.config.TrapdConfigFactory;
 import org.opennms.netmgt.mock.EventAnticipator;
 import org.opennms.netmgt.mock.OpenNMSTestCase;
+import org.opennms.netmgt.snmp.SnmpObjId;
+import org.opennms.netmgt.snmp.SnmpUtils;
+import org.opennms.netmgt.snmp.SnmpV1TrapBuilder;
 import org.opennms.netmgt.xml.event.Event;
 import org.opennms.netmgt.xml.event.Logmsg;
-import org.snmp4j.CommunityTarget;
-import org.snmp4j.PDU;
-import org.snmp4j.PDUv1;
-import org.snmp4j.Snmp;
-import org.snmp4j.Target;
-import org.snmp4j.TransportMapping;
-import org.snmp4j.mp.SnmpConstants;
-import org.snmp4j.smi.IpAddress;
-import org.snmp4j.smi.OID;
-import org.snmp4j.smi.UdpAddress;
-import org.snmp4j.transport.DefaultUdpTransportMapping;
 
 public class TrapdTest extends OpenNMSTestCase {
-    private static String m_port = "1162";
+    private static int m_port = 1162;
 	private static Trapd m_trapd = new Trapd();
-    private static TrapdConfig m_config;
     private static String TRAPD_CONFIG = "<?xml version=\"1.0\"?>\n" + 
             "<trapd-configuration snmp-trap-port=\""+m_port+"\" new-suspect-on-trap=\"true\"/>\n" + 
             "\n";
@@ -52,33 +43,22 @@ public class TrapdTest extends OpenNMSTestCase {
 	}
 
 	public void testSnmpV1TrapSend() throws Exception {
-
-
-        UdpAddress address = new UdpAddress(myLocalHost()+"/"+m_port);
-        TransportMapping transport = new DefaultUdpTransportMapping();
+		String localhost = myLocalHost();
+		InetAddress localAddr = InetAddress.getByName(myLocalHost());
         
-        Target target = new CommunityTarget();
-        target.setAddress(address);
-        target.setRetries(1);
-        target.setTimeout(800);
-        target.setVersion(SnmpConstants.version1);
+        SnmpV1TrapBuilder pdu = SnmpUtils.getV1TrapBuilder();
+        pdu.setEnterprise(SnmpObjId.get(".1.3.6.1.4.1.5813"));
+        pdu.setGeneric(1);
+        pdu.setSpecific(0);
+        pdu.setTimeStamp(System.currentTimeMillis());
+        pdu.setAgentAddress(localAddr);
         
-        Snmp snmp = new Snmp(transport);
-        //snmp.listen();
 
-        PDUv1 trapPdu = new PDUv1();
-        trapPdu.setType(PDU.V1TRAP);
-        OID eOID = new OID(".1.3.6.1.4.1.5813");
-        trapPdu.setEnterprise(eOID);
-        trapPdu.setGenericTrap(1);
-        trapPdu.setSpecificTrap(0);
-        trapPdu.setAgentAddress(new IpAddress(address.getInetAddress()));
-        System.err.println(trapPdu.getBERLength());
         
         Event e = new Event();
         e.setUei("uei.opennms.org/default/trap");
         e.setSource("trapd");
-        e.setInterface(((UdpAddress)address).toString());
+        e.setInterface(localhost);
         Logmsg logmsg = new Logmsg();
         logmsg.setDest("logndisplay");
         e.setLogmsg(logmsg);
@@ -86,10 +66,10 @@ public class TrapdTest extends OpenNMSTestCase {
         EventAnticipator ea = new EventAnticipator();
         ea.anticipateEvent(e);
 
-        snmp.send(trapPdu, target);
-        snmp.send(trapPdu, target);
-        snmp.send(trapPdu, target);
-        snmp.send(trapPdu, target);
+        pdu.send(localhost, m_port, "public");
+        pdu.send(localhost, m_port, "public");
+        pdu.send(localhost, m_port, "public");
+        pdu.send(localhost, m_port, "public");
         
         assertEquals(1, ea.waitForAnticipated(1000).size());
         Thread.sleep(2000);
