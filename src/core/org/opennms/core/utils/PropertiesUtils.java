e15e739e69fc7efbde14eda905513709b1c344fd
Matt Brozowski <brozow@users.sourceforge.net>
2006-01-16 22:32:56 +0000

    add an interface so the '${..}' substitions can be used on more than just properties

--- src/core/org/opennms/core/utils/PropertiesUtils.java
+++ src/core/org/opennms/core/utils/PropertiesUtils.java
@@ -36,6 +36,20 @@ import java.util.List;
 import java.util.Properties;
 
 public class PropertiesUtils {
+	
+	public static interface SymbolTable {
+		public String getSymbolValue(String symbol);
+	}
+	
+	private static class PropertyBasedSymbolTable implements SymbolTable {
+		Properties m_properties;
+		PropertyBasedSymbolTable(Properties properties) {
+			m_properties = properties;
+		}
+		public String getSymbolValue(String symbol) {
+			return m_properties.getProperty(symbol);
+		}
+	}
     
     /**
      * This recursively substitutes occurrences ${property.name} in initialString with the value of 
@@ -46,10 +60,14 @@ public class PropertiesUtils {
      * @return The string with appropriate substitutions made.
      */
     public static String substitute(String initialString, Properties properties) {
-        return substitute(initialString, properties, new ArrayList());
+        return substitute(initialString, new PropertyBasedSymbolTable(properties));
     }
 
-    private static String substitute(String initialString, Properties properties, List list) {
+    public static String substitute(String initialString, SymbolTable symbols) {
+        return substitute(initialString, symbols, new ArrayList());
+    }
+
+    private static String substitute(String initialString, SymbolTable symTable, List list) {
         if (initialString == null) return null;
         
         StringBuffer result = new StringBuffer(initialString);
@@ -60,13 +78,13 @@ public class PropertiesUtils {
             int beginIndex = result.indexOf("${", startIndex);
             int endIndex = (beginIndex < 0 ? -1 : result.indexOf("}", beginIndex+"${".length()));
             if (endIndex >= 0) {
-                String propName = result.substring(beginIndex+"${".length(), endIndex);
-                if (list.contains(propName))
-                    throw new IllegalStateException("recursive loop involving property ${"+propName+"}");
-                String propValue = properties.getProperty(propName);
-                if (propValue != null) {
-                    list.add(propName);
-                    String substVal = substitute(propValue, properties, list);
+                String symbol = result.substring(beginIndex+"${".length(), endIndex);
+                if (list.contains(symbol))
+                    throw new IllegalStateException("recursive loop involving symbol ${"+symbol+"}");
+                String symbolVal = symTable.getSymbolValue(symbol);
+                if (symbolVal != null) {
+                    list.add(symbol);
+                    String substVal = substitute(symbolVal, symTable, list);
                     list.remove(list.size()-1);
                     result.replace(beginIndex, endIndex+1, substVal);
                     startIndex = beginIndex + substVal.length();
