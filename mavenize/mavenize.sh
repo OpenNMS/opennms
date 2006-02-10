7da7b49b9d9b5280031b0f7a57cd63340bb58cc3
DJ Gregor <djgregor@users.sourceforge.net>
2006-02-10 23:18:53 +0000

    make it perty and do some error checking

--- mavenize/mavenize.sh
+++ mavenize/mavenize.sh
@@ -1,10 +1,11 @@
+#!/bin/sh -
 
 # Builde the mavenizer
-mvn -DdescriptorId=jar-with-dependencies assembly:assembly
+mvn -DdescriptorId=jar-with-dependencies -Dmaven.test.skip=true \
+	assembly:assembly || exit 1
 
 # Run the mavenizer
-java -cp target/opennms-mavenizer-1.0-SNAPSHOT-jar-with-dependencies.jar -Dopennms.dir=.. org.opennms.mavenize.Mavenize src/test/resources/opennmsMavenizeSpec.xml
-
-
-
-
+java -cp target/opennms-mavenizer-1.0-SNAPSHOT-jar-with-dependencies.jar \
+	-Dopennms.dir=.. \
+	org.opennms.mavenize.Mavenize \
+	src/test/resources/opennmsMavenizeSpec.xml || exit 1
