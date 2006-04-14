65c47b24f3574e3bdd084c2d58aded380cf67da9
Mike Huot <mhuot@users.sourceforge.net>
2006-04-14 23:16:47 +0000

    OnmsUserNotification OpenNMSification

--- opennms-model/src/main/java/org/opennms/netmgt/model/OnmsUserNotification.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/OnmsUserNotification.java
@@ -11,41 +11,41 @@ import org.springframework.core.style.ToStringCreator;
  *         table="usersnotified"
  *     
 */
-public class OnmsUserNotification implements Serializable {
+public class OnmsUserNotification extends OnmsEntity implements Serializable {
 
     /**
      * 
      */
     private static final long serialVersionUID = -1750912427062821742L;
     
-    private Integer id;
+    private Integer m_id;
 
     /** identifier field */
-    private String userid;
+    private String m_userId;
 
     /** identifier field */
-    private Date notifytime;
+    private Date m_notifyTime;
 
     /** identifier field */
-    private String media;
+    private String m_media;
 
     /** identifier field */
-    private String contactinfo;
+    private String m_contactInfo;
 
     /** identifier field */
-    private String autonotify;
+    private String m_autoNotify;
 
     /** persistent field */
-    private OnmsNotification notification;
+    private OnmsNotification m_notification;
 
     /** full constructor */
-    public OnmsUserNotification(String userid, Date notifytime, String media, String contactinfo, String autonotify, org.opennms.netmgt.model.OnmsNotification notification) {
-        this.userid = userid;
-        this.notifytime = notifytime;
-        this.media = media;
-        this.contactinfo = contactinfo;
-        this.autonotify = autonotify;
-        this.notification = notification;
+    public OnmsUserNotification(String userId, Date notifyTime, String media, String contactInfo, String autoNotify, OnmsNotification notification) {
+        m_userId = userId;
+        m_notifyTime = notifyTime;
+        m_media = media;
+        m_contactInfo = contactInfo;
+        m_autoNotify = autoNotify;
+        m_notification = notification;
     }
 
     /** default constructor */
@@ -60,39 +60,39 @@ public class OnmsUserNotification implements Serializable {
      *         
      */
     public Integer getId() {
-        return id;
+        return m_id;
     }
     
     public void setId(Integer id) {
-        this.id = id;
+        m_id = id;
     }
 
     /** 
      *                @hibernate.property
-     *                 column="userid"
+     *                 column="userId"
      *                 length="256"
      *             
      */
-    public String getUserid() {
-        return this.userid;
+    public String getUserId() {
+        return m_userId;
     }
 
-    public void setUserid(String userid) {
-        this.userid = userid;
+    public void setUserId(String userId) {
+        m_userId = userId;
     }
 
     /** 
      *                @hibernate.property
-     *                 column="notifytime"
+     *                 column="notifyTime"
      *                 length="8"
      *             
      */
-    public Date getNotifytime() {
-        return this.notifytime;
+    public Date getNotifyTime() {
+        return m_notifyTime;
     }
 
-    public void setNotifytime(Date notifytime) {
-        this.notifytime = notifytime;
+    public void setNotifyTime(Date notifyTime) {
+        m_notifyTime = notifyTime;
     }
 
     /** 
@@ -102,39 +102,39 @@ public class OnmsUserNotification implements Serializable {
      *             
      */
     public String getMedia() {
-        return this.media;
+        return m_media;
     }
 
     public void setMedia(String media) {
-        this.media = media;
+        m_media = media;
     }
 
     /** 
      *                @hibernate.property
-     *                 column="contactinfo"
+     *                 column="contactInfo"
      *                 length="64"
      *             
      */
-    public String getContactinfo() {
-        return this.contactinfo;
+    public String getContactInfo() {
+        return m_contactInfo;
     }
 
-    public void setContactinfo(String contactinfo) {
-        this.contactinfo = contactinfo;
+    public void setContactInfo(String contactInfo) {
+        m_contactInfo = contactInfo;
     }
 
     /** 
      *                @hibernate.property
-     *                 column="autonotify"
+     *                 column="autoNotify"
      *                 length="1"
      *             
      */
-    public String getAutonotify() {
-        return this.autonotify;
+    public String getAutoNotify() {
+        return m_autoNotify;
     }
 
-    public void setAutonotify(String autonotify) {
-        this.autonotify = autonotify;
+    public void setAutoNotify(String autoNotify) {
+        m_autoNotify = autoNotify;
     }
 
     /** 
@@ -143,22 +143,28 @@ public class OnmsUserNotification implements Serializable {
      *            @hibernate.column name="notifyid"         
      *         
      */
-    public org.opennms.netmgt.model.OnmsNotification getNotification() {
-        return this.notification;
+    public OnmsNotification getNotification() {
+        return m_notification;
     }
 
-    public void setNotification(org.opennms.netmgt.model.OnmsNotification notification) {
-        this.notification = notification;
+    public void setNotification(OnmsNotification notification) {
+        m_notification = notification;
     }
 
     public String toString() {
         return new ToStringCreator(this)
-            .append("userid", getUserid())
-            .append("notifytime", getNotifytime())
+            .append("userId", getUserId())
+            .append("notifyTime", getNotifyTime())
             .append("media", getMedia())
-            .append("contactinfo", getContactinfo())
-            .append("autonotify", getAutonotify())
+            .append("contactInfo", getContactInfo())
+            .append("autoNotify", getAutoNotify())
             .toString();
     }
+
+	public void visit(EntityVisitor visitor) {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("visitor method not implemented");
+
+	}
 
 }
