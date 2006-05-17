1e729c510983d1e4cc9d662a213f1bf68273b3dc
David Hustace <dhustace@users.sourceforge.net>
2006-05-17 08:12:52 +0000

    fixed find ip interfaces by service type query in JDBC DAO.

--- opennms-model/src/main/java/org/opennms/netmgt/dao/jdbc/ipif/FindByServiceType.java
+++ opennms-model/src/main/java/org/opennms/netmgt/dao/jdbc/ipif/FindByServiceType.java
@@ -9,7 +9,10 @@ import org.springframework.jdbc.core.SqlParameter;
 public class FindByServiceType extends IpInterfaceMappingQuery {
 
     public FindByServiceType(DataSource ds) {
-        super(ds, "FROM ipInterface, ifservices, service where service.servicename = ?");
+        super(ds, "FROM ipInterface JOIN ifServices ON (ifServices.nodeid = ipInterface.nodeid " +
+                  "and ifServices.ipaddr = ipInterface.ipaddr) " +
+                  "JOIN service ON (service.serviceid = ifServices.serviceid) " +
+                  "where service.servicename = ?");
         declareParameter(new SqlParameter("serviceName", Types.VARCHAR));
         compile();
     }
