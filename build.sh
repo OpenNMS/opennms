103a296530913a0da50751e9f44e48de04539355
Matt Brozowski <brozow@users.sourceforge.net>
2006-06-07 23:49:55 +0000

    enables tests but ignore failures

--- build.sh
+++ build.sh
@@ -51,4 +51,4 @@ fi
 #    -jar devlib/ant/lib/ant-launcher.jar "$@"
 
 export M2_OPTS=-Xmx256m
-devlib/maven-2.0.4/bin/mvn -Dmaven.test.skip=true "$@"
+devlib/maven-2.0.4/bin/mvn -Dmaven.test.failure.ignore=true "$@"
