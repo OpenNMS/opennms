f0d3f5e608a81680a6508f0894e6ed4172aad61c
Matt Brozowski <brozow@users.sourceforge.net>
2006-07-12 23:51:15 +0000

    Fix tests

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/NodeDaoTest.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/NodeDaoTest.java
@@ -37,7 +37,7 @@ import java.util.Set;
 import org.opennms.netmgt.model.OnmsDistPoller;
 import org.opennms.netmgt.model.OnmsNode;
 
-public class NodeDaoTest extends BaseDaoTestCase {
+public class NodeDaoTest extends AbstractDaoTestCase {
     
     public void testSave() {
         OnmsDistPoller distPoller = new OnmsDistPoller("localhost", "127.0.0.1");
