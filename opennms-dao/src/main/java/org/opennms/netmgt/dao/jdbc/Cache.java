af1739f99aa1dfa3e64d3dd4abbb2210aee99a7c
Matt Brozowski <brozow@users.sourceforge.net>
2006-07-13 00:02:59 +0000

    don't forget to register teh AgentFactory

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/jdbc/Cache.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/jdbc/Cache.java
@@ -36,6 +36,7 @@ import java.util.Map;
 
 import javax.sql.DataSource;
 
+import org.opennms.netmgt.dao.jdbc.agent.AgentFactory;
 import org.opennms.netmgt.dao.jdbc.alarm.AlarmFactory;
 import org.opennms.netmgt.dao.jdbc.asset.AssetRecordFactory;
 import org.opennms.netmgt.dao.jdbc.category.CategoryFactory;
@@ -122,6 +123,7 @@ public class Cache {
      * @deprecated
      */
 	public static void registerFactories(DataSource dataSource) {
+		AgentFactory.register(dataSource);
 		AssetRecordFactory.register(dataSource);
 		CategoryFactory.register(dataSource);
 		DistPollerFactory.register(dataSource);
