744aa6d4e2be53640a9a82b069ddae5c046df968
DJ Gregor <djgregor@users.sourceforge.net>
2006-07-24 14:21:15 +0000

    Move opennms-webapp-ng to opennms-webapp

--- opennms-webapp-ng/src/main/java/org/opennms/web/services/PollerService.java
+++
@@ -1,9 +0,0 @@
-package org.opennms.web.services;
-
-import org.opennms.netmgt.model.OnmsMonitoredService;
-
-public interface PollerService {
-
-	void poll(OnmsMonitoredService monSvc, int demandPollId);
-
-}
