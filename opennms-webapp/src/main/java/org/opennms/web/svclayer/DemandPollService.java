329ee5c3f07768896a519fa62158a3a2e6c1964e
DJ Gregor <djgregor@users.sourceforge.net>
2006-07-24 16:07:25 +0000

    move opennms-webapp-ng to opennms-webapp

--- opennms-webapp/src/main/java/org/opennms/web/svclayer/DemandPollService.java
+++ opennms-webapp/src/main/java/org/opennms/web/svclayer/DemandPollService.java
@@ -1,11 +1,12 @@
 package org.opennms.web.svclayer;
 
-import org.opennms.netmgt.model.PollResult;
+import org.opennms.netmgt.model.DemandPoll;
+import org.opennms.netmgt.utils.EventProxyException;
 
 public interface DemandPollService {
 	
-	 PollResult pollMonitoredService(int nodeid, String ipAddr, int ifIndex, int serviceId);
+	 DemandPoll pollMonitoredService(int nodeid, String ipAddr, int ifIndex, int serviceId) throws EventProxyException;
 	 
-	 PollResult getUpdatedResults(int resultId);
+	 DemandPoll getUpdatedResults(int resultId);
 
 }
