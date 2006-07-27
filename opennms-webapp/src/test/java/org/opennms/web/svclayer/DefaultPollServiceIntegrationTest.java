2e07b9f576d81ca63941d3a83a8a83a587ccde39
David Hustace <dhustace@users.sourceforge.net>
2006-07-27 05:55:14 +0000

    Integration Test for poll now, still needs help with actual TcpEvent Proxy and if the pollresults.

---
+++ opennms-webapp/src/test/java/org/opennms/web/svclayer/DefaultPollServiceIntegrationTest.java
@@ -0,0 +1,37 @@
+package org.opennms.web.svclayer;
+
+import static org.easymock.EasyMock.createMock;
+
+import org.opennms.netmgt.model.DemandPoll;
+import org.opennms.web.services.PollerService;
+import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
+import org.springframework.transaction.annotation.Transactional;
+
+public class DefaultPollServiceIntegrationTest extends AbstractTransactionalDataSourceSpringContextTests{
+
+	private DemandPollService m_demandPollService;
+	
+	@Override
+	protected String[] getConfigLocations() {
+		return new String[] {
+				"org/opennms/web/svclayer/applicationContextOutage-web.xml" };
+	}
+
+	public DemandPollService getDemandPollService() {
+		return m_demandPollService;
+	}
+
+	public void setDemandPollService(DemandPollService pollService) {
+		m_demandPollService = pollService;
+	}
+	
+	@Transactional(readOnly=false)
+	public void testPollMonitoredService() {
+		PollerService api = createMock(PollerService.class);
+		((DefaultDemandPollService)m_demandPollService).setPollerAPI(api);
+		
+		DemandPoll poll = m_demandPollService.pollMonitoredService(1, "192.168.2.100", 1, 1);
+		assertNotNull("DemandPoll should not be null", poll);
+		assertTrue("Polled service addr doesn't match...", poll.getId() >= 1);
+	}
+}
