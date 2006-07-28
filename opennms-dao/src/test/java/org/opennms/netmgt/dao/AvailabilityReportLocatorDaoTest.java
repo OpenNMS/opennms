501949bfcf7a5dccbdf19ede1eb92b6408501ab8
Jonathan Sartin <jsartin@users.sourceforge.net>
2006-07-28 05:11:43 +0000

    more availability report locator changes

---
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/AvailabilityReportLocatorDaoTest.java
@@ -0,0 +1,34 @@
+package org.opennms.netmgt.dao;
+
+import java.util.Date;
+
+import org.opennms.netmgt.model.AvailabilityReportLocator;
+
+public class AvailabilityReportLocatorDaoTest extends BaseDaoTestCase {
+
+	private AvailabilityReportLocatorDao m_availabilityReportLocatorDao;
+
+	public void setAvailabilityReportLocatorDao(AvailabilityReportLocatorDao availabilityReportLocatorDao) {
+		m_availabilityReportLocatorDao = availabilityReportLocatorDao;
+	}
+	
+	public void testFindAll() {
+		
+		AvailabilityReportLocator locator = new AvailabilityReportLocator();
+		locator.setAvailable(true);
+		locator.setCategory("cat1");
+		locator.setDate(new Date());
+		locator.setFormat("HTML");
+		locator.setType("Random String");
+		
+		m_availabilityReportLocatorDao.save(locator);
+		
+		
+		AvailabilityReportLocator retrieved = m_availabilityReportLocatorDao.get(locator.getId());
+		
+		assertEquals(retrieved.getId(), locator.getId());
+		assertEquals(retrieved.getAvailable(), locator.getAvailable());
+		assertEquals(retrieved.getCategory(), locator.getCategory());
+	}
+	
+}
