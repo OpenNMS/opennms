4d1112d07eaaa234977350f87f2c723a206b0437
Matt Brozowski <brozow@users.sourceforge.net>
2006-07-28 21:50:15 +0000

    make current outages actually return 'current' outages

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/jdbc/outage/FindCurrentOutages.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/jdbc/outage/FindCurrentOutages.java
@@ -5,12 +5,12 @@ import javax.sql.DataSource;
 public class FindCurrentOutages extends OutageMappingQuery {
 
     public FindCurrentOutages(DataSource ds) {
-        super(ds,"FROM outages as outages, ifservices as ifservices WHERE outages.nodeID = ifservices.nodeID and outages.ipAddr = ifservices.ipAddr and outages.serviceID = ifservices.serviceID");
+        super(ds,"FROM outages as outages, ifservices as ifservices WHERE outages.nodeID = ifservices.nodeID and outages.ipAddr = ifservices.ipAddr and outages.serviceID = ifservices.serviceID and outages.ifRegainedService is null ");
         compile();
     }
     
     public FindCurrentOutages(DataSource ds, Integer offset, Integer limit) {
-        super(ds,"FROM outages as outages, ifservices as ifservices WHERE outages.nodeID = ifservices.nodeID and outages.ipAddr = ifservices.ipAddr and outages.serviceID = ifservices.serviceID OFFSET " + offset + " LIMIT " + limit);
+        super(ds,"FROM outages as outages, ifservices as ifservices WHERE outages.nodeID = ifservices.nodeID and outages.ipAddr = ifservices.ipAddr and outages.serviceID = ifservices.serviceID  and outages.ifRegainedService is null OFFSET " + offset + " LIMIT " + limit);
         compile();
     }
 
