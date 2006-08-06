fb274666f30db76a43652d888ffc8526a6dfca0d
Mike Huot <mhuot@users.sourceforge.net>
2006-08-06 17:53:00 +0000

    Preliminary ibatis work with pathoutage table

---
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/ibatis/pathoutage/PathOutageDomainObject.java
@@ -0,0 +1,44 @@
+package org.opennms.netmgt.dao.ibatis.pathoutage;
+
+public class PathOutageDomainObject {
+	private int nodeId;
+	private String criticalPathIp;
+	private String criticalPathServiceName;
+	
+	//Operations
+	public String toString() {
+		StringBuffer result = new StringBuffer(50);
+		result.append("pathOutage { nodeID: ");
+		result.append(nodeId);
+		result.append(", criticalPathIp: ");
+		result.append(criticalPathIp);
+		result.append(", criticalPathServiceName: ");
+		result.append(criticalPathServiceName);
+		result.append(" }");
+		return result.toString();
+	}
+
+	public String getCriticalPathIp() {
+		return criticalPathIp;
+	}
+
+	public void setCriticalPathIp(String criticalPathIp) {
+		this.criticalPathIp = criticalPathIp;
+	}
+
+	public String getCriticalPathServiceName() {
+		return criticalPathServiceName;
+	}
+
+	public void setCriticalPathServiceName(String criticalPathServiceName) {
+		this.criticalPathServiceName = criticalPathServiceName;
+	}
+
+	public int getNodeId() {
+		return nodeId;
+	}
+
+	public void setNodeId(int nodeId) {
+		this.nodeId = nodeId;
+	}
+}
