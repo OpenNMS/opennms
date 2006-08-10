ef679f616a0f4fb95a57395790e5f4c1d0afa3ae
David Hustace <dhustace@users.sourceforge.net>
2006-08-10 21:25:33 +0000

    A bit of maintenance: added Copyright and GPL info, orgainzied imports, got rid of some "unused" warninings in the C3P0ConnectionFactory.

--- opennms-webapp/src/test/java/org/opennms/web/svclayer/catstatus/support/DefaultCategoryStatusServiceTest.java
+++ opennms-webapp/src/test/java/org/opennms/web/svclayer/catstatus/support/DefaultCategoryStatusServiceTest.java
@@ -1,11 +1,48 @@
+//
+// This file is part of the OpenNMS(R) Application.
+//
+// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included code and modified
+// code that was published under the GNU General Public License. Copyrights for modified 
+// and included code are below.
+//
+// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// For more information contact:
+// OpenNMS Licensing       <license@opennms.org>
+//     http://www.opennms.org/
+//     http://www.opennms.com/
+//
+
 package org.opennms.web.svclayer.catstatus.support;
 
-import junit.framework.TestCase;
+import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+
+import junit.framework.TestCase;
 
 import org.opennms.netmgt.config.categories.Category;
 import org.opennms.netmgt.config.viewsdisplay.Section;
@@ -22,12 +59,6 @@ import org.opennms.web.svclayer.catstatus.dao.ViewDisplayDao;
 import org.opennms.web.svclayer.catstatus.model.StatusCategory;
 import org.opennms.web.svclayer.catstatus.model.StatusNode;
 import org.opennms.web.svclayer.catstatus.model.StatusSection;
-
-import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.verify;
-import static org.easymock.EasyMock.isA;
 
 
 public class DefaultCategoryStatusServiceTest extends TestCase {
