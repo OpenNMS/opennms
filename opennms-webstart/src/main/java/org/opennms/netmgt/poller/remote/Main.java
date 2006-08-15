b1a8be5ae0204c4c83e9d64d26bfa54f31849ef7
Matt Brozowski <brozow@users.sourceforge.net>
2006-08-15 21:52:05 +0000

    add some webstart scaffolding

---
+++ opennms-webstart/src/main/java/org/opennms/netmgt/poller/remote/Main.java
@@ -0,0 +1,23 @@
+package org.opennms.netmgt.poller.remote;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class Main {
+		
+	public static void main(String[] args) {
+		
+		String[] configs = {
+				"classpath:/META-INF/opennms/applicationContext-ws-gui.xml",
+				"classpath:/META-INF/opennms/applicationContext-ws-svclayer.xml"
+		};
+		
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(configs);
+        PollerView gui = (PollerView) ctx.getBean("pollerView");
+        gui.showView();
+
+	}
+
+
+}
--- opennms-webstart/src/main/java/org/opennms/netmgt/poller/remote/SimplePollerView.java
+++ opennms-webstart/src/main/java/org/opennms/netmgt/poller/remote/SimplePollerView.java
@@ -1,6 +1,7 @@
-package org.opennms.webstart;
+package org.opennms.netmgt.poller.remote;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
@@ -8,32 +9,34 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 
-public class Main {
-	
-	JFrame frame;
-	
-	public static void main(String[] args) {
+public class SimplePollerView implements PollerView {
+
+	private Component m_contents;
+
+	public void showView() {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				createAndShowGUI();
 			}
 		});
 	}
-
-	protected static void createAndShowGUI() {
+	
+	public void setContents(Component contents) {
+		m_contents = contents;
+	}
+	
+	protected void createAndShowGUI() {
 		JFrame.setDefaultLookAndFeelDecorated(true);
 		
 		JFrame frame = new JFrame("OpenNMS Remote Poller");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JLabel helloWorld = new JLabel("Welcome to OpenNMS's Remote Poller");
-		helloWorld.setPreferredSize(new Dimension(300, 200));
-		helloWorld.setHorizontalAlignment(SwingConstants.CENTER);
-		frame.getContentPane().add(helloWorld, BorderLayout.CENTER);
+		frame.getContentPane().add(m_contents, BorderLayout.CENTER);
 		
 		frame.pack();
 		frame.setVisible(true);
 		
 	}
+
 
 }
