8e654de1b716e7922e52143e560b571dae27388c
DJ Gregor <djgregor@users.sourceforge.net>
2006-08-24 08:01:31 +0000

    Format code

--- opennms-services/src/test/java/org/opennms/netmgt/collectd/SnmpCollectorTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/collectd/SnmpCollectorTest.java
@@ -3,8 +3,6 @@ package org.opennms.netmgt.collectd;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.Collection;
@@ -33,17 +31,19 @@ import org.opennms.test.ConfigurationTestUtils;
 import junit.framework.TestCase;
 
 public class SnmpCollectorTest extends TestCase {
-    private static final String s_rrdConfig = "org.opennms.rrd.strategyClass=org.opennms.netmgt.rrd.jrobin.JRobinRrdStrategy";
-    
-	private SnmpCollector m_snmpCollector;
-	private MockSnmpAgent m_agent;
-	
+    private static final String s_rrdConfig ="org.opennms.rrd.strategyClass=org.opennms.netmgt.rrd.jrobin.JRobinRrdStrategy";
+
+    private SnmpCollector m_snmpCollector;
+
+    private MockSnmpAgent m_agent;
+
     @Override
-	protected void setUp() throws Exception {
-		super.setUp();
-        MockUtil.println("------------ Begin Test "+getName()+" --------------------------");
-		MockLogAppender.setupLogging();
-		
+    protected void setUp() throws Exception {
+        super.setUp();
+        MockUtil.println("------------ Begin Test " + getName()
+                + " --------------------------");
+        MockLogAppender.setupLogging();
+
         MockNetwork m_network = new MockNetwork();
         m_network.setCriticalService("ICMP");
         m_network.addNode(1, "Router");
@@ -52,56 +52,57 @@ public class SnmpCollectorTest extends TestCase {
 
         MockDatabase m_db = new MockDatabase();
         m_db.populate(m_network);
-        
+
         DataSourceFactory.setInstance(m_db);
     }
 
     @Override
-	public void runTest() throws Throwable {
-		super.runTest();
-		MockLogAppender.assertNoWarningsOrGreater();
-	}
-	
-	@Override
-	protected void tearDown() throws Exception {
-		if (m_agent != null) {
-			m_agent.shutDownAndWait();
-		}
-        MockUtil.println("------------ End Test "+getName()+" --------------------------");
+    public void runTest() throws Throwable {
+        super.runTest();
+        MockLogAppender.assertNoWarningsOrGreater();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        if (m_agent != null) {
+            m_agent.shutDownAndWait();
+        }
+        MockUtil.println("------------ End Test " + getName()
+                + " --------------------------");
         super.tearDown();
-	}
+    }
 
-	
-	public void testInstantiate() throws Exception {
-		new SnmpCollector();
-	}
-	
-	public void testInitialize() throws Exception {
-		initialize();
-	}
+    public void testInstantiate() throws Exception {
+        new SnmpCollector();
+    }
 
-	public void testCollect() throws Exception {
-		String svcName = "SNMP";
-		
-		String m_snmpConfig = "<?xml version=\"1.0\"?>\n"
-			+ "<snmp-config port=\"1691\" retry=\"3\" timeout=\"800\"\n"
-		    + "               read-community=\"public\"\n"
-		    + "               version=\"v1\">\n"
-		    + "</snmp-config>\n";
-		
+    public void testInitialize() throws Exception {
+        initialize();
+    }
+
+    public void testCollect() throws Exception {
+        String svcName = "SNMP";
+
+        String m_snmpConfig = "<?xml version=\"1.0\"?>\n"
+                + "<snmp-config port=\"1691\" retry=\"3\" timeout=\"800\"\n"
+                + "               read-community=\"public\"\n"
+                + "               version=\"v1\">\n" + "</snmp-config>\n";
+
         initializeAgent();
-		
-		Reader dataCollectionConfig = ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/config/datacollection-config.xml");
-		initialize(new StringReader(m_snmpConfig), dataCollectionConfig);
-		dataCollectionConfig.close();
 
-		OnmsNode node = new OnmsNode();
-		node.setId(new Integer(1));
-		node.setSysObjectId(".1.3.6.1.4.1.1588.2.1.1.1");
-		OnmsIpInterface iface = new OnmsIpInterface("127.0.0.1", node);
-		
+        Reader dataCollectionConfig = ConfigurationTestUtils.getReaderForResource(
+                                                                                  this,
+                                                                                  "/org/opennms/netmgt/config/datacollection-config.xml");
+        initialize(new StringReader(m_snmpConfig), dataCollectionConfig);
+        dataCollectionConfig.close();
+
+        OnmsNode node = new OnmsNode();
+        node.setId(new Integer(1));
+        node.setSysObjectId(".1.3.6.1.4.1.1588.2.1.1.1");
+        OnmsIpInterface iface = new OnmsIpInterface("127.0.0.1", node);
+
         Collection outageCalendars = new LinkedList();
-        
+
         Package pkg = new Package();
         Filter filter = new Filter();
         filter.setContent("IPADDR IPLIKE *.*.*.*");
@@ -109,37 +110,44 @@ public class SnmpCollectorTest extends TestCase {
         Service service = new Service();
         service.setName(svcName);
         pkg.addService(service);
-        
+
         CollectdPackage wpkg = new CollectdPackage(pkg, "foo", false);
-        CollectionSpecification spec = new CollectionSpecification(wpkg, svcName, outageCalendars, m_snmpCollector);
-        
+        CollectionSpecification spec = new CollectionSpecification(
+                                                                   wpkg,
+                                                                   svcName,
+                                                                   outageCalendars,
+                                                                   m_snmpCollector);
+
         CollectionAgent agent = new CollectionAgent(iface);
 
-        assertEquals("collection status", ServiceCollector.COLLECTION_SUCCEEDED, spec.collect(agent));
-	}
-	
-	public void testBrocadeCollect() throws Exception {
-		String svcName = "SNMP";
-		
-		String m_snmpConfig = "<?xml version=\"1.0\"?>\n"
-			+ "<snmp-config port=\"1691\" retry=\"3\" timeout=\"800\"\n"
-		    + "               read-community=\"public\"\n"
-		    + "               version=\"v1\">\n"
-		    + "</snmp-config>\n";
+        assertEquals("collection status",
+                     ServiceCollector.COLLECTION_SUCCEEDED,
+                     spec.collect(agent));
+    }
+
+    public void testBrocadeCollect() throws Exception {
+        String svcName = "SNMP";
+
+        String m_snmpConfig = "<?xml version=\"1.0\"?>\n"
+                + "<snmp-config port=\"1691\" retry=\"3\" timeout=\"800\"\n"
+                + "               read-community=\"public\"\n"
+                + "               version=\"v1\">\n" + "</snmp-config>\n";
 
         initializeAgent("target/test-classes/org/opennms/netmgt/snmp/brocadeTestData1.properties");
 
-		Reader dataCollectionConfig = ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/config/datacollection-brocade-config.xml");
-		initialize(new StringReader(m_snmpConfig), dataCollectionConfig);
-		dataCollectionConfig.close();
+        Reader dataCollectionConfig = ConfigurationTestUtils.getReaderForResource(
+                                                                                  this,
+                                                                                  "/org/opennms/netmgt/config/datacollection-brocade-config.xml");
+        initialize(new StringReader(m_snmpConfig), dataCollectionConfig);
+        dataCollectionConfig.close();
 
-		OnmsNode node = new OnmsNode();
-		node.setId(new Integer(1));
-		node.setSysObjectId(".1.3.6.1.4.1.1588.2.1.1.1");
-		OnmsIpInterface iface = new OnmsIpInterface("127.0.0.1", node);
-		
+        OnmsNode node = new OnmsNode();
+        node.setId(new Integer(1));
+        node.setSysObjectId(".1.3.6.1.4.1.1588.2.1.1.1");
+        OnmsIpInterface iface = new OnmsIpInterface("127.0.0.1", node);
+
         Collection outageCalendars = new LinkedList();
-        
+
         Package pkg = new Package();
         Filter filter = new Filter();
         filter.setContent("IPADDR IPLIKE *.*.*.*");
@@ -147,47 +155,60 @@ public class SnmpCollectorTest extends TestCase {
         Service service = new Service();
         service.setName(svcName);
         pkg.addService(service);
-        
+
         CollectdPackage wpkg = new CollectdPackage(pkg, "foo", false);
-        CollectionSpecification spec = new CollectionSpecification(wpkg, svcName, outageCalendars, m_snmpCollector);
-        
+        CollectionSpecification spec = new CollectionSpecification(
+                                                                   wpkg,
+                                                                   svcName,
+                                                                   outageCalendars,
+                                                                   m_snmpCollector);
+
         CollectionAgent agent = new CollectionAgent(iface);
 
-        assertEquals("collection status", ServiceCollector.COLLECTION_SUCCEEDED, spec.collect(agent));
-	}
+        assertEquals("collection status",
+                     ServiceCollector.COLLECTION_SUCCEEDED,
+                     spec.collect(agent));
+    }
 
-	
-	public void initialize(Reader snmpConfig, Reader dataCollectionConfig) throws MarshalException, ValidationException, IOException {
-		RrdConfig.loadProperties(new ByteArrayInputStream(s_rrdConfig.getBytes()));
+    public void initialize(Reader snmpConfig, Reader dataCollectionConfig)
+            throws MarshalException, ValidationException, IOException {
+        RrdConfig.loadProperties(new ByteArrayInputStream(
+                                                          s_rrdConfig.getBytes()));
 
-		SnmpPeerFactory.setInstance(new SnmpPeerFactory(snmpConfig));
-		DataCollectionConfigFactory.setInstance(new DataCollectionConfigFactory(dataCollectionConfig));
-		
-        Reader rdr = ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/config/test-database-schema.xml");
-        DatabaseSchemaConfigFactory.setInstance(new DatabaseSchemaConfigFactory(rdr));
+        SnmpPeerFactory.setInstance(new SnmpPeerFactory(snmpConfig));
+        DataCollectionConfigFactory.setInstance(new DataCollectionConfigFactory(
+                                                                                dataCollectionConfig));
+
+        Reader rdr = ConfigurationTestUtils.getReaderForResource(this,
+                                                                 "/org/opennms/netmgt/config/test-database-schema.xml");
+        DatabaseSchemaConfigFactory.setInstance(new DatabaseSchemaConfigFactory(
+                                                                                rdr));
         rdr.close();
-		
-		m_snmpCollector = new SnmpCollector();
-		m_snmpCollector.initialize(null); // no properties are passed
-	}
 
-	public void initialize() throws IOException, MarshalException, ValidationException {
-		Reader snmpConfig = ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/config/snmp-config.xml");
-		Reader dataCollectionConfig = ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/config/datacollection-config.xml");
-		
-		initialize(snmpConfig, dataCollectionConfig);
-		
-		snmpConfig.close();
-		dataCollectionConfig.close();
-	}
-    
+        m_snmpCollector = new SnmpCollector();
+        m_snmpCollector.initialize(null); // no properties are passed
+    }
+
+    public void initialize() throws IOException, MarshalException,
+            ValidationException {
+        Reader snmpConfig = ConfigurationTestUtils.getReaderForResource(this,
+                                                                        "/org/opennms/netmgt/config/snmp-config.xml");
+        Reader dataCollectionConfig = ConfigurationTestUtils.getReaderForResource(
+                                                                                  this,
+                                                                                  "/org/opennms/netmgt/config/datacollection-config.xml");
+
+        initialize(snmpConfig, dataCollectionConfig);
+
+        snmpConfig.close();
+        dataCollectionConfig.close();
+    }
+
     private void initializeAgent(String testData) throws InterruptedException {
-        m_agent = MockSnmpAgent.createAgentAndRun(
-				new File(testData),
-				"127.0.0.1/1691");
-	}
-    
+        m_agent = MockSnmpAgent.createAgentAndRun(new File(testData),
+                                                  "127.0.0.1/1691");
+    }
+
     private void initializeAgent() throws InterruptedException {
-    	initializeAgent("target/test-classes/org/opennms/netmgt/snmp/mock/loadSnmpDataTest.properties");
+        initializeAgent("target/test-classes/org/opennms/netmgt/snmp/mock/loadSnmpDataTest.properties");
     }
 }
