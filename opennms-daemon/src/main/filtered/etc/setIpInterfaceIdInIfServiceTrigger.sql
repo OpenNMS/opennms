082322c219e2f99001b8fad5bad42b3cdf42ef08
DJ Gregor <djgregor@users.sourceforge.net>
2006-08-30 13:42:44 +0000

    Need to use different escape sequences otherwise there are errors from PostgreSQL when the installer adds the function.

--- opennms-daemon/src/main/filtered/etc/setIpInterfaceIdInIfServiceTrigger.sql
+++ opennms-daemon/src/main/filtered/etc/setIpInterfaceIdInIfServiceTrigger.sql
@@ -1,9 +1,9 @@
 --First step to refactoring ipInterface, snmpInterface tables to have actual IDs
 --This trigger puts IpInterface IDs in the IfServices table
 
-DROP TRIGGER setIpInterfaceIdInIfServiceTrigger ON ifServices;
+DROP TRIGGER setIpInterfaceIdInIfService ON ifServices;
 
-CREATE OR REPLACE FUNCTION getIpInterfaceId() RETURNS trigger AS $IpInterfaceId$
+CREATE OR REPLACE FUNCTION getIpInterfaceId() RETURNS trigger AS '
 BEGIN
   IF NEW.ipInterfaceId IS NULL THEN
      SELECT ipif.id INTO NEW.ipInterfaceId
@@ -11,14 +11,14 @@ BEGIN
        WHERE (ipif.nodeid = NEW.nodeid AND ipif.ipAddr = NEW.ipAddr AND ipif.ifIndex = NEW.ifIndex);
        
        IF NOT FOUND THEN
-          RAISE EXCEPTION 'Invalid Interface';
+          RAISE EXCEPTION \'Invalid Interface\';
        END IF;
   END IF;
   RETURN NEW;
 END;
-$IpInterfaceId$ LANGUAGE 'plpgsql';
+' LANGUAGE 'plpgsql';
 
-CREATE TRIGGER setIpInterfaceIdInIfServiceTrigger 
+CREATE TRIGGER setIpInterfaceIdInIfService
    BEFORE INSERT OR UPDATE
    ON ifServices FOR EACH ROW
    EXECUTE PROCEDURE getIpInterfaceId();
