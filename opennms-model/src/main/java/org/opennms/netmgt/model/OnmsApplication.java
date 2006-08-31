fdc77d83a72a76503a8f11647d4f0e16f310f919
David Hustace <dhustace@users.sourceforge.net>
2006-08-31 02:23:24 +0000

    added annotations, at this point, not adding to create.sql

--- opennms-model/src/main/java/org/opennms/netmgt/model/OnmsApplication.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/OnmsApplication.java
@@ -1,25 +1,76 @@
+//
+// This file is part of the OpenNMS(R) Application.
+//
+// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included code and modified
+// code that was published under the GNU General Public License. Copyrights for modified 
+// and included code are below.
+//
+// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// For more information contact:
+// OpenNMS Licensing       <license@opennms.org>
+//     http://www.opennms.org/
+//     http://www.opennms.com/
+//
+
 package org.opennms.netmgt.model;
 
 import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="applications")
 public class OnmsApplication {
 	
 	private Integer m_id;
 	private String m_label;
 	private Set<OnmsMonitoredService> m_memberServices;
 	
+    @Id
+    @SequenceGenerator(name="opennmsSequence", sequenceName="opennmsNxtId")
+    @GeneratedValue(generator="opennmsSequence")
 	public Integer getId() {
 		return m_id;
 	}
 	public void setId(Integer id) {
 		m_id = id;
 	}
+    
+    @Column(name="id")
 	public String getLabel() {
 		return m_label;
 	}
 	public void setLabel(String label) {
 		m_label = label;
 	}
+    
+    @OneToMany
+    @JoinColumn(name="ifServiceId")
 	public Set<OnmsMonitoredService> getMemberServices() {
 		return m_memberServices;
 	}
