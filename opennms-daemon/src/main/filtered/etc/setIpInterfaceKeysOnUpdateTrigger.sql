68a1196a8697e4008f9b7ffd61d159fcb6c830fe
DJ Gregor <djgregor@users.sourceforge.net>
2006-09-01 16:55:25 +0000

    Move trigger stored procedures into the *Trigger.sql file.

--- opennms-daemon/src/main/filtered/etc/setIpInterfaceKeysOnUpdateTrigger.sql
+++ opennms-daemon/src/main/filtered/etc/setIpInterfaceKeysOnUpdateTrigger.sql
@@ -3,6 +3,52 @@
 
 DROP TRIGGER setIpInterfaceKeysOnUpdateTrigger ON ifServices;
 
+-- Refactoring DB with IpInterface IDs
+
+CREATE OR REPLACE FUNCTION setIpInterfaceKeysOnUpdate() RETURNS trigger AS '
+BEGIN
+
+  --
+  -- (Used with Trigger Update with old style foreign key)
+  -- This condition keeps ipinterfaceid inSync with the composite foreign key of nodeid, ipaddr, ifindex
+  -- This usually happens when a record is being updated by old JDBC code (non-Hibernate DAOs) and has changed
+  -- one or more of the composite key values, the ipInterfaceId needs to be updated
+  --
+  IF (NEW.ipInterfaceId = OLD.ipInterfaceId) AND (NEW.nodeId != OLD.nodeId OR NEW.ipAddr != OLD.ipAddr OR NEW.ifIndex != OLD.ifIndex) 
+  THEN
+     SELECT ipif.id INTO NEW.ipInterfaceId
+       FROM ipinterface ifsvc
+       WHERE (ipif.nodeid = NEW.nodeid AND ipif.ipAddr = NEW.ipAddr AND ipif.ifIndex = NEW.ifIndex);
+       
+     IF NOT FOUND THEN
+        RAISE EXCEPTION ''IfServices Trigger Exception, Condition 3: No IpInterface found for... nodeid: %  ipaddr: %  ifindex: %'', NEW.nodeid, NEW.ipAddr, NEW.ifIndex;
+     ELSE
+        RAISE NOTICE ''IfServices Trigger Success, Condition 3: IpInterface found for... nodeid: %  ipaddr: %  serviceid: %'', NEW.nodeid, NEW.ipAddr, NEW.ifIndex;
+     END IF;
+     
+  --
+  -- (Used with Trigger Update with new style foreign key)
+  -- This condition keeps the composite foreign key of nodeid, ipaddr, ifindex inSync with the ipinterfaceid
+  -- This usually happens with the Hibernate DAOs decide to change the ipinterfaceid represented
+  -- by the ifservices.
+  --
+  ELSIF NEW.ipInterfaceId != OLD.ipInterfaceId
+  THEN
+     SELECT ipif.nodeId, ipif.ipAddr, ipif.ifIndex INTO NEW.nodeId, NEW.ipAddr, NEW.ifIndex
+       FROM ipinterface ipif
+      WHERE (ipif.id = NEW.ipInterfaceId);
+      
+      IF NOT FOUND THEN
+         RAISE EXCEPTION ''IfServices Trigger Exception, Condition 4: No IpInterface found for ipInterfaceId: %'', NEW.ipInterfaceId;
+      ELSE
+         RAISE NOTICE ''IfServices Trigger Success, Condition 4: IpInterface found for ipIntefaceId: %'', NEW.ipInterfaceId;
+      END IF;
+  END IF;
+
+  RETURN NEW;
+END;
+' LANGUAGE 'plpgsql';
+
 CREATE TRIGGER setIpInterfaceKeysOnUpdateTrigger
    BEFORE UPDATE
    ON ifServices FOR EACH ROW
--- opennms-daemon/src/main/filtered/etc/setSnmpInterfaceKeysOnUpdateTrigger.sql
+++ opennms-daemon/src/main/filtered/etc/setSnmpInterfaceKeysOnUpdateTrigger.sql
@@ -3,6 +3,52 @@
 
 DROP TRIGGER setSnmpInterfaceKeysOnUpdateTrigger ON ipInterface;
 
+--DB Refactoring function to introduce SnmpInterface IDs
+
+CREATE OR REPLACE FUNCTION setSnmpInterfaceKeysOnUpdate() RETURNS trigger AS '
+BEGIN
+
+  --
+  -- (Used for Trigger update with old style foreign key)
+  -- This condition keeps snmpinterfaceid inSync with the composite foreign key of nodeid, ipaddr, ifindex
+  -- This usually happens when a record is being updated by old JDBC code (non-Hibernate DAOs) and has changed
+  -- one or more of the composite key values, the snmpInterfaceId needs to be updated
+  --
+  IF (NEW.snmpInterfaceId = OLD.snmpInterfaceId) AND (NEW.nodeId != OLD.nodeId OR NEW.ipAddr != OLD.ipAddr OR NEW.ifIndex != OLD.ifIndex) 
+  THEN
+     SELECT snmpif.id INTO NEW.snmpInterfaceId
+       FROM snmpinterface snmpif
+       WHERE (snmpif.nodeid = NEW.nodeid AND snmpif.ipAddr = NEW.ipAddr AND snmpif.ifIndex = NEW.ifIndex);
+       
+     IF NOT FOUND THEN
+        RAISE NOTICE ''IpInterface Trigger Notice, Condition 3: No SnmpInterface found for... nodeid: %  ipaddr: %  ifindex: %'', NEW.nodeid, NEW.ipAddr, NEW.ifIndex;
+     ELSE
+        RAISE NOTICE ''IpInterface Trigger Notice, Condition 3: SnmpInterface found for... nodeid: %  ipaddr: %  serviceid: %'', NEW.nodeid, NEW.ipAddr, NEW.ifIndex;
+     END IF;
+     
+  --
+  -- (Used for Trigger update with new style foreign key)
+  -- This condition keeps the composite foreign key of nodeid, ipaddr, ifindex inSync with the snmpinterfaceid
+  -- This usually happens with the Hibernate DAOs decide to change the snmpinterfaceid represented
+  -- by the ipinterface.
+  --
+  ELSIF NEW.snmpInterfaceId != OLD.snmpInterfaceId
+  THEN
+     SELECT snmpif.nodeId, snmpif.ipAddr, snmpif.ifIndex INTO NEW.nodeId, NEW.ipAddr, NEW.ifIndex
+       FROM snmpinterface snmpif
+      WHERE (snmpif.id = NEW.snmpInterfaceId);
+      
+      IF NOT FOUND THEN
+         RAISE NOTICE ''IpInterface Trigger Notice, Condition 4: No SnmpInterface found for snmpInterfaceId: %'', NEW.snmpInterfaceId;
+      ELSE
+         RAISE NOTICE ''IpInterface Trigger Notice, Condition 4: SnmpInterface found for snmpInterfaceId: %'', NEW.snmpInterfaceId;
+      END IF;
+  END IF;
+
+  RETURN NEW;
+END;
+' LANGUAGE 'plpgsql';
+
 CREATE TRIGGER setSnmpInterfaceKeysOnUpdateTrigger
    BEFORE UPDATE
    ON ipInterface FOR EACH ROW
