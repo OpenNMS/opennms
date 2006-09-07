b3011682f92061c7a95979dc3229ab2b45ffc2cd
DJ Gregor <djgregor@users.sourceforge.net>
2006-09-07 09:59:07 +0000

    Don't do anything with ipAddr.  Also don't try to setup snmpInterface relationship if ifIndex is null.

--- opennms-daemon/src/main/filtered/etc/setSnmpInterfaceKeysOnInsertTrigger.sql
+++ opennms-daemon/src/main/filtered/etc/setSnmpInterfaceKeysOnInsertTrigger.sql
@@ -16,16 +16,19 @@ BEGIN
   --
   IF NEW.snmpInterfaceId IS NULL 
   THEN
-     SELECT snmpif.id INTO NEW.snmpInterfaceId
-       FROM snmpinterface snmpif
-       WHERE (snmpif.nodeid = NEW.nodeid AND snmpif.ipAddr = NEW.ipAddr AND (snmpif.snmpIfIndex = NEW.ifIndex OR (snmpif.snmpIfIndex IS NULL AND NEW.ifIndex IS NULL)));
+     IF NEW.ifIndex IS NOT NULL
+     THEN
+       SELECT snmpif.id INTO NEW.snmpInterfaceId
+         FROM snmpinterface snmpif
+         WHERE (snmpif.nodeid = NEW.nodeid AND snmpif.snmpIfIndex = NEW.ifIndex);
        
        IF NOT FOUND 
        THEN
-          RAISE NOTICE ''IpInterface Trigger Notice, Condition 1: No SnmpInterface found for... nodeid: %  ipaddr: %  ifindex: %'', NEW.nodeid, NEW.ipAddr, NEW.ifIndex;
+          RAISE EXCEPTION ''IpInterface Trigger Notice, Condition 1: No SnmpInterface found for... nodeid: % ifindex: %'', NEW.nodeid, NEW.ifIndex;
        ELSE
-          RAISE NOTICE ''IpInterface Trigger Notice, Condition 1: SnmpInterface found for... nodeid: %  ipaddr: %  ifindex: %'', NEW.nodeid, NEW.ipAddr, NEW.ifIndex;
+         RAISE NOTICE ''IpInterface Trigger Notice, Condition 1: SnmpInterface found for... nodeid: % ifindex: %'', NEW.nodeid, NEW.ifIndex;
        END IF;
+     END IF;
        
   --
   -- (Used for Insert with new style foreign key)
@@ -34,17 +37,17 @@ BEGIN
   -- the composite key columns
   --
 
-  ELSIF NEW.snmpInterfaceId IS NOT NULL AND (NEW.nodeId IS NULL OR NEW.ipAddr IS NULL OR NEW.ifIndex IS NULL)
+  ELSIF NEW.snmpInterfaceId IS NOT NULL AND (NEW.nodeId IS NULL OR NEW.ifIndex IS NULL)
   THEN
-     SELECT snmpif.nodeid, snmpif.ipAddr, snmpif.ifIndex INTO NEW.nodeid, NEW.ipAddr, NEW.ifIndex
+     SELECT snmpif.nodeid, snmpif.ifIndex INTO NEW.nodeid, NEW.ifIndex
        FROM snmpinterface snmpif
       WHERE (snmpif.id = NEW.snmpInterfaceId);
       
       IF NOT FOUND
       THEN
-         RAISE NOTICE ''IpInterface Trigger Notice: No SnmpInterface found for snmpInterfaceId: %'', NEW.snmpInterfaceId;
+         RAISE EXCEPTION ''IpInterface Trigger Notice: No SnmpInterface found for snmpInterfaceId: %'', NEW.snmpInterfaceId;
       ELSE
-         RAISE EXCEPTION ''IpInterface Trigger Notice: SnmpInterface found for snmpInterfaceId: %'', NEW.snmpInterfaceId;
+         RAISE NOTICE ''IpInterface Trigger Notice: SnmpInterface found for snmpInterfaceId: %'', NEW.snmpInterfaceId;
       END IF;
   END IF;
   RETURN NEW;
