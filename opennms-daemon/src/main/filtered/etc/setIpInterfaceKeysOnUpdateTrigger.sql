2831d4b4a53a40bf4738be22836e0985ad0f5b2b
DJ Gregor <djgregor@users.sourceforge.net>
2006-09-08 04:29:49 +0000

    This should be the completion of database sanity checking per bug 1570

--- opennms-daemon/src/main/filtered/etc/setIpInterfaceKeysOnUpdateTrigger.sql
+++ opennms-daemon/src/main/filtered/etc/setIpInterfaceKeysOnUpdateTrigger.sql
@@ -8,22 +8,28 @@ DROP TRIGGER setIpInterfaceKeysOnUpdateTrigger ON ifServices;
 CREATE OR REPLACE FUNCTION setIpInterfaceKeysOnUpdate() RETURNS trigger AS '
 BEGIN
 
+  -- ifServices must have an IP address that is *not* 0.0.0.0
+  IF NEW.ipAddr IS NOT NULL AND NEW.ipAddr = ''0.0.0.0''
+  THEN
+    RAISE EXCEPTION ''IfServices Trigger Exception, Condition 0: ipAddr of 0.0.0.0 is not allowed in ifServices table'';
+  END IF;
+
   --
   -- (Used with Trigger Update with old style foreign key)
   -- This condition keeps ipinterfaceid inSync with the composite foreign key of nodeid, ipaddr, ifindex
   -- This usually happens when a record is being updated by old JDBC code (non-Hibernate DAOs) and has changed
   -- one or more of the composite key values, the ipInterfaceId needs to be updated
   --
-  IF (NEW.ipInterfaceId = OLD.ipInterfaceId) AND (NEW.nodeId != OLD.nodeId OR NEW.ipAddr != OLD.ipAddr OR NEW.ifIndex != OLD.ifIndex) 
+  IF (NEW.ipInterfaceId = OLD.ipInterfaceId) AND (NEW.nodeId != OLD.nodeId OR NEW.ipAddr != OLD.ipAddr) 
   THEN
      SELECT ipif.id INTO NEW.ipInterfaceId
-       FROM ipinterface ifsvc
-       WHERE (ipif.nodeid = NEW.nodeid AND ipif.ipAddr = NEW.ipAddr AND ipif.ifIndex = NEW.ifIndex);
+       FROM ipinterface ipif
+       WHERE (ipif.nodeid = NEW.nodeid AND ipif.ipAddr = NEW.ipAddr AND ipif.ipAddr != ''0.0.0.0'');
        
      IF NOT FOUND THEN
-        RAISE EXCEPTION ''IfServices Trigger Exception, Condition 3: No IpInterface found for... nodeid: %  ipaddr: %  ifindex: %'', NEW.nodeid, NEW.ipAddr, NEW.ifIndex;
+        RAISE EXCEPTION ''IfServices Trigger Exception, Condition 3: No IpInterface found for... nodeid: %  ipaddr: % '', NEW.nodeid, NEW.ipAddr;
      ELSE
-        RAISE NOTICE ''IfServices Trigger Success, Condition 3: IpInterface found for... nodeid: %  ipaddr: %  serviceid: %'', NEW.nodeid, NEW.ipAddr, NEW.ifIndex;
+        RAISE NOTICE ''IfServices Trigger Success, Condition 3: IpInterface found for... nodeid: %  ipaddr: %  ipInterfaceId: %'', NEW.nodeid, NEW.ipAddr, NEW.ipInterfaceId;
      END IF;
      
   --
@@ -42,6 +48,11 @@ BEGIN
          RAISE EXCEPTION ''IfServices Trigger Exception, Condition 4: No IpInterface found for ipInterfaceId: %'', NEW.ipInterfaceId;
       ELSE
          RAISE NOTICE ''IfServices Trigger Success, Condition 4: IpInterface found for ipIntefaceId: %'', NEW.ipInterfaceId;
+      END IF;
+      
+      IF NEW.ipAddr = ''0.0.0.0''
+      THEN
+         RAISE EXCEPTION ''IfServices Trigger Exception, Condition 5: IpInterface found for ipInterfaceId: % has 0.0.0.0 ipAddr'', NEW.ipInterfaceId;
       END IF;
   END IF;
 
