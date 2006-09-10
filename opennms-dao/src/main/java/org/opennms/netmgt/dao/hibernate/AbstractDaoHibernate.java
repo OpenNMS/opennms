982c285c8aa3b3739d2d6febd9758a023b57da47
DJ Gregor <djgregor@users.sourceforge.net>
2006-09-10 21:13:41 +0000

    Format code

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/hibernate/AbstractDaoHibernate.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/hibernate/AbstractDaoHibernate.java
@@ -1,14 +1,18 @@
 //
 // This file is part of the OpenNMS(R) Application.
 //
-// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
-// OpenNMS(R) is a derivative work, containing both original code, included code and modified
-// code that was published under the GNU General Public License. Copyrights for modified 
+// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc. All rights
+// reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included
+// code and modified
+// code that was published under the GNU General Public License. Copyrights
+// for modified
 // and included code are below.
 //
 // OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
 //
-// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+// Original code base Copyright (C) 1999-2001 Oculan Corp. All rights
+// reserved.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,7 +21,7 @@
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
@@ -25,9 +29,9 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //
 // For more information contact:
-// OpenNMS Licensing       <license@opennms.org>
-//     http://www.opennms.org/
-//     http://www.opennms.com/
+// OpenNMS Licensing <license@opennms.org>
+// http://www.opennms.org/
+// http://www.opennms.com/
 //
 package org.opennms.netmgt.dao.hibernate;
 
@@ -42,116 +46,123 @@ import org.opennms.netmgt.dao.OnmsDao;
 import org.springframework.orm.hibernate3.HibernateCallback;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public abstract class AbstractDaoHibernate<T, K extends Serializable> extends HibernateDaoSupport implements OnmsDao<T, K> {
-	
-	Class<T> m_entityClass;
-	
-	public AbstractDaoHibernate(Class<T> entityClass) {
-		m_entityClass = entityClass;
-	}
-	
+public abstract class AbstractDaoHibernate<T, K extends Serializable> extends
+        HibernateDaoSupport implements OnmsDao<T, K> {
+
+    Class<T> m_entityClass;
+
+    public AbstractDaoHibernate(Class<T> entityClass) {
+        m_entityClass = entityClass;
+    }
 
     public T initialize(T obj) {
         getHibernateTemplate().initialize(obj);
         return obj;
     }
-    
+
     public void flush() {
         getHibernateTemplate().flush();
     }
-    
+
     public void clear() {
         getHibernateTemplate().clear();
     }
-    
+
     public void evict(T entity) {
         getHibernateTemplate().evict(entity);
     }
-    
+
     public void merge(T entity) {
         getHibernateTemplate().merge(entity);
     }
-    
+
     @SuppressWarnings("unchecked")
-	public Collection<T> find(String query) {
+    public Collection<T> find(String query) {
         return getHibernateTemplate().find(query);
     }
-    
+
     @SuppressWarnings("unchecked")
-	public Collection<T> find(String query, Object... values) {
+    public Collection<T> find(String query, Object... values) {
         return getHibernateTemplate().find(query, values);
     }
-    
+
     protected int queryInt(final String query) {
-        Object result = getHibernateTemplate().execute(new HibernateCallback() {
-		    
-            public Object doInHibernate(Session session) throws HibernateException, SQLException {
-                return session.createQuery(query)
-                            .uniqueResult();
+        HibernateCallback callback = new HibernateCallback() {
+
+            public Object doInHibernate(Session session)
+                    throws HibernateException, SQLException {
+                return session.createQuery(query).uniqueResult();
             }
-            
-        });
-        return ((Number)result).intValue();
+
+        };
+
+        Object result = getHibernateTemplate().execute(callback);
+        return ((Number) result).intValue();
     }
 
     protected T findUnique(final String query) {
-        Object result = getHibernateTemplate().execute(new HibernateCallback() {
-		    
-		            public Object doInHibernate(Session session) throws HibernateException, SQLException {
-		                return session.createQuery(query)
-		                            .uniqueResult();
-		            }
-		            
-		        });
-		return m_entityClass.cast(result);
+        HibernateCallback callback = new HibernateCallback() {
+
+            public Object doInHibernate(Session session)
+                    throws HibernateException, SQLException {
+                return session.createQuery(query).uniqueResult();
+            }
+
+        };
+        Object result = getHibernateTemplate().execute(callback);
+        return m_entityClass.cast(result);
     }
-    
+
     protected T findUnique(final String queryString, final Object... args) {
-        Object result = getHibernateTemplate().execute(new HibernateCallback() {
-		            
-		            public Object doInHibernate(Session session) throws HibernateException, SQLException {
-		            	Query query = session.createQuery(queryString);
-		            	for (int i = 0; i < args.length; i++) {
-							query.setParameter(i, args[i]);
-						}
-		                return query.uniqueResult();
-		            }
-		            
-		        });
-		return m_entityClass.cast(result);
+        HibernateCallback callback = new HibernateCallback() {
+
+            public Object doInHibernate(Session session)
+                    throws HibernateException, SQLException {
+                Query query = session.createQuery(queryString);
+                for (int i = 0; i < args.length; i++) {
+                    query.setParameter(i, args[i]);
+                }
+                return query.uniqueResult();
+            }
+
+        };
+        Object result = getHibernateTemplate().execute(callback);
+        return m_entityClass.cast(result);
     }
-    
+
     public int countAll() {
-    	return queryInt("select count(*) from "+m_entityClass.getName());
+        return queryInt("select count(*) from " + m_entityClass.getName());
     }
-    
-	public void delete(T entity) {
-		getHibernateTemplate().delete(entity);
-	}
 
-	@SuppressWarnings("unchecked")
-	public Collection<T> findAll() {
-		return getHibernateTemplate().loadAll(m_entityClass);
-	}
+    public void delete(T entity) {
+        getHibernateTemplate().delete(entity);
+    }
 
-	public T get(K id) {
-		return m_entityClass.cast(getHibernateTemplate().get(m_entityClass, id));
-	}
+    @SuppressWarnings("unchecked")
+    public Collection<T> findAll() {
+        return getHibernateTemplate().loadAll(m_entityClass);
+    }
 
-	public T load(K id) {
-		return m_entityClass.cast(getHibernateTemplate().load(m_entityClass, id));
-	}
+    public T get(K id) {
+        return m_entityClass.cast(getHibernateTemplate().get(m_entityClass,
+                                                             id));
+    }
 
-	public void save(T entity) {
-		getHibernateTemplate().save(entity);
-	}
+    public T load(K id) {
+        return m_entityClass.cast(getHibernateTemplate().load(m_entityClass,
+                                                              id));
+    }
 
-	public void saveOrUpdate(T entity) {
-		getHibernateTemplate().saveOrUpdate(entity);
-	}
+    public void save(T entity) {
+        getHibernateTemplate().save(entity);
+    }
 
-	public void update(T entity) {
-		getHibernateTemplate().update(entity);
-	}
+    public void saveOrUpdate(T entity) {
+        getHibernateTemplate().saveOrUpdate(entity);
+    }
+
+    public void update(T entity) {
+        getHibernateTemplate().update(entity);
+    }
 
 }
