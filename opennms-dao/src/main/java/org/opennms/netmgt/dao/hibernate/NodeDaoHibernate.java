31f050fa88e49b3c0edfb1fba9aa1e39c585f996
David Hustace <dhustace@users.sourceforge.net>
2006-09-10 23:24:39 +0000

    update example surveillance-views.xml working on NodeDaoHibernate bugs (in progress) add log to surveillance views factory

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/hibernate/NodeDaoHibernate.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/hibernate/NodeDaoHibernate.java
@@ -110,11 +110,14 @@ public class NodeDaoHibernate extends AbstractDaoHibernate<OnmsNode, Integer>
                 + "where c in ( ? )", categories);
     }
 
-    public Collection<OnmsNode> findAllByCategoryLists(
-            Collection<OnmsCategory> rowCatNames,
-            Collection<OnmsCategory> colCatNames) {
-        // TODO Auto-generated method stub
-        return null;
+    public Collection<OnmsNode> findAllByCategoryLists( Collection<OnmsCategory> rowCatNames, Collection<OnmsCategory> colCatNames) {
+        return find("select distinct n from OnmsNode as n "
+                + "join n.categories c " + "left join fetch n.assetRecord "
+                + "left join fetch n.ipInterfaces as iface "
+                + "left join fetch iface.monitoredServices as monSvc "
+                + "left join fetch monSvc.serviceType "
+                + "left join fetch monSvc.currentOutages "
+                + "where c.name in ( ? )", rowCatNames);
     }
 
 }
