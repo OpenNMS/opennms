0277f34b7fe04e2b9789dad62213e4070ae477e9
David Hustace <dhustace@users.sourceforge.net>
2006-09-19 06:28:04 +0000

    Code supporting the configuration of remote monitoring locations.

---
+++ opennms-services/src/main/java/org/opennms/netmgt/config/MonitoringLocationsFactory.java
@@ -0,0 +1,186 @@
+//
+// This file is part of the OpenNMS(R) Application.
+//
+// OpenNMS(R) is Copyright (C) 2002-2006 The OpenNMS Group, Inc.  All rights reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included code and modified
+// code that was published under the GNU General Public License. Copyrights for modified 
+// and included code are below.
+//
+// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+// Modifications:
+//
+// 2003 Jan 31: Cleaned up some unused imports.
+//
+// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.                                                            
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//       
+// For more information contact: 
+//      OpenNMS Licensing       <license@opennms.org>
+//      http://www.opennms.org/
+//      http://www.opennms.com/
+//
+
+package org.opennms.netmgt.config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.log4j.Category;
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.ValidationException;
+import org.opennms.core.utils.ThreadCategory;
+import org.opennms.netmgt.ConfigFileConstants;
+import org.opennms.netmgt.config.monitoringLocations.LocationDef;
+import org.opennms.netmgt.config.monitoringLocations.MonitoringLocationsConfiguration;
+
+/**
+ * 
+ * @author <a href="mailto:david@opennms.org">David Hustace</a>
+ * 
+ */
+public class MonitoringLocationsFactory {
+    /** The singleton instance. */
+    private static MonitoringLocationsFactory m_instance;
+
+    private static boolean m_loadedFromFile = false;
+
+    /** Boolean indicating if the init() method has been called. */
+    protected boolean initialized = false;
+
+    /** Timestamp of the config file, used to know when to reload from disk. */
+    protected static long m_lastModified;
+
+    protected static Map<String,LocationDef> m_defsMap;
+
+    private static MonitoringLocationsConfiguration m_config;
+
+    public MonitoringLocationsFactory(String configFile) throws MarshalException, ValidationException, IOException {
+        InputStreamReader rdr = new InputStreamReader(new FileInputStream(configFile));
+        
+        try {
+            initialize(rdr);
+        } finally {
+            rdr.close();
+        }
+    }
+
+    public MonitoringLocationsFactory(Reader rdr) throws MarshalException, ValidationException {
+        initialize(rdr);
+    }
+
+    private void initialize(Reader rdr) throws MarshalException, ValidationException {
+        log().debug("initialize: initializing monitoring locations factory.");
+        m_config = (MonitoringLocationsConfiguration) Unmarshaller.unmarshal(MonitoringLocationsConfiguration.class, rdr);
+
+        m_defsMap = new HashMap<String, LocationDef>();
+        Collection defList = m_config.getLocations().getLocationDefCollection();
+        Iterator i = defList.iterator();
+        while (i.hasNext()) {
+            LocationDef def = (LocationDef) i.next();
+            m_defsMap.put(def.getLocationName(), def);
+        }
+    }
+
+    /** Be sure to call this method before calling getInstance(). */
+    public static synchronized void init() throws IOException, FileNotFoundException, MarshalException, ValidationException {
+        if (m_instance == null) {
+            File cfgFile = ConfigFileConstants.getFile(ConfigFileConstants.MONITORING_LOCATIONS_FILE_NAME);
+            m_instance = new MonitoringLocationsFactory(cfgFile.getPath());
+            m_lastModified = cfgFile.lastModified();
+            m_loadedFromFile = true;
+        }
+    }
+
+    /**
+     * Singleton static call to get the only instance that should exist for the
+     * MonioringLocationsFactory
+     * 
+     * @return the monitoring locations factory instance
+     * @throws IllegalStateException
+     *             if init has not been called
+     */
+    public static synchronized MonitoringLocationsFactory getInstance() {
+        if (m_instance == null) {
+            throw new IllegalStateException("You must call init() before calling getInstance().");
+        }
+
+        return m_instance;
+    }
+    
+    public static synchronized void setInstance(MonitoringLocationsFactory instance) {
+        m_instance = instance;
+        m_loadedFromFile = false;
+    }
+
+    public synchronized void reload() throws IOException, FileNotFoundException, MarshalException, ValidationException {
+        m_instance = null;
+        init();
+    }
+
+    /** Can't be null */
+    public LocationDef getDef(String defName) throws IOException, MarshalException, ValidationException {
+        if (defName == null) {
+            throw new IllegalArgumentException("Cannot take null parameters.");
+        }
+
+        this.updateFromFile();
+        LocationDef def = (LocationDef) m_defsMap.get(defName);
+        return def;
+    }
+
+    /**
+     * Reload the config file if it has been changed since we last
+     * read it.
+     */
+    protected void updateFromFile() throws IOException, MarshalException, ValidationException {
+        if (m_loadedFromFile) {
+            File monitoringLocationsFile = ConfigFileConstants.getFile(ConfigFileConstants.MONITORING_LOCATIONS_FILE_NAME);
+            if (m_lastModified != monitoringLocationsFile.lastModified()) {
+                this.reload();
+            }
+        }
+    }
+
+    public synchronized static MonitoringLocationsConfiguration getConfig() {
+        return m_config;
+    }
+
+    public synchronized static void setConfig(MonitoringLocationsConfiguration m_config) {
+        MonitoringLocationsFactory.m_config = m_config;
+    }
+
+    public synchronized static Map<String, LocationDef> getDefsMap() {
+        return m_defsMap;
+    }
+
+    public synchronized static void setDefsMap(Map<String, LocationDef> map) {
+        m_defsMap = map;
+    }
+    
+    private Category log() {
+        return ThreadCategory.getInstance();
+    }
+}
---
+++ opennms-services/src/test/java/org/opennms/netmgt/config/MonitoringLocationsFactoryTest.java
@@ -0,0 +1,120 @@
+//
+// This file is part of the OpenNMS(R) Application.
+//
+// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included code and modified
+// code that was published under the GNU General Public License. Copyrights for modified 
+// and included code are below.
+//
+// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// For more information contact:
+// OpenNMS Licensing       <license@opennms.org>
+//     http://www.opennms.org/
+//     http://www.opennms.com/
+//
+
+package org.opennms.netmgt.config;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Collection;
+
+import junit.framework.TestCase;
+
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.ValidationException;
+import org.opennms.netmgt.config.monitoringLocations.LocationDef;
+import org.opennms.netmgt.config.poller.Package;
+import org.opennms.netmgt.mock.MockDatabase;
+import org.opennms.netmgt.mock.MockNetwork;
+
+/**
+ * 
+ * @author <a href="mailto:david@opennms.org">David Hustace</a>
+ *
+ */
+public class MonitoringLocationsFactoryTest extends TestCase {
+	
+	private MonitoringLocationsFactory m_locationFactory;
+    private PollerConfigManager m_pollerConfigManager;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		MockNetwork network = new MockNetwork();
+		
+		MockDatabase db = new MockDatabase();
+		db.populate(network);
+		
+		DataSourceFactory.setInstance(db);
+
+		Reader rdr = new InputStreamReader(getClass().getResourceAsStream("/org/opennms/netmgt/config/monitoring-locations.testdata.xml"));
+		m_locationFactory = new MonitoringLocationsFactory(rdr);
+		rdr.close();
+        
+        rdr = new InputStreamReader(getClass().getResourceAsStream("/org/opennms/netmgt/config/poller-configuration.testdata.xml"));
+        m_pollerConfigManager = new TestPollerConfigManager(rdr, "localhost", false);
+		rdr.close();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	@SuppressWarnings("unchecked")
+    public void testGetName() throws MarshalException, ValidationException, IOException {
+        final String locationName = "RDU";
+        LocationDef def = m_locationFactory.getDef(locationName);
+        assertNotNull(def);
+        assertEquals(locationName, def.getLocationName());
+        assertEquals("raleigh", def.getMonitoringArea());
+        
+        Collection<Package> pkgs = m_pollerConfigManager.getConfiguration().getPackageCollection();
+        boolean matched = true;
+        for (Package pkg : pkgs) {
+            if (locationName.equals(pkg.getName())) {
+                matched = true;
+            }
+        }
+        assertTrue(matched);
+	}
+    
+    static class TestPollerConfigManager extends PollerConfigManager {
+        String m_xml;
+        public TestPollerConfigManager(Reader rdr, String localServer, boolean verifyServer) throws MarshalException, ValidationException, IOException {
+            super(rdr, localServer, verifyServer);
+            save();
+        }
+
+        public void update() throws IOException, MarshalException, ValidationException {
+            reloadXML(new StringReader(m_xml));
+        }
+
+        protected void saveXml(String xml) throws IOException {
+            m_xml = xml;
+        }
+
+        public String getXml() {
+            return m_xml;
+        }
+    }
+}
