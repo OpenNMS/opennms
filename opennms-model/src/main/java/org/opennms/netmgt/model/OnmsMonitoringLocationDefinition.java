be1c7863893f12864e1cca402249b8f522358d12
DJ Gregor <djgregor@users.sourceforge.net>
2006-09-26 19:26:18 +0000

    Format code

--- opennms-model/src/main/java/org/opennms/netmgt/model/OnmsMonitoringLocationDefinition.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/OnmsMonitoringLocationDefinition.java
@@ -1,26 +1,33 @@
 package org.opennms.netmgt.model;
 
 public class OnmsMonitoringLocationDefinition {
-	
-	private String m_area;
-	private String m_name;
+
+    private String m_area;
+
+    private String m_name;
+
     private String m_pollingPackageName;
-	
-	public String getArea() {
-		return m_area;
-	}
-	public void setArea(String area) {
-		m_area = area;
-	}
-	public String getName() {
-		return m_name;
-	}
-	public void setName(String name) {
-		m_name = name;
-	}
+
+    public String getArea() {
+        return m_area;
+    }
+
+    public void setArea(String area) {
+        m_area = area;
+    }
+
+    public String getName() {
+        return m_name;
+    }
+
+    public void setName(String name) {
+        m_name = name;
+    }
+
     public String getPollingPackageName() {
         return m_pollingPackageName;
     }
+
     public void setPollingPackageName(String pollingPackageName) {
         m_pollingPackageName = pollingPackageName;
     }
