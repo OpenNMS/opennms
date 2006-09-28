18ea04b88557baf964dff0bbd595c78f7a8eb913
Mike Huot <mhuot@users.sourceforge.net>
2006-09-28 23:55:49 +0000

    Test for b00g 1654.  Basically one of the issnmpprimary values being retrieved from ipinterface is null.  I am unsure where we should deal with this.  In the get? Should we constrain the table? Somewhere this is being written as a null, so that needs to be fixed too.

--- opennms-model/src/test/java/org/opennms/netmgt/model/OnmsIpInterfaceTest.java
+++ opennms-model/src/test/java/org/opennms/netmgt/model/OnmsIpInterfaceTest.java
@@ -22,10 +22,30 @@ public class OnmsIpInterfaceTest extends TestCase {
 	public void testCollectionTypeGetTwoChars () {
 		
 		try {
+			@SuppressWarnings("unused")
 			CollectionType collectionType = CollectionType.get(" MN  ");
 			fail("Expected to catch an exception here");
 		} catch (IllegalArgumentException e) {
 		}
 	}
 
+	public void testCollectionTypeGetZ () {
+		
+		try {
+			@SuppressWarnings("unused")
+			CollectionType collectionType = CollectionType.get("Z");
+			fail("Expected to catch an exception here");
+		} catch (IllegalArgumentException e) {
+		}
+	}
+	
+	public void testCollectionTypeComparison () {
+		CollectionType left = new CollectionType('N');
+		CollectionType right = null;
+		try {
+			left.isLessThan(right);
+			fail("Expected to catch an exception here");
+		} catch (NullPointerException e) {
+		}
+	}
 }
