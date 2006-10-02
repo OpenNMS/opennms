f7a8aaa6cd59316cd2f631d7bd512dddd133ccf0
DJ Gregor <djgregor@users.sourceforge.net>
2006-10-02 14:01:19 +0000

    Work on fixing first test... still needs more work

--- opennms-webapp/src/test/java/org/opennms/web/svclayer/support/DefaultSiteStatusServiceTest.java
+++ opennms-webapp/src/test/java/org/opennms/web/svclayer/support/DefaultSiteStatusServiceTest.java
@@ -43,22 +43,32 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
 
+import org.opennms.netmgt.config.siteStatusViews.Category;
+import org.opennms.netmgt.config.siteStatusViews.RowDef;
+import org.opennms.netmgt.config.siteStatusViews.Rows;
+import org.opennms.netmgt.config.siteStatusViews.View;
+import org.opennms.netmgt.dao.CategoryDao;
 import org.opennms.netmgt.dao.NodeDao;
 import org.opennms.netmgt.model.AggregateStatusDefinition;
 import org.opennms.netmgt.model.AggregateStatusView;
 import org.opennms.netmgt.model.OnmsCategory;
 import org.opennms.netmgt.model.OnmsNode;
 import org.opennms.web.svclayer.AggregateStatus;
+import org.opennms.web.svclayer.dao.SiteStatusViewConfigDao;
 
 import junit.framework.TestCase;
 
 public class DefaultSiteStatusServiceTest extends TestCase {
     
     private NodeDao m_nodeDao;
+    private CategoryDao m_categoryDao;
+    private SiteStatusViewConfigDao m_siteStatusViewConfigDao;
     
     public void setUp() throws Exception {
         super.setUp();
         m_nodeDao = createMock(NodeDao.class);
+        m_categoryDao = createMock(CategoryDao.class);
+        m_siteStatusViewConfigDao = createMock(SiteStatusViewConfigDao.class);
     }
     
     public void testBogus() {
@@ -69,17 +79,23 @@ public class DefaultSiteStatusServiceTest extends TestCase {
         Collection<AggregateStatus> aggrStati;
         Collection<AggregateStatusDefinition> defs = new HashSet<AggregateStatusDefinition>();
         
+        OnmsCategory catRouters = new OnmsCategory("routers");
+        OnmsCategory catSwitches = new OnmsCategory("switches");
+        
         AggregateStatusDefinition definition = 
-            new AggregateStatusDefinition("Routers/Switches", new HashSet<OnmsCategory>(Arrays.asList(new OnmsCategory[]{ new OnmsCategory("routers"), new OnmsCategory("switches") })));
+            new AggregateStatusDefinition("Routers/Switches", new HashSet<OnmsCategory>(Arrays.asList(new OnmsCategory[]{ catRouters, catSwitches })));
         defs.add(definition);
         
+        OnmsCategory catServers = new OnmsCategory("servers");
+        
         definition = 
-            new AggregateStatusDefinition("Servers", new HashSet<OnmsCategory>(Arrays.asList(new OnmsCategory[]{ new OnmsCategory("servers") })));
-            
+            new AggregateStatusDefinition("Servers", new HashSet<OnmsCategory>(Arrays.asList(new OnmsCategory[]{ catServers })));
         defs.add(definition);
         
         DefaultSiteStatusViewService aggregateSvc = new DefaultSiteStatusViewService();
         aggregateSvc.setNodeDao(m_nodeDao);
+        aggregateSvc.setCategoryDao(m_categoryDao);
+        aggregateSvc.setSiteStatusViewConfigDao(m_siteStatusViewConfigDao);
         
         OnmsNode node = new OnmsNode();
         node.setId(1);
@@ -90,10 +106,43 @@ public class DefaultSiteStatusServiceTest extends TestCase {
         for (AggregateStatusDefinition def : defs) {
             expect(m_nodeDao.findAllByVarCharAssetColumnCategoryList("building", "HQ", def.getCategories())).andReturn(nodes);
         }
+        for (OnmsNode n : nodes) {
+            expect(m_nodeDao.load(n.getId())).andReturn(n);
+        }
         replay(m_nodeDao);
         
+        expect(m_categoryDao.findByName("switches")).andReturn(catSwitches);
+        expect(m_categoryDao.findByName("routers")).andReturn(catRouters);
+        expect(m_categoryDao.findByName("servers")).andReturn(catServers);
+        replay(m_categoryDao);
+        
+        Rows rows = new Rows();
+        RowDef rowDef = new RowDef();
+        Category category = new Category();
+        category.setName("servers");
+        rowDef.addCategory(category);
+        rows.addRowDef(rowDef);
+        
+        rows = new Rows();
+        rowDef = new RowDef();
+        category = new Category();
+        category.setName("switches");
+        rowDef.addCategory(category);
+        category = new Category();
+        category.setName("routers");
+        rowDef.addCategory(category);
+        rows.addRowDef(rowDef);
+
+        View view = new View();
+        view.setRows(rows);
+        expect(m_siteStatusViewConfigDao.getView("building")).andReturn(view);
+        replay(m_siteStatusViewConfigDao);
+        
         aggrStati = aggregateSvc.createAggregateStatusesUsingNodeId(node.getId(), "building");
+        
         verify(m_nodeDao);
+        verify(m_categoryDao);
+        verify(m_siteStatusViewConfigDao);
         
         assertNotNull(aggrStati);
     }
