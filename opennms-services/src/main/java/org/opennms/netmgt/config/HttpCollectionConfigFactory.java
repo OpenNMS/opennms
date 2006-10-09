a9af2aaf525f8fccb8f34adefb754e8088229a6f
David Hustace <dhustace@users.sourceforge.net>
2006-10-09 22:59:20 +0000

    Beginnings of http collector: now need to associate uris with resources and write the actual collector.

---
+++ opennms-services/src/main/java/org/opennms/netmgt/config/HttpCollectionConfigFactory.java
@@ -0,0 +1,155 @@
+//
+// This file is part of the OpenNMS(R) Application.
+//
+// OpenNMS(R) is Copyright (C) 2002-2003 The OpenNMS Group, Inc.  All rights reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included code and modified
+// code that was published under the GNU General Public License. Copyrights for modified 
+// and included code are below.
+//
+// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+// Modifications:
+//
+// 2003 Jan 31: Cleaned up some unused imports.
+//
+// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.                                                            
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//       
+// For more information contact: 
+//      OpenNMS Licensing       <license@opennms.org>
+//      http://www.opennms.org/
+//      http://www.opennms.com/
+//
+// Tab Size = 8
+//
+
+package org.opennms.netmgt.config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.apache.log4j.Category;
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.ValidationException;
+import org.opennms.core.utils.ThreadCategory;
+import org.opennms.netmgt.ConfigFileConstants;
+import org.opennms.netmgt.config.datacollection.HttpDatacollectionConfig;
+
+/**
+ * 
+ * @author <a href="mailto:david@opennms.org">David Hustace</a>
+ *
+ */
+public class HttpCollectionConfigFactory {
+    /** The singleton instance. */
+    private static HttpCollectionConfigFactory m_instance;
+
+    private static boolean m_loadedFromFile = false;
+
+    /** Boolean indicating if the init() method has been called. */
+    protected boolean initialized = false;
+
+    /** Timestamp of the viewDisplay file, used to know when to reload from disk. */
+    protected static long m_lastModified;
+
+    private static HttpDatacollectionConfig m_config;
+
+    public HttpCollectionConfigFactory(String configFile) throws MarshalException, ValidationException, IOException {
+        InputStreamReader rdr = new InputStreamReader(new FileInputStream(configFile));
+        
+        try {
+            initialize(rdr);
+        } finally {
+            rdr.close();
+        }
+    }
+
+    public HttpCollectionConfigFactory(Reader rdr) throws MarshalException, ValidationException {
+        initialize(rdr);
+    }
+
+    private void initialize(Reader rdr) throws MarshalException, ValidationException {
+        log().debug("initialize: initializing surveillance views factory.");
+        m_config = (HttpDatacollectionConfig) Unmarshaller.unmarshal(HttpDatacollectionConfig.class, rdr);
+    }
+
+    /** Be sure to call this method before calling getInstance(). */
+    public static synchronized void init() throws IOException, FileNotFoundException, MarshalException, ValidationException {
+        
+        if (m_instance == null) {
+            File cfgFile = ConfigFileConstants.getFile(ConfigFileConstants.HTTP_COLLECTION_CONFIG_FILE_NAME);
+            m_instance = new HttpCollectionConfigFactory(cfgFile.getPath());
+            m_lastModified = cfgFile.lastModified();
+            m_loadedFromFile = true;
+        }
+    }
+
+    /**
+     * Singleton static call to get the only instance that should exist
+     * 
+     * @return the single factory instance
+     * @throws IllegalStateException
+     *             if init has not been called
+     */
+    public static synchronized HttpCollectionConfigFactory getInstance() {
+        
+        if (m_instance == null) {
+            throw new IllegalStateException("You must call HttpCollectionConfigFactory.init() before calling getInstance().");
+        }
+        return m_instance;
+    }
+    
+    public static synchronized void setInstance(HttpCollectionConfigFactory instance) {
+        m_instance = instance;
+        m_loadedFromFile = false;
+    }
+
+    public synchronized void reload() throws IOException, FileNotFoundException, MarshalException, ValidationException {
+        m_instance = null;
+        init();
+    }
+
+
+    /**
+     * Reload the http-datacollection-config.xml file if it has been changed since we last
+     * read it.
+     */
+    protected void updateFromFile() throws IOException, MarshalException, ValidationException {
+        if (m_loadedFromFile) {
+            File surveillanceViewsFile = ConfigFileConstants.getFile(ConfigFileConstants.HTTP_COLLECTION_CONFIG_FILE_NAME);
+            if (m_lastModified != surveillanceViewsFile.lastModified()) {
+                this.reload();
+            }
+        }
+    }
+
+    public synchronized static HttpDatacollectionConfig getConfig() {
+        return m_config;
+    }
+
+    public synchronized static void setConfig(HttpDatacollectionConfig m_config) {
+        HttpCollectionConfigFactory.m_config = m_config;
+    }
+
+    private Category log() {
+        return ThreadCategory.getInstance();
+    }
+}
