e1827a32364004a963d67f683e2f131249f3788e
DJ Gregor <djgregor@users.sourceforge.net>
2006-10-10 09:28:11 +0000

    Oops... this got missed in the last commit

---
+++ opennms-webapp/src/main/java/org/opennms/web/svclayer/DistributedStatusService.java
@@ -0,0 +1,7 @@
+package org.opennms.web.svclayer;
+
+public interface DistributedStatusService {
+//  public List<OnmsLocationSpecificStatus> findLocationSpecificStatus(OnmsMonitoringLocationDefinition location, OnmsApplication application);
+//    public List<OnmsLocationSpecificStatus> findLocationSpecificStatus(String locationName, String applicationName);
+    public SimpleWebTable createStatusTable(String locationName, String applicationLabel);
+}
