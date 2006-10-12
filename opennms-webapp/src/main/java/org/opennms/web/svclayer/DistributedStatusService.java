efecc26f30c179d5cf2c4d710eaadb54ea39d1cf
DJ Gregor <djgregor@users.sourceforge.net>
2006-10-12 10:24:47 +0000

    Add percentage calculation

--- opennms-webapp/src/main/java/org/opennms/web/svclayer/DistributedStatusService.java
+++ opennms-webapp/src/main/java/org/opennms/web/svclayer/DistributedStatusService.java
@@ -1,7 +1,9 @@
 package org.opennms.web.svclayer;
 
+import java.util.Date;
+
 public interface DistributedStatusService {
     public SimpleWebTable createStatusTable(String locationName, String applicationLabel);
     
-    public SimpleWebTable createFacilityStatusTable();
+    public SimpleWebTable createFacilityStatusTable(Date startDate, Date endDate);
 }
