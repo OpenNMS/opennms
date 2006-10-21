9e393952df103184e840c8957a545d121323bd2b
DJ Gregor <djgregor@users.sourceforge.net>
2006-10-21 04:25:33 +0000

    Make the service layer transactional (and read-only)

--- opennms-webapp/src/main/java/org/opennms/web/svclayer/DistributedStatusService.java
+++ opennms-webapp/src/main/java/org/opennms/web/svclayer/DistributedStatusService.java
@@ -2,6 +2,9 @@ package org.opennms.web.svclayer;
 
 import java.util.Date;
 
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly=true)
 public interface DistributedStatusService {
     public SimpleWebTable createStatusTable(String locationName, String applicationLabel);
     
