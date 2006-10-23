5449776e6ec212ab11e4d26ed1afb7a64b4052ba
Jonathan Sartin <jsartin@users.sourceforge.net>
2006-10-23 21:09:36 +0000

    AvailabilityReportLocator DAO annotate for hibernate and comment

--- opennms-model/src/main/java/org/opennms/netmgt/model/AvailabilityReportLocator.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/AvailabilityReportLocator.java
@@ -1,9 +1,53 @@
+//
+// This file is part of the OpenNMS(R) Application.
+//
+// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included code and modified
+// code that was published under the GNU General Public License. Copyrights for modified 
+// and included code are below.
+//
+// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// For more information contact:
+// OpenNMS Licensing       <license@opennms.org>
+//     http://www.opennms.org/
+//     http://www.opennms.com/
+//
 package org.opennms.netmgt.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-public class AvailabilityReportLocator {
+@Entity
+@Table(name="reportLocator")
+public class AvailabilityReportLocator implements Serializable {
+	
+	private static final long serialVersionUID = 4674675490758669590L;
 
 	private Integer m_id;
 	
@@ -33,46 +77,11 @@ public class AvailabilityReportLocator {
 	
 	//* getters and setters */
 	
-	public String getLocation() {
-		return m_location;
-	}
 
-	public void setLocation(String location) {
-		m_location = location;
-	}
-
-	public String getType() {
-		return m_type;
-	}
-	
-	public void setType(String type) {
-		m_type = type;
-	}
-	
-	public String getFormat() {
-		return m_format;
-	}
-	
-	public void setFormat(String format) {
-		m_format = format;
-	}
-	
-	public Date getDate() {
-		return m_date;
-	}
-	
-	public void setDate(Date date) {
-		m_date = date;
-	}
-	
-	public String getCategory() {
-		return m_category;
-	}
-	
-	public void setCategory(String category) {
-		m_category = category;
-	}
-
+    @Id
+    @Column(name="reportId")
+    @SequenceGenerator(name="reportSequence", sequenceName="reportNxtId")
+    @GeneratedValue(generator="reportSequence")
 	public Integer getId() {
 		return m_id;
 	}
@@ -81,6 +90,54 @@ public class AvailabilityReportLocator {
 		m_id = id;
 	}
 	
+	@Column(name="reportCategory", length=256)
+	public String getCategory() {
+		return m_category;
+	}
+	
+	public void setCategory(String category) {
+		m_category = category;
+	}
+	
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="reportDate", nullable=false)
+    	public Date getDate() {
+		return m_date;
+	}
+	
+	public void setDate(Date date) {
+		m_date = date;
+	}
+	
+	@Column(name="reportFormat", length=256)
+	public String getFormat() {
+		return m_format;
+	}
+	
+	public void setFormat(String format) {
+		m_format = format;
+	}
+
+	@Column(name="reportType", length=256)
+	public String getType() {
+		return m_type;
+	}
+	
+	public void setType(String type) {
+		m_type = type;
+	}
+
+	@Column(name="reportLocation", length=256)
+	public String getLocation() {
+		return m_location;
+	}
+
+	public void setLocation(String location) {
+		m_location = location;
+	}
+
+	
+	@Column(name="reportAvailable")
 	public Boolean getAvailable() {
 		return m_available;
 	}
