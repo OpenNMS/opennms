6c49239804ddfeae3d39fcc2f92e572ddbc9de4c
DJ Gregor <djgregor@users.sourceforge.net>
2006-10-26 20:39:43 +0000

    Add the new bits

---
+++ opennms-webapp/src/main/java/org/opennms/web/controller/NavBarController.java
@@ -0,0 +1,126 @@
+package org.opennms.web.controller;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.opennms.core.resource.Vault;
+import org.opennms.netmgt.dao.LocationMonitorDao;
+import org.opennms.web.Util;
+import org.opennms.web.acegisecurity.Authentication;
+import org.opennms.web.navigate.NavBarEntry;
+import org.opennms.web.svclayer.dao.SurveillanceViewConfigDao;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class NavBarController extends AbstractController {
+
+    private SurveillanceViewConfigDao m_surveillanceViewConfigDao;
+    private LocationMonitorDao m_locationMonitorDao;
+
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        assertPropertiesSet();
+        return new ModelAndView("navBar", "model", createNavBarModel(request));
+    }
+    
+    public void setSurveillanceViewConfigDao(SurveillanceViewConfigDao dao) {
+        m_surveillanceViewConfigDao = dao;
+    }
+    
+    public void setLocationMonitorDao(LocationMonitorDao dao) {
+        m_locationMonitorDao = dao;
+    }
+    
+    private void assertPropertiesSet() {
+        if (m_surveillanceViewConfigDao == null) {
+            throw new IllegalStateException("surveillanceViewConfigDao "
+                                            + "property has not been set");
+        }
+        if (m_locationMonitorDao == null) {
+            throw new IllegalStateException("locationMonitorDao "
+                                            + "property has not been set");
+        }
+    }
+    
+
+    private NavBarModel createNavBarModel(HttpServletRequest request) {
+        String mapEnableLocation = Vault.getHomeDir() + File.separator
+                + "etc" + File.separator + "map.enable";
+        File mapEnableFile = new File(mapEnableLocation);
+
+        String vulnEnableLocation = Vault.getHomeDir() + File.separator
+                + "etc" + File.separator + "vulnerabilities.enable";
+        File vulnEnableFile = new File(vulnEnableLocation);
+
+        LinkedList<NavBarEntry> navBar = new LinkedList<NavBarEntry>();
+        navBar.add(new NavBarEntry("nodelist", "element/nodelist.jsp",
+                                   "Node List"));
+        navBar.add(new NavBarEntry("element", "element/index.jsp", "Search"));
+        navBar.add(new NavBarEntry("outages", "outage/index.jsp", "Outages"));
+        navBar.add(new NavBarEntry("pathOutage", "pathOutage/index.jsp",
+                                   "Path Outages"));
+        navBar.add(new NavBarEntry("event", "event/index.jsp", "Events"));
+        navBar.add(new NavBarEntry("alarm", "alarm/index.jsp", "Alarms"));
+        navBar.add(new NavBarEntry("notification", "notification/index.jsp",
+                                   "Notification"));
+        navBar.add(new NavBarEntry("asset", "asset/index.jsp", "Assets"));
+        navBar.add(new NavBarEntry("report", "report/index.jsp", "Reports"));
+        navBar.add(new NavBarEntry("chart", "charts/index.jsp", "Charts"));
+        
+        if (m_surveillanceViewConfigDao.getViews().getViewCount() > 0 &&
+                m_surveillanceViewConfigDao.getDefaultView() != null) {
+            String viewName =
+                m_surveillanceViewConfigDao.getDefaultView().getName();
+            navBar.add(new NavBarEntry("surveillance",
+                                       "surveillanceView.htm?viewName="
+                                           + Util.htmlify(viewName),
+                                       "Surveillance"));
+        }
+        
+        if (m_locationMonitorDao.findAllMonitoringLocationDefinitions().size() > 0) {
+            navBar.add(new NavBarEntry("distributedstatus",
+                                       "distributedStatusSummary.htm",
+                                       "Distributed Status"));
+        }
+        
+        if (vulnEnableFile.exists()) {
+            navBar.add(new NavBarEntry("vulnerability",
+                                       "vulnerability/index.jsp",
+                                       "Vulnerabilities"));
+        }
+        if (mapEnableFile.exists()) {
+            navBar.add(new NavBarEntry("map", "map/index.jsp", "Map"));
+        }
+        if (request.isUserInRole(Authentication.ADMIN_ROLE)) {
+            navBar.add(new NavBarEntry("admin", "admin/index.jsp", "Admin"));
+        }
+        navBar.add(new NavBarEntry("help", "help/index.jsp", "Help"));
+
+        return new NavBarModel(navBar, request.getParameter("location"));
+    }
+
+    public class NavBarModel {
+        List<NavBarEntry> m_entries;
+
+        String m_location;
+
+        public NavBarModel(List<NavBarEntry> entries, String location) {
+            m_entries = entries;
+            m_location = location;
+        }
+
+        public List<NavBarEntry> getEntries() {
+            return m_entries;
+        }
+
+        public String getLocation() {
+            return m_location;
+        }
+
+    }
+}
---
+++ opennms-webapp/src/main/webapp/WEB-INF/jsp/navBar.jsp
@@ -0,0 +1,29 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java"%>
+<%@ taglib uri="http://java.sun.com/jstl/core" prefix="c"%>
+
+<div class="navbar">
+<ul>
+	<c:forEach var="entry" items="${model.entries}">
+		<c:choose>
+			<c:when test="${entry.name == 'Help'}">
+				<li class="last">
+			</c:when>
+			<c:otherwise>
+				<li>
+			</c:otherwise>
+		</c:choose>
+
+		<c:choose>
+			<c:when test="${model.location == entry.locationMatch}">
+				<c:out value="${entry.name}" />
+			</c:when>
+			<c:otherwise>
+				<a href="<c:out value="${entry.URL}"/>"><c:out
+					value="${entry.name}" /></a>
+			</c:otherwise>
+		</c:choose>
+		</li>
+	</c:forEach>
+</ul>
+</div>
+<!-- id="navbar" -->
