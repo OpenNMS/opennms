80d53b133a6268bd0f8954504cd1042ebcdfb527
DJ Gregor <djgregor@users.sourceforge.net>
2006-10-28 05:49:59 +0000

    Add a version of AbstractDaoTestCase that uses the Spring transactional data source underneath

---
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/AbstractTransactionalDaoTestCaseTest.java
@@ -0,0 +1,9 @@
+package org.opennms.netmgt.dao;
+
+public class AbstractTransactionalDaoTestCaseTest extends
+        AbstractTransactionalDaoTestCase {
+
+    public void testInitialize() {
+        // do nothing, just make sure that setUp() and tearDown() work
+    }
+}
