30903c7531ada03ff645005929d94ff9bbf6eb75
DJ Gregor <djgregor@users.sourceforge.net>
2006-10-28 22:44:52 +0000

    format code

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/OnmsDao.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/OnmsDao.java
@@ -34,30 +34,28 @@ package org.opennms.netmgt.dao;
 import java.io.Serializable;
 import java.util.Collection;
 
-
 public interface OnmsDao<T, K extends Serializable> {
-	
+
     public abstract void initialize(Object obj);
 
     public abstract void flush();
-    
+
     public abstract void clear();
-    
+
     public abstract int countAll();
-    
+
     public abstract void delete(T entity);
-    
+
     public abstract Collection<T> findAll();
-    
-	public abstract T get(K id);
-    
+
+    public abstract T get(K id);
+
     public abstract T load(K id);
 
     public abstract void save(T entity);
 
     public abstract void saveOrUpdate(T entity);
 
-	public abstract void update(T entity);
+    public abstract void update(T entity);
 
-    
 }
