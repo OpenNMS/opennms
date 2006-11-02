2b0df1b02affac06a2192ce9a20ecdc7fe600029
Craig Gallen <cgallen@users.sourceforge.net>
2006-11-02 11:36:15 +0000

    added qosdaemon example configuration to contrib

---
+++ opennms-daemon/src/main/resources/contrib/qosdaemon/qos_example_configuration/testtraps/ossjtesttraps_clear.sh
@@ -0,0 +1,6 @@
+                  
+# OpenNMS OSSJ Test Traps Generator
+     
+# OSSJ_TEST_ALARM_1 clear
+./trapgen v1 -d 127.0.0.1:162 -i 127.0.0.1 -c public -o 1.3.6.1.4.1.5813.3 -g 6 -s 2       
+
