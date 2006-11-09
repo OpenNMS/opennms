70e0fdfcd8f14b036bf2a6ac2f0cf9ce8466944b
Jonathan Sartin <jsartin@users.sourceforge.net>
2006-11-09 16:56:35 +0000

    Add FindAll() to CategoryConfigDao for availability report pages

--- opennms-webapp/src/main/java/org/opennms/web/svclayer/dao/CategoryConfigDao.java
+++ opennms-webapp/src/main/java/org/opennms/web/svclayer/dao/CategoryConfigDao.java
@@ -1,13 +1,13 @@
 package org.opennms.web.svclayer.dao;
 
+import java.util.Collection;
+
 import org.opennms.netmgt.config.categories.Category;
-
-
 
 public interface CategoryConfigDao {
 
 	public Category getCategoryByLabel(String label);
-		
 	
+	public Collection<Category> findAll();
 	
 }
