e0a6ad27408e1a67764cc7b64a7442d37d343272
DJ Gregor <djgregor@users.sourceforge.net>
2006-11-10 19:09:43 +0000

    Format code

--- opennms-services/src/test/java/org/opennms/netmgt/config/MonitoringLocationsFactoryTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/config/MonitoringLocationsFactoryTest.java
@@ -1,14 +1,18 @@
 //
 // This file is part of the OpenNMS(R) Application.
 //
-// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
-// OpenNMS(R) is a derivative work, containing both original code, included code and modified
-// code that was published under the GNU General Public License. Copyrights for modified 
+// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc. All rights
+// reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included
+// code and modified
+// code that was published under the GNU General Public License. Copyrights
+// for modified
 // and included code are below.
 //
 // OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
 //
-// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+// Original code base Copyright (C) 1999-2001 Oculan Corp. All rights
+// reserved.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -17,7 +21,7 @@
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
@@ -25,9 +29,9 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //
 // For more information contact:
-// OpenNMS Licensing       <license@opennms.org>
-//     http://www.opennms.org/
-//     http://www.opennms.com/
+// OpenNMS Licensing <license@opennms.org>
+// http://www.opennms.org/
+// http://www.opennms.com/
 //
 
 package org.opennms.netmgt.config;
@@ -48,58 +52,67 @@ import org.opennms.netmgt.mock.MockNetwork;
 import org.opennms.netmgt.model.OnmsMonitoredService;
 
 /**
- * 
  * @author <a href="mailto:david@opennms.org">David Hustace</a>
- *
  */
 public class MonitoringLocationsFactoryTest extends TestCase {
-	
-	private MonitoringLocationsFactory m_locationFactory;
+
+    private MonitoringLocationsFactory m_locationFactory;
+
     private PollerConfigManager m_pollerConfigManager;
 
-	protected void setUp() throws Exception {
-		super.setUp();
-		
-		MockNetwork network = new MockNetwork();
-		
-		MockDatabase db = new MockDatabase();
-		db.populate(network);
-		
-		DataSourceFactory.setInstance(db);
+    protected void setUp() throws Exception {
+        super.setUp();
 
-		Reader rdr = new InputStreamReader(getClass().getResourceAsStream("/org/opennms/netmgt/config/monitoring-locations.testdata.xml"));
-		m_locationFactory = new MonitoringLocationsFactory(rdr);
-		rdr.close();
-        
-        rdr = new InputStreamReader(getClass().getResourceAsStream("/org/opennms/netmgt/config/poller-configuration.testdata.xml"));
-        m_pollerConfigManager = new TestPollerConfigManager(rdr, "localhost", false);
-		rdr.close();
-	}
+        MockNetwork network = new MockNetwork();
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
-	
-	@SuppressWarnings("unchecked")
-    public void testGetName() throws MarshalException, ValidationException, IOException {
+        MockDatabase db = new MockDatabase();
+        db.populate(network);
+
+        DataSourceFactory.setInstance(db);
+
+        Reader rdr = new InputStreamReader(
+                                           getClass().getResourceAsStream(
+                                                                          "/org/opennms/netmgt/config/monitoring-locations.testdata.xml"));
+        m_locationFactory = new MonitoringLocationsFactory(rdr);
+        rdr.close();
+
+        rdr = new InputStreamReader(
+                                    getClass().getResourceAsStream(
+                                                                   "/org/opennms/netmgt/config/poller-configuration.testdata.xml"));
+        m_pollerConfigManager = new TestPollerConfigManager(rdr, "localhost",
+                                                            false);
+        rdr.close();
+    }
+
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testGetName() throws MarshalException, ValidationException,
+            IOException {
         final String locationName = "RDU";
         LocationDef def = m_locationFactory.getDef(locationName);
         assertNotNull(def);
         assertEquals(locationName, def.getLocationName());
         assertEquals("raleigh", def.getMonitoringArea());
-        
+
         assertNotNull(m_pollerConfigManager.getPackage(def.getPollingPackageName()));
-        
-	}
-    
+
+    }
+
     static class TestPollerConfigManager extends PollerConfigManager {
         String m_xml;
-        public TestPollerConfigManager(Reader rdr, String localServer, boolean verifyServer) throws MarshalException, ValidationException, IOException {
+
+        public TestPollerConfigManager(Reader rdr, String localServer,
+                boolean verifyServer) throws MarshalException,
+                ValidationException, IOException {
             super(rdr, localServer, verifyServer);
             save();
         }
 
-        public void update() throws IOException, MarshalException, ValidationException {
+        public void update() throws IOException, MarshalException,
+                ValidationException {
             reloadXML(new StringReader(m_xml));
         }
 
@@ -111,6 +124,5 @@ public class MonitoringLocationsFactoryTest extends TestCase {
             return m_xml;
         }
 
-        
     }
 }
