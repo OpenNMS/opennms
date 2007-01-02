36a6ee4bf02168ffcb85ca2dbf082431bf327b83
DJ Gregor <djgregor@users.sourceforge.net>
2007-01-02 22:01:35 +0000

    Indent

--- opennms-services/src/main/java/org/opennms/netmgt/daemon/AbstractServiceDaemon.java
+++ opennms-services/src/main/java/org/opennms/netmgt/daemon/AbstractServiceDaemon.java
@@ -1,34 +1,34 @@
-//
-// This file is part of the OpenNMS(R) Application.
-//
-// OpenNMS(R) is Copyright (C) 2005 The OpenNMS Group, Inc.  All rights reserved.
-// OpenNMS(R) is a derivative work, containing both original code, included code and modified
-// code that was published under the GNU General Public License. Copyrights for modified 
-// and included code are below.
-//
-// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
-//
-// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-//
-// For more information contact:
-// OpenNMS Licensing       <license@opennms.org>
-//     http://www.opennms.org/
-//     http://www.opennms.com/
-//
+
+//This file is part of the OpenNMS(R) Application.
+
+//OpenNMS(R) is Copyright (C) 2005 The OpenNMS Group, Inc.  All rights reserved.
+//OpenNMS(R) is a derivative work, containing both original code, included code and modified
+//code that was published under the GNU General Public License. Copyrights for modified 
+//and included code are below.
+
+//OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+
+//Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+
+//This program is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+//For more information contact:
+//OpenNMS Licensing       <license@opennms.org>
+//http://www.opennms.org/
+//http://www.opennms.com/
+
 package org.opennms.netmgt.daemon;
 
 import org.apache.log4j.Category;
@@ -40,14 +40,14 @@ public abstract class AbstractServiceDaemon implements ServiceDaemon, Initializi
     public void afterPropertiesSet() throws Exception {
         init();
     }
-    
+
     /**
      * The current status of this fiber
      */
     private int m_status;
-    
+
     private String m_name;
-    
+
     abstract protected void onInit();
 
     protected void onPause() {}
@@ -59,10 +59,10 @@ public abstract class AbstractServiceDaemon implements ServiceDaemon, Initializi
     protected void onStop() {}
 
     final public String getName() { return m_name; }
-    
+
     protected AbstractServiceDaemon(String name) {
-    	m_name = name;
-    	setStatus(START_PENDING);
+        m_name = name;
+        setStatus(START_PENDING);
     }
 
     protected synchronized void setStatus(int status) {
@@ -72,7 +72,7 @@ public abstract class AbstractServiceDaemon implements ServiceDaemon, Initializi
     public synchronized int getStatus() {
         return m_status;
     }
-    
+
     public String getStatusText() {
         return STATUS_NAMES[getStatus()];
     }
@@ -96,105 +96,105 @@ public abstract class AbstractServiceDaemon implements ServiceDaemon, Initializi
     protected synchronized boolean isStarting() {
         return m_status == STARTING;
     }
-    
+
     protected Category log() {
-    	return ThreadCategory.getInstance();
+        return ThreadCategory.getInstance();
     }
-    
+
     final public void init() {
-		String prefix = ThreadCategory.getPrefix();
-		try {
-			ThreadCategory.setPrefix(getName());
-			log().debug(getName()+" initializing.");
-			
-			onInit();
-			
-			log().debug(getName()+" initialization complete.");
-		} finally {
-			ThreadCategory.setPrefix(prefix);
-		}
+        String prefix = ThreadCategory.getPrefix();
+        try {
+            ThreadCategory.setPrefix(getName());
+            log().debug(getName()+" initializing.");
+
+            onInit();
+
+            log().debug(getName()+" initialization complete.");
+        } finally {
+            ThreadCategory.setPrefix(prefix);
+        }
     }
-    
 
 
-	final public void pause() {
-		String prefix = ThreadCategory.getPrefix();
-		try {
-			ThreadCategory.setPrefix(getName());
 
-			if (!isRunning())
-				return;
+    final public void pause() {
+        String prefix = ThreadCategory.getPrefix();
+        try {
+            ThreadCategory.setPrefix(getName());
 
-			setStatus(PAUSE_PENDING);
+            if (!isRunning())
+                return;
 
-			onPause();
+            setStatus(PAUSE_PENDING);
 
-			setStatus(PAUSED);
+            onPause();
 
-			log().debug(getName()+" paused.");
+            setStatus(PAUSED);
 
-		} finally {
-			ThreadCategory.setPrefix(prefix);
-		}
-	}
+            log().debug(getName()+" paused.");
 
-	final public void resume() {
-		String prefix = ThreadCategory.getPrefix();
-		try {
-			ThreadCategory.setPrefix(getName());
-			if (!isPaused())
-				return;
+        } finally {
+            ThreadCategory.setPrefix(prefix);
+        }
+    }
 
-			setStatus(RESUME_PENDING);
+    final public void resume() {
+        String prefix = ThreadCategory.getPrefix();
+        try {
+            ThreadCategory.setPrefix(getName());
+            if (!isPaused())
+                return;
 
-			onResume();
+            setStatus(RESUME_PENDING);
 
-			setStatus(RUNNING);
+            onResume();
 
-			log().debug(getName()+" resumed.");
-		} finally {
-			ThreadCategory.setPrefix(prefix);
-		}
-	}
+            setStatus(RUNNING);
 
-	final public synchronized void start() {
-		String prefix = ThreadCategory.getPrefix();
-		try {
-			ThreadCategory.setPrefix(getName());
-			log().debug(getName()+" starting.");
+            log().debug(getName()+" resumed.");
+        } finally {
+            ThreadCategory.setPrefix(prefix);
+        }
+    }
 
-			setStatus(STARTING);
+    final public synchronized void start() {
+        String prefix = ThreadCategory.getPrefix();
+        try {
+            ThreadCategory.setPrefix(getName());
+            log().debug(getName()+" starting.");
 
-			onStart();
+            setStatus(STARTING);
 
-			setStatus(RUNNING);
+            onStart();
 
-			log().info(getName()+" started.");
-		} finally {
-			ThreadCategory.setPrefix(prefix);
-		}
-	
-	}
+            setStatus(RUNNING);
 
-	/**
-	 * Stops the currently running service. If the service is not running then
-	 * the command is silently discarded.
-	 */
-	final public synchronized void stop() {
-		String prefix = ThreadCategory.getPrefix();
-		try {
-			ThreadCategory.setPrefix(getName());
-			log().debug(getName()+" stopping.");
-			setStatus(STOP_PENDING);
+            log().info(getName()+" started.");
+        } finally {
+            ThreadCategory.setPrefix(prefix);
+        }
 
-			onStop();
+    }
 
-			setStatus(STOPPED);
+    /**
+     * Stops the currently running service. If the service is not running then
+     * the command is silently discarded.
+     */
+    final public synchronized void stop() {
+        String prefix = ThreadCategory.getPrefix();
+        try {
+            ThreadCategory.setPrefix(getName());
+            log().debug(getName()+" stopping.");
+            setStatus(STOP_PENDING);
 
-			log().info(getName()+" stopped.");
-		} finally {
-			ThreadCategory.setPrefix(prefix);
-		}
-	}
+            onStop();
+
+            setStatus(STOPPED);
+
+            log().info(getName()+" stopped.");
+        } finally {
+            ThreadCategory.setPrefix(prefix);
+        }
+    }
 
 }
