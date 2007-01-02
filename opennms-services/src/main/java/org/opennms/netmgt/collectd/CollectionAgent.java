0060a295d310463ceadc5264d29207304cff5691
DJ Gregor <djgregor@users.sourceforge.net>
2007-01-02 22:06:19 +0000

    Indent

--- opennms-services/src/main/java/org/opennms/netmgt/collectd/CollectionAgent.java
+++ opennms-services/src/main/java/org/opennms/netmgt/collectd/CollectionAgent.java
@@ -1,38 +1,38 @@
-//
-// This file is part of the OpenNMS(R) Application.
-//
-// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
-// OpenNMS(R) is a derivative work, containing both original code, included code and modified
-// code that was published under the GNU General Public License. Copyrights for modified 
-// and included code are below.
-//
-// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
-//
-// Modifications:
-//
-// 2006 Aug 15: Javadoc. - dj@opennms.org
-//
-// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-//
-// For more information contact:
-// OpenNMS Licensing       <license@opennms.org>
-//     http://www.opennms.org/
-//     http://www.opennms.com/
-//
+
+//This file is part of the OpenNMS(R) Application.
+
+//OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
+//OpenNMS(R) is a derivative work, containing both original code, included code and modified
+//code that was published under the GNU General Public License. Copyrights for modified 
+//and included code are below.
+
+//OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+
+//Modifications:
+
+//2006 Aug 15: Javadoc. - dj@opennms.org
+
+//Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+
+//This program is free software; you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation; either version 2 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+//For more information contact:
+//OpenNMS Licensing       <license@opennms.org>
+//http://www.opennms.org/
+//http://www.opennms.com/
+
 
 package org.opennms.netmgt.collectd;
 
@@ -54,120 +54,120 @@ public class CollectionAgent extends IPv4NetworkInterface {
     private static final long serialVersionUID = 6694654071513990997L;
 
     // the interface of the Agent
-	private OnmsIpInterface m_iface;
-    
-     // miscellaneous junk?
+    private OnmsIpInterface m_iface;
+
+    // miscellaneous junk?
     private int m_maxVarsPerPdu = 0;
     private int m_ifCount = -1;
 
-	public CollectionAgent(OnmsIpInterface iface) {
-		super(iface.getInetAddress());
-		m_iface = iface;
-	}
-	
-	public OnmsIpInterface getIpInterface() {
-		return m_iface;
-	}
-	
-	public OnmsNode getNode() {
-		return m_iface.getNode();
-	}
-    
+    public CollectionAgent(OnmsIpInterface iface) {
+        super(iface.getInetAddress());
+        m_iface = iface;
+    }
+
+    public OnmsIpInterface getIpInterface() {
+        return m_iface;
+    }
+
+    public OnmsNode getNode() {
+        return m_iface.getNode();
+    }
+
     public void setMaxVarsPerPdu(int maxVarsPerPdu) {
         m_maxVarsPerPdu = maxVarsPerPdu;
-		if (log().isDebugEnabled()) {
-			log().debug("maxVarsPerPdu=" + maxVarsPerPdu);
-		}
-	}
-    
+        if (log().isDebugEnabled()) {
+            log().debug("maxVarsPerPdu=" + maxVarsPerPdu);
+        }
+    }
+
     public int getMaxVarsPerPdu() {
         return m_maxVarsPerPdu;
     }
 
-	public String getHostAddress() {
-		return getInetAddress().getHostAddress();
-	}
+    public String getHostAddress() {
+        return getInetAddress().getHostAddress();
+    }
 
-	public void setSavedIfCount(int ifCount) {
+    public void setSavedIfCount(int ifCount) {
         m_ifCount = ifCount;
-	}
+    }
 
-	int getSavedIfCount() {
+    int getSavedIfCount() {
         return m_ifCount;
-	}
+    }
 
-	int getNodeId() {
-		return getIpInterface().getNode().getId() == null ? -1 : getIpInterface().getNode().getId().intValue();
-	}
+    int getNodeId() {
+        return getIpInterface().getNode().getId() == null ? -1 : getIpInterface().getNode().getId().intValue();
+    }
 
-	int getIfIndex() {
-		return (getIpInterface().getIfIndex() == null ? -1 : getIpInterface().getIfIndex().intValue());
-	}
+    int getIfIndex() {
+        return (getIpInterface().getIfIndex() == null ? -1 : getIpInterface().getIfIndex().intValue());
+    }
 
-	public String getSysObjectId() {
-		return getIpInterface().getNode().getSysObjectId();
-	}
+    public String getSysObjectId() {
+        return getIpInterface().getNode().getSysObjectId();
+    }
 
-	private CollectionType getCollectionType() {
-		return getIpInterface().getIsSnmpPrimary();
-	}
+    private CollectionType getCollectionType() {
+        return getIpInterface().getIsSnmpPrimary();
+    }
 
     private void logCompletion() {
-    	
-    	if (log().isDebugEnabled()) {
-    		log().debug(
-    				"initialize: initialization completed: nodeid = " + getNodeId()
-    				+ ", address = " + getHostAddress()
-    				+ ", primaryIfIndex = " + getIfIndex()
-    				+ ", isSnmpPrimary = " + getCollectionType()
-    				+ ", sysoid = " + getSysObjectId()
-    				);
-    	}
-    
+
+        if (log().isDebugEnabled()) {
+            log().debug(
+                        "initialize: initialization completed: nodeid = " + getNodeId()
+                        + ", address = " + getHostAddress()
+                        + ", primaryIfIndex = " + getIfIndex()
+                        + ", isSnmpPrimary = " + getCollectionType()
+                        + ", sysoid = " + getSysObjectId()
+            );
+        }
+
     }
 
     private void validateSysObjId() {
-    	if (getSysObjectId() == null) {
-    		throw new RuntimeException("System Object ID for interface "
-    				+ getHostAddress()
-    				+ " does not exist in the database.");
-    	}
+        if (getSysObjectId() == null) {
+            throw new RuntimeException("System Object ID for interface "
+                                       + getHostAddress()
+                                       + " does not exist in the database.");
+        }
     }
 
     private void logCollectionParms() {
-    	if (log().isDebugEnabled()) {
-    		log().debug(
-    				"initialize: db retrieval info: nodeid = " + getNodeId()
-    				+ ", address = " + getHostAddress()
-    				+ ", primaryIfIndex = " + getIfIndex()
-    				+ ", isSnmpPrimary = " + getCollectionType()
-    				+ ", sysoid = " + getSysObjectId()
-    				);
-    	}
+        if (log().isDebugEnabled()) {
+            log().debug(
+                        "initialize: db retrieval info: nodeid = " + getNodeId()
+                        + ", address = " + getHostAddress()
+                        + ", primaryIfIndex = " + getIfIndex()
+                        + ", isSnmpPrimary = " + getCollectionType()
+                        + ", sysoid = " + getSysObjectId()
+            );
+        }
     }
 
     private void validateIsSnmpPrimary() {
-    	if (!CollectionType.PRIMARY.equals(getCollectionType())) {
-    		throw new RuntimeException("Interface "
-    				+ getHostAddress()
-    				+ " is not the primary SNMP interface for nodeid "
-    				+ getNodeId());
-    	}
+        if (!CollectionType.PRIMARY.equals(getCollectionType())) {
+            throw new RuntimeException("Interface "
+                                       + getHostAddress()
+                                       + " is not the primary SNMP interface for nodeid "
+                                       + getNodeId());
+        }
     }
 
     private void validatePrimaryIfIndex() {
-    	if (getIfIndex() == -1) {
-    		// allow this for nodes without ipAddrTables
-    		// throw new RuntimeException("Unable to retrieve ifIndex for
-    		// interface " + ipAddr.getHostAddress());
-    		if (log().isDebugEnabled()) {
-    			log().debug(
-    					"initialize: db retrieval info: node " + getNodeId()
-    					+ " does not have a legitimate "
-    					+ "primaryIfIndex.  Assume node does not "
-    					+ "supply ipAddrTable and continue...");
-    		}
-    	}
+        if (getIfIndex() == -1) {
+            // allow this for nodes without ipAddrTables
+            // throw new RuntimeException("Unable to retrieve ifIndex for
+            // interface " + ipAddr.getHostAddress());
+            if (log().isDebugEnabled()) {
+                log().debug(
+                            "initialize: db retrieval info: node " + getNodeId()
+                            + " does not have a legitimate "
+                            + "primaryIfIndex.  Assume node does not "
+                            + "supply ipAddrTable and continue...");
+            }
+        }
     }
 
     public void validateAgent() {
