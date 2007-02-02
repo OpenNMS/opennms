6f5677a6f9407ae14e3505743a36fb1a4f2efbc9
Matt Brozowski <brozow@users.sourceforge.net>
2007-02-02 00:27:16 +0000

    add flapping rules that almost work

---
+++ opennms-correlation/drools-correlation-engine/src/main/java/org/opennms/netmgt/correlation/drools/Flap.java
@@ -0,0 +1,69 @@
+package org.opennms.netmgt.correlation.drools;
+
+import java.util.Date;
+
+public class Flap {
+    Long m_nodeid;
+    String m_ipAddr;
+    String m_svcName;
+    Date m_startTime;
+    Date m_endTime;
+    Integer m_locationMonitor;
+    boolean m_counted;
+    
+    public Flap(Long nodeid, String ipAddr, String svcName, Integer locationMonitor) {
+        m_nodeid = nodeid;
+        m_ipAddr = ipAddr;
+        m_svcName = svcName;
+        m_locationMonitor = locationMonitor;
+        m_startTime = new Date();
+        m_counted = false;
+    }
+    
+    public Date getEndTime() {
+        return m_endTime;
+    }
+    public void setEndTime(Date end) {
+        m_endTime = end;
+    }
+    public String getIpAddr() {
+        return m_ipAddr;
+    }
+    public void setIpAddr(String ipAddr) {
+        m_ipAddr = ipAddr;
+    }
+    public Integer getLocationMonitor() {
+        return m_locationMonitor;
+    }
+    public void setLocationMonitor(Integer locationMonitor) {
+        m_locationMonitor = locationMonitor;
+    }
+    public Long getNodeid() {
+        return m_nodeid;
+    }
+    public void setNodeid(Long nodeid) {
+        m_nodeid = nodeid;
+    }
+    public Date getStartTime() {
+        return m_startTime;
+    }
+    public void setStartTime(Date start) {
+        m_startTime = start;
+    }
+    public String getSvcName() {
+        return m_svcName;
+    }
+    public void setSvcName(String svcName) {
+        m_svcName = svcName;
+    }
+
+    public boolean isCounted() {
+        return m_counted;
+    }
+
+    public void setCounted(boolean counted) {
+        m_counted = counted;
+    }
+    
+    
+}
--- opennms-correlation/drools-correlation-engine/src/test/java/org/opennms/netmgt/correlation/drools/DroolsCorrelationEngineTest.java
+++ opennms-correlation/drools-correlation-engine/src/test/java/org/opennms/netmgt/correlation/drools/DroolsCorrelationEngineTest.java
@@ -13,8 +13,7 @@ public class DroolsCorrelationEngineTest extends TestCase {
     
     private static final String WS_OUTAGE_UEI = "uei.opennms.org/correlation/locationMonitors/wideSpreadOutage";
     private static final String WS_RESOLVED_UEI = "uei.opennms.org/correlation/locationMonitors/wideSpreadOutageResolved";
-    private static final String ISO_OUTAGE_UEI = "uei.opennms.org/correlation/locationMonitors/isolatedOutage";
-    private static final String ISO_RESOLVED_UEI = "uei.opennms.org/correlation/locationMonitors/isolatedOutageResolved";
+    private static final String SERVICE_FLAPPING_UEI = "uei.opennms.org/correlation/locationMonitors/serviceFlapping";
     
     private MockEventIpcManager m_eventIpcMgr;
 	private EventAnticipator m_anticipator;
@@ -38,6 +37,9 @@ public class DroolsCorrelationEngineTest extends TestCase {
         
         m_engine = new DroolsCorrelationEngine();
 		m_engine.setEventIpcManager(m_eventIpcMgr);
+        m_engine.setWideSpreadThreshold(3);
+        m_engine.setFlapCount(3);
+        m_engine.setFlapInterval(1000L);
         m_engine.afterPropertiesSet();
 		
 	}
@@ -50,19 +52,19 @@ public class DroolsCorrelationEngineTest extends TestCase {
 		verify();
 	}
 
-	public void testMultipleLocationMonitorOutage() throws Exception {
+	public void testWideSpreadLocationMonitorOutage() throws Exception {
 
 		anticipateWideSpreadOutageEvent();
 		
         // received outage events for all monitors
         m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 7));
-        
-        Thread.sleep(5000);
-        
         m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 8));
+        m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 9));
+
         
         // expect memory to contain only the single 'affliction' for this service
-        m_anticipatedMemorySize = 1;
+        // and the flap tracker for each monitor
+        m_anticipatedMemorySize = 4;
         
         verify();
         
@@ -70,15 +72,21 @@ public class DroolsCorrelationEngineTest extends TestCase {
         
         // received outage events for all monitors
         m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 7));
-        
-        Thread.sleep(5000);
-        
+        m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 9));
         m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 8));
         
+        // expect the flap tracker to remain
+        m_anticipatedMemorySize = 6;
+        
+
+        verify();
+        
+        // need to time the flap trackers out
+        Thread.sleep(1100);
+
         m_anticipatedMemorySize = 0;
         
         verify();
-        
         
     }
     
@@ -86,41 +94,86 @@ public class DroolsCorrelationEngineTest extends TestCase {
 	
     public void testSingleLocationMonitorOutage() throws Exception {
         
-        anticipateIsolatedOutageEvent();
         
     	// recieve outage event for only a single monitor
         m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 7));
         
-        Thread.sleep(31000);
-    	
         // expect memory to contain only the single 'afflication' for htis service
-        m_anticipatedMemorySize = 1;
+        m_anticipatedMemorySize = 2;
         
         verify();
         
-        anticipateIsolatedOutageResolvedEvent();
-        
         m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        
+        m_anticipatedMemorySize = 2;
+        
+        verify();
+        
+        // let flaps time otu
+        Thread.sleep(1100);
         
         m_anticipatedMemorySize = 0;
         
         verify();
     }
     
-    public void testOutageEscalation() {
-        // we detect an isolated outage from a single monitor
-        // anticipateIsolated(mon 7)
+    
+    public void testDoubleLocationMonitorOutage() throws Exception {
         
-        // sometime later the outage is detected from additional monitors (becomes wide spread)
-        // anticipateWideSpread
+        // recieve outage event for only a single monitor
+        m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 8));
         
-        // 
+        // expect memory to contain only the single 'afflication' for this service
+        m_anticipatedMemorySize = 3;
+        
+        verify();
+        
+        m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 8));
+        
+        m_anticipatedMemorySize = 4;
+        
+        verify();
+        
+        Thread.sleep(1100);
+
+        m_anticipatedMemorySize = 0;
+        
+        verify();
     }
     
-    public void testFlappingMonitor() {
-    	// receive three non alertable outages in 10 minutes
-    	
-    	// send a flapping event
+    public void testFlappingMonitor() throws Exception {
+        
+        anticipateServiceFlappingEvent();
+        
+        // recieve outage event for only a single monitor
+        m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        
+        m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 7));
+
+        m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        
+        m_engine.correlate(createRemoteNodeLostServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        m_engine.correlate(createRemoteNodeRegainedServiceEvent(1, "192.168.1.1", "HTTP", 7));
+        
+        m_anticipatedMemorySize = 5;
+        
+        verify();
+        
+        Thread.sleep(1100);
+        
+        // FIXME:This doesnt work unless we wait an extra secode for each flap.  NOT GOOD
+        Thread.sleep(3000);
+        
+        m_anticipatedMemorySize = 0;
+        
+        verify();
+        
+        
     }
 
 	private void anticipateWideSpreadOutageEvent() {
@@ -141,17 +194,8 @@ public class DroolsCorrelationEngineTest extends TestCase {
         m_anticipator.anticipateEvent(bldr.getEvent());
     }
 
-    private void anticipateIsolatedOutageResolvedEvent() {
-        EventBuilder bldr = new EventBuilder(ISO_RESOLVED_UEI, "Drools");
-        bldr.setNodeid(1)
-            .setInterface("192.168.1.1")
-            .setService("HTTP");
-        
-        m_anticipator.anticipateEvent(bldr.getEvent());
-    }
-
-    private void anticipateIsolatedOutageEvent() {
-        EventBuilder bldr = new EventBuilder(ISO_OUTAGE_UEI, "Drools");
+    private void anticipateServiceFlappingEvent() {
+        EventBuilder bldr = new EventBuilder(SERVICE_FLAPPING_UEI, "Drools");
         bldr.setNodeid(1)
             .setInterface("192.168.1.1")
             .setService("HTTP");
