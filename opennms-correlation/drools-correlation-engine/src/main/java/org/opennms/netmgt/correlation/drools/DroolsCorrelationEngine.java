6f92ca9398d59af149ed3f169f734e1b153b555e
Matt Brozowski <brozow@users.sourceforge.net>
2007-02-02 00:27:16 +0000

    add flapping rules that almost work

--- opennms-correlation/drools-correlation-engine/src/main/java/org/opennms/netmgt/correlation/drools/DroolsCorrelationEngine.java
+++ opennms-correlation/drools-correlation-engine/src/main/java/org/opennms/netmgt/correlation/drools/DroolsCorrelationEngine.java
@@ -10,6 +10,7 @@ import org.drools.RuleBase;
 import org.drools.RuleBaseFactory;
 import org.drools.WorkingMemory;
 import org.drools.compiler.PackageBuilder;
+import org.drools.compiler.PackageBuilderConfiguration;
 import org.opennms.netmgt.EventConstants;
 import org.opennms.netmgt.correlation.AbstractCorrelationEngine;
 import org.opennms.netmgt.xml.event.Event;
@@ -18,6 +19,9 @@ import org.springframework.beans.factory.InitializingBean;
 public class DroolsCorrelationEngine extends AbstractCorrelationEngine implements InitializingBean {
 
     private WorkingMemory m_workingMemory;
+    private Integer m_wideSpreadThreshold = 3;
+    private Long m_flapInterval = 60000L;
+    private Integer m_flapCount = 3;
 
     public void correlate(Event e) {
         m_workingMemory.assertObject(e);
@@ -34,7 +38,9 @@ public class DroolsCorrelationEngine extends AbstractCorrelationEngine implement
     }
 
     public void afterPropertiesSet() throws Exception {
-        PackageBuilder builder = new PackageBuilder();
+        PackageBuilderConfiguration conf = new PackageBuilderConfiguration();
+        conf.setJavaLanguageLevel( "1.5" );
+        PackageBuilder builder = new PackageBuilder( conf );
         
         Reader rdr = null;
         try {
@@ -49,10 +55,37 @@ public class DroolsCorrelationEngine extends AbstractCorrelationEngine implement
 
         m_workingMemory = ruleBase.newWorkingMemory();
         m_workingMemory.setGlobal("engine", this);
+        m_workingMemory.setGlobal("wideSpreadThreshold", m_wideSpreadThreshold);
+        m_workingMemory.setGlobal("flapInterval", m_flapInterval);
+        m_workingMemory.setGlobal("flapCount", m_flapCount);
     }
     
     public int getMemorySize() {
     	return m_workingMemory.getObjects().size();
+    }
+
+    public Integer getFlapCount() {
+        return m_flapCount;
+    }
+
+    public void setFlapCount(Integer flapCount) {
+        m_flapCount = flapCount;
+    }
+
+    public Long getFlapInterval() {
+        return m_flapInterval;
+    }
+
+    public void setFlapInterval(Long flapInterval) {
+        m_flapInterval = flapInterval;
+    }
+
+    public Integer getWideSpreadThreshold() {
+        return m_wideSpreadThreshold;
+    }
+
+    public void setWideSpreadThreshold(Integer wideSpreadThreshold) {
+        m_wideSpreadThreshold = wideSpreadThreshold;
     }
 
 }
---
+++ opennms-correlation/drools-correlation-engine/src/main/java/org/opennms/netmgt/correlation/drools/FlapCount.java
@@ -0,0 +1,78 @@
+package org.opennms.netmgt.correlation.drools;
+
+
+public class FlapCount {
+    Long m_nodeid;
+    String m_ipAddr;
+    String m_svcName;
+    Integer m_locationMonitor;
+    boolean m_alerted;
+
+    Integer m_count;
+    
+    public FlapCount(Long nodeid, String ipAddr, String svcName, Integer locationMonitor) {
+        m_nodeid = nodeid;
+        m_ipAddr = ipAddr;
+        m_svcName = svcName;
+        m_locationMonitor = locationMonitor;
+        m_count = 1;
+        m_alerted = false;
+    }
+    
+    public void increment() {
+        m_count += 1;
+    }
+    
+    public void decrement() {
+        m_count -= 1;
+    }
+
+    public Integer getCount() {
+        return m_count;
+    }
+
+    public void setCount(Integer count) {
+        m_count = count;
+    }
+
+    public String getIpAddr() {
+        return m_ipAddr;
+    }
+
+    public void setIpAddr(String ipAddr) {
+        m_ipAddr = ipAddr;
+    }
+
+    public Long getNodeid() {
+        return m_nodeid;
+    }
+
+    public void setNodeid(Long nodeid) {
+        m_nodeid = nodeid;
+    }
+
+    public String getSvcName() {
+        return m_svcName;
+    }
+
+    public void setSvcName(String svcName) {
+        m_svcName = svcName;
+    }
+
+    public boolean isAlerted() {
+        return m_alerted;
+    }
+
+    public void setAlerted(boolean alerted) {
+        m_alerted = alerted;
+    }
+
+    public Integer getLocationMonitor() {
+        return m_locationMonitor;
+    }
+
+    public void setLocationMonitor(Integer locationMonitor) {
+        m_locationMonitor = locationMonitor;
+    }
+
+}
