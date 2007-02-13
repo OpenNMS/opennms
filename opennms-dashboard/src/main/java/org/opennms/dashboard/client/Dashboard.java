15eedd984d022b2adcf71b0aacff67cb3722032a
Matt Brozowski <brozow@users.sourceforge.net>
2007-02-13 05:03:15 +0000

    more tweaks in the design

--- opennms-dashboard/src/main/java/org/opennms/dashboard/client/Dashboard.java
+++ opennms-dashboard/src/main/java/org/opennms/dashboard/client/Dashboard.java
@@ -2,21 +2,29 @@ package org.opennms.dashboard.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 public class Dashboard implements EntryPoint {
 
     public void onModuleLoad() {
 
-        DashboardLayout db = new DashboardLayout();
-        db.add("siteStatus", new SiteStatusViewer());
-        db.add("alarms", new AlarmViewer());
-        db.add("outages", new OutageViewer());
-        db.add("events", new EventViewer());
-        db.add("nodeStatus", new NodeStatusViewer());
-        db.add("graphs", new GraphViewer());
+        add(new SiteStatusViewer(),   "siteStatus");
+        add(new AlarmViewer(),        "alarms");
+        add(new OutageViewer(),       "outages");
+        add(new EventViewer(),        "events");
+        add(new NodeStatusViewer(),   "nodeStatus");
+        add(new NotificationViewer(), "notifications");
+        add(new GraphViewer(),        "graphs");
         
-
-        RootPanel.get().add(db);
     }
+    
+    public void add(Widget widget, String elementId) {
+        RootPanel panel = RootPanel.get(elementId);
+        if (panel == null) {
+            throw new IllegalArgumentException("element with id '"+elementId+"' not found!");
+        }
+        panel.add(widget);
+    }
+
 
 }
