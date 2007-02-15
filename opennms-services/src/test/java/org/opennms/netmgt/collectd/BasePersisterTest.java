f1759b3a129f84b1bc2830e44ef99f3d92517e0d
DJ Gregor <djgregor@users.sourceforge.net>
2007-02-15 09:35:04 +0000

    Fix for bug #1817 from agalue@sync.com.ve including test case. Don't declare an attribute to the builder (which is used just for RRD data) if it is not a NumericAttributeType. This also needs the recent fix to PersistOperationBuilder.commit(), otherwise exceptions will be thrown when an attempt is made to create an RRD file with no data sources.,
    Thanks, Alejandro!

--- opennms-services/src/test/java/org/opennms/netmgt/collectd/BasePersisterTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/collectd/BasePersisterTest.java
@@ -1,23 +1,64 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ * 
+ * OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified 
+ * and included code are below.
+ * 
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ * 
+ * Copyright (C) 2007 The OpenNMS Group, Inc.  All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
 package org.opennms.netmgt.collectd;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Properties;
 
-import org.opennms.netmgt.config.DataCollectionConfig;
+import junit.framework.TestCase;
+
 import org.opennms.netmgt.config.MibObject;
 import org.opennms.netmgt.mock.MockDataCollectionConfig;
 import org.opennms.netmgt.model.OnmsIpInterface;
 import org.opennms.netmgt.model.OnmsNode;
 import org.opennms.netmgt.model.RrdRepository;
+import org.opennms.netmgt.rrd.RrdConfig;
+import org.opennms.netmgt.rrd.RrdUtils;
+import org.opennms.netmgt.rrd.jrobin.JRobinRrdStrategy;
 import org.opennms.netmgt.snmp.TestSnmpValue;
 import org.opennms.test.FileAnticipator;
 import org.opennms.test.mock.MockLogAppender;
 import org.opennms.test.mock.MockUtil;
 
-import junit.framework.TestCase;
-
+/**
+ * JUnit TestCase for the BasePersister.
+ * 
+ * @author <a href="mailto:dj@opennms.org">DJ Gregor</a>
+ */
 public class BasePersisterTest extends TestCase {
+    private static boolean s_rrdInitialized = false;
     
     private FileAnticipator m_fileAnticipator;
     private File m_snmpDirectory;
@@ -38,6 +79,15 @@ public class BasePersisterTest extends TestCase {
         m_node = new OnmsNode();
         m_node.setId(1);
         m_intf.setNode(m_node);
+        m_intf.setIpAddress("1.1.1.1");
+        
+        // Grumble grumble... side effects... grumble grumble
+        if (!s_rrdInitialized) {
+            RrdConfig.setProperties(new Properties());
+            RrdUtils.setStrategy(new JRobinRrdStrategy());
+            RrdUtils.initialize();
+            s_rrdInitialized  = true;
+        }
     }
     
     @Override
@@ -84,13 +134,44 @@ public class BasePersisterTest extends TestCase {
         Attribute attribute = buildStringAttribute();
         m_persister.persistStringAttribute(attribute);
     }
+    
+    /**
+     * Test for bug #1817 where a string attribute will get persisted to
+     * both strings.properties and an RRD file if it is a numeric value.
+     */
+    public void testPersistStringAttributeUsingBuilder() throws Exception {
+        initPersister();
+        
+        File nodeDir = m_fileAnticipator.expecting(getSnmpRrdDirectory(), m_node.getId().toString());
+        m_fileAnticipator.expecting(nodeDir, "strings.properties");
+        
+        Attribute attribute = buildStringAttribute();
+
+        m_persister.pushShouldPersist(attribute.getResource());
+        
+        m_persister.pushShouldPersist(attribute);
+
+        m_persister.createBuilder(attribute.getResource(), attribute.getName(), attribute.getAttributeType());
+
+        // This will end up calling m_persister.persistStringAttribute(attribute);
+        m_persister.storeAttribute(attribute);
+
+        m_persister.commitBuilder();
+        
+        m_persister.popShouldPersist();
+    }
 
     private Attribute buildStringAttribute() {
         CollectionAgent agent = new CollectionAgent(m_intf);
-        DataCollectionConfig dataCollectionConfig = new MockDataCollectionConfig();
+        
+        MockDataCollectionConfig dataCollectionConfig = new MockDataCollectionConfig();
+        
         OnmsSnmpCollection collection = new OnmsSnmpCollection(agent, new ServiceParameters(new HashMap<String, String>()), dataCollectionConfig);
+        
         NodeResourceType resourceType = new NodeResourceType(agent, collection);
+        
         CollectionResource resource = new NodeInfo(resourceType, agent);
+        
         MibObject mibObject = new MibObject();
         mibObject.setOid(".1.1.1.1");
         mibObject.setAlias("mibObjectAlias");
@@ -98,17 +179,24 @@ public class BasePersisterTest extends TestCase {
         mibObject.setInstance("0");
         mibObject.setMaxval(null);
         mibObject.setMinval(null);
+        
         AttributeType attributeType = new StringAttributeType(resourceType, "some-collection", mibObject, new AttributeGroupType("mibGroup", "ignore"));
-        Attribute a = new Attribute(resource, attributeType, new TestSnmpValue.StringSnmpValue("foo"));
-        return a;
+        
+        return new Attribute(resource, attributeType, new TestSnmpValue.StringSnmpValue("foo"));
     }
 
     private void initPersister() throws IOException {
         m_persister = new BasePersister();
-        
+        m_persister.setRepository(createRrdRepository());
+    }
+
+    private RrdRepository createRrdRepository() throws IOException {
         RrdRepository repository = new RrdRepository();
         repository.setRrdBaseDir(getSnmpRrdDirectory());
-        m_persister.setRepository(repository);
+        repository.setHeartBeat(600);
+        repository.setStep(300);
+        repository.setRraList(Collections.singletonList("RRA:AVERAGE:0.5:1:100"));
+        return repository;
     }
 
     private File getSnmpRrdDirectory() throws IOException {
