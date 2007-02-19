d3b0b753907a688029a89398b0b4cab9b21da8e3
Matt Brozowski <brozow@users.sourceforge.net>
2007-02-19 19:11:07 +0000

    rename SiteStatus to SurveillanceView

--- opennms-webapp/src/main/java/org/opennms/dashboard/client/SurveillanceViewer.java
+++ opennms-webapp/src/main/java/org/opennms/dashboard/client/SurveillanceViewer.java
@@ -3,10 +3,10 @@ package org.opennms.dashboard.client;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 
-public class SiteStatusViewer extends Composite {
+public class SurveillanceViewer extends Composite {
     
-    public SiteStatusViewer() {
-        Label label = new Label("siteStatus");
+    public SurveillanceViewer() {
+        Label label = new Label("surveillanceView");
         initWidget(label);
     }
 
--- opennms-webapp/src/main/java/org/opennms/dashboard/server/NodeServiceImpl.java
+++ opennms-webapp/src/main/java/org/opennms/dashboard/server/NodeServiceImpl.java
@@ -1,6 +1,7 @@
 package org.opennms.dashboard.server;
 
 import org.opennms.dashboard.client.NodeService;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
@@ -8,6 +9,7 @@ public class NodeServiceImpl extends RemoteServiceServlet implements
         NodeService {
 
     public String[] getNodeNames() {
+        
         return new String[] {
                 "node1",
                 "node2",
