88cd68cae2f563d2e36c9a1ff2afe71c15231313
Matt Brozowski <brozow@users.sourceforge.net>
2007-02-22 00:06:20 +0000

    refactor SurveillanceDashlet to have a Loader/Viewer add a Loader/Viewer to AlarmDashlet

---
+++ opennms-webapp/src/main/java/org/opennms/dashboard/client/DashletLoader.java
@@ -0,0 +1,20 @@
+package org.opennms.dashboard.client;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+
+public class DashletLoader extends Composite {
+    
+    Label m_label = new Label();
+    
+    DashletLoader() {
+        initWidget(m_label);
+    }
+
+    public void setStatus(String status) {
+        m_label.setText(status);
+    }
+    
+    
+
+}
---
+++ opennms-webapp/src/main/java/org/opennms/dashboard/client/DashletView.java
@@ -0,0 +1,7 @@
+package org.opennms.dashboard.client;
+
+import com.google.gwt.user.client.ui.Composite;
+
+public class DashletView extends Composite {
+
+}
