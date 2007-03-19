5b3e5ca534cb6f7908d75a4d5719a45e5bd389cd
DJ Gregor <djgregor@users.sourceforge.net>
2007-03-19 17:07:18 +0000

    Add copyright and class comment. Add a test to get the values from PRINT.

--- opennms-rrd/opennms-rrd-jrobin/src/test/java/org/opennms/netmgt/rrd/jrobin/JRobinRrdStrategyTest.java
+++ opennms-rrd/opennms-rrd-jrobin/src/test/java/org/opennms/netmgt/rrd/jrobin/JRobinRrdStrategyTest.java
@@ -1,3 +1,34 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Copyright (C) 2007 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
 package org.opennms.netmgt.rrd.jrobin;
 
 import java.io.File;
@@ -6,9 +37,16 @@ import junit.framework.TestCase;
 
 import org.jrobin.graph.RrdGraph;
 import org.jrobin.graph.RrdGraphDef;
+import org.jrobin.graph.RrdGraphInfo;
 import org.opennms.netmgt.rrd.RrdException;
 import org.opennms.test.ThrowableAnticipator;
+import org.opennms.test.mock.MockLogAppender;
 
+/**
+ * Unit tests for the JrobinRrdStrategy.
+ * 
+ * @author <a href="mailto:dj@opennms.org">DJ Gregor</a>
+ */
 public class JRobinRrdStrategyTest extends TestCase {
     
     private JRobinRrdStrategy m_strategy;
@@ -17,8 +55,14 @@ public class JRobinRrdStrategyTest extends TestCase {
     protected void setUp() throws Exception {
         super.setUp();
         
+        MockLogAppender.setupLogging();
+        
         m_strategy = new JRobinRrdStrategy();
         m_strategy.initialize();
+    }
+    
+    public void testInitilize() {
+       // Don't do anything... just check that setUp works 
     }
     
     public void testCommandWithoutDrawing() throws Exception {
@@ -108,5 +152,30 @@ public class JRobinRrdStrategyTest extends TestCase {
         int secondHeight = graph2.getRrdGraphInfo().getHeight();
 
         assertFalse("first graph height " + firstHeight + " and second graph height " + secondHeight + " should not be equal... there should be another line with a newline in the second one making it taller", firstHeight == secondHeight);
+    }
+    
+    public void testPrint() throws Exception {
+        long end = System.currentTimeMillis();
+        long start = end - (24 * 60 * 60 * 1000);
+        String[] command = new String[] {
+                "--start=" + start,
+                "--end=" + end,
+                "CDEF:something=1",
+                "PRINT:something:AVERAGE:\"%le\""
+        };
+
+        RrdGraphDef graphDef = m_strategy.createGraphDef(new File(""), command);
+        RrdGraph graph = new RrdGraph(graphDef);
+        assertNotNull("graph object", graph);
+        
+        RrdGraphInfo info = graph.getRrdGraphInfo();
+        assertNotNull("graph info object", info);
+        
+        String[] printLines = info.getPrintLines();
+        assertNotNull("graph printLines", printLines);
+        assertEquals("graph printLines size", 1, printLines.length);
+        assertEquals("graph printLines item 0", "1.000000e+00", printLines[0]);
+        double d = Double.parseDouble(printLines[0]);
+        assertEquals("graph printLines item 0 as a double", 1.0, d);
     }
 }
