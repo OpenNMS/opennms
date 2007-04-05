a71b1fbed1a5ea01d32b94e2433a220d2adfb984
DJ Gregor <djgregor@users.sourceforge.net>
2007-04-05 11:51:13 +0000

    Add some javadocs

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/FilterDao.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/FilterDao.java
@@ -8,7 +8,19 @@ import java.util.SortedMap;
 import org.opennms.netmgt.filter.FilterParseException;
 
 public interface FilterDao {
+    /**
+     * This method returns a map of all node IDs and node labels that match
+     * the rule that is passed in, sorted by node ID.
+     * 
+     * @param rule an expression rule to be parsed and executed.
+     * 
+     * @return SortedMap containing all node IDs and node labels selected by the rule.
+     * 
+     * @exception FilterParseException if a rule is syntactically incorrect or failed in
+     *                executing the SQL statement
+     */
     public SortedMap<Integer, String> getNodeMap(String rule) throws FilterParseException;
+    
     public Map<String, Set<String>> getIPServiceMap(String rule) throws FilterParseException;
     public List<String> getIPList(String rule) throws FilterParseException;
     public boolean isValid(String addr, String rule) throws FilterParseException;
