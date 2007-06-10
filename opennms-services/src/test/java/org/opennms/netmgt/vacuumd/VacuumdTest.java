032c6a691de9e6cbe1c43620a90f9a62fc2e6221
DJ Gregor <djgregor@users.sourceforge.net>
2007-06-10 01:28:56 +0000

    Move vacuumd test config into its own test resource file.

--- opennms-services/src/test/java/org/opennms/netmgt/vacuumd/VacuumdTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/vacuumd/VacuumdTest.java
@@ -1,44 +1,42 @@
-//
-// This file is part of the OpenNMS(R) Application.
-//
-// OpenNMS(R) is Copyright (C) 2005 The OpenNMS Group, Inc.  All rights reserved.
-// OpenNMS(R) is a derivative work, containing both original code, included code and modified
-// code that was published under the GNU General Public License. Copyrights for modified 
-// and included code are below.
-//
-// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
-//
-// Modifications:
-//
-// 2007 Mar 13: VacuumdConfigFactory.setConfigReader(Reader) is gone.  Use new VacuumdConfigFactory(Reader) and setInstance, instead. - dj@opennms.org
-//
-// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.                                                            
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-//    
-// For more information contact: 
-//   OpenNMS Licensing       <license@opennms.org>
-//   http://www.opennms.org/
-//   http://www.opennms.com/
-//
-// Tab Size = 8
-
+/*
+ * This file is part of the OpenNMS(R) Application.
+ * 
+ * OpenNMS(R) is Copyright (C) 2005 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified 
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ *
+ * 2007 Jun 09: Move the config into a test resource. - dj@opennms.org
+ * 2007 Mar 13: VacuumdConfigFactory.setConfigReader(Reader) is gone.  Use new VacuumdConfigFactory(Reader) and setInstance, instead. - dj@opennms.org
+ *
+ * Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.                                                            
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *    
+ * For more information contact: 
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
 package org.opennms.netmgt.vacuumd;
 
 import java.io.Reader;
-import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -46,6 +44,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.commons.io.IOUtils;
 import org.opennms.core.fiber.Fiber;
 import org.opennms.core.fiber.PausableFiber;
 import org.opennms.netmgt.config.DataSourceFactory;
@@ -58,123 +57,35 @@ import org.opennms.netmgt.utils.EventBuilder;
 import org.opennms.netmgt.utils.Querier;
 import org.opennms.netmgt.utils.SingleResultQuerier;
 import org.opennms.netmgt.xml.event.Event;
+import org.opennms.test.ConfigurationTestUtils;
 import org.opennms.test.mock.MockUtil;
 
 public class VacuumdTest extends OpenNMSTestCase {
-
-    private final static String VACUUMD_CONFIG = 
-        "<VacuumdConfiguration period=\"86400000\" >\n" + 
-        "   <statement>\n" + 
-        "       <!-- this deletes all the nodes that have been marked as deleted - it relies on cascading deletes -->\n" + 
-        "       DELETE FROM node WHERE node.nodeType = \'D\';\n" + 
-        "   </statement>\n" + 
-        "   <statement>\n" + 
-        "       <!-- this deletes all the interfaces that have been marked as deleted - it relies on cascading deletes -->\n" + 
-        "       DELETE FROM ipInterface WHERE ipInterface.isManaged = \'D\';\n" + 
-        "   </statement>\n" + 
-        "   <statement>\n" + 
-        "      <!-- this deletes all the services that have been marked as deleted - it relies on cascading deletes -->\n" + 
-        "      DELETE FROM ifServices WHERE ifServices.status = \'D\';\n" + 
-        "   </statement>\n" + 
-        "    <statement>\n" + 
-        "       <!-- this deletes any events that are not associated with outages - Thanks to Chris Fedde for this -->\n" + 
-        "      DELETE FROM events WHERE NOT EXISTS \n" + 
-        "          (SELECT svclosteventid FROM outages WHERE svclosteventid = events.eventid  \n" + 
-        "        UNION \n" + 
-        "          SELECT svcregainedeventid FROM outages WHERE svcregainedeventid = events.eventid \n" + 
-        "        UNION \n" + 
-        "          SELECT eventid FROM notifications WHERE eventid = events.eventid) \n" + 
-        "       AND eventtime &lt; now() - interval \'6 weeks\';\n" + 
-        "    </statement>\n" +
-        "    <automations>\n" + 
-        "           <automation name=\"autoEscalate\" interval=\"10000\" trigger-name=\"selectWithCounter\" auto-event-name=\"escalationEvent\" action-name=\"escalate\" active=\"true\" />\n" + 
-        "           <automation name=\"testActionEvent\" interval=\"10000\" trigger-name=\"selectAll\" action-event=\"escalationEvent\" action-name=\"escalate\" active=\"true\" />\n" + 
-        "           <automation name=\"cleanUpAlarms\" interval=\"300000\" action-name=\"deleteDayOldAlarms\" active=\"true\" />\n" +
-        "           <automation name=\"cosmicClear\" interval=\"30000\" trigger-name=\"selectResolvers\" action-name=\"clearProblems\" active=\"true\" />\n" + 
-        "           <automation name=\"stormDetect\" interval=\"60000\" trigger-name=\"stormTrigger\" action-name=\"null\" auto-event-name=\"stormAlert\" active=\"true\" />" +
-        "           <automation name=\"testZeroResults\" interval=\"60000\" trigger-name=\"zeroResults\" action-name=\"null\" auto-event-name=\"stormAlert\" active=\"true\" />" +
-        "    </automations>\n" + 
-        "    <triggers>\n" +
-        "           <trigger name=\"zeroResults\" operator=\"&lt;\" row-count=\"1\" >\n" +
-        "               <statement>SELECT * FROM alarms where alarmid &lt; 0</statement>" +
-        "           </trigger>\n" +
-        "           <trigger name=\"selectAll\" operator=\"&gt;=\" row-count=\"1\" >\n" + 
-        "               <statement>SELECT * FROM alarms</statement>\n" + 
-        "           </trigger>\n" + 
-        "           <trigger name=\"selectWithCounter\" >\n" + 
-        "               <statement>SELECT alarmid FROM alarms WHERE counter &gt;= 2</statement>\n" + 
-        "           </trigger>\n" + 
-        "           <trigger name=\"selectResolvers\" operator=\"&gt;=\" row-count=\"1\" >\n" + 
-        "               <statement>SELECT * FROM alarms WHERE alarmType=2</statement>\n" + 
-        "           </trigger>\n" + 
-        "           <trigger name=\"stormTrigger\" operator=\"&gt;=\" row-count=\"1\" >\n" + 
-        "               <statement>SELECT * FROM events</statement>\n" + 
-        "           </trigger>\n" + 
-        "    </triggers>\n" + 
-        "    <actions>\n" + 
-        "           <action name=\"clear\" >\n" + 
-        "               <statement>UPDATE alarms SET severity=2 WHERE alarmid = ${alarmid}</statement>\n" + 
-        "           </action>\n" + 
-        "           <action name=\"clearProblems\" >\n" + 
-        "               <statement>UPDATE alarms SET severity=2 WHERE alarmType=1 AND severity > 2 AND eventUei = ${clearUei} AND dpName = ${dpName} and nodeID = ${nodeID} and ipaddr = ${ipaddr} and serviceID = ${serviceID}</statement>\n" + 
-//        "               <statement>UPDATE alarms SET severity=2 WHERE alarmType=1 AND severity > 2 AND dpName = ${dpName} and nodeID = ${nodeID} and ipaddr = ${ipaddr} and serviceID = ${serviceID}</statement>\n" + 
-        "           </action>\n" + 
-        "           <action name=\"escalate\" >\n" + 
-        "               <statement>UPDATE alarms SET severity = severity+1 WHERE alarmid = ${alarmid} and alarmAckUser is null and severity &lt; 7</statement>\n" + 
-        "           </action>\n" + 
-        "           <action name=\"delete\" >\n" + 
-        "               <statement>DELETE FROM alarms WHERE nodeid = ${nodeid} and eventuei = ${eventuei}</statement>\n" + 
-        "           </action>\n" + 
-        "           <action name=\"deleteDayOldAlarms\" >\n" + 
-        "               <statement>DELETE FROM alarms WHERE alarmAckUser IS NOT NULL AND lastEventTime &lt; CURRENT_TIMESTAMP</statement>\n" + 
-        "           </action>\n" + 
-        "           <action name=\"null\" >\n" + 
-        "               <statement>update node SET nodelabel = \'foxtel\' where dpname = \'1.1.1.1\'</statement>\n" + 
-        "           </action>\n" + 
-        "    </actions>\n" + 
-        "    <auto-events>\n" +
-        "        <auto-event name=\"escalationEvent\" >\n" +
-        "            <uei>uei.opennms.org/vacuumd/alarmEscalated</uei>\n" +
-        "        </auto-event>\n" +
-        "        <auto-event name=\"stormAlert\" >\n" + 
-        "            <uei>FOXTEL/System/OpenNMS/MessageStormDetected</uei>\n" + 
-        "        </auto-event>\n" + 
-        "    </auto-events>" +
-        "   <action-events>" +
-        "       <action-event name=\"escalationEvent\" for-each-result=\"true\">" +
-        "           <assignment type=\"field\" name=\"uei\" value=\"uei.opennms.org/vacuumd/alarmEscalated\" />" +
-        "           <assignment type=\"field\" name=\"nodeid\" value=\"${nodeid}\" />" +
-        "           <assignment type=\"field\" name=\"interface\" value=\"${ipaddr}\" />" +
-        "           <assignment type=\"field\" name=\"service\" value=\"TEST\" />" +
-        "           <assignment type=\"parameter\" name=\"alarmId\" value=\"${alarmId}\" />" +
-        "       </action-event>" +
-        "   </action-events>\n" +
-        "" + 
-        "</VacuumdConfiguration>";
+    private static final long TEAR_DOWN_WAIT_MILLIS = 1000;
     
     private Vacuumd m_vacuumd;
-    private long m_tearDownWaitMillis = 1000;
 
     protected void setUp() throws Exception {
-        
         super.setUp();
-        Reader rdr = new StringReader(VACUUMD_CONFIG);
-        VacuumdConfigFactory.setInstance(new VacuumdConfigFactory(rdr));
+        
+        Reader rdr = ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/vacuumd/vacuumd-configuration.xml");
+        try {
+            VacuumdConfigFactory.setInstance(new VacuumdConfigFactory(rdr));
+        } finally {
+            IOUtils.closeQuietly(rdr);
+        }
+
         m_vacuumd = Vacuumd.getSingleton();
         m_vacuumd.setEventManager(m_eventdIpcMgr);
         m_vacuumd.init();
-        
-        //The rdr is closed by init, too, but doesn't hurt
-        rdr.close();
 
         MockUtil.println("------------ Finished setup for: "+getName()+" --------------------------");
-        
     }
 
     protected void tearDown() throws Exception {
         super.tearDown();
-        MockUtil.println("Sleeping for "+m_tearDownWaitMillis+" millis in tearDown...");
-        Thread.sleep(m_tearDownWaitMillis);
+        MockUtil.println("Sleeping for "+TEAR_DOWN_WAIT_MILLIS+" millis in tearDown...");
+        Thread.sleep(TEAR_DOWN_WAIT_MILLIS);
     }
     
     /**
@@ -182,7 +93,6 @@ public class VacuumdTest extends OpenNMSTestCase {
      * @throws InterruptedException
      */
     public final void testConcurrency() throws InterruptedException {
-        
         /*
          * Test status of threads
          */
@@ -206,16 +116,15 @@ public class VacuumdTest extends OpenNMSTestCase {
         assertEquals(PausableFiber.PAUSED, m_vacuumd.getStatus());
         assertEquals(PausableFiber.PAUSED, m_vacuumd.getScheduler().getStatus());
 
-
         m_vacuumd.resume();
         Thread.sleep(200);
         assertEquals(PausableFiber.RUNNING, m_vacuumd.getStatus());
         assertEquals(PausableFiber.RUNNING, m_vacuumd.getScheduler().getStatus());
-
         
         //Get an alarm in the db
         bringNodeDownCreatingEvent(1);
         Thread.sleep(500);
+        
         /*
          * Changes to the automations to the VACUUMD_CONFIG will
          * probably affect this.  There should be one node down
@@ -248,14 +157,12 @@ public class VacuumdTest extends OpenNMSTestCase {
                 
         //Stop what you start.
         m_vacuumd.stop();
-
     }
     
     /**
      * Test resultSetHasRequiredActionColumns method
      * @throws SQLException 
      */
-    
     public final void testResultSetHasRequiredActionColumns() throws SQLException {
         Connection conn = DataSourceFactory.getInstance().getConnection();
         Statement stmt = conn.createStatement();
@@ -264,6 +171,7 @@ public class VacuumdTest extends OpenNMSTestCase {
         AutomationProcessor ap = new AutomationProcessor(VacuumdConfigFactory.getInstance().getAutomation("cosmicClear"));
         assertTrue(ap.resultSetHasRequiredActionColumns(rs, columns));
     }
+
     /**
      * Simple test on a helper method.
      */
@@ -324,7 +232,6 @@ public class VacuumdTest extends OpenNMSTestCase {
      * Simple test running a trigger.
      */
     public final void testRunTrigger() throws InterruptedException {
-        
         //Get all the triggers defined in the config
         ArrayList triggers = (ArrayList)VacuumdConfigFactory.getInstance().getTriggers();
         assertEquals(5, triggers.size());
@@ -349,7 +256,6 @@ public class VacuumdTest extends OpenNMSTestCase {
      * @throws InterruptedException 
      */
     public final void testRunAutomation() throws SQLException, InterruptedException {
-
         final int major = 6;
         
         bringNodeDownCreatingEvent(1);
@@ -365,11 +271,9 @@ public class VacuumdTest extends OpenNMSTestCase {
         Thread.sleep(500);
         assertTrue(ap.runAutomation());        
         assertEquals(major+1, getSingleResultSeverity());
-        
     }
     
     public final void testRunAutomationWithNoTrigger() throws InterruptedException, SQLException {
-        
         bringNodeDownCreatingEvent(1);
         Thread.sleep(500);
         
@@ -394,7 +298,6 @@ public class VacuumdTest extends OpenNMSTestCase {
      * @throws InterruptedException 
      */
     public final void testCosmicClearAutomation() throws InterruptedException {
-
         // create node down events with severity 6
         bringNodeDownCreatingEvent(1);
         bringNodeDownCreatingEvent(2);
@@ -443,21 +346,18 @@ public class VacuumdTest extends OpenNMSTestCase {
         srq.execute();
         rows = (Integer)srq.getResult();
         assertEquals(2, rows.intValue());
-        
     }
 
     /**
      * Test the ability to find tokens in a statement.
      */
     public void testGetTokenizedColumns() {
-        
         AutomationProcessor ap = new AutomationProcessor(VacuumdConfigFactory.getInstance().getAutomation("cosmicClear"));
         
         Collection tokens = ap.getAction().getActionColumns();
 
         //just this for now
         assertFalse(tokens.isEmpty());
-        
     }
     
     /**
@@ -478,7 +378,6 @@ public class VacuumdTest extends OpenNMSTestCase {
         Automation auto = VacuumdConfigFactory.getInstance().getAutomation("cleanUpAlarms");
         AutomationProcessor ap = new AutomationProcessor(auto);
         assertEquals(null, ap.getTrigger().getTriggerSQL());
-        
     }
     
     /**
@@ -556,5 +455,4 @@ public class VacuumdTest extends OpenNMSTestCase {
         MockNode node = m_network.getNode(nodeid);
         m_eventd.processEvent(node.createUpEvent());
     }
-
 }
