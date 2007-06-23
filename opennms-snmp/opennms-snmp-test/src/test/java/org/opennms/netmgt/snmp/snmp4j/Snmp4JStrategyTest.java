b6bc012f0481287f198e16af6cda3e5ed1b9254e
DJ Gregor <djgregor@users.sourceforge.net>
2007-06-23 17:46:25 +0000

    - Add tests for building PDUs.

--- opennms-snmp/opennms-snmp-test/src/test/java/org/opennms/netmgt/snmp/snmp4j/Snmp4JStrategyTest.java
+++ opennms-snmp/opennms-snmp-test/src/test/java/org/opennms/netmgt/snmp/snmp4j/Snmp4JStrategyTest.java
@@ -8,6 +8,10 @@
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
+ * Modifications:
+ * 
+ * 2007 Jun 23: Add tests for building PDUs. - dj@opennms.org
+ * 
  * Copyright (C) 2007 The OpenNMS Group, Inc.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
@@ -35,13 +39,18 @@ import java.net.InetAddress;
 
 import junit.framework.TestCase;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.spi.LoggingEvent;
 import org.opennms.mock.snmp.MockSnmpAgent;
 import org.opennms.netmgt.snmp.SnmpAgentConfig;
 import org.opennms.netmgt.snmp.SnmpObjId;
 import org.opennms.netmgt.snmp.SnmpValue;
+import org.opennms.netmgt.snmp.mock.TestSnmpValue.StringSnmpValue;
 import org.opennms.test.mock.MockLogAppender;
 import org.opennms.test.mock.MockUtil;
 import org.snmp4j.PDU;
+import org.snmp4j.smi.SMIConstants;
+import org.snmp4j.smi.VariableBinding;
 import org.springframework.core.io.ClassPathResource;
 
 /**
@@ -88,11 +97,34 @@ public class Snmp4JStrategyTest extends TestCase {
     public void testNothing() throws Exception {
         // Just test our setUp()/tearDown()
     }
+    
+    public void XXXtestSendWithNullConfig() throws Exception {
+        SnmpObjId[] oids = new SnmpObjId[] { SnmpObjId.get(".1.3.5.1.1.3.0") };
+        SnmpValue[] retvalues = null;
+        
+        PDU pdu = m_strategy.buildPdu(null, PDU.GET, oids, null);
+        if (pdu != null) {
+            retvalues = m_strategy.send(null, pdu, true);
+        }
+        
+        SnmpValue[] values = retvalues;
+        
+        assertNotNull("values should not be null", values);
+        assertEquals("values list size", 1, values.length);
+        assertSnmpValueEquals("values[0]", SnmpValue.SNMP_INT32, 42, values[0]);
+    }
 
     public void testSendWithGetPduSingleValue() throws Exception {
         SnmpObjId[] oids = new SnmpObjId[] { SnmpObjId.get(".1.3.5.1.1.3.0") };
+        Snmp4JAgentConfig agentConfig = new Snmp4JAgentConfig(getAgentConfig());
+        SnmpValue[] retvalues = null;
         
-        SnmpValue[] values = m_strategy.send(new Snmp4JAgentConfig(getAgentConfig()), PDU.GET, oids);
+        PDU pdu = m_strategy.buildPdu(agentConfig, PDU.GET, oids, null);
+        if (pdu != null) {
+            retvalues = m_strategy.send(agentConfig, pdu, true);
+        }
+        
+        SnmpValue[] values = retvalues;
         
         assertNotNull("values should not be null", values);
         assertEquals("values list size", 1, values.length);
@@ -104,8 +136,15 @@ public class Snmp4JStrategyTest extends TestCase {
                 SnmpObjId.get(".1.3.5.1.1.3.0"),
                 SnmpObjId.get(".1.3.5.1.1.4.0"),
         };
+        Snmp4JAgentConfig agentConfig = new Snmp4JAgentConfig(getAgentConfig());
+        SnmpValue[] retvalues = null;
         
-        SnmpValue[] values = m_strategy.send(new Snmp4JAgentConfig(getAgentConfig()), PDU.GET, oids);
+        PDU pdu = m_strategy.buildPdu(agentConfig, PDU.GET, oids, null);
+        if (pdu != null) {
+            retvalues = m_strategy.send(agentConfig, pdu, true);
+        }
+        
+        SnmpValue[] values = retvalues;
         
         assertNotNull("values should not be null", values);
         assertEquals("values list size", 2, values.length);
@@ -116,8 +155,15 @@ public class Snmp4JStrategyTest extends TestCase {
 
     public void testSendWithGetNextPduSingleValue() throws Exception {
         SnmpObjId[] oids = new SnmpObjId[] { SnmpObjId.get(".1.3.5.1.1.3.0") };
+        Snmp4JAgentConfig agentConfig = new Snmp4JAgentConfig(getAgentConfig());
+        SnmpValue[] retvalues = null;
         
-        SnmpValue[] values = m_strategy.send(new Snmp4JAgentConfig(getAgentConfig()), PDU.GETNEXT, oids);
+        PDU pdu = m_strategy.buildPdu(agentConfig, PDU.GETNEXT, oids, null);
+        if (pdu != null) {
+            retvalues = m_strategy.send(agentConfig, pdu, true);
+        }
+        
+        SnmpValue[] values = retvalues;
         
         assertNotNull("values should not be null", values);
         assertEquals("values list size", 1, values.length);
@@ -130,8 +176,19 @@ public class Snmp4JStrategyTest extends TestCase {
                 SnmpObjId.get(".1.3.5.1.1.3.0"),
                 SnmpObjId.get(".1.3.5.1.1.4.0"),
         };
+        Snmp4JAgentConfig agentConfig = new Snmp4JAgentConfig(getAgentConfig());
+        SnmpValue[] retvalues = null;
         
-        SnmpValue[] values = m_strategy.send(new Snmp4JAgentConfig(getAgentConfig()), PDU.GETNEXT, oids);
+        /*
+         * Build the PDU first, since there isn't any value in doing other
+         * work like setting up the session if we don't have anything to send.
+         */
+        PDU pdu = m_strategy.buildPdu(agentConfig, PDU.GETNEXT, oids, null);
+        if (pdu != null) {
+            retvalues = m_strategy.send(agentConfig, pdu, true);
+        }
+        
+        SnmpValue[] values = retvalues;
         
         assertNotNull("values should not be null", values);
         assertEquals("values list size", 2, values.length);
@@ -190,6 +247,96 @@ public class Snmp4JStrategyTest extends TestCase {
         assertSnmpValueEquals("values[0]", SnmpValue.SNMP_GAUGE32, 42, values[0]);
         // Expect the *next* value, so for .1.3.5.1.1.5.0
         assertSnmpValueEquals("values[1]", SnmpValue.SNMP_COUNTER32, 42, values[1]);
+    }
+    
+    public void testPreparePduWithNoValues() throws Exception {
+        SnmpObjId[] oids = new SnmpObjId[] {
+                SnmpObjId.get(".1.3.5.1.1.3.0"),
+                SnmpObjId.get(".1.3.5.1.1.4.0"),
+        };
+        
+        SnmpValue[] values = null;
+        
+        PDU pdu = m_strategy.buildPdu(new Snmp4JAgentConfig(getAgentConfig()), PDU.SET, oids, values);
+        assertNotNull("PDU should not be null", pdu);
+        
+        assertEquals("PDU variable bindings size", oids.length, pdu.getVariableBindings().size());
+        
+        for (int i = 0; i < oids.length; i++) {
+            VariableBinding vb = pdu.get(i);
+            assertEquals("PDU variable binding " + i + " OID", oids[i].toString(), "." + vb.getOid().toString());
+            assertEquals("PDU variable binding " + i + " syntax", vb.getSyntax(), SMIConstants.SYNTAX_NULL);
+            assertEquals("PDU variable binding " + i + " variable syntax", vb.getVariable().getSyntax(), SMIConstants.SYNTAX_NULL);
+        }
+    }
+    
+    public void testPreparePduWithValues() throws Exception {
+        SnmpObjId[] oids = new SnmpObjId[] {
+                SnmpObjId.get(".1.3.5.1.1.3.0"),
+                SnmpObjId.get(".1.3.5.1.1.4.0"),
+        };
+        
+        SnmpValue[] values = new SnmpValue[] {
+                new StringSnmpValue("foo"),
+                new StringSnmpValue("bar")
+        };
+        
+        PDU pdu = m_strategy.buildPdu(new Snmp4JAgentConfig(getAgentConfig()), PDU.SET, oids, values);
+        assertNotNull("PDU should not be null", pdu);
+        
+        assertEquals("PDU variable bindings size", oids.length, pdu.getVariableBindings().size());
+        
+        for (int i = 0; i < oids.length; i++) {
+            VariableBinding vb = pdu.get(i);
+            assertEquals("PDU variable binding " + i + " OID", oids[i].toString(), "." + vb.getOid().toString());
+            assertEquals("PDU variable binding " + i + " syntax", vb.getSyntax(), SMIConstants.SYNTAX_OCTET_STRING);
+            assertEquals("PDU variable binding " + i + " variable syntax", vb.getVariable().getSyntax(), SMIConstants.SYNTAX_OCTET_STRING);
+            assertEquals("PDU variable binding " + i + " variable value", vb.getVariable().toString(), values[i].toString());
+        }
+    }
+    
+    public void testPreparePduWithTooFewValues() throws Exception {
+        SnmpObjId[] oids = new SnmpObjId[] {
+                SnmpObjId.get(".1.3.5.1.1.3.0"),
+                SnmpObjId.get(".1.3.5.1.1.4.0"),
+        };
+        
+        SnmpValue[] values = new SnmpValue[] {
+                new StringSnmpValue("foo"),
+        };
+        
+        PDU pdu = m_strategy.buildPdu(new Snmp4JAgentConfig(getAgentConfig()), PDU.SET, oids, values);
+        assertNull("PDU should be null", pdu);
+        
+        LoggingEvent[] events = MockLogAppender.getEventsGreaterOrEqual(Level.WARN);
+        assertEquals("number of logged events", 1, events.length);
+        assertEquals("first logged event severity (should be ERROR)", Level.ERROR, events[0].getLevel());
+        
+        MockLogAppender.resetEvents();
+        MockLogAppender.resetLogLevel();
+    }
+    
+    public void testPreparePduWithTooManyValues() throws Exception {
+        SnmpObjId[] oids = new SnmpObjId[] {
+                SnmpObjId.get(".1.3.5.1.1.3.0"),
+                SnmpObjId.get(".1.3.5.1.1.4.0"),
+        };
+        
+        SnmpValue[] values = new SnmpValue[] {
+                new StringSnmpValue("foo"),
+                new StringSnmpValue("bar"),
+                new StringSnmpValue("baz")
+        };
+        
+        PDU pdu = m_strategy.buildPdu(new Snmp4JAgentConfig(getAgentConfig()), PDU.SET, oids, values);
+        assertNull("PDU should be null", pdu);
+        
+        LoggingEvent[] events = MockLogAppender.getEventsGreaterOrEqual(Level.WARN);
+        assertEquals("number of logged events", 1, events.length);
+        assertEquals("first logged event severity (should be ERROR)", Level.ERROR, events[0].getLevel());
+        
+        MockLogAppender.resetEvents();
+        MockLogAppender.resetLogLevel();
     }
     
     private SnmpAgentConfig getAgentConfig() {
