92a1f5a0c3d7bae009b0af73c5dcacf77d400a1a
DJ Gregor <djgregor@users.sourceforge.net>
2007-07-03 16:24:55 +0000

    Organize imports.

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/jmx/ServiceDaemonStub.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/jmx/ServiceDaemonStub.java
@@ -8,6 +8,10 @@
 //
 // OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
 //
+// Modifications:
+//
+// 2007 Jul 03: Organize imports. - dj@opennms.org
+//
 // Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
 //
 // This program is free software; you can redistribute it and/or modify
@@ -34,7 +38,6 @@
  */
 package org.opennms.netmgt.dao.jmx;
 
-import org.opennms.netmgt.model.ServiceDaemon;
 
 public class ServiceDaemonStub implements ServiceDaemonStubMBean {
 	
