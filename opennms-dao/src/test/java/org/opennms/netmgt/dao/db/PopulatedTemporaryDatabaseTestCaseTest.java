6d0009b1bc9f4ee387d824f764c84cb9105b4691
DJ Gregor <djgregor@users.sourceforge.net>
2007-07-03 16:12:10 +0000

    Work on having the tests use the Pl/PgSQL version of iplike.

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/db/PopulatedTemporaryDatabaseTestCaseTest.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/db/PopulatedTemporaryDatabaseTestCaseTest.java
@@ -57,7 +57,7 @@ public class PopulatedTemporaryDatabaseTestCaseTest extends
     }
     
     public void testUpdateIplike() throws Exception {
-        getInstallerDb().setPgIpLikeLocation(findIpLikeLibrary().getAbsolutePath());
+        getInstallerDb().setPgIpLikeLocation(null);
         getInstallerDb().updateIplike();
         getInstallerDb().closeConnection();
     }
