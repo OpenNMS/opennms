4e37ac190138a93e15e581aad83a0f08b30e8494
Matt Brozowski <brozow@users.sourceforge.net>
2007-07-13 20:49:19 +0000

    add attributeSieve, reorg Controller to use a command object, only show resources that have attributes

--- opennms-webapp/src/main/java/org/opennms/web/controller/RrdSummaryController.java
+++ opennms-webapp/src/main/java/org/opennms/web/controller/RrdSummaryController.java
@@ -7,15 +7,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.exolab.castor.xml.Marshaller;
 import org.opennms.netmgt.config.attrsummary.Summary;
-import org.opennms.web.MissingParameterException;
 import org.opennms.web.svclayer.RrdSummaryService;
+import org.opennms.web.svclayer.SummarySpecification;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.AbstractFormController;
 
-public class RrdSummaryController extends AbstractController implements InitializingBean {
+public class RrdSummaryController extends AbstractFormController implements InitializingBean {
 	
 	static class MarshalledView implements View {
 
@@ -24,8 +25,8 @@ public class RrdSummaryController extends AbstractController implements Initiali
 		}
 
 		public void render(Map model, HttpServletRequest request, HttpServletResponse response) throws Exception {
-			Assert.notNull(model.get("root"), "root must not be null.. unable to marshall xml");
-			Marshaller.marshal(model.get("root"), response.getWriter());
+			Assert.notNull(model.get("summary"), "summary must not be null.. unable to marshall xml");
+			Marshaller.marshal(model.get("summary"), response.getWriter());
 		}
 		
 	}
@@ -33,46 +34,17 @@ public class RrdSummaryController extends AbstractController implements Initiali
 	private RrdSummaryService m_rrdSummaryService;
 	
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-
-        String[] requiredParameters = new String[] {
-                "filterRule",
-                "start",
-                "end"
-        };
-        
-        for (String requiredParameter : requiredParameters) {
-            if (request.getParameter(requiredParameter) == null) {
-                throw new MissingParameterException(requiredParameter,
-                                                    requiredParameters);
-            }
-        }
-
-        String filterRule = request.getParameter("filterRule");
-        String start = request.getParameter("start");
-        String end = request.getParameter("end");
-        
-        long startTime;
-        long endTime;
-        try {
-            startTime = Long.parseLong(start);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Could not parse start '"
-                                               + start + "' as an integer time: " + e.getMessage(), e);
-        }
-        try {
-            endTime = Long.parseLong(end);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Could not parse end '"
-                                               + end + "' as an integer time: " + e.getMessage(), e);
-        }
-        
-        
-        Summary summary = m_rrdSummaryService.getSummary(filterRule, startTime, endTime);
-        return new ModelAndView(new MarshalledView(), "root", summary); 
+	public RrdSummaryController() {
+	    super();
+	    setCommandClass(SummarySpecification.class);
 	}
+
+    private ModelAndView getSummary(SummarySpecification spec) {
+        Summary summary = m_rrdSummaryService.getSummary(spec);
+        return new ModelAndView(new MarshalledView(), "summary", summary);
+    }
+	
+	
 
 
 	public void afterPropertiesSet() throws Exception {
@@ -83,5 +55,27 @@ public class RrdSummaryController extends AbstractController implements Initiali
 	public void setRrdSummaryService(RrdSummaryService rrdSummaryService) {
 		m_rrdSummaryService = rrdSummaryService;
 	}
+
+    @Override
+    protected boolean isFormSubmission(HttpServletRequest request) {
+        return true;
+    }
+
+    @Override
+    protected ModelAndView processFormSubmission(HttpServletRequest request,
+            HttpServletResponse response, Object command, BindException errors)
+            throws Exception {
+        return getSummary((SummarySpecification)command);
+        
+    }
+
+    @Override
+    protected ModelAndView showForm(HttpServletRequest request,
+            HttpServletResponse response, BindException errors)
+            throws Exception {
+        throw new UnsupportedOperationException("RrdSummaryController.showForm is not yet implemented");
+    }
+
+
 
 }
