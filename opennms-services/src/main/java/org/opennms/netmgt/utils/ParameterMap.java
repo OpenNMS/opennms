4ed47cf180490c8cd7d0b534a702a15af3b92a08
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-08-14 20:21:22 +0000

    move the existing ICMP code to the new API the discovery code has not been refactored yet; I fear it's crazy threading stuff  :)

--- opennms-services/src/main/java/org/opennms/netmgt/utils/ParameterMap.java
+++ opennms-services/src/main/java/org/opennms/netmgt/utils/ParameterMap.java
@@ -48,6 +48,35 @@ import org.opennms.core.utils.ThreadCategory;
  * map.
  */
 public class ParameterMap extends Object {
+	
+    /**
+     * This method is used to lookup a specific key in the map. If the mapped
+     * value is a string it is converted to a long and the original string
+     * value is replaced in the map. The converted value is returned to the
+     * caller. If the value cannot be converted then the default value is stored
+     * in the map. If the specified key does not exist in the map then the
+     * default value is returned.
+     * 
+     * @return The long value associated with the key.
+     */
+	public static long getKeyedLong(Map map, String key, long defValue) {
+        long value = defValue;
+        Object oValue = map.get(key);
+
+        if (oValue != null && oValue instanceof String) {
+            try {
+                value = Integer.parseInt((String) oValue);
+            } catch (NumberFormatException ne) {
+                value = defValue;
+                ThreadCategory.getInstance(ParameterMap.class).info("getLongByKey: Failed to convert value " + oValue + " for key " + key);
+            }
+            map.put(key, new Long(value));
+        } else if (oValue != null) {
+            value = ((Long) oValue).longValue();
+        }
+        return value;
+	}
+	
     /**
      * This method is used to lookup a specific key in the map. If the mapped
      * value is a string it is converted to an integer and the original string
@@ -59,21 +88,7 @@ public class ParameterMap extends Object {
      * @return The int value associated with the key.
      */
     public static int getKeyedInteger(Map map, String key, int defValue) {
-        int value = defValue;
-        Object oValue = map.get(key);
-
-        if (oValue != null && oValue instanceof String) {
-            try {
-                value = Integer.parseInt((String) oValue);
-            } catch (NumberFormatException ne) {
-                value = defValue;
-                ThreadCategory.getInstance(ParameterMap.class).info("getIntByKey: Failed to convert value " + oValue + " for key " + key);
-            }
-            map.put(key, new Integer(value));
-        } else if (oValue != null) {
-            value = ((Integer) oValue).intValue();
-        }
-        return value;
+    	return new Long(ParameterMap.getKeyedLong(map, key, new Long(defValue))).intValue();
     }
 
     /**
