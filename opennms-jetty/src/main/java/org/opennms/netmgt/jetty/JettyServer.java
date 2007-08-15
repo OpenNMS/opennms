04fd54653ce66b1b21b2271417af995c1b6809a8
David Hustace <dhustace@users.sourceforge.net>
2007-08-15 21:28:22 +0000

    added license info and javadoc

--- opennms-jetty/src/main/java/org/opennms/netmgt/jetty/JettyServer.java
+++ opennms-jetty/src/main/java/org/opennms/netmgt/jetty/JettyServer.java
@@ -1,9 +1,41 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ *
+ * Copyright (C) 2007 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
 package org.opennms.netmgt.jetty;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.util.Hashtable;
 
 import org.mortbay.jetty.Connector;
 import org.mortbay.jetty.Server;
@@ -12,14 +44,16 @@ import org.mortbay.jetty.nio.SelectChannelConnector;
 import org.mortbay.jetty.webapp.WebAppContext;
 import org.opennms.netmgt.daemon.AbstractServiceDaemon;
 import org.opennms.netmgt.daemon.SpringServiceDaemon;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Hashtable;
-
-import org.opennms.serviceregistration.ServiceRegistrationStrategy;
 import org.opennms.serviceregistration.ServiceRegistrationFactory;
+import org.opennms.serviceregistration.ServiceRegistrationStrategy;
 
+/**
+ * Implements Web Application within OpenNMS as a Service Daemon.
+ * 
+ * @author <a href="mailto:brozow@opennms.org">Mathew Brozowski</a>
+ * @author <a href="mailto:david@opennms.org">David Hustace</a>
+ *
+ */
 public class JettyServer extends AbstractServiceDaemon implements SpringServiceDaemon {
     
 	int m_port = 8080;
@@ -109,6 +143,4 @@ public class JettyServer extends AbstractServiceDaemon implements SpringServiceD
         }
     }
     
-    
-
 }
