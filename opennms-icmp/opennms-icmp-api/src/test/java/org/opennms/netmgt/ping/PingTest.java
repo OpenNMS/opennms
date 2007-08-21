4956923c06b51e27ab2f67fe2933e1b594d86a86
DJ Gregor <djgregor@users.sourceforge.net>
2007-08-21 02:25:33 +0000

    s/tabs/spaces/

--- opennms-icmp/opennms-icmp-api/src/test/java/org/opennms/netmgt/ping/PingTest.java
+++ opennms-icmp/opennms-icmp-api/src/test/java/org/opennms/netmgt/ping/PingTest.java
@@ -11,9 +11,9 @@ import junit.framework.TestCase;
 
 public class PingTest extends TestCase {
     private Pinger m_pinger = null;
-	private InetAddress m_goodHost = null;
-	private InetAddress m_badHost = null;
-    
+    private InetAddress m_goodHost = null;
+    private InetAddress m_badHost = null;
+
     /**
      * Don't run this test unless the <classNameWithPackage>.runTest property
      * is set to "true".
@@ -24,7 +24,7 @@ public class PingTest extends TestCase {
             System.err.println("Skipping test '" + getName() + "' because system property '" + getRunTestProperty() + "' is not set to 'true'");
             return;
         }
-        
+
         super.runTest();
     }
 
@@ -37,50 +37,50 @@ public class PingTest extends TestCase {
     }
 
     @Override
-	protected void setUp() throws Exception {
+    protected void setUp() throws Exception {
         if (!isRunTest()) {
             return;
         }
 
         super.setUp();
-		m_pinger = new Pinger();
-		m_goodHost = InetAddress.getByName("www.google.com");
-		m_badHost  = InetAddress.getByName("1.1.1.1");
-	}
+        m_pinger = new Pinger();
+        m_goodHost = InetAddress.getByName("www.google.com");
+        m_badHost  = InetAddress.getByName("1.1.1.1");
+    }
 
-	public void testSinglePing() throws Exception {
-		assertTrue(m_pinger.ping(m_goodHost) > 0);
-		Thread.sleep(1000);
-	}
-	
-	public void testSinglePingFailure() throws Exception {
-		assertNull(m_pinger.ping(m_badHost));
-		Thread.sleep(1000);
-	}
+    public void testSinglePing() throws Exception {
+        assertTrue(m_pinger.ping(m_goodHost) > 0);
+        Thread.sleep(1000);
+    }
 
-	public void testParallelPing() throws Exception {
-		Map<String,Number> ret = m_pinger.parallelPing(m_goodHost, 10);
-		ArrayList<Number> items = new ArrayList<Number>();
-		for (String key : ret.keySet()) {
-			items.add(ret.get(key));
-			System.out.println(key + ": " + ret.get(key));
-		}
-		ret.remove("loss");
-		ret.remove("median");
-		ret.remove("response-time");
-		System.out.println("pings = " + items.size() + ", passed = " + CollectionMath.countNotNull(items) + " (" + CollectionMath.percentNotNull(items) + "%), failed = " + CollectionMath.countNull(items) + " (" + CollectionMath.percentNull(items) + "%), average = " + (CollectionMath.average(items).floatValue() / 1000F) + "ms");
-		Thread.sleep(1000);
-		assertTrue(CollectionMath.countNotNull(items) > 0);
-	}
-	
-	public void testParallelPingFailure() throws Exception {
-		Map<String,Number> ret = m_pinger.parallelPing(m_badHost, 10);
-		ret.remove("loss");
-		ret.remove("median");
-		ret.remove("response-time");
-		ArrayList<Number>items = new ArrayList<Number>();
-		System.out.println("pings = " + items.size() + ", passed = " + CollectionMath.countNotNull(items) + " (" + CollectionMath.percentNotNull(items) + "%), failed = " + CollectionMath.countNull(items) + " (" + CollectionMath.percentNull(items) + "%), average = " + (CollectionMath.average(items).floatValue() / 1000F) + "ms");
-		Thread.sleep(1000);
-		assertTrue(CollectionMath.countNotNull(items) == 0);
-	}
+    public void testSinglePingFailure() throws Exception {
+        assertNull(m_pinger.ping(m_badHost));
+        Thread.sleep(1000);
+    }
+
+    public void testParallelPing() throws Exception {
+        Map<String,Number> ret = m_pinger.parallelPing(m_goodHost, 10);
+        ArrayList<Number> items = new ArrayList<Number>();
+        for (String key : ret.keySet()) {
+            items.add(ret.get(key));
+            System.out.println(key + ": " + ret.get(key));
+        }
+        ret.remove("loss");
+        ret.remove("median");
+        ret.remove("response-time");
+        System.out.println("pings = " + items.size() + ", passed = " + CollectionMath.countNotNull(items) + " (" + CollectionMath.percentNotNull(items) + "%), failed = " + CollectionMath.countNull(items) + " (" + CollectionMath.percentNull(items) + "%), average = " + (CollectionMath.average(items).floatValue() / 1000F) + "ms");
+        Thread.sleep(1000);
+        assertTrue(CollectionMath.countNotNull(items) > 0);
+    }
+
+    public void testParallelPingFailure() throws Exception {
+        Map<String,Number> ret = m_pinger.parallelPing(m_badHost, 10);
+        ret.remove("loss");
+        ret.remove("median");
+        ret.remove("response-time");
+        ArrayList<Number>items = new ArrayList<Number>();
+        System.out.println("pings = " + items.size() + ", passed = " + CollectionMath.countNotNull(items) + " (" + CollectionMath.percentNotNull(items) + "%), failed = " + CollectionMath.countNull(items) + " (" + CollectionMath.percentNull(items) + "%), average = " + (CollectionMath.average(items).floatValue() / 1000F) + "ms");
+        Thread.sleep(1000);
+        assertTrue(CollectionMath.countNotNull(items) == 0);
+    }
 }
