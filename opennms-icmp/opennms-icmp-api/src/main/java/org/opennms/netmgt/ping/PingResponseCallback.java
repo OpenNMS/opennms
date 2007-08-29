4d4ce11fb99c1892e760622f61cc20a7ad76603f
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-08-29 20:28:26 +0000

    clean up the icmp api a bit

--- opennms-icmp/opennms-icmp-api/src/main/java/org/opennms/netmgt/ping/PingResponseCallback.java
+++ opennms-icmp/opennms-icmp-api/src/main/java/org/opennms/netmgt/ping/PingResponseCallback.java
@@ -1,11 +1,13 @@
 package org.opennms.netmgt.ping;
 
+import java.net.InetAddress;
+
 import org.opennms.protocols.icmp.ICMPEchoPacket;
 
 public interface PingResponseCallback {
 
-	public void handleResponse(ICMPEchoPacket packet);
-	public void handleTimeout(ICMPEchoPacket packet);
-    public void handleError(PingRequest request, Throwable t);
+	public void handleResponse(InetAddress address, ICMPEchoPacket packet);
+	public void handleTimeout(InetAddress address, ICMPEchoPacket packet);
+    public void handleError(InetAddress address, ICMPEchoPacket packet, Throwable t);
 
 }
