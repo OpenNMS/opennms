fb7f8736d69b70149acbb8ee9602ea2573be4b1a
Matt Brozowski <brozow@users.sourceforge.net>
2007-09-21 16:12:36 +0000

    fix handling of inline editing so as not to try to rename across filesystems

---
+++ opennms-config/subst.groovy
@@ -0,0 +1,34 @@
+import java.io.File;
+
+basedir = project.basedir;
+
+def subst(srcFile, pattern, substition) {
+    File workDir = new File("${basedir}/target/groovy_tmp");
+    workDir.mkdirs();
+    
+    File tmpFile = File.createTempFile("subst", "tmp", workDir);
+    if (!srcFile.renameTo(tmpFile)) {
+        fail("Unable to rename "+srcFile+" to "+tmpFile+" Aborting!");
+    }
+    
+    srcFile.withPrintWriter { out ->
+        def lineNo = 0;
+        tmpFile.eachLine { line ->
+            lineNo++;
+            def newline = line.replaceAll(pattern, substition);
+            if (newline != line) {
+                log.debug("${srcFile}");
+                log.info("${srcFile.name}: Replaced line ${lineNo} >${line}< with >${newline}<");
+            }
+            out.println(newline);
+        }
+    }
+    
+    
+}
+
+def argFile = new File("${basedir}/target/generated-sources/castor/org/opennms/netmgt/config/notificationCommands/Argument.java")
+
+subst(argFile, /@param\s+_switch\s*$/, "");
+subst(argFile, /@param\s+switch/, "@param _switch");
+
