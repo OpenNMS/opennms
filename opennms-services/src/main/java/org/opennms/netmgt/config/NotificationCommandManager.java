b86cd09133e0b5ccc6c7f64a3fa838fe24e97150
DJ Gregor <djgregor@users.sourceforge.net>
2007-09-23 04:14:38 +0000

    Merged revisions 6938 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/devjam-007
    ........
      r6938 | djgregor | 2007-07-31 15:58:14 -0400 (Tue, 31 Jul 2007) | 1 line
      
      Format code.
    ........

--- opennms-services/src/main/java/org/opennms/netmgt/config/NotificationCommandManager.java
+++ opennms-services/src/main/java/org/opennms/netmgt/config/NotificationCommandManager.java
@@ -1,36 +1,40 @@
-//
-// This file is part of the OpenNMS(R) Application.
-//
-// OpenNMS(R) is Copyright (C) 2005 The OpenNMS Group, Inc.  All rights reserved.
-// OpenNMS(R) is a derivative work, containing both original code, included code and modified
-// code that was published under the GNU General Public License. Copyrights for modified 
-// and included code are below.
-//
-// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
-//
-// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.                                                            
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-//    
-// For more information contact: 
-//   OpenNMS Licensing       <license@opennms.org>
-//   http://www.opennms.org/
-//   http://www.opennms.com/
-//
-// Tab Size = 8
-
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2005 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified 
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ *
+ * 2007 Jul 31: Format. - dj@opennms.org
+ * 2007 Jul 31: Use CastorUtils.unmarshal, Java 5 for loops, and don't set
+ *              m_commands until we've finished populating the hash. - dj@opennms.org
+ *
+ * Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact: 
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
 package org.opennms.netmgt.config;
 
 import java.io.InputStream;
@@ -66,14 +70,19 @@ public abstract class NotificationCommandManager {
      * @throws ValidationException
      */
     protected void parseXML(Reader reader) throws MarshalException, ValidationException {
-        Collection commands = ((NotificationCommands) Unmarshaller.unmarshal(NotificationCommands.class, reader)).getCommandCollection();
-        m_commands = new HashMap<String, Command>();
-    
-        Iterator i = commands.iterator();
-        while (i.hasNext()) {
-            Command curCommand = (Command) i.next();
-            m_commands.put(curCommand.getName(), curCommand);
+        NotificationCommands config = CastorUtils.unmarshal(NotificationCommands.class, reader);
+
+        Map<String, Command> commands = new HashMap<String, Command>();
+        for (Command curCommand : getCommandsFromConfig(config)) {
+            commands.put(curCommand.getName(), curCommand);
         }
+
+        m_commands = commands;
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<Command> getCommandsFromConfig(NotificationCommands config) {
+        return config.getCommandCollection();
     }
 
     /**
@@ -89,5 +98,4 @@ public abstract class NotificationCommandManager {
     public Map<String, Command> getCommands() {
         return m_commands;
     }
-
 }
