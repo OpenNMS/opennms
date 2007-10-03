336a151e0f94f653e5ef1846232c04e7789b09a4
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-10-03 20:05:02 +0000

    fix a test, also fix a potential issue with considering a timeout as passing

--- opennms-services/src/test/java/org/opennms/netmgt/protocols/SshTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/protocols/SshTest.java
@@ -1,26 +1,41 @@
 package org.opennms.netmgt.protocols;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import org.opennms.netmgt.protocols.ssh.Poll;
 
 import junit.framework.TestCase;
 
 public class SshTest extends TestCase {
-    private static final String GOOD_HOST = "localhost";
-    private static final String BAD_HOST = "asdfkjdflsjiaweofjiasodfjasdfoisadfoiasjdfoiasdjf.com";
+    private static final String GOOD_HOST = "127.0.0.1";
+    private static final String BAD_HOST = "127.1.1.1";
     private static final int PORT = 22;
     private static final int TIMEOUT = 30000;
+
+    Poll p;
+    InetAddress good, bad;
     
-    public void testSsh() throws Exception {
-        Poll p = new Poll();
+    public void setUp() throws Exception {
+        p = new Poll();
         p.setPort(PORT);
         p.setTimeout(TIMEOUT);
-        
-        p.setAddress(InetAddress.getByName(GOOD_HOST));
-        assertTrue(p.poll().isAvailable());
 
-        p.setAddress(InetAddress.getByName(BAD_HOST));
+        try {
+            good = InetAddress.getByName(GOOD_HOST);
+            bad  = InetAddress.getByName(BAD_HOST);
+        } catch (UnknownHostException e) {
+            throw e;
+        }
+    }
+    
+    public void testSshGoodHost() throws Exception {
+        p.setAddress(good);
+        assertTrue(p.poll().isAvailable());
+    }
+    
+    public void testSshBadHost() throws Exception {
+        p.setAddress(bad);
         assertFalse(p.poll().isAvailable());
     }
 }
