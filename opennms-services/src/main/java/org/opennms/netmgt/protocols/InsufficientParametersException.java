c6ad7ba15285347f7d8deb00881c067a58bb3d83
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-10-03 19:17:47 +0000

    Merged revisions 7609-7610 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/feature-ssh-rework
    ........
      r7610 | rangerrick | 2007-10-03 15:13:16 -0400 (Wed, 03 Oct 2007) | 1 line
      
      cleaner implementation of the ssh monitors
    ........

---
+++ opennms-services/src/main/java/org/opennms/netmgt/protocols/InsufficientParametersException.java
@@ -0,0 +1,9 @@
+package org.opennms.netmgt.protocols;
+
+public class InsufficientParametersException extends Exception {
+
+    public InsufficientParametersException(String message) {
+        super(message);
+    }
+
+}
