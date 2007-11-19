11f5ff8a34a2d1533d7675c8021516140b35ffb2
Matt Brozowski <brozow@users.sourceforge.net>
2007-11-19 20:38:18 +0000

    make sure database connection is safely closed

--- opennms-services/src/main/java/org/opennms/netmgt/charts/ChartUtils.java
+++ opennms-services/src/main/java/org/opennms/netmgt/charts/ChartUtils.java
@@ -260,19 +260,25 @@ public class ChartUtils {
          * single series data sets returned from sql query to a base data set
          * to be displayed in a the chart. 
          */
-        Connection conn = DataSourceFactory.getInstance().getConnection();
-        Iterator it = chartConfig.getSeriesDefCollection().iterator();
-        while (it.hasNext()) {
-            SeriesDef def = (SeriesDef) it.next();
-            JDBCCategoryDataset dataSet = new JDBCCategoryDataset(conn, def.getJdbcDataSet().getSql());
+        Connection conn = null;
+        try {
+            conn = DataSourceFactory.getInstance().getConnection();
+            Iterator it = chartConfig.getSeriesDefCollection().iterator();
+            while (it.hasNext()) {
+                SeriesDef def = (SeriesDef) it.next();
+                JDBCCategoryDataset dataSet = new JDBCCategoryDataset(conn, def.getJdbcDataSet().getSql());
             
-            for (int i = 0; i < dataSet.getRowCount(); i++) {
-                for (int j = 0; j < dataSet.getColumnCount(); j++) {
-                    baseDataSet.addValue(dataSet.getValue(i, j), def.getSeriesName(), dataSet.getColumnKey(j));
+                for (int i = 0; i < dataSet.getRowCount(); i++) {
+                    for (int j = 0; j < dataSet.getColumnCount(); j++) {
+                        baseDataSet.addValue(dataSet.getValue(i, j), def.getSeriesName(), dataSet.getColumnKey(j));
+                    }
                 }
             }
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
         }
-        conn.close();
         return baseDataSet;
     }
     
