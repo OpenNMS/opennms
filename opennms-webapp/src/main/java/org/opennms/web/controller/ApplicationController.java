02f9045eade714b564663bc903018803fc925fc8
Benjamin Reed <rangerrick@users.sourceforge.net>
2007-12-03 22:42:07 +0000

    fix for bug #2058 (bad bounds-checking on application add dialog)

--- opennms-webapp/src/main/java/org/opennms/web/controller/ApplicationController.java
+++ opennms-webapp/src/main/java/org/opennms/web/controller/ApplicationController.java
@@ -52,13 +52,23 @@ public class ApplicationController extends AbstractController {
 
     private AdminApplicationService m_adminApplicationService;
 
+    private String getNonEmptyParameter(HttpServletRequest request, String parameter) {
+    	if (request != null) {
+    		String p = request.getParameter(parameter);
+    		if (p != null && !p.equals("")) {
+    			return p;
+    		}
+    	}
+    	return null;
+    }
+    
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        String removeApplicationIdString = request.getParameter("removeApplicationId");
-        String newApplicationName = request.getParameter("newApplicationName");
-        String applicationIdString = request.getParameter("applicationid");
-        String editString = request.getParameter("edit");
-        String ifServiceIdString = request.getParameter("ifserviceid");
+        String removeApplicationIdString = getNonEmptyParameter(request, "removeApplicationId");
+        String newApplicationName = getNonEmptyParameter(request, "newApplicationName");
+        String applicationIdString = getNonEmptyParameter(request, "applicationid");
+        String editString = getNonEmptyParameter(request, "edit");
+        String ifServiceIdString = getNonEmptyParameter(request, "ifserviceid");
         
         if (removeApplicationIdString != null) {
             m_adminApplicationService.removeApplication(removeApplicationIdString);
@@ -81,7 +91,7 @@ public class ApplicationController extends AbstractController {
         }
         
         if (applicationIdString != null && editString != null) {
-            String editAction = request.getParameter("action");
+            String editAction = getNonEmptyParameter(request, "action");
             if (editAction != null) {
                 String[] toAdd = request.getParameterValues("toAdd");
                 String[] toDelete = request.getParameterValues("toDelete");
@@ -113,7 +123,7 @@ public class ApplicationController extends AbstractController {
         }
         
         if (ifServiceIdString != null && editString != null) {
-            String editAction = request.getParameter("action");
+            String editAction = getNonEmptyParameter(request, "action");
             if (editAction != null) {
                 String[] toAdd = request.getParameterValues("toAdd");
                 String[] toDelete = request.getParameterValues("toDelete");
