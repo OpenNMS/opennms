8d1ebd134808f4db88fd9835a8ddfd222d9a964f
DJ Gregor <djgregor@users.sourceforge.net>
2007-12-24 23:16:24 +0000

    Test that included event files exist and that there are no extra event files that exist but are not used.

--- opennms-services/src/test/java/org/opennms/netmgt/eventd/EventConfigurationManagerTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/eventd/EventConfigurationManagerTest.java
@@ -1,9 +1,18 @@
 package org.opennms.netmgt.eventd;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import junit.framework.TestCase;
 
 import org.exolab.castor.xml.ValidationException;
+import org.opennms.netmgt.dao.castor.CastorUtils;
+import org.opennms.netmgt.xml.eventconf.Events;
 import org.opennms.test.ConfigurationTestUtils;
+import org.springframework.util.StringUtils;
 
 public class EventConfigurationManagerTest extends TestCase {
     public void testLoadConfigurationSingleConfig() throws Exception {
@@ -47,6 +56,42 @@ public class EventConfigurationManagerTest extends TestCase {
         
         if (!caughtExceptionThatWeWanted) {
             fail("Did not get the exception that we wanted");
+        }
+    }
+    
+    /**
+     * This is disabled because the last check fails.
+     */
+    public void DISABLEDtestIncludedEventFilesExistAndNoExtras() throws Exception {
+        File eventConfFile = ConfigurationTestUtils.getFileForConfigFile("eventconf.xml");
+        File eventsDirFile = new File(eventConfFile.getParentFile(), "events");
+        assertTrue("events directory exists at " + eventsDirFile.getAbsolutePath(), eventsDirFile.exists());
+        assertTrue("events directory is a directory at " + eventsDirFile.getAbsolutePath(), eventsDirFile.isDirectory());
+        
+        File[] eventFilesOnDiskArray = eventsDirFile.listFiles(new FilenameFilter() {
+            public boolean accept(File file, String name) {
+                return name.endsWith(".xml");
+            } });
+        Set<File> eventFilesOnDisk = new HashSet<File>(Arrays.asList(eventFilesOnDiskArray));
+
+        Events events = CastorUtils.unmarshal(Events.class, ConfigurationTestUtils.getReaderForConfigFile("eventconf.xml"));
+        Set<File> eventFilesIncluded = new HashSet<File>(events.getEventFileCollection().size());
+        for (String eventFile : events.getEventFileCollection()) {
+            eventFilesIncluded.add(new File(eventConfFile.getParentFile(), eventFile.replaceAll("@install.etc.dir@/", "")));
+        }
+        
+        Set<File> includedNotOnDisk = new HashSet<File>(eventFilesIncluded);
+        includedNotOnDisk.removeAll(eventFilesOnDisk);
+        if (!includedNotOnDisk.isEmpty()) {
+            fail("Event configuration file " + eventConfFile.getAbsolutePath() + " references included files that could not be found:\n\t"
+                    + StringUtils.collectionToDelimitedString(includedNotOnDisk, "\n\t"));
+        }
+        
+        Set<File> onDiskNotIncluded = new HashSet<File>(eventFilesOnDisk);
+        onDiskNotIncluded.removeAll(eventFilesIncluded);
+        if (!onDiskNotIncluded.isEmpty()) {
+            fail("Events directory " + eventsDirFile.getAbsolutePath() + " contains event files that are not referenced in event configuration file " + eventConfFile.getAbsolutePath() + ":\n\t"
+                    + StringUtils.collectionToDelimitedString(onDiskNotIncluded, "\n\t"));
         }
     }
 }
