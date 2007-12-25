195da1ae04bee8da642fbc22b66077c37654343b
DJ Gregor <djgregor@users.sourceforge.net>
2007-12-25 04:11:40 +0000

    Allow relative paths in event-file entries within eventconf.xml.

--- opennms-services/src/test/java/org/opennms/netmgt/eventd/EventConfigurationManagerTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/eventd/EventConfigurationManagerTest.java
@@ -2,6 +2,9 @@ package org.opennms.netmgt.eventd;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -16,18 +19,18 @@ import org.springframework.util.StringUtils;
 
 public class EventConfigurationManagerTest extends TestCase {
     public void testLoadConfigurationSingleConfig() throws Exception {
-        EventConfigurationManager.loadConfiguration(ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/config/eventd/singleConfig/eventconf.xml"));
+        EventConfigurationManager.loadConfiguration(getFilteredReaderForConfig("singleConfig/eventconf.xml"));
     }
 
     public void testLoadConfigurationTwoDeepConfig() throws Exception {
-        EventConfigurationManager.loadConfiguration(ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/config/eventd/twoDeepConfig/eventconf.xml"));
+        EventConfigurationManager.loadConfiguration(getFilteredReaderForConfig("twoDeepConfig/eventconf.xml"));
     }
 
     public void testLoadConfigurationThreeDeepConfig() throws Exception {
         boolean caughtExceptionThatWeWanted = false;
         
         try {
-            EventConfigurationManager.loadConfiguration(ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/config/eventd/threeDeepConfig/eventconf.xml"));
+            EventConfigurationManager.loadConfiguration(getFilteredReaderForConfig("threeDeepConfig/eventconf.xml"));
         } catch (ValidationException e) {
             if (e.getMessage().contains("cannot include other configuration files")) {
                 caughtExceptionThatWeWanted = true;
@@ -45,7 +48,7 @@ public class EventConfigurationManagerTest extends TestCase {
         boolean caughtExceptionThatWeWanted = false;
         
         try {
-            EventConfigurationManager.loadConfiguration(ConfigurationTestUtils.getReaderForResource(this, "/org/opennms/netmgt/config/eventd/twoDeepConfigWithGlobal/eventconf.xml"));
+            EventConfigurationManager.loadConfiguration(getFilteredReaderForConfig("twoDeepConfigWithGlobal/eventconf.xml"));
         } catch (ValidationException e) {
             if (e.getMessage().contains("cannot have a 'global' element")) {
                 caughtExceptionThatWeWanted = true;
@@ -58,7 +61,13 @@ public class EventConfigurationManagerTest extends TestCase {
             fail("Did not get the exception that we wanted");
         }
     }
-    
+
+    public void testLoadConfigurationRelativeTwoDeepConfig() throws Exception {
+        String relativeResourcePath = "relativeTwoDeepConfig/eventconf.xml";
+        URL url = getUrlForRelativeResourcePath(relativeResourcePath);
+        EventConfigurationManager.loadConfiguration(getFilteredReaderForConfig(relativeResourcePath), new File(url.getFile()));
+    }
+
     /**
      * This is disabled because the last check fails.
      */
@@ -77,7 +86,7 @@ public class EventConfigurationManagerTest extends TestCase {
         Events events = CastorUtils.unmarshal(Events.class, ConfigurationTestUtils.getReaderForConfigFile("eventconf.xml"));
         Set<File> eventFilesIncluded = new HashSet<File>(events.getEventFileCollection().size());
         for (String eventFile : events.getEventFileCollection()) {
-            eventFilesIncluded.add(new File(eventConfFile.getParentFile(), eventFile.replaceAll("@install.etc.dir@/", "")));
+            eventFilesIncluded.add(new File(eventConfFile.getParentFile(), eventFile));
         }
         
         Set<File> includedNotOnDisk = new HashSet<File>(eventFilesIncluded);
@@ -93,5 +102,23 @@ public class EventConfigurationManagerTest extends TestCase {
             fail("Events directory " + eventsDirFile.getAbsolutePath() + " contains event files that are not referenced in event configuration file " + eventConfFile.getAbsolutePath() + ":\n\t"
                     + StringUtils.collectionToDelimitedString(onDiskNotIncluded, "\n\t"));
         }
+    }
+
+    private Reader getFilteredReaderForConfig(String resourceSuffix) throws IOException {
+        URL url = getUrlForRelativeResourcePath(resourceSuffix);
+        
+        Reader reader = ConfigurationTestUtils.getReaderForResourceWithReplacements(this, getResourceForRelativePath(resourceSuffix),
+                new String[] { "@install.etc.dir@", new File(url.getFile()).getParent() });
+        return reader;
+    }
+
+    private URL getUrlForRelativeResourcePath(String resourceSuffix) {
+        URL url = getClass().getResource(getResourceForRelativePath(resourceSuffix));
+        assertNotNull("URL for resource " + getResourceForRelativePath(resourceSuffix) + " must not be null", url);
+        return url;
+    }
+
+    private String getResourceForRelativePath(String resourceSuffix) {
+        return "/org/opennms/netmgt/config/eventd/" + resourceSuffix;
     }
 }
