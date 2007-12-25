dce6364eac3ce5d52ca950887e0fc55079917f29
DJ Gregor <djgregor@users.sourceforge.net>
2007-12-25 01:56:32 +0000

    - Move config file to external file and reuse existing opennms-services/src/test/resources/org/opennms/netmgt/eventd/datablock/eventconf.xml. - Organize imports. - Add missed modification note for last commit.

--- opennms-services/src/test/java/org/opennms/netmgt/eventd/datablock/EventConfDaoTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/eventd/datablock/EventConfDaoTest.java
@@ -8,6 +8,11 @@
 //
 // OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
 //
+// Modifications:
+//
+// 2007 Dec 24: Indent, move config file to external file, organize
+//              imports. - dj@opennms.org
+//
 // Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
 //
 // This program is free software; you can redistribute it and/or modify
@@ -36,6 +41,7 @@
 package org.opennms.netmgt.eventd.datablock;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -45,227 +51,15 @@ import junit.framework.TestCase;
 
 import org.apache.commons.io.FileUtils;
 import org.opennms.core.utils.Base64;
-import org.opennms.netmgt.eventd.EventConfigurationManager;
 import org.opennms.netmgt.mock.EventConfWrapper;
-import org.opennms.netmgt.mock.EventWrapper;
 import org.opennms.netmgt.utils.EventBuilder;
 import org.opennms.netmgt.xml.event.Event;
-import org.opennms.netmgt.xml.event.Snmp;
 import org.opennms.netmgt.xml.eventconf.Logmsg;
 import org.opennms.test.mock.MockLogAppender;
 
 public class EventConfDaoTest extends TestCase {
 
     private EventConfDao m_eventConfDao;
-    private static final String EVENT_CONF = "<events xmlns=\"http://xmlns.opennms.org/xsd/eventconf\">\n" +
-    " <global>\n" +
-    "  <security>\n" +
-    "   <doNotOverride>logmsg</doNotOverride>\n" +
-    "   <doNotOverride>operaction</doNotOverride>\n" +
-    "   <doNotOverride>autoaction</doNotOverride>\n" +
-    "   <doNotOverride>tticket</doNotOverride>\n" +
-    "   <doNotOverride>script</doNotOverride>\n" +
-    "  </security>\n" +
-    " </global>\n" +
-    "\n" +
-    " <event>\n" +
-    "  <mask>\n" +
-    "   <maskelement>\n" +
-    "    <mename>id</mename>\n" +
-    "    <mevalue>.1.3.6.1.2.1.15.7</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>generic</mename>\n" +
-    "    <mevalue>6</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>specific</mename>\n" +
-    "    <mevalue>1</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>snmphost</mename>\n" +
-    "    <mevalue>192.168.1.%</mevalue>\n" +
-    "   </maskelement>\n" +
-    "  </mask>\n" +
-    "  <uei>uei.opennms.org/IETF/BGP/traps/bgpEstablished</uei>\n" +
-    "  <event-label>BGP4-MIB defined trap event: bgpEstablished</event-label>\n" +
-    "  <descr>&lt;p&gt;The BGP Established event is generated when\n" +
-    "   the BGP FSM enters the ESTABLISHED state.&lt;/p&gt;&lt;table&gt;\n" +
-    "   &lt;tr&gt;&lt;td&gt;&lt;b&gt;\n" +
-    "   bgpPeerLastError&lt;/b&gt;&lt;/td&gt;&lt;td&gt;%parm[#1]%\n" +
-    "   &lt;/td&gt;&lt;td&gt;&lt;p;&gt;&lt;/p&gt;&lt;/td;&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;\n" +
-    "   bgpPeerState&lt;/b&gt;&lt;/td&gt;&lt;td&gt;%parm[#2]%\n" +
-    "   &lt;/td&gt;&lt;td&gt;&lt;p;&gt;\n" +
-    "   idle(1) connect(2) active(3) opensent(4) openconfirm(5) established(6)&lt;/p&gt;\n" +
-    "   &lt;/td;&gt;&lt;/tr&gt;&lt;/table&gt;\n" +
-    "  </descr>\n" +
-    "  <logmsg dest='discardtraps'>&lt;p&gt;BGP Event: FSM entered connected state.&lt;/p&gt;</logmsg>\n" +
-    "  <severity>Normal</severity>\n" +
-    " </event>\n" +
-    "\n" +
-    " <event>\n" +
-    "  <mask>\n" +
-    "   <maskelement>\n" +
-    "    <mename>id</mename>\n" +
-    "    <mevalue>.1.3.6.1.2.1.15.7</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>generic</mename>\n" +
-    "    <mevalue>6</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>specific</mename>\n" +
-    "    <mevalue>1</mevalue>\n" +
-    "   </maskelement>\n" +
-    "  </mask>\n" +
-    "  <uei>uei.opennms.org/IETF/BGP/traps/bgpEstablished</uei>\n" +
-    "  <event-label>BGP4-MIB defined trap event: bgpEstablished</event-label>\n" +
-    "  <descr>&lt;p&gt;The BGP Established event is generated when\n" +
-    "   the BGP FSM enters the ESTABLISHED state.&lt;/p&gt;&lt;table&gt;\n" +
-    "   &lt;tr&gt;&lt;td&gt;&lt;b&gt;\n" +
-    "   bgpPeerLastError&lt;/b&gt;&lt;/td&gt;&lt;td&gt;%parm[#1]%\n" +
-    "   &lt;/td&gt;&lt;td&gt;&lt;p;&gt;&lt;/p&gt;&lt;/td;&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;\n" +
-    "   bgpPeerState&lt;/b&gt;&lt;/td&gt;&lt;td&gt;%parm[#2]%\n" +
-    "   &lt;/td&gt;&lt;td&gt;&lt;p;&gt;\n" +
-    "   idle(1) connect(2) active(3) opensent(4) openconfirm(5) established(6)&lt;/p&gt;\n" +
-    "   &lt;/td;&gt;&lt;/tr&gt;&lt;/table&gt;\n" +
-    "  </descr>\n" +
-    "  <logmsg dest='logndisplay'>&lt;p&gt;BGP Event: FSM entered connected state.&lt;/p&gt;</logmsg>\n" +
-    "  <severity>Normal</severity>\n" +
-    " </event>\n" +
-    "\n" +
-    " <event>\n" +
-    "  <mask>\n" +
-    "   <maskelement>\n" +
-    "    <mename>id</mename>\n" +
-    "    <mevalue>.1.3.6.1.2.1.15.7</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>generic</mename>\n" +
-    "    <mevalue>6</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>specific</mename>\n" +
-    "    <mevalue>2</mevalue>\n" +
-    "   </maskelement>\n" +
-    "  </mask>\n" +
-    "  <uei>uei.opennms.org/IETF/BGP/traps/bgpBackwardTransition</uei>\n" +
-    "  <event-label>BGP4-MIB defined trap event: bgpBackwardTransition</event-label>\n" +
-    "  <descr>&lt;p&gt;The BGPBackwardTransition Event is generated\n" +
-    "   when the BGP FSM moves from a higher numbered\n" +
-    "   state to a lower numbered state.&lt;/p&gt;&lt;table&gt;\n" +
-    "   &lt;tr&gt;&lt;td&gt;&lt;b&gt;\n" +
-    "   bgpPeerLastError&lt;/b&gt;&lt;/td&gt;&lt;td&gt;%parm[#1]%\n" + 
-    "   &lt;/td&gt;&lt;td&gt;&lt;p;&gt;&lt;/p&gt;&lt;/td;&gt;&lt;/tr&gt;&lt;tr&gt;&lt;td&gt;&lt;b&gt;\n" +
-    "   bgpPeerState&lt;/b&gt;&lt;/td&gt;&lt;td&gt;%parm[#2]%\n" + 
-    "   &lt;/td&gt;&lt;td&gt;&lt;p;&gt;\n" + 
-    "   idle(1) connect(2) active(3) opensent(4) openconfirm(5) established(6)&lt;/p&gt;\n" +
-    "   &lt;/td;&gt;&lt;/tr&gt;&lt;/table&gt;\n" +
-    "  </descr>\n" + 
-    "  <logmsg dest='discardtraps'>&lt;p&gt;BGP Event: FSM Backward Transistion.&lt;/p&gt;</logmsg>\n" + 
-    "  <severity>Warning</severity>\n" +
-    " </event>\n" +
-    "\n" +
-    " <event>\n" +
-    "  <mask>\n" +
-    "   <maskelement>\n" +
-    "    <mename>generic</mename>\n" +
-    "    <mevalue>0</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>snmphost</mename>\n" +
-    "    <mevalue>192.168.1.1</mevalue>\n" +
-    "   </maskelement>\n" +
-    "  </mask>\n" +
-    "  <uei>uei.opennms.org/generic/traps/SNMP_Cold_Start</uei>\n" +
-    "  <event-label>OpenNMS-defined trap event: SNMP_Cold_Start</event-label>\n" +
-    "  <descr>\n" +
-    "	&lt;p&gt;A coldStart trap signifies that the sending\n" +
-    "	protocol entity is reinitializing itself such that the\n" +
-    "	agent's configuration or the protocol entity implementation\n" +
-    "	may be altered.&lt;/p&gt;\n" +
-    "  </descr>\n" +
-    "  <logmsg dest='discardtraps'>\n" +
-    "	Agent Up with Possible Changes (coldStart Trap)\n" +
-    "	enterprise:%id% (%id%) args(%parm[##]%):%parm[all]%\n" +
-    "  </logmsg>\n" +
-    "  <severity>Normal</severity>\n" +
-    " </event>\n" +
-    "\n" +
-    " <event>\n" +
-    "  <mask>\n" +
-    "   <maskelement>\n" +
-    "    <mename>generic</mename>\n" +
-    "    <mevalue>0</mevalue>\n" +
-    "   </maskelement>\n" +
-    "  </mask>\n" +
-    "  <uei>uei.opennms.org/generic/traps/SNMP_Cold_Start</uei>\n" +
-    "  <event-label>OpenNMS-defined trap event: SNMP_Cold_Start</event-label>\n" +
-    "  <descr>\n" +
-    "	&lt;p&gt;A coldStart trap signifies that the sending\n" +
-    "	protocol entity is reinitializing itself such that the\n" +
-    "	agent's configuration or the protocol entity implementation\n" +
-    "	may be altered.&lt;/p&gt;\n" +
-    "  </descr>\n" +
-    "  <logmsg dest='logndisplay'>\n" +
-    "	Agent Up with Possible Changes (coldStart Trap)\n" +
-    "	enterprise:%id% (%id%) args(%parm[##]%):%parm[all]%\n" +
-    "  </logmsg>\n" +
-    "  <severity>Normal</severity>\n" +
-    " </event>\n" +
-    " <event>\n" +
-    "  <mask>\n" +
-    "   <maskelement>\n" +
-    "    <mename>generic</mename>\n" +
-    "    <mevalue>1</mevalue>\n" +
-    "   </maskelement>\n" +
-    "  </mask>\n" +
-    "  <uei>uei.opennms.org/generic/traps/SNMP_Warm_Start</uei>\n" +
-    "  <event-label>OpenNMS-defined trap event: SNMP_Warm_Start</event-label>\n" +
-    "  <descr>\n" +
-    "	&lt;p&gt;A warmStart trap signifies that the sending\n" +
-    "	protocol entity is reinitializing itself such that the\n" +
-    "	agent's configuration or the protocol entity implementation\n" +
-    "	may be altered.&lt;/p&gt;\n" +
-    "  </descr>\n" +
-    "  <logmsg dest='logndisplay'>\n" +
-    "	Agent Up with Possible Changes (warmStart Trap)\n" +
-    "	enterprise:%id% (%id%) args(%parm[##]%):%parm[all]%\n" +
-    "  </logmsg>\n" +
-    "  <severity>Normal</severity>\n" +
-    " </event>\n" +
-    " <event>\n" +
-    "  <mask>\n" +
-    "   <maskelement>\n" +
-    "    <mename>id</mename>\n" +
-    "    <mevalue>.1.3.6.1.4.1.14179.2.6.3</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>generic</mename>\n" +
-    "    <mevalue>6</mevalue>\n" +
-    "   </maskelement>\n" +
-    "   <maskelement>\n" +
-    "    <mename>specific</mename>\n" +
-    "    <mevalue>8</mevalue>\n" +
-    "   </maskelement>\n" +
-    "  </mask>\n" +
-    "  <uei>uei.opennms.org/vendor/cisco/bsnAPDisassociated</uei>\n" +
-    "  <event-label>AIRESPACE-WIRELESS-MIB defined trap event: bsnAPDisassociated</event-label>\n" +
-    "  <descr>\n" +
-    "   &lt;p&gt;When Airespace AP disassociates from Airespace Switch, AP disassociated notification\n" +
-    "   will be sent with dot3 MAC address of Airespace AP\n" +
-    "   management system to remove Airespace AP from this Airespace Switch&lt;/p&gt;&lt;table&gt;\n" +
-    "   &lt;tr&gt;&lt;td&gt;&lt;b&gt;\n\n" +
-    "   bsnAPMacAddrTrapVariable&lt;/b&gt;&lt;/td&gt;&lt;td&gt;\n" +
-    "   %parm[#1]%;&lt;/td&gt;&lt;td&gt;&lt;p;&gt;&lt;/p&gt;&lt;/td;&gt;&lt;/tr&gt;&lt;/table&gt;\n" +
-    "  </descr>\n" +
-    "  <logmsg dest='logndisplay'>&lt;p&gt;\n" +
-    "    bsnAPDisassociated trap received\n" +
-    "    bsnAPMacAddrTrapVariable=%parm[#1]%&lt;/p&gt;\n" +
-    "  </logmsg>\n" +
-    "  <severity>Warning</severity>\n" +
-    " </event>\n" +
-    "</events>";
 
     protected void setUp() throws Exception {
         MockLogAppender.setupLogging(false);
@@ -274,8 +68,12 @@ public class EventConfDaoTest extends TestCase {
         File configFile = File.createTempFile("eventConf", "xml", targetDir);
         configFile.deleteOnExit();
 
-        FileUtils.writeStringToFile(configFile, EVENT_CONF, "ISO_8859-1");
-
+        String resource = "eventconf.xml";
+        URL url = getClass().getResource(resource);
+        assertNotNull("Could not get resource for " + resource, url);
+        
+        FileUtils.copyURLToFile(url, configFile);
+        
         DefaultEventConfDao eventConfDao = new DefaultEventConfDao();
         eventConfDao.setConfigFile(configFile);
         eventConfDao.afterPropertiesSet();
