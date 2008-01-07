350d53574aa669143f22aa9cdcd89f2b95c7d69c
DJ Gregor <djgregor@users.sourceforge.net>
2008-01-07 04:13:21 +0000

    Add marshalWithTranslatedExceptions.

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/castor/CastorUtils.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/castor/CastorUtils.java
@@ -1,8 +1,10 @@
 package org.opennms.netmgt.dao.castor;
 
 import java.io.Reader;
+import java.io.Writer;
 
 import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.Marshaller;
 import org.exolab.castor.xml.Unmarshaller;
 import org.exolab.castor.xml.ValidationException;
 import org.springframework.dao.DataAccessException;
@@ -14,12 +16,31 @@ import org.springframework.dao.DataAccessException;
  */
 public class CastorUtils {
     private static final CastorExceptionTranslator CASTOR_EXCEPTION_TRANSLATOR = new CastorExceptionTranslator();
-
     
     /** Private constructor since this class only has static methods (so far). */
     private CastorUtils() {
     }
-    
+
+    /**
+     * Marshal a Castor XML configuration file.
+     *
+     * @param obj the object representing the objected to be marshalled to XML
+     * @param writer where the marshalled XML will be written
+     * @throws DataAccessException if the underlying Castor
+     *      Marshaller.marshal() call throws a MarshalException or
+     *      ValidationException.  The underlying exception will be translated
+     *      using CastorExceptionTranslator.
+     */
+    public static void marshalWithTranslatedExceptions(Object obj, Writer writer) throws DataAccessException {
+        try {
+            Marshaller.marshal(obj, writer);
+        } catch (MarshalException e) {
+            throw CASTOR_EXCEPTION_TRANSLATOR.translate("Marshalling XML file", e);
+        } catch (ValidationException e) {
+            throw CASTOR_EXCEPTION_TRANSLATOR.translate("Marshalling XML file", e);
+        }
+    }
+
     /**
      * Unmarshal a Castor XML configuration file.  Uses Java 5 generics for
      * return type. 
@@ -39,7 +60,6 @@ public class CastorUtils {
     public static <T> T unmarshal(Class<T> clazz, Reader reader) throws MarshalException, ValidationException {
         return (T) Unmarshaller.unmarshal(clazz, reader);
     }
-    
 
     /**
      * Unmarshal a Castor XML configuration file.  Uses Java 5 generics for
