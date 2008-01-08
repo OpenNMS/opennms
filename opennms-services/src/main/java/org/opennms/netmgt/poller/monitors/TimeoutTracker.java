3f397fd9fbc392870bbb2c0b93c7433d70b32301
David Hustace <dhustace@users.sourceforge.net>
2008-01-08 01:48:43 +0000

    Add license and copyright notice

--- opennms-services/src/main/java/org/opennms/netmgt/poller/monitors/TimeoutTracker.java
+++ opennms-services/src/main/java/org/opennms/netmgt/poller/monitors/TimeoutTracker.java
@@ -1,3 +1,36 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2008 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ *
+ * Copyright (C) 2008 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
 package org.opennms.netmgt.poller.monitors;
 
 import java.util.Map;
@@ -5,6 +38,13 @@ import java.util.concurrent.TimeUnit;
 
 import org.opennms.netmgt.utils.ParameterMap;
 
+/**
+ * Helper class used for tracking retires and timeouts for ServiceMonitors.
+ * 
+ * @author <a href="mailto:ranger@opennms.org">Ben Reed</a>
+ * @author <a href="mailto:brozow@opennms.org">Mathew Brozowski</a>
+ *
+ */
 public class TimeoutTracker {
 
     private int m_retry;
