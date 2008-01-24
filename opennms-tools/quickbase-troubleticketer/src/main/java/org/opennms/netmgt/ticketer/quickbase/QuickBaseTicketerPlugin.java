3d8c6ef3b61250788dc83a75fb36db568114c72b
Matt Brozowski <brozow@users.sourceforge.net>
2008-01-24 22:56:16 +0000

    begin quickbase integration work

--- opennms-tools/quickbase-troubleticketer/src/main/java/org/opennms/netmgt/ticketer/quickbase/QuickBaseTicketerPlugin.java
+++ opennms-tools/quickbase-troubleticketer/src/main/java/org/opennms/netmgt/ticketer/quickbase/QuickBaseTicketerPlugin.java
@@ -36,6 +36,8 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -50,6 +52,7 @@ import org.springframework.util.Assert;
 import org.w3c.dom.Element;
 
 import com.intuit.quickbase.util.QuickBaseClient;
+import com.intuit.quickbase.util.QuickBaseException;
 
 /**
  * OpenNMS Trouble Ticket Plugin API implementation for Intuit's QuickBase Trouble Ticketing.
@@ -59,15 +62,105 @@ import com.intuit.quickbase.util.QuickBaseClient;
  *
  */
 public class QuickBaseTicketerPlugin implements TicketerPlugin {
-
+    
 	public Ticket get(String ticketId) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public void saveOrUpdate(Ticket ticket) {
-		// TODO Auto-generated method stub
-		
+	private QuickBaseClient createClient() {
+	    return new QuickBaseClient("brozow@opennms.org", "password");
 	}
+	
+	private Properties getProperties() {
+	    File home = new File(System.getProperty("opennms.home"));
+	    File etc = new File(home, "etc");
+	    File config = new File(etc, "quickbase.properties");
+	    
+	    
+	    Properties props = new Properties();
+	    
+	    InputStream in = null;
+	    try {
+	        in = new FileInputStream(config);
+	        props.load(in);
+	    } catch (IOException e) {
+	        log().error("Unable to load "+config+" ignoring.", e);
+	    } finally {
+	        IOUtils.closeQuietly(in);
+	    }  
+	        
+	    return props; 
+
+	}
+
+	/**
+     * Convenience logging.
+     * @return a log4j Category for this class
+     */
+    private Category log() {
+        return ThreadCategory.getInstance(getClass());
+    }
+
+    public void saveOrUpdate(Ticket ticket) {
+
+	    try {
+	        
+	       Properties props = getProperties();
+	       
+	       QuickBaseClient qdb = createClient();
+	       
+	       String appName = "TPMG Support";
+	       String dbId = qdb.findDbByName(appName);
+	       
+           HashMap<String, String> record = new HashMap<String, String>();
+           
+           record.put(getSummaryField(props), ticket.getSummary());
+           record.put(getDetailsField(props), ticket.getDetails());
+           record.put(getStateField(props), getQuickBaseStateValue(ticket.getState(), props));
+           
+	       if (ticket.getId() == null) {
+	           addAdditionCreationFields(record, props);
+	           String recordId = qdb.addRecord(dbId, record);
+	           ticket.setId(recordId);
+	       } else {
+	           
+	           
+	           
+	           qdb.editRecord(dbId, record, ticket.getId());
+	       }
+	       
+	    } catch (Exception e) {
+            throw new DataRetrievalFailureException("Failed to commit QuickBase transaction: "+e.getMessage(), e);
+	    }
+	        
+	}
+
+    private String getQuickBaseStateValue(State state, Properties props) {
+        return props.getProperty("statemap.ticket."+state.name());
+    }
+
+    private String getStateField(Properties props) {
+        return props.getProperty("ticket.state");
+    }
+
+    private String getDetailsField(Properties props) {
+        return props.getProperty("ticket.details");
+    }
+
+    private String getSummaryField(Properties props) {
+        return props.getProperty("ticket.summary");
+    }
+
+    private void addAdditionCreationFields(HashMap<String, String> record, Properties props) {
+        final String prefix = "quickbase.create.";
+        Enumeration keys = props.propertyNames();
+        while(keys.hasMoreElements()) {
+            String key = (String)keys.nextElement();
+            if (key.startsWith(prefix)) {
+                String field = key.substring(prefix.length());
+                record.put(field, props.getProperty(key));
+            }
+        }
+    }
     
  }
