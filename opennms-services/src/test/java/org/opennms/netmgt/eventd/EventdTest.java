b788d5a30725a5626808e429cc8508b4167f10a0
DJ Gregor <djgregor@users.sourceforge.net>
2008-01-24 02:53:21 +0000

    Indent

--- opennms-services/src/test/java/org/opennms/netmgt/eventd/EventdTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/eventd/EventdTest.java
@@ -57,13 +57,13 @@ import org.opennms.test.mock.MockUtil;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class EventdTest extends OpenNMSTestCase {
-    
+
     public EventdTest() {
         DaoTestConfigBean daoTestConfig = new DaoTestConfigBean();
         daoTestConfig.setRelativeHomeDirectory("src/test/resources");
         daoTestConfig.afterPropertiesSet();
     }
-    
+
 
     public static void main(String[] args) {
         junit.textui.TestRunner.run(EventdTest.class);
@@ -78,18 +78,18 @@ public class EventdTest extends OpenNMSTestCase {
         m_eventd.stop();
         MockLogAppender.assertNoWarningsOrGreater();
     }
-        
+
     public void testPersistEvent() throws Exception {
-        
+
         assertEquals(0, m_db.countRows("select * from events"));
-        
+
         MockNode node = m_network.getNode(1);
         sendNodeDownEvent(null, node);
         sleep(1000);
         assertEquals(1, m_db.countRows("select * from events"));
-        
+
     }
-    
+
     /**
      * Test that eventd's service ID lookup works properly.
      */
@@ -106,16 +106,16 @@ public class EventdTest extends OpenNMSTestCase {
         assertEquals("event count", 1, m_db.countRows("select * from events"));
         assertNotSame("service ID for event", 0, new JdbcTemplate(m_db.getDataSource()).queryForInt("select serviceID from events"));
     }
-    
+
     public void testPersistAlarm() throws Exception {
-        
+
         //there should be no alarms in the alarms table
         assertEquals(0, m_db.countRows("select * from alarms"));
-        
+
         MockNode node = m_network.getNode(1);
         sendNodeDownEvent(null, node);
         sleep(1000);
-        
+
         //this should be the first occurrence of this alarm
         //there should be 1 alarm now
         assertEquals(1, m_db.countRows("select * from alarms"));
@@ -125,13 +125,13 @@ public class EventdTest extends OpenNMSTestCase {
         sendNodeDownEvent(null, node);
         sleep(1000);
         assertEquals(1, m_db.countRows("select * from alarms"));
-        
+
         //this should be a new alarm because of the new key
         //there should now be 2 alarms
         sendNodeDownEvent("DontReduceThis", node);
         sleep(1000);
         assertEquals(2, m_db.countRows("select * from alarms"));
-        
+
         MockUtil.println("Going for the print of the counter column");
 
         //TODO: Change this to use Querier class
@@ -141,59 +141,59 @@ public class EventdTest extends OpenNMSTestCase {
             conn = m_db.getConnection();
             stmt = conn.prepareStatement("select reductionKey, sum(counter) from alarms group by reductionKey");
             ResultSet rs = stmt.executeQuery();
-            
+
             //should be 2 rows
             while( rs.next()) {
                 MockUtil.println("count for reductionKey: "+rs.getString(1)+" is: "+rs.getObject(2));
             }
-            
+
         } finally {
             stmt.close();
             conn.close();
         }
     }
-    
+
     public void testPersistManyAlarmsAtOnce() throws InterruptedException {
-        
+
         int numberOfAlarmsToReduce = 10;
         //there should be no alarms in the alarms table
         assertEquals(0, m_db.countRows("select * from alarms"));
- 
+
         final String reductionKey = "countThese";
         final MockNode node = m_network.getNode(1);
-        
+
         final long millis = System.currentTimeMillis()+2500;
-        
+
         final BarrierSignaler signal = new BarrierSignaler(numberOfAlarmsToReduce);
-        
+
         for (int i=1; i<= numberOfAlarmsToReduce; i++) {
             MockUtil.println("Creating Runnable: "+i+" of "+numberOfAlarmsToReduce+" events to reduce.");
 
             class EventRunner implements Runnable {
                 public void run() {
-                	try {
-                		while (System.currentTimeMillis() < millis) {
-                			try {
-                				Thread.sleep(10);
-                			} catch (InterruptedException e) {
-                				MockUtil.println(e.getMessage());
-                			}
-                		}
-                		sendNodeDownEvent(reductionKey, node);
-                	} finally {
-                		signal.signal();
-                	}
+                    try {
+                        while (System.currentTimeMillis() < millis) {
+                            try {
+                                Thread.sleep(10);
+                            } catch (InterruptedException e) {
+                                MockUtil.println(e.getMessage());
+                            }
+                        }
+                        sendNodeDownEvent(reductionKey, node);
+                    } finally {
+                        signal.signal();
+                    }
                 }
             }
-            
+
             Runnable r = new EventRunner();
             Thread p = new Thread(r);
             p.start();
         }
-        
+
         signal.waitFor();
         sleep(20000);
-        
+
         //this should be the first occurrence of this alarm
         //there should be 1 alarm now
         int rowCount = m_db.countRows("select * from alarms");
@@ -202,12 +202,12 @@ public class EventdTest extends OpenNMSTestCase {
         MockUtil.println("counterColumn is: "+counterColumn+", expected "+numberOfAlarmsToReduce);
         assertEquals(1, rowCount);
         assertEquals(numberOfAlarmsToReduce, counterColumn.intValue());
-        
+
         Integer alarmId = m_db.getAlarmId(reductionKey);
         rowCount = m_db.countRows("select * from events where alarmid = "+alarmId);
         MockUtil.println(String.valueOf(rowCount) + " of events with alarmid: "+alarmId);
-//        assertEquals(numberOfAlarmsToReduce, rowCount);
-        
+//      assertEquals(numberOfAlarmsToReduce, rowCount);
+
         rowCount = m_db.countRows("select * from events where alarmid is null");
         MockUtil.println(String.valueOf(rowCount) + " of events with null alarmid");
         assertEquals(0, rowCount);
@@ -220,7 +220,7 @@ public class EventdTest extends OpenNMSTestCase {
      */
     private void sendNodeDownEvent(String reductionKey, MockNode node) {
         Event e = MockEventUtil.createNodeDownEvent("Test", node);
-        
+
         if (reductionKey != null) {
             AlarmData data = new AlarmData();
             data.setAlarmType(1);
@@ -229,20 +229,20 @@ public class EventdTest extends OpenNMSTestCase {
         } else {
             e.setAlarmData(null);
         }
-        
+
         Logmsg logmsg = new Logmsg();
         logmsg.setDest("logndisplay");
         logmsg.setContent("testing");
         e.setLogmsg(logmsg);
         m_eventd.processEvent(e);
     }
-    
+
     /**
      * @param reductionKey
      */
     private void sendServiceDownEvent(String reductionKey, MockService svc) {
         Event e = MockEventUtil.createServiceUnresponsiveEvent("Test", svc, "Not responding");
-        
+
         if (reductionKey != null) {
             AlarmData data = new AlarmData();
             data.setAlarmType(1);
@@ -251,13 +251,13 @@ public class EventdTest extends OpenNMSTestCase {
         } else {
             e.setAlarmData(null);
         }
-        
+
         Logmsg logmsg = new Logmsg();
         logmsg.setDest("logndisplay");
         logmsg.setContent("testing");
         e.setLogmsg(logmsg);
         m_eventd.processEvent(e);
     }
-    
+
 
 }
