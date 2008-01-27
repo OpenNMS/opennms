db9f4c62dc39b3208ed5aa0a34d6fe2af85a69b7
DJ Gregor <djgregor@users.sourceforge.net>
2008-01-27 07:49:03 +0000

    Push methods in Persist that are only used by a single subclass into that subclass.

--- opennms-services/src/main/java/org/opennms/netmgt/eventd/AlarmWriter.java
+++ opennms-services/src/main/java/org/opennms/netmgt/eventd/AlarmWriter.java
@@ -10,6 +10,8 @@
 //
 // Modifications:
 //
+// 2008 Jan 27: Push methods in Persist that are only used by a single
+//              subclass into that subclass.
 // 2008 Jan 26: Dependency injection using setter injection instead of
 //              constructor injection and implement InitializingBean.
 //              Move some common setters and initializion into Persist.
@@ -36,13 +38,15 @@
 //      http://www.opennms.org/
 //      http://www.opennms.com/
 //
-// Tab Size = 8
-//
-
 package org.opennms.netmgt.eventd;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
 
+import org.opennms.netmgt.eventd.db.Constants;
 import org.opennms.netmgt.xml.event.Event;
 import org.opennms.netmgt.xml.event.Header;
 import org.springframework.util.Assert;
@@ -167,11 +171,240 @@ final class AlarmWriter extends Persist {
         }
     }
 
+    public void insertOrUpdateAlarm(Header eventHeader, Event event) throws SQLException {
+        int alarmId = isReductionNeeded(eventHeader, event);
+        if (alarmId != -1) {
+            if (log().isDebugEnabled()) {
+                log().debug("Reducing event for: " + event.getDbid() + ": " + event.getUei());
+            }
+            updateAlarm(eventHeader, event, alarmId);
+            
+            // This removes all previous events that have been reduced.
+            if (log().isDebugEnabled()) {
+                log().debug("insertOrUpdate: auto-clean is: " + event.getAlarmData().getAutoClean());
+            }
+            if (event.getAlarmData().getAutoClean() == true) {
+                log().debug("insertOrUpdate: deleting previous events");
+                cleanPreviousEvents(alarmId, event.getDbid());
+            }
+            
+        } else {
+            if (log().isDebugEnabled()) {
+                log().debug("Not reducing event for: " + event.getDbid() + ": " + event.getUei());
+            }
+            insertAlarm(eventHeader, event);
+        }
+    }
+
+    /**
+     * Insert values into the ALARMS table
+     * 
+     * @exception java.sql.SQLException
+     *                Thrown if there is an error adding the event to the
+     *                database.
+     * @exception java.lang.NullPointerException
+     *                Thrown if a required resource cannot be found in the
+     *                properties file.
+     */
+    protected void insertAlarm(Header eventHeader, Event event) throws SQLException {
+        int alarmID = -1;
+        alarmID = getNextId();
+        if (log().isDebugEnabled()) {
+            log().debug("AlarmWriter: DBID: " + alarmID);
+        }
+    
+        //Column 1, alarmId
+        m_insStmt.setInt(1, alarmID);
+        
+        //Column 2, eventUie
+        m_insStmt.setString(2, Constants.format(event.getUei(), EVENT_UEI_FIELD_SIZE));
+        
+        //Column 3, dpName
+        m_insStmt.setString(3, (eventHeader != null) ? Constants.format(eventHeader.getDpName(), EVENT_DPNAME_FIELD_SIZE) : "undefined");
+        
+        // Column 4, nodeID
+        int nodeid = (int) event.getNodeid();
+        m_insStmt.setObject(4, event.hasNodeid() ? new Integer(nodeid) : null);
+        
+        // Column 5, ipaddr
+        m_insStmt.setString(5, event.getInterface());
+        
+        //Column 6, serviceId
+        //
+        // convert the service name to a service id
+        //
+        int svcId = -1;
+        if (event.getService() != null) {
+            try {
+                svcId = getServiceID(event.getService());
+            } catch (SQLException e) {
+                log().warn("insertAlarm: Error converting service name \"" + event.getService() + "\" to an integer identifier, storing -1: " + e, e);
+            }
+        }
+        m_insStmt.setObject(6, (svcId == -1 ? null : new Integer(svcId)));
+    
+        //Column 7, reductionKey
+        m_insStmt.setString(7, event.getAlarmData().getReductionKey());
+        
+        //Column 8, alarmType
+        m_insStmt.setInt(8, event.getAlarmData().getAlarmType());
+        
+        //Column 9, counter
+        m_insStmt.setInt(9, 1);
+        
+        //Column 10, serverity
+        set(m_insStmt, 10, Constants.getSeverity(event.getSeverity()));
+    
+        //Column 11, lastEventId
+        m_insStmt.setInt(11, event.getDbid());
+        
+        //Column 12, firstEventTime
+        //Column 13, lastEventTime
+        Timestamp eventTime = getEventTime(event);
+        m_insStmt.setTimestamp(12, eventTime);
+        m_insStmt.setTimestamp(13, eventTime);
+        
+        //Column 14, description
+        set(m_insStmt, 14, Constants.format(event.getDescr(), EVENT_DESCR_FIELD_SIZE));
+    
+        //Column 15, logMsg
+        if (event.getLogmsg() != null) {
+            // set log message
+            set(m_insStmt, 15, Constants.format(event.getLogmsg().getContent(), EVENT_LOGMSG_FIELD_SIZE));
+        } else {
+            m_insStmt.setNull(15, Types.VARCHAR);
+        }
+    
+        //Column 16, operInstruct
+        set(m_insStmt, 16, Constants.format(event.getOperinstruct(), EVENT_OPERINSTRUCT_FIELD_SIZE));
+        
+        //Column 17, tticketId
+        //Column 18, tticketState
+        if (event.getTticket() != null) {
+            set(m_insStmt, 17, Constants.format(event.getTticket().getContent(), EVENT_TTICKET_FIELD_SIZE));
+            int ttstate = 0;
+            if (event.getTticket().getState().equals("on")) {
+                ttstate = 1;
+            }
+            set(m_insStmt, 18, ttstate);
+        } else {
+            m_insStmt.setNull(17, Types.VARCHAR);
+            m_insStmt.setNull(18, Types.INTEGER);
+        }
+    
+        //Column 19, mouseOverText
+        set(m_insStmt, 19, Constants.format(event.getMouseovertext(), EVENT_MOUSEOVERTEXT_FIELD_SIZE));
+    
+        //Column 20, suppressedUntil
+        set(m_insStmt, 20, eventTime);
+        
+        //Column 21, suppressedUser
+        m_insStmt.setString(21, null);
+        
+        //Column 22, suppressedTime
+        set(m_insStmt, 22, eventTime);
+        
+        //Column 23, alarmAckUser
+        m_insStmt.setString(23, null);
+        
+        //Column 24, alarmAckTime
+        m_insStmt.setTimestamp(24, null);
+        
+        //Column 25, clearUie
+        //Column 26, x733AlarmType
+        //Column 27, x733ProbableCause
+        //Column 28, clearKey
+        if (event.getAlarmData() == null) {
+            m_insStmt.setString(25, null);
+            m_insStmt.setString(26, null);
+            m_insStmt.setInt(27, -1);
+            m_insStmt.setString(28, null);
+        } else {
+            m_insStmt.setString(25, Constants.format(event.getAlarmData().getClearUei(), EVENT_UEI_FIELD_SIZE));
+            m_insStmt.setString(26, Constants.format(event.getAlarmData().getX733AlarmType(), EVENT_X733_ALARMTYPE_SIZE));
+            set(m_insStmt, 27, event.getAlarmData().getX733ProbableCause());
+            set(m_insStmt, 28, event.getAlarmData().getClearKey());
+        }
+        
+        if (log().isDebugEnabled()) {
+            log().debug("m_insStmt is: " + m_insStmt.toString());
+        }
+        
+        m_insStmt.executeUpdate();
+        
+        m_updateEventStmt.setInt(1, alarmID);
+        m_updateEventStmt.setInt(2, event.getDbid());
+        m_updateEventStmt.executeUpdate();
+    
+        if (log().isDebugEnabled()) {
+            log().debug("SUCCESSFULLY added " + event.getUei() + " related  data into the ALARMS table");
+        }
+    
+    }
+
     public String getGetNextAlarmIdStr() {
         return m_getNextAlarmIdStr;
     }
 
     public void setGetNextAlarmIdStr(String getNextAlarmIdStr) {
         m_getNextAlarmIdStr = getNextAlarmIdStr;
+    }
+
+    protected void updateAlarm(Header eventHeader, Event event, int alarmId) throws SQLException {
+        m_upDateStmt.setInt(1, event.getDbid());
+        m_upDateStmt.setTimestamp(2, getEventTime(event));
+        set(m_upDateStmt, 3, Constants.format(event.getLogmsg().getContent(), EVENT_LOGMSG_FIELD_SIZE));
+        m_upDateStmt.setString(4, event.getAlarmData().getReductionKey());
+    
+        if (log().isDebugEnabled()) {
+            log().debug("Persist.updateAlarm: reducing event: " + event.getDbid() +  " into alarm");
+        }
+        
+        m_upDateStmt.executeUpdate();
+    
+        m_updateEventStmt.setInt(1, alarmId);
+        m_updateEventStmt.setInt(2, event.getDbid());
+        m_updateEventStmt.executeUpdate();
+    }
+
+    protected int isReductionNeeded(Header eventHeader, Event event) throws SQLException {
+        if (log().isDebugEnabled()) {
+            log().debug("Persist.isReductionNeeded: reductionKey: " + event.getAlarmData().getReductionKey());
+        }
+    
+        m_reductionQuery.setString(1, event.getAlarmData().getReductionKey());
+    
+        ResultSet rs = null;
+        int alarmId;
+        try {
+            rs = m_reductionQuery.executeQuery();
+            alarmId = -1;
+            while (rs.next()) {
+                alarmId = rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            rs.close();
+        }
+        return alarmId;
+    }
+
+    protected void cleanPreviousEvents(int alarmId, int eventId) {
+        PreparedStatement stmt = null;
+        try {
+            stmt = m_dsConn.prepareStatement("DELETE FROM events WHERE alarmId = ? AND eventId != ?");
+            stmt.setInt(1, alarmId);
+            stmt.setInt(2, eventId);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            log().error("cleanPreviousEvents: Couldn't remove old events: " + e, e);
+        }
+    
+        try {
+            stmt.close();
+        } catch (SQLException e) {
+            log().error("cleanPreviousEvents: Couldn't close statement: " + e, e);
+        }
     }
 }
--- opennms-services/src/main/java/org/opennms/netmgt/eventd/EventWriter.java
+++ opennms-services/src/main/java/org/opennms/netmgt/eventd/EventWriter.java
@@ -10,6 +10,8 @@
 //
 // Modifications:
 //
+// 2008 Jan 27: Push methods in Persist that are only used by a single
+//              subclass into that subclass.
 // 2008 Jan 26: Dependency injection using setter injection instead of
 //              constructor injection and implement InitializingBean.
 //              Move some common setters and initializion into Persist.
@@ -36,15 +38,23 @@
 //      http://www.opennms.org/
 //      http://www.opennms.com/
 //
-// Tab Size = 8
-//
-
 package org.opennms.netmgt.eventd;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.opennms.netmgt.eventd.db.AutoAction;
+import org.opennms.netmgt.eventd.db.Constants;
+import org.opennms.netmgt.eventd.db.OperatorAction;
+import org.opennms.netmgt.eventd.db.Parameter;
+import org.opennms.netmgt.eventd.db.SnmpInfo;
 import org.opennms.netmgt.xml.event.Event;
 import org.opennms.netmgt.xml.event.Header;
+import org.opennms.netmgt.xml.event.Operaction;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
 
@@ -154,6 +164,292 @@ final class EventWriter extends Persist implements InitializingBean {
                 log().debug("EventWriter finished for : " + event.getUei());
             }
         }
+    }
+    
+
+
+    /**
+     * Insert values into the EVENTS table
+     * 
+     * @exception java.sql.SQLException
+     *                Thrown if there is an error adding the event to the
+     *                database.
+     * @exception java.lang.NullPointerException
+     *                Thrown if a required resource cannot be found in the
+     *                properties file.
+     */
+    protected void insertEvent(Header eventHeader, Event event) throws SQLException {
+        // Execute the statement to get the next event id
+        int eventID = getNextId();
+
+        if (log().isDebugEnabled()) {
+            log().debug("EventWriter: DBID: " + eventID);
+        }
+
+        synchronized (event) {
+            event.setDbid(eventID);
+        }
+
+        // Set up the sql information now
+
+        // eventID
+        m_insStmt.setInt(1, eventID);
+
+        // eventUEI
+        m_insStmt.setString(2, Constants.format(event.getUei(), EVENT_UEI_FIELD_SIZE));
+
+        // nodeID
+        int nodeid = (int) event.getNodeid();
+        set(m_insStmt, 3, event.hasNodeid() ? nodeid : -1);
+
+        // eventTime
+        m_insStmt.setTimestamp(4, getEventTime(event));
+        
+        // Resolve the event host to a hostname using the ipInterface table
+        String hostname = getEventHost(event);
+
+        // eventHost
+        set(m_insStmt, 5, Constants.format(hostname, EVENT_HOST_FIELD_SIZE));
+
+        // ipAddr
+        set(m_insStmt, 6, Constants.format(event.getInterface(), EVENT_INTERFACE_FIELD_SIZE));
+
+        // eventDpName
+        m_insStmt.setString(7, (eventHeader != null) ? Constants.format(eventHeader.getDpName(), EVENT_DPNAME_FIELD_SIZE) : "undefined");
+
+        // eventSnmpHost
+        set(m_insStmt, 8, Constants.format(event.getSnmphost(), EVENT_SNMPHOST_FIELD_SIZE));
+
+        // service identifier - convert the service name to a service id
+        set(m_insStmt, 9, getEventServiceId(event));
+
+        // eventSnmp
+        if (event.getSnmp() != null) {
+            m_insStmt.setString(10, SnmpInfo.format(event.getSnmp(), EVENT_SNMP_FIELD_SIZE));
+        } else {
+            m_insStmt.setNull(10, Types.VARCHAR);
+        }
+
+        // eventParms
+
+        // Replace any null bytes with a space, otherwise postgres will complain about encoding in UNICODE 
+        String parametersString=(event.getParms() != null) ? Parameter.format(event.getParms()) : null;
+        if (parametersString != null) {
+            parametersString=parametersString.replace((char)0, ' ');
+        }
+        
+        set(m_insStmt, 11, parametersString);
+
+        // grab the ifIndex out of the parms if it is defined   
+        if (event.getIfIndex() != null) {
+            if (event.getParms() != null) {
+                Parameter.format(event.getParms());
+            }
+        }
+
+        // eventCreateTime
+        Timestamp eventCreateTime = new Timestamp(System.currentTimeMillis());
+        m_insStmt.setTimestamp(12, eventCreateTime);
+
+        // eventDescr
+        set(m_insStmt, 13, Constants.format(event.getDescr(), EVENT_DESCR_FIELD_SIZE));
+
+        // eventLoggroup
+        set(m_insStmt, 14, (event.getLoggroupCount() > 0) ? Constants.format(event.getLoggroup(), EVENT_LOGGRP_FIELD_SIZE) : null);
+
+        // eventLogMsg
+        // eventLog
+        // eventDisplay
+        if (event.getLogmsg() != null) {
+            // set log message
+            set(m_insStmt, 15, Constants.format(event.getLogmsg().getContent(), EVENT_LOGMSG_FIELD_SIZE));
+            String logdest = event.getLogmsg().getDest();
+            if (logdest.equals("logndisplay")) {
+                // if 'logndisplay' set both log and display column to yes
+                set(m_insStmt, 16, MSG_YES);
+                set(m_insStmt, 17, MSG_YES);
+            } else if (logdest.equals("logonly")) {
+                // if 'logonly' set log column to true
+                set(m_insStmt, 16, MSG_YES);
+                set(m_insStmt, 17, MSG_NO);
+            } else if (logdest.equals("displayonly")) {
+                // if 'displayonly' set display column to true
+                set(m_insStmt, 16, MSG_NO);
+                set(m_insStmt, 17, MSG_YES);
+            } else if (logdest.equals("suppress")) {
+                // if 'suppress' set both log and display to false
+                set(m_insStmt, 16, MSG_NO);
+                set(m_insStmt, 17, MSG_NO);
+            }
+        } else {
+            m_insStmt.setNull(15, Types.VARCHAR);
+
+            /*
+             * If this is an event that had no match in the event conf
+             * mark it as to be logged and displayed so that there
+             * are no events that slip through the system
+             * without the user knowing about them
+             */
+            set(m_insStmt, 17, MSG_YES);
+        }
+
+        // eventSeverity
+        set(m_insStmt, 18, Constants.getSeverity(event.getSeverity()));
+
+        // eventPathOutage
+        set(m_insStmt, 19, (event.getPathoutage() != null) ? Constants.format(event.getPathoutage(), EVENT_PATHOUTAGE_FIELD_SIZE) : null);
+
+        // eventCorrelation
+        set(m_insStmt, 20, (event.getCorrelation() != null) ? org.opennms.netmgt.eventd.db.Correlation.format(event.getCorrelation(), EVENT_CORRELATION_FIELD_SIZE) : null);
+
+        // eventSuppressedCount
+        m_insStmt.setNull(21, Types.INTEGER);
+
+        // eventOperInstruct
+        set(m_insStmt, 22, Constants.format(event.getOperinstruct(), EVENT_OPERINSTRUCT_FIELD_SIZE));
+
+        // eventAutoAction
+        set(m_insStmt, 23, (event.getAutoactionCount() > 0) ? AutoAction.format(event.getAutoaction(), EVENT_AUTOACTION_FIELD_SIZE) : null);
+
+        // eventOperAction / eventOperActionMenuText
+        if (event.getOperactionCount() > 0) {
+            List<Operaction> a = new ArrayList<Operaction>();
+            List<String> b = new ArrayList<String>();
+
+            for (Operaction eoa : event.getOperactionCollection()) {
+                a.add(eoa);
+                b.add(eoa.getMenutext());
+            }
+
+            set(m_insStmt, 24, OperatorAction.format(a, EVENT_OPERACTION_FIELD_SIZE));
+            set(m_insStmt, 25, Constants.format(b, EVENT_OPERACTION_MENU_FIELD_SIZE));
+        } else {
+            m_insStmt.setNull(24, Types.VARCHAR);
+            m_insStmt.setNull(25, Types.VARCHAR);
+        }
+
+        // eventNotification, this column no longer needed
+        m_insStmt.setNull(26, Types.VARCHAR);
+
+        // eventTroubleTicket / eventTroubleTicket state
+        if (event.getTticket() != null) {
+            set(m_insStmt, 27, Constants.format(event.getTticket().getContent(), EVENT_TTICKET_FIELD_SIZE));
+            int ttstate = 0;
+            if (event.getTticket().getState().equals("on")) {
+                ttstate = 1;
+            }
+
+            set(m_insStmt, 28, ttstate);
+        } else {
+            m_insStmt.setNull(27, Types.VARCHAR);
+            m_insStmt.setNull(28, Types.INTEGER);
+        }
+
+        // eventForward
+        set(m_insStmt, 29, (event.getForwardCount() > 0) ? org.opennms.netmgt.eventd.db.Forward.format(event.getForward(), EVENT_FORWARD_FIELD_SIZE) : null);
+
+        // event mouseOverText
+        set(m_insStmt, 30, Constants.format(event.getMouseovertext(), EVENT_MOUSEOVERTEXT_FIELD_SIZE));
+
+        // eventAckUser
+        if (event.getAutoacknowledge() != null && event.getAutoacknowledge().getState().equals("on")) {
+            set(m_insStmt, 31, Constants.format(event.getAutoacknowledge().getContent(), EVENT_ACKUSER_FIELD_SIZE));
+
+            // eventAckTime - if autoacknowledge is present,
+            // set time to event create time
+            set(m_insStmt, 32, eventCreateTime);
+        } else {
+            m_insStmt.setNull(31, Types.INTEGER);
+            m_insStmt.setNull(32, Types.TIMESTAMP);
+        }
+
+        // eventSource
+        set(m_insStmt, 33, Constants.format(event.getSource(), EVENT_SOURCE_FIELD_SIZE));
+
+        // execute
+        m_insStmt.executeUpdate();
+
+        if (log().isDebugEnabled()) {
+            log().debug("SUCCESSFULLY added " + event.getUei() + " related  data into the EVENTS table");
+        }
+    }
+
+
+    /**
+     * This method is used to convert the event host into a hostname id by
+     * performing a lookup in the database. If the conversion is successful then
+     * the corresponding hosname will be returned to the caller.
+     * 
+     * @param hostip
+     *            The event host
+     * 
+     * @return The hostname
+     * 
+     * @exception java.sql.SQLException
+     *                Thrown if there is an error accessing the stored data or
+     *                the SQL text is malformed.
+     * 
+     * @see EventdConstants#SQL_DB_HOSTIP_TO_HOSTNAME
+     * 
+     */
+    private String getHostName(String hostip) throws SQLException {
+        // talk to the database and get the identifer
+        String hostname = hostip;
+
+        m_getHostNameStmt.setString(1, hostip);
+        ResultSet rset = null;
+        
+        try {
+            rset = m_getHostNameStmt.executeQuery();
+            if (rset.next()) {
+                hostname = rset.getString(1);
+            }
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            rset.close();
+        }
+
+        // hostname can be null - if it is, return the ip
+        if (hostname == null) {
+            hostname = hostip;
+        }
+
+        return hostname;
+    }
+
+    /**
+     * @param event
+     * @param log
+     * @return
+     */
+    private int getEventServiceId(Event event) {
+        int svcId = -1;
+        if (event.getService() != null) {
+            try {
+                svcId = getServiceID(event.getService());
+            } catch (SQLException e) {
+                log().warn("EventWriter.add: Error converting service name \"" + event.getService() + "\" to an integer identifier, storing -1: e" + e, e);
+            }
+        }
+        return svcId;
+    }
+
+    /**
+     * @param event
+     * @return
+     */
+    private String getEventHost(Event event) {
+        String hostname = event.getHost();
+        if (hostname != null) {
+            try {
+                hostname = getHostName(hostname);
+            } catch (SQLException sqlE) {
+                // hostname can be null - so use the IP
+                hostname = event.getHost();
+            }
+        }
+        return hostname;
     }
 
     public String getGetNextEventIdStr() {
