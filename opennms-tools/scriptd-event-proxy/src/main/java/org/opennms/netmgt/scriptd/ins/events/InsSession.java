ce62727b88fef40f10e7756a365cca59ffcd14d8
Antonio Russo <rssntn67@users.sourceforge.net>
2008-02-08 16:09:22 +0000

    remove method using UEI and add a method that use the Sql Restriction Added 2 super classes

--- opennms-tools/scriptd-event-proxy/src/main/java/org/opennms/netmgt/scriptd/ins/events/InsSession.java
+++ opennms-tools/scriptd-event-proxy/src/main/java/org/opennms/netmgt/scriptd/ins/events/InsSession.java
@@ -22,52 +22,36 @@ import org.opennms.netmgt.model.OnmsEvent;
 import org.opennms.netmgt.xml.event.AlarmData;
 import org.opennms.netmgt.xml.event.Event;
 import org.opennms.netmgt.xml.event.Logmsg;
+import org.opennms.netmgt.xml.event.Parm;
+import org.opennms.netmgt.xml.event.Parms;
+import org.opennms.netmgt.xml.event.Value;
 import org.springframework.beans.factory.access.BeanFactoryReference;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
-class InsSession extends Thread{
-	
+class InsSession extends InsAbstractSession {
+
 	private Category log;
-	
-	/**
-	 * The uei of the event representing an alarm
-	 */
-	private String alarmUEI;
-	
-	/**
-	 * The uei of the event to clear corrensponding alarm
-	 */
-	private String clearAlarmUEI;
-	
+
 	private Socket server;
 
 	private String line, input;
-	
-	//private CurrentAlarmsSender sendCurrentAlarmsThread=null;
+
+	// private CurrentAlarmsSender sendCurrentAlarmsThread=null;
 
 	private PrintStream streamToClient;
-	
-	
-	/**
-	 * the shared string for authentication
-	 */ 
-	private String sharedAsciiAuthString = null;
-	
-  
-	
+
 	// Client requests
 	private final static String START_AUTHENTICATION_REQUEST = "AUTH";
 
 	private final static String LIST_CURRENT_ALARM_REQUEST = "LIST_CURRENT_ALARM_REQUEST";
-	
+
 	private final static String STOP_ALARM_REQUEST = "STOP_ALARM";
-	
 
 	// Server answers
 	private final static String AUTH_REQUIRED_ACK = "AUTH_REQUIRED";
-	
+
 	private final static String AUTH_NOT_REQUIRED_ACK = "AUTH_NOT_REQUIRED";
 
 	private final static String AUTHENTICATION_SUCCESS = "AUTH-SUCCESS";
@@ -78,53 +62,23 @@ class InsSession extends Thread{
 
 	private final static String ACTIVE_ALARM_END = "ACTIVE_ALARM_END";
 
-	
 	// session statuses
 	private final int STARTING_SESSION_STATUS = 0;
 
 	private final int AUTHENTICATING_STATUS = 1;
 
 	private final int AUTHENTICATED_STATUS = 2;
-	
+
 	private final int DATAFLOW_STATUS = 3;
 
 	private int status = STARTING_SESSION_STATUS;
 
-	InsSession(Socket server)throws IOException {
-		log=ThreadCategory.getInstance(this.getClass());
+	InsSession(Socket server) throws IOException {
+		log = ThreadCategory.getInstance(this.getClass());
 		this.server = server;
 		streamToClient = new PrintStream(server.getOutputStream());
 	}
 
-	
-	public String getAlarmUEI() {
-		return alarmUEI;
-	}
-
-
-	public void setAlarmUEI(String alarmUEI) {
-		this.alarmUEI = alarmUEI;
-	}
-
-
-	public String getClearAlarmUEI() {
-		return clearAlarmUEI;
-	}
-
-
-	public void setClearAlarmUEI(String clearAlarmUEI) {
-		this.clearAlarmUEI = clearAlarmUEI;
-	}
-
-
-	public void setSharedAsciiAuthString(String sharedAsciiAuthString) {
-		this.sharedAsciiAuthString = sharedAsciiAuthString;
-	}
-	
-	public String getSharedAsciiAuthString() {
-		return sharedAsciiAuthString;
-	}
-	
 	public void run() {
 		input = "";
 
@@ -132,27 +86,26 @@ class InsSession extends Thread{
 			// Get input from the client
 			BufferedReader in = new BufferedReader(new InputStreamReader(server
 					.getInputStream()));
-			
 
-			readingFromClient:while ((line = in.readLine()) != null) {
+			readingFromClient: while ((line = in.readLine()) != null) {
 				input = input + "\n" + line;
 				log.debug("Client wrote: " + line + " from "
 						+ server.getInetAddress());
 
 				if (status == STARTING_SESSION_STATUS) {
 					if (line.equalsIgnoreCase(START_AUTHENTICATION_REQUEST)) {
-						if(sharedAsciiAuthString!=null){ 
-							//authorization required
+						if (sharedAuthAsciiString != null) {
+							// authorization required
 							streamToClient.println(AUTH_REQUIRED_ACK);
 							log.debug("Starting authentication, sending "
 									+ AUTH_REQUIRED_ACK + " to the client");
 							status = AUTHENTICATING_STATUS;
-						}else{
-							//authorization not required
+						} else {
+							// authorization not required
 							streamToClient.println(AUTH_NOT_REQUIRED_ACK);
 							log.debug("Starting authentication, sending "
 									+ AUTH_NOT_REQUIRED_ACK + " to the client");
-							status=AUTHENTICATED_STATUS;
+							status = AUTHENTICATED_STATUS;
 						}
 						continue readingFromClient;
 					} else {
@@ -163,17 +116,18 @@ class InsSession extends Thread{
 					}
 				}
 
-				if (status == AUTHENTICATING_STATUS){
-					if(sharedAsciiAuthString!=null){ 
+				if (status == AUTHENTICATING_STATUS) {
+					if (sharedAuthAsciiString != null) {
 						// authentication required (security check)
-						if (line.equals(sharedAsciiAuthString)) {
+						if (line.equals(sharedAuthAsciiString)) {
 							status = AUTHENTICATED_STATUS;
 							log.debug("Authentication success!");
 							streamToClient.println(AUTHENTICATION_SUCCESS);
 							continue readingFromClient;
 						} else {
 							streamToClient.println(RESET_SIGNAL);
-							log.warn("Authentication failure! Resetting session.");
+							log
+									.warn("Authentication failure! Resetting session.");
 							break readingFromClient;
 						}
 					}
@@ -182,116 +136,189 @@ class InsSession extends Thread{
 				if (status == AUTHENTICATED_STATUS || status == DATAFLOW_STATUS) {
 					if (line.equalsIgnoreCase(LIST_CURRENT_ALARM_REQUEST)) {
 						log.debug("Sending alarms to the client");
-						status=DATAFLOW_STATUS;
+						status = DATAFLOW_STATUS;
 						synchronized (streamToClient) {
 							streamToClient.println(ACTIVE_ALARM_BEGIN);
-							
-							BeanFactoryReference bf = BeanUtils.getBeanFactory("daoContext");
-				            final EventDao eventDao = BeanUtils.getBean(bf, "eventDao",
-				                    EventDao.class);
-				             
-				           
-				            TransactionTemplate transTemplate = BeanUtils.getBean(bf,
-				                    "transactionTemplate", TransactionTemplate.class);
-				            final StringWriter sw =  (StringWriter) transTemplate.execute(
-				            		new TransactionCallback() {
-				    		        	public Object doInTransaction(final TransactionStatus status) {
-				    		            	List<OnmsEvent> events = null;
-				    		            	StringWriter sw=new StringWriter();
-				    		            	try {
-				    		            		//TODO to control the query
-				    							final OnmsCriteria criteria = new OnmsCriteria(OnmsEvent.class);
-				    							criteria.add(Restrictions.sqlRestriction("eventuei = '"+alarmUEI+"' and alarmid not in (select alarmid from events where eventuei = '"+clearAlarmUEI+"')"));
-				    							events = eventDao.findMatching(criteria);
-				    							if (log.isDebugEnabled())
-				    								log.debug("Found "+events.size()+ " open event/s (alarms)");
-				    							Iterator<OnmsEvent> ite = events.iterator();
-				    							while(ite.hasNext()){ 
-				    			            		OnmsEvent ev = ite.next();
-				    			            		try {
-		    			    		            		Event e=new Event();
-		    			    		            		e.setDbid(ev.getId());
-		    		    				            	e.setUei(ev.getEventUei());
-		    		    				            	e.setSource(ev.getEventSource());
-		    		    				            	if (ev.getNode() != null) 
-		    		    				            		e.setNodeid(ev.getNode().getId());
-		    		    				            	DateFormat dfInstance = DateFormat.getDateInstance(DateFormat.FULL);
-		    		    				            	e.setTime(dfInstance.format(ev.getEventTime()));
-		    		    				            	e.setHost(ev.getEventHost());
-		    		    				            	e.setInterface(ev.getIpAddr());
-		    		    				            	if(ev.getServiceType()!=null)
-		    		    				            		e.setService(ev.getServiceType().getName());
-		    		    				            	e.setDescr(ev.getEventDescr());
-		    		    				            	Logmsg msg = new Logmsg();
-		    		    				            	msg.setContent(ev.getEventLogMsg());
-		    		    				            	e.setLogmsg(msg);
-		    		    				            	e.setSeverity(Constants.getSeverityString(ev.getEventSeverity()));
-		    		    				            	AlarmData ad = new AlarmData();
-		    		    				            	OnmsAlarm onmsAlarm = ev.getAlarm();
-		    		    				            	try {
-															if(onmsAlarm!=null){
-																ad.setReductionKey(onmsAlarm.getReductionKey());
-																ad.setAlarmType(onmsAlarm.getAlarmType());
-																ad.setClearKey(onmsAlarm.getClearKey());
-																e.setAlarmData(ad);
+
+							BeanFactoryReference bf = BeanUtils
+									.getBeanFactory("daoContext");
+							final EventDao eventDao = BeanUtils.getBean(bf,
+									"eventDao", EventDao.class);
+
+							TransactionTemplate transTemplate = BeanUtils
+									.getBean(bf, "transactionTemplate",
+											TransactionTemplate.class);
+							final StringWriter sw = (StringWriter) transTemplate
+									.execute(new TransactionCallback() {
+										public Object doInTransaction(
+												final TransactionStatus status) {
+											List<OnmsEvent> events = null;
+											StringWriter sw = new StringWriter();
+											try {
+												// TODO to control the query
+												final OnmsCriteria criteria = new OnmsCriteria(
+														OnmsEvent.class);
+//												criteria
+//														.add(Restrictions
+//																.sqlRestriction("eventuei = '"
+//																		+ alarmUEI
+//																		+ "' and alarmid not in (select alarmid from events where eventuei = '"
+//																		+ clearAlarmUEI
+//																		+ "')"));
+												criteria.add(Restrictions.sqlRestriction(criteriaRestriction));
+												events = eventDao
+														.findMatching(criteria);
+												if (log.isDebugEnabled())
+													log
+															.debug("Found "
+																	+ events
+																			.size()
+																	+ " open event/s (alarms)");
+												Iterator<OnmsEvent> ite = events
+														.iterator();
+												while (ite.hasNext()) {
+													OnmsEvent ev = ite.next();
+													try {
+														Event e = new Event();
+														e.setDbid(ev.getId());
+														e.setUei(ev
+																.getEventUei());
+														e
+																.setSource(ev
+																		.getEventSource());
+														if (ev.getNode() != null)
+															e.setNodeid(ev
+																	.getNode()
+																	.getId());
+														DateFormat dfInstance = DateFormat
+																.getDateInstance(DateFormat.FULL);
+														e
+																.setTime(dfInstance
+																		.format(ev
+																				.getEventTime()));
+														e
+																.setHost(ev
+																		.getEventHost());
+														e.setInterface(ev
+																.getIpAddr());
+														if (ev.getServiceType() != null)
+															e
+																	.setService(ev
+																			.getServiceType()
+																			.getName());
+														e
+																.setDescr(ev
+																		.getEventDescr());
+														Logmsg msg = new Logmsg();
+														msg
+																.setContent(ev
+																		.getEventLogMsg());
+														e.setLogmsg(msg);
+														e
+																.setSeverity(Constants
+																		.getSeverityString(ev
+																				.getEventSeverity()));
+														/*
+														Parms parms = new Parms();
+
+														String[] paramslistString = ev.getEventParms().split(";");
+														if (paramslistString != null) {
+															for (int ij =0; ij< paramslistString.length;ij++) {
+															    String[] paramEncoded = paramslistString[ij].split("=");
+															    if (paramEncoded != null && paramEncoded.length == 2) {
+																	Parm parm = new Parm();
+																	parm.setParmName(paramEncoded[0]);
+																	Value value = new Value();
+																	value.setContent(paramEncoded[1]);
+																	parm.setValue(value);
+															    	parms.addParm(parm);
+															    }
+															}
+														}
+														if (parms != null) e.setParms(parms);
+														*/
+														AlarmData ad = new AlarmData();
+														OnmsAlarm onmsAlarm = ev
+																.getAlarm();
+														try {
+															if (onmsAlarm != null) {
+																ad
+																		.setReductionKey(onmsAlarm
+																				.getReductionKey());
+																ad
+																		.setAlarmType(onmsAlarm
+																				.getAlarmType());
+																ad
+																		.setClearKey(onmsAlarm
+																				.getClearKey());
+																e
+																		.setAlarmData(ad);
 															}
 														} catch (ObjectNotFoundException e1) {
-															log.warn("correlated alarm data not found "+e1);
-															log.debug("correlated alarm data not found ",e1);
+															log
+																	.warn("correlated alarm data not found "
+																			+ e1);
+															log
+																	.debug(
+																			"correlated alarm data not found ",
+																			e1);
 														}
 														e.marshal(sw);
-		    			    						} catch (Exception ex) {
-		    			    							log.error("Error while getting event ",ex);
-		    			    							return null;
-		    			    						}
-		    			    						sw.flush();
-				    							}
-				    						} catch (final RuntimeException e) {
-				    							log.error("Error while getting events ",e);
-				    							return null;
-				    						}
-				    						return sw;
-				    						
-				    		            }
-				    		        	
-				    		        });
-						            if(sw!=null)
-						            	streamToClient.print(sw.toString());
-						            else
-						            	break readingFromClient;
-						            streamToClient.println(ACTIVE_ALARM_END);
-									continue readingFromClient;
-								}
-							} else {
-									if (line.equalsIgnoreCase(STOP_ALARM_REQUEST)) {
-										log.debug("Closing session due client request.");
-										break readingFromClient;
-									}else{
-										log.warn("Wrong client request");
-										continue readingFromClient;
-									}
-							}
+													} catch (Exception ex) {
+														log
+																.error(
+																		"Error while getting event ",
+																		ex);
+														return null;
+													}
+													sw.flush();
+												}
+											} catch (final RuntimeException e) {
+												log
+														.error(
+																"Error while getting events ",
+																e);
+												return null;
+											}
+											return sw;
+
+										}
+
+									});
+							if (sw != null)
+								streamToClient.print(sw.toString());
+							else
+								break readingFromClient;
+							streamToClient.println(ACTIVE_ALARM_END);
+							continue readingFromClient;
+						}
+					} else {
+						if (line.equalsIgnoreCase(STOP_ALARM_REQUEST)) {
+							log.debug("Closing session due client request.");
+							break readingFromClient;
+						} else {
+							log.warn("Wrong client request");
+							continue readingFromClient;
+						}
+					}
 				}
 			}
 
-			log.debug("Overall message from "
-					+ server.getInetAddress() + " is:" + input);
-			log.debug("\nClosing session with "+server.getInetAddress()+"...\n\n");
-			// close the session (socket) with the InsClient
+			log.debug("Overall message from " + server.getInetAddress()
+					+ " is:" + input);
+			log.debug("\nClosing session with " + server.getInetAddress()
+					+ "...\n\n");
 			server.close();
 		} catch (IOException ioe) {
-			log.warn("IOException on socket listen: " + ioe,ioe);
+			log.warn("IOException on socket listen: " + ioe, ioe);
 		}
 	}
-	
-	/*public CurrentAlarmsSender getSendCurrentAlarmsThread() {
-		return sendCurrentAlarmsThread;
-	}*/
-
 
 	public PrintStream getStreamToClient() {
-		if(status==DATAFLOW_STATUS)
+		if (status == DATAFLOW_STATUS)
 			return streamToClient;
-		else return null;
+		else
+			return null;
 	}
+
 }
