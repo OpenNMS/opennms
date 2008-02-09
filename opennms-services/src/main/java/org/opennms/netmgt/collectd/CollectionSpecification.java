dcd90ef5be3f81bf93501476f33d983067533c55
DJ Gregor <djgregor@users.sourceforge.net>
2008-02-09 17:17:29 +0000

    Indent, format code and comments.

--- opennms-services/src/main/java/org/opennms/netmgt/collectd/CollectionSpecification.java
+++ opennms-services/src/main/java/org/opennms/netmgt/collectd/CollectionSpecification.java
@@ -8,6 +8,10 @@
 //
 // OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
 //
+// Modfications:
+//
+// 2008 Feb 09: Indent, format code and comments. - dj@opennms.org
+//
 // Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
 //
 // This program is free software; you can redistribute it and/or modify
@@ -29,7 +33,6 @@
 //     http://www.opennms.org/
 //     http://www.opennms.com/
 //
-
 package org.opennms.netmgt.collectd;
 
 import java.util.Collection;
@@ -52,106 +55,106 @@ import org.opennms.netmgt.xml.event.Event;
 import org.opennms.netmgt.xml.event.Log;
 
 public class CollectionSpecification {
-	
-	private CollectdPackage m_package;
-	private String m_svcName;
-	private ServiceCollector m_collector;
-	private Map m_parameters;
-  
+
+    private CollectdPackage m_package;
+    private String m_svcName;
+    private ServiceCollector m_collector;
+    private Map m_parameters;
+
     //FIXME: Why is this not used?
-	private Collection m_outageCalendars;
-	
-	
-	public CollectionSpecification(CollectdPackage wpkg, String svcName, Collection outageCalendars, ServiceCollector collector) {
-		m_package = wpkg;
-		m_svcName = svcName;
-		m_collector = collector;
-		m_outageCalendars = outageCalendars;
-		initializeParameters();
-	}
+    private Collection m_outageCalendars;
 
-	public String getPackageName() {
-		return m_package.getName();
-	}
-	
-	private String storeByIfAlias() {
-		return m_package.storeByIfAlias();
-	}
 
-	private String ifAliasComment() {
-		return m_package.ifAliasComment();
-	}
+    public CollectionSpecification(CollectdPackage wpkg, String svcName, Collection outageCalendars, ServiceCollector collector) {
+        m_package = wpkg;
+        m_svcName = svcName;
+        m_collector = collector;
+        m_outageCalendars = outageCalendars;
+        initializeParameters();
+    }
 
-	private String storeFlagOverride() {
-		return m_package.getStorFlagOverride();
-	}
+    public String getPackageName() {
+        return m_package.getName();
+    }
 
-	private String ifAliasDomain() {
-		return m_package.ifAliasDomain();
-	}
+    private String storeByIfAlias() {
+        return m_package.storeByIfAlias();
+    }
 
-	private String storeByNodeId() {
-		return m_package.storeByNodeId();
-	}
+    private String ifAliasComment() {
+        return m_package.ifAliasComment();
+    }
 
-	private Service getService() {
-		return m_package.getService(m_svcName);
-	}
+    private String storeFlagOverride() {
+        return m_package.getStorFlagOverride();
+    }
 
-	public String getServiceName() {
-		return m_svcName;
-	}
+    private String ifAliasDomain() {
+        return m_package.ifAliasDomain();
+    }
 
-	private void setPackage(CollectdPackage pkg) {
-		m_package = pkg;
-	}
-	
-	public long getInterval() {
-		return getService().getInterval();
-		
-	}
-	
-	public String toString() {
-		return m_svcName + '/' + m_package.getName();
-	}
+    private String storeByNodeId() {
+        return m_package.storeByNodeId();
+    }
 
-	private ServiceCollector getCollector() {
-		return m_collector;
-	}
+    private Service getService() {
+        return m_package.getService(m_svcName);
+    }
 
-	private Map getPropertyMap() {
-		return m_parameters;
-	}
+    public String getServiceName() {
+        return m_svcName;
+    }
 
-        /**
-         * Return a read only instance of the parameters, which consists of the overall service parameters,
-         * plus various other Collection specific parameters (e.g. storeByNodeID etc)
-         * @return A read only Map instance
-         */
-	public Map getReadOnlyPropertyMap() {
-	    return Collections.unmodifiableMap(m_parameters);
-	}
-        
-	private Category log() {
-		return ThreadCategory.getInstance(getClass());
-	}
+    private void setPackage(CollectdPackage pkg) {
+        m_package = pkg;
+    }
 
-	private boolean isTrue(String stg) {
-		return stg.equalsIgnoreCase("yes") || stg.equalsIgnoreCase("on") || stg.equalsIgnoreCase("true");
-	}
+    public long getInterval() {
+        return getService().getInterval();
 
-	private boolean isFalse(String stg) {
-		return (stg.equalsIgnoreCase("no") || stg.equalsIgnoreCase("off") || stg.equalsIgnoreCase("false"));
-	}
+    }
 
-	@SuppressWarnings("unchecked")
+    public String toString() {
+        return m_svcName + '/' + m_package.getName();
+    }
+
+    private ServiceCollector getCollector() {
+        return m_collector;
+    }
+
+    private Map getPropertyMap() {
+        return m_parameters;
+    }
+
+    /**
+     * Return a read only instance of the parameters, which consists of the overall service parameters,
+     * plus various other Collection specific parameters (e.g. storeByNodeID etc)
+     * @return A read only Map instance
+     */
+    public Map getReadOnlyPropertyMap() {
+        return Collections.unmodifiableMap(m_parameters);
+    }
+
+    private Category log() {
+        return ThreadCategory.getInstance(getClass());
+    }
+
+    private boolean isTrue(String stg) {
+        return stg.equalsIgnoreCase("yes") || stg.equalsIgnoreCase("on") || stg.equalsIgnoreCase("true");
+    }
+
+    private boolean isFalse(String stg) {
+        return stg.equalsIgnoreCase("no") || stg.equalsIgnoreCase("off") || stg.equalsIgnoreCase("false");
+    }
+
+    @SuppressWarnings("unchecked")
     private void initializeParameters() {
-		Map<String, String> m = new TreeMap<String, String>();
-		m.put("SERVICE", m_svcName);
+        Map<String, String> m = new TreeMap<String, String>();
+        m.put("SERVICE", m_svcName);
         StringBuffer sb;
         Collection<Parameter> params = getService().getParameterCollection();
         for (Parameter p : params) {
-            if(log().isDebugEnabled()) {
+            if (log().isDebugEnabled()) {
                 sb = new StringBuffer();
                 sb.append("initializeParameters: adding service: ");
                 sb.append(getServiceName());
@@ -164,27 +167,27 @@ public class CollectionSpecification {
             m.put(p.getKey(), p.getValue());
         }
 
-		if(storeByIfAlias() != null && isTrue(storeByIfAlias())) {
-			m.put("storeByIfAlias", "true");
-			if(storeByNodeId() != null) {
-				if(isTrue(storeByNodeId())) {
-					m.put("storeByNodeID", "true");
-				} else if(isFalse(storeByNodeId())) {
-					m.put("storeByNodeID", "false");
-				} else {
-					m.put("storeByNodeID", "normal");
-				}
-			}
-			if(ifAliasDomain() != null) {
-				m.put("domain", ifAliasDomain());
-			} else {
-				m.put("domain", getPackageName());
-			}
-			if(storeFlagOverride() != null && isTrue(storeFlagOverride())) {
-				m.put("storFlagOverride", "true");
-			}
-			m.put("ifAliasComment", ifAliasComment());
-			if (log().isDebugEnabled()) {
+        if (storeByIfAlias() != null && isTrue(storeByIfAlias())) {
+            m.put("storeByIfAlias", "true");
+            if (storeByNodeId() != null) {
+                if (isTrue(storeByNodeId())) {
+                    m.put("storeByNodeID", "true");
+                } else if(isFalse(storeByNodeId())) {
+                    m.put("storeByNodeID", "false");
+                } else {
+                    m.put("storeByNodeID", "normal");
+                }
+            }
+            if (ifAliasDomain() != null) {
+                m.put("domain", ifAliasDomain());
+            } else {
+                m.put("domain", getPackageName());
+            }
+            if (storeFlagOverride() != null && isTrue(storeFlagOverride())) {
+                m.put("storFlagOverride", "true");
+            }
+            m.put("ifAliasComment", ifAliasComment());
+            if (log().isDebugEnabled()) {
                 sb = new StringBuffer();
                 sb.append("ifAliasDomain = ");
                 sb.append(ifAliasDomain());
@@ -196,94 +199,96 @@ public class CollectionSpecification {
                 sb.append(storeFlagOverride());
                 sb.append(", ifAliasComment = ");
                 sb.append(ifAliasComment());
-				log().debug(sb.toString());
+                log().debug(sb.toString());
             }
-		}
-		m_parameters = m;
-	}
+        }
+        m_parameters = m;
+    }
 
-	public void initialize(CollectionAgent agent) {
+    public void initialize(CollectionAgent agent) {
         Collectd.instrumentation().beginCollectorInitialize(agent.getNodeId(), agent.getHostAddress(), m_svcName);
-	    try {
-	        m_collector.initialize(agent, getPropertyMap());
-	    } finally {
-	        Collectd.instrumentation().endCollectorInitialize(agent.getNodeId(), agent.getHostAddress(), m_svcName);
-	    }
-	}
+        try {
+            m_collector.initialize(agent, getPropertyMap());
+        } finally {
+            Collectd.instrumentation().endCollectorInitialize(agent.getNodeId(), agent.getHostAddress(), m_svcName);
+        }
+    }
 
-	public void release(CollectionAgent agent) {
+    public void release(CollectionAgent agent) {
         Collectd.instrumentation().beginCollectorRelease(agent.getNodeId(), agent.getHostAddress(), m_svcName);
-	    try {
-	        m_collector.release(agent);
-	    } finally {
-	        Collectd.instrumentation().endCollectorRelease(agent.getNodeId(), agent.getHostAddress(), m_svcName);
-	    }
-	}
+        try {
+            m_collector.release(agent);
+        } finally {
+            Collectd.instrumentation().endCollectorRelease(agent.getNodeId(), agent.getHostAddress(), m_svcName);
+        }
+    }
 
-	public CollectionSet collect(CollectionAgent agent) {
+    public CollectionSet collect(CollectionAgent agent) {
         Collectd.instrumentation().beginCollectorCollect(agent.getNodeId(), agent.getHostAddress(), m_svcName);
-	    try {
-	        return getCollector().collect(agent, eventProxy(), getPropertyMap());
-	    } finally {
-	        Collectd.instrumentation().endCollectorCollect(agent.getNodeId(), agent.getHostAddress(), m_svcName);
-	    }
-	        
-	}
+        try {
+            return getCollector().collect(agent, eventProxy(), getPropertyMap());
+        } finally {
+            Collectd.instrumentation().endCollectorCollect(agent.getNodeId(), agent.getHostAddress(), m_svcName);
+        }
 
-	private EventProxy eventProxy() {
-		return new EventProxy() {
-		    public void send(Event e) {
-		        EventIpcManagerFactory.getIpcManager().sendNow(e);
-		    }
-	
-		    public void send(Log log) {
-		        EventIpcManagerFactory.getIpcManager().sendNow(log);
-		    }
-		};
-	}
+    }
 
-	public boolean scheduledOutage(CollectionAgent agent) {
-		boolean outageFound = false;
-	
-	    PollOutagesConfigFactory outageFactory = PollOutagesConfigFactory.getInstance();
-	
-	    // Iterate over the outage names defined in the interface's package.
-	    // For each outage...if the outage contains a calendar entry which
-	    // applies to the current time and the outage applies to this
-	    // interface then break and return true. Otherwise process the
-	    // next outage.
-	    // 
-		Iterator iter = m_package.getPackage().getOutageCalendarCollection().iterator();
-	    while (iter.hasNext()) {
-	        String outageName = (String) iter.next();
-	
-	        // Does the outage apply to the current time?
-	        if (outageFactory.isCurTimeInOutage(outageName)) {
-	            // Does the outage apply to this interface?
-				if ((outageFactory.isNodeIdInOutage(agent.getNodeId(), outageName)) ||
-			(outageFactory.isInterfaceInOutage(agent.getHostAddress(), outageName)))
-		{
-					if (log().isDebugEnabled())
-	                    log().debug("scheduledOutage: configured outage '" + outageName + "' applies, interface " + agent.getHostAddress() + " will not be collected for " + this);
-	                outageFound = true;
-	                break;
-	            }
-	        }
-	    }
-	
-	    return outageFound;
-	}
+    private EventProxy eventProxy() {
+        return new EventProxy() {
+            public void send(Event e) {
+                EventIpcManagerFactory.getIpcManager().sendNow(e);
+            }
 
-	public void refresh(CollectorConfigDao collectorConfigDao) {
-		CollectdPackage refreshedPackage=collectorConfigDao.getPackage(getPackageName());
-		if(refreshedPackage!=null) {
-			setPackage(refreshedPackage);
-		}
-	}
+            public void send(Log log) {
+                EventIpcManagerFactory.getIpcManager().sendNow(log);
+            }
+        };
+    }
+
+    public boolean scheduledOutage(CollectionAgent agent) {
+        boolean outageFound = false;
+
+        PollOutagesConfigFactory outageFactory = PollOutagesConfigFactory.getInstance();
+
+        /*
+         * Iterate over the outage names defined in the interface's package.
+         * For each outage...if the outage contains a calendar entry which
+         * applies to the current time and the outage applies to this
+         * interface then break and return true. Otherwise process the
+         * next outage.
+         */ 
+        Iterator<String> iter = m_package.getPackage().getOutageCalendarCollection().iterator();
+        while (iter.hasNext()) {
+            String outageName = iter.next();
+
+            // Does the outage apply to the current time?
+            if (outageFactory.isCurTimeInOutage(outageName)) {
+                // Does the outage apply to this interface?
+                if ((outageFactory.isNodeIdInOutage(agent.getNodeId(), outageName)) ||
+                        (outageFactory.isInterfaceInOutage(agent.getHostAddress(), outageName)))
+                {
+                    if (log().isDebugEnabled()) {
+                        log().debug("scheduledOutage: configured outage '" + outageName + "' applies, interface " + agent.getHostAddress() + " will not be collected for " + this);
+                    }
+                    outageFound = true;
+                    break;
+                }
+            }
+        }
+
+        return outageFound;
+    }
+
+    public void refresh(CollectorConfigDao collectorConfigDao) {
+        CollectdPackage refreshedPackage = collectorConfigDao.getPackage(getPackageName());
+        if (refreshedPackage != null) {
+            setPackage(refreshedPackage);
+        }
+    }
 
 
     public RrdRepository getRrdRepository(String collectionName) {
         return m_collector.getRrdRepository(collectionName);
     }
-	
+
 }
