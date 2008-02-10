94d7e0ce786655928e390a325fbbae5ba1817f94
DJ Gregor <djgregor@users.sourceforge.net>
2008-02-10 19:42:34 +0000

    - Eliminate warnings in tests. - Refactor C3P0ConnectionFactory while I'm here.

--- opennms-dao/src/test/java/org/opennms/netmgt/config/C3P0ConnectionFactoryTest.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/config/C3P0ConnectionFactoryTest.java
@@ -3,7 +3,6 @@ package org.opennms.netmgt.config;
 import java.beans.PropertyVetoException;
 import java.io.IOException;
 import java.io.Reader;
-import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,46 +11,36 @@ import junit.framework.TestCase;
 
 import org.exolab.castor.xml.MarshalException;
 import org.exolab.castor.xml.ValidationException;
+import org.opennms.test.ConfigurationTestUtils;
 
 public class C3P0ConnectionFactoryTest extends TestCase {
+    public void testMarshalDataSourceFromConfig() throws Exception {
+        makeFactory("opennms");
+        C3P0ConnectionFactory factory2 = makeFactory("opennms2");
+        
+        Connection conn = null;
+        Statement s = null;
+        try {
+            conn = factory2.getConnection();
+            s = conn.createStatement();
+            s.execute("select * from service");
+        } finally {
+            if (s != null) {
+                s.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
 
-	private static String m_config = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
-			"<datasource-configuration xmlns:this=\"http://xmlns.opennms.org/xsd/config/opennms-datasources\" \n" + 
-			"  xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" \n" + 
-			"  xsi:schemaLocation=\"http://xmlns.opennms.org/xsd/config/opennms-datasources \n" + 
-			"                      http://www.opennms.org/xsd/config/opennms-datasources.xsd \">\n" + 
-			"  <jdbc-data-source name=\"opennms\" \n" + 
-			"                    class-name=\"org.postgresql.Driver\" \n" + 
-			"                    url=\"jdbc:postgresql://localhost:5432/opennms\"\n" + 
-			"                    user-name=\"opennms\"\n" + 
-			"                    password=\"opennms\" />\n" + 
-			"  <jdbc-data-source name=\"opennms2\" " + 
-			"                    class-name=\"org.postgresql.Driver\" " + 
-			"                    url=\"jdbc:postgresql://localhost:5432/opennms\"> " + 
-			"                    <param name=\"user\" value=\"opennms\"/> " + 
-			"                    <param name=\"password\" value=\"opennms\"/> " +
-			"  </jdbc-data-source> " + 
-			"</datasource-configuration>";
-	
-	public void testMarshalDataSourceFromConfig() throws MarshalException, ValidationException, PropertyVetoException, SQLException, IOException {
-		
-		Connection conn = null;
-		Statement s = null;
-		try {
-		Reader rdr = new StringReader(m_config);
-		C3P0ConnectionFactory factory = new C3P0ConnectionFactory(rdr, "opennms");
-		rdr.close();
-		rdr = new StringReader(m_config);
-		C3P0ConnectionFactory factory2 = new C3P0ConnectionFactory(rdr, "opennms2");
-		rdr.close();
-		conn = factory2.getConnection();
-		s = conn.createStatement();
-		s.execute("select * from service");
-		} finally {
-			if (s != null ) s.close();
-			if (conn != null) conn.close();
-		}
-		
-	}
+    }
 
+    private C3P0ConnectionFactory makeFactory(String database) throws MarshalException, ValidationException, PropertyVetoException, SQLException, IOException {
+        Reader rdr = ConfigurationTestUtils.getReaderForResource(this, "opennms-datasources.xml");
+        try {
+            return new C3P0ConnectionFactory(rdr, database);
+        } finally {
+            rdr.close();
+        }
+    }
 }
