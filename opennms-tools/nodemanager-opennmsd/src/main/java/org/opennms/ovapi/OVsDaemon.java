f6b538cd3d1f206bcec87e76569b68287c445c30
Matt Brozowski <brozow@users.sourceforge.net>
2008-02-16 18:52:42 +0000

    framework for OVsPmd, Snmp, and timers in opennms daemon

--- opennms-tools/nodemanager-opennmsd/src/main/java/org/opennms/ovapi/OVsDaemon.java
+++ opennms-tools/nodemanager-opennmsd/src/main/java/org/opennms/ovapi/OVsDaemon.java
@@ -31,82 +31,99 @@ package org.opennms.ovapi;
 
 import java.io.FileWriter;
 import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.opennms.nnm.swig.NNM;
 import org.opennms.nnm.swig.OVsPMDCommand;
 import org.opennms.nnm.swig.fd_set;
 import org.opennms.nnm.swig.timeval;
 
-import edu.emory.mathcs.backport.java.util.concurrent.Callable;
-
-public abstract class OVsDaemon implements Callable {
+public abstract class OVsDaemon {
     
-    private static class DefaultOVsDaemon extends OVsDaemon {
+    public abstract static class ScheduledTask implements Runnable {
+        
+        long m_nextRuntimeInMillis = -1;
+        long m_period = 0;
 
+        public long getNextRuntimeMillis() {
+            return m_nextRuntimeInMillis;
+        }
+        
+        public void setNextRuntimeMillis(long nextRunTimeMillis) {
+            m_nextRuntimeInMillis = nextRunTimeMillis;
+        }
+        
+        public long getPeriod() {
+            return m_period;
+        }
+        
+        public void setPeriod(long period) {
+            m_period = period;
+        }
+        
+        public abstract void run();
+
+        public void setTimeout(long now, timeval tm) {
+            long remaining = (m_nextRuntimeInMillis - now);
+            if (remaining < tm.getTimeInMillis()) {
+                tm.setTimeInMillis(remaining);
+            }
+        }
+
+        public boolean isReady(long now) {
+            return now > m_nextRuntimeInMillis;
+        }
+
+        boolean setNextPeriod() {
+            if (!isPeriodic()) {
+                return false;
+            } else {
+                setNextRuntimeMillis(getNextRuntimeMillis()+getPeriod());
+                return true;
+            }
+        }
+
+        private boolean isPeriodic() {
+            return getPeriod() > 0;
+        }
+        
+    }
+    
+    static class DefaultOVsDaemon extends OVsDaemon {
+        
+        
         protected String onInit() {
+            
+            ScheduledTask statusReporter = new ScheduledTask() {
+                
+                long m_lastRun = System.currentTimeMillis();
+
+                public void run() {
+                    long now = System.currentTimeMillis();
+                    setStatus("Timer run at "+now+" after " + (now - m_lastRun) + " millis.  Period is "+getPeriod());
+                    m_lastRun = now;
+                }
+                
+            };
+            
+            schedule(statusReporter, 10000, 20000);
+            
             return "DefaultOVsDaemon has finished initializing.";
         }
 
         protected String onStop() {
-            return "stop called";
-        }
-
-        public Object XXcall() throws Exception {
-            
-            long start = System.currentTimeMillis();
-            long end = start;
-            while ( (end - start) < 120000 ) {
-                setStatus("DefaultOVsDaemon has been running for "  + (end - start) + " ms");
-                end = System.currentTimeMillis();
-            }
-            
-            return "DefaultOVsDaemon has finished and is now exiting.";
-        }
-        
-        public Object call() throws Exception {
-            long start = System.currentTimeMillis();
-            long end = start;
-            int code = NNM.OVS_CMD_NOOP; 
-            
-
-            fd_set fdset = new fd_set();
-            timeval tm = new timeval();
-
-            boolean finished = false;
-            while (!finished) {
-                fdset.zero();
-                fdset.set(getPmdFd());
-            
-                tm.setTimeInMillis(1000);
-                
-            
-                long selectStart = System.currentTimeMillis();
-                int fds = NNM.select(getPmdFd()+1, fdset, null, null, tm);
-                long selectEnd = System.currentTimeMillis();
-                end = selectEnd;
-                
-                setStatus("Select returned after "+(selectEnd - selectStart)+ " millis.  pmd " + 
-                        (fdset.isSet(getPmdFd()) ? "is" : "is not") + " set in readfds, return val is "+fds+", elapsed time = "+(end - start)+ "ms");
-                
-                code = NNM.OVS_CMD_NOOP;
-                if (fdset.isSet(getPmdFd())) {
-                    code = readPmdCmd();
-                    setStatus("Received cmd code "+code+" from pmd");
-                }
-                
-                if (code == NNM.OVS_CMD_EXIT) {
-                    finished = true;
-                }
-            }     
-            
-            Thread.sleep(5000);
-            end = System.currentTimeMillis();
-            return "DefaultOVsDaemon has finished and is now exiting after "+(end-start)+" ms. code was "+code+" pmdfs set is "+fdset.isSet(getPmdFd());
+            return "DefaultOVsDaemon has exited successfully.";
         }
         
     }
     
     private int m_ovspmdFd;
+    private boolean m_finished = false;
+    
+    private List m_scheduledTasks = new LinkedList();
+    private long m_selectedMillis;
 
     protected abstract String onInit();
 
@@ -185,6 +202,99 @@ public abstract class OVsDaemon implements Callable {
         }
         
         return command.getCode();
+    }
+
+    boolean isFinished() {
+        return m_finished;
+    }
+
+    void setFinished(boolean finished) {
+        m_finished = finished;
+    }
+
+    public Object call() throws Exception {
+        long start = System.currentTimeMillis();
+        long end = start;
+        
+    
+        fd_set fdset = new fd_set();
+        timeval tm = new timeval();
+
+    
+        while (!isFinished()) {
+            fdset.zero();
+            tm.setTimeInMillis(Integer.MAX_VALUE);
+            int maxFDs = getRetryInfo(fdset, tm);
+            m_selectedMillis = tm.getTimeInMillis();
+            long selectStart = System.currentTimeMillis();
+            int fds = NNM.select(maxFDs, fdset, null, null, tm);
+            long selectEnd = System.currentTimeMillis();
+            log("Returned from select after "+(selectEnd - selectStart)+" ms.");
+            if (fds > 0) {
+                processReads(fdset);
+            }
+            
+            processTimeouts();
+        }     
+        
+        end = System.currentTimeMillis();
+        
+        return onStop();
+    }
+
+    protected void processTimeouts() {
+
+        // we clone the list so we don't have concurrent modifications if tasks add new tasks
+        List tasks = new LinkedList(m_scheduledTasks);
+
+        long now = System.currentTimeMillis();
+        for(Iterator it = tasks.iterator(); it.hasNext(); ) {
+            ScheduledTask task = (ScheduledTask)it.next();
+            if (task.isReady(now)) {
+                task.run();
+                
+                if (task.setNextPeriod()) {
+                    // reschedule it for next time
+                    m_scheduledTasks.add(task);
+                }
+            }
+        }
+    }
+
+    protected void processReads(fd_set fdset) {
+        if (fdset.isSet(getPmdFd())) {
+            int code = readPmdCmd();
+            setStatus("Received cmd code "+code+" from pmd");
+    
+            if (code == NNM.OVS_CMD_EXIT) {
+                setFinished(true);
+            }
+        }
+    
+    }
+    
+    public void schedule(ScheduledTask task, long delay) {
+        schedule(task, delay, 0);
+    }
+    
+    public void schedule(ScheduledTask task, long delay, long period) {
+        task.setNextRuntimeMillis(System.currentTimeMillis()+delay);
+        task.setPeriod(period);
+        m_scheduledTasks.add(task);
+    }
+
+    protected int getRetryInfo(fd_set fdset, timeval tm) {
+        fdset.set(getPmdFd());
+        setTimeout(tm);
+        return getPmdFd()+1;
+    }
+
+    private void setTimeout(timeval tm) {
+        long now = System.currentTimeMillis();
+        for(Iterator it = m_scheduledTasks.iterator(); it.hasNext();) {
+            ScheduledTask task = (ScheduledTask)it.next();
+            task.setTimeout(now, tm);
+        }
     }
 
     public static void log(String msg) {
