371c82d56f9f9862a3b76631b2baf78ee1b11d7d
David Hustace <dhustace@users.sourceforge.net>
2008-03-14 15:32:38 +0000

    Merged revisions 8611-8612,8614-8616 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ........
      r8611 | brozow | 2008-03-12 14:54:31 -0400 (Wed, 12 Mar 2008) | 1 line
      
      bug# 2336 Change shutdown hook registration
    ........
      r8612 | brozow | 2008-03-12 17:26:20 -0400 (Wed, 12 Mar 2008) | 1 line
      
      bug #2336 make sure that we avoid calling System.exit from shutdown hook
    ........
      r8614 | brozow | 2008-03-13 12:16:59 -0400 (Thu, 13 Mar 2008) | 1 line
      
      bug #1871 make sure we exit when getting exceptions during registration
    ........
      r8615 | brozow | 2008-03-13 13:00:50 -0400 (Thu, 13 Mar 2008) | 1 line
      
      bug #1871 enhance exception handling and introduce FatalError state so force exitting during any unexpected exception
    ........
      r8616 | brozow | 2008-03-13 15:43:36 -0400 (Thu, 13 Mar 2008) | 1 line
      
      make sure initialize throws exceptions so we can exit during a failure
    ........

--- opennms-webstart/src/main/java/org/opennms/webstart/poller/Main.java
+++ opennms-webstart/src/main/java/org/opennms/webstart/poller/Main.java
@@ -32,6 +32,8 @@
 
 package org.opennms.webstart.poller;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -51,6 +53,7 @@ public class Main {
     PollerFrontEnd m_frontEnd;
     String m_url;
     String m_locationName;
+    boolean m_shuttingDown = false;
     
     
     private Main(String[] args) {
@@ -59,11 +62,18 @@ public class Main {
     
     private void run() {
         
-        parseArguments();
+        try {
         
-        createAppContext();
+            parseArguments();
         
-        registerShutDownHook();
+            createAppContext();
+        
+            registerShutDownHook();
+        
+        } catch(Exception e) {
+            System.err.println("Error occured starting!" + e);
+            System.exit(27);
+        }
 
     }
 
@@ -90,7 +100,13 @@ public class Main {
     }
 
     private void registerShutDownHook() {
-        m_context.registerShutdownHook();
+        Thread shutdownHook = new Thread() {
+            public void run() {
+                m_shuttingDown = true;
+                m_context.close();
+            }
+        };
+        Runtime.getRuntime().addShutdownHook(shutdownHook);
     }
 
     private void createAppContext() {
@@ -114,6 +130,18 @@ public class Main {
         
         m_context = new ClassPathXmlApplicationContext(configs);
         m_frontEnd = (PollerFrontEnd) m_context.getBean("pollerFrontEnd");
+        
+        m_frontEnd.addPropertyChangeListener(new PropertyChangeListener() {
+
+            public void propertyChange(PropertyChangeEvent evt) {
+                if ("exitNecessary".equals(evt.getPropertyName()) && Boolean.TRUE.equals(evt.getNewValue())) {
+                    if (!m_shuttingDown) {
+                        System.exit(27);
+                    }
+                }
+            }
+            
+        });
     }
         
     private void usage() {
