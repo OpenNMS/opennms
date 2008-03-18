56269f771951b0196726e7902092a66e14ec8a88
Mike Huot <mhuot@users.sourceforge.net>
2008-03-18 15:01:25 +0000

    Merged revisions 8644-8665,8667-8668 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/bug-913-branch
    ........
      r8649 | mhuot | 2008-03-17 21:57:28 -0500 (Mon, 17 Mar 2008) | 1 line
      
      Majority of items in QueuingRrdStrategy are now available in the QueuedMBean
    ........
      r8665 | mhuot | 2008-03-18 09:33:30 -0500 (Tue, 18 Mar 2008) | 1 line
      
      Smoothed out things a bit and added start time
    ........

--- opennms-services/src/main/java/org/opennms/netmgt/queued/jmx/Queued.java
+++ opennms-services/src/main/java/org/opennms/netmgt/queued/jmx/Queued.java
@@ -1,18 +1,135 @@
 package org.opennms.netmgt.queued.jmx;
 
 import org.opennms.netmgt.daemon.AbstractSpringContextJmxServiceDaemon;
+import org.opennms.netmgt.rrd.QueuingRrdStrategy;
+import org.opennms.netmgt.rrd.RrdUtils;
 
 public class Queued extends AbstractSpringContextJmxServiceDaemon implements
-        QueuedMBean {
+QueuedMBean {
 
-    @Override
-    protected String getLoggingPrefix() {
-        return "OpenNMS.Queued";
-    }
+	@Override
+	protected String getLoggingPrefix() {
+		return "OpenNMS.Queued";
+	}
 
-    @Override
-    protected String getSpringContext() {
-        return "queuedContext";
-    }
+	@Override
+	protected String getSpringContext() {
+		return "queuedContext";
+	}
 
+	private QueuingRrdStrategy getRrdStrategy() {
+		return (QueuingRrdStrategy) RrdUtils.getStrategy();
+	}
+
+
+	public boolean getStatsStatus() {
+		if (RrdUtils.getStrategy() instanceof QueuingRrdStrategy) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public long getElapsedTime() {
+		return System.currentTimeMillis() - getStartTime();
+	}
+
+
+	public long getCreatesCompleted() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getCreatesCompleted();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getTotalOperationsPending() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getTotalOperationsPending();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getErrors() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getErrors();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getUpdatesCompleted() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getUpdatesCompleted();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getPromotionCount() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getPromotionCount();
+		} else {
+			return 0;
+		}
+	}
+	
+	public long getDequeuedItems() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getDequeuedItems();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getDequeuedOperations() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getDequeuedOperations();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getEnqueuedOperations() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getEnqueuedOperations();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getSignificantOpsDequeued() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getSignificantOpsDequeued();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getSignificantOpsEnqueued() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getSignificantOpsEnqueued();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getSignificantOpsCompleted() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getSignificantOpsCompleted();
+		} else {
+			return 0;
+		}
+	}
+
+	public long getStartTime() {
+		if (getStatsStatus()) {
+			return getRrdStrategy().getStartTime();
+		} else {
+			return 0;
+		}
+	}
+	
+		
 }
