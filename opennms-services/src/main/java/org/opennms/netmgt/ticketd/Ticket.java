ed1700ed355b82b63ae3fa152ba376b5d7e93193
David Hustace <dhustace@users.sourceforge.net>
2008-03-31 17:15:20 +0000

    simple code clean up issues.

--- opennms-services/src/main/java/org/opennms/netmgt/ticketd/Ticket.java
+++ opennms-services/src/main/java/org/opennms/netmgt/ticketd/Ticket.java
@@ -10,6 +10,8 @@
  *
  * Modifications:
  *
+ * Created: May 10, 2007
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
