5631d3e6bb308522f260c2a57b0ac36b0be4c4b5
David Hustace <dhustace@users.sourceforge.net>
2008-03-31 19:20:30 +0000

    simple code clean up issues on test packages

--- opennms-services/src/test/java/org/opennms/spring/xmlrpc/XmlRpcTest.java
+++ opennms-services/src/test/java/org/opennms/spring/xmlrpc/XmlRpcTest.java
@@ -1,19 +1,38 @@
 /*
- * Copyright 2002-2005 the original author or authors.
+ * This file is part of the OpenNMS(R) Application.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * OpenNMS(R) is Copyright (C) 2006-2007 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Modifications:
+ * 
+ * Created: August 16, 2006
+ *
+ * Copyright (C) 2006-2007 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
  */
-
 package org.opennms.spring.xmlrpc;
 
 import java.io.IOException;
@@ -28,6 +47,9 @@ import org.opennms.test.mock.MockLogAppender;
 import org.springframework.remoting.RemoteAccessException;
 
 /**
+ * 
+ * @author <a href="mailto:brozow@opennms.org">Mathew Brozowski</a>
+ * @author <a href="mailto:dj@opennms.org">DJ Gregor</a>
  */
 public class XmlRpcTest extends TestCase {
     
