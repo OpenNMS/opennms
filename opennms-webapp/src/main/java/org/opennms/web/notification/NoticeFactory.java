b76ef84a4e0130af03e3b7800ef04abcecf15343
David Hustace <dhustace@users.sourceforge.net>
2008-04-01 12:29:37 +0000

    simple code clean up issues.

--- opennms-webapp/src/main/java/org/opennms/web/notification/NoticeFactory.java
+++ opennms-webapp/src/main/java/org/opennms/web/notification/NoticeFactory.java
@@ -41,7 +41,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.Vector;
