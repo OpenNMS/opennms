9f10d910fb820039b958bffd22ed5ddde9f459ae
Benjamin Reed <rangerrick@users.sourceforge.net>
2008-04-01 12:46:49 +0000

    Merged revisions 8880-8882 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/trunk
    ........
      r8880 | dhustace | 2008-03-31 23:10:13 -0400 (Mon, 31 Mar 2008) | 1 line
      
      simple code clean up issues.
    ........
      r8881 | dhustace | 2008-03-31 23:27:42 -0400 (Mon, 31 Mar 2008) | 1 line
      
      simple code clean up issues.
    ........
      r8882 | dhustace | 2008-04-01 08:29:37 -0400 (Tue, 01 Apr 2008) | 1 line
      
      simple code clean up issues.
    ........

--- opennms-webapp/src/main/java/org/opennms/web/map/view/VElementInfo.java
+++ opennms-webapp/src/main/java/org/opennms/web/map/view/VElementInfo.java
@@ -1,13 +1,41 @@
 /*
- * Created on 9-dic-2004
+ * This file is part of the OpenNMS(R) Application.
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * OpenNMS(R) is Copyright (C) 2007 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ * 
+ * Created: Juy 6, 2007
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
  */
 package org.opennms.web.map.view;
 
 /**
- * @author
+ * @author <a href="mailto:antonio@opennms.it">Antonio Russo</a>
  * 
  * TODO To change the template for this generated type comment go to Window -
  * Preferences - Java - Code Style - Code Templates
