88184cabb5f980a55ea71a4dd2323ebd65d360b3
David Hustace <dhustace@users.sourceforge.net>
2008-04-01 03:27:42 +0000

    simple code clean up issues.

--- opennms-webapp/src/main/java/org/opennms/web/rss/NotificationFeed.java
+++ opennms-webapp/src/main/java/org/opennms/web/rss/NotificationFeed.java
@@ -1,3 +1,37 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2007 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ * 
+ * Created: September 16, 2007
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
 package org.opennms.web.rss;
 
 import java.sql.SQLException;
@@ -11,6 +45,10 @@ import com.sun.syndication.feed.synd.SyndEntryImpl;
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.feed.synd.SyndFeedImpl;
 
+/**
+ * 
+ * @author <a href="mailto:ranger@opennms.org">Benjamin Reed</a>
+ */
 public class NotificationFeed extends AbstractFeed {
 
     public SyndFeed getFeed() {
