c8be7645e8533ded9fb592a1da3cadcd4881a682
DJ Gregor <djgregor@users.sourceforge.net>
2008-05-10 17:08:38 +0000

    Merged revisions 9086 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ........
      r9086 | rangerrick | 2008-05-09 11:48:08 -0400 (Fri, 09 May 2008) | 1 line
      
      fix for bug #2318, log4j initialization in jetty
    ........

---
+++ opennms-webapp/src/main/java/org/opennms/web/Log4jConfigListener.java
@@ -0,0 +1,21 @@
+package org.opennms.web;
+
+import javax.servlet.ServletContextEvent;
+import org.springframework.web.util.Log4jWebConfigurer;
+
+public class Log4jConfigListener implements javax.servlet.ServletContextListener {
+
+    public void contextDestroyed(ServletContextEvent event) {
+        if (! event.getServletContext().getServerInfo().toLowerCase().contains("jetty")) {
+            Log4jWebConfigurer.initLogging(event.getServletContext());
+        }
+    }
+
+    public void contextInitialized(ServletContextEvent event) {
+        if (! event.getServletContext().getServerInfo().toLowerCase().contains("jetty")) {
+            Log4jWebConfigurer.shutdownLogging(event.getServletContext());
+        }
+    }
+
+    
+}
