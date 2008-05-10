0bf7f426710795136ba29ce60dce326900aeaa0f
DJ Gregor <djgregor@users.sourceforge.net>
2008-05-10 09:18:30 +0000

    Add support for sending events on failed login attemps, user-initiated logouts, and session timeouts.

--- opennms-webapp/src/main/java/org/opennms/web/acegisecurity/AcegiAuthenticationEventOnmsEventBuilder.java
+++ opennms-webapp/src/main/java/org/opennms/web/acegisecurity/AcegiAuthenticationEventOnmsEventBuilder.java
@@ -8,6 +8,10 @@
 //
 // OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
 //
+// Modifications:
+//
+// 2008 May 10: Add support for failed events. - dj@opennms.org
+//
 // Copyright (C) 2008 Daniel J. Gregor, Jr..  All Rights Reserved.
 //
 // This program is free software; you can redistribute it and/or modify
@@ -34,6 +38,8 @@ package org.opennms.web.acegisecurity;
 import java.util.Date;
 
 import org.acegisecurity.Authentication;
+import org.acegisecurity.event.authentication.AbstractAuthenticationEvent;
+import org.acegisecurity.event.authentication.AbstractAuthenticationFailureEvent;
 import org.acegisecurity.event.authentication.AuthenticationSuccessEvent;
 import org.acegisecurity.ui.WebAuthenticationDetails;
 import org.apache.log4j.Category;
@@ -49,6 +55,7 @@ import org.springframework.util.Assert;
 
 public class AcegiAuthenticationEventOnmsEventBuilder implements ApplicationListener, InitializingBean {
     public static final String SUCCESS_UEI = "uei.opennms.org/internal/authentication/successfulLogin";
+    public static final String FAILURE_UEI = "uei.opennms.org/internal/authentication/failure";
 
     private EventProxy m_eventProxy;
     
@@ -56,18 +63,34 @@ public class AcegiAuthenticationEventOnmsEventBuilder implements ApplicationList
         if (event instanceof AuthenticationSuccessEvent) {
             AuthenticationSuccessEvent authEvent = (AuthenticationSuccessEvent) event;
 
-            EventBuilder builder = new EventBuilder(SUCCESS_UEI, "OpenNMS.WebUI");
-            builder.setTime(new Date(authEvent.getTimestamp()));
-            Authentication auth = authEvent.getAuthentication();
-            builder.addParam("user", auth.getName());
-            if (auth.getDetails() instanceof WebAuthenticationDetails) {
-                WebAuthenticationDetails webDetails = (WebAuthenticationDetails) auth.getDetails();
-                if (webDetails.getRemoteAddress() != null) {
-                    builder.addParam("ip", webDetails.getRemoteAddress());
-                }
-            }
+            EventBuilder builder = createEvent(SUCCESS_UEI, authEvent);
             sendEvent(builder.getEvent());
         }
+        
+        if (event instanceof AbstractAuthenticationFailureEvent) {
+            AbstractAuthenticationFailureEvent authEvent = (AbstractAuthenticationFailureEvent) event;
+
+            EventBuilder builder = createEvent(FAILURE_UEI, authEvent);
+            builder.addParam("exceptionName", authEvent.getException().getClass().getSimpleName());
+            builder.addParam("exceptionMessage", authEvent.getException().getMessage());
+            sendEvent(builder.getEvent());
+        }
+    }
+
+    private EventBuilder createEvent(String uei, AbstractAuthenticationEvent authEvent) {
+        EventBuilder builder = new EventBuilder(uei, "OpenNMS.WebUI");
+        builder.setTime(new Date(authEvent.getTimestamp()));
+        Authentication auth = authEvent.getAuthentication();
+        if (auth != null && auth.getName() != null) {
+            builder.addParam("user", auth.getName());
+        }
+        if (auth != null && auth.getDetails() != null && auth.getDetails() instanceof WebAuthenticationDetails) {
+            WebAuthenticationDetails webDetails = (WebAuthenticationDetails) auth.getDetails();
+            if (webDetails.getRemoteAddress() != null) {
+                builder.addParam("ip", webDetails.getRemoteAddress());
+            }
+        }
+        return builder;
     }
     
     private void sendEvent(Event onmsEvent) {
