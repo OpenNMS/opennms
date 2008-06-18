6a371de4f923f9687c80e39c24f9d65003718279
Matt Brozowski <brozow@users.sourceforge.net>
2008-06-18 15:59:08 +0000

    more tests that fail when the server is busy.. adjusted the timing

--- opennms-services/src/test/java/org/opennms/netmgt/vacuumd/VacuumdTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/vacuumd/VacuumdTest.java
@@ -310,10 +310,10 @@ public class VacuumdTest extends OpenNMSTestCase {
         // create node down events with severity 6
         bringNodeDownCreatingEvent(1);
         bringNodeDownCreatingEvent(2);
-        Thread.sleep(500);
+        Thread.sleep(1000);
         // create node up event with severity 3
         bringNodeUpCreatingEvent(1);
-        Thread.sleep(500);
+        Thread.sleep(1000);
         
         assertEquals("clearUei for nodeUp", "uei.opennms.org/nodes/nodeDown", getJdbcTemplate().queryForObject("select clearUei from alarms where eventUei = ?", String.class, "uei.opennms.org/nodes/nodeUp"));
         
