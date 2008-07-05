6c04ce50ce2b0b6e2bb7a816a9a012d696c77886
DJ Gregor <djgregor@users.sourceforge.net>
2008-07-05 16:54:33 +0000

    Fix all broken tests (bug #1607).

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/OutageDaoTest.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/OutageDaoTest.java
@@ -10,6 +10,7 @@
 //
 // Modifications:
 //
+// 2008 Jul 05: Fix all broken tests (bug #1607). - dj@opennms.org
 // 2008 Mar 25: Convert to use AbstractTransactionalDaoTestCase. - dj@opennms.org
 // 2007 Jul 03: Eliminate a warning. - dj@opennms.org
 //
@@ -41,6 +42,8 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 
+import org.opennms.netmgt.filter.FilterDaoFactory;
+import org.opennms.netmgt.model.OnmsDistPoller;
 import org.opennms.netmgt.model.OnmsEvent;
 import org.opennms.netmgt.model.OnmsIpInterface;
 import org.opennms.netmgt.model.OnmsMonitoredService;
@@ -54,21 +57,39 @@ import org.opennms.netmgt.model.ServiceSelector;
  *
  */
 public class OutageDaoTest extends AbstractTransactionalDaoTestCase {
-    public void testBogus() {
-        // empty test so that JUnit doesn't complain
+    @Override
+    protected void onSetUpBeforeTransaction() throws Exception {
+        super.onSetUpBeforeTransaction();
     }
     
-    public void FIXMEtestSave() {
-        OnmsOutage outage = new OnmsOutage();
-        outage.setServiceLostEvent(new OnmsEvent());
-        outage.setIfLostService(new Date());
-        OnmsNode node = new OnmsNode(getDistPollerDao().load("localhost"));
+    @Override
+    protected void onSetUpInTransactionIfEnabled() {
+        super.onSetUpInTransactionIfEnabled();
+        
+        // Ensure that we get a new JdbcFilterDao every time since our DataSource changes
+        FilterDaoFactory.setInstance(null);
+        FilterDaoFactory.getInstance();
+    }
+    
+    public void testSave() {
+        OnmsNode node = new OnmsNode(getLocalHostDistPoller());
+        getNodeDao().save(node);
+
         OnmsIpInterface ipInterface = new OnmsIpInterface("172.16.1.1", node);
+
         OnmsServiceType serviceType = getServiceTypeDao().findByName("ICMP");
         assertNotNull(serviceType);
-        outage.setMonitoredService(new OnmsMonitoredService(ipInterface, serviceType));
-        getNodeDao().save(node);
+
+        OnmsMonitoredService monitoredService = new OnmsMonitoredService(ipInterface, serviceType);
+
+        OnmsEvent event = new OnmsEvent();
+
+        OnmsOutage outage = new OnmsOutage();
+        outage.setServiceLostEvent(event);
+        outage.setIfLostService(new Date());
+        outage.setMonitoredService(monitoredService);
         getOutageDao().save(outage);
+
         //it works we're so smart! hehe
         outage = getOutageDao().load(outage.getId());
         assertEquals("ICMP", outage.getMonitoredService().getServiceType().getName());
@@ -76,17 +97,78 @@ public class OutageDaoTest extends AbstractTransactionalDaoTestCase {
         
     }
     
-    public void FIXMEtestGetMatchingOutages() {
-    	String[] svcs = new String[] { "SNMP" };
-    	ServiceSelector selector = new ServiceSelector("ipAddr IPLIKE 192.168.1.1", Arrays.asList(svcs));
+    public void testGetMatchingOutages() {
+        insertEntitiesAndOutage();
+        
+        /*
+         * We need to flush and finish the transaction because JdbcFilterDao
+         * gets its own connection from the DataSource and won't see our data
+         * otherwise.
+         */
+        flushOutageDaoAndStartNewTransaction();
+        
+        String[] svcs = new String[] { "ICMP" };
+        ServiceSelector selector = new ServiceSelector("ipAddr IPLIKE 172.16.1.1", Arrays.asList(svcs));
     	Collection<OnmsOutage> outages = getOutageDao().matchingCurrentOutages(selector);
-    	assertEquals(1, outages.size());
+    	assertEquals("outage count", 1, outages.size());
     }
     
-    public void FIXMEtestGetMatchingOutagesWithEmptyServiceList() {
-    	ServiceSelector selector = new ServiceSelector("ipAddr IPLIKE 192.168.1.1", new ArrayList<String>(0));
+    public void testGetMatchingOutagesWithEmptyServiceList() {
+        insertEntitiesAndOutage();
+        
+        /*
+         * We need to flush and finish the transaction because JdbcFilterDao
+         * gets its own connection from the DataSource and won't see our data
+         * otherwise.
+         */
+        flushOutageDaoAndStartNewTransaction();
+
+        ServiceSelector selector = new ServiceSelector("ipAddr IPLIKE 172.16.1.1", new ArrayList<String>(0));
     	Collection<OnmsOutage> outages = getOutageDao().matchingCurrentOutages(selector);
     	assertEquals(1, outages.size());
     }
 
+    private OnmsDistPoller getLocalHostDistPoller() {
+        return getDistPollerDao().load("localhost");
+    }
+    
+    private OnmsOutage insertEntitiesAndOutage() {
+        OnmsNode node = new OnmsNode(getLocalHostDistPoller());
+        getNodeDao().save(node);
+
+        OnmsIpInterface ipInterface = new OnmsIpInterface("172.16.1.1", node);
+        getIpInterfaceDao().save(ipInterface);
+
+        OnmsServiceType serviceType = getServiceTypeDao().findByName("ICMP");
+        assertNotNull(serviceType);
+        
+        OnmsMonitoredService monitoredService = new OnmsMonitoredService(ipInterface, serviceType);
+        getMonitoredServiceDao().save(monitoredService);
+        
+        OnmsEvent event = new OnmsEvent();
+        event.setDistPoller(getLocalHostDistPoller());
+        event.setEventUei("foo!");
+        event.setEventTime(new Date());
+        event.setEventCreateTime(new Date());
+        event.setEventSeverity(1);
+        event.setEventSource("your mom");
+        event.setEventLog("Y");
+        event.setEventDisplay("Y");
+        getEventDao().save(event);
+
+        OnmsOutage outage = new OnmsOutage();
+        outage.setMonitoredService(monitoredService);
+        outage.setServiceLostEvent(event);
+        outage.setIfLostService(new Date());
+        getOutageDao().save(outage);
+        
+        return outage;
+    }
+
+    private void flushOutageDaoAndStartNewTransaction() {
+        getOutageDao().flush();
+        setComplete();
+        endTransaction();
+        startNewTransaction();
+    }
 }
