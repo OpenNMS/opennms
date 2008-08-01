1c0550b4cc9dbfef033042e4e0abf37c43740a5a
Alejandro Galue <agalue@users.sourceforge.net>
2008-08-01 23:30:56 +0000

    Adding test for collections

--- opennms-webapp/src/test/java/org/opennms/web/rest/NodeRestServiceTest.java
+++ opennms-webapp/src/test/java/org/opennms/web/rest/NodeRestServiceTest.java
@@ -12,23 +12,30 @@ import org.springframework.mock.web.MockHttpServletResponse;
 public class NodeRestServiceTest extends AbstractSpringJerseyRestTestCase {
     
     public void testNode() throws Exception {
+        // Testing POST
         createNode();
-        String url = "/nodes/1";
+        String url = "/nodes";
+        // Testing GET Collection
         String xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("Darwin TestMachine 9.4.0 Darwin Kernel Version 9.4.0"));
+        url += "/1";
+        // Testing PUT
         sendPut(url, "sysContact=OpenNMS&assetRecord.manufacturer=Apple&assetRecord.operatingSystem=MacOSX Leopard");
+        // Testing GET Single Object
         xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("<sysContact>OpenNMS</sysContact>"));        
         assertTrue(xml.contains("<operatingSystem>MacOSX Leopard</operatingSystem>"));        
+        // Testing DELETE
         sendRequest(DELETE, url, 200);
         sendRequest(GET, url, 204);
     }
 
     public void testIpInterface() throws Exception {
         createIpInterface();
-        String url = "/nodes/1/ipinterfaces/10.10.10.10";
+        String url = "/nodes/1/ipinterfaces";
         String xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("<ipAddress>10.10.10.10</ipAddress>"));
+        url += "/10.10.10.10";
         sendPut(url, "ipStatus=0");
         xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("<ipStatus>0</ipStatus>"));
@@ -38,9 +45,10 @@ public class NodeRestServiceTest extends AbstractSpringJerseyRestTestCase {
 
     public void testSnmpInterface() throws Exception {
         createSnmpInterface();
-        String url = "/nodes/1/snmpinterfaces/6";
+        String url = "/nodes/1/snmpinterfaces";
         String xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("<ifIndex>6</ifIndex>"));
+        url += "/6";
         sendPut(url, "ifName=eth0");
         xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("<ifName>eth0</ifName>"));
@@ -50,9 +58,10 @@ public class NodeRestServiceTest extends AbstractSpringJerseyRestTestCase {
 
     public void testMonitoredService() throws Exception {
         createService();
-        String url = "/nodes/1/ipinterfaces/10.10.10.10/services/ICMP";
+        String url = "/nodes/1/ipinterfaces/10.10.10.10/services";
         String xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("<name>ICMP</name>"));
+        url += "/ICMP";
         sendPut(url, "status=A");
         xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("<status>A</status>"));
@@ -62,9 +71,10 @@ public class NodeRestServiceTest extends AbstractSpringJerseyRestTestCase {
     
     public void testCategory() throws Exception {
         createCategory();
-        String url = "/nodes/1/categories/Routers";
+        String url = "/nodes/1/categories";
         String xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("<name>Routers</name>"));
+        url += "/Routers";
         sendPut(url, "description=My Equipment");
         xml = sendRequest(GET, url, 200);
         assertTrue(xml.contains("<description>My Equipment</description>"));
