7513e47510078b0a9a25bd8ae9befdbd0767a865
Alexander Finger <af48@users.sourceforge.net>
2008-08-15 20:38:36 +0000

    Moved the client to use the RESTful interface.

--- opennms-tools/nmsdesk-air/js/OpenNMS.js
+++ opennms-tools/nmsdesk-air/js/OpenNMS.js
@@ -39,59 +39,209 @@
 			}
 	}
 
-	function rssoutage(){
+	function outages() {
 		loadproperties();
-		var request=new air.URLRequest("http://demo.opennms.org/opennms/rss.jsp?feed=outage");
-		var loader=new air.URLLoader();
-		loader.addEventListener(air.Event.COMPLETE, outagesCompleteHandler);
+		var request = new air.URLRequest(urlbase+"/outages");
+		var loader = new air.URLLoader();
+		loader.addEventListener(air.Event.COMPLETE, restoutagesCompleteHandler);
 		loader.load(request);
+		
 	}
+	
+	function alarms() {
+		loadproperties();
+		var request = new air.URLRequest(urlbase+"/alarms");
+		var loader = new air.URLLoader();
+		loader.addEventListener(air.Event.COMPLETE, alarmsCompleteHandler);
+		loader.load(request);
+		
+	}	
+	
+	function notifications() {
+		loadproperties();
+		var request = new air.URLRequest(urlbase+"/notifications");
+		var loader = new air.URLLoader();
+		loader.addEventListener(air.Event.COMPLETE, notificationsCompleteHandler);
+		loader.load(request);
+		
+	}	
+	
+/* Notifications as they come out:
+<notifications count="1">
+<onmsNotification>
+<eventUei>uei.opennms.org/nodes/nodeDown</eventUei>
+<notifConfigName>nodeDown</notifConfigName>
+<notifyId>1</notifyId>
+<numericMsg>111-1</numericMsg>
+<pageTime>2008-08-15T22:11:04.810+02:00</pageTime>
+<queueId>default</queueId>
+<subject>Notice #1: node 192.168.1.150 down.</subject>
+<textMsg>
+All services are down on node 192.168.1.150.  New Outage records have 
+been created and service level availability calculations will 
+be impacted until this outage is resolved.  
+</textMsg>
+</onmsNotification>
+</notifications>
+ */	
 
-	function outagesCompleteHandler(event)
-	{ 
-		mydataobj=document.getElementById("mydata");
+function notificationsCompleteHandler(event){
 		var loader2 = event.target;
-        	air.trace("loaded" + loader2.data);
-		var parser=new DOMParser();
-		var xmldoc=parser.parseFromString(loader2.data,"text/xml");
+		air.trace("loaded" + loader2.data);
+		var parser = new DOMParser();
+		var xmldoc = parser.parseFromString(loader2.data, "text/xml");
 		air.trace("calling parser" + xmldoc);
+		
+		// <id>3</id>
+		// <ifLostService>2008-03-22T15:03:09+01:00</ifLostService>
+		// <ifRegainedService>2008-03-22T15:03:40+01:00</ifRegainedService>
 
-// 
-		var entries=xmldoc.getElementsByTagName("entry");
+		var entries = xmldoc.getElementsByTagName("onmsNotification");
 		air.trace("found " + entries.length + " entries.");
 		var position = document.getElementById("results");
 		var oldtable = document.getElementById("resultstable");
 		oldtable.parentNode.removeChild(oldtable);
-		
-		
-	
-			
-			
-
 		var table = document.createElement("table");
-		table.setAttribute("id","resultstable");
+		table.setAttribute("id", "resultstable");
 		position.appendChild(table);
+		
+		for (Index = 0; Index < entries.length; Index++) {
+			var id = entries[Index].getElementsByTagName("notifyId")[0].textContent;
+			var eventUei = entries[Index].getElementsByTagName("eventUei")[0].textContent;
+			var textMsg = entries[Index].getElementsByTagName("textMsg")[0].textContent;
+			var subject = entries[Index].getElementsByTagName("subject")[0].textContent;
 
-		for(Index = 0; Index < entries.length; Index++) {
-			var title=entries[Index].getElementsByTagName("title")[0].textContent;
-			var published=entries[Index].getElementsByTagName("published")[0].textContent;
-			var link = entries[Index].getElementsByTagName("link")[0].getAttribute("href");
+			RegainedServiceElement = entries[Index].getElementsByTagName("ifRegainedService")[0];
+			if (RegainedServiceElement) {
+				RegainedService = RegainedServiceElement.textContent;
+			}
+			else {
+				RegainedService = "never";
+			}
 			//var myline = "" + title + " " + published + "\n";
 			//mydataobj.appendChild(document.createTextNode(myline));
-			writeFileResult(title,published,link);
+			writeOutageResult(table, id, subject, eventUei);
+			
+		}
+	}
+	
+function restoutagesCompleteHandler(event){
+		var loader2 = event.target;
+		air.trace("loaded" + loader2.data);
+		var parser = new DOMParser();
+		var xmldoc = parser.parseFromString(loader2.data, "text/xml");
+		air.trace("calling parser" + xmldoc);
+		
+		// <id>3</id>
+		// <ifLostService>2008-03-22T15:03:09+01:00</ifLostService>
+		// <ifRegainedService>2008-03-22T15:03:40+01:00</ifRegainedService>
 
+		var entries = xmldoc.getElementsByTagName("onmsOutage");
+		air.trace("found " + entries.length + " entries.");
+		var position = document.getElementById("results");
+		var oldtable = document.getElementById("resultstable");
+		oldtable.parentNode.removeChild(oldtable);
+		var table = document.createElement("table");
+		table.setAttribute("id", "resultstable");
+		position.appendChild(table);
+		
+		for (Index = 0; Index < entries.length; Index++) {
+			var id = entries[Index].getElementsByTagName("id")[0].textContent;
+			var LostService = entries[Index].getElementsByTagName("ifLostService")[0].textContent;
+			RegainedServiceElement = entries[Index].getElementsByTagName("ifRegainedService")[0];
+			if (RegainedServiceElement) {
+				RegainedService = RegainedServiceElement.textContent;
 			}
+			else {
+				RegainedService = "never";
+			}
+			//var myline = "" + title + " " + published + "\n";
+			//mydataobj.appendChild(document.createTextNode(myline));
+			writeOutageResult(table, id, LostService, RegainedService);
+			
+		}
+	}
 
+/* Alarm Definition
+<alarmType>1</alarmType>
+<counter>1</counter>
++
+<description>
+ <p>All interfaces on node 192.168.1.140 are down.</p> <p>This event is generated when node outage processing determines that all interfaces on the node are down.</p> <p>New outage records have been created and service level availability calculations will be impacted until this outage is resolved.</p>
+</description>
+<firstEventTime>2008-08-15T16:20:45+02:00</firstEventTime>
+<id>7</id>
+<lastEventTime>2008-08-15T16:20:45+02:00</lastEventTime>
+<logMsg>Node 192.168.1.140 is down. </logMsg>
+<reductionKey>uei.opennms.org/nodes/nodeDown::4</reductionKey>
+<severity>6</severity>
+<suppressedTime>2008-08-15T16:20:45+02:00</suppressedTime>
+<suppressedUntil>2008-08-15T16:20:45+02:00</suppressedUntil>
+<uei>uei.opennms.org/nodes/nodeDown</uei>
+<x733ProbableCause>0</x733ProbableCause> 
+*/
 
-		//var date = items[0].getElementsByTagName("published");
-		air.trace("finished the rss magic");
+	function alarmsCompleteHandler(event){
+		var loader2 = event.target;
+		air.trace("loaded" + loader2.data);
+		var parser = new DOMParser();
+		var xmldoc = parser.parseFromString(loader2.data, "text/xml");
+		air.trace("calling parser" + xmldoc);
+		
+	
 
-//
+		var entries = xmldoc.getElementsByTagName("onmsAlarm");
+		air.trace("found " + entries.length + " entries.");
+		var position = document.getElementById("results");
+		var oldtable = document.getElementById("resultstable");
+		oldtable.parentNode.removeChild(oldtable);
+		var table = document.createElement("table");
+		table.setAttribute("id", "resultstable");
+		position.appendChild(table);
+		
+		for (Index = 0; Index < entries.length; Index++) {
+			var id = entries[Index].getElementsByTagName("id")[0].textContent;
+			var description = entries[Index].getElementsByTagName("description")[0].textContent;
+			var logMsg = entries[Index].getElementsByTagName("logMsg")[0].textContent;
 
-		// output
-		//mydataobj.appendChild(document.createTextNode(titles[1].textContent)); //loader2.data
+			RegainedServiceElement = entries[Index].getElementsByTagName("ifRegainedService")[0];
 
-		function writeFileResult(title, published, link) 
+			if (RegainedServiceElement) {
+				RegainedService = RegainedServiceElement.textContent;
+			}
+			else {
+				RegainedService = "never";
+			}
+			//var myline = "" + title + " " + published + "\n";
+			//mydataobj.appendChild(document.createTextNode(myline));
+			writeOutageResult(table, id, logMsg, RegainedService);
+			
+		}
+	}
+
+		function writeOutageResult(table, id, LostService, RegainedService) 
+			{
+		
+    			var tr = document.createElement("tr");
+    			tr.setAttribute("class", "grid");
+			
+    			var td = document.createElement("td");
+				td.innerHTML= id;
+    			tr.appendChild(td);
+			
+    			td = document.createElement("td");
+				td.innerHTML= LostService;
+    			tr.appendChild(td);
+			
+				td = document.createElement("td");
+				td.innerHTML= RegainedService;
+    			tr.appendChild(td);
+			
+    			table.appendChild(tr); 
+			}
+		
+			
+		function writeFileResult(table, title, published, link) 
 			{
 		
     			var tr = document.createElement("tr");
@@ -108,5 +258,6 @@
 			
     			table.appendChild(tr); 
 			}
-		} 
+		 
+		 
 
