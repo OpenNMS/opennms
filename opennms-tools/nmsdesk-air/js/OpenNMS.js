3db94cfaff08d8b7ce4eb371b515bfa1564caf90
Alexander Finger <af48@users.sourceforge.net>
2008-08-15 13:25:25 +0000

    Added an alert if no prefs file exists and the stub to read it.

--- opennms-tools/nmsdesk-air/js/OpenNMS.js
+++ opennms-tools/nmsdesk-air/js/OpenNMS.js
@@ -1,19 +1,46 @@
 	// https://m.sceur.ch/opennms/rss.jsp?feed=notification
-
+	var hostname;
+	var port;
+	var protocol;
+	var username;
+	var password;
+	var urlbase;
+	
 	function loadproperties() {
-		<!--
-		var prefsfile = air.File.applicationStorageDirectory;
+		var prefsFile;
+		var prefsXML; // The XML data
+		var stream; // The FileStream object used to read and write prefsFile data.
+		
+		prefsFile = air.File.applicationStorageDirectory;
 		prefsFile = prefsFile.resolvePath("preferences.xml");
-		readXML();
+	
 		stream = new air.FileStream();
-		if (prefsFile.exists) {
-    		stream.open(prefsFile, air.FileMode.READ);
-    		processXMLData();
+			if (prefsFile.exists) {
+				stream.open(prefsFile, air.FileMode.READ);
+				//processXMLData();
+				
+				prefsXML = stream.readUTFBytes(stream.bytesAvailable);
+				stream.close();
+				var domParser = new DOMParser();
+				prefsXML = domParser.parseFromString(prefsXML, "text/xml");
+				var nmsprefs = prefsXML.getElementsByTagName("nmsdesk")[0];
+				hostname = nmsprefs.getElementsByTagName("hostname")[0].textContent;
+				protocol = nmsprefs.getElementsByTagName("protocol")[0].textContent;
+				port = nmsprefs.getElementsByTagName("port")[0].textContent;
+				username = nmsprefs.getElementsByTagName("username")[0].textContent;
+				password = nmsprefs.getElementsByTagName("password")[0].textContent;
+				path = nmsprefs.getElementsByTagName("path")[0].textContent;
+				urlbase = protocol + "://" + hostname + ":" + port + path;
+				air.trace("Host " + urlbase );
+
 			}
-		-->
+			else {
+				alert("preferences.xml not found at " + air.File.applicationStorageDirectory.nativePath + "; using demo.opennms.org")
+			}
 	}
 
-	function init(){
+	function rssoutage(){
+		loadproperties();
 		var request=new air.URLRequest("http://demo.opennms.org/opennms/rss.jsp?feed=outage");
 		var loader=new air.URLLoader();
 		loader.addEventListener(air.Event.COMPLETE, outagesCompleteHandler);
