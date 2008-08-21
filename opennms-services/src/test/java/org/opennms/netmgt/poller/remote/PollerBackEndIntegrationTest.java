f50f84c92ca1fd297a989f6f780755fcb880ca04
Benjamin Reed <rangerrick@users.sourceforge.net>
2008-08-21 02:58:54 +0000

    put things back the way they were, but this time use the mockEventIpcManager.xml in the test stuff

--- opennms-services/src/test/java/org/opennms/netmgt/poller/remote/PollerBackEndIntegrationTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/poller/remote/PollerBackEndIntegrationTest.java
@@ -63,6 +63,7 @@ public class PollerBackEndIntegrationTest extends AbstractTransactionalTemporary
     @Override
     protected String[] getConfigLocations() {
         return new String[] {
+                "classpath:/META-INF/opennms/mockEventIpcManager.xml",
                 "classpath:/META-INF/opennms/applicationContext-dao.xml",
                 "classpath:/META-INF/opennms/applicationContext-daemon.xml",
                 "classpath:/META-INF/opennms/applicationContext-pollerBackEnd.xml",
