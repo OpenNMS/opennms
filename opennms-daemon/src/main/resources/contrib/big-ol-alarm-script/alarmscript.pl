31d7f1ab1b76f6dd1888ce3a3e7150066fccffd8
Benjamin Reed <rangerrick@users.sourceforge.net>
2008-09-04 21:04:34 +0000

    Merged revisions 10336,10339,10350-10351 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ........
      r10336 | rangerrick | 2008-09-03 12:31:33 -0400 (Wed, 03 Sep 2008) | 1 line
      
      sortova's changes, also make it so warnings and criticals still print
    ........
      r10339 | rangerrick | 2008-09-03 16:59:56 -0400 (Wed, 03 Sep 2008) | 1 line
      
      update to latest snmp4j
    ........
      r10350 | brozow | 2008-09-04 12:59:58 -0400 (Thu, 04 Sep 2008) | 1 line
      
      fix tests that assumed order incorrectly
    ........
      r10351 | brozow | 2008-09-04 15:51:27 -0400 (Thu, 04 Sep 2008) | 1 line
      
      treat an Unknown status returned from a Monitor as a 'noop'
    ........

--- opennms-daemon/src/main/resources/contrib/big-ol-alarm-script/alarmscript.pl
+++ opennms-daemon/src/main/resources/contrib/big-ol-alarm-script/alarmscript.pl
@@ -11,42 +11,72 @@ my $outputfile = shift(@ARGV) || 'index.html';
 
 my $dbh = DBI->connect('dbi:Pg:dbname=opennms', $username, $password) or die "unable to connect to database: " . DBI->errstr;
 
-my $critical = $dbh->selectall_arrayref('SELECT node.nodelabel FROM node, alarms WHERE severity > 3 AND node.nodeid=alarms.nodeid AND alarms.serviceid = 1')  or die "unable to select nodes (critical): " . $dbh->errstr;
-my $warning  = $dbh->selectall_arrayref('SELECT node.nodelabel FROM node, alarms WHERE severity > 3 AND node.nodeid=alarms.nodeid AND alarms.serviceid <> 1') or die "unable to select nodes (warning): " . $dbh->errstr;
+my $unix_critical = $dbh->selectall_arrayref('SELECT DISTINCT node.nodelabel FROM node, alarms WHERE severity > 3 AND node.nodeid=alarms.nodeid AND alarms.eventuei like \'%nodeDown%\' and node.nodeid in (select distinct nodeid from category_node where categoryid in (select categoryid from categories where categoryname=\'unix\' or categoryname=\'network\' or categoryname=\'storage\'))')  or die "unable to select nodes (critical): " . $dbh->errstr;
+
+my $unix_warning  = $dbh->selectall_arrayref('SELECT DISTINCT node.nodelabel FROM node, alarms WHERE severity > 3 AND node.nodeid=alarms.nodeid AND alarms.serviceid <> 1 and node.nodeid in (select distinct nodeid from category_node where categoryid in (select categoryid from categories where categoryname=\'unix\' or categoryname=\'network\' or categoryname=\'storage\'))') or die "unable to select nodes (warning): " . $dbh->errstr;
+
+my $nonunix_critical = $dbh->selectall_arrayref('SELECT DISTINCT node.nodelabel FROM node, alarms WHERE severity > 3 AND node.nodeid=alarms.nodeid AND alarms.eventuei like \'%nodeDown%\' and node.nodeid in (select distinct nodeid from category_node where categoryid in (select categoryid from categories where categoryname=\'ntservers\' or categoryname=\'netware\' or categoryname=\'storage\'))')  or die "unable to select nodes (critical): " . $dbh->errstr;
+
+my $nonunix_warning  = $dbh->selectall_arrayref('SELECT DISTINCT node.nodelabel FROM node, alarms WHERE severity > 3 AND node.nodeid=alarms.nodeid AND alarms.serviceid <> 1 and node.nodeid in (select distinct nodeid from category_node where categoryid in (select categoryid from categories where categoryname=\'ntservers\' or categoryname=\'netware\'))') or die "unable to select nodes (warning): " . $dbh->errstr;
+
+my $localt = localtime( );
 
 open (FILEOUT, '>' . $outputfile . '.tmp') or die "unable to write to ${outputfile}.tmp: $!";
 print FILEOUT <<END;
 <html>
  <head>
   <title>Alarms Dashboard</title>
-  <meta http-equiv="Refresh" content="120" />
+  <meta http-equiv="Refresh" content="60" />
  </head>
- <body bgcolor="black">
+ <body bgcolor="white" style="font-family:lucida grande,verdana,sans-serif">
   <table width="100%" height="100%">
    <tr>
-    <td valign="middle" align="center">
+    <td valign="top">
      <table>
       <tr>
        <td>
-        <h1><font color="white">List of Critical Nodes</font></h1>
+        <h1 style="color:darkblue;font-size:250%;margin:0px 0px">OpenNMS Problem Summaries</h1>
+        <p style="color:darkblue;font-size:150%;margin:0px 0px">
+          $localt
+        </p>
+        <hr style="color:darkblue" />
+        <h2 style="color:black;font-size:250%">UNIX, Network and Storage Devices</h2>
 END
 
-if (@$critical) {
-	for my $row (@$critical) {
-		print FILEOUT "         <font size=\"5\" color=\"red\">$row->[0]</font><br />\n";
+if (@$unix_critical) {
+	for my $row (@$unix_critical) {
+		print FILEOUT "         <p style=\"color:red;font-size:250%;margin:0px 0px\">$row->[0]</p>\n";
 	}
-} else {
-	print FILEOUT "        <font color=\"white\">(none)</font>\n";
+}
+if (@$unix_warning) {
+	for my $row (@$unix_warning) {
+		print FILEOUT "         <p style=\"color:orange;font-size:250%;margin:0px 0px\">$row->[0]</p>\n";
+	}
+}
+if (not @$unix_critical and not @$unix_warning) {
+	print FILEOUT "        <p style=\"color:green;font-size:250%\">No UNIX Problems Reported</font>\n";
 }
 
-print FILEOUT "        <h1><font color=\"white\">List of Warning Nodes</font></h1>\n";
+print FILEOUT <<END;
+        <h2 style="color:black;font-size:300%;font-size:250%">Non-UNIX Devices</h2>
+       </td>
+      </tr>
+      <tr>
+       <td>
+END
 
-if (@$warning) {
-	for my $row (@$warning) {
-		print FILEOUT "         <font size=\"5\" color=\"yellow\">$row->[0]</font><br />\n";
+if (@$nonunix_critical) {
+	for my $row (@$nonunix_critical) {
+		print FILEOUT "         <p style=\"color:red;font-size:250%;margin:0px 0px\">$row->[0]</p>\n";
 	}
-} else {
-	print FILEOUT "        <font color=\"white\">(none)</font>\n";
+}
+if (@$nonunix_warning) {
+	for my $row (@$nonunix_warning) {
+		print FILEOUT "         <p style=\"color:orange;font-size:250%;margin:0px 0px\">$row->[0]</p>\n";
+	}
+}
+if (not @$nonunix_critical and not @$nonunix_warning) {
+	print FILEOUT "        <p style=\"color:green;font-size:250%\">No Non-UNIX Problems Reported</font>\n";
 }
 
 print FILEOUT <<END;
