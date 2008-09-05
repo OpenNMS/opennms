c9888ad043f8c711134e87dd8c15b67b323673c1
Benjamin Reed <rangerrick@users.sourceforge.net>
2008-09-05 19:52:32 +0000

    Merged revisions 9974,9977-9978,9980,9990,10002-10003,10005,10010,10043-10046,10049 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/trunk
    ........
      r9974 | dhustace | 2008-08-01 16:42:14 -0400 (Fri, 01 Aug 2008) | 1 line
      
      first iteration of tl1 implemenetation
    ........
      r9977 | dhustace | 2008-08-01 16:51:26 -0400 (Fri, 01 Aug 2008) | 1 line
      
      add message processor interface
    ........
      r9978 | dhustace | 2008-08-01 16:53:09 -0400 (Fri, 01 Aug 2008) | 1 line
      
      message processor interface update
    ........
      r9980 | dhustace | 2008-08-01 16:58:30 -0400 (Fri, 01 Aug 2008) | 1 line
      
      message processor interface update version 2
    ........
      r9990 | dhustace | 2008-08-01 19:05:07 -0400 (Fri, 01 Aug 2008) | 1 line
      
      more tl1 enhancements.
    ........
      r10002 | brozow | 2008-08-01 19:46:20 -0400 (Fri, 01 Aug 2008) | 1 line
      
      add configuration for Tl1d
    ........
      r10003 | brozow | 2008-08-01 19:49:19 -0400 (Fri, 01 Aug 2008) | 1 line
      
      removed unused member
    ........
      r10005 | dhustace | 2008-08-02 07:59:27 -0400 (Sat, 02 Aug 2008) | 1 line
      
      start tl1 message processing thread and add tl1d configuration.
    ........
      r10010 | dhustace | 2008-08-04 10:18:28 -0400 (Mon, 04 Aug 2008) | 1 line
      
      more tl1d refactoring
    ........
      r10043 | dhustace | 2008-08-04 19:27:10 -0400 (Mon, 04 Aug 2008) | 1 line
      
      Cleaned up a few TL1 message parsing issues.
    ........
      r10044 | dhustace | 2008-08-04 23:30:46 -0400 (Mon, 04 Aug 2008) | 1 line
      
      enable reconnect-delay configuration
    ........
      r10045 | dhustace | 2008-08-04 23:48:15 -0400 (Mon, 04 Aug 2008) | 1 line
      
      clean up tl1 client socket loop
    ........
      r10046 | dhustace | 2008-08-05 01:26:23 -0400 (Tue, 05 Aug 2008) | 1 line
      
      added ability to define Tl1Client implementation class name from configuration.
    ........
      r10049 | brozow | 2008-08-05 14:39:09 -0400 (Tue, 05 Aug 2008) | 1 line
      
      remove duplicate config dao declaration
    ........

---
+++ opennms-services/src/main/java/org/opennms/netmgt/tl1d/Tl1ClientImpl.java
@@ -0,0 +1,275 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2008 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ * 
+ * Created August 1, 2008
+ *
+ * Copyright (C) 2008 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
+package org.opennms.netmgt.tl1d;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+
+import org.apache.log4j.Category;
+import org.opennms.netmgt.config.tl1d.Tl1Element;
+
+/**
+ * Default Implementation of the Tl1Client API.
+ * 
+ * @author <a href=mailto:david@opennms.org>David Hustace</a>
+ */
+public class Tl1ClientImpl implements Tl1Client {
+
+    String m_host;
+    int m_port;
+    boolean m_started = false;
+
+    private Socket m_tl1Socket;
+    private Thread m_socketReader;
+    private BlockingQueue<Tl1AutonomousMessage> m_tl1Queue;
+    private BufferedReader m_reader;
+    private TimeoutSleeper m_sleeper;
+    private Category m_log;
+    private Tl1AutonomousMessageProcessor m_messageProcessor;
+    private long m_reconnectionDelay;
+    
+    
+    public Tl1ClientImpl() {
+    }
+    
+    public Tl1ClientImpl(BlockingQueue<Tl1AutonomousMessage> queue, Tl1Element element, Category log) 
+        throws InstantiationException, IllegalAccessException, ClassNotFoundException {
+        
+        m_host = element.getHost();
+        m_port = element.getPort();
+        
+        m_tl1Queue = queue;
+        m_messageProcessor = (Tl1AutonomousMessageProcessor) Class.forName(element.getTl1MessageParser()).newInstance();
+        m_reconnectionDelay = element.getReconnectDelay();
+        m_log = log;
+    }
+
+    /* (non-Javadoc)
+     * @see org.opennms.netmgt.tl1d.Tl1Client#start()
+     */
+    public void start() {
+        m_log.info("Starting TL1 client: "+m_host+":"+String.valueOf(m_port));
+        m_started = true;
+
+        m_socketReader = new Thread("TL1-Socket-Reader") {
+
+            public void run() {
+                readMessages();
+            }
+
+        };
+
+        m_socketReader.start();
+        m_log.info("Started TL1 client: "+m_host+":"+String.valueOf(m_port));
+    }
+
+    /* (non-Javadoc)
+     * @see org.opennms.netmgt.tl1d.Tl1Client#stop()
+     */
+    public void stop() {
+        m_log.info("Stopping TL1 client: "+m_host+":"+String.valueOf(m_port));
+        m_started = false;
+    }
+
+    private BufferedReader getReader() {
+        if (m_reader == null) {
+            m_reader = createReader();
+        }
+        return m_reader;
+    }
+
+    private BufferedReader createReader() {
+        BufferedReader reader;
+        while (m_started) {
+            try {
+                m_tl1Socket = new Socket(m_host, m_port);
+                reader = new BufferedReader(new InputStreamReader(m_tl1Socket.getInputStream()));
+                resetTimeout();
+                return reader;
+            } catch (IOException e) {
+                e.printStackTrace();
+                waitUntilNextConnectTime();
+            } 
+        }
+        return null;
+    }
+
+    private void resetTimeout() {
+        m_sleeper = null;
+    }
+
+    private void waitUntilNextConnectTime() {
+        if (m_started) {
+            if (m_sleeper == null) {
+                m_sleeper = new TimeoutSleeper();
+            }
+            try { m_sleeper.sleep(); } catch (InterruptedException e) { }
+        }
+    }
+
+    private void readMessages() {
+        StringBuilder rawMessageBuilder = new StringBuilder();
+        
+        m_log.info("readMessages: Begin reading off socket...");
+        while(m_started) {
+            try {
+                m_log.info("readMessages: reading line from TL1 socket...");
+                BufferedReader reader = getReader();
+                
+                if (reader != null) {
+                    int ch;
+                    while((ch = reader.read()) != -1) {
+                        rawMessageBuilder.append((char)ch);
+                        
+                        if((char)ch == ';') {
+                            createAndQueueTl1Message(rawMessageBuilder);
+                            rawMessageBuilder.setLength(0);
+                        }
+                    }
+                    m_log.warn("readMessages: resetting socket reader to client: "+m_host+":"+m_port);
+                    resetReader(null);
+                }
+            } catch (IOException e) {
+                resetReader(e);
+            }
+        }
+        m_log.info("Stopping TL1 client: "+m_host+":"+String.valueOf(m_port));
+    }
+
+    private void createAndQueueTl1Message(StringBuilder rawMessageBuilder) {
+        m_log.debug("readMessages: offering message to queue: "+rawMessageBuilder.toString());
+        Tl1AutonomousMessage message = detectMessageType(rawMessageBuilder);
+        if (message != null) {
+            m_tl1Queue.offer(message);
+            m_log.debug("readMessages: successfully offered to queue.");
+        } else {
+            m_log.debug("readMessages: message was null, not offered to queue.");
+        }
+    }
+
+    //TODO: Lots of work to do here
+    private Tl1AutonomousMessage detectMessageType(StringBuilder rawMessage) {
+        
+        //check token 5 to see if this is a reply message.  This implies that the Tl1Client must
+        //track message TAGs (Correlation TAGs (CTAG) vs. Autonomous TAGs (ATAG))
+        
+        if(isAutonomousMessage(rawMessage)) {
+            return m_messageProcessor.process(rawMessage.toString(), Tl1Message.AUTONOMOUS);
+        }
+        
+        return null;
+    }
+
+    //TODO: Lots of work to do here
+    private boolean isAutonomousMessage(StringBuilder rawMessage) {
+        return true;
+    }
+
+    private void resetReader(IOException ex) {
+        if (ex != null) {
+            m_log.error("resetReader: connection failure.", ex);
+        }
+        try {
+            m_reader.close();
+        } catch (IOException e) { 
+        } finally {
+            m_reader = null;
+        }
+        try {
+            m_tl1Socket.close();
+        } catch (IOException e) {
+            m_tl1Socket = null;
+        }
+    }
+
+    /* (non-Javadoc)
+     * @see org.opennms.netmgt.tl1d.Tl1Client#getHost()
+     */
+    public String getHost() {
+        return m_host;
+    }
+
+    /* (non-Javadoc)
+     * @see org.opennms.netmgt.tl1d.Tl1Client#setHost(java.lang.String)
+     */
+    public void setHost(String host) {
+        m_host = host;
+    }
+
+    /* (non-Javadoc)
+     * @see org.opennms.netmgt.tl1d.Tl1Client#getPort()
+     */
+    public int getPort() {
+        return m_port;
+    }
+
+    /* (non-Javadoc)
+     * @see org.opennms.netmgt.tl1d.Tl1Client#setPort(int)
+     */
+    public void setPort(int port) {
+        m_port = port;
+    }
+
+    public BlockingQueue<Tl1AutonomousMessage> getTl1Queue() {
+        return m_tl1Queue;
+    }
+
+    public void setTl1Queue(BlockingQueue<Tl1AutonomousMessage> tl1Queue) {
+        m_tl1Queue = tl1Queue;
+    }
+
+    public Tl1AutonomousMessageProcessor getMessageProcessor() {
+        return m_messageProcessor;
+    }
+
+    public void setMessageProcessor(Tl1AutonomousMessageProcessor messageProcessor) {
+        m_messageProcessor = messageProcessor;
+    }
+
+    public void setLog(Category log) {
+        m_log = log;
+    }
+
+    private class TimeoutSleeper {
+
+        public void sleep() throws InterruptedException {
+            Thread.sleep(m_reconnectionDelay);
+        }
+    }
+}
