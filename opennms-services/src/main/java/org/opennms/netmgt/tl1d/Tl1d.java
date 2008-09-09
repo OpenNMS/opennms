06b68f77d89458b21192ee26f9565f6d7460e6d7
Matt Brozowski <brozow@users.sourceforge.net>
2008-09-09 21:39:49 +0000

    Merged revisions 10382 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ................
      r10382 | rangerrick | 2008-09-05 15:52:32 -0400 (Fri, 05 Sep 2008) | 61 lines
      
      Merged revisions 9974,9977-9978,9980,9990,10002-10003,10005,10010,10043-10046,10049 via svnmerge from 
      https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/trunk
      
      ........
        r9974 | dhustace | 2008-08-01 16:42:14 -0400 (Fri, 01 Aug 2008) | 1 line
        
        first iteration of tl1 implemenetation
      ........
        r9977 | dhustace | 2008-08-01 16:51:26 -0400 (Fri, 01 Aug 2008) | 1 line
        
        add message processor interface
      ........
        r9978 | dhustace | 2008-08-01 16:53:09 -0400 (Fri, 01 Aug 2008) | 1 line
        
        message processor interface update
      ........
        r9980 | dhustace | 2008-08-01 16:58:30 -0400 (Fri, 01 Aug 2008) | 1 line
        
        message processor interface update version 2
      ........
        r9990 | dhustace | 2008-08-01 19:05:07 -0400 (Fri, 01 Aug 2008) | 1 line
        
        more tl1 enhancements.
      ........
        r10002 | brozow | 2008-08-01 19:46:20 -0400 (Fri, 01 Aug 2008) | 1 line
        
        add configuration for Tl1d
      ........
        r10003 | brozow | 2008-08-01 19:49:19 -0400 (Fri, 01 Aug 2008) | 1 line
        
        removed unused member
      ........
        r10005 | dhustace | 2008-08-02 07:59:27 -0400 (Sat, 02 Aug 2008) | 1 line
        
        start tl1 message processing thread and add tl1d configuration.
      ........
        r10010 | dhustace | 2008-08-04 10:18:28 -0400 (Mon, 04 Aug 2008) | 1 line
        
        more tl1d refactoring
      ........
        r10043 | dhustace | 2008-08-04 19:27:10 -0400 (Mon, 04 Aug 2008) | 1 line
        
        Cleaned up a few TL1 message parsing issues.
      ........
        r10044 | dhustace | 2008-08-04 23:30:46 -0400 (Mon, 04 Aug 2008) | 1 line
        
        enable reconnect-delay configuration
      ........
        r10045 | dhustace | 2008-08-04 23:48:15 -0400 (Mon, 04 Aug 2008) | 1 line
        
        clean up tl1 client socket loop
      ........
        r10046 | dhustace | 2008-08-05 01:26:23 -0400 (Tue, 05 Aug 2008) | 1 line
        
        added ability to define Tl1Client implementation class name from configuration.
      ........
        r10049 | brozow | 2008-08-05 14:39:09 -0400 (Tue, 05 Aug 2008) | 1 line
        
        remove duplicate config dao declaration
      ........
    ................

---
+++ opennms-services/src/main/java/org/opennms/netmgt/tl1d/Tl1d.java
@@ -0,0 +1,187 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2008 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ * 
+ * Created August 1, 2008
+ *
+ * Copyright (C) 2008 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
+package org.opennms.netmgt.tl1d;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import org.opennms.core.fiber.PausableFiber;
+import org.opennms.netmgt.config.tl1d.Tl1Element;
+import org.opennms.netmgt.daemon.AbstractServiceDaemon;
+import org.opennms.netmgt.dao.Tl1ConfigurationDao;
+import org.opennms.netmgt.eventd.EventIpcManager;
+import org.opennms.netmgt.eventd.EventListener;
+import org.opennms.netmgt.utils.EventBuilder;
+import org.opennms.netmgt.xml.event.Event;
+import org.springframework.beans.factory.InitializingBean;
+
+/**
+ * OpenNMS TL1 Daemon!
+ * 
+ * @author <a href="mailto:david@opennms.org">David Hustace</a>
+ */
+public class Tl1d extends AbstractServiceDaemon implements PausableFiber, InitializingBean, EventListener {
+
+    /*
+     * The last status sent to the service control manager.
+     */
+    private int m_status = START_PENDING;
+    private BlockingQueue<Tl1AutonomousMessage> m_tl1Queue;
+    private Thread m_tl1MesssageProcessor;
+    private ArrayList<Tl1Client> m_tl1Clients;
+    private EventIpcManager m_eventManager;
+	private Tl1ConfigurationDao m_configurationDao;
+
+    public Tl1d() {
+        super("OpenNMS.Tl1d");
+    }
+	
+	public void setConfigurationDao(Tl1ConfigurationDao configurationDao) {
+	    m_configurationDao = configurationDao;
+	}
+
+
+    public void setEventManager(EventIpcManager eventManager) {
+        m_eventManager = eventManager;
+    }
+
+    public synchronized void onInit() {
+        log().info("onInit: Initializing Tl1d connections." );
+        m_tl1Queue = new LinkedBlockingQueue<Tl1AutonomousMessage>();
+    
+        //initialize a factory of configuration
+    
+        List<Tl1Element> configElements = m_configurationDao.getElements();
+    
+        m_tl1Clients = new ArrayList<Tl1Client>();
+        
+        for(Tl1Element element : configElements) {
+            try {
+                Tl1Client client = (Tl1Client) Class.forName(element.getTl1ClientApi()).newInstance();
+                client.setHost(element.getHost());
+                client.setPort(element.getPort());
+                client.setTl1Queue(m_tl1Queue);
+                client.setMessageProcessor((Tl1AutonomousMessageProcessor) Class.forName(element.getTl1MessageParser()).newInstance());
+                client.setLog(log());
+                m_tl1Clients.add(client);
+            } catch (InstantiationException e) {
+                log().error("onInit: could not instantiate specified class.", e);
+            } catch (IllegalAccessException e) {
+                log().error("onInit: could not access specified class.", e);
+            } catch (ClassNotFoundException e) {
+                log().error("onInit: could not find specified class.", e);
+            }
+        }
+
+        log().info("onInit: Finished Initializing Tl1d connections.");  
+    }
+
+    public synchronized void onStart() {
+        log().info("onStart: Initializing Tl1d message processing." );
+        m_tl1MesssageProcessor = new Thread("Tl1-Message-Processor") {
+            public void run() {
+                doMessageProcessing();
+            }
+        };
+
+        m_tl1MesssageProcessor.start();
+
+        for (Tl1Client client : m_tl1Clients) {
+            client.start();
+        }
+        log().info("onStart: Finished Initializing Tl1d connections.");
+    }
+
+	public synchronized void onStop() {
+		for (Tl1Client client : m_tl1Clients) {
+			client.stop();
+		}
+		m_tl1MesssageProcessor.interrupt();
+	}
+
+
+    private void processMessage(Tl1AutonomousMessage message) {
+        log().debug("processMessage: Processing message: "+message);
+
+        EventBuilder bldr = new EventBuilder(Tl1AutonomousMessage.UEI, "Tl1d");
+        bldr.setHost(message.getHost());
+        bldr.setTime(message.getTimestamp());
+        bldr.addParam("raw-message", message.getRawMessage());
+        bldr.addParam("alarm-code", message.getId().getAlarmCode());
+        bldr.addParam("atag", message.getId().getAlarmTag());
+        bldr.addParam("verb", message.getId().getVerb());
+        bldr.addParam("autoblock", message.getAutoBlock().getBlock());
+        
+        m_eventManager.sendNow(bldr.getEvent());
+        log().debug("processMessage: Message processed: "+message);
+    }
+
+
+    public void onPause() {
+    }
+
+    public void onResume() {
+    }
+
+    /**
+     * Returns the current status of the service.
+     * 
+     * @return The service's status.
+     */
+    public synchronized int getStatus() {
+        return m_status;
+    }
+
+    private void doMessageProcessing() {
+        log().debug("doMessageProcessing: Processing messages.");
+        boolean cont = true;
+        while (cont ) {
+            try {
+                Tl1AutonomousMessage message = m_tl1Queue.take();
+                processMessage(message);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        log().debug("doMessageProcessing: Exiting processing messages.");
+    }
+
+    public void onEvent(Event e) {
+    }
+
+}
