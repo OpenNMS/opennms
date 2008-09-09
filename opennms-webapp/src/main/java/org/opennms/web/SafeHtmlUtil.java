44e5f05b7692cd4e6ff86433b4fed25a01fdc0dd
Jeff Gehlbach <jgehlbach@users.sourceforge.net>
2008-09-09 01:58:27 +0000

    Merged revisions 10153,10193,10292 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ........
      r10153 | rangerrick | 2008-08-15 10:34:13 -0400 (Fri, 15 Aug 2008) | 1 line
      
      no canonicalize since it is java6 only
    ........
      r10193 | rangerrick | 2008-08-19 16:03:23 -0400 (Tue, 19 Aug 2008) | 1 line
      
      it turns out you have to actually *remove* the java6-only code for it to compile with 5.  :P
    ........
      r10292 | rangerrick | 2008-08-27 22:57:26 -0400 (Wed, 27 Aug 2008) | 1 line
      
      fix pattern replacement; also, the code basically doesn't resemble the original anymore, at this point I don't think there's really a conflict for release.
    ........

--- opennms-webapp/src/main/java/org/opennms/web/SafeHtmlUtil.java
+++ opennms-webapp/src/main/java/org/opennms/web/SafeHtmlUtil.java
@@ -1,51 +1,48 @@
-package org.opennms.web;
+//
+// This file is part of the OpenNMS(R) Application.
+//
+// OpenNMS(R) is Copyright (C) 2002-2008 The OpenNMS Group, Inc.  All rights reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included code and modified
+// code that was published under the GNU General Public License. Copyrights for modified 
+// and included code are below.
+//
+// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// For more information contact:
+//      OpenNMS Licensing       <license@opennms.org>
+//      http://www.opennms.org/
+//      http://www.opennms.com/
 
-// see: http://mc4j.org/confluence/display/stripes/XSS+filter
+package org.opennms.web;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import sun.text.Normalizer;
-
-
 public class SafeHtmlUtil
 {
+    private static Pattern scriptPattern = Pattern.compile("script", Pattern.CASE_INSENSITIVE);
+
     public static String sanitize(String raw)
     {
         if (raw==null || raw.length()==0)
             return raw;
 
-        return HTMLEntityEncode(canonicalize(raw));
-    }
-
-    private static Pattern scriptPattern = Pattern.compile("script", Pattern.CASE_INSENSITIVE);
-    private static Matcher scriptMatcher = scriptPattern.matcher("&#x73;cript");
-
-    public static String HTMLEntityEncode(String input)
-    {
-        String next = scriptMatcher.replaceAll(input);
-
-        StringBuffer sb = new StringBuffer();
-        for ( int i = 0; i < next.length(); ++i )
-        {
-            char ch = next.charAt( i );
-
-            if (ch=='<')
-                sb.append("&lt;");
-            else if (ch=='>')
-                sb.append("&gt;");
-            else
-                sb.append(ch);
-        }
-
-        return sb.toString();
-    }
-
-
-    // "Simplifies input to its simplest form to make encoding tricks more difficult"
-    // though it didn't do seem to do anything to hex or HTML encoded characters... *shrug* maybe for unicode?
-    public static String canonicalize( String input )
-    {
-        return Normalizer.normalize( input, Normalizer.DECOMP, 0 );
+        Matcher scriptMatcher = scriptPattern.matcher(raw);
+        String next = scriptMatcher.replaceAll("&#x73;cript");
+        return next.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
     }
 }
