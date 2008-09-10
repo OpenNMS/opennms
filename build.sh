e8aad6b0d0a0d36978d4b63a5a971a02633de2f1
Paul Donohue <paulsd@users.sourceforge.net>
2008-09-10 22:06:40 +0000

    Bumping default -Xmx in build.sh ... build keeps running out of memory for me

--- build.sh
+++ build.sh
@@ -54,7 +54,7 @@ if [ -n "$JAVA_HOME" ]; then
 	export PATH
 fi
 
-[ -z "$MAVEN_OPTS" ] && MAVEN_OPTS=-Xmx512m
+[ -z "$MAVEN_OPTS" ] && MAVEN_OPTS=-Xmx1g
 export MAVEN_OPTS
 [ -z "$MVN" ] && MVN="$PREFIX/maven/bin/mvn"
 "$MVN" -Droot.dir=$PREFIX -D$MAVEN_SKIP=true "$@"
