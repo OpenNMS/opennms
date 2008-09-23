6fa04e88dcf7f0276d4790c5ba46540d0522f346
Matt Brozowski <brozow@users.sourceforge.net>
2008-09-23 17:55:43 +0000

    Merged revisions 10572 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ........
      r10572 | rangerrick | 2008-09-18 09:25:56 -0400 (Thu, 18 Sep 2008) | 2 lines
      
      bug 2731 - honor java.conf in remote-poller.sh
    ........

--- opennms-daemon/src/main/filtered/bin/remote-poller.sh
+++ opennms-daemon/src/main/filtered/bin/remote-poller.sh
@@ -3,35 +3,49 @@
 # Starts the remote poller.
 # w00t
 #
+# Thu Sep 18 08:54:00 EST 2008 - ranger@opennms.org
+# - updated to use runjava
 # Mon Jun 16 15:52:00 EST 2008 - ranger@opennms.org
 # - updated to use the installed location
 # Tue Dec 12 23:05:42 GMT 2006 - dj@opennms.org
 
-#JAVA_HOME="/usr/java/jdk1.6.0_02"
+OPENNMS_HOME="@install.dir@"
+JAVA_CONF="$OPENNMS_HOME/etc/java.conf"
+
+#JAVA_HOME=""
 #RMI_LOCATION="RDU"
 #RMI_HOST="demo.opennms.org"
-MONITOR_JAR="@install.dir@/bin/remote-poller.jar"
+MONITOR_JAR="$OPENNMS_HOME/bin/remote-poller.jar"
 RMI_PORT=1099
 
-
-if [ "$JAVA_HOME" = "" ]; then
-  echo "Set \$JAVA_HOME and try again."
-  exit 1
+if [ -f "$JAVA_CONF" ]; then
+        JAVA_EXE="`cat $JAVA_CONF`"
 fi
+
+if [ "$JAVA_EXE" = "" ]; then
+        if [ "$JAVA_HOME" = "" ] || [ ! -x "$JAVA_HOME/bin/java" ]; then
+                echo "ERROR: $JAVA_CONF file not found, and \$JAVA_HOME is not set to a valid JDK."
+                echo "Try running $OPENNMS_HOME/bin/runjava, or set JAVA_HOME."
+                exit 1
+        else
+                JAVA_EXE="$JAVA_HOME/bin/java"
+        fi
+fi
+
 if [ "$RMI_LOCATION" = "" ]; then
-  echo "Set \$RMI_LOCATION and try again."
-  exit 1
+        echo "Set \$RMI_LOCATION and try again."
+        exit 1
 fi
 if [ "$RMI_HOST" = "" ]; then
-  echo "Set the RMI host in this script."
-  exit 1
+        echo "Set the RMI host in this script."
+        exit 1
 fi
 
 #log_file="poll.log.`date '+%Y%m%d-%H%M%S'`"
 #log_file="/tmp/poll.log"
 log_file="/dev/null"
 
-exec nohup $JAVA_HOME/bin/java \
+exec nohup $JAVA_EXE \
         -Djava.rmi.activation.port="$RMI_PORT" \
         -Dlog4j.logger="DEBUG" \
         -jar "$MONITOR_JAR" \
