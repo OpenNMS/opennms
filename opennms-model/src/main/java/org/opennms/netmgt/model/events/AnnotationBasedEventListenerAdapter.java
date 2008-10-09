e4ae0e2bfb515591c5536db8b45d31dd40858fa6
Benjamin Reed <rangerrick@users.sourceforge.net>
2008-10-09 15:40:36 +0000

    Merged revisions 10827-10828 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ........
      r10827 | brozow | 2008-10-06 23:32:09 -0400 (Mon, 06 Oct 2008) | 1 line
      
      Bug 2714: Inspect implementers of ReadyRunnable and EventListener and mark appropriately as final or volatile
    ........
      r10828 | brozow | 2008-10-07 10:42:30 -0400 (Tue, 07 Oct 2008) | 1 line
      
      fix broken AnnotationBasedEventListener
    ........

--- opennms-model/src/main/java/org/opennms/netmgt/model/events/AnnotationBasedEventListenerAdapter.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/events/AnnotationBasedEventListenerAdapter.java
@@ -40,7 +40,6 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
