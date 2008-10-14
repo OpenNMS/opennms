b6d9fa50407ae71c5fba9657b2b890bcbb0a5347
Benjamin Reed <rangerrick@users.sourceforge.net>
2008-10-14 14:56:29 +0000

    Merged revisions 10837-10840,10866-10867,10878,10883-10884,10887-10888,10890-10891 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ........
      r10837 | thedesloge | 2008-10-08 16:44:18 -0400 (Wed, 08 Oct 2008) | 2 lines
      
      Bug 2779: Fixed the GroupManager renameUser function, was throwing a java.util.ConcurrentModificationException upon renaming a user that was also in a group.
    ........
      r10838 | brozow | 2008-10-08 19:08:29 -0400 (Wed, 08 Oct 2008) | 1 line
      
      increase time to deal with slow systems..... again
    ........
      r10839 | rangerrick | 2008-10-08 21:56:57 -0400 (Wed, 08 Oct 2008) | 1 line
      
      copyright updates
    ........
      r10840 | dhustace | 2008-10-09 11:17:22 -0400 (Thu, 09 Oct 2008) | 1 line
      
      fix outage maintenance bug
    ........
      r10866 | jgehlbach | 2008-10-10 16:57:31 -0400 (Fri, 10 Oct 2008) | 2 lines
      
      Fix for bug 2806, allow HttpCollector to collect strings
      that don't parse as numbers
    ........
      r10867 | jgehlbach | 2008-10-10 17:11:03 -0400 (Fri, 10 Oct 2008) | 1 line
      
      Test for bug 2806
    ........
      r10878 | rangerrick | 2008-10-13 09:54:20 -0400 (Mon, 13 Oct 2008) | 1 line
      
      started in on the userguide, but realizing I am not best qualified to work on that yet; gonna mess with the install guide instead
    ........
      r10883 | rangerrick | 2008-10-13 16:39:45 -0400 (Mon, 13 Oct 2008) | 1 line
      
      revamp the documentation stuff quite a bit, also, releasenotes are updated to be cleaned out of old junk, and rearranged format-wise a bit
    ........
      r10884 | dhustace | 2008-10-13 16:52:55 -0400 (Mon, 13 Oct 2008) | 1 line
      
      Corrected URL for importing nodes from Hyperic HQ
    ........
      r10887 | rangerrick | 2008-10-13 17:16:38 -0400 (Mon, 13 Oct 2008) | 1 line
      
      more documentation tweaks
    ........
      r10888 | rangerrick | 2008-10-13 17:25:24 -0400 (Mon, 13 Oct 2008) | 1 line
      
      forgot to reenable pdf
    ........
      r10890 | dhustace | 2008-10-13 21:29:53 -0400 (Mon, 13 Oct 2008) | 1 line
      
      fixed example LDAP configuration according to list post today.
    ........
      r10891 | rangerrick | 2008-10-13 23:14:23 -0400 (Mon, 13 Oct 2008) | 1 line
      
      fix foCustomization when a full build is run
    ........

--- opennms-services/src/test/java/org/opennms/netmgt/config/UserGroupManagerTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/config/UserGroupManagerTest.java
@@ -35,15 +35,22 @@
 //
 package org.opennms.netmgt.config;
 
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
-import junit.framework.TestCase;
-
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.opennms.netmgt.config.groups.Group;
 import org.opennms.netmgt.config.groups.Role;
 import org.opennms.netmgt.config.users.User;
@@ -52,7 +59,7 @@ import org.opennms.netmgt.notifd.mock.MockUserManager;
 import org.opennms.test.ConfigurationTestUtils;
 import org.opennms.test.mock.MockLogAppender;
 
-public class UserGroupManagerTest extends TestCase {
+public class UserGroupManagerTest {
     private GroupManager m_groupManager;
     private UserManager m_userManager;
 
@@ -72,7 +79,8 @@ public class UserGroupManagerTest extends TestCase {
         junit.textui.TestRunner.run(UserGroupManagerTest.class);
     }
 
-    protected void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
         MockLogAppender.setupLogging();
         m_groupManager = new MockGroupManager(ConfigurationTestUtils.getConfigForResourceWithReplacements(this, "groups.xml", new String[][] {}));
         m_userManager = new MockUserManager(m_groupManager, ConfigurationTestUtils.getConfigForResourceWithReplacements(this, "users.xml", new String[][] {}));
@@ -103,11 +111,12 @@ public class UserGroupManagerTest extends TestCase {
         assertEquals("unscheduled", unscheduled.getName());
     }
 
-    protected void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         MockLogAppender.assertNoWarningsOrGreater();
-        super.tearDown();
     }
     
+    @Test
     public void testGetUserNames() throws Exception {
         List userNameList = m_userManager.getUserNames();
         assertEquals(4, userNameList.size());
@@ -117,6 +126,25 @@ public class UserGroupManagerTest extends TestCase {
         assertTrue(userNameList.contains("david"));
     }
     
+    @Test
+    public void testRenameUser() throws Exception{
+        m_userManager.renameUser("brozow", "brozow2");
+        List userNameList = m_userManager.getUserNames();
+       
+        assertEquals(4, userNameList.size());
+        assertTrue(userNameList.contains("brozow2"));
+        
+        List group1Users = m_groupManager.getGroup("InitialGroup").getUserCollection();
+        List group2Users = m_groupManager.getGroup("EscalationGroup").getUserCollection();
+        List group3Users = m_groupManager.getGroup("UpGroup").getUserCollection();
+        
+        assertFalse(group1Users.contains("brozow"));
+        assertFalse(group2Users.contains("brozow"));
+        assertFalse(group3Users.contains("brozow"));
+        
+    }
+    
+    @Test
     public void testSaveUser() throws Exception {
         String userName = "brozow";
         User user = brozow;
@@ -140,6 +168,7 @@ public class UserGroupManagerTest extends TestCase {
         
     }
     
+    @Test
     public void testGetGroupNames() throws Exception {
         List userNameList = m_groupManager.getGroupNames();
         assertEquals(3, userNameList.size());
@@ -148,7 +177,8 @@ public class UserGroupManagerTest extends TestCase {
         assertTrue(userNameList.contains("UpGroup"));
 
     }
-
+    
+    @Test
     public void testSaveGroups() throws Exception {
         final String groupName = "UpGroup";
         Group group = m_groupManager.getGroup(groupName);
@@ -179,10 +209,12 @@ public class UserGroupManagerTest extends TestCase {
 
     }
     
+    @Test
     public void testGetRoles() {
         assertRoles(m_groupManager.getRoleNames(), new Role[] { oncall, unscheduled });
     }
     
+    @Test
     public void testUserHasRole() throws Exception {
         assertTrue(m_userManager.userHasRole(brozow, "oncall"));
         assertTrue(m_userManager.userHasRole(admin, "oncall"));
@@ -190,12 +222,14 @@ public class UserGroupManagerTest extends TestCase {
         assertTrue(m_userManager.userHasRole(david, "oncall"));
     }
     
+    @Test
     public void testGetUsersWithRole() throws Exception {
         String[] userNames = m_userManager.getUsersWithRole("oncall");
         assertUsers(userNames, new User[] { brozow, admin, david });
         
     }
     
+    @Test
     public void testUserScheduledForRoleNew() throws Exception {
         Date[] dates = new Date[] {night, day, sunday};
         for (int i = 0; i < dates.length; i++) {
@@ -210,7 +244,7 @@ public class UserGroupManagerTest extends TestCase {
             
         }
     }
-
+    
     private void testUsersScheduleForRoleAt(String role, Date date) throws Exception {
         for (Iterator it = m_userManager.getUserNames().iterator(); it.hasNext();) {
             String userId = (String) it.next();
@@ -218,11 +252,12 @@ public class UserGroupManagerTest extends TestCase {
             testUserScheduledForRoleAt(u, role, date);
         }
     }
-
+    
     private void testUserScheduledForRoleAt(User u, String role, Date date) throws Exception {
         assertEquals("Unexpected value "+u.getUserId()+" for role "+role+" at "+date, m_userManager.isUserScheduledForRole(u, role, date), m_userManager.isUserScheduledForRole(u, role, date));
     }
-
+    
+    @Test
     public void testUserScheduledForRole() throws Exception {
         // day and night are mondays at 11 am and 11 pm respectively
         
@@ -252,6 +287,7 @@ public class UserGroupManagerTest extends TestCase {
 
     }
     
+    @Test
     public void testGetUsersScheduledForRole() throws Exception {
         String[] nightUserNames = m_userManager.getUsersScheduledForRole("oncall", night);
         assertUsers(nightUserNames, new User[]{ david });
