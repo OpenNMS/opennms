0bcf4b334d4c2c93ddf3a0fb6e6230f5ddb55900
Matt Brozowski <brozow@users.sourceforge.net>
2008-10-15 11:53:50 +0000

    FIXME tests.. maybe a real bug too

--- opennms-webapp/src/test/java/org/opennms/web/svclayer/support/DefaultSurveillanceServiceTest.java
+++ opennms-webapp/src/test/java/org/opennms/web/svclayer/support/DefaultSurveillanceServiceTest.java
@@ -32,34 +32,57 @@
 
 package org.opennms.web.svclayer.support;
 
-import static org.easymock.EasyMock.createMock;
+import static org.easymock.EasyMock.eq;
+import static org.easymock.EasyMock.expect;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import junit.framework.TestCase;
-
+import org.junit.Before;
+import org.junit.Test;
+import org.opennms.netmgt.config.surveillanceViews.Columns;
+import org.opennms.netmgt.config.surveillanceViews.Rows;
+import org.opennms.netmgt.config.surveillanceViews.View;
 import org.opennms.netmgt.dao.CategoryDao;
 import org.opennms.netmgt.dao.NodeDao;
 import org.opennms.netmgt.dao.SurveillanceViewConfigDao;
 import org.opennms.netmgt.model.OnmsCategory;
 import org.opennms.netmgt.model.OnmsNode;
+import org.opennms.test.mock.EasyMockUtils;
 
-public class DefaultSurveillanceServiceTest extends TestCase {
+public class DefaultSurveillanceServiceTest {
+    
+    private EasyMockUtils m_mockUtils;
     
     private NodeDao m_nodeDao;
     private CategoryDao m_categoryDao;
     private SurveillanceViewConfigDao m_surveillanceViewConfigDao;
     
-    @Override
+    @Before
     public void setUp() throws Exception {
-        m_nodeDao = createMock(NodeDao.class);
-        m_categoryDao = createMock(CategoryDao.class);
-        m_surveillanceViewConfigDao = createMock(SurveillanceViewConfigDao.class);
+        m_mockUtils = new EasyMockUtils();
+        
+        m_nodeDao = m_mockUtils.createMock(NodeDao.class);
+        m_categoryDao = m_mockUtils.createMock(CategoryDao.class);
+        m_surveillanceViewConfigDao = m_mockUtils.createMock(SurveillanceViewConfigDao.class);
     }
     
+    @Test
     public void testCreateSurveillanceTable() {
+        
+        View view = new View();
+        Rows rows = new Rows();
+        
+        view.setRows(rows);
+        
+        Columns columns = new Columns();
+        
+        view.setColumns(columns);
+        
+        expect(m_surveillanceViewConfigDao.getView(eq("default"))).andReturn(view).atLeastOnce();
+        
+        m_mockUtils.replayAll();
         
         DefaultSurveillanceService surveillanceSvc = new DefaultSurveillanceService();
         surveillanceSvc.setNodeDao(m_nodeDao);
@@ -67,6 +90,8 @@ public class DefaultSurveillanceServiceTest extends TestCase {
         surveillanceSvc.setSurveillanceConfigDao(m_surveillanceViewConfigDao);
 
         surveillanceSvc.createSurveillanceTable();
+        
+        m_mockUtils.verifyAll();
         
     }
 
@@ -84,6 +109,7 @@ public class DefaultSurveillanceServiceTest extends TestCase {
         return categories;
     }
     
+    @Test
     public void testUrlMaker() {
         System.err.println(createNodePageUrl("1 of 10"));
         
