5cf1fc1a632f81bf83ec3c7b5d1f470b7941febc
Donald Desloge <thedesloge@users.sourceforge.net>
2008-10-31 15:28:13 +0000

    Updated dependencies in the simple detector's pom, new SmtpDetector and test

---
+++ opennms-provision/opennms-simple-detectors/src/test/java/org/opennms/netmgt/provision/detector/SmtpDetectorTest.java
@@ -0,0 +1,76 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2008 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ * OpenNMS Licensing       <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ */
+package org.opennms.netmgt.provision.detector;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.opennms.netmgt.provision.server.SimpleServer;
+import org.opennms.netmgt.provision.support.NullDetectorMonitor;
+
+
+/**
+ * @author Donald Desloge
+ *
+ */
+public class SmtpDetectorTest {
+    
+    private SmtpDetector m_detector;
+    private SimpleServer m_server;
+    
+    @Before
+    public void setUp() throws Exception {
+        m_server = getServer();
+        m_server.init();
+        m_server.startServer();
+        
+        m_detector = new SmtpDetector();
+        m_detector.init();
+        m_detector.setPort(m_server.getLocalPort());
+    }
+    
+    @Test
+    public void testMyDetector() {
+        assertTrue(m_detector.isServiceDetected(m_server.getInetAddress(), new NullDetectorMonitor()));
+    }
+    
+    private SimpleServer getServer() {
+        return new SimpleServer() {
+            
+            public void onInit() {
+                setBanner("220 ewhserver279.edgewebhosting.net");
+                addResponseHandler(matches("HELO LOCALHOST"), singleLineReply("250-First line"));
+                addResponseHandler(matches("QUIT"), shutdownServer("221 Service closing transmission channel"));
+            }
+        };
+    }
+}
