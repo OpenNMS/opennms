5ac84aef01cbd6cb9c01e6d4542c044a107ba766
Matt Brozowski <brozow@users.sourceforge.net>
2008-11-05 18:13:18 +0000

    move some EventUtils function from capsd to model.events

--- opennms-model/src/main/java/org/opennms/netmgt/EventConstants.java
+++ opennms-model/src/main/java/org/opennms/netmgt/EventConstants.java
@@ -818,6 +818,12 @@ public class EventConstants {
     public final static String PARM_PASSIVE_SERVICE_NAME = "passiveServiceName";
     public final static String PARM_PASSIVE_SERVICE_STATUS = "passiveStatus";
     public final static String PARM_PASSIVE_REASON_CODE = "passiveReasonCode";
+
+    /**
+     * Parm used to importer event
+     */
+    public static final String PARM_FOREIGN_SOURCE = "foreignSource";
+
     
     /**
      * Parms used for configureSnmp events
@@ -893,6 +899,7 @@ public class EventConstants {
      * active
      */
     static final int STATE_OFF = 0;
+
 
 
     /**
