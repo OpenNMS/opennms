f27aa3872ed1ef68fd91df6733267f457915fb65
Matt Brozowski <brozow@users.sourceforge.net>
2008-11-13 04:34:11 +0000

    refactor persisting code into the provision service

--- opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/Main.java
+++ opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/Main.java
@@ -43,7 +43,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             ApplicationContext appContext = new ClassPathXmlApplicationContext("/META-INF/modelImport-appContext.xml");
-            ModelImporter importer = (ModelImporter)appContext.getBean("modelImporter");
+            BaseImporter importer = (BaseImporter)appContext.getBean("modelImporter");
             Resource resource = new FileSystemResource(args[0]);
             importer.importModelFromResource(resource);            
         } catch (ModelImportException e) {
