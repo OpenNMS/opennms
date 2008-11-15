a0505d47d78535acffffee74d56a071b1d78043a
Matt Brozowski <brozow@users.sourceforge.net>
2008-11-15 04:07:32 +0000

    reduce the logging for the ProvisionerTests

--- opennms-provision/opennms-provisiond/src/test/java/org/opennms/netmgt/provision/service/BaseProvisionerTest.java
+++ opennms-provision/opennms-provisiond/src/test/java/org/opennms/netmgt/provision/service/BaseProvisionerTest.java
@@ -151,6 +151,7 @@ public class BaseProvisionerTest {
     public void setUp() {
         Properties props = new Properties();
         props.setProperty("log4j.logger.org.hibernate", "INFO");
+        props.setProperty("log4j.logger.org.springframework", "INFO");
         props.setProperty("log4j.logger.org.hibernate.SQL", "DEBUG");
 
         MockLogAppender.setupLogging(props);
