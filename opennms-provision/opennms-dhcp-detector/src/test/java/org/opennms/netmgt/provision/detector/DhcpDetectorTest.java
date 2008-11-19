3b752a7918b644864eb6c638853c148a1610bdfb
Donald Desloge <thedesloge@users.sourceforge.net>
2008-11-19 00:17:23 +0000

    * RadiusAuthDetector added * DhcpDetector added

---
+++ opennms-provision/opennms-dhcp-detector/src/test/java/org/opennms/netmgt/provision/detector/DhcpDetectorTest.java
@@ -0,0 +1,26 @@
+package org.opennms.netmgt.provision.detector;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.ValidationException;
+import org.junit.Test;
+import org.opennms.netmgt.provision.support.dhcp.DhcpdConfigFactory;
+
+public class DhcpDetectorTest{
+	
+	@Test
+	public void testMyDetector() throws MarshalException, ValidationException, IOException{
+	    //FIXME: Finish test
+	    //	    DhcpdConfigFactory.init();
+//		DhcpDetector detect = new DhcpDetector();
+//		long retVal = detect.isService(InetAddress.getByName("192.168.1.101"), 5818, 1000);
+//		System.out.println("returning: " + retVal);
+//		assertNotNull(retVal);
+	}
+	
+}
