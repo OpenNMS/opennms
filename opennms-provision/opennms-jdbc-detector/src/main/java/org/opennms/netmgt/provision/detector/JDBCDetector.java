90037f972aab25d52f2d7ac561e3f4693f799f01
Donald Desloge <thedesloge@users.sourceforge.net>
2008-11-25 00:11:24 +0000

    * The forgotten JDBCDetector project.

---
+++ opennms-provision/opennms-jdbc-detector/src/main/java/org/opennms/netmgt/provision/detector/JDBCDetector.java
@@ -0,0 +1,9 @@
+package org.opennms.netmgt.provision.detector;
+
+import org.opennms.netmgt.provision.support.BasicDetector;
+
+public class JDBCDetector{ //extends BasicDetector<Request, Response>{
+	
+    
+    
+}
