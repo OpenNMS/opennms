227b238aca85a5b381a1434fb7e57e19b7adc434
Donald Desloge <thedesloge@users.sourceforge.net>
2008-11-25 17:05:15 +0000

    * JDBCDetector updated

--- opennms-provision/opennms-jdbc-detector/src/main/java/org/opennms/netmgt/provision/detector/JDBCClient.java
+++ opennms-provision/opennms-jdbc-detector/src/main/java/org/opennms/netmgt/provision/detector/JDBCClient.java
@@ -30,11 +30,102 @@
  */
 package org.opennms.netmgt.provision.detector;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.Driver;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+import java.util.Properties;
+
+import org.opennms.netmgt.provision.support.jdbc.DBTools;
+import org.opennms.netmgt.provision.support.Client;
+
 /**
  * @author thedesloge
  *
  */
-public class JDBCClient {
+public class JDBCClient implements Client<JDBCRequest, JDBCResponse> {
+
+    private String m_dbDriver;
+    private String m_user;
+    private String m_password;
+    private String m_url;
     
+    private ResultSet m_result;
+    private Connection m_connection;
+    
+    public void close() {
+        if(m_connection != null) {
+            try {
+                m_connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void connect(InetAddress address, int port, int timeout) throws IOException, Exception {
+        
+        System.out.println("Loading JDBC driver: '" + getDbDriver() + "'");
+        Driver driver = (Driver)Class.forName(getDbDriver()).newInstance();
+        System.out.println("JDBC driver loaded: '" + getDbDriver() + "'");
+
+        String url = DBTools.constructUrl(getUrl(), address.getCanonicalHostName());
+        System.out.println("Constructed JDBC url: '" + url + "'");
+
+        Properties props = new Properties();
+        props.setProperty("user", getUser());
+        props.setProperty("password", getPassword());
+        props.setProperty("timeout", String.valueOf(timeout/1000));
+        m_connection = driver.connect(url, props);
+
+        System.out.println("Got database connection: '" + m_connection + "' (" + url + ", " + getUser() + ", " + getPassword() + ")");
+        
+    }
+
+    public JDBCResponse receiveBanner() throws IOException, Exception {
+        JDBCResponse response = new JDBCResponse();
+        response.receive(m_connection);
+        return response;
+    }
+
+    public JDBCResponse sendRequest(JDBCRequest request) throws IOException, Exception {
+        return null;
+    }
+
+    public void setDbDriver(String dbDriver) {
+        m_dbDriver = dbDriver;
+    }
+
+    public String getDbDriver() {
+        return m_dbDriver;
+    }
+
+    public void setUser(String user) {
+        m_user = user;
+    }
+
+    public String getUser() {
+        return m_user;
+    }
+
+    public void setPassword(String password) {
+        m_password = password;
+    }
+
+    public String getPassword() {
+        return m_password;
+    }
+
+    public void setUrl(String url) {
+        m_url = url;
+    }
+
+    public String getUrl() {
+        return m_url;
+    }
     
 }
