ef87d5153fe01e9c0755590576efb843f2dace48
Donald Desloge <thedesloge@users.sourceforge.net>
2008-11-25 17:05:15 +0000

    * JDBCDetector updated

--- opennms-provision/opennms-jdbc-detector/src/main/java/org/opennms/netmgt/provision/detector/JDBCDetector.java
+++ opennms-provision/opennms-jdbc-detector/src/main/java/org/opennms/netmgt/provision/detector/JDBCDetector.java
@@ -1,8 +1,82 @@
 package org.opennms.netmgt.provision.detector;
 
 import org.opennms.netmgt.provision.support.BasicDetector;
+import org.opennms.netmgt.provision.support.Client;
+import org.opennms.netmgt.provision.support.ClientConversation.ResponseValidator;
+import org.opennms.netmgt.provision.support.jdbc.DBTools;
 
-public class JDBCDetector{ //extends BasicDetector<Request, Response>{
+public class JDBCDetector extends BasicDetector<JDBCRequest, JDBCResponse>{
+    
+    private static int DEFAULT_PORT = 3306;
+    private static int DEFAULT_TIMEOUT = 1000;
+    private static int DEFAULT_RETRIES = 0;
+    
+    
+    private String m_dbDriver = DBTools.DEFAULT_JDBC_DRIVER;
+    private String m_user = DBTools.DEFAULT_DATABASE_USER;
+    private String m_password = DBTools.DEFAULT_DATABASE_PASSWORD;
+    private String m_url = DBTools.DEFAULT_URL;
+    
+    protected JDBCDetector() {
+        super(DEFAULT_PORT, DEFAULT_TIMEOUT, DEFAULT_RETRIES);
+        setServiceName("JDBC");
+    }
+
+    @Override
+    protected void onInit() {
+        expectBanner(resultSetNotNull());
+    }
+    
+    @Override
+    protected Client<JDBCRequest, JDBCResponse> getClient() {
+        JDBCClient client = new JDBCClient();
+        client.setDbDriver(getDbDriver());
+        client.setUser(getUser());
+        client.setPassword(getPassword());
+        client.setUrl(getUrl());
+        return client;
+    }
+    
+    private ResponseValidator<JDBCResponse> resultSetNotNull(){
+        return new ResponseValidator<JDBCResponse>() {
+
+            public boolean validate(JDBCResponse response) throws Exception {
+                return response.resultSetNotNull();
+            }
+        };
+    }
+
+    public void setDbDriver(String dbDriver) {
+        m_dbDriver = dbDriver;
+    }
+
+    public String getDbDriver() {
+        return m_dbDriver;
+    }
+
+    public void setUser(String username) {
+        m_user = username;
+    }
+
+    public String getUser() {
+        return m_user;
+    }
+
+    public void setPassword(String password) {
+        m_password = password;
+    }
+
+    public String getPassword() {
+        return m_password;
+    }
+
+    public void setUrl(String url) {
+        m_url = url;
+    }
+
+    public String getUrl() {
+        return m_url;
+    }
 	
     
     
--- opennms-provision/opennms-jdbc-detector/src/test/java/org/opennms/netmgt/provision/detector/JDBCDetectorTest.java
+++ opennms-provision/opennms-jdbc-detector/src/test/java/org/opennms/netmgt/provision/detector/JDBCDetectorTest.java
@@ -1,12 +1,67 @@
 package org.opennms.netmgt.provision.detector;
 
+import static org.junit.Assert.*;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.opennms.netmgt.provision.support.NullDetectorMonitor;
 
 public class JDBCDetectorTest{
 	
+    //Tested detector on a MySQL database on local network.
+    
+    private JDBCDetector m_detector;
+    
+    @Before
+    public void setUp() {
+      m_detector = new JDBCDetector();
+    }
+    
 	@Test
-	public void emptyFIXMETest(){
-		
+	public void testDetectorSuccess() throws UnknownHostException{
+//		m_detector.setDbDriver("com.mysql.jdbc.Driver");
+//		m_detector.setUrl("jdbc:mysql://192.168.1.103:3306/test");
+//		m_detector.setUser("root");
+//		m_detector.setPassword("password1!");
+//		m_detector.init();
+//		
+//		assertTrue(m_detector.isServiceDetected(InetAddress.getByName("192.168.1.103"), new NullDetectorMonitor()));
 	}
+	
+//	@Test
+//    public void testDetectorFailWrongUser() throws UnknownHostException{
+//        m_detector.setDbDriver("com.mysql.jdbc.Driver");
+//        m_detector.setUrl("jdbc:mysql://192.168.1.103:3306/test");
+//        m_detector.setUser("roo");
+//        m_detector.setPassword("password1!");
+//        m_detector.init();
+//        
+//        assertFalse(m_detector.isServiceDetected(InetAddress.getByName("192.168.1.103"), new NullDetectorMonitor()));
+//    }
+//	
+//	@Test
+//    public void testDetectorFailWrongPassword() throws UnknownHostException{
+//        m_detector.setDbDriver("com.mysql.jdbc.Driver");
+//        m_detector.setUrl("jdbc:mysql://192.168.1.103:3306/test");
+//        m_detector.setUser("root");
+//        m_detector.setPassword("pass");
+//        m_detector.init();
+//        
+//        assertFalse(m_detector.isServiceDetected(InetAddress.getByName("192.168.1.103"), new NullDetectorMonitor()));
+//    }
+//	
+//	@Test
+//    public void testDetectorFailWrongUrl() throws UnknownHostException{
+//        m_detector.setDbDriver("com.mysql.jdbc.Driver");
+//        m_detector.setUrl("jdbc:mysql://localhost:3306/test");
+//        m_detector.setUser("root");
+//        m_detector.setPassword("password");
+//        m_detector.init();
+//        
+//        assertFalse(m_detector.isServiceDetected(InetAddress.getByName("192.168.1.103"), new NullDetectorMonitor()));
+//    }
 	
 }
