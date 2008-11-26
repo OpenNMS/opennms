827471ea011530b99c9c75fd2b3c7d4471990ad1
Donald Desloge <thedesloge@users.sourceforge.net>
2008-11-26 02:45:58 +0000

    * SshDetector update * DhcpDetector Update

---
+++ opennms-provision/opennms-ssh-detector/src/main/java/org/opennms/netmgt/provision/support/ssh/Ssh.java
@@ -0,0 +1,294 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2007-2008 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ * 
+ * 2008 Aug 11: Fixes for bug 2574
+ * 2007 Oct 03: Initial version
+ *
+ * Copyright (C) 2007-2008 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
+package org.opennms.netmgt.provision.support.ssh;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+import org.apache.log4j.Category;
+import org.opennms.core.utils.ThreadCategory;
+import org.opennms.netmgt.model.PollStatus;
+
+/**
+ * 
+ * @author <a href="mailto:ranger@opennms.org">Benjamin Reed</a>
+ */
+public class Ssh extends AbstractPoll {
+    
+    // SSH port is 22
+    public static final int DEFAULT_PORT = 22;
+
+    // Default to 1.99 (v1 + v2 support)
+    public static final String DEFAULT_CLIENT_BANNER = "SSH-1.99-OpenNMS_1.5";
+
+    protected int m_port = DEFAULT_PORT;
+    protected String m_username;
+    protected String m_password;
+    protected String m_banner = DEFAULT_CLIENT_BANNER;
+    protected String m_serverBanner = "";
+    protected InetAddress m_address;
+    protected Throwable m_error;
+    
+    private Socket m_socket = null;
+    private BufferedReader m_reader = null;
+    private OutputStream m_writer = null;
+
+    public Ssh() { }
+    
+    public Ssh(InetAddress address) {
+        setAddress(address);
+    }
+    
+    public Ssh(InetAddress address, int port) {
+        setAddress(address);
+        setPort(port);
+    }
+    
+    public Ssh(InetAddress address, int port, int timeout) {
+        setAddress(address);
+        setPort(port);
+        setTimeout(timeout);
+    }
+
+    /**
+     * Set the address to connect to.
+     * 
+     * @param address the address
+     */
+    public void setAddress(InetAddress address) {
+        m_address = address;
+    }
+ 
+    /**
+     * Get the address to connect to.
+     * @return the address
+     */
+    public InetAddress getAddress() {
+        return m_address;
+    }
+
+    /**
+     * Set the port to connect to.
+     * @param port the port
+     */
+    public void setPort(int port) {
+        m_port = port;
+    }
+    
+    /**
+     * Get the port to connect to.
+     * @return the port
+     */
+    public int getPort() {
+        if (m_port == 0) {
+            return 22;
+        }
+        return m_port;
+    }
+    
+    /**
+     * Set the username to connect as.
+     * @param username the username
+     */
+    public void setUsername(String username) {
+        m_username = username;
+    }
+    
+    /**
+     * Get the username to connect as.
+     * @return the username
+     */
+    public String getUsername() {
+        return m_username;
+    }
+    
+    /**
+     * Set the password to connect with.
+     * @param password the password
+     */
+    public void setPassword(String password) {
+        m_password = password;
+    }
+
+    /**
+     * Get the password to connect with.
+     * @return the password
+     */
+    public String getPassword() {
+        return m_password;
+    }
+
+    /**
+     * Set the banner string to use when connecting
+     * @param banner the banner
+     */
+    public void setClientBanner(String banner) {
+        m_banner = banner;
+    }
+
+    /**
+     * Get the banner string used when connecting
+     * @return the banner
+     */
+    public String getClientBanner() {
+        return m_banner;
+    }
+    
+    /**
+     * Get the SSH server version banner.
+     * @return the version string
+     */
+    public String getServerBanner() {
+        return m_serverBanner;
+    }
+
+    protected void setError(Throwable t) {
+        m_error = t;
+    }
+    
+    protected Throwable getError() {
+        return m_error;
+    }
+
+    /**
+     * Attempt to connect, based on the parameters which have been set in
+     * the object.
+     * 
+     * @return true if it is able to connect
+     * @throws InsufficientParametersException
+     */
+    protected boolean tryConnect() throws InsufficientParametersException {
+        if (getAddress() == null) {
+            throw new InsufficientParametersException("you must specify an address");
+        }
+
+        try {
+            m_socket = new Socket();
+            m_socket.setTcpNoDelay(true);
+            m_socket.connect(new InetSocketAddress(getAddress(), getPort()), getTimeout());
+            m_socket.setSoTimeout(getTimeout());
+            
+            m_reader = new BufferedReader(new InputStreamReader(m_socket.getInputStream()));
+            m_writer = m_socket.getOutputStream();
+
+            // read the banner
+            m_serverBanner = m_reader.readLine();
+
+            // write our own
+            m_writer.write((getClientBanner() + "\r\n").getBytes());
+
+            // then, disconnect
+            disconnect();
+
+            return true;
+        } catch (NumberFormatException e) {
+            log().debug("unable to parse server version", e);
+            setError(e);
+            disconnect();
+        } catch (Exception e) {
+            log().debug("connection failed", e);
+            setError(e);
+            disconnect();
+        }
+        return false;
+    }
+
+    protected void disconnect() {
+        if (m_writer != null) {
+            try {
+                m_writer.close();
+            } catch (IOException e) {
+                log().warn("error disconnecting output stream", e);
+            }
+        }
+        if (m_reader != null) {
+            try {
+                m_reader.close();
+            } catch (IOException e) {
+                log().warn("error disconnecting input stream", e);
+            }
+        }
+        if (m_socket != null) {
+            try {
+                m_socket.close();
+            } catch (IOException e) {
+                log().warn("error disconnecting socket", e);
+            }
+        }
+    }
+
+    public PollStatus poll(TimeoutTracker tracker) throws InsufficientParametersException {
+        tracker.startAttempt();
+        boolean isAvailable = tryConnect();
+        double responseTime = tracker.elapsedTimeInMillis();
+
+        PollStatus ps = PollStatus.unavailable();
+        
+        String errorMessage = "";
+        if (getError() != null) {
+            errorMessage = getError().getMessage();
+            ps.setReason(errorMessage);
+        }
+
+        if (isAvailable) {
+            ps = PollStatus.available(responseTime);
+        } else if (errorMessage.matches("^.*Authentication:.*$")) {
+            ps = PollStatus.unavailable("authentication failed");
+        } else if (errorMessage.matches("^.*java.net.NoRouteToHostException.*$")) {
+            ps = PollStatus.unavailable("no route to host");
+        } else if (errorMessage.matches("^.*(timeout: socket is not established|java.io.InterruptedIOException|java.net.SocketTimeoutException).*$")) {
+            ps = PollStatus.unavailable("connection timed out");
+        } else if (errorMessage.matches("^.*(connection is closed by foreign host|java.net.ConnectException).*$")) {
+            ps = PollStatus.unavailable("connection exception");
+        } else if (errorMessage.matches("^.*NumberFormatException.*$")) {
+            ps = PollStatus.unavailable("an error occurred parsing the server version number");
+        } else if (errorMessage.matches("^.*java.io.IOException.*$")) {
+            ps = PollStatus.unavailable("I/O exception");
+        }
+        
+        return ps;
+    }
+
+    private Category log() {
+        return ThreadCategory.getInstance(getClass());
+    }
+}
---
+++ opennms-provision/opennms-ssh-detector/src/main/java/org/opennms/netmgt/provision/support/ssh/TimeoutTracker.java
@@ -0,0 +1,170 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2008 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * Modifications:
+ *
+ * Copyright (C) 2008 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ *      OpenNMS Licensing       <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ */
+package org.opennms.netmgt.provision.support.ssh;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Helper class used for tracking retires and timeouts for ServiceMonitors.
+ * 
+ * @author <a href="mailto:ranger@opennms.org">Ben Reed</a>
+ * @author <a href="mailto:brozow@opennms.org">Mathew Brozowski</a>
+ *
+ */
+public class TimeoutTracker {
+
+    private int m_retry;
+    private long m_timeoutInNanos;
+    private long m_timeoutInMillis;
+    private long m_timeoutInSeconds;
+    private boolean m_strictTimeouts;
+    
+    private int m_attempt = 0;
+    private long m_nextRetryTimeNanos = -1L;
+    private long m_attemptStartTimeNanos = -1L;
+
+    public TimeoutTracker(Map parameters, int defaultRetry, int defaultTimeout) {
+        m_retry = ParameterMap.getKeyedInteger(parameters, "retry", defaultRetry);
+
+        // make sure the timeout is a least 10 millis
+        m_timeoutInMillis = Math.max(10L, ParameterMap.getKeyedInteger(parameters, "timeout", defaultTimeout));
+        m_timeoutInNanos = Math.max(10000000L, TimeUnit.NANOSECONDS.convert(m_timeoutInMillis, TimeUnit.MILLISECONDS));
+        m_timeoutInSeconds = Math.max(1L, TimeUnit.SECONDS.convert(m_timeoutInMillis, TimeUnit.SECONDS));
+
+
+        m_strictTimeouts = ParameterMap.getKeyedBoolean(parameters, "strict-timeout", false);
+        
+        resetAttemptStartTime();
+
+    }
+
+    public boolean shouldRetry() {
+        return m_attempt <= m_retry;
+    }
+    
+    public long getTimeoutInMillis() {
+        return m_timeoutInMillis;
+    }
+    
+    public long getTimeoutInSeconds() {
+        return m_timeoutInSeconds;
+    }
+    
+
+    public void reset() {
+        m_attempt = 0;
+        resetAttemptStartTime();
+    }
+
+    private void resetAttemptStartTime() {
+        m_attemptStartTimeNanos = -1L;
+    }
+
+    public void nextAttempt() {
+        m_attempt++;
+        resetAttemptStartTime();
+    }
+
+    public int getAttempt() {
+        return m_attempt;
+    }
+
+    public void startAttempt() {
+        long now = System.nanoTime();
+        while (m_strictTimeouts && now < m_nextRetryTimeNanos) {
+            sleep(m_nextRetryTimeNanos - now);
+            now = System.nanoTime();
+        }
+
+        m_attemptStartTimeNanos = System.nanoTime();
+        m_nextRetryTimeNanos = m_attemptStartTimeNanos + m_timeoutInNanos;
+
+    }
+
+    private void sleep(long nanos) {
+        long millis = nanos / 1000000L;
+        int remainingNanos = (int)(nanos % 1000000L);
+
+        try { Thread.sleep(millis, remainingNanos); } catch (InterruptedException e) {
+            // we ignore InterruptedExceptions
+        }
+    }
+
+    private void assertStarted() {
+        if (m_attemptStartTimeNanos < 0) {
+            throw new IllegalStateException("Failed to call startAttempt before requesting elapsedTime.. This is most likely a bug");
+        }
+    }
+    
+    public double elapsedTimeInMillis() {
+        return convertFromNanos(elapsedTimeNanos(), TimeUnit.MILLISECONDS);
+    }
+    
+    public long elapsedTimeNanos() {
+        long nanoTime = System.nanoTime();
+        assertStarted();
+        return nanoTime - m_attemptStartTimeNanos;
+    }
+    
+    public double elapsedTime(TimeUnit unit) {
+        return convertFromNanos(elapsedTimeNanos(), unit);
+    }
+
+    private double convertFromNanos(double nanos, TimeUnit unit) {
+        double nanosPerUnit = TimeUnit.NANOSECONDS.convert(1, unit);
+        return nanos/nanosPerUnit;
+    }
+    
+    @Override
+    public String toString() {
+        return new StringBuilder(64)
+            .append("timeout: ").append(getTimeoutInMillis()).append("ms")
+            .append(" retry: ").append(m_attempt).append(" of ").append(m_retry)
+            .toString();
+
+    }
+
+    public int getSoTimeout() {
+        return (int)getTimeoutInMillis();
+    }
+    
+    public int getConnectionTimeout() {
+        return (int)getTimeoutInMillis();
+    }
+    
+
+
+}
