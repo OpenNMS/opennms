adb03807b501545dae45513e988d4bc873b1cfe5
Benjamin Reed <rangerrick@users.sourceforge.net>
2008-12-01 22:12:52 +0000

    Merged revisions 11408-11410,11413 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ........
      r11408 | rangerrick | 2008-12-01 09:29:08 -0500 (Mon, 01 Dec 2008) | 1 line
      
      update documentation for fink package
    ........
      r11409 | dhustace | 2008-12-01 12:21:18 -0500 (Mon, 01 Dec 2008) | 1 line
      
      correct LinkUp/Down translation example.
    ........
      r11410 | dhustace | 2008-12-01 14:09:48 -0500 (Mon, 01 Dec 2008) | 1 line
      
      make translated linkdown/linkup events the default behaviour.
    ........
      r11413 | rangerrick | 2008-12-01 16:52:14 -0500 (Mon, 01 Dec 2008) | 1 line
      
      dave removed the example xml file, so remove the test as well
    ........

--- opennms-services/src/test/java/org/opennms/netmgt/config/WillItUnmarshalTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/config/WillItUnmarshalTest.java
@@ -456,10 +456,6 @@ public class WillItUnmarshalTest {
         unmarshalExample("translator-configuration.xml", EventTranslatorConfiguration.class);
     }
     @Test
-    public void testExampleTranslatorEventConfiguration() throws Exception {
-        unmarshalExample("OpenNMS.translator.events.xml", Events.class);
-    }
-    @Test
     public void testExampleHypericTranslatorConfiguration() throws Exception {
         unmarshalExample("hyperic-integration/translator-configuration.xml", EventTranslatorConfiguration.class);
     }
