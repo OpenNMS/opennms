cee772c9eacfa0d47ae838ef72001839810fc845
Jonathan Sartin <jsartin@users.sourceforge.net>
2008-12-15 22:54:03 +0000

    Merged revisions 11330,11349,11417-11418,11434,11450,11483-11484,11486,11518-11519,11526 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/feature-ticketer-improvements
    ........
      r11330 | jsartin | 2008-11-15 11:23:07 +0000 (Sat, 15 Nov 2008) | 1 line
      
      Add "failed" states for Alarm Trouble Ticket State and Plugin Exception.
    ........
      r11349 | jsartin | 2008-11-18 22:12:16 +0000 (Tue, 18 Nov 2008) | 1 line
      
      depends on 1.0.2-SNAPSHOT of opennms-ticketing
    ........
      r11417 | jsartin | 2008-12-02 22:43:15 +0000 (Tue, 02 Dec 2008) | 1 line
      
      First Checkin of working code with test
    ........
      r11418 | jsartin | 2008-12-03 12:07:29 +0000 (Wed, 03 Dec 2008) | 1 line
      
      Fix issues with update
    ........
      r11434 | jsartin | 2008-12-04 22:42:23 +0000 (Thu, 04 Dec 2008) | 1 line
      
      modify RunTestProperty and add properties file to opennms-base-assembly
    ........
      r11450 | jsartin | 2008-12-09 11:51:59 +0000 (Tue, 09 Dec 2008) | 1 line
      
      no longer depends on opennms-services
    ........
      r11483 | jsartin | 2008-12-11 15:01:12 +0000 (Thu, 11 Dec 2008) | 1 line
      
      mmmm. Comments
    ........
      r11484 | jsartin | 2008-12-11 15:07:51 +0000 (Thu, 11 Dec 2008) | 1 line
      
      add rt integration
    ........
      r11486 | jsartin | 2008-12-11 15:50:37 +0000 (Thu, 11 Dec 2008) | 1 line
      
      added opennms-integration-rt to base-assmebly
    ........
      r11518 | jsartin | 2008-12-15 13:55:36 +0000 (Mon, 15 Dec 2008) | 1 line
      
      misc fixes and have the webapp react sensibly to failed creates/updates/etc
    ........
      r11519 | jsartin | 2008-12-15 14:03:32 +0000 (Mon, 15 Dec 2008) | 1 line
      
      Add forgotten projects
    ........
      r11526 | jsartin | 2008-12-15 22:36:52 +0000 (Mon, 15 Dec 2008) | 1 line
      
      Remove clutter and annotate config files
    ........

---
+++ opennms-integration-rt/src/test/java/org/opennms/netmgt/ticketer/rt/RtTicketerPluginTest.java
@@ -0,0 +1,115 @@
+package org.opennms.netmgt.ticketer.rt;
+
+import java.io.File;
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.opennms.api.integration.ticketing.PluginException;
+import org.opennms.api.integration.ticketing.Ticket;
+
+public class RtTicketerPluginTest extends TestCase {
+    
+    /**
+     * Test Cases for RtTicketerPlugin
+     * 
+     * @author <a href="mailto:jonathan@opennms.org">Jonathan Sartin</a>
+     * 
+     */
+
+	DefaultRtConfigDao m_configDao;
+	
+	RtTicketerPlugin m_ticketer;
+	
+	Ticket m_ticket;
+	
+	String ticketID;
+	
+
+    /**
+     * Don't run this test unless the runRtTests property
+     * is set to "true".
+     */
+    @Override
+    protected void runTest() throws Throwable {
+        if (!isRunTest()) {
+            System.err.println("Skipping test '" + getName() + "' because system property '" + getRunTestProperty() + "' is not set to 'true'");
+            return;
+        }
+            
+        try {
+            System.err.println("------------------- begin "+getName()+" ---------------------");
+            super.runTest();
+        } finally {
+            System.err.println("------------------- end "+getName()+" -----------------------");
+        }
+    }
+
+    private boolean isRunTest() {
+        return Boolean.getBoolean(getRunTestProperty());
+    }
+
+    private String getRunTestProperty() {
+        return "runRtTests";
+    }
+
+	 @Override
+	 protected void setUp() throws Exception {
+
+	        System.setProperty("opennms.home", "src" + File.separatorChar + "test" + File.separatorChar + "opennms-home");
+
+	        System.out.println("src" + File.separatorChar + "test" + File.separatorChar + "opennms-home");
+
+	        m_ticketer = new RtTicketerPlugin();
+	        
+	        m_ticket = new Ticket();
+	        m_ticket.setState(Ticket.State.OPEN);
+	        m_ticket.setSummary("Ticket Summary for ticket: " + new Date());
+	        m_ticket.setDetails("First Article for ticket: " + new Date());
+	        m_ticket.setUser("root@localhost");
+	        
+	        
+	}
+	
+	public void testSaveAndGet() {
+
+
+		try {
+            m_ticketer.saveOrUpdate(m_ticket);
+            Ticket retrievedTicket = m_ticketer.get(m_ticket.getId());
+            assertTicketEquals(m_ticket, retrievedTicket);
+        } catch (PluginException e) {
+            fail("Something failed in the ticketer plugin");
+            e.printStackTrace();
+        }
+        
+	}
+	
+public void testUpdateAndGet() {
+             
+        try {
+            m_ticketer.saveOrUpdate(m_ticket);
+            Ticket savedTicket = m_ticketer.get(m_ticket.getId());
+            assertTicketEquals(m_ticket, savedTicket);
+            m_ticket.setState(Ticket.State.CLOSED);
+            m_ticketer.saveOrUpdate(m_ticket);
+            System.out.println("before update, ticket status was " + savedTicket.getState().toString());
+            Ticket updatedTicket =  m_ticketer.get(m_ticket.getId());
+            System.out.println("after update, ticket status was " + updatedTicket.getState().toString());
+            assertTicketEquals(m_ticket, updatedTicket);
+        } catch (PluginException e) {
+            fail("Something failed in the ticketer plugin");
+            e.printStackTrace();
+        }
+
+        
+    }
+		
+	 private void assertTicketEquals(Ticket existing, Ticket retrieved) {
+	        assertEquals(existing.getId(), retrieved.getId());
+	        assertEquals(existing.getState(), retrieved.getState());
+	        assertEquals(existing.getUser(), retrieved.getUser());
+	        assertEquals(existing.getSummary(), retrieved.getSummary());
+	 }
+	 
+}
