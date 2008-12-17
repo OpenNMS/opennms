f28b5684c28f22723f6c7d8f95e31ab4288f82e8
Benjamin Reed <rangerrick@users.sourceforge.net>
2008-12-17 20:15:09 +0000

    parameters are actually named properly; now to get rid of <parameters>

--- opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/ParameterMapAdaptor.java
+++ opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/ParameterMapAdaptor.java
@@ -1,36 +1,18 @@
 package org.opennms.netmgt.provision.persist;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-public class ParameterMapAdaptor extends XmlAdapter<JAXBPluginParameter[], Map<String,String>> {
-    @XmlElementWrapper(name="")
-    @XmlElement(name="parameter")
-    JAXBPluginParameter[] parms;
-    
+public class ParameterMapAdaptor extends XmlAdapter<JAXBPluginParameterWrapper, Map<String,String>> {
     @Override
-    public JAXBPluginParameter[] marshal(Map<String, String> v) throws Exception {
-        List<JAXBPluginParameter> pluginParms = new ArrayList<JAXBPluginParameter>();
-        for (Map.Entry<String,String> e : v.entrySet()) {
-            pluginParms.add(new JAXBPluginParameter(e));
-        }
-        parms = pluginParms.toArray(new JAXBPluginParameter[pluginParms.size()]);
-        return parms;
+    public JAXBPluginParameterWrapper marshal(Map<String, String> v) throws Exception {
+        return new JAXBPluginParameterWrapper(v);
     }
 
     @Override
-    public Map<String,String> unmarshal(JAXBPluginParameter[] v) throws Exception {
-        Map<String,String> m = new HashMap<String,String>();
-        for (JAXBPluginParameter p : v) {
-            m.put(p.getKey(), p.getValue());
-        }
-        return m;
+    public Map<String,String> unmarshal(JAXBPluginParameterWrapper w) throws Exception {
+        return w.getMap();
     }
 
 }
