4d0492e46f2dc53409940538309e6dfcf59009d4
Donald Desloge <thedesloge@users.sourceforge.net>
2008-12-17 19:41:33 +0000

    The detectors are now wired up into Spring

---
+++ opennms-provision/opennms-jmx-detectors/src/main/java/org/opennms/netmgt/provision/detector/jmx/JMXDetector.java
@@ -0,0 +1,38 @@
+package org.opennms.netmgt.provision.detector.jmx;
+
+import org.opennms.netmgt.provision.detector.jmx.client.JMXClient;
+import org.opennms.netmgt.provision.support.BasicDetector;
+import org.opennms.netmgt.provision.support.ClientConversation.ResponseValidator;
+import org.opennms.netmgt.provision.support.jmx.connectors.ConnectionWrapper;
+
+
+public abstract class JMXDetector extends BasicDetector<ConnectionWrapper, Integer>{
+
+    protected JMXDetector(int defaultPort, int defaultTimeout, int defaultRetries) {
+        super(defaultPort, defaultTimeout, defaultRetries);
+    }
+
+    
+    @Override
+    protected abstract JMXClient getClient();
+
+    
+    @Override
+    protected abstract void onInit();
+    
+    protected void expectBeanCount(ResponseValidator<Integer> bannerValidator) {
+        getConversation().expectBanner(bannerValidator);
+    }
+    
+    protected ResponseValidator<Integer> greatThan(final int count){
+        return new ResponseValidator<Integer>() {
+
+            public boolean validate(Integer response) throws Exception {
+                
+                return (response >= count);
+            }
+            
+        };
+    }
+	
+}
