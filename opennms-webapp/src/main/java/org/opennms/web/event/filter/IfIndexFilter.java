da44803ec7c939ba25fe1bc724b182eb0088d8ad
Antonio Russo <rssntn67@users.sourceforge.net>
2008-12-20 19:38:27 +0000

    Added IfIndexFiletr to filter on ifindex on events table for use in GUI. Modified the interface jsp page so that it is possible to view the event relted to specific interface.

---
+++ opennms-webapp/src/main/java/org/opennms/web/event/filter/IfIndexFilter.java
@@ -0,0 +1,80 @@
+//
+// This file is part of the OpenNMS(R) Application.
+//
+// OpenNMS(R) is Copyright (C) 2002-2003 The OpenNMS Group, Inc.  All rights reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included code and modified
+// code that was published under the GNU General Public License. Copyrights for modified 
+// and included code are below.
+//
+// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+// Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// For more information contact:
+//      OpenNMS Licensing       <license@opennms.org>
+//      http://www.opennms.org/
+//      http://www.opennms.com/
+//
+
+package org.opennms.web.event.filter;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/** Encapsulates all interface filtering functionality. */
+public class IfIndexFilter extends Object implements Filter {
+    public static final String TYPE = "ifindex";
+
+    protected int ifIndex;
+
+    public IfIndexFilter(int ifIndex) {
+        this.ifIndex = ifIndex;
+    }
+
+    public String getSql() {
+        return (" ifindex=" + this.ifIndex );
+    }
+    
+    public String getParamSql() {
+        return (" ifindex=?");
+    }
+    
+    public int bindParam(PreparedStatement ps, int parameterIndex) throws SQLException {
+    	ps.setInt(parameterIndex, this.ifIndex);
+    	return 1;
+    }
+
+    public String getDescription() {
+        return (TYPE + "=" + this.ifIndex);
+    }
+
+    public String getTextDescription() {
+        return this.getDescription();
+    }
+
+    public String toString() {
+        return ("<EventFactory.InterfaceFilter: " + this.getDescription() + ">");
+    }
+
+    public int getIfIndex() {
+        return (this.ifIndex);
+    }
+
+    public boolean equals(Object obj) {
+        return (this.toString().equals(obj.toString()));
+    }
+}
