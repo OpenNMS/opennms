09d9b66275708469615ee408ad8960c79ae4b696
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-01-06 22:11:18 +0000

    RESOLVED - issue WOTO-26: Create NodeCategoryPolicy to automatically assign nodes to categories

---
+++ opennms-provision/opennms-provision-persistence/src/test/java/org/opennms/netmgt/provision/persist/policies/NodeCategoryPolicyTest.java
@@ -0,0 +1,99 @@
+package org.opennms.netmgt.provision.persist.policies;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opennms.netmgt.dao.DatabasePopulator;
+import org.opennms.netmgt.dao.NodeDao;
+import org.opennms.netmgt.dao.db.JUnitTemporaryDatabase;
+import org.opennms.netmgt.dao.db.OpenNMSConfigurationExecutionListener;
+import org.opennms.netmgt.dao.db.TemporaryDatabaseExecutionListener;
+import org.opennms.netmgt.model.OnmsCategory;
+import org.opennms.netmgt.model.OnmsNode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({
+    OpenNMSConfigurationExecutionListener.class,
+    TemporaryDatabaseExecutionListener.class,
+    DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class,
+    TransactionalTestExecutionListener.class
+})
+@ContextConfiguration(locations={
+        "classpath:/META-INF/opennms/applicationContext-dao.xml",
+        "classpath:/META-INF/opennms/applicationContext-databasePopulator.xml"
+})
+
+@JUnitTemporaryDatabase()
+public class NodeCategoryPolicyTest {
+    @Autowired
+    private NodeDao m_nodeDao;
+
+    @Autowired
+    private DatabasePopulator m_populator;
+
+    private List<OnmsNode> m_nodes;
+    
+    @Before
+    public void setUp() {
+        m_populator.populateDatabase();
+        m_nodes = m_nodeDao.findAll();
+    }
+    
+    @Test
+    @Transactional
+    public void testMatchingLabel() {
+        NodeCategoryPolicy p = new NodeCategoryPolicy();
+        p.setParameter("label", "~^node1$");
+        p.setParameter("category", "PolicyTest");
+
+        List<OnmsNode> matchedNodes = matchPolicy(p, "1");
+        assertTrue(matchedNodes.get(0).getCategories().contains(new OnmsCategory("PolicyTest")));
+    }
+
+    @Test
+    @Transactional
+    public void testMatchingNothing() {
+        NodeCategoryPolicy p = new NodeCategoryPolicy();
+        p.setParameter("label", "~^wankerdoodle$");
+        p.setParameter("category", "PolicyTest");
+
+        List<OnmsNode> matchedNodes = matchPolicy(p, null);
+        assertEquals(0, matchedNodes.size());
+    }
+
+    private List<OnmsNode> matchPolicy(NodeCategoryPolicy p, String matchingId) {
+        OnmsNode o;
+        List<OnmsNode> populatedNodes = new ArrayList<OnmsNode>();
+        List<OnmsNode> matchedNodes = new ArrayList<OnmsNode>();
+        
+        for (OnmsNode node : m_nodes) {
+            System.err.println(node);
+            o = p.apply(node);
+            if (o != null) {
+                matchedNodes.add(o);
+            }
+            if (node.getNodeId().equals(matchingId)) {
+                populatedNodes.add(node);
+            }
+        }
+        
+        assertEquals(populatedNodes, matchedNodes);
+        return matchedNodes;
+    }
+
+}
