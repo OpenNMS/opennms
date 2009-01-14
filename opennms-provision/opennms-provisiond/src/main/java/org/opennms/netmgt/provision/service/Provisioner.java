9ff27a029d9574a158d9d7af0744b7e86976b0c8
Matt Brozowski <brozow@users.sourceforge.net>
2009-01-14 20:52:33 +0000

    combine Provisioner and BaseProvisioner

--- opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/Provisioner.java
+++ opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/Provisioner.java
@@ -1,47 +1,52 @@
-/*
- This file is part of the OpenNMS(R) Application.
-
- OpenNMS(R) is Copyright (C) 2002-2006 The OpenNMS Group, Inc.  All rights reserved.
- OpenNMS(R) is a derivative work, containing both original code, included code and modified
- code that was published under the GNU General Public License. Copyrights for modified 
- and included code are below.
-
- OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
-
- Modifications:
-
- 2007 Aug 25: Implement SpringServiceDaemon. - dj@opennms.org
- 2007 Jun 24: Use Java 5 generics. - dj@opennms.org
- 2006 May 11: Added Event parameter support for setting the URL and foreignSource
- 2004 Dec 27: Changed SQL_RETRIEVE_INTERFACES to omit interfaces that have been
-              marked as deleted.
- 2004 Feb 12: Rebuild the package to ip list mapping while a new discoveried interface
-              to be scheduled.
- 2003 Jan 31: Cleaned up some unused imports.
- 
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.                                                            
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-       
- For more information contact: 
-      OpenNMS Licensing       <license@opennms.org>
-      http://www.opennms.org/
-      http://www.opennms.com/
-
-*/
-
+//
+// This file is part of the OpenNMS(R) Application.
+//
+// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
+// OpenNMS(R) is a derivative work, containing both original code, included code and modified
+// code that was published under the GNU General Public License. Copyrights for modified 
+// and included code are below.
+//
+// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+//
+// Modifications:
+//
+// 2008 Mar 20: Remove System.err.println. - dj@opennms.org
+// 2007 Jun 24: Organize imports, use Java 5 generics. - dj@opennms.org
+//
+// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//
+// For more information contact:
+// OpenNMS Licensing       <license@opennms.org>
+//     http://www.opennms.org/
+//     http://www.opennms.com/
+//
 package org.opennms.netmgt.provision.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Category;
+import org.opennms.core.utils.ThreadCategory;
 import org.opennms.netmgt.EventConstants;
 import org.opennms.netmgt.daemon.SpringServiceDaemon;
 import org.opennms.netmgt.model.events.EventBuilder;
@@ -50,136 +55,285 @@ import org.opennms.netmgt.model.events.EventSubscriptionService;
 import org.opennms.netmgt.model.events.EventUtils;
 import org.opennms.netmgt.model.events.annotations.EventHandler;
 import org.opennms.netmgt.model.events.annotations.EventListener;
+import org.opennms.netmgt.provision.service.lifecycle.LifeCycleInstance;
+import org.opennms.netmgt.provision.service.lifecycle.LifeCycleRepository;
+import org.opennms.netmgt.provision.service.operations.NoOpProvisionMonitor;
+import org.opennms.netmgt.provision.service.operations.ProvisionMonitor;
 import org.opennms.netmgt.xml.event.Event;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.util.Assert;
 
 @EventListener(name="Provisiond:EventListener")
-public class Provisioner extends BaseProvisioner implements SpringServiceDaemon {
+public class Provisioner implements SpringServiceDaemon {
+    
+    public static final String NAME = "Provisioner";
+
+    private List<Object> m_providers;
+    private LifeCycleRepository m_lifeCycleRepository;
+    private ProvisionService m_provisionService;
+    private ScheduledExecutorService m_scheduledExecutor;
+    private final Map<Integer, ScheduledFuture<?>> m_scheduledNodes = new HashMap<Integer, ScheduledFuture<?>>();
+    private volatile Resource m_importResource;
+    private volatile EventSubscriptionService m_eventSubscriptionService;
+    private volatile EventForwarder m_eventForwarder;
+
+    private volatile TimeTrackingMonitor m_stats;
+    
 	
-	public static final String NAME = "Provisioner";
-
-	private volatile Resource m_importResource;
-	private volatile EventSubscriptionService m_eventSubscriptionService;
-	private volatile EventForwarder m_eventForwarder;
-	private volatile TimeTrackingMonitor m_stats;
-
-            
-	public void doImport() {
-	    doImport(null);
+	public void setProvisionService(ProvisionService provisionService) {
+	    m_provisionService = provisionService;
 	}
+	
+	public ProvisionService getProvisionService() {
+	    return m_provisionService;
+	}
+	
+	public void setScheduledExecutor(ScheduledExecutorService scheduledExecutor) {
+	    m_scheduledExecutor = scheduledExecutor;
+	}
+
+	public void setLifeCycleRepository(LifeCycleRepository lifeCycleRepository) {
+	    m_lifeCycleRepository = lifeCycleRepository;
+	}
+
+	public void setProviders(List<Object> providers) {
+	    m_providers = providers;
+	}
+	
+    public void start() throws Exception {
+        scheduleRescanForExistingNodes();
+    }
+	
+    public void afterPropertiesSet() throws Exception {
+        Assert.notNull(getProvisionService(), "provisionService property must be set");
+        Assert.notNull(m_scheduledExecutor, "scheduledExecutor property must be set");
+        Assert.notNull(m_lifeCycleRepository, "lifeCycleRepository property must be set");
+    }
+    
+    protected void scheduleRescanForExistingNodes() {        
+        List<NodeScanSchedule> schedules = m_provisionService.getScheduleForNodes();
         
+        checkNodeListForRemovals(schedules);
+        
+        for(NodeScanSchedule schedule : schedules) {
+            if(!m_scheduledNodes.containsKey(schedule.getNodeId())) {
+                addToScheduleQueue(schedule);
+            }else {
+                updateNodeScheduleInQueue(schedule);
+            }            
+        }
+        
+    }
+
+    protected Runnable nodeScanner(final NodeScanSchedule schedule) {
+        return new Runnable() {
+            public void run() {
+                System.out.println(String.format("Gotta write the node scan code for node %s", schedule.getNodeId()));
+            }
+        };
+    }
+    
+    //Helper functions for the schedule
+    protected void addToScheduleQueue(NodeScanSchedule schedule) {
+        m_scheduledNodes.put(schedule.getNodeId(), m_scheduledExecutor.scheduleWithFixedDelay(nodeScanner(schedule), schedule.getInitialDelay(), schedule.getScanInterval(), TimeUnit.MILLISECONDS));
+    }
+    
+    protected void updateNodeScheduleInQueue(NodeScanSchedule schedule) {
+        ScheduledFuture<?> scheduledFuture = m_scheduledNodes.get(schedule.getNodeId());
+        
+        if(!scheduledFuture.isDone() && !scheduledFuture.isCancelled()) {
+            scheduledFuture.cancel(true);
+            scheduledFuture = m_scheduledExecutor.scheduleWithFixedDelay(nodeScanner(schedule), schedule.getInitialDelay(), schedule.getScanInterval(), TimeUnit.MILLISECONDS);
+        }
+    }
+    
+    protected void removeNodeFromScheduleQueue(Integer nodeId) {
+        ScheduledFuture<?> scheduledFuture = m_scheduledNodes.remove(nodeId);
+        
+        if(scheduledFuture != null && !scheduledFuture.isDone()) {
+            scheduledFuture.cancel(true);
+        }
+        
+    }
+    
+    protected void removeFromScheduleQueue(List<Integer> nodeIds) {
+        for(Integer nodeId : nodeIds) {
+            removeNodeFromScheduleQueue(nodeId);
+        }
+    }
+    
+    /**
+     * @param schedules
+     */
+    protected void checkNodeListForRemovals(List<NodeScanSchedule> schedules) {
+        Set<Integer> keySet = m_scheduledNodes.keySet();
+        List<Integer> markedForDelete = new ArrayList<Integer>(); 
+        
+        for(int nodeId : keySet) {
+            boolean isDirty = false;
+            
+            for(NodeScanSchedule schedule : schedules) {
+                if(schedule.getNodeId() == nodeId) {
+                    isDirty = true;
+                }
+            }
+            
+            if(!isDirty) {
+                markedForDelete.add(nodeId);
+            }
+        }
+        
+        removeFromScheduleQueue(markedForDelete);
+    }
+   
+
+    public int getScheduleLength() {
+        return m_scheduledNodes.size();
+    }
+    //^ Helper functions for the schedule
+    
+    protected void importModelFromResource(Resource resource) throws Exception {
+    	importModelFromResource(null, resource, new NoOpProvisionMonitor());
+    }
+
+    protected void importModelFromResource(String foreignSource, Resource resource, ProvisionMonitor monitor)
+            throws Exception {
+        doImport(resource, monitor, new ImportManager(), foreignSource);
+    }
+
+    private void doImport(Resource resource, final ProvisionMonitor monitor,
+            ImportManager importManager, final String foreignSource) throws Exception {
+        
+        importManager.getClass();
+        
+        LifeCycleInstance doImport = m_lifeCycleRepository.createLifeCycleInstance("import", m_providers.toArray());
+        doImport.setAttribute("resource", resource);
+        doImport.setAttribute("foreignSource", foreignSource);
+        
+        doImport.trigger();
+        
+        doImport.waitFor();
+    }
+
+    public Category log() {
+    	return ThreadCategory.getInstance(getClass());
+	}
+
+    public void setImportResource(Resource resource) {
+        m_importResource = resource;
+    }
+
+    public Resource getImportResource() {
+        return m_importResource;
+    }
+
+    public EventSubscriptionService getEventSubscriptionService() {
+        return m_eventSubscriptionService;
+    }
+
+    public void setEventSubscriptionService(EventSubscriptionService eventManager) {
+    	m_eventSubscriptionService = eventManager;
+    }
+
+    public void setEventForwarder(EventForwarder eventForwarder) {
+        m_eventForwarder = eventForwarder;
+    }
+
+    public EventForwarder getEventForwarder() {
+        return m_eventForwarder;
+    }
+
+    public void doImport() {
+        doImport(null);
+    }
+
     /**
      * Begins importing from resource specified in model-importer.properties file or
      * in event parameter: url.  Import Resources are managed with a "key" called 
-     * "foreignSource" specified in the XML retreived by the resource and can be overridden 
+     * "foreignSource" specified in the XML retrieved by the resource and can be overridden 
      * as a parameter of an event.
      * @param event
      */
-	@EventHandler(uei=EventConstants.RELOAD_IMPORT_UEI)
+    @EventHandler(uei = EventConstants.RELOAD_IMPORT_UEI)
     public void doImport(Event event) {
         Resource resource = null;
         try {
             m_stats = new TimeTrackingMonitor();
             
-            resource = ((event != null && getEventUrl(event) != null) ? new UrlResource(getEventUrl(event)) : m_importResource); 
+            resource = ((event != null && getEventUrl(event) != null) ? new UrlResource(getEventUrl(event)) : getImportResource()); 
             String foreignSource = event == null ? null : getEventForeignSource(event); 
-
+    
             send(importStartedEvent(resource));
             
-
-			importModelFromResource(foreignSource, resource, m_stats);
-
-			log().info("Finished Importing: "+m_stats);
-
-			send(importSuccessEvent(m_stats, resource));
-
+    
+    		importModelFromResource(foreignSource, resource, m_stats);
+    
+    		log().info("Finished Importing: "+m_stats);
+    
+    		send(importSuccessEvent(m_stats, resource));
+    
         } catch (Exception e) {
             String msg = "Exception importing "+resource;
-			log().error(msg, e);
+    		log().error(msg, e);
             send(importFailedEvent((msg+": "+e.getMessage()), resource));
         }
     }
-    
+
     /**
      * @param e
      */
-    @EventHandler(uei=EventConstants.NODE_ADDED_EVENT_UEI)
+    @EventHandler(uei = EventConstants.NODE_ADDED_EVENT_UEI)
     public void handleNodeAddedEvent(Event e) {
         NodeScanSchedule scheduleForNode = getProvisionService().getScheduleForNode(new Long(e.getNodeid()).intValue());
         if (scheduleForNode != null) {
             addToScheduleQueue(scheduleForNode);
         }
     }
-    
 
     /**
      * @param e
      */
-    @EventHandler(uei=EventConstants.NODE_DELETED_EVENT_UEI)
+    @EventHandler(uei = EventConstants.NODE_DELETED_EVENT_UEI)
     public void handleNodeDeletedEvent(Event e) {
         removeNodeFromScheduleQueue(new Long(e.getNodeid()).intValue());
     }
-    
+
     private String getEventUrl(Event event) {
         return EventUtils.getParm(event, EventConstants.PARM_URL);
     }
-    
+
     public String getStats() { return (m_stats == null ? "No Stats Availabile" : m_stats.toString()); }
 
     private Event importSuccessEvent(TimeTrackingMonitor stats, Resource resource) {
-
+    
         return new EventBuilder( EventConstants.IMPORT_SUCCESSFUL_UEI, NAME )
             .addParam( EventConstants.PARM_IMPORT_RESOURCE, resource.toString() )
             .addParam( EventConstants.PARM_IMPORT_STATS, stats.toString() )
             .getEvent();
     }
-    
-	private void send(Event event) {
-        m_eventForwarder.sendNow(event);
+
+    private void send(Event event) {
+        getEventForwarder().sendNow(event);
     }
 
     private Event importFailedEvent(String msg, Resource resource) {
-
+    
         return new EventBuilder( EventConstants.IMPORT_FAILED_UEI, NAME )
             .addParam( EventConstants.PARM_IMPORT_RESOURCE, resource.toString() )
             .addParam( EventConstants.PARM_FAILURE_MESSAGE, msg )
             .getEvent();
     }
 
-	private Event importStartedEvent(Resource resource) {
-
-	    return new EventBuilder( EventConstants.IMPORT_STARTED_UEI, NAME )
+    private Event importStartedEvent(Resource resource) {
+    
+        return new EventBuilder( EventConstants.IMPORT_STARTED_UEI, NAME )
             .addParam( EventConstants.PARM_IMPORT_RESOURCE, resource.toString() )
             .getEvent();
     }
 
-    public void setImportResource(Resource resource) {
-        m_importResource = resource;
-    }
-
-	public EventSubscriptionService getEventSubscriptionService() {
-	    return m_eventSubscriptionService;
-	}
-
-	public void setEventSubscriptionService(EventSubscriptionService eventManager) {
-		m_eventSubscriptionService = eventManager;
-	}
-
-	public void setEventForwarder(EventForwarder eventForwarder) {
-	    m_eventForwarder = eventForwarder;
-	}
-	
-	public void afterPropertiesSet() throws Exception {
-	    super.afterPropertiesSet();
-	}
-
     protected String getEventForeignSource(Event event) {
         return EventUtils.getParm(event, EventConstants.PARM_FOREIGN_SOURCE);
-    }
-
-    public void start() throws Exception {
-        scheduleRescanForExistingNodes();
     }
 
 }
