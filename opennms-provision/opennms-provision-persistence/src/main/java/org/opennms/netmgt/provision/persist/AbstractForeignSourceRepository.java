1c20b356e79453d914cf084478c1a8eabe74f5f1
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-01-16 22:19:38 +0000

    implement filesystem foreign source repository, and marshaling/unmarshaling of OnmsResource and OnmsForeignSource objects

--- opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/AbstractForeignSourceRepository.java
+++ opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/AbstractForeignSourceRepository.java
@@ -3,32 +3,13 @@ package org.opennms.netmgt.provision.persist;
 import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
 
-public class AbstractForeignSourceRepository implements
-        ForeignSourceRepository {
+public abstract class AbstractForeignSourceRepository implements ForeignSourceRepository {
 
-    public OnmsRequisition createRequisition(Resource resource) {
+    public OnmsRequisition createRequisition(Resource resource) throws ForeignSourceRepositoryException {
         Assert.notNull(resource);
         OnmsRequisition r = new OnmsRequisition();
         r.loadResource(resource);
         return r;
-    }
-
-    public OnmsForeignSource get(String foreignSourceName) {
-        return null;
-    }
-
-    public OnmsRequisition getRequisition(String foreignSourceName) {
-        return null;
-    }
-
-    public OnmsRequisition getRequisition(OnmsForeignSource foreignSource) {
-        return null;
-    }
-
-    public void save(OnmsForeignSource foreignSource) {
-    }
-
-    public void save(OnmsRequisition requisition) {
     }
 
 }
