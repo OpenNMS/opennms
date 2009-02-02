ae32864c9c92a0624f352fdaf27abdb5baaf56cf
Guglielmo Incisa di Camerana <guic@users.sourceforge.net>
2009-02-02 10:44:20 +0000

    Remove unused methods and unuseful comments from rancid gui and cupport classes.

--- opennms-webapp/src/main/java/org/opennms/web/inventory/InventoryLayer.java
+++ opennms-webapp/src/main/java/org/opennms/web/inventory/InventoryLayer.java
@@ -67,105 +67,91 @@ public class InventoryLayer {
     
  
         
-    static public boolean hasRancidInfo(String nodeLabe){
-        return true;        
-    }
-    
-    static public Map<String, Object> getInventoryElement(String rancidName) throws RancidApiException{
-        
-        try {
-        
-            String group = "laboratorio";
-            
-            
-            RancidNode rn = RWSClientApi.getRWSRancidNode(_URL, group, rancidName);
-    
-            
-    
-            InventoryNode in = new InventoryNode(rn);
-            InventoryElement ie = new InventoryElement(in);
-            
-            ie.setElementName("Router1");
-            ie.setVendor("Cisco");
-            ie.setSysOid("1.1.1.1.1.1.1");
-            ie.setModelType("x123");
-            ie.setSerialNumber("17266577871");
-            ie.setProductPartNumber("21211212");
-            ie.setHardwareVersion("1.1");
-            ie.setRamSize(8);
-            ie.setNwRamSize(4);
-            ie.setElementId(4);
-            
-            Map<String, Object> nodeModel = new TreeMap<String, Object>();
-    //      Integer ii = Integer(ie.getElementId(4));
-    //      nodeModel.put("id", ii.toString());
-    
-            nodeModel.put("status", ie.getParent().getParent().getState());
-            nodeModel.put("name", ie.getElementName());
-            nodeModel.put("elementid", ie.getElementId());
-            nodeModel.put("vendor", ie.getVendor());
-            nodeModel.put("sysoid", ie.getSysOid());
-            nodeModel.put("modeltype", ie.getModelType());
-            nodeModel.put("serialnumber", ie.getSerialNumber());
-            nodeModel.put("productpartnumber", ie.getProductPartNumber());
-            nodeModel.put("hardwareversion", ie.getHardwareVersion());
-            nodeModel.put("ramsize", ie.getRamSize());        
-            nodeModel.put("nwramsize", ie.getNwRamSize());
-            nodeModel.put("group", group);;
-            nodeModel.put("url", _URL);
-
-    
-            
-            return nodeModel;
-        }
-        catch (RancidApiException e) {
-            throw e;
-        }
-    }
-    
-//    static public Map<String, Object> getInventoryNode2(String rancidName) throws RancidApiException{
+//    static public boolean hasRancidInfo(String nodeLabe){
+//        return true;        
+//    }
+//    
+//    static public Map<String, Object> getInventoryElement(String rancidName) throws RancidApiException{
 //        
 //        try {
 //        
 //            String group = "laboratorio";
-//            String url = "http://www.rionero.com/rws-current";
 //            
-//            RancidNode rn = RWSClientApi.getRWSRancidNode(url, group, rancidName);
+//            
+//            RancidNode rn = RWSClientApi.getRWSRancidNode(_URL, group, rancidName);
+//    
+//            
+//    
+//            InventoryNode in = new InventoryNode(rn);
+//            InventoryElement ie = new InventoryElement(in);
+//            
+//            ie.setElementName("Router1");
+//            ie.setVendor("Cisco");
+//            ie.setSysOid("1.1.1.1.1.1.1");
+//            ie.setModelType("x123");
+//            ie.setSerialNumber("17266577871");
+//            ie.setProductPartNumber("21211212");
+//            ie.setHardwareVersion("1.1");
+//            ie.setRamSize(8);
+//            ie.setNwRamSize(4);
+//            ie.setElementId(4);
+//            
+//            Map<String, Object> nodeModel = new TreeMap<String, Object>();
+//    //      Integer ii = Integer(ie.getElementId(4));
+//    //      nodeModel.put("id", ii.toString());
+//    
+//            nodeModel.put("status", ie.getParent().getParent().getState());
+//            nodeModel.put("name", ie.getElementName());
+//            nodeModel.put("elementid", ie.getElementId());
+//            nodeModel.put("vendor", ie.getVendor());
+//            nodeModel.put("sysoid", ie.getSysOid());
+//            nodeModel.put("modeltype", ie.getModelType());
+//            nodeModel.put("serialnumber", ie.getSerialNumber());
+//            nodeModel.put("productpartnumber", ie.getProductPartNumber());
+//            nodeModel.put("hardwareversion", ie.getHardwareVersion());
+//            nodeModel.put("ramsize", ie.getRamSize());        
+//            nodeModel.put("nwramsize", ie.getNwRamSize());
+//            nodeModel.put("group", group);;
+//            nodeModel.put("url", _URL);
+//
+//    
+//            
+//            return nodeModel;
+//        }
+//        catch (RancidApiException e) {
+//            throw e;
+//        }
+//    }
+//
+//    static public Map<String, Object> getInventoryNodeList(String rancidName) throws RancidApiException{
+//        
+//        try {
+//        
+//            String group = "laboratorio";
+//      
+//            
+//            RancidNode rn = RWSClientApi.getRWSRancidNode(_URL, group, rancidName);
 //    
 //    
 //            InventoryNode in = new InventoryNode(rn);
 //            
-//            //in.setVersionId("version1");
-//            Date dc = new Date();
-//            //in.setCreationDate(dc);
-//            in.setExpirationDate(dc);
-//            
-//            //software configuration
-//            in.setSoftwareVersion("1.1");
-//            in.setSoftwareImageUrl("http://rancid.net/softwareimage");
-//            
-//            //configuration
-//            in.setConfigurationUrl("http://rancid.net/configuration");
-//            in.setRootConfigurationUrl("http://rancid.net/rootconfiguration");
-//                        
-//            Map<String, Object> nodeModel = new TreeMap<String, Object>();
-//            
-//            List<InventoryNode> lista = new LinkedList();
-//            lista.add(in);
-////            InventoryElement i2 = new InventoryElement(in);
-////            i2.setElementName("Bridge2");
-////            lista.add(i2);
-////            InventoryElement i3 = new InventoryElement(in);
-////            i3.setElementName("Bridge3");
-////            lista.add(i3);
-////            InventoryElement i4 = new InventoryElement(in);
-////            i4.setElementName("Bridge4");
-////            lista.add(i4);
-////            in.setNodeElements(lista);
+//            List<InventoryElement> lista = new LinkedList();
+//            InventoryElement i1 = new InventoryElement(in);
+//            i1.setElementName("Bridge1");
+//            lista.add(i1);
+//            InventoryElement i2 = new InventoryElement(in);
+//            i2.setElementName("Bridge2");
+//            lista.add(i2);
+//            InventoryElement i3 = new InventoryElement(in);
+//            i3.setElementName("Bridge3");
+//            lista.add(i3);
+//            InventoryElement i4 = new InventoryElement(in);
+//            i4.setElementName("Bridge4");
+//            lista.add(i4);
+//            in.setNodeElements(lista);
 //
-//    //      Integer ii = Integer(ie.getElementId(4));
-//    //      nodeModel.put("id", ii.toString());
-//    
+//            
+//            Map<String, Object> nodeModel = new TreeMap<String, Object>();    
 //
 //            nodeModel.put("inventory", lista);
 //            return nodeModel;
@@ -174,199 +160,8 @@ public class InventoryLayer {
 //            throw e;
 //        }
 //    }
-   
+//   
 
-    static public Map<String, Object> getInventoryNodeList(String rancidName) throws RancidApiException{
-        
-        try {
-        
-            String group = "laboratorio";
-      
-            
-            RancidNode rn = RWSClientApi.getRWSRancidNode(_URL, group, rancidName);
-    
-    
-            InventoryNode in = new InventoryNode(rn);
-            
-            List<InventoryElement> lista = new LinkedList();
-            InventoryElement i1 = new InventoryElement(in);
-            i1.setElementName("Bridge1");
-            lista.add(i1);
-            InventoryElement i2 = new InventoryElement(in);
-            i2.setElementName("Bridge2");
-            lista.add(i2);
-            InventoryElement i3 = new InventoryElement(in);
-            i3.setElementName("Bridge3");
-            lista.add(i3);
-            InventoryElement i4 = new InventoryElement(in);
-            i4.setElementName("Bridge4");
-            lista.add(i4);
-            in.setNodeElements(lista);
-
-            
-            Map<String, Object> nodeModel = new TreeMap<String, Object>();
-    //      Integer ii = Integer(ie.getElementId(4));
-    //      nodeModel.put("id", ii.toString());
-    
-
-            nodeModel.put("inventory", lista);
-            return nodeModel;
-        }
-        catch (RancidApiException e) {
-            throw e;
-        }
-    }
-   
-//    static public RWSResourceList getRancidPage(String rancidName ){
-//        
-//        try {
-//        
-//        String url = "http://www.rionero.com/rws-current";
-//        
-//        //RancidNodeAggregate rna = RWSClientApi.getRancidNodeAggregate("http://www.rionero.com/rws-current",rancidName);
-//
-//        Map<String, Object> nodemodel = new TreeMap<String, Object>();
-//        RancidNode rn = RWSClientApi.getRancidNodeAggregate("http://www.rionero.com/rws-current",rancidName);
-//        nodemodel.put("aggregate", rna);
-//        nodemodel.put("groups",rna.getGroups());
-//        
-//        List<String> groupList = rna.getGroups();
-//        
-//        Iterator iter = groupList.iterator();
-//        
-//        String tmpg;
-//        
-//
-//        tmpg = (String)iter.next(); 
-//        nodemodel.put("group",tmpg);
-//            
-//        nodemodel.put("id",rna.getRancidAggregate().get(tmpg).getDeviceName());
-//        nodemodel.put("devicename",rna.getRancidAggregate().get(tmpg).getDeviceName());
-//        
-////        <th>Rancid Name</th>
-////        <th>${model.id}</th>
-////    </tr>
-////    <tr>
-////        <th>Device Name</th>
-////        <th>${model.devicename}</th>
-////    </tr>   
-////    <tr>
-////        <th>Device Type</th>
-////        <th>${model.devicetype}</th>
-////    </tr>
-////    <tr>
-////        <th>Comment</th>
-////        <th>${model.comment}</th>
-////    </tr>
-//        
-//        return nodemodel;
-//        
-//        
-//        //GROUPS
-//        
-////        //////////////////////////////
-////        //////////////////////////////
-////        //tutto con il rancid node aggregate
-////        
-////        List<String> lista = new ArrayList<String>();
-////        lista.add("laboratorio");
-////        lista.add("disasterrecovery");
-////        lista.add("lorenteggio");
-////        
-////        
-////        nodemodel.put("groups", lista);
-////        
-////        
-////        //Rancid basic info with first group of list
-////        
-////        RancidNode rn1 = RWSClientApi.getRWSRancidNode(url, (String)lista.get(0), rancidName);
-////        //      disasterrecovery
-////        RancidNode rn2 = RWSClientApi.getRWSRancidNode(url, (String)lista.get(0), rancidName); 
-////        //      lorenteggio
-////        RancidNode rn3 = RWSClientApi.getRWSRancidNode(url, (String)lista.get(0), rancidName); 
-////        
-////        //mostro solo i dati del nodo trovato nel primo gruppo
-////        nodeModel.put("status", rn1.getState());
-////        nodeModel.put("devicename", rn1.getDeviceName());
-////        nodeModel.put("devicetype", rn1.getDeviceType());
-////        nodeModel.put("comment", rn1.getComment());
-////        
-////        //Associated info
-////        InventoryNode in1 = new InventoryNode(rn1);
-////        InventoryNode in2 = new InventoryNode(rn1);
-////        InventoryNode in3 = new InventoryNode(rn1);
-////        
-////        rn2.setGroup("disasterrecovery");
-////        InventoryNode in4 = new InventoryNode(rn2);
-////        InventoryNode in5 = new InventoryNode(rn2);
-////        InventoryNode in6 = new InventoryNode(rn2);
-////        
-////        rn3.setGroup("lorenteggio");
-////        InventoryNode in7 = new InventoryNode(rn3);
-////        InventoryNode in8 = new InventoryNode(rn3);
-////        InventoryNode in9 = new InventoryNode(rn3);
-////        
-////        Date dc = new Date();
-////        in1.setExpirationDate(dc);
-////        in2.setExpirationDate(dc);
-////        in3.setExpirationDate(dc);
-////        in4.setExpirationDate(dc);
-////        in5.setExpirationDate(dc);
-////        in6.setExpirationDate(dc);
-////        in7.setExpirationDate(dc);
-////        in8.setExpirationDate(dc);
-////        in9.setExpirationDate(dc);
-////        in1.setSoftwareVersion("1.0");
-////        in2.setSoftwareVersion("1.1");
-////        in3.setSoftwareVersion("1.2");
-////        in4.setSoftwareVersion("2.0");
-////        in5.setSoftwareVersion("2.1");
-////        in6.setSoftwareVersion("2.2");
-////        in7.setSoftwareVersion("3.0");
-////        in8.setSoftwareVersion("3.1");
-////        in9.setSoftwareVersion("3.2");
-////        
-//////        in.setSoftwareImageUrl("http://rancid.net/softwareimage");
-//////        in.setConfigurationUrl("http://rancid.net/configuration");
-//////        in.setRootConfigurationUrl("http://rancid.net/rootconfiguration");
-////        
-////        
-////        
-////        HashMap<String, InventoryNode> node1 = new HashMap<String, InventoryNode>();
-////        HashMap<String, InventoryNode> node2 = new HashMap<String, InventoryNode>();
-////        HashMap<String, InventoryNode> node3 = new HashMap<String, InventoryNode>();
-////        
-////        node1.put("1.0", in1);
-////        node1.put("1.1", in2);
-////        node1.put("1.2", in3);
-////        rn1.setNodeVersions(node1);
-////        
-////        
-////        node2.put("2.0", in4);
-////        node2.put("2.1", in5);
-////        node2.put("2.2", in6);
-////        rn2.setNodeVersions(node2);
-////        
-////        node3.put("3.0", in7);
-////        node3.put("3.1", in8);
-////        node3.put("3.2", in9);
-////        rn3.setNodeVersions(node3);
-////        
-////        RancidNodeAggregate rna = new RancidNodeAggregate();
-////        rna.addRancidAggregate("laboratorio", rn1);
-////        rna.addRancidAggregate("disasterrecovery", rn2);
-////        rna.addRancidAggregate("lorenteggio", rn3);
-//        
-//        nodemodel.put("rancidnodeaggregate", rna);
-//        
-//        return rna;
-//        }
-//        catch (Exceptio e) {
-//            throw e;
-//        }
-//         
-//        
-//    }
     
     
    // NEW CODE
@@ -586,42 +381,3 @@ static public Map<String, Object> getRancidNodeList(String rancidName) throws Ra
        }
    }
 }
-
-//BACKUP CODE
-//// Inventory ListList<InventoryWrapper> wrinv = new ArrayList<InventoryWrapper>();id
-//RWSResourceList versList = RWSClientApi.getRWSResourceConfigList(url,group,rancidName);
-//
-//List<String> configlist = versList.getResource();
-//Iterator iter1 = configlist.iterator();
-//
-//RancidNode rn = RWSClientApi.getRWSRancidNodeInventory(url, group, rancidName); 
-//List<InventoryNode> invList = new ArrayList<InventoryNode>();
-//
-//List<InventoryWrapper> wrinv = new ArrayList<InventoryWrapper>(); 
-//
-////String vs;
-//InventoryNode in;
-//while (iter1.hasNext()){
-//  vs = (String)iter1.next();
-//  in = (InventoryNode)rn.getNodeVersions().get(vs);
-//  InventoryWrapper tmpw = new InventoryWrapper(vs, in.getExpirationDate(), group);
-//  wrinv.add(tmpw);
-//                
-////}
-//public static Map<String, Object> getUrl(){
-//    try {
-//        RWSConfigFactory.init();
-//    }
-//    catch (Exception e){
-//        e.printStackTrace();
-//    }
-//    
-//    rwsCfgFactory = RWSConfigFactory.getInstance();
-//    burl = rwsCfgFactory.getUrls();
-//    _URL = burl[0].getServer_url();
-//    
-//    Map<String, Object> nodeModel = new TreeMap<String, Object>(); 
-//    nodeModel.put("url", _URL);    
-//    return nodeModel;
-//
-//}
