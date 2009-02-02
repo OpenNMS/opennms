988f1c76cf66ef70a2f8f1367291a6cc9f93344b
Guglielmo Incisa di Camerana <guic@users.sourceforge.net>
2009-02-02 12:52:12 +0000

    Fix compilation error

--- opennms-webapp/src/main/java/org/opennms/web/inventory/InventoryLayer.java
+++ opennms-webapp/src/main/java/org/opennms/web/inventory/InventoryLayer.java
@@ -71,57 +71,57 @@ public class InventoryLayer {
 //        return true;        
 //    }
 //    
-//    static public Map<String, Object> getInventoryElement(String rancidName) throws RancidApiException{
-//        
-//        try {
-//        
-//            String group = "laboratorio";
-//            
-//            
-//            RancidNode rn = RWSClientApi.getRWSRancidNode(_URL, group, rancidName);
-//    
-//            
-//    
-//            InventoryNode in = new InventoryNode(rn);
-//            InventoryElement ie = new InventoryElement(in);
-//            
-//            ie.setElementName("Router1");
-//            ie.setVendor("Cisco");
-//            ie.setSysOid("1.1.1.1.1.1.1");
-//            ie.setModelType("x123");
-//            ie.setSerialNumber("17266577871");
-//            ie.setProductPartNumber("21211212");
-//            ie.setHardwareVersion("1.1");
-//            ie.setRamSize(8);
-//            ie.setNwRamSize(4);
-//            ie.setElementId(4);
-//            
-//            Map<String, Object> nodeModel = new TreeMap<String, Object>();
-//    //      Integer ii = Integer(ie.getElementId(4));
-//    //      nodeModel.put("id", ii.toString());
-//    
-//            nodeModel.put("status", ie.getParent().getParent().getState());
-//            nodeModel.put("name", ie.getElementName());
-//            nodeModel.put("elementid", ie.getElementId());
-//            nodeModel.put("vendor", ie.getVendor());
-//            nodeModel.put("sysoid", ie.getSysOid());
-//            nodeModel.put("modeltype", ie.getModelType());
-//            nodeModel.put("serialnumber", ie.getSerialNumber());
-//            nodeModel.put("productpartnumber", ie.getProductPartNumber());
-//            nodeModel.put("hardwareversion", ie.getHardwareVersion());
-//            nodeModel.put("ramsize", ie.getRamSize());        
-//            nodeModel.put("nwramsize", ie.getNwRamSize());
-//            nodeModel.put("group", group);;
-//            nodeModel.put("url", _URL);
-//
-//    
-//            
-//            return nodeModel;
-//        }
-//        catch (RancidApiException e) {
-//            throw e;
-//        }
-//    }
+    static public Map<String, Object> getInventoryElement(String rancidName) throws RancidApiException{
+        
+        try {
+        
+            String group = "laboratorio";
+            
+            
+            RancidNode rn = RWSClientApi.getRWSRancidNode(_URL, group, rancidName);
+    
+            
+    
+            InventoryNode in = new InventoryNode(rn);
+            InventoryElement ie = new InventoryElement(in);
+            
+            ie.setElementName("Router1");
+            ie.setVendor("Cisco");
+            ie.setSysOid("1.1.1.1.1.1.1");
+            ie.setModelType("x123");
+            ie.setSerialNumber("17266577871");
+            ie.setProductPartNumber("21211212");
+            ie.setHardwareVersion("1.1");
+            ie.setRamSize(8);
+            ie.setNwRamSize(4);
+            ie.setElementId(4);
+            
+            Map<String, Object> nodeModel = new TreeMap<String, Object>();
+    //      Integer ii = Integer(ie.getElementId(4));
+    //      nodeModel.put("id", ii.toString());
+    
+            nodeModel.put("status", ie.getParent().getParent().getState());
+            nodeModel.put("name", ie.getElementName());
+            nodeModel.put("elementid", ie.getElementId());
+            nodeModel.put("vendor", ie.getVendor());
+            nodeModel.put("sysoid", ie.getSysOid());
+            nodeModel.put("modeltype", ie.getModelType());
+            nodeModel.put("serialnumber", ie.getSerialNumber());
+            nodeModel.put("productpartnumber", ie.getProductPartNumber());
+            nodeModel.put("hardwareversion", ie.getHardwareVersion());
+            nodeModel.put("ramsize", ie.getRamSize());        
+            nodeModel.put("nwramsize", ie.getNwRamSize());
+            nodeModel.put("group", group);;
+            nodeModel.put("url", _URL);
+
+    
+            
+            return nodeModel;
+        }
+        catch (RancidApiException e) {
+            throw e;
+        }
+    }
 //
 //    static public Map<String, Object> getInventoryNodeList(String rancidName) throws RancidApiException{
 //        
