e152b774c28e17048182e462ef94d7ff3c88426c
Matt Brozowski <brozow@users.sourceforge.net>
2009-02-15 16:58:34 +0000

    temporarily fix namespace parsing so provision gui works

---
+++ opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/requisition/package-info.java
@@ -0,0 +1,4 @@
+@XmlSchema(namespace = "http://xmlns.opennms.org/xsd/config/model-import", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.opennms.netmgt.provision.persist.requisition;
+import javax.xml.bind.annotation.XmlSchema;
+
