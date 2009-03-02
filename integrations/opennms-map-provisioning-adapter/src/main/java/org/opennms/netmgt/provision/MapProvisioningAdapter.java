8aef3b08b905a5f5bd50e9b9a0e9c982a973c0e2
Antonio Russo <rssntn67@users.sourceforge.net>
2009-03-02 18:44:37 +0000

    Refactored the sendEvent method

--- integrations/opennms-map-provisioning-adapter/src/main/java/org/opennms/netmgt/provision/MapProvisioningAdapter.java
+++ integrations/opennms-map-provisioning-adapter/src/main/java/org/opennms/netmgt/provision/MapProvisioningAdapter.java
@@ -58,6 +58,7 @@ import org.opennms.netmgt.model.OnmsMapElement;
 import org.opennms.netmgt.model.OnmsNode;
 import org.opennms.netmgt.model.events.EventBuilder;
 import org.opennms.netmgt.model.events.EventForwarder;
+import org.opennms.netmgt.xml.event.Event;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -252,7 +253,8 @@ public class MapProvisioningAdapter implements ProvisioningAdapter, Initializing
     }
 
     private void sendAndThrow(int nodeId, Exception e) {
-        m_eventForwarder.sendNow(buildEvent(EventConstants.PROVISIONING_ADAPTER_FAILED, nodeId).addParam("reason", MESSAGE_PREFIX+e.getLocalizedMessage()).getEvent());
+        Event event = buildEvent(EventConstants.PROVISIONING_ADAPTER_FAILED, nodeId).addParam("reason", MESSAGE_PREFIX+e.getLocalizedMessage()).getEvent();
+        m_eventForwarder.sendNow(event);
         throw new ProvisioningAdapterException(MESSAGE_PREFIX, e);
     }
 
