c4564c4b6f753adc3908d23435c79c9768cd3617
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-03-06 22:05:43 +0000

    generate XSDs for JAXB stuff, and include it in the xsd assembly

---
+++ opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/foreignsource/package-info.java
@@ -0,0 +1,4 @@
+@XmlSchema(namespace = "http://xmlns.opennms.org/xsd/config/foreign-source", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.opennms.netmgt.provision.persist.foreignsource;
+import javax.xml.bind.annotation.XmlSchema;
+
