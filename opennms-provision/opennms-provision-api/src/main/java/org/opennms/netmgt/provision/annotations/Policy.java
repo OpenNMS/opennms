3ef2b44012331e5f6fe4b39425b690842dd0798a
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-03-11 00:55:53 +0000

    rearrange a bit, also make a policy annotation

---
+++ opennms-provision/opennms-provision-api/src/main/java/org/opennms/netmgt/provision/annotations/Policy.java
@@ -0,0 +1,12 @@
+package org.opennms.netmgt.provision.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value={ElementType.TYPE})
+@Retention(value=RetentionPolicy.RUNTIME)
+public @interface Policy {
+    String value();
+}
