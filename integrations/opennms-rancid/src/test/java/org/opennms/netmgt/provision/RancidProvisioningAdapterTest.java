9361b427832a66ba6ff3bf9e5b26acd78d33160a
Matt Brozowski <brozow@users.sourceforge.net>
2009-03-12 21:18:29 +0000

    ignore broken rancid tests

--- integrations/opennms-rancid/src/test/java/org/opennms/netmgt/provision/RancidProvisioningAdapterTest.java
+++ integrations/opennms-rancid/src/test/java/org/opennms/netmgt/provision/RancidProvisioningAdapterTest.java
@@ -37,7 +37,8 @@ import org.springframework.transaction.annotation.Transactional;
         "classpath:/META-INF/opennms/applicationContext-daemon.xml",
         "classpath:/META-INF/opennms/mockEventIpcManager.xml",
         "classpath:/META-INF/opennms/applicationContext-databasePopulator.xml",
-        "classpath:/META-INF/opennms/provisiond-extensions.xml"
+        "classpath:/META-INF/opennms/provisiond-extensions.xml",
+        "classpath:/rancidAdapterTestContext.xml"
         })
 @JUnitTemporaryDatabase()
 public class RancidProvisioningAdapterTest {
