d9077451c0c38e1ec944e3e0e849e784bcdacb9b
Donald Desloge <thedesloge@users.sourceforge.net>
2009-03-17 21:01:35 +0000

    Add All button on the choose resource to graph page now adds all resources to the graph bin

--- opennms-webapp/src/main/webapp/js/opennms/ux/LocalPageableProxy.js
+++ opennms-webapp/src/main/webapp/js/opennms/ux/LocalPageableProxy.js
@@ -1,8 +1,9 @@
 Ext.namespace("OpenNMS.ux");
-OpenNMS.ux.LocalPageableProxy = function(data, pagesize){
+OpenNMS.ux.LocalPageableProxy = function(data, pagesize, recordMap){
 	OpenNMS.ux.LocalPageableProxy.superclass.constructor.call(this);
     this.data = data;
     this.pagesize = pagesize;
+    this.recordMap = recordMap;
 };
 
 Ext.extend(OpenNMS.ux.LocalPageableProxy, Ext.data.DataProxy, {
@@ -48,6 +49,20 @@ Ext.extend(OpenNMS.ux.LocalPageableProxy, Ext.data.DataProxy, {
 		return Array.prototype.slice.call(iterable);
 
 		
+    },
+    
+    getAllData:function(){
+    	var DataRecord = Ext.data.Record.create(this.recordMap);
+    	
+    	var records = new Array();
+
+    	for each(dataObj in this.data.records){
+    		
+    		if(!(dataObj instanceof Function)){
+    			records.push(new DataRecord(dataObj));
+    		}
+    	}
+    	return records;
     }
     
 });
