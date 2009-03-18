1a47643233834d207918f1fd69ea7c54add9fec9
Guglielmo Incisa di Camerana <guic@users.sourceforge.net>
2009-03-18 14:25:00 +0000

    Iframe for viewVc on rancid gui

---
+++ opennms-webapp/src/main/java/org/opennms/web/controller/inventory/RancidViewVcController.java
@@ -0,0 +1,39 @@
+package org.opennms.web.controller.inventory;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.opennms.web.WebSecurityUtils;
+import org.opennms.web.acegisecurity.Authentication;
+import org.opennms.web.svclayer.inventory.InventoryService;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class RancidViewVcController implements Controller {
+
+    InventoryService m_inventoryService;
+    
+    public InventoryService getInventoryService() {
+        return m_inventoryService;
+    }
+
+    public void setInventoryService(InventoryService inventoryService) {
+        m_inventoryService = inventoryService;
+    }
+
+    public ModelAndView handleRequest(HttpServletRequest request,
+            HttpServletResponse arg1) throws Exception {
+       
+        String node = request.getParameter("node");
+        String iframelink = request.getParameter("viewvc");
+        int nodeid = WebSecurityUtils.safeParseInt(node);
+        Map<String, Object> model = m_inventoryService.getRancidNode(nodeid,request.isUserInRole(Authentication.ADMIN_ROLE));
+        model.put("iframelink", iframelink);
+        ModelAndView modelAndView = new ModelAndView("inventory/rancidViewVc","model",model);
+        return modelAndView;
+    }
+
+}
