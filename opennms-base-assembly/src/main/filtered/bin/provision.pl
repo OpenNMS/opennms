cca7ae2207d9099c5d99487ce29b8e730b93ce50
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-03-22 03:14:15 +0000

    work in progress, command-line interface to the provisioning group editor.  can add/remove requisitions and nodes, need to do categories, assets, and interfaces/services still

---
+++ opennms-base-assembly/src/main/filtered/bin/provision.pl
@@ -0,0 +1,471 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+provision.pl - Command-line interface to the provisioner
+
+=head1 SYNOPSIS
+
+provision.pl [options] command [arguments ...]
+
+=cut
+
+use Carp;
+use Data::Dumper;
+use File::Path;
+use Getopt::Long;
+use HTTP::Cookies;
+use HTTP::Request;
+use LWP;
+use LWP::UserAgent;
+use Pod::Usage;
+use URI;
+use URI::Escape;
+use XML::Twig;
+
+use vars qw(
+	$BUILD
+	$BROWSER
+	$XML
+
+	$print_help
+	$print_longhelp
+
+	$username
+	$password
+
+	$url_root
+);
+
+$BUILD = (qw$LastChangedRevision 1 $)[-1];
+$XML = XML::Twig->new();
+%XMLARGS = (
+	KeyAttr => {
+		'requisition' => 'foreign-source',
+	},
+	ForceArray => [
+		'asset',
+		'category',
+		'interface',
+		'node',
+		'requisition',
+	],
+);
+
+
+# set defaults
+$url_root = 'http://localhost:8980/opennms/rest';
+$username = 'admin';
+$password = 'admin';
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--help>
+
+Print a brief help message and exit.
+
+=item B<--version>
+
+Print the version and exit.
+
+=item B<--username>
+
+The username to use when connecting to the RESTful API.  This
+user must have administrative privileges in the OpenNMS web UI.
+
+Defaults to 'admin'.
+
+=item B<--password>
+
+The password associated with the administrative username specified
+in B<-username>.
+
+Defaults to 'password'.
+
+=item B<--url>
+
+The URL of the OpenNMS REST interface.  Defaults to
+'http://localhost:8980/opennms/rest'.
+
+=back
+
+=cut
+
+Getopt::Long::Configure( "require_order" );
+my $result = GetOptions(
+	"help|h"     => \$print_help,
+	"longhelp|l" => \$print_longhelp,
+	"version|v"  => \&print_version,
+
+	"username|u" => \$username,
+	"password|p" => \$password,
+
+	"url"        => \$url_root,
+);
+
+pod2usage(1) if $print_help;
+pod2usage(-exitstatus => 0, -verbose => 2) if $print_longhelp;
+
+set_up_environment();
+
+=head1 ARGUMENTS
+
+=over 8
+
+=cut
+
+my $command = shift(@ARGV);
+
+if (exists &{"cmd_$command"}) {
+	&{"cmd_$command"}(@ARGV);
+} else {
+	pod2usage(-exitval => 1, -message => "Unknown command: $command");
+}
+
+### commands ###
+
+=item B<list>
+
+List the available requisition foreign sources.
+
+=cut
+
+sub cmd_list {
+	my @args = @_;
+
+	my $path = shift @args || 'pending';
+
+	my $response = get($path);
+	dump_xml($response->content);
+}
+
+=item B<requisition>
+
+=over 8
+
+=item B<requisition add E<lt>foreign-sourceE<gt>>
+
+Add a requisition with the given foreign source.
+
+=item B<requisition remove E<lt>foreign-sourceE<gt>>
+
+Remove the requisition with the given foreign source.
+
+=item B<requisition import E<lt>foreign-sourceE<gt>>
+
+Import the requisition with the given foreign source.
+
+=back
+
+=cut
+
+sub cmd_requisition {
+	my @args = @_;
+
+	my $command        = shift @args;
+	my $foreign_source = shift @args;
+
+	if (not defined $foreign_source or $foreign_source eq "") {
+		pod2usage(-exitval => 1, -message => "Error: You must specify a foreign source!");
+	}
+
+	if ($command eq 'add' or $command eq 'create') {
+		my $xml = get_element('model-import');
+		my $root = $xml->root;
+		$root->{'att'}->{'foreign-source'} = $foreign_source;
+		post('pending', $root);
+	} elsif ($command eq 'remove' or $command eq 'delete' or $command eq 'del' or $command eq 'rm') {
+		remove('pending/' . $foreign_source);
+	} elsif ($command eq 'import' or $command eq 'deploy') {
+		put('pending/' . $foreign_source . '/deploy');
+	} else {
+		pod2usage(-exitval => 1, -message => "Unknown command: requisition $command");
+	}
+}
+
+=item B<node>
+
+=over 8
+
+=item B<node add E<lt>foreign-sourceE<gt> E<lt>foreign-idE<gt> E<lt>node-labelE<gt>>
+
+Add a node to the requisition identified by the given foreign source.
+
+=item B<node remove E<lt>foreign-sourceE<gt> E<lt>foreign-idE<gt>>
+
+Remove a node from the requisition identified by the given foreign source and foreign ID.
+
+=item B<node set E<lt>foreign-sourceE<gt> E<lt>foreign-idE<gt> B<E<lt>keyE<gt>> B<[value]>>
+
+Set a property on a node, given the foreign source and foreign id.  Valid properties are:
+
+=over 4
+
+=item * building
+
+=item * city
+
+=item * node-label
+
+=item * parent-foreign-id
+
+=item * parent-node-label
+
+=back
+
+=back
+
+=cut
+
+sub cmd_node {
+	my @args = @_;
+
+	my $command        = shift @args;
+	my $foreign_source = shift @args;
+	my $foreign_id     = shift @args;
+
+	if (not defined $foreign_source or $foreign_source eq "") {
+		pod2usage(-exitval => 1, -message => "Error: You must specify a foreign source!");
+	}
+	if (not defined $foreign_id or $foreign_id eq "") {
+		pod2usage(-exitval => 1, -message => "Error: You must specify a foreign id!");
+	}
+
+	if ($command eq 'add' or $command eq 'create') {
+		my $node_label = shift @args;
+		if (not defined $node_label or $node_label eq "") {
+			pod2usage(-exitval => 1, -message => "Error: You must specify a node label!");
+		}
+		my $xml = get_element('node');
+		my $root = $xml->root;
+		$root->{'att'}->{'foreign-id'} = $foreign_id;
+		$root->{'att'}->{'node-label'} = $node_label;
+		post("pending/$foreign_source/nodes", $root);
+	} elsif ($command eq 'remove' or $command eq 'delete' or $command eq 'del' or $command eq 'rm') {
+		remove('pending/' . $foreign_source . '/nodes/' . $foreign_id);
+	} elsif ($command eq 'set') {
+		my $key   = shift @args;
+		my $value = shift @args;
+
+		if (not defined $key or $key eq "") {
+			pod2usage(-exitval => 1, -message => "Error: You must specify a key!");
+		}
+
+		$key   = uri_escape_utf8($key);
+		$value = uri_escape_utf8($value);
+		put('pending/' . $foreign_source . '/nodes/' . $foreign_id, "$key=$value");
+	} else {
+		pod2usage(-exitval => 1, -message => "Unknown command: requisition $command");
+	}
+}
+
+sub get_element {
+	my $root = shift;
+
+	my $xml = XML::Twig->new();
+	$xml->set_root(XML::Twig::Elt->new($root));
+	return $xml;
+}
+
+sub set_up_environment {
+	$BROWSER = LWP::UserAgent->new(agent => "provision.pl/$BUILD");
+
+	mkpath($ENV{'HOME'} . '/.opennms');
+	$BROWSER->cookie_jar(
+		HTTP::Cookies->new(
+			file     => $ENV{'HOME'} . '/.opennms/rest-cookies.txt',
+			autosave => 1,
+		)
+	);
+
+	my $uri = URI->new($url_root);
+
+	$BROWSER->credentials(
+		$uri->host_port(),
+		'OpenNMS Realm',
+		$username => $password,
+	);
+}
+
+sub get {
+	my $path = shift;
+
+	my $response = $BROWSER->get( $url_root . '/requisitions/' . $path );
+	if ($response->is_success) {
+		return $response;
+	}
+	croak($response->status_line);
+}
+
+sub put {
+	my $path = shift;
+	my $arguments = shift;
+
+	my $put = HTTP::Request->new(PUT => $url_root . '/requisitions/' . $path );
+	$put->content_type('application/x-www-form-urlencoded');
+	$put->content($arguments);
+	my $response = $BROWSER->request($put);
+	if ($response->is_success) {
+		return $response;
+	}
+	croak($response->status_line);
+}
+
+sub remove {
+	my $path = shift;
+
+	my $delete = HTTP::Request->new(DELETE => $url_root . '/requisitions/' . $path );
+	my $response = $BROWSER->request($delete);
+	if ($response->is_success) {
+		return $response;
+	}
+	croak($response->status_line);
+}
+
+sub post {
+	my $path = shift;
+	my $twig = shift;
+
+	$twig->{'att'}->{'xmlns'} = 'http://xmlns.opennms.org/xsd/config/model-import';
+	my $post = HTTP::Request->new(POST => $url_root . '/requisitions/' . $path );
+	$post->content_type('application/xml');
+	$post->content($twig->sprint);
+	my $response = $BROWSER->request($post);
+	if ($response->is_success) {
+		return $response;
+	}
+	croak($response->status_line);
+}
+
+sub dump_xml {
+	my $content = shift;
+
+	$XML->parse($content);
+	dump_requisitions($XML->root);
+	$XML->flush;
+}
+
+sub dump_requisitions {
+	my $xml = shift;
+
+	for my $child ($xml->children) {
+		dump_requisition($child);
+	}
+}
+
+sub dump_requisition {
+	my $xml = shift;
+
+	print $xml->{'att'}->{'foreign-source'}, " (last updated: ", $xml->{'att'}->{'date-stamp'}, ")\n";
+	print "  * nodes:\n";
+	for my $node ($xml->children) {
+		dump_node($node);
+	}
+}
+
+sub dump_node {
+	my $node = shift;
+
+	print ("    * ", $node->{'att'}->{'node-label'}, " (foreign ID: ", $node->{'att'}->{'foreign-id'}, ")\n");
+	print ("      * parent: ", $node->{'att'}->{'parent-node-label'}, " (foreign ID: ", $node->{'att'}->{'parent-foreign-id'}, ")\n") if ($node->{'att'}->{'parent-node-label'} or $node->{'att'}->{'parent-foreign-id'});
+	print ("      * city: ", $node->{'att'}->{'city'}, "\n") if ($node->{'att'}->{'city'});
+	print ("      * building: ", $node->{'att'}->{'building'}, "\n") if ($node->{'att'}->{'building'});
+	print ("      * assets:\n") if ($node->descendants('asset'));
+	for my $asset ($node->descendants('asset')) {
+		dump_asset($asset);
+	}
+	print ("      * categories:\n") if ($node->descendants('category'));
+	for my $category ($node->descendants('category')) {
+		dump_category($category);
+	}
+	print ("      * interfaces:\n") if ($node->descendants('interface'));
+	for my $interface ($node->descendants('interface')) {
+		dump_interface($interface);
+	}
+}
+
+sub dump_asset {
+	my $asset = shift;
+	print "        * ", $asset->{'att'}->{'name'}, "=", $asset->{'att'}->{'value'}, "\n";
+}
+
+sub dump_category {
+	my $category = shift;
+	print "        * ", $category->{'att'}->{'name'}, "\n";
+}
+
+sub dump_interface {
+	my $interface = shift;
+
+	print("        * ", $interface->{'att'}->{'ip-addr'});
+	print(" (", $interface->{'att'}->{'descr'}, ")") if ($interface->{'att'}->{'descr'});
+	print "\n";
+	print("          * SNMP Primary: ", $interface->{'att'}->{'snmp-primary'}, "\n");
+	print("          * Status: ", $interface->{'att'}->{'status'}, "\n");
+}
+
+sub dump_hash {
+	my $hash   = shift;
+	my $indent = shift || -1;
+
+	if ($indent == -1) {
+		next if ($key eq "xmlns");
+		next if ($key eq "count");
+		print $hash->{'foreign-source'}, ' (', $hash->{'date-stamp'}, ")\n";
+	} else {
+
+	for my $key (sort keys %$hash) {
+		if (ref $hash->{$key}) {
+			dump_hash($hash->{$key}, $indent + 1);
+		} else {
+				print " " x $indent, $key, ": ", $hash->{$key}, "\n";
+			}
+		}
+	}
+}
+
+sub print_version {
+	printf("%s build %d\n", (split('/', $0))[-1], $BUILD);
+	exit 0;
+}
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> provides an interface to the RESTful API of the
+provisioner, available in OpenNMS 1.8 and higher.  It mimics the
+Requisition editor in the "Manage Provisioning Groups" portion
+of the administrative UI.
+
+=head1 AUTHOR
+
+Benjamin Reed <ranger@opennms.org>
+
+=head1 COPYRIGHT AND DISCLAIMER
+
+Copyright 2009, The OpenNMS Group, Inc.  All rights reserved.
+
+OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+For more information contact:
+
+	OpenNMS Licensing <license@opennms.org>
+
+=cut
