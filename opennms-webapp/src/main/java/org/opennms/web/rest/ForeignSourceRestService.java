0532f791596562cb62cee121c42d8b09b20527c6
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-03-24 12:45:36 +0000

    whups, didn't mean to commit that line

--- opennms-webapp/src/main/java/org/opennms/web/rest/ForeignSourceRestService.java
+++ opennms-webapp/src/main/java/org/opennms/web/rest/ForeignSourceRestService.java
@@ -57,7 +57,6 @@ import org.joda.time.Duration;
 import org.opennms.netmgt.dao.NodeDao;
 import org.opennms.netmgt.model.OnmsNodeList;
 import org.opennms.netmgt.provision.persist.ForeignSourceRepository;
-import org.opennms.netmgt.provision.persist.ForeignSourceService;
 import org.opennms.netmgt.provision.persist.StringIntervalPropertyEditor;
 import org.opennms.netmgt.provision.persist.foreignsource.DetectorCollection;
 import org.opennms.netmgt.provision.persist.foreignsource.DetectorWrapper;
@@ -147,9 +146,6 @@ public class ForeignSourceRestService extends OnmsRestService {
     @Qualifier("deployed")
     private ForeignSourceRepository m_deployedForeignSourceRepository;
 
-    @Autowired
-    ForeignSourceService m_foreignSourceService;
-    
     @Autowired
     private NodeDao m_nodeDao;
     
