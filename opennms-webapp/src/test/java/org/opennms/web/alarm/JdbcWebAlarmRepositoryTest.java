5f94bec049639ca1b90a9f2f7838cc694c9c4268
Donald Desloge <thedesloge@users.sourceforge.net>
2009-03-24 17:57:00 +0000

    Created a test for JdbcWebAlarmRepository

---
+++ opennms-webapp/src/test/java/org/opennms/web/alarm/JdbcWebAlarmRepositoryTest.java
@@ -0,0 +1,61 @@
+package org.opennms.web.alarm;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opennms.netmgt.dao.DatabasePopulator;
+import org.opennms.netmgt.dao.db.JUnitTemporaryDatabase;
+import org.opennms.netmgt.dao.db.OpenNMSConfigurationExecutionListener;
+import org.opennms.netmgt.dao.db.TemporaryDatabaseExecutionListener;
+import org.opennms.web.alarm.filter.AlarmCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({
+    OpenNMSConfigurationExecutionListener.class,
+    TemporaryDatabaseExecutionListener.class,
+    DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class,
+    TransactionalTestExecutionListener.class
+})
+@ContextConfiguration(locations= {"classpath:/META-INF/opennms/applicationContext-dao.xml",
+                                  "classpath:/jdbcWebAlarmRepositoryTest.xml"})
+@JUnitTemporaryDatabase()
+public class JdbcWebAlarmRepositoryTest{
+    
+    @Autowired
+    DatabasePopulator m_dbPopulator;
+    
+    @Autowired
+    WebAlarmRepository m_alarmRepo;
+    
+    @Before
+    public void setUp(){
+        m_dbPopulator.populateDatabase();
+    }
+    
+    @After
+    public void tearDown(){
+        
+    }
+   
+    @Test
+    public void TestCountMatchingAlarms(){
+        assertNotNull(m_alarmRepo);
+        
+        AlarmCriteria criteria = new AlarmCriteria();
+        int alamrs = m_alarmRepo.countMatchingAlarms(criteria);
+        
+        assertEquals(1, alamrs);
+    }
+}
