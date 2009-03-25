349ca09191a342b7135cee10907586f05497f077
Donald Desloge <thedesloge@users.sourceforge.net>
2009-03-25 19:55:28 +0000

    Update to JdbcWebEventRepositoryTest

--- opennms-webapp/src/test/java/org/opennms/web/event/JdbcWebEventRepositoryTest.java
+++ opennms-webapp/src/test/java/org/opennms/web/event/JdbcWebEventRepositoryTest.java
@@ -3,15 +3,19 @@ package org.opennms.web.event;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.Date;
+
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.netmgt.dao.DatabasePopulator;
 import org.opennms.netmgt.dao.db.JUnitTemporaryDatabase;
 import org.opennms.netmgt.dao.db.OpenNMSConfigurationExecutionListener;
 import org.opennms.netmgt.dao.db.TemporaryDatabaseExecutionListener;
+import org.opennms.web.event.filter.AcknowledgedByFilter;
 import org.opennms.web.event.filter.EventCriteria;
+import org.opennms.web.event.filter.EventIdFilter;
+import org.opennms.web.event.filter.SeverityFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
@@ -51,6 +55,53 @@ public class JdbcWebEventRepositoryTest {
         int event = m_eventRepo.countMatchingEvents(criteria);
         
         assertEquals(1, event);
+    }
+    
+    @Test
+    public void testCountMatchingEventsBySeverity(){
+        EventCriteria criteria = new EventCriteria(new SeverityFilter(3));
+        int[] matchingAlarms = m_eventRepo.countMatchingEventsBySeverity(criteria);
+        
+        assertEquals(8, matchingAlarms.length);
+    }
+    
+    @Test
+    public void testGetEvent(){
+        Event event = m_eventRepo.getEvent(1);
+        assertNotNull(event);
+    }
+    
+    @Test
+    public void testAcknowledgeUnacknowledgeMatchingAlarms(){
+        m_eventRepo.acknowledgeMatchingEvents("TestUser", new Date(), new EventCriteria(new EventIdFilter(1)));
+        
+        int matchingEventCount = m_eventRepo.countMatchingEvents(new EventCriteria(new AcknowledgedByFilter("TestUser")));
+        assertEquals(1, matchingEventCount);
+        
+        m_eventRepo.unacknowledgeMatchingEvents(new EventCriteria(new AcknowledgedByFilter("TestUser")));
+        
+        matchingEventCount = m_eventRepo.countMatchingEvents(new EventCriteria(new AcknowledgedByFilter("TestUser")));
+        assertEquals(0, matchingEventCount);
+    }
+    
+    @Test
+    public void testAcknowledgeUnacknowledgeAllAlarms(){
+        m_eventRepo.acknowledgeAll("TestUser", new Date());
+        
+        int matchingEventCount = m_eventRepo.countMatchingEvents(new EventCriteria(new AcknowledgedByFilter("TestUser")));
+        assertEquals(1, matchingEventCount);
+        
+        m_eventRepo.unacknowledgeAll();
+        
+        matchingEventCount = m_eventRepo.countMatchingEvents(new EventCriteria(new AcknowledgedByFilter("TestUser")));
+        assertEquals(0, matchingEventCount);
+    }
+    
+    @Test
+    public void testCountMatchingBySeverity(){
+        
+        int[] matchingEventCount = m_eventRepo.countMatchingEventsBySeverity(new EventCriteria(new SeverityFilter(3)));
+        assertEquals(8, matchingEventCount.length);
     }
 
 }
