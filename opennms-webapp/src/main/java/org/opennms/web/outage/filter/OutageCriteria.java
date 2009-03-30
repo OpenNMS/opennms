e3c178a43e6c1a7360ec53a33f93da596cccb119
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-03-30 17:54:34 +0000

    the incomplete outage repository stuff

---
+++ opennms-webapp/src/main/java/org/opennms/web/outage/filter/OutageCriteria.java
@@ -0,0 +1,56 @@
+package org.opennms.web.outage.filter;
+
+import org.opennms.web.outage.OutageFactory;
+import org.opennms.web.outage.OutageFactory.OutageType;
+import org.opennms.web.outage.OutageFactory.SortStyle;
+
+public class OutageCriteria {
+
+    public static interface OutageCriteriaVisitor<E extends Exception> {
+        public void visitOutageType(OutageType ackType) throws E; 
+        public void visitFilter(Filter filter) throws E;
+        public void visitSortStyle(SortStyle sortStyle) throws E;
+        public void visitLimit(int limit, int offset) throws E;
+    }
+
+    public static class BaseOutageCriteriaVisitor<E extends Exception> implements OutageCriteriaVisitor<E> {
+        public void visitOutageType(OutageType ackType) throws E { }
+        public void visitFilter(Filter filter) throws E { }
+        public void visitLimit(int limit, int offset) throws E { }
+        public void visitSortStyle(SortStyle sortStyle) throws E { }
+    }
+    
+    Filter[] m_filters = null;
+    SortStyle m_sortStyle = OutageFactory.DEFAULT_SORT_STYLE;
+    OutageType m_outageType = OutageType.CURRENT;
+    int m_limit = -1;
+    int m_offset = -1;
+    
+    public OutageCriteria(Filter... filters) {
+        this(filters, null, null, -1, -1);
+    }
+    
+    public OutageCriteria(Filter[] filters, SortStyle sortStyle, OutageType outageType, int limit, int offset) {
+        m_filters = filters;
+        m_sortStyle = sortStyle;
+        m_outageType = outageType;
+        m_limit = limit;
+        m_offset = offset;
+    }
+    
+    
+    public <E extends Exception> void visit(OutageCriteriaVisitor<E> visitor) throws E {
+        if (m_outageType != null) {
+            visitor.visitOutageType(m_outageType);
+        }
+        for(Filter filter : m_filters) {
+            visitor.visitFilter(filter);
+        }
+        if (m_sortStyle != null) {
+            visitor.visitSortStyle(m_sortStyle);
+        }
+        if (m_limit > 0 && m_offset > -1) {
+            visitor.visitLimit(m_limit, m_offset);
+        }
+    }
+}
