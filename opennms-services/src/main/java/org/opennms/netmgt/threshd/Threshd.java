8e4f39dcdc21f86cb1d1f9336a24098fda16d8f0
Matt Brozowski <brozow@users.sourceforge.net>
2009-03-31 16:21:32 +0000

    Bug 2960: add a check for null and logged warning -- looks like a misconfiguration

--- opennms-services/src/main/java/org/opennms/netmgt/threshd/Threshd.java
+++ opennms-services/src/main/java/org/opennms/netmgt/threshd/Threshd.java
@@ -436,6 +436,11 @@ public final class Threshd extends AbstractServiceDaemon {
                 // Initialize the thresholder with the service.
                 //
                 ServiceThresholder thresholder = this.getServiceThresholder(svcName);
+                if (thresholder == null) {
+                    // no thresholder exists for this service so go on to the next one
+                    log().warn("Unable to find a Thresholder for service "+svcName+"! But it is configured for Thresholding!");
+                    continue;
+                }
                 thresholder.initialize(tSvc, tSvc.getPropertyMap());
 
                 // Add new service to the thresholdable service list.
