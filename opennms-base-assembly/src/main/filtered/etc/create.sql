d5ef4f5effc391b9d2ee9a5e564a408da8311c30
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-04-20 19:06:36 +0000

    Merged revisions 12793-12794,12811,12832 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/feature-acls
    ........
      r12793 | dhustace | 2009-03-24 19:31:07 -0400 (Tue, 24 Mar 2009) | 1 line
      
      just getting stuff in so I can merge changes from trunk... this won't build
    ........
      r12794 | dhustace | 2009-03-24 19:32:03 -0400 (Tue, 24 Mar 2009) | 1 line
      
      missed one
    ........
      r12811 | dhustace | 2009-03-26 10:28:53 -0400 (Thu, 26 Mar 2009) | 1 line
      
      more low hangin spring security fruit
    ........
      r12832 | dhustace | 2009-03-27 12:19:10 -0400 (Fri, 27 Mar 2009) | 1 line
      
      working on the user model
    ........

--- opennms-base-assembly/src/main/filtered/etc/create.sql
+++ opennms-base-assembly/src/main/filtered/etc/create.sql
@@ -1,7 +1,7 @@
 --# create.sql -- SQL to build the initial tables for the OpenNMS Project
 --#
 --# Modifications:
---#
+--# 2009 Mar 27: Added Users, Groups tables
 --# 2009 Jan 28: Added Acks tables - david@opennms.org
 --# 2007 Apr 10: Added statistics report tables - dj@opennms.org
 --# 2006 Apr 17: Added pathOutage table
@@ -68,6 +68,11 @@ drop table statisticsReportData cascade;
 drop table resourceReference cascade;
 drop table statisticsReport cascade;
 drop table acks cascade;
+drop table users cascade;
+drop table groups cascade;
+drop table group_user cascade;
+drop table category_user cascade;
+drop table category_group cascade;
 
 drop sequence catNxtId;
 drop sequence nodeNxtId;
@@ -1178,17 +1183,17 @@ CREATE UNIQUE INDEX category_idx ON categories(categoryName);
 --# are no categories in the category table
 --##################################################################
 --# criteria: SELECT count(*) = 0 from categories
-insert into categories (categoryId, categoryName, categoryDescription) values (nextVal('catNxtId'), 'Routers', null);
+insert into categories values (nextVal('catNxtId'), 'Routers', null);
 --# criteria: SELECT count(*) = 0 from categories
-insert into categories (categoryId, categoryName, categoryDescription) values (nextVal('catNxtId'), 'Switches', null);
+insert into categories values (nextVal('catNxtId'), 'Switches', null);
 --# criteria: SELECT count(*) = 0 from categories
-insert into categories (categoryId, categoryName, categoryDescription) values (nextVal('catNxtId'), 'Servers', null);
+insert into categories values (nextVal('catNxtId'), 'Servers', null);
 --# criteria: SELECT count(*) = 0 from categories
-insert into categories (categoryId, categoryName, categoryDescription) values (nextVal('catNxtId'), 'Production', null);
+insert into categories values (nextVal('catNxtId'), 'Production', null);
 --# criteria: SELECT count(*) = 0 from categories
-insert into categories (categoryId, categoryName, categoryDescription) values (nextVal('catNxtId'), 'Test', null);
+insert into categories values (nextVal('catNxtId'), 'Test', null);
 --# criteria: SELECT count(*) = 0 from categories
-insert into categories (categoryId, categoryName, categoryDescription) values (nextVal('catNxtId'), 'Development', null);
+insert into categories values (nextVal('catNxtId'), 'Development', null);
 
 --########################################################################
 --# category_node table - Many-to-Many mapping table of categories to nodes
@@ -2069,6 +2074,117 @@ CREATE TABLE acks (
 create index ack_time_idx on acks(ackTime);
 create index ack_user_idx on acks(ackUser);
 
+--########################################################################
+--#
+--#  users table -- This table used for OpenNMS User security (replaces users.xml)
+--# 
+--#  userId             : Unique ID
+--#  name               : The user's full name
+--#  password           : The user's password (encrypted)
+--#  comments           : OpenNMS Admin's notes about this user account
+--#  contact            : Place holder for now until figure out something better
+--#  dutySchedule       : The user's duty schedule, same form as users.xml version for now
+--#  tuiPin             : Pager ID (not sure why it is called tuiPin
+--#  readOnly           : Indicates read-only account.  Only here temporarily 
+--#                     : to maintain backward compatiblity
+--########################################################################
+
+CREATE TABLE users (
+    id          varchar(16) not null,
+    name        varchar(64) not null,
+    password    varchar(64) not null,
+    comments    varchar(128),
+--these will probably change
+    contact     varchar(128),
+    dutySchedule    varchar(64),
+    tuiPin          varchar(32),
+    readOnly    boolean not null default false,
+    
+    constraint pk_users_id primary key (id)
+);
+
+--########################################################################
+--#
+--#  groups table -- This table used for OpenNMS Group security (replaces groups.xml)
+--# 
+--#  groupId            : Unique ID
+--#  comments           : OpenNMS Admin's notes about this group account
+--#  dutySchedule       : The groups's duty schedule, same form as groups.xml version for now
+--########################################################################
+
+CREATE TABLE groups (
+    id              varchar(16) not null,
+    comments        varchar(128),
+    dutySchedule    varchar(64),
+    
+    constraint pk_groups_id primary key (id)
+
+);
+
+
+--########################################################################
+--#
+--#  groups to user mapping table -- This table used for maintaining a many-to-many
+--#     relationship between users and groups
+--# 
+--#  groupId          : References foreign key in the groups table
+--#  userId           : References foreign key in the users table
+--########################################################################
+
+CREATE TABLE group_user (
+    groupId     varchar(16) not null,
+    userId      varchar(16) not null,
+    
+    constraint groupid_fkey1 foreign key (groupId) references groups (id) ON DELETE CASCADE,
+    constraint userid_fkey1 foreign key (userId) references users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX groupid_idx on group_user(groupId);
+CREATE INDEX userid_idx on group_user(userId);
+CREATE UNIQUE INDEX groupuser_unique_idx on group_user(groupId, userId);
+
+
+--########################################################################
+--#
+--#  categories to users mapping table -- This table used for maintaining a many-to-many
+--#     relationship between categories and users
+--# 
+--#  categoryId       : References foreign key in the groups table
+--#  userId           : References foreign key in the users table
+--########################################################################
+
+CREATE TABLE category_user (
+    categoryId  integer not null,
+    userId      varchar(16) not null,
+    
+    constraint categoryid_fkey2 foreign key (categoryId) references categories ON DELETE CASCADE,
+    constraint userid_fkey2 foreign key (userId) references users (id) ON DELETE CASCADE
+);
+
+CREATE INDEX catid_idx2 on category_user(categoryId);
+CREATE INDEX catuser_idx on category_user(userId);
+CREATE UNIQUE INDEX catuser_unique_idx on category_user(categoryId, userId);
+
+--########################################################################
+--#
+--#  categories to groups mapping table -- This table used for maintaining a many-to-many
+--#     relationship between categories and groups
+--# 
+--#  categoryId       : References foreign key in the groups table
+--#  groupId          : References foreign key in the users table
+--########################################################################
+
+CREATE TABLE category_group (
+    categoryId  integer not null,
+    groupId     varchar(16) not null,
+    
+    constraint categoryid_fkey2 foreign key (categoryId) references categories ON DELETE CASCADE,
+    constraint groupid_fkey2 foreign key (groupId) references groups (id) ON DELETE CASCADE
+);
+
+CREATE INDEX catid_idx3 on category_group(categoryId);
+CREATE INDEX catgroup_idx on category_group(groupId);
+CREATE UNIQUE INDEX catgroup_unique_idx on category_group(categoryId, groupId);
 
 --# Begin Quartz persistence tables
 
