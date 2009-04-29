df068655d848a1b5e8613ff007724c7e8144c844
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-04-29 20:36:12 +0000

    make sure extensions and docs are built when tagging/releasing

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ LC_ALL=C
 PATH=$PWD/maven/bin:$PATH
 export LANG LC_ALL PATH
 
-./build.sh -Ptog-sign -Darguments='-X -Droot.dir=${basedir} -Dmaven.test.skip.exec=true -DexcludePackageNames=org.opennms.netmgt.config.notificationCommands' "$@" release:perform
+./build.sh -Dtagging -Ptog-sign -Darguments='-Dtagging -Droot.dir=${basedir} -Dmaven.test.skip.exec=true -DexcludePackageNames=org.opennms.netmgt.config.notificationCommands' "$@" release:perform
