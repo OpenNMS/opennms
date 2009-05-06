823a60be54cefdd65b0177c0315d098a7163b242
Donald Desloge <thedesloge@users.sourceforge.net>
2009-05-06 15:41:51 +0000

    Added the DiskUsageDetector

--- opennms-provision/opennms-detector-radius/src/test/java/org/opennms/netmgt/provision/detector/RadiusAuthDetectorTest.java
+++ opennms-provision/opennms-detector-radius/src/test/java/org/opennms/netmgt/provision/detector/RadiusAuthDetectorTest.java
@@ -1,14 +1,43 @@
 package org.opennms.netmgt.provision.detector;
 
+import static org.junit.Assert.assertFalse;
+
+import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opennms.netmgt.provision.detector.radius.RadiusAuthDetector;
+import org.opennms.netmgt.provision.support.NullDetectorMonitor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-public class RadiusAuthDetectorTest{
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:/META-INF/opennms/detectors.xml"})
+public class RadiusAuthDetectorTest implements ApplicationContextAware{
+    
+    @Autowired
+    public RadiusAuthDetector m_detector;
+
+    @Before
+    public void setUp(){
+         
+    }
     
 	@Test
-	public void testMyDetector() throws UnknownHostException{
-		//assertTrue(m_detector.isServiceDetected(InetAddress.getByName("192.168.1.100"), new NullDetectorMonitor()));
+	public void testDetectorFail() throws UnknownHostException{
+	    m_detector.onInit();
+	    m_detector.setTimeout(1);
+		assertFalse(m_detector.isServiceDetected(InetAddress.getByName("192.168.1.100"), new NullDetectorMonitor()));
 	}
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        
+    }
 	
 }
