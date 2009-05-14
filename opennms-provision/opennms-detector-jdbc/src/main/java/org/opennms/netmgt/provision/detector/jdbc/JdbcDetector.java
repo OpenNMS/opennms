8e9940f807565278cc5a07d33fbb3b449abfec19
Donald Desloge <thedesloge@users.sourceforge.net>
2009-05-14 18:16:33 +0000

    fix for the JdbcDetectorTest, missed a test dependency

--- opennms-provision/opennms-detector-jdbc/src/main/java/org/opennms/netmgt/provision/detector/jdbc/JdbcDetector.java
+++ opennms-provision/opennms-detector-jdbc/src/main/java/org/opennms/netmgt/provision/detector/jdbc/JdbcDetector.java
@@ -30,28 +30,14 @@
  */
 package org.opennms.netmgt.provision.detector.jdbc;
 
-import org.opennms.netmgt.provision.detector.jdbc.client.JDBCClient;
-import org.opennms.netmgt.provision.detector.jdbc.request.JDBCRequest;
-import org.opennms.netmgt.provision.detector.jdbc.response.JDBCResponse;
-import org.opennms.netmgt.provision.support.BasicDetector;
-import org.opennms.netmgt.provision.support.Client;
-import org.opennms.netmgt.provision.support.ClientConversation.ResponseValidator;
-import org.opennms.netmgt.provision.support.jdbc.DBTools;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("prototype")
-public class JdbcDetector extends BasicDetector<JDBCRequest, JDBCResponse>{
+public class JdbcDetector extends AbstractJdbcDetector{
     
     private static final String DEFAULT_SERVICE_NAME = "JDBC";
-
-    private static int DEFAULT_PORT = 3306;
-    
-    private String m_dbDriver = DBTools.DEFAULT_JDBC_DRIVER;
-    private String m_user = DBTools.DEFAULT_DATABASE_USER;
-    private String m_password = DBTools.DEFAULT_DATABASE_PASSWORD;
-    private String m_url = DBTools.DEFAULT_URL;
     
     /**
      * Default constructor
@@ -60,72 +46,9 @@ public class JdbcDetector extends BasicDetector<JDBCRequest, JDBCResponse>{
         super(DEFAULT_SERVICE_NAME, DEFAULT_PORT);
     }
 
-    /**
-     * Constructor for creating a non-default service based on this protocol
-     * 
-     * @param serviceName
-     * @param port
-     */
-    protected JdbcDetector(String serviceName, int port) {
-        super(serviceName, port);
-    }
-
     @Override
     protected void onInit() {
         expectBanner(resultSetNotNull());
     }
-    
-    @Override
-    protected Client<JDBCRequest, JDBCResponse> getClient() {
-        JDBCClient client = new JDBCClient();
-        client.setDbDriver(getDbDriver());
-        client.setUser(getUser());
-        client.setPassword(getPassword());
-        client.setUrl(getUrl());
-        return client;
-    }
-    
-    protected ResponseValidator<JDBCResponse> resultSetNotNull(){
-        return new ResponseValidator<JDBCResponse>() {
-
-            public boolean validate(JDBCResponse response) throws Exception {
-                return response.resultSetNotNull();
-            }
-        };
-    }
-
-    public void setDbDriver(String dbDriver) {
-        m_dbDriver = dbDriver;
-    }
-
-    public String getDbDriver() {
-        return m_dbDriver;
-    }
-
-    public void setUser(String username) {
-        m_user = username;
-    }
-
-    public String getUser() {
-        return m_user;
-    }
-
-    public void setPassword(String password) {
-        m_password = password;
-    }
-
-    public String getPassword() {
-        return m_password;
-    }
-
-    public void setUrl(String url) {
-        m_url = url;
-    }
-
-    public String getUrl() {
-        return m_url;
-    }
-	
-    
     
 }
