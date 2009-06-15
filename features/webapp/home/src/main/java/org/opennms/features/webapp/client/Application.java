3fabb2e0209aa479648234eecce1efa394fe95e4
Donald Desloge <thedesloge@users.sourceforge.net>
2009-06-15 20:06:11 +0000

    moved some things aroung and renaming somethings

---
+++ features/webapp/home/src/main/java/org/opennms/features/webapp/client/Application.java
@@ -0,0 +1,23 @@
+package org.opennms.features.webapp.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Application
+    implements EntryPoint
+{
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad()
+  {
+     final Label label = new Label ( "gwt-maven-plugin Archetype :: Project org.opennms.features.webapp.org.opennms.features.webapp.home" );
+     RootPanel.get().add( label );
+  }
+}
