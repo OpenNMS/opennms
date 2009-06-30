9f4ad879ae528eb13656715dcbeef4028bb0ffb1
David Hustace <dhustace@users.sourceforge.net>
2009-06-30 14:48:07 +0000

    Added delete cascade for cleaning up alarms when a node is deleted from the DB and added note for Capsd to continue deleting alarms manually because of its notion of 'flagging' for deletion.

--- opennms-base-assembly/src/main/filtered/etc/create.sql
+++ opennms-base-assembly/src/main/filtered/etc/create.sql
@@ -976,7 +976,7 @@ create table alarms (
 	alarmID                 INTEGER, CONSTRAINT pk_alarmID PRIMARY KEY (alarmID),
 	eventUei                VARCHAR(256) NOT NULL,
 	dpName                  VARCHAR(12) NOT NULL,
-	nodeID                  INTEGER,
+	nodeID                  INTEGER, CONSTRAINT fk_alarms_nodeid FOREIGN KEY (nodeID) REFERENCES node (nodeID) ON DELETE CASCADE,
 	ipaddr                  VARCHAR(16),
 	serviceID               INTEGER,
 	reductionKey            VARCHAR(256),
