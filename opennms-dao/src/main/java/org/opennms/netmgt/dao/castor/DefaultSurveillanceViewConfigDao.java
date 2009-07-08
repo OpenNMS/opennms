9bcca4c77aee71f858706ff017f197fdee482161
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-07-08 18:06:15 +0000

    license updates: opennms-dao

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/castor/DefaultSurveillanceViewConfigDao.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/castor/DefaultSurveillanceViewConfigDao.java
@@ -1,23 +1,7 @@
-/*
+/*******************************************************************************
  * This file is part of the OpenNMS(R) Application.
  *
- * OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
- * OpenNMS(R) is a derivative work, containing both original code, included code and modified
- * code that was published under the GNU General Public License. Copyrights for modified
- * and included code are below.
- *
- * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
- *
- * Modifications:
- *
- * 2008 Feb 15: Pass the newly required description argument to our super's constructor. - dj@opennms.org
- * 2007 Apr 08: Switch to use Spring Resources instead of File for the
- *              configuration files, since this is what FileReloadContainer
- *              now uses. - dj@opennms.org
- * 2007 Apr 07: Reorganize a bit.  Close Readers in the same method that causes
- *              them to be opened as suggested by brozow@. - dj@opennms.org 
- *
- * Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+ * Copyright (C) 2007-2008 The OpenNMS Group, Inc.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -31,13 +15,21 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc.:
+ *
+ *      51 Franklin Street
+ *      5th Floor
+ *      Boston, MA 02110-1301
+ *      USA
  *
  * For more information contact:
- *      OpenNMS Licensing       <license@opennms.org>
+ *
+ *      OpenNMS Licensing <license@opennms.org>
  *      http://www.opennms.org/
  *      http://www.opennms.com/
- */
+ *
+ *******************************************************************************/
+
 package org.opennms.netmgt.dao.castor;
 
 import java.util.Map;
--- opennms-dao/src/test/java/org/opennms/netmgt/dao/support/DefaultResourceDaoTest.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/support/DefaultResourceDaoTest.java
@@ -1,20 +1,7 @@
-/*
+/*******************************************************************************
  * This file is part of the OpenNMS(R) Application.
  *
- * OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
- * OpenNMS(R) is a derivative work, containing both original code, included code and modified
- * code that was published under the GNU General Public License. Copyrights for modified
- * and included code are below.
- *
- * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
- *
- * Modifications:
- * 
- * 2008 Oct 22: Use new getResourceById/loadResourceById methods. - dj@opennms.org
- * 2007 Sep 09: Test getResourceById when the node doesn't exist. - dj@opennms.org
- * 2007 Apr 24: Fix tests and deduplicate node and ipInterface creation. - dj@opennms.org
- * 
- * Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
+ * Copyright (C) 2007-2009 The OpenNMS Group, Inc.  All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,13 +15,21 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * Foundation, Inc.:
+ *
+ *      51 Franklin Street
+ *      5th Floor
+ *      Boston, MA 02110-1301
+ *      USA
  *
  * For more information contact:
- *      OpenNMS Licensing       <license@opennms.org>
+ *
+ *      OpenNMS Licensing <license@opennms.org>
  *      http://www.opennms.org/
  *      http://www.opennms.com/
- */
+ *
+ *******************************************************************************/
+
 package org.opennms.netmgt.dao.support;
 
 import static org.easymock.EasyMock.expect;
