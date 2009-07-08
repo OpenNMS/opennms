cbbabd2abc21b08c0e0ef1faa76bf70cf2928809
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-07-08 17:23:43 +0000

    license updates: tests

--- tests/src/org/opennms/test/trapd.java
+++ tests/src/org/opennms/test/trapd.java
@@ -1,28 +1,35 @@
-//
-// Copyright (C) 2000 Shivakumar C. Patil <shivakumar.patil@stdc.com>
-//  
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-// 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software 
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-// 
-// Tab Size = 8
-//
-// trapd.java,v 1.1.1.1 2001/11/11 17:35:57 ben Exp
-//
-// Log:
-//	06/08/00 - Brian Weaver <weave@oculan.com>
-//		Commented and added file to CVS
-//
+/*******************************************************************************
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * Copyright (C) 2002-2003, 2008 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.:
+ *
+ *      51 Franklin Street
+ *      5th Floor
+ *      Boston, MA 02110-1301
+ *      USA
+ *
+ * For more information contact:
+ *
+ *      OpenNMS Licensing <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ *
+ *******************************************************************************/
+
 package org.opennms.test;
 
 import java.lang.*;
