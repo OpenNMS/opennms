d3f38db0f3c93712772ea5efe3c016377798f490
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-07-08 17:09:31 +0000

    license updates: opennms-provision

--- opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/RuntimePersistenceException.java
+++ opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/RuntimePersistenceException.java
@@ -1,3 +1,35 @@
+/*******************************************************************************
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * Copyright (C) 2009 The OpenNMS Group, Inc.  All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc.:
+ *
+ *      51 Franklin Street
+ *      5th Floor
+ *      Boston, MA 02110-1301
+ *      USA
+ *
+ * For more information contact:
+ *
+ *      OpenNMS Licensing <license@opennms.org>
+ *      http://www.opennms.org/
+ *      http://www.opennms.com/
+ *
+ *******************************************************************************/
+
 package org.opennms.netmgt.provision.persist;
 
 public class RuntimePersistenceException extends RuntimeException {
