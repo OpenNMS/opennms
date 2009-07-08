7f6d5efa0480eca7fbefd3a95b4cc2c41a99d5f4
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-07-08 20:55:46 +0000

    Merged revisions 13942,13945 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/trunk
    ........
      r13942 | thedesloge | 2009-07-08 14:58:43 -0400 (Wed, 08 Jul 2009) | 1 line
      
      added a util for converting a java string with " \n \t \r \b to show up correctly in the javascript in the graph page
    ........
      r13945 | thedesloge | 2009-07-08 15:59:39 -0400 (Wed, 08 Jul 2009) | 1 line
      
      updated the SNMPInterfacegrid, when searching on ifIndex it does not send the comparator contains
    ........

---
+++ opennms-webapp/src/test/java/org/opennms/web/JsStringConvertTest.java
@@ -0,0 +1,19 @@
+package org.opennms.web;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class JsStringConvertTest {
+    
+    @Test
+    public void testJsStringConversion(){
+        String convertStr = "a\\\"b\\tz\\n";
+        
+        String convertedStr = Util.convertToJsSafeString(convertStr);
+        
+        assertTrue("a\\\\\\\"b\\\\tz\\\\n".equals(convertedStr));
+    }
+}
