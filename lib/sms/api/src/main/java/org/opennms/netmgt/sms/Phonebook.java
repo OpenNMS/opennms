d56efeb1ff13d76f77f766440917e4fedf59005b
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-07-13 17:48:46 +0000

    added the Phonebook API and a simple implementation that uses a property file

---
+++ lib/sms/api/src/main/java/org/opennms/netmgt/sms/Phonebook.java
@@ -0,0 +1,15 @@
+package org.opennms.netmgt.sms;
+
+
+public interface Phonebook {
+
+    /**
+     * Get an SMS message target given an IP address.
+     * 
+     * @param address the IP address
+     * @return a string representing the SMS "to" (usually a phone number or SMS email address)
+     * @throws @{link PhonebookException}
+     */
+    String getTargetForAddress(String address) throws PhonebookException;
+
+}
