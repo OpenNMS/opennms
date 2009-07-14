5ba0aad3ddbcf62ae02d8590602edb264096049d
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-07-14 02:16:18 +0000

    fix unit test for new layout

--- opennms-webapp/src/test/java/org/opennms/web/rest/NodeRestServiceTest.java
+++ opennms-webapp/src/test/java/org/opennms/web/rest/NodeRestServiceTest.java
@@ -69,7 +69,7 @@ public class NodeRestServiceTest extends AbstractSpringJerseyRestTestCase {
         url += "/ICMP";
         sendPut(url, "status=A");
         xml = sendRequest(GET, url, 200);
-        assertTrue(xml.contains("<status>A</status>"));
+        assertTrue(xml.contains("status=\"A\""));
         sendRequest(DELETE, url, 200);
         sendRequest(GET, url, 204);
     }
@@ -79,7 +79,7 @@ public class NodeRestServiceTest extends AbstractSpringJerseyRestTestCase {
         createCategory();
         String url = "/nodes/1/categories";
         String xml = sendRequest(GET, url, 200);
-        assertTrue(xml.contains("<name>Routers</name>"));
+        assertTrue(xml.contains("name=\"Routers\""));
         url += "/Routers";
         sendPut(url, "description=My Equipment");
         xml = sendRequest(GET, url, 200);
@@ -161,10 +161,9 @@ public class NodeRestServiceTest extends AbstractSpringJerseyRestTestCase {
 
     private void createCategory() throws Exception {
         createNode();
-        String service = "<category>" +
-        "<name>Routers</name>" +
-        "<description>Core Routers</description>" +
-        "</category>";
+        String service = "<category name=\"Routers\">" +
+            "<description>Core Routers</description>" +
+            "</category>";
         sendPost("/nodes/1/categories", service);
     }
 
