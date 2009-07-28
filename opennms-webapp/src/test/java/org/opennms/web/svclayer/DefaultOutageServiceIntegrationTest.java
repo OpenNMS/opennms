7d9c264ad9a8fcd0662fc0eb8255b6b3b4bafbf0
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-07-28 22:13:24 +0000

    move the rwsconfig stuff into a separate module, fix tests to incorporate it properly

--- opennms-webapp/src/test/java/org/opennms/web/svclayer/DefaultOutageServiceIntegrationTest.java
+++ opennms-webapp/src/test/java/org/opennms/web/svclayer/DefaultOutageServiceIntegrationTest.java
@@ -70,7 +70,7 @@ public class DefaultOutageServiceIntegrationTest extends AbstractTransactionalTe
 		return new String[] {
 				"META-INF/opennms/applicationContext-dao.xml",
 				"org/opennms/web/svclayer/applicationContext-svclayer.xml",
-				"META-INF/opennms/component-dao.xml",
+				"classpath*:/META-INF/opennms/component-dao.xml",
                 "classpath:/META-INF/opennms/applicationContext-databasePopulator.xml"
 		};
 	}
