aed0b81f310c2969b2eedd9603fde7f61840fc58
David Hustace <dhustace@users.sourceforge.net>
2009-08-03 15:36:44 +0000

    attempt to reduce number of parse and replace calls.  Bug: http://bugzilla.opennms.org/show_bug.cgi?id=3276

--- opennms-services/src/main/java/org/opennms/netmgt/threshd/JdbcIfInfoGetter.java
+++ opennms-services/src/main/java/org/opennms/netmgt/threshd/JdbcIfInfoGetter.java
@@ -55,42 +55,8 @@ public class JdbcIfInfoGetter implements IfInfoGetter {
      * @see org.opennms.netmgt.threshd.IfInfoGetter#getIfInfoForNodeAndLabel(int, java.lang.String)
      */
     public Map<String, String> getIfInfoForNodeAndLabel(int nodeId, String ifLabel) {
-        // Get database connection
-        java.sql.Connection dbConn = null;
-        try {
-            dbConn = DataSourceFactory.getInstance().getConnection();
-        } catch (SQLException e) {
-            //log().error("checkIfDir: Failed getting connection to the database: " + e, e);
-            throw new UndeclaredThrowableException(e);
-        }
-    
-        // Make certain we close the connection
         Map<String, String> ifInfo = new HashMap<String, String>();
-        try {
-            ifInfo = IfLabel.getInterfaceInfoFromIfLabel(dbConn, nodeId, ifLabel);
-        } catch (SQLException e) {
-            /*
-             * Logging a warning message but processing will
-             * continue for
-             * this thresholding event, when the event is
-             * created it
-             * will be created with an interface value set
-             * to the primary
-             * SNMP interface address and an event source
-             * set to
-             * <datasource>:<ifLabel>.
-             */
-            //log().warn("Failed to retrieve interface info from database using ifLabel '" + ifLabel + "': " + e, e);
-        } finally {
-            // Done with the database so close the connection
-            try {
-                if (dbConn != null) {
-                    dbConn.close();
-                }
-            } catch (SQLException e) {
-                //log().info("checkIfDir: SQLException while closing database connection: " + e, e);
-            }
-        }
+        ifInfo = IfLabel.getInterfaceInfoFromIfLabel(nodeId, ifLabel);
         return ifInfo;
     }
 
