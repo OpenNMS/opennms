fae26cf6fc9fe2bae7a5e5c677b140e70f21d02e
Donald Desloge <thedesloge@users.sourceforge.net>
2009-08-03 22:59:29 +0000

    setup a gateway listener to add gateways to the service bundle

---
+++ opennms-tools/sms-reflector/service-publisher/src/main/java/org/opennms/sms/reflector/GatewayListener.java
@@ -0,0 +1,63 @@
+package org.opennms.sms.reflector;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.smslib.AGateway;
+import org.smslib.GatewayException;
+import org.smslib.Service;
+import org.smslib.TimeoutException;
+import org.smslib.AGateway.Protocols;
+import org.smslib.Service.ServiceStatus;
+import org.springframework.osgi.service.importer.OsgiServiceLifecycleListener;
+
+public class GatewayListener implements OsgiServiceLifecycleListener {
+	
+	private Service m_service;
+	
+	public void setService(Service service){
+		m_service = service;
+	}
+	
+	public void addGateway(AGateway gateway, Map properties){
+		if(m_service != null){
+			try{
+				if(m_service.getServiceStatus() == ServiceStatus.STARTED){
+					m_service.stopService();
+				}
+		        
+				m_service.addGateway(gateway);
+				m_service.startService();
+				
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	public void removeGateway(AGateway gateway, Map properties){
+		if(m_service != null){
+			try{
+				if(m_service.getServiceStatus() == ServiceStatus.STARTED){
+					m_service.stopService();
+				}
+				m_service.removeGateway(gateway);
+				m_service.startService();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+		}
+	}
+
+	public void bind(Object arg0, Map arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void unbind(Object arg0, Map arg1) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
