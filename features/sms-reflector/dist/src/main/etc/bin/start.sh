61628870c14d01c55d73bf4b8c40680f0fae7a89
Donald Desloge <thedesloge@users.sourceforge.net>
2009-09-03 15:19:43 +0000

    most packaging goodness

--- features/sms-reflector/dist/src/main/etc/bin/start.sh
+++ features/sms-reflector/dist/src/main/etc/bin/start.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+PROG_PATH=`dirname "$0"`
+WHAT_HOME=`cd "$PROG_PATH/.."; pwd`
+
+cat>runner.args<<EOF
+--downloadFeedback=false
+--log=NONE
+--vmOptions=-Dbundles.configuration.location=../../conf -Dsms.modemConfig.home=$WHAT_HOME
+--platform=equinox
+--repositories=file:../equinox
+scan-dir:../lib
+EOF
+
 exec java -jar pax-runner-0.17.0.jar
