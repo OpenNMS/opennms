581434559b81d30cd38b7ece912e81d29fb9cb9c
Donald Desloge <thedesloge@users.sourceforge.net>
2009-09-09 21:07:37 +0000

    forgot a needed property to set in the start script

--- features/sms-reflector/dist/src/main/etc/bin/start.sh
+++ features/sms-reflector/dist/src/main/etc/bin/start.sh
@@ -6,7 +6,7 @@ SMS_REFLECTOR_HOME=`cd "$PROG_PATH/.."; pwd`
 cat>runner.args<<EOF
 --downloadFeedback=false
 --log=NONE
---vmOptions=-Dbundles.configuration.location=$SMS_REFLECTOR_HOME/conf -Dsms.modemConfig.home=$SMS_REFLECTOR_HOME -Dsms.modemConfig.url=file://$SMS_REFLECTOR_HOME/modemConfig.properties -Dgnu.io.SerialPorts=/dev/ttyACM0:/dev/ttyACM1:/dev/ttyACM2:/dev/ttyACM3:/dev/ttyACM4:/dev/ttyACM5
+--vmOptions=-Dbundles.configuration.location=$SMS_REFLECTOR_HOME/conf -Dsms.modemConfig.home=$SMS_REFLECTOR_HOME -Dsms.modemConfig.url=file://$SMS_REFLECTOR_HOME/modemConfig.properties -Dgnu.io.SerialPorts=/dev/ttyACM0:/dev/ttyACM1:/dev/ttyACM2:/dev/ttyACM3:/dev/ttyACM4:/dev/ttyACM5 -Dsmslib.serial.polling=true
 --platform=equinox
 --repositories=file:../equinox
 scan-dir:../lib
