7b62b70400c8c6f48c02bcd7cb35449e39836ac8
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-09-30 15:53:14 +0000

    make thread-safe

--- features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/internal/config/SequenceConfigFactory.java
+++ features/sms-reflector/sms-monitor/src/main/java/org/opennms/sms/monitor/internal/config/SequenceConfigFactory.java
@@ -16,8 +16,6 @@ import org.opennms.sms.monitor.internal.SequenceException;
 public class SequenceConfigFactory {
 	private static SequenceConfigFactory m_singleton = null;
 	private JAXBContext m_context;
-	private Unmarshaller m_unmarshaller;
-	private Marshaller m_marshaller;
 
 	private SequenceConfigFactory() {
 	}
@@ -32,26 +30,35 @@ public class SequenceConfigFactory {
 	protected synchronized void initializeContext() throws JAXBException {
 		if (m_context == null) {
 			m_context = JAXBContext.newInstance(
-					MobileSequenceConfig.class,
-					SequenceSessionVariable.class,
-	    			SmsSequenceRequest.class,
-	    			UssdSequenceRequest.class,
-	    			SmsSequenceResponse.class,
-	    			UssdSequenceResponse.class,
-	    			SmsFromRecipientResponseMatcher.class,
-	    			SmsSourceMatcher.class,
-	    			TextResponseMatcher.class,
-	    			UssdSessionStatusMatcher.class
+				MobileSequenceConfig.class,
+				SequenceSessionVariable.class,
+				SmsSequenceRequest.class,
+				UssdSequenceRequest.class,
+				SmsSequenceResponse.class,
+				UssdSequenceResponse.class,
+				SmsFromRecipientResponseMatcher.class,
+				SmsSourceMatcher.class,
+				TextResponseMatcher.class,
+				UssdSessionStatusMatcher.class
 			);
-			m_marshaller = m_context.createMarshaller();
-			m_unmarshaller = m_context.createUnmarshaller();
-			
-	    	m_marshaller = m_context.createMarshaller();
-	    	m_marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-	        m_marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", new MobileSequenceNamespacePrefixMapper());
+		}
+	}
 
-	    	m_unmarshaller = m_context.createUnmarshaller();
-	    	m_unmarshaller.setSchema(null);
+	protected Marshaller getMarshaller() throws JAXBException {
+		synchronized(m_context) {
+			Marshaller m = m_context.createMarshaller();
+			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			m.setProperty("com.sun.xml.bind.namespacePrefixMapper", new MobileSequenceNamespacePrefixMapper());
+			return m;
+		}
+	}
+
+	protected Unmarshaller getUnmarshaller() throws JAXBException {
+		synchronized(m_context) {
+			Unmarshaller u = m_context.createUnmarshaller();
+			u.setSchema(null);
+			u.setEventHandler(new javax.xml.bind.helpers.DefaultValidationEventHandler());
+			return u;
 		}
 	}
 
@@ -59,9 +66,8 @@ public class SequenceConfigFactory {
 		try {
 			initializeContext();
 			Reader r = new StringReader(sequenceXml);
-			m_unmarshaller.setEventHandler(new javax.xml.bind.helpers.DefaultValidationEventHandler());
-			MobileSequenceConfig s = (MobileSequenceConfig)m_unmarshaller.unmarshal(r);
-	    	return s;
+			MobileSequenceConfig s = (MobileSequenceConfig)getUnmarshaller().unmarshal(r);
+			return s;
 		} catch (JAXBException e) {
 			throw new SequenceException("An error occurred reading the sequence.", e);
 		}
