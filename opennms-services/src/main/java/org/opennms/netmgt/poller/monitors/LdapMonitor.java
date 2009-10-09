a618328ecbee7c5dd92d338a4ad90c50b99d62d2
Jeff Gehlbach <jgehlbach@users.sourceforge.net>
2009-10-09 20:32:49 +0000

    Merged revisions 14985 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/1.6-testing
    ........
      r14985 | jgehlbach | 2009-10-09 16:25:43 -0400 (Fri, 09 Oct 2009) | 1 line
      
      Fix dumb syntax error in last commit (bug 3381)
    ........

--- opennms-services/src/main/java/org/opennms/netmgt/poller/monitors/LdapMonitor.java
+++ opennms-services/src/main/java/org/opennms/netmgt/poller/monitors/LdapMonitor.java
@@ -237,7 +237,7 @@ final public class LdapMonitor extends IPv4Monitor {
 
                     if (results != null && results.hasMore()) {
                         responseTime = tracker.elapsedTimeInMillis();
-                        if (log().isDebugEnabled()
+                        if (log().isDebugEnabled())
                                 log().debug("search yielded " + results.getCount() + " result(s)");
                         serviceStatus = PollStatus.SERVICE_AVAILABLE;
                     } else {
@@ -259,7 +259,7 @@ final public class LdapMonitor extends IPv4Monitor {
 
                 try {
                     lc.disconnect();
-                    if (log().isDebugEnabled()
+                    if (log().isDebugEnabled())
                             log().debug("disconected from LDAP server " + address + " on port " + ldapPort);
                 } catch (LDAPException e) {
                     log().debug(e);
