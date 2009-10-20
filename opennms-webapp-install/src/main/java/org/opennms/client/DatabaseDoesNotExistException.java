c25174803ae80edcb14f2359f4b4f0cccdf17ab2
Seth Leger <sethl3@users.sourceforge.net>
2009-10-20 18:34:48 +0000

    Added DatabaseDoesNotExistException that signals the UI that the "opennms" database hasn't been created yet.

---
+++ opennms-webapp-install/src/main/java/org/opennms/client/DatabaseDoesNotExistException.java
@@ -0,0 +1,8 @@
+package org.opennms.client;
+
+/**
+ * This exception is used to signal the webapp that the database has not yet
+ * been created in the database.
+ */
+public class DatabaseDoesNotExistException extends IllegalStateException {
+}
