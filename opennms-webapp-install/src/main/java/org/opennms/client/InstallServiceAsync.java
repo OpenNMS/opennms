f23004c1a7d421b342b35a58f76df013afb3233c
Seth Leger <sethl3@users.sourceforge.net>
2009-11-04 20:40:05 +0000

    Removed unnecessary throw statements from the async interface.

--- opennms-webapp-install/src/main/java/org/opennms/client/InstallServiceAsync.java
+++ opennms-webapp-install/src/main/java/org/opennms/client/InstallServiceAsync.java
@@ -10,9 +10,9 @@ public interface InstallServiceAsync {
 	public void resetOwnershipFilename(AsyncCallback<Void> callback);
 	public void isAdminPasswordSet(AsyncCallback<Boolean> callback);
 	public void setAdminPassword(String password, AsyncCallback<Void> callback);
-	public void getDatabaseConnectionSettings(AsyncCallback<DatabaseConnectionSettings> callback) throws IllegalStateException;
-	public void connectToDatabase(String driver, String dbName, String dbAdminUser, String dbAdminPassword, String dbAdminUrl, String dbNmsUser, String dbNmsPassword, String dbNmsUrl, AsyncCallback<Void> callback) throws IllegalStateException;
-	public void createDatabase(String driver, String dbName, String dbAdminUser, String dbAdminPassword, String dbAdminUrl, String dbNmsUser, String dbNmsPassword, AsyncCallback<Void> callback) throws IllegalStateException;
+	public void getDatabaseConnectionSettings(AsyncCallback<DatabaseConnectionSettings> callback);
+	public void connectToDatabase(String driver, String dbName, String dbAdminUser, String dbAdminPassword, String dbAdminUrl, String dbNmsUser, String dbNmsPassword, String dbNmsUrl, AsyncCallback<Void> callback);
+	public void createDatabase(String driver, String dbName, String dbAdminUser, String dbAdminPassword, String dbAdminUrl, String dbNmsUser, String dbNmsPassword, AsyncCallback<Void> callback);
 	// protected void setDatabaseConfig(String dbName, String user, String password, String driver, String url, String binaryDirectory, AsyncCallback<Void> callback);
 	public void getDatabaseUpdateLogs(int offset, AsyncCallback<List<LoggingEvent>> callback);
 	public void getDatabaseUpdateLogsAsStrings(AsyncCallback<List<String>> callback);
