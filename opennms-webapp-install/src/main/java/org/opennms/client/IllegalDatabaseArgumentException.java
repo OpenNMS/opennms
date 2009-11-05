205a8bd215bbdc96eded6ab92430cca855da2ee3
Seth Leger <sethl3@users.sourceforge.net>
2009-11-05 19:50:48 +0000

    Added new checked exception for server-side database settings check.

---
+++ opennms-webapp-install/src/main/java/org/opennms/client/IllegalDatabaseArgumentException.java
@@ -0,0 +1,11 @@
+package org.opennms.client;
+
+public class IllegalDatabaseArgumentException extends Exception {
+    public IllegalDatabaseArgumentException() {
+        super();
+    }
+
+    public IllegalDatabaseArgumentException(String string) {
+        super(string);
+    }
+}
