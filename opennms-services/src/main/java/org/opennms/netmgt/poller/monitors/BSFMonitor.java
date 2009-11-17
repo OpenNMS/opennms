1b6dbd4c62aa66670967ba4eff453d05fd378334
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-11-17 16:32:54 +0000

    Merged revisions 13490,13495,13664,13687,13690,13693-13695,13732,13781,13799-13801,14082,14267,14328 via svnmerge from https://opennms.svn.sourceforge.net/svnroot/opennms/opennms/branches/feature-new-plugins
    ........
      r13490 | jasonaras | 2009-05-27 19:55:39 -0400 (Wed, 27 May 2009) | 1 line
      
      first go at some simple jdbc pollers
    ........
      r13495 | jasonaras | 2009-05-29 14:51:31 -0400 (Fri, 29 May 2009) | 1 line
      
      have BgpSessionMonitor return the admin status in english, not as its int value
    ........
      r13664 | jasonaras | 2009-06-10 14:34:55 -0400 (Wed, 10 Jun 2009) | 1 line
      
      woops.. got this right this time
    ........
      r13687 | jasonaras | 2009-06-17 12:32:29 -0400 (Wed, 17 Jun 2009) | 1 line
      
      check in new http(s) monitor
    ........
      r13690 | jasonaras | 2009-06-17 17:00:31 -0400 (Wed, 17 Jun 2009) | 1 line
      
      complimentary capsd plugin + a little debug + proper retrun values... not 100% yet
    ........
      r13693 | jasonaras | 2009-06-17 19:06:16 -0400 (Wed, 17 Jun 2009) | 1 line
      
      this still needs more logging.. but checking it in for the night
    ........
      r13694 | jasonaras | 2009-06-18 00:01:37 -0400 (Thu, 18 Jun 2009) | 1 line
      
      first go at the BSFPoller..  compiles clean.. in theory does what it should.. will do the test bits tomorrow
    ........
      r13695 | jasonaras | 2009-06-18 00:07:31 -0400 (Thu, 18 Jun 2009) | 1 line
      
      that would help
    ........
      r13732 | jasonaras | 2009-06-19 19:38:17 -0400 (Fri, 19 Jun 2009) | 1 line
      
      commit to merge trunk into the branch
    ........
      r13781 | jasonaras | 2009-06-22 20:44:03 -0400 (Mon, 22 Jun 2009) | 1 line
      
      BSFMonitor works now, though jython does not play nice groovy does.  I have to do a bit more cleanup and testing before it is ready to roll.
    ........
      r13799 | jasonaras | 2009-06-23 18:34:53 -0400 (Tue, 23 Jun 2009) | 1 line
      
      a little clean up, I am going to call this good unless anyone has any thoughts
    ........
      r13800 | jasonaras | 2009-06-23 20:03:25 -0400 (Tue, 23 Jun 2009) | 1 line
      
      Off by one... and it works
    ........
      r13801 | jasonaras | 2009-06-23 20:25:17 -0400 (Tue, 23 Jun 2009) | 1 line
      
      a little clean up.  rs.first() should always return 1... by default lets check for one or more rows in the Monitor.
    ........
      r14082 | jasonaras | 2009-07-22 18:44:42 -0400 (Wed, 22 Jul 2009) | 1 line
      
      both JDBCQuery(Monitor|Plugin) and Web(Monitor|Plugin) are running in a prod env... not all features tested of the Web* stuff.. anyone want to lend a hand?
    ........
      r14267 | jasonaras | 2009-08-05 18:20:50 -0400 (Wed, 05 Aug 2009) | 1 line
      
      check for response text that is not a regex and a straight up match
    ........
      r14328 | jasonaras | 2009-08-10 19:39:38 -0400 (Mon, 10 Aug 2009) | 1 line
      
      check for exact text match
    ........

---
+++ opennms-services/src/main/java/org/opennms/netmgt/poller/monitors/BSFMonitor.java
@@ -0,0 +1,85 @@
+package org.opennms.netmgt.poller.monitors;
+
+import java.util.Map;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.bsf.util.IOUtils;
+import org.apache.bsf.BSFException;
+import org.apache.bsf.BSFManager;
+
+import org.opennms.netmgt.model.PollStatus;
+import org.opennms.netmgt.poller.Distributable;
+import org.opennms.netmgt.poller.DistributionContext;
+import org.opennms.netmgt.poller.MonitoredService;
+import org.opennms.core.utils.ParameterMap;
+
+
+// this might actually be able to pushed out to the remote poller....  though prob not so easy out of the box
+@Distributable(DistributionContext.DAEMON)
+
+public class BSFMonitor extends IPv4Monitor {
+    
+    @SuppressWarnings("unchecked")
+    
+    public PollStatus poll(MonitoredService svc, Map map) {
+        BSFManager bsfManager = new BSFManager();
+        PollStatus pollStatus = PollStatus.unavailable();
+        String fileName = ParameterMap.getKeyedString(map,"file-name", null);
+        String lang = ParameterMap.getKeyedString(map, "lang-class", null);
+        String langEngine = ParameterMap.getKeyedString(map, "bsf-engine", null);
+        String langExtensions[] = ParameterMap.getKeyedString(map, "file-extensions", "").split(",");
+        File file = new File(fileName);
+
+        try {
+           
+            if(lang==null)
+                lang = BSFManager.getLangFromFilename(fileName);
+                
+            if(langEngine!=null && lang!=null && langExtensions.length > 0 ){
+                BSFManager.registerScriptingEngine(lang,langEngine,langExtensions);
+            }
+            
+            if(file.exists() && file.canRead()){   
+                    String code = IOUtils.getStringFromReader(new FileReader(file));
+                    String status = new String();
+                    
+                    bsfManager.declareBean("map", map, Map.class);
+                    bsfManager.declareBean("ip_addr",svc.getIpAddr(),String.class);
+                    bsfManager.declareBean("node_id",svc.getNodeId(),int.class );
+                    bsfManager.declareBean("node_label", svc.getNodeLabel(), String.class);
+                    bsfManager.declareBean("svc_name", svc.getSvcName(), String.class);
+        
+                    for(Object obj : map.keySet()){
+                        bsfManager.declareBean(obj.toString(),map.get(obj),String.class);
+                    }
+                    
+                    status = (String) bsfManager.eval(lang, "BSFMonitor", 0, 0, code).toString();
+                    
+                    if(status=="OK"){
+                        pollStatus = PollStatus.available();
+                    } else {
+                        pollStatus = PollStatus.unavailable(status);
+                    }
+                   
+            } else {
+                pollStatus = PollStatus.unavailable("can not locate or read file: " + fileName);
+            }            
+
+        } catch (BSFException e) {
+            pollStatus = PollStatus.unavailable(e.toString());
+        } catch (FileNotFoundException e){
+           pollStatus = PollStatus.unavailable("Could not find file: " + fileName);
+        } catch (IOException e) {
+            pollStatus = PollStatus.unavailable(e.toString());
+        } finally { 
+            bsfManager.terminate();
+        }
+
+        return pollStatus;
+    }
+    
+}
