0f83c2eb2db4579d0e1f0c3f61934ab665faf9a1
Seth Leger <sethl3@users.sourceforge.net>
2009-12-07 21:40:17 +0000

    Fixed GwtApplicationTestCase so that it uses delayTestFinish()/finishTest() to properly execute the GWT RPC calls inside the unit test.

--- opennms-webapp-install/src/test/java/org/opennms/client/GwtApplicationTestCase.java
+++ opennms-webapp-install/src/test/java/org/opennms/client/GwtApplicationTestCase.java
@@ -1,9 +1,9 @@
 package org.opennms.client;
 
-import java.io.File;
+// import java.io.File;
 import java.io.IOException;
 
-import org.apache.log4j.Logger;
+// import org.apache.log4j.Logger;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.junit.client.GWTTestCase;
@@ -18,41 +18,51 @@ public class GwtApplicationTestCase extends GWTTestCase {
     public void testOwnershipFile() {
         final InstallServiceAsync installService = (InstallServiceAsync)GWT.create(InstallService.class);
 
+        // Add a delay to allow the async call to complete
+        this.delayTestFinish(10000);
+
         installService.checkOwnershipFileExists(new AsyncCallback<Boolean>() {
-            public void onSuccess(Boolean arg0) {
-                fail("Unexpected success when checking ownership file.");
+            public void onSuccess(Boolean result) {
+                if (result) {
+                    fail("Unexpected success when checking ownership file.");
+                } else {
+                    installService.getOwnershipFilename(new AsyncCallback<String>() {
+                        public void onSuccess(String result) {
+                            // Touch the ownership file
+                            /*
+                            File pwn = new File(new File("target"), result);
+                            Logger.getLogger(this.getClass()).info("Creating file: " + pwn.getPath());
+                            try {
+                                pwn.createNewFile();
+                            } catch (IOException e) {
+                                fail("Exception thrown while creating ownership file: " + e.getMessage());
+                            }
+                            Logger.getLogger(this.getClass()).info("File created: " + pwn.getPath());
+                            */
+
+                            installService.checkOwnershipFileExists(new AsyncCallback<Boolean>() {
+                                public void onSuccess(Boolean result) {
+                                    assertTrue("File does not exist!", result);
+                                    // Logger.getLogger(this.getClass()).info("File check succeeded");
+                                    finishTest();
+                                    return;
+                                }
+
+                                public void onFailure(Throwable e) {
+                                    fail("Unexpected failure when checking ownership file.");
+                                }
+                            });
+                        }
+
+                        public void onFailure(Throwable e) {
+                            fail("Unexpected failure when retrieving ownership filename.");
+                        }
+                    });
+                }
             }
 
-            public void onFailure(Throwable arg0) {
-                installService.getOwnershipFilename(new AsyncCallback<String>() {
-                    public void onSuccess(String result) {
-                        // Touch the ownership file
-                        File pwn = new File(new File("target"), result);
-                        Logger.getLogger(this.getClass()).info("Creating file: " + pwn.getPath());
-                        try {
-                            pwn.createNewFile();
-                        } catch (IOException e) {
-                            fail("Exception thrown while creating ownership file: " + e.getMessage());
-                        }
-                        Logger.getLogger(this.getClass()).info("File created: " + pwn.getPath());
-
-                        installService.checkOwnershipFileExists(new AsyncCallback<Boolean>() {
-                            public void onSuccess(Boolean result) {
-                                assertTrue("File does not exist!", result);
-                                Logger.getLogger(this.getClass()).info("File check succeeded");
-                                return;
-                            }
-
-                            public void onFailure(Throwable e) {
-                                fail("Unexpected failure when checking ownership file.");
-                            }
-                        });
-                    }
-
-                    public void onFailure(Throwable arg0) {
-                        fail("Unexpected failure when retrieving ownership filename.");
-                    }
-                });
+            public void onFailure(Throwable e) {
+                fail("Unexpected failure when checking ownership file.");
             }
         });
     }
