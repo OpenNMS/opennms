55cae6c77a3575a4c56c95e88c67bc591fbd3264
Donald Desloge <thedesloge@users.sourceforge.net>
2009-12-09 19:03:52 +0000

    fix for WOTO-346

--- opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/DefaultProvisionService.java
+++ opennms-provision/opennms-provisiond/src/main/java/org/opennms/netmgt/provision/service/DefaultProvisionService.java
@@ -201,6 +201,16 @@ public class DefaultProvisionService implements ProvisionService {
         
         OnmsIpInterface dbIface = m_ipInterfaceDao.findByNodeIdAndIpAddress(nodeId, scannedIface.getIpAddress());
         if (dbIface != null) {
+            if(dbIface.isManaged() && !scannedIface.isManaged()){
+                Set<OnmsMonitoredService> monSvcs = dbIface.getMonitoredServices();
+                
+                for(OnmsMonitoredService monSvc : monSvcs ){
+                    monSvc.visit(new DeleteEventVisitor(m_eventForwarder));
+                }
+                
+                dbIface.setMonitoredServices(Collections.<OnmsMonitoredService>emptySet());
+            }
+            
             dbIface.mergeInterfaceAttributes(scannedIface);
             info("Updating IpInterface %s", dbIface);
             m_ipInterfaceDao.update(dbIface);
