a20f3f1b5aed5b676069788f908d33b6a39213f4
Jason Aras <jasonaras@users.sourceforge.net>
2009-12-10 16:41:23 +0000

    glued in missing report schedule, adding targets for the reports to the xsd and have a sample report/template glued in

--- opennms-enterprise-reporting/opennms-reportd/src/main/java/org/opennms/netmgt/reporting/service/ReportJobFactory.java
+++ opennms-enterprise-reporting/opennms-reportd/src/main/java/org/opennms/netmgt/reporting/service/ReportJobFactory.java
@@ -1,5 +1,45 @@
 package org.opennms.netmgt.reporting.service;
 
-public class ReportJobFactory {
+
+import org.quartz.Job;
+import org.quartz.JobDetail;
+import org.quartz.spi.JobFactory;
+import org.quartz.SchedulerException;
+import org.quartz.spi.TriggerFiredBundle;
+
+public class ReportJobFactory implements JobFactory {
+
+    private Reportd m_reportd;
+
+    public Job newJob(TriggerFiredBundle bundle) throws SchedulerException {
+
+        JobDetail jobDetail = bundle.getJobDetail();
+        Class<ReportJob> jobClass = getJobClass(jobDetail);
+        
+        ReportJob job = null;
+        
+        try {
+            job = jobClass.newInstance();
+            job.setReportd(getReportd());
+            return job;
+        } catch (Exception e) {
+            SchedulerException se = new SchedulerException("failed to create job class: "+ jobDetail.getJobClass().getName()+"; "+
+                                                           e.getLocalizedMessage(), e);
+            throw se;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private Class<ReportJob> getJobClass(JobDetail jobDetail) {
+        return (Class<ReportJob>)jobDetail.getJobClass();
+    }
+
+    public void setReportd(Reportd reportd) {
+        m_reportd = reportd;
+    }
+    
+    private Reportd getReportd() {
+        return m_reportd;
+    }
 
 }
