fd4a458b27c084e6583c28dc57e2533a9de4a97f
Jason Aras <jasonaras@users.sourceforge.net>
2009-12-19 13:07:20 +0000

    cleaning this up to be a bit more tidy

--- opennms-enterprise-reporting/opennms-reportd/src/main/java/org/opennms/netmgt/reporting/service/JavaMailDeliveryService.java
+++ opennms-enterprise-reporting/opennms-reportd/src/main/java/org/opennms/netmgt/reporting/service/JavaMailDeliveryService.java
@@ -3,12 +3,10 @@ package org.opennms.netmgt.reporting.service;
 import java.io.File;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 
 import org.opennms.core.utils.LogUtils;
 import org.opennms.javamail.JavaMailerException;
 import org.opennms.javamail.JavaSendMailer;
-import org.opennms.netmgt.config.common.SendmailConfig;
 import org.opennms.netmgt.config.reportd.Report;
 import org.opennms.netmgt.dao.JavaMailConfigurationDao;
 import org.springframework.mail.javamail.MimeMailMessage;
@@ -16,53 +14,44 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 
 public class JavaMailDeliveryService implements ReportDeliveryService {
 
-    SendmailConfig m_sendMailConfig = null;
-    
-    public JavaMailConfigurationDao getJavamailConfigDao() {
-        return m_JavamailConfigDao;
-    }
-
-    public void setJavamailConfigDao(JavaMailConfigurationDao javamailConfigDao) {
-        m_JavamailConfigDao = javamailConfigDao;
-        m_sendMailConfig = javamailConfigDao.getDefaultSendmailConfig();
-    }
-
     JavaMailConfigurationDao m_JavamailConfigDao;
 
-
     public void deliverReport(Report report, String fileName) {
-
-        
         try {
-            
+
+            JavaSendMailer sm = new JavaSendMailer(m_JavamailConfigDao.getDefaultSendmailConfig());
+            MimeMessageHelper helper = new MimeMessageHelper(sm.getMessage().getMimeMessage(),true);
+                
             for(String recipient : report.getRecipientCollection()){
-                JavaSendMailer sm = new JavaSendMailer(m_sendMailConfig);
-                
-               // MimeMailMessage message = sm.getMessage();
-                
-                
-                MimeMessageHelper helper = new MimeMessageHelper(sm.getMessage().getMimeMessage(),true);
-                helper.setTo(recipient);
-                helper.setSubject("OpenNMS Report");
-                helper.setText("OpenNMS Report: ");
-                helper.addAttachment(fileName, new File(fileName));
-                
-                sm.setMessage(new MimeMailMessage(helper));
-                sm.send();
-                
+                helper.addTo(recipient);
             }
-            
+                
+            helper.setSubject("OpenNMS Report " + report.getReportName());
+            helper.setText("OpenNMS Report: ");
+                
+            helper.addAttachment(fileName, new File(fileName));
+                
+            sm.setMessage(new MimeMailMessage(helper));
+            sm.send();
+
         } catch (JavaMailerException e) {
             LogUtils.errorf(this,"Problem with JavaMailer %s", e.getMessage());
         } catch (MessagingException e) {
-LogUtils.errorf(this,"Problem with Messaing %s", e.getMessage());
-} catch (Exception e) {
+            LogUtils.errorf(this,"Problem with Messaing %s", e.getMessage());
+        } catch (Exception e) {
             LogUtils.errorf(this,"Non Specific Error: %s",e.getMessage());
-            
         }
         
     }
 
+
+    public JavaMailConfigurationDao getJavamailConfigDao() {
+        return m_JavamailConfigDao;
+    }
+
     
-    
+    public void setJavamailConfigDao(JavaMailConfigurationDao javamailConfigDao) {
+        m_JavamailConfigDao = javamailConfigDao;
+    }
+
 }
