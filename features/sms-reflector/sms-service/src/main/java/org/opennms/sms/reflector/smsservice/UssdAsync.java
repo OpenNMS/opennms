6855bd3cb8c7222827d7fd6bab1f17861e6b8ba5
Benjamin Reed <rangerrick@users.sourceforge.net>
2009-12-21 17:39:56 +0000

    short-circuit if an earlier part of the sequence has failed

--- features/sms-reflector/sms-service/src/main/java/org/opennms/sms/reflector/smsservice/UssdAsync.java
+++ features/sms-reflector/sms-service/src/main/java/org/opennms/sms/reflector/smsservice/UssdAsync.java
@@ -9,21 +9,23 @@ import org.smslib.USSDRequest;
 
 public class UssdAsync implements Async<MobileMsgResponse> {
 	private final MobileMsgTracker m_tracker;
+	private final MobileMsgSequence m_sequence;
 	private final USSDRequest m_request;
 	private final MobileMsgResponseMatcher m_matcher;
 	private long m_timeout;
 	private int m_retries;
 
-	public UssdAsync(MobileMsgTracker tracker, long timeout, int retries, USSDRequest req, MobileMsgResponseMatcher matcher) {
+	public UssdAsync(MobileMsgTracker tracker, MobileMsgSequence sequence, long timeout, int retries, USSDRequest req, MobileMsgResponseMatcher matcher) {
 		this.m_tracker = tracker;
+		this.m_sequence = sequence;
 		this.m_timeout = timeout;
 		this.m_retries = retries;
 		this.m_request = req;
 		this.m_matcher = matcher;
 	}
 
-	public UssdAsync(MobileMsgTracker tracker, String gatewayId, long timeout, int retries, String text, MobileMsgResponseMatcher matcher) {
-		this(tracker, timeout, retries, getRequest(gatewayId, text), matcher);
+	public UssdAsync(MobileMsgTracker tracker, MobileMsgSequence sequence, String gatewayId, long timeout, int retries, String text, MobileMsgResponseMatcher matcher) {
+		this(tracker, sequence, timeout, retries, getRequest(gatewayId, text), matcher);
 	}
 
 	private static USSDRequest getRequest(String gatewayId, String text) {
@@ -33,6 +35,10 @@ public class UssdAsync implements Async<MobileMsgResponse> {
 	}
 
 	public void submit(final Callback<MobileMsgResponse> cb) {
+		if (m_sequence.hasFailed()) {
+			cb.complete(null);
+		}
+
 		MobileMsgResponseCallback mmrc = new MobileMsgCallbackAdapter(cb);
 
 		try {
