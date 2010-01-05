a1cc6f7cf70ba56af6e00d11897c26dd0a9c316e
Robert Drake <rdrake@direcpath.com>
2010-01-05 08:21:08 -0500

    fixed provision.pl to support -u admin -p PANTS (or whatever your password is)
    Signed-off-by: Benjamin Reed <rangerrick@befunk.com>

--- opennms-base-assembly/src/main/filtered/bin/provision.pl
+++ opennms-base-assembly/src/main/filtered/bin/provision.pl
@@ -88,10 +88,10 @@ my $result = GetOptions(
 	"longhelp|l" => \$print_longhelp,
 	"version|v"  => \&print_version,
 
-	"username|u" => \$username,
-	"password|p" => \$password,
+	"username|u=s" => \$username,
+	"password|p=s" => \$password,
 
-	"url"        => \$url_root,
+	"url=s"        => \$url_root,
 );
 
 pod2usage(1) if $print_help;
