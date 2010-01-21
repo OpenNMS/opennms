3169da3788f3f367756f2ba69ed2d7ad6b8bcf2d
Benjamin Reed <ranger@marvin.internal.opennms.com>
2010-01-21 16:23:55 -0500

    port logutils and threadcategory to slf4j directly

--- core/lib/src/main/java/org/opennms/core/utils/LogUtils.java
+++ core/lib/src/main/java/org/opennms/core/utils/LogUtils.java
@@ -1,90 +1,99 @@
 package org.opennms.core.utils;
 
-import org.apache.log4j.Appender;
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.SimpleLayout;
+import org.slf4j.Logger;
 
 public class LogUtils {
-    static volatile Appender m_appender;
-    
     public static void tracef(Object logee, String format, Object... args) {
-        logf(Level.TRACE, logee, null, format, args);
+        tracef(logee, null, format, args);
     }
 
     public static void tracef(Object logee, Throwable throwable, String format, Object... args) {
-        logf(Level.TRACE, logee, throwable, format, args);
+        Logger log = getLogger(logee);
+        if (log.isTraceEnabled()) {
+            if (throwable == null) {
+                log.trace(String.format(format, args));
+            } else {
+                log.trace(String.format(format, args), throwable);
+            }
+        }
     }
 
     public static void debugf(Object logee, String format, Object... args) {
-        logf(Level.DEBUG, logee, null, format, args);
+        debugf(logee, null, format, args);
     }
 
     public static void debugf(Object logee, Throwable throwable, String format, Object... args) {
-        logf(Level.DEBUG, logee, throwable, format, args);
+        Logger log = getLogger(logee);
+        if (log.isDebugEnabled()) {
+            if (throwable == null) {
+                log.debug(String.format(format, args));
+            } else {
+                log.debug(String.format(format, args), throwable);
+            }
+        }
     }
 
     public static void infof(Object logee, String format, Object... args) {
-        logf(Level.INFO, logee, null, format, args);
+        infof(logee, null, format, args);
     }
 
     public static void infof(Object logee, Throwable throwable, String format, Object... args) {
-        logf(Level.INFO, logee, throwable, format, args);
+        Logger log = getLogger(logee);
+        if (log.isInfoEnabled()) {
+            if (throwable == null) {
+                log.info(String.format(format, args));
+            } else {
+                log.info(String.format(format, args), throwable);
+            }
+        }
     }
 
     public static void warnf(Object logee, String format, Object... args) {
-        logf(Level.WARN, logee, null, format, args);
+        warnf(logee, null, format, args);
     }
 
     public static void warnf(Object logee, Throwable throwable, String format, Object... args) {
-        logf(Level.WARN, logee, throwable, format, args);
+        Logger log = getLogger(logee);
+        if (log.isWarnEnabled()) {
+            if (throwable == null) {
+                log.warn(String.format(format, args));
+            } else {
+                log.warn(String.format(format, args), throwable);
+            }
+        }
     }
 
     public static void errorf(Object logee, String format, Object... args) {
-        logf(Level.ERROR, logee, null, format, args);
+        errorf(logee, null, format, args);
     }
 
     public static void errorf(Object logee, Throwable throwable, String format, Object... args) {
-        logf(Level.ERROR, logee, throwable, format, args);
+        Logger log = getLogger(logee);
+        if (log.isErrorEnabled()) {
+            if (throwable == null) {
+                log.error(String.format(format, args));
+            } else {
+                log.error(String.format(format, args), throwable);
+            }
+        }
     }
 
     public static void fatalf(Object logee, String format, Object... args) {
-        logf(Level.FATAL, logee, null, format, args);
+        errorf(logee, null, format, args);
     }
 
     public static void fatalf(Object logee, Throwable throwable, String format, Object... args) {
-        logf(Level.FATAL, logee, throwable, format, args);
-    }
-
-    public static void logf(Level level, Object logee, Throwable throwable, String format, Object... args) {
-        Logger log = getLogger(logee);
-        if (log.isEnabledFor(level)) {
-            if (throwable == null) {
-                log.log(level, String.format(format, args));
-            } else {
-                log.log(level, String.format(format, args), throwable);
-            }
-        }
+        errorf(logee, null, format, args);
     }
 
     private static Logger getLogger(Object logee) {
         Logger log;
         if (logee instanceof String) {
-            log = ThreadCategory.getInstance((String)logee);
+            log = ThreadCategory.getSlf4jInstance((String)logee);
         } else if (logee instanceof Class<?>) {
-            log = ThreadCategory.getInstance((Class<?>)logee);
+            log = ThreadCategory.getSlf4jInstance((Class<?>)logee);
         } else {
-            log = ThreadCategory.getInstance(logee.getClass());
-        }
-        if (!log.getAllAppenders().hasMoreElements()) {
-            System.err.println("LogUtils: creating a default STDERR appender.");
-            log.setLevel(Level.DEBUG);
-            
-            if (m_appender == null) {
-                m_appender = new ConsoleAppender(new SimpleLayout(), "System.err");
-            }
-            log.addAppender(m_appender);
+            log = ThreadCategory.getSlf4jInstance(logee.getClass());
         }
         return log;
     }
