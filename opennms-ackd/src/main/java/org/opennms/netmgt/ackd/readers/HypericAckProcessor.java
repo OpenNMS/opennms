1d58e13a187df0709c55afed7317e3fbe7a04d92
Seth Leger <seth@opennms.org>
2010-01-29 21:26:18 -0500

    Exposed configuration object to HypericAckProcessor. Added more skeleton code to the body of the class.

--- opennms-ackd/src/main/java/org/opennms/netmgt/ackd/readers/HypericAckProcessor.java
+++ opennms-ackd/src/main/java/org/opennms/netmgt/ackd/readers/HypericAckProcessor.java
@@ -35,13 +35,36 @@
  */
 package org.opennms.netmgt.ackd.readers;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.apache.log4j.Logger;
+import org.hibernate.criterion.Restrictions;
+
+/*
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.params.HttpClientParams;
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.HttpVersion;
+ */
+
 import org.opennms.core.utils.ThreadCategory;
 import org.opennms.netmgt.dao.AckdConfigurationDao;
+import org.opennms.netmgt.dao.AlarmDao;
+import org.opennms.netmgt.model.OnmsAlarm;
+import org.opennms.netmgt.model.OnmsCriteria;
+import org.opennms.netmgt.xml.event.Parms;
 
 public class HypericAckProcessor implements AckProcessor {
 
     private AckdConfigurationDao m_ackdDao;
+    private AlarmDao m_alarmDao;
 
     private static Logger log() {
         return ThreadCategory.getInstance(HypericAckProcessor.class);
@@ -54,7 +77,50 @@ public class HypericAckProcessor implements AckProcessor {
     }
 
     public void run() {
+        int count = 0;
+
         // Parse Hyperic alert states
+        try {
+            log().info("run: Processing Hyperic acknowledgments..." );
+
+            OnmsCriteria criteria = new OnmsCriteria(OnmsAlarm.class, "alarm");
+            criteria.add(Restrictions.isNull("alarmAckUser"));
+            // Restrict to Hyperic alerts
+            criteria.add(Restrictions.eq("uei", "uei.opennms.org/external/hyperic/alert"));
+            // TODO Figure out how to query by parameters
+
+            Map<String,List<OnmsAlarm>> organizedAlarms = new TreeMap<String,List<OnmsAlarm>>();
+
+            // Query list of outstanding alerts with remote platform identifiers
+            List<OnmsAlarm> unAckdAlarms = m_alarmDao.findMatching(criteria);
+
+            // Organize the alarms according to the Hyperic system where they originated
+            for (OnmsAlarm alarm : unAckdAlarms) {
+                String hypericSystem = alarm.getEventParms();
+                // Figure out how to parse the event parms into a list, get the platform.agent.address or platform.id
+                String key = "blah";
+                List<OnmsAlarm> targetList = organizedAlarms.get(key);
+                if (targetList == null) {
+                    targetList = new ArrayList<OnmsAlarm>();
+                    organizedAlarms.put(key, targetList);
+                }
+                targetList.add(alarm);
+            }
+
+            // Connect to each Hyperic system and query for the status of corresponding alerts 
+            for (Map.Entry<String, List<OnmsAlarm>> alarmList : organizedAlarms.entrySet()) {
+                String hypericSystem = alarmList.getKey();
+                for (OnmsAlarm alarmsForSystem : alarmList.getValue()) {
+                    // Construct a sane query for the Hyperic system
+                }
+            }
+
+            // Iterate and update any acknowledged or fixed alerts
+
+            log().info("run: Finished processing Hyperic acknowledgments (" + count + " acks processed)" );
+        } catch (Throwable e) {
+            log().warn("run: threw exception: "+e);
+        }
     }
 
     public synchronized void setAckdConfigDao(final AckdConfigurationDao configDao) {
@@ -62,5 +128,9 @@ public class HypericAckProcessor implements AckProcessor {
     }
 
     public void afterPropertiesSet() throws Exception {
+    }
+
+    public synchronized void setAlertDao(final AlarmDao dao) {
+        m_alarmDao = dao;
     }
 }
