f0512c8e8c2516859211ee492898ad0d045f6377
Donald Desloge <desloge@opennms.com>
2010-02-03 14:06:02 -0500

    Fixed testVirtualHost test

--- opennms-services/src/test/java/org/opennms/netmgt/poller/monitors/PageSequenceMonitorTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/poller/monitors/PageSequenceMonitorTest.java
@@ -165,7 +165,7 @@ public class PageSequenceMonitorTest extends TestCase {
 		m_params.put("page-sequence", "" +
 				"<?xml version=\"1.0\"?>" +
 				"<page-sequence>\n" + 
-				"  <page path=\"/\" port=\"80\" successMatch=\"Get the Network to Work\" virtual-host=\"www.opennms.com\"/>\n" + 
+				"  <page user-agent=\"Donald\" path=\"/\" port=\"80\" successMatch=\"Get the Network to Work\" virtual-host=\"www.opennms.com\"/>\n" + 
 				"</page-sequence>\n");
 		
 		
@@ -173,5 +173,18 @@ public class PageSequenceMonitorTest extends TestCase {
 		assertTrue("Expected available but was "+status+": reason = "+status.getReason(), status.isAvailable());
 		
 	}
+	
+	public void testVirtualHostBadBehaviorForWordpressPlugin() throws Exception {
+        m_params.put("page-sequence", "" +
+                "<?xml version=\"1.0\"?>" +
+                "<page-sequence>\n" + 
+                "  <page path=\"/\" port=\"80\" successMatch=\"Get the Network to Work\" virtual-host=\"www.opennms.com\"/>\n" + 
+                "</page-sequence>\n");
+        
+        
+        PollStatus status = m_monitor.poll(getHttpService("www.opennms.com"), m_params);
+        assertTrue("Expected unavailable but was "+status+": reason = "+status.getReason(), status.isDown());
+        
+    }
 	
 }
