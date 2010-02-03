482fa5645f0109981fa25c8ddd919ee4750ae560
Jonathan Sartin <jonathan@opennms.org>
2010-02-03 18:41:03 +0000

    add missing validator

---
+++ opennms-webapp/src/main/java/org/opennms/web/report/database/ReportParametersValidator.java
@@ -0,0 +1,53 @@
+package org.opennms.web.report.database;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.opennms.api.reporting.parameter.ReportParameters;
+import org.opennms.api.reporting.parameter.ReportDateParm;
+import org.opennms.api.reporting.parameter.ReportIntParm;
+import org.opennms.api.reporting.parameter.ReportStringParm;
+import org.springframework.binding.message.MessageBuilder;
+import org.springframework.binding.message.MessageContext;
+import org.springframework.binding.validation.ValidationContext;
+
+public class ReportParametersValidator {
+    
+    public void  validateReportParameters(ReportParameters reportCriteria, ValidationContext context) {
+        MessageContext messages = context.getMessageContext();
+        
+        
+        List<ReportDateParm> dateParms = reportCriteria.getDateParms();
+        
+        for (Iterator<ReportDateParm> dateParmIter = dateParms.iterator(); dateParmIter.hasNext();) {
+            ReportDateParm dateParm = dateParmIter.next();
+            if (dateParm.getValue() == null) {
+                messages.addMessage(new MessageBuilder().error().source("date parms").
+                                    defaultText("cannot have null date field" + dateParm.getDisplayName()).build());
+            }
+        }
+        
+        List<ReportStringParm> stringParms = reportCriteria.getStringParms();
+        
+        for (Iterator<ReportStringParm> stringParmIter = stringParms.iterator(); stringParmIter.hasNext();) {
+            ReportStringParm stringParm = stringParmIter.next();
+            if (stringParm.getValue() == "" ) {
+                messages.addMessage(new MessageBuilder().error().source("string parms").
+                                    defaultText("cannot have empty string field" + stringParm.getDisplayName()).build());
+            }
+        }
+        
+        List<ReportIntParm> intParms = reportCriteria.getIntParms();
+        
+        for (Iterator<ReportIntParm> intParmIter = intParms.iterator(); intParmIter.hasNext();) {
+            ReportIntParm intParm = intParmIter.next();
+            // TODO add a more sensible check here - I think we probably can have zero int parm
+            if (intParm.getValue() == 0 ) {
+                messages.addMessage(new MessageBuilder().error().source("int parms").
+                                    defaultText("cannot have zero integer field" + intParm.getDisplayName()).build());
+            }
+        }
+        
+    }
+
+}
