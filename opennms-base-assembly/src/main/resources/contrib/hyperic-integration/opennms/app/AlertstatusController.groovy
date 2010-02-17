7324440ded0c787c904f7ade2652af325082131b
Seth Leger <seth@opennms.org>
2010-02-17 12:55:04 -0500

    Added first revision of the AlertstatusController.groovy servlet that will serve up a list of Hyperic alert statuses.

--- opennms-base-assembly/src/main/resources/contrib/hyperic-integration/opennms/app/AlertstatusController.groovy
+++ opennms-base-assembly/src/main/resources/contrib/hyperic-integration/opennms/app/AlertstatusController.groovy
@@ -26,45 +26,39 @@
  *      http://www.opennms.org/
  *      http://www.opennms.com/
  */
- 
-/*
- * This script has the following bindings:
- *    org.hyperic.hq.events.AlertDefinitionInterface alertDef
- *    org.hyperic.hq.events.AlertInterface alert
- *    org.hyperic.hq.events.ActionExecutionInfo action
- *    org.hyperic.hq.authz.server.session.Resource resource
- *
- */
-import org.hyperic.hq.hqu.rendit.BaseController
 
 import java.text.SimpleDateFormat
 import org.hyperic.hibernate.PageInfo
-import org.hyperic.hq.authz.server.session.ResourceSortField
+import org.hyperic.hq.hqu.rendit.BaseController
 import org.hyperic.hq.appdef.server.session.PlatformManagerEJBImpl as PlatformManager
+import org.hyperic.hq.events.server.session.AlertManagerEJBImpl as AlertManager
 
-class ExporterController 
+class AlertstatusController
     extends BaseController
 {
-    def ExporterController() {
+    def AlertstatusController() {
         setXMLMethods(['list'])
     }
 
     def list(xml, params) {
-        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-        def platforms = resourceHelper.findPlatforms(new PageInfo(ResourceSortField.NAME, true));
-        def man = PlatformManager.one
-        xml.'model-import'('foreign-source':'HQ', 'date-stamp':formatter.format(new Date())) {
-            platforms.each { res ->   
-                def p = man.findPlatformById(res.instanceId)
-                node('node-label':p.fqdn, 'foreign-id':p.id) {
-                    'interface'('ip-addr': p.agent.address, descr: 'agent-address', status: 1, 'snmp-primary': 'N') {
-                        'monitored-service'('service-name': 'ICMP')
-                        'monitored-service'('service-name': 'HypericAgent')
-                    }
+        def man = AlertManager.one
+
+        xml.'hyperic-alert-statuses'() {
+            if (params['id'] == null) {
+                // TODO: Fetch all alerts
+            } else {
+                params['id'].each { aval ->
+                    def a = man.findAlertById(aval.toInteger())
+                    def attribs = [:]
+                    attribs['fixed'] = a.fixed
+                    attribs['id'] = a.id
+                    attribs['state'] = a.stateId
+                    if(a.ackedBy != null) { attribs['ackedBy'] =  a.ackedBy }
+                    //'alert'(fixed: a.fixed, id: a.id, state: a.stateId, 'ackUser': a.ackedBy )
+                    'alert'(attribs)
                 }
             }
         }
-
         xml
     }
 }
