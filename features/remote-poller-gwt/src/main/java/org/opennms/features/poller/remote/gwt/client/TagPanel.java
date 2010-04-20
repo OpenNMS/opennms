9e3350054192a2a049be4141a0ddd61f2bc0bfd7
Seth Leger <seth@opennms.org>
2010-04-20 15:04:47 -0400

    Added all tag panel updates that are necessary and added tag selection events to the links.

--- features/remote-poller-gwt/src/main/java/org/opennms/features/poller/remote/gwt/client/TagPanel.java
+++ features/remote-poller-gwt/src/main/java/org/opennms/features/poller/remote/gwt/client/TagPanel.java
@@ -8,13 +8,17 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HandlerManager;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
 public class TagPanel extends Composite implements Collection<String> {
@@ -46,6 +50,28 @@ public class TagPanel extends Composite implements Collection<String> {
     @UiField 
     TagStyles tagStyles;
 
+    private interface TagSelectedEventHandler extends EventHandler {
+        public void onTagSelected(String tagName);
+    }
+
+    private static class TagSelectedEvent extends GwtEvent<TagSelectedEventHandler>
+    {
+        public static Type<TagSelectedEventHandler> TYPE = new Type<TagSelectedEventHandler>();
+
+        private final String m_tagName;
+
+        public TagSelectedEvent(String tagName) {
+            m_tagName = tagName;
+        }
+
+        protected void dispatch(TagSelectedEventHandler handler) {
+            handler.onTagSelected(m_tagName);
+        }
+
+        public GwtEvent.Type<TagSelectedEventHandler> getAssociatedType() {
+            return TYPE;
+        }
+    }
 
     public TagPanel() {
         super();
@@ -88,11 +114,14 @@ public class TagPanel extends Composite implements Collection<String> {
     }
 
     public boolean addAll(Collection<? extends String> c) {
-        return m_delegate.addAll(c);
+        boolean retval = m_delegate.addAll(c);
+        updatePanel();
+        return retval;
     }
 
     public void clear() {
         m_delegate.clear();
+        updatePanel();
     }
 
     public boolean contains(Object o) {
@@ -112,15 +141,21 @@ public class TagPanel extends Composite implements Collection<String> {
     }
 
     public boolean remove(Object o) {
-        return m_delegate.remove(o);
+        boolean retval = m_delegate.remove(o);
+        updatePanel();
+        return retval;
     }
 
     public boolean removeAll(Collection<?> c) {
-        return m_delegate.removeAll(c);
+        boolean retval = m_delegate.removeAll(c);
+        updatePanel();
+        return retval;
     }
 
     public boolean retainAll(Collection<?> c) {
-        return m_delegate.retainAll(c);
+        boolean retval = m_delegate.retainAll(c);
+        updatePanel();
+        return retval;
     }
 
     public int size() {
@@ -154,9 +189,14 @@ public class TagPanel extends Composite implements Collection<String> {
         tagPanel.clear();
 
         for (Map.Entry<String, Integer> entry : tagCounts.entrySet()) {
-            Label tagLabel = new Label();
+            final Anchor tagLabel = new Anchor();
             double rawValue = (double)(entry.getValue() - minCount) * TagStyles.COUNT / (double)(maxCount - minCount);
             tagLabel.setText(entry.getKey());
+            tagLabel.addClickHandler(new ClickHandler() {
+                public void onClick(ClickEvent event) {
+                    m_eventBus.fireEvent(new TagSelectedEvent(tagLabel.getText()));
+                }
+            });
             switch ((int)Math.round(rawValue)) {
             case(0):
                 tagLabel.addStyleName(tagStyles.tag0());
