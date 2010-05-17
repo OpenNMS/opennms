bd2c051e4cf1549945a0495a2f25b1155d7efa94
Benjamin Reed <rangerrick@befunk.com>
2010-05-17 17:04:50 -0400

    event listener for remote poller config updates

---
+++ opennms-services/src/main/java/org/opennms/netmgt/poller/remote/support/PollerBackEndEventProcessor.java
@@ -0,0 +1,60 @@
+package org.opennms.netmgt.poller.remote.support;
+
+import org.opennms.netmgt.EventConstants;
+import org.opennms.netmgt.model.events.annotations.EventHandler;
+import org.opennms.netmgt.model.events.annotations.EventListener;
+import org.opennms.netmgt.poller.remote.PollerBackEnd;
+import org.opennms.netmgt.xml.event.Event;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@EventListener(name="pollerBackEnd")
+public class PollerBackEndEventProcessor {
+    @Autowired
+    private PollerBackEnd m_pollerBackEnd;
+
+    @EventHandler(uei=EventConstants.NODE_ADDED_EVENT_UEI)
+    public void handleNodeAdded(final Event event) {
+        m_pollerBackEnd.configurationUpdated();
+    }
+
+    @EventHandler(uei=EventConstants.NODE_GAINED_INTERFACE_EVENT_UEI)
+    public void handleNodeGainedInterface(final Event event) {
+        m_pollerBackEnd.configurationUpdated();
+    }
+
+    @EventHandler(uei=EventConstants.NODE_GAINED_SERVICE_EVENT_UEI)
+    public void handleNodeGainedService(final Event event) {
+        m_pollerBackEnd.configurationUpdated();
+    }
+
+    @EventHandler(uei=EventConstants.NODE_CONFIG_CHANGE_UEI)
+    public void handleNodeConfigChanged(final Event event) {
+        m_pollerBackEnd.configurationUpdated();
+    }
+
+    @EventHandler(uei=EventConstants.NODE_INFO_CHANGED_EVENT_UEI)
+    public void handleNodeInfoChanged(final Event event) {
+        m_pollerBackEnd.configurationUpdated();
+    }
+
+    @EventHandler(uei=EventConstants.SERVICE_DELETED_EVENT_UEI)
+    public void handleServiceDeleted(final Event event) {
+        m_pollerBackEnd.configurationUpdated();
+    }
+
+    @EventHandler(uei=EventConstants.SERVICE_UNMANAGED_EVENT_UEI)
+    public void handleServiceUnmanaged(final Event event) {
+        m_pollerBackEnd.configurationUpdated();
+    }
+
+    @EventHandler(uei=EventConstants.INTERFACE_DELETED_EVENT_UEI)
+    public void handleInterfaceDeleted(final Event event) {
+        m_pollerBackEnd.configurationUpdated();
+    }
+
+    @EventHandler(uei=EventConstants.NODE_DELETED_EVENT_UEI)
+    public void handleNodeDeleted(final Event event) {
+        m_pollerBackEnd.configurationUpdated();
+    }
+
+}
