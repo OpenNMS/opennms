9af4f8318a29eb46180e3945ff46cb343ac91a2c
Donald Desloge <desloge@opennms.com>
2010-05-17 16:51:27 -0400

    Added isWithinBounds to MarkeState which returns true if it is within the bounds of the map

--- features/remote-poller-gwt/src/main/java/org/opennms/features/poller/remote/gwt/client/DefaultLocationManager.java
+++ features/remote-poller-gwt/src/main/java/org/opennms/features/poller/remote/gwt/client/DefaultLocationManager.java
@@ -217,7 +217,7 @@ public class DefaultLocationManager implements LocationManager, RemotePollerPres
         for (final LocationInfo location : m_locations.values()) {
             final GWTMarkerState markerState = location.getMarkerState();
             GWTBounds mapBounds = m_mapPanel.getBounds();
-            if (markerState.isSelected() && mapBounds.contains(markerState.getLatLng()) && markerState.isVisible()) {
+            if (markerState.isSelected() && markerState.isWithinBounds(mapBounds) && markerState.isVisible()) {
                 visibleLocations.add(location);
             }
         }
