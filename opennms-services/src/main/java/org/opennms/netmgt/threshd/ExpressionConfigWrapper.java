3493c8ea275ce27cb3cc32a6c05a5e36c5b5faea
Seth Leger <seth@opennms.org>
2010-06-03 21:18:04 -0400

    Uncommented all method signatures for java.lang.Math functions inside ExpressionConfigWrapper.

--- opennms-services/src/main/java/org/opennms/netmgt/threshd/ExpressionConfigWrapper.java
+++ opennms-services/src/main/java/org/opennms/netmgt/threshd/ExpressionConfigWrapper.java
@@ -135,9 +135,9 @@ public class ExpressionConfigWrapper extends BaseThresholdDefConfigWrapper {
 	 */
 	public static class MathBinding {
 		public double abs(double a) { return Math.abs(a); }
-		// public float abs(float a) { return Math.abs(a); }
-		// public int abs(int a) { return Math.abs(a); }
-		// public long abs(long a) { return Math.abs(a); }
+		public float abs(float a) { return Math.abs(a); }
+		public int abs(int a) { return Math.abs(a); }
+		public long abs(long a) { return Math.abs(a); }
 		public double acos(double a) { return Math.acos(a); }
 		public double asin(double a) { return Math.asin(a); }
 		public double atan(double a) { return Math.atan(a); }
@@ -145,40 +145,40 @@ public class ExpressionConfigWrapper extends BaseThresholdDefConfigWrapper {
 		public double cbrt(double a) { return Math.cbrt(a); }
 		public double ceil(double a) { return Math.ceil(a); }
 		public double copySign(double magnitude, double sign) { return Math.copySign(magnitude, sign); }
-		// public float copySign(float magnitude, float sign) { return Math.copySign(magnitude, sign); }
+		public float copySign(float magnitude, float sign) { return Math.copySign(magnitude, sign); }
 		public double cos(double a) { return Math.cos(a); }
 		public double cosh(double a) { return Math.cosh(a); }
 		public double exp(double a) { return Math.exp(a); }
 		public double expm1(double a) { return Math.expm1(a); }
 		public double floor(double a) { return Math.floor(a); }
 		public int getExponent(double a) { return Math.getExponent(a); }
-		// public int getExponent(float a) { return Math.getExponent(a); }
+		public int getExponent(float a) { return Math.getExponent(a); }
 		public double hypot(double a, double b) { return Math.hypot(a, b); }
 		public double IEEEremainder(double a, double b) { return Math.IEEEremainder(a, b); }
 		public double log(double a) { return Math.log(a); }
 		public double log10(double a) { return Math.log10(a); }
 		public double log1p(double a) { return Math.log1p(a); }
 		public double max(double a, double b) { return Math.max(a, b); }
-		// public float max(float a, float b) { return Math.max(a, b); }
-		// public int max(int a, int b) { return Math.max(a, b); }
-		// public long max(long a, long b) { return Math.max(a, b); }
+		public float max(float a, float b) { return Math.max(a, b); }
+		public int max(int a, int b) { return Math.max(a, b); }
+		public long max(long a, long b) { return Math.max(a, b); }
 		public double min(double a, double b) { return Math.min(a, b); }
-		// public float min(float a, float b) { return Math.min(a, b); }
-		// public int min(int a, int b) { return Math.min(a, b); }
-		// public long min(long a, long b) { return Math.min(a, b); }
+		public float min(float a, float b) { return Math.min(a, b); }
+		public int min(int a, int b) { return Math.min(a, b); }
+		public long min(long a, long b) { return Math.min(a, b); }
 		public double nextAfter(double a, double direction) { return Math.nextAfter(a, direction); }
-		// public float nextAfter(float a, double direction) { return Math.nextAfter(a, direction); }
+		public float nextAfter(float a, double direction) { return Math.nextAfter(a, direction); }
 		public double nextUp(double a) { return Math.nextUp(a); }
-		// public float nextUp(float a) { return Math.nextUp(a); }
+		public float nextUp(float a) { return Math.nextUp(a); }
 		public double pow(double a, double b) { return Math.pow(a, b); }
 		public double random() { return Math.random(); }
 		public double rint(double a) { return Math.rint(a); }
 		public long round(double a) { return Math.round(a); }
-		// public int round(float a) { return Math.round(a); }
+		public int round(float a) { return Math.round(a); }
 		public double scalb(double a, int scaleFactor) { return Math.scalb(a, scaleFactor); }
-		// public float scalb(float a, int scaleFactor) { return Math.scalb(a, scaleFactor); }
+		public float scalb(float a, int scaleFactor) { return Math.scalb(a, scaleFactor); }
 		public double signum(double a) { return Math.signum(a); }
-		// public float signum(float a) { return Math.signum(a); }
+		public float signum(float a) { return Math.signum(a); }
 		public double sin(double a) { return Math.sin(a); }
 		public double sinh(double a) { return Math.sinh(a); }
 		public double sqrt(double a) { return Math.sqrt(a); }
@@ -187,7 +187,7 @@ public class ExpressionConfigWrapper extends BaseThresholdDefConfigWrapper {
 		public double toDegrees(double a) { return Math.toDegrees(a); }
 		public double toRadians(double a) { return Math.toRadians(a); }
 		public double ulp(double a) { return Math.ulp(a); }
-		// public float ulp(float a) { return Math.ulp(a); }
+		public float ulp(float a) { return Math.ulp(a); }
 	}
 
 	@Override
