3306f8cf5085e6189522c44ebea6f012b5dc315b
Benjamin Reed <rangerrick@befunk.com>
2010-06-24 11:25:24 -0400

    fix pollstatus

--- core/api/src/main/java/org/opennms/netmgt/model/PollStatus.java
+++ core/api/src/main/java/org/opennms/netmgt/model/PollStatus.java
@@ -257,7 +257,7 @@ public class PollStatus implements Serializable {
     }
 
     public void setReason(final String reason) {
-        m_reason = reason.substring(0, 255);
+        m_reason = (reason == null? null : reason.substring(0, 255));
     }
 
     @Column(name="responseTime", nullable=true)
