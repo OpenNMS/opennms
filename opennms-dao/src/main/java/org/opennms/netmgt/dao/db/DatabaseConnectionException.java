1ccbbf81497b2387a28320d239374240ad13cfe4
Benjamin Reed <rangerrick@befunk.com>
2010-06-25 16:59:50 -0400

    remove extraneous constructors (redux)

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/db/DatabaseConnectionException.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/db/DatabaseConnectionException.java
@@ -5,15 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnectionException extends SQLException {
     private static final long serialVersionUID = 1L;
 
-    public DatabaseConnectionException() {
-        super();
-    }
-
     public DatabaseConnectionException(String reason) {
         super(reason);
-    }
-
-    public DatabaseConnectionException(Throwable cause) {
-        super(cause);
     }
 }
