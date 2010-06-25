376d8a89fcae9600438ea656f6f00385739a7ae2
Benjamin Reed <rangerrick@befunk.com>
2010-06-25 16:52:16 -0400

    remove extraneous constructors

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/db/DatabaseConnectionException.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/db/DatabaseConnectionException.java
@@ -16,25 +16,4 @@ public class DatabaseConnectionException extends SQLException {
     public DatabaseConnectionException(Throwable cause) {
         super(cause);
     }
-
-    public DatabaseConnectionException(String reason, String SQLState) {
-        super(reason, SQLState);
-    }
-
-    public DatabaseConnectionException(String reason, Throwable cause) {
-        super(reason, cause);
-    }
-
-    public DatabaseConnectionException(String reason, String SQLState, int vendorCode) {
-        super(reason, SQLState, vendorCode);
-    }
-
-    public DatabaseConnectionException(String reason, String sqlState, Throwable cause) {
-        super(reason, sqlState, cause);
-    }
-
-    public DatabaseConnectionException(String reason, String sqlState, int vendorCode, Throwable cause) {
-        super(reason, sqlState, vendorCode, cause);
-    }
-
 }
