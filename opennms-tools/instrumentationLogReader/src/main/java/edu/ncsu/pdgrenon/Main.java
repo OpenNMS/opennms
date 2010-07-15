322de7c7f85a672817841e44a076e0b435733dd5
Philip Grenon <pdgrenon@opennms.org>
2010-07-15 11:42:52 -0400

    Added Instrumentation Log Reader to the opennms-tools directory.

---
+++ opennms-tools/instrumentationLogReader/src/main/java/edu/ncsu/pdgrenon/Main.java
@@ -0,0 +1,14 @@
+package edu.ncsu.pdgrenon;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	Collector c = new Collector();
+	c.sortAndPrintServiceCount();
+
+	}
+
+}
