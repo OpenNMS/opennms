d60850fe8293a4c423369ea388ec68fa93b1cef6
Donald Desloge <desloge@opennms.com>
2010-07-29 12:40:29 -0500

    created a real archetype to use for GWT modules

---
+++ opennms-tools/opennms-gwt-archetype/src/main/resources/archetype-resources/src/main/java/client/Application.java
@@ -0,0 +1,26 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Application
+    implements EntryPoint
+{
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad()
+  {
+     final Label label = new Label ( "gwt-maven-plugin Archetype :: Project org.codehaus.mojo.gwt-maven-plugin" );
+     RootPanel.get().add( label );
+  }
+}
