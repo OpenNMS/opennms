5f6906401a7455437b2963b0b3b1e2a2e72742c6
David S. Hustace <david@opennms.org>
2010-08-07 09:25:11 -0400

    Fix for bug <a href="http://bugzilla.opennms.org/show_bug.cgi?id=4022">4022</a> Also added missing copyright notice.

--- opennms-webapp/src/main/java/org/opennms/web/rest/OnmsMonitoredServiceResource.java
+++ opennms-webapp/src/main/java/org/opennms/web/rest/OnmsMonitoredServiceResource.java
@@ -1,3 +1,32 @@
+/*
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 2009 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a derivative work, containing both original code, included code and modified
+ * code that was published under the GNU General Public License. Copyrights for modified
+ * and included code are below.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * For more information contact:
+ * OpenNMS Licensing       <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ */
 package org.opennms.web.rest;
 
 import javax.ws.rs.Consumes;
@@ -24,6 +53,7 @@ import org.opennms.netmgt.model.OnmsNode;
 import org.opennms.netmgt.model.OnmsServiceType;
 import org.opennms.netmgt.model.events.EventProxy;
 import org.opennms.netmgt.model.events.EventProxyException;
+import org.opennms.netmgt.model.events.EventUtils;
 import org.opennms.netmgt.xml.event.Event;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
@@ -127,13 +157,10 @@ public class OnmsMonitoredServiceResource extends OnmsRestService {
         service.setIpInterface(intf);
         log().debug("addService: adding service " + service);
         m_serviceDao.save(service);
-        Event e = new Event();
-        e.setUei(EventConstants.NODE_GAINED_SERVICE_EVENT_UEI);
-        e.setNodeid(node.getId());
-        e.setInterface(intf.getIpAddress());
-        e.setService(service.getServiceName());
-        e.setSource(getClass().getName());
-        e.setTime(EventConstants.formatToString(new java.util.Date()));
+        
+        Event e = EventUtils.createNodeGainedServiceEvent(getClass().getName(), node.getId(), intf.getInetAddress(), 
+                service.getServiceName(), node.getLabel(), node.getLabelSource(), node.getSysName(), node.getSysDescription());
+        
         try {
             m_eventProxy.send(e);
         } catch (EventProxyException ex) {
