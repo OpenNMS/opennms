0e008aaf0efa9fe54659dc2e88bf3d3488c0c687
Benjamin Reed <rangerrick@befunk.com>
2010-09-22 09:58:11 -0400

    create system-report command-line script
    - refactored Bootstrap to allow subclassing
    - created shell script to call SystemReport

--- features/system-report/src/main/java/org/opennms/systemreport/system/JavaReportPlugin.java
+++ features/system-report/src/main/java/org/opennms/systemreport/system/JavaReportPlugin.java
@@ -1,10 +1,9 @@
 package org.opennms.systemreport.system;
 
-import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ClassLoadingMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
-import java.util.List;
-import java.util.Map;
+import java.lang.management.RuntimeMXBean;
 import java.util.TreeMap;
 
 import org.opennms.core.utils.LogUtils;
@@ -44,7 +43,26 @@ public class JavaReportPlugin extends AbstractSystemReportPlugin {
         LogUtils.tracef(this, "bean = %s", memoryBean.toString());
         addGetters(memoryBean, map);
 
-        List<GarbageCollectorMXBean> beans = getBeans(ManagementFactory.GARBAGE_COLLECTOR_MXBEAN_DOMAIN_TYPE + ",*", GarbageCollectorMXBean.class);
+        RuntimeMXBean runtimeBean = getBean(ManagementFactory.RUNTIME_MXBEAN_NAME, RuntimeMXBean.class);
+        if (runtimeBean == null) {
+            LogUtils.infof(this, "falling back to local VM RuntimeMXBean");
+            runtimeBean = ManagementFactory.getRuntimeMXBean();
+        }
+
+        LogUtils.tracef(this, "bean = %s", runtimeBean.toString());
+        addGetters(runtimeBean, map);
+
+        ClassLoadingMXBean classBean = getBean(ManagementFactory.CLASS_LOADING_MXBEAN_NAME, ClassLoadingMXBean.class);
+        if (classBean == null) {
+            LogUtils.infof(this, "falling back to local VM ClassLoadingMXBean");
+            classBean = ManagementFactory.getClassLoadingMXBean();
+        }
+
+        LogUtils.tracef(this, "bean = %s", classBean.toString());
+        addGetters(classBean, map);
+
+        /* this stuff is really not giving us anything useful
+        List<GarbageCollectorMXBean> beans = getBeans(ManagementFactory.GARBAGE_COLLECTOR_MXBEAN_DOMAIN_TYPE, GarbageCollectorMXBean.class);
         if (beans == null || beans.size() == 0) {
             LogUtils.infof(this, "falling back to local VM MemoryMXBean");
             beans = ManagementFactory.getGarbageCollectorMXBeans();
@@ -63,6 +81,7 @@ public class JavaReportPlugin extends AbstractSystemReportPlugin {
             if (sb.length() > 0) sb.deleteCharAt(sb.length());
             map.put("Garbage Collector " + collectorNum, getResource(sb.toString()));
         }
+        */
 
         return map;
     }
