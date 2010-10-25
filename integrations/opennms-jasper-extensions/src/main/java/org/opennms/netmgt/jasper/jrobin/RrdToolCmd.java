e82a3b014daf0af1d83bd9bda45cda65efb52b24
Donald Desloge <desloge@opennms.com>
2010-10-24 21:57:59 -0400

    Added opennms-jasper-extensions, with this extension one can create Jasper reports from Rrd data.
    http://www.opennms.org/wiki/Jasper_Report_RRD_Datasource_Specification

---
+++ integrations/opennms-jasper-extensions/src/main/java/org/opennms/netmgt/jasper/jrobin/RrdToolCmd.java
@@ -0,0 +1,141 @@
+package org.opennms.netmgt.jasper.jrobin;
+
+import java.io.IOException;
+
+import net.sf.jasperreports.engine.JRDataSource;
+
+import org.jrobin.core.RrdDb;
+import org.jrobin.core.RrdDbPool;
+import org.jrobin.core.RrdDef;
+import org.jrobin.core.RrdException;
+
+abstract class RrdToolCmd {
+
+    private RrdCmdScanner cmdScanner;
+
+    abstract String getCmdType();
+
+    abstract JRDataSource execute() throws RrdException, IOException;
+
+    JRDataSource executeCommand(String command) throws RrdException, IOException {
+        cmdScanner = new RrdCmdScanner(command);
+        return execute();
+    }
+
+    String getOptionValue(String shortForm, String longForm) throws RrdException {
+        return cmdScanner.getOptionValue(shortForm, longForm);
+    }
+
+    String getOptionValue(String shortForm, String longForm, String defaultValue) throws RrdException {
+        return cmdScanner.getOptionValue(shortForm, longForm, defaultValue);
+    }
+
+    String[] getMultipleOptionValues(String shortForm, String longForm) throws RrdException {
+        return cmdScanner.getMultipleOptions(shortForm, longForm);
+    }
+
+    boolean getBooleanOption(String shortForm, String longForm) {
+        return cmdScanner.getBooleanOption(shortForm, longForm);
+    }
+
+    String[] getRemainingWords() {
+        return cmdScanner.getRemainingWords();
+    }
+
+    static boolean rrdDbPoolUsed = true;
+    static boolean standardOutUsed = true;
+
+    static boolean isRrdDbPoolUsed() {
+        return rrdDbPoolUsed;
+    }
+
+    static void setRrdDbPoolUsed(boolean rrdDbPoolUsed) {
+        RrdToolCmd.rrdDbPoolUsed = rrdDbPoolUsed;
+    }
+
+    static boolean isStandardOutUsed() {
+        return standardOutUsed;
+    }
+
+    static void setStandardOutUsed(boolean standardOutUsed) {
+        RrdToolCmd.standardOutUsed = standardOutUsed;
+    }
+
+    static long parseLong(String value) throws RrdException {
+        try {
+            return Long.parseLong(value);
+        }
+        catch (NumberFormatException nfe) {
+            throw new RrdException(nfe);
+        }
+    }
+
+    static int parseInt(String value) throws RrdException {
+        try {
+            return Integer.parseInt(value);
+        }
+        catch (NumberFormatException nfe) {
+            throw new RrdException(nfe);
+        }
+    }
+
+    static double parseDouble(String value) throws RrdException {
+        if (value.equals("U")) {
+            return Double.NaN;
+        }
+        try {
+            return Double.parseDouble(value);
+        }
+        catch (NumberFormatException nfe) {
+            throw new RrdException(nfe);
+        }
+    }
+
+    static void print(String s) {
+        if (standardOutUsed) {
+            System.out.print(s);
+        }
+    }
+
+    static void println(String s) {
+        if (standardOutUsed) {
+            System.out.println(s);
+        }
+    }
+
+    static RrdDb getRrdDbReference(String path) throws IOException, RrdException {
+        if (rrdDbPoolUsed) {
+            return RrdDbPool.getInstance().requestRrdDb(path);
+        }
+        else {
+            return new RrdDb(path);
+        }
+    }
+
+    static RrdDb getRrdDbReference(String path, String xmlPath) throws IOException, RrdException {
+        if (rrdDbPoolUsed) {
+            return RrdDbPool.getInstance().requestRrdDb(path, xmlPath);
+        }
+        else {
+            return new RrdDb(path, xmlPath);
+        }
+    }
+
+    static RrdDb getRrdDbReference(RrdDef rrdDef) throws IOException, RrdException {
+        if (rrdDbPoolUsed) {
+            return RrdDbPool.getInstance().requestRrdDb(rrdDef);
+        }
+        else {
+            return new RrdDb(rrdDef);
+        }
+    }
+
+    static void releaseRrdDbReference(RrdDb rrdDb) throws IOException, RrdException {
+        if (rrdDbPoolUsed) {
+            RrdDbPool.getInstance().release(rrdDb);
+        }
+        else {
+            rrdDb.close();
+        }
+    }
+}
