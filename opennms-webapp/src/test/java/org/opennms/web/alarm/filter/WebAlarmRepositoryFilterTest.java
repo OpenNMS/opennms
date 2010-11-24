fe8cdff219fa05fe329381e7e990b6c32fdac327
Benjamin Reed <ranger@opennms.org>
2010-11-24 15:52:11 -0500

    add NetworkElementFactoryContext.xml for unit test

--- opennms-webapp/src/test/java/org/opennms/web/alarm/filter/WebAlarmRepositoryFilterTest.java
+++ opennms-webapp/src/test/java/org/opennms/web/alarm/filter/WebAlarmRepositoryFilterTest.java
@@ -70,6 +70,7 @@ import org.springframework.transaction.annotation.Transactional;
 @ContextConfiguration(locations= {"classpath:/META-INF/opennms/applicationContext-dao.xml",
                                   "classpath*:/META-INF/opennms/component-dao.xml",
                                   "classpath*:/META-INF/opennms/component-service.xml",
+                                  "classpath:/NetworkElementFactoryContext.xml",
                                   "classpath:/daoWebAlarmRepositoryTestContext.xml",
                                   "classpath:/jdbcWebAlarmRepositoryTest.xml"})
 @JUnitTemporaryDatabase()
