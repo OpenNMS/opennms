10c114d1d14c6fe63a63b5be1a8fdee9fc9ded71
Benjamin Reed <rangerrick@befunk.com>
2010-12-11 08:42:22 -0500

    fix generation of faulty xsd

--- opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/foreignsource/DetectorCollection.java
+++ opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/foreignsource/DetectorCollection.java
@@ -1,8 +1,10 @@
 package org.opennms.netmgt.provision.persist.foreignsource;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -15,27 +17,20 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @version $Id: $
  */
 @XmlRootElement(name="detectors")
-public class DetectorCollection extends LinkedList<PluginConfig> {
+public class DetectorCollection implements List<PluginConfig> {
+    private LinkedList<PluginConfig> m_list = null;
 
 	/**
      * 
      */
-    private static final long serialVersionUID = -3054011579260606775L;
+    private static final long serialVersionUID = 10L;
 
-    /**
-	 * <p>Constructor for DetectorCollection.</p>
-	 */
-	public DetectorCollection() {
-        super();
+    public DetectorCollection() {
+        m_list = new LinkedList<PluginConfig>();
     }
-
-    /**
-     * <p>Constructor for DetectorCollection.</p>
-     *
-     * @param c a {@link java.util.Collection} object.
-     */
-    public DetectorCollection(Collection<? extends PluginConfig> c) {
-        super(c);
+    
+    public DetectorCollection(Collection<? extends PluginConfig> collection) {
+        m_list = new LinkedList<PluginConfig>(collection);
     }
 
     /**
@@ -45,7 +40,10 @@ public class DetectorCollection extends LinkedList<PluginConfig> {
      */
     @XmlElement(name="detector")
     public List<PluginConfig> getDetectors() {
-        return this;
+        synchronized(m_list) {
+            if (m_list != null) return m_list;
+        }
+        return null;
     }
 
     /**
@@ -53,9 +51,103 @@ public class DetectorCollection extends LinkedList<PluginConfig> {
      *
      * @param detectors a {@link java.util.List} object.
      */
-    public void setDetectors(List<PluginConfig> detectors) {
-        clear();
-        addAll(detectors);
+    public void setDetectors(final List<PluginConfig> detectors) {
+        synchronized(m_list) {
+            m_list.clear();
+            m_list.addAll(detectors);
+        }
+    }
+
+    public boolean add(final PluginConfig config) {
+        return m_list.add(config);
+    }
+
+    public void add(final int index, final PluginConfig config) {
+        m_list.add(index, config);
+    }
+
+    public boolean addAll(final Collection<? extends PluginConfig> configs) {
+        return m_list.addAll(configs);
+    }
+
+    public boolean addAll(final int index, final Collection<? extends PluginConfig> configs) {
+        return m_list.addAll(index, configs);
+    }
+
+    public void clear() {
+        m_list.clear();
+    }
+
+    public boolean contains(final Object object) {
+        return m_list.contains(object);
+    }
+
+    public boolean containsAll(final Collection<?> objects) {
+        return m_list.containsAll(objects);
+    }
+
+    public PluginConfig get(final int index) {
+        return m_list.get(index);
+    }
+
+    public int indexOf(final Object object) {
+        return m_list.indexOf(object);
+    }
+
+    public boolean isEmpty() {
+        return m_list.isEmpty();
+    }
+
+    public Iterator<PluginConfig> iterator() {
+        return m_list.iterator();
+    }
+
+    public int lastIndexOf(final Object object) {
+        return m_list.lastIndexOf(object);
+    }
+
+    public ListIterator<PluginConfig> listIterator() {
+        return m_list.listIterator();
+    }
+
+    public ListIterator<PluginConfig> listIterator(final int index) {
+        return m_list.listIterator(index);
+    }
+
+    public boolean remove(final Object object) {
+        return m_list.remove(object);
+    }
+
+    public PluginConfig remove(final int index) {
+        return m_list.remove(index);
+    }
+
+    public boolean removeAll(final Collection<?> objects) {
+        return m_list.removeAll(objects);
+    }
+
+    public boolean retainAll(final Collection<?> objects) {
+        return m_list.retainAll(objects);
+    }
+
+    public PluginConfig set(final int index, PluginConfig config) {
+        return m_list.set(index, config);
+    }
+
+    public int size() {
+        return m_list.size();
+    }
+
+    public List<PluginConfig> subList(final int start, int end) {
+        return m_list.subList(start, end);
+    }
+
+    public Object[] toArray() {
+        return m_list.toArray();
+    }
+
+    public <T> T[] toArray(final T[] type) {
+        return m_list.toArray(type);
     }
 }
 
--- opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/foreignsource/ForeignSourceCollection.java
+++ opennms-provision/opennms-provision-persistence/src/main/java/org/opennms/netmgt/provision/persist/foreignsource/ForeignSourceCollection.java
@@ -1,8 +1,10 @@
 package org.opennms.netmgt.provision.persist.foreignsource;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
@@ -16,15 +18,16 @@ import javax.xml.bind.annotation.XmlRootElement;
  * @version $Id: $
  */
 @XmlRootElement(name="foreign-sources")
-public class ForeignSourceCollection extends LinkedList<ForeignSource> {
+public class ForeignSourceCollection implements List<ForeignSource> {
+    private LinkedList<ForeignSource> m_list = null;
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 10L;
 
 	/**
 	 * <p>Constructor for ForeignSourceCollection.</p>
 	 */
 	public ForeignSourceCollection() {
-        super();
+	    m_list = new LinkedList<ForeignSource>();
     }
 
     /**
@@ -33,7 +36,7 @@ public class ForeignSourceCollection extends LinkedList<ForeignSource> {
      * @param c a {@link java.util.Collection} object.
      */
     public ForeignSourceCollection(Collection<? extends ForeignSource> c) {
-        super(c);
+        m_list = new LinkedList<ForeignSource>(c);
     }
 
     /**
@@ -65,5 +68,96 @@ public class ForeignSourceCollection extends LinkedList<ForeignSource> {
     public Integer getCount() {
         return this.size();
     }
-}
 
+    public boolean add(final ForeignSource config) {
+        return m_list.add(config);
+    }
+
+    public void add(final int index, final ForeignSource config) {
+        m_list.add(index, config);
+    }
+
+    public boolean addAll(final Collection<? extends ForeignSource> configs) {
+        return m_list.addAll(configs);
+    }
+
+    public boolean addAll(final int index, final Collection<? extends ForeignSource> configs) {
+        return m_list.addAll(index, configs);
+    }
+
+    public void clear() {
+        m_list.clear();
+    }
+
+    public boolean contains(final Object object) {
+        return m_list.contains(object);
+    }
+
+    public boolean containsAll(final Collection<?> objects) {
+        return m_list.containsAll(objects);
+    }
+
+    public ForeignSource get(final int index) {
+        return m_list.get(index);
+    }
+
+    public int indexOf(final Object object) {
+        return m_list.indexOf(object);
+    }
+
+    public boolean isEmpty() {
+        return m_list.isEmpty();
+    }
+
+    public Iterator<ForeignSource> iterator() {
+        return m_list.iterator();
+    }
+
+    public int lastIndexOf(final Object object) {
+        return m_list.lastIndexOf(object);
+    }
+
+    public ListIterator<ForeignSource> listIterator() {
+        return m_list.listIterator();
+    }
+
+    public ListIterator<ForeignSource> listIterator(final int index) {
+        return m_list.listIterator(index);
+    }
+
+    public boolean remove(final Object object) {
+        return m_list.remove(object);
+    }
+
+    public ForeignSource remove(final int index) {
+        return m_list.remove(index);
+    }
+
+    public boolean removeAll(final Collection<?> objects) {
+        return m_list.removeAll(objects);
+    }
+
+    public boolean retainAll(final Collection<?> objects) {
+        return m_list.retainAll(objects);
+    }
+
+    public ForeignSource set(final int index, ForeignSource config) {
+        return m_list.set(index, config);
+    }
+
+    public int size() {
+        return m_list.size();
+    }
+
+    public List<ForeignSource> subList(final int start, int end) {
+        return m_list.subList(start, end);
+    }
+
+    public Object[] toArray() {
+        return m_list.toArray();
+    }
+
+    public <T> T[] toArray(final T[] type) {
+        return m_list.toArray(type);
+    }
+}
