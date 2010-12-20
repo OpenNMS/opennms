62057472ec70049ce92bd80d6342815417860b15
Benjamin Reed <ranger@opennms.org>
2010-12-20 15:36:29 -0500

    Merged the features/lowmem-surv-views branch
    This branch uses a very different algorithm for computing the surveillance
    view list.  All of the work is now done in the database, which is much more
    efficient.

--- opennms-webapp/src/test/java/org/opennms/web/svclayer/support/DefaultSurveillanceServiceIntegrationTest.java
+++ opennms-webapp/src/test/java/org/opennms/web/svclayer/support/DefaultSurveillanceServiceIntegrationTest.java
@@ -87,11 +87,15 @@ public class DefaultSurveillanceServiceIntegrationTest extends AbstractTransacti
        assertNotNull(m_databasePopulator);
        m_databasePopulator.populateDatabase();
        
+       setDefaultRollback(false);
+       endTransaction();
+       startNewTransaction();
+              
         String viewName = "default";
         SimpleWebTable table = m_surveillanceService.createSurveillanceTable(viewName, new ProgressMonitor() {
 
 			public void beginNextPhase(String string) {
-							
+			    System.err.println("PHASE: " + string);
 			}
 
 			public void setPhaseCount(int i) {
