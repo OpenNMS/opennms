457ed187602657314a199b9e15e2462a240902cb
Seth Leger <seth@opennms.org>
2011-01-21 11:48:19 -0500

    Updated the UserGroupLdapAuthoritiesPopulator so that it uses inheritance to extend the behavior of DefaultLdapAuthoritiesPopulator.

--- features/springframework-security/src/main/java/org/opennms/web/springframework/security/UserGroupLdapAuthoritiesPopulator.java
+++ features/springframework-security/src/main/java/org/opennms/web/springframework/security/UserGroupLdapAuthoritiesPopulator.java
@@ -1,23 +1,7 @@
-/* Copyright 2004, 2005, 2006 Acegi Technology Pty Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.opennms.web.springframework.security;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -32,234 +16,119 @@ import org.springframework.security.GrantedAuthority;
 import org.springframework.security.GrantedAuthorityImpl;
 import org.springframework.security.ldap.LdapAuthoritiesPopulator;
 import org.springframework.security.ldap.SpringSecurityLdapTemplate;
-import org.springframework.util.Assert;
-
+import org.springframework.security.ldap.populator.DefaultLdapAuthoritiesPopulator;
 
 /**
- * <p>UserGroupLdapAuthoritiesPopulator class.</p>
- *
- * @author Luke Taylor
- * @version $Id: DefaultLdapAuthoritiesPopulator.java 3260 2008-08-26 12:38:02Z luke_t $
+ * This class adds the ability to provide a concrete map of associations between specific
+ * group values and roles. These associations can be provided by setting the <code>groupToRoleMap</code>
+ * property either in a Spring context file or by calling {@link #setGroupToRoleMap(Map)}.
  */
-public class UserGroupLdapAuthoritiesPopulator implements LdapAuthoritiesPopulator {
-    //~ Static fields/initializers =====================================================================================
+public class UserGroupLdapAuthoritiesPopulator extends DefaultLdapAuthoritiesPopulator implements LdapAuthoritiesPopulator {
 
-    private static final Log logger = LogFactory.getLog(UserGroupLdapAuthoritiesPopulator.class);
+	private final Log logger = LogFactory.getLog(UserGroupLdapAuthoritiesPopulator.class);
 
-    //~ Instance fields ================================================================================================
+	private final SearchControls searchControls = new SearchControls();
 
-    /**
-     * A default role which will be assigned to all authenticated users if set
-     */
-    private GrantedAuthority defaultRole;
+	private final SpringSecurityLdapTemplate ldapTemplate;
 
-    private SpringSecurityLdapTemplate ldapTemplate;
+	/**
+	 * Default value is <code>cn</code>.
+	 */
+	private String groupRoleAttribute = "cn";
 
-    /**
-     * Controls used to determine whether group searches should be performed over the full sub-tree from the
-     * base DN. Modified by searchSubTree property
-     */
-    private SearchControls searchControls = new SearchControls();
+	/**
+	 * Default value is <code>(member={0})</code>.
+	 */
+	private String groupSearchFilter = "(member={0})";
 
-    /**
-     * The ID of the attribute which contains the role name for a group
-     */
-    private String groupRoleAttribute = "cn";
+	/**
+	 * Map is empty by default.
+	 */
+	private Map<String, List<String>> groupToRoleMap = new HashMap<String, List<String>>();
 
-    /**
-     * The base DN from which the search for group membership should be performed
-     */
-    private String groupSearchBase;
+	public UserGroupLdapAuthoritiesPopulator(ContextSource contextSource, String groupSearchBase) {
+		super(contextSource, groupSearchBase);
+		this.ldapTemplate = new SpringSecurityLdapTemplate(contextSource);
+		this.ldapTemplate.setSearchControls(searchControls);
+	}
 
-    /**
-     * The pattern to be used for the user search. {0} is the user's DN
-     */
-    private String groupSearchFilter = "(member={0})";
-    
-    
-    private Map<String, List<String>> groupToRoleMap = new HashMap<String, List<String>>();
+	/**
+	 *
+	 * This function returns a list of roles from the given set of groups
+	 * based on the value of the <code>groupToRoleMap</code> property.
+	 * 
+	 * @return a {@link java.util.Set} object.
+	 */
+	@Override
+	protected Set<GrantedAuthority> getAdditionalRoles(DirContextOperations user, String username) {
+		String userDn = user.getNameInNamespace();
+		Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
 
-    //~ Constructors ===================================================================================================
+		if (getGroupSearchBase() == null) {
+			return authorities;
+		}
 
-    /**
-     * Constructor for group search scenarios. <tt>userRoleAttributes</tt> may still be
-     * set as a property.
-     *
-     * @param contextSource supplies the contexts used to search for user roles.
-     * @param groupSearchBase          if this is an empty string the search will be performed from the root DN of the
-     *                                 context factory.
-     */
-    public UserGroupLdapAuthoritiesPopulator(ContextSource contextSource, String groupSearchBase) {
-        Assert.notNull(contextSource, "contextSource must not be null");
-        ldapTemplate = new SpringSecurityLdapTemplate(contextSource);
-        ldapTemplate.setSearchControls(searchControls);
-        setGroupSearchBase(groupSearchBase);
-    }
+		if (logger.isDebugEnabled()) {
+			logger.debug("Searching for roles for user '" + username + "', DN = " + "'" + userDn + "', with filter "
+					+ groupSearchFilter + " in search base '" + getGroupSearchBase() + "'");
+		}
 
-    //~ Methods ========================================================================================================
+		@SuppressWarnings("unchecked")
+		Set<String> userRoles = ldapTemplate.searchForSingleAttributeValues(
+				getGroupSearchBase(), 
+				groupSearchFilter,
+				new String[]{userDn, username}, 
+				groupRoleAttribute
+		);
 
-    /**
-     * {@inheritDoc}
-     *
-     * Obtains the authorities for the user who's directory entry is represented by
-     * the supplied LdapUserDetails object.
-     */
-    public final GrantedAuthority[] getGrantedAuthorities(DirContextOperations user, String username) {
-        String userDn = user.getNameInNamespace();
+		for(String group : userRoles) {
+			List<String> rolesForGroup = groupToRoleMap.get(group);
+			logger.debug("Checking " + group + " for an associated role");
+			if (rolesForGroup != null) {
+				for(String role : rolesForGroup) {
+					authorities.add(new GrantedAuthorityImpl(role));
+					logger.debug("Added role: " + role + " based on group " + group);
+				}
+			}
+		}
 
-        if (logger.isDebugEnabled()) {
-            logger.debug("Getting authorities for user " + userDn);
-        }
+		return authorities;
+	}
 
-        Set<GrantedAuthority> roles = getGroupMembershipRoles(userDn, username);
+	@Override
+	public void setGroupRoleAttribute(String groupRoleAttribute) {
+		super.setGroupRoleAttribute(groupRoleAttribute);
+		this.groupRoleAttribute = groupRoleAttribute;
+	}
 
-        if (defaultRole != null) {
-            roles.add(defaultRole);
-            logger.debug("Added default role: " + defaultRole);
-        }
+	@Override
+	public void setGroupSearchFilter(String groupSearchFilter) {
+		super.setGroupSearchFilter(groupSearchFilter);
+		this.groupSearchFilter = groupSearchFilter;
+	}
 
-        return (GrantedAuthority[]) roles.toArray(new GrantedAuthority[roles.size()]);
-    }
-
-    /**
-     * <p>getGroupMembershipRoles</p>
-     *
-     * @param userDn a {@link java.lang.String} object.
-     * @param username a {@link java.lang.String} object.
-     * @return a {@link java.util.Set} object.
-     */
-    @SuppressWarnings("unchecked")
-	public Set<GrantedAuthority> getGroupMembershipRoles(String userDn, String username) {
-        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
-
-        if (getGroupSearchBase() == null) {
-            return authorities;
-        }
-
-        logger.debug("Searching for groups for user '" + username + "', DN = " + "'" + userDn + "', with filter "
-                    + groupSearchFilter + " in search base '" + getGroupSearchBase() + "'");
-
-        Set<String> groups = ldapTemplate.searchForSingleAttributeValues(getGroupSearchBase(), groupSearchFilter,
-                new String[]{userDn, username}, groupRoleAttribute);
-
-        logger.debug("Groups from search: " + groups); 
-        
-        
-        Set<String> roles = getRolesFromGroups(groups);
-
-        Iterator<String> it = roles.iterator();
-        while (it.hasNext()) {
-            String role = (String) it.next();
-
-            authorities.add(new GrantedAuthorityImpl(role));
-        }
-
-        return authorities;
-    }
-    
-    /**
-     * <p>getRolesFromGroups</p>
-     *
-     * @param groups a {@link java.util.Set} object.
-     * @return a {@link java.util.Set} object.
-     */
-    protected Set<String> getRolesFromGroups(Set<String> groups) {
-         
-        Set<String> roles = new HashSet<String>();
-        
-        for(String group : groups) {
-            List<String> rolesForGroup = groupToRoleMap.get(group);
-            logger.debug("Checking " + group + " for an associated role");
-            if (rolesForGroup != null) {
-                for(String role : rolesForGroup) {
-                    roles.add(role);
-                    logger.debug("Added role: " + role + " based on group " + group);
-                }
-            }
-        }
-        
-        return roles;
-        
-    }
-
-    /**
-     * <p>getContextSource</p>
-     *
-     * @return a {@link org.springframework.ldap.core.ContextSource} object.
-     */
-    protected ContextSource getContextSource() {
-        return ldapTemplate.getContextSource();
-    }
-
-    /**
-     * Set the group search base (name to search under)
-     *
-     * @param groupSearchBase if this is an empty string the search will be performed from the root DN of the context
-     *                        factory.
-     */
-    private void setGroupSearchBase(String groupSearchBase) {
-        Assert.notNull(groupSearchBase, "The groupSearchBase (name to search under), must not be null.");
-        this.groupSearchBase = groupSearchBase;
-        if (groupSearchBase.length() == 0) {
-            logger.info("groupSearchBase is empty. Searches will be performed from the context source base");
-        }
-    }
-
-    /**
-     * <p>Getter for the field <code>groupSearchBase</code>.</p>
-     *
-     * @return a {@link java.lang.String} object.
-     */
-    protected String getGroupSearchBase() {
-        return groupSearchBase;
-    }
-    
-    /**
-     * <p>Setter for the field <code>groupToRoleMap</code>.</p>
-     *
-     * @param groupToRoleMap a {@link java.util.Map} object.
-     */
-    public void setGroupToRoleMap(Map<String, List<String>> groupToRoleMap) {
-        this.groupToRoleMap = groupToRoleMap;
-    }
-
-    /**
-     * The default role which will be assigned to all users.
-     *
-     * @param defaultRole the role name, including any desired prefix.
-     */
-    public void setDefaultRole(String defaultRole) {
-        Assert.notNull(defaultRole, "The defaultRole property cannot be set to null");
-        this.defaultRole = new GrantedAuthorityImpl(defaultRole);
-    }
-
-    /**
-     * <p>Setter for the field <code>groupRoleAttribute</code>.</p>
-     *
-     * @param groupRoleAttribute a {@link java.lang.String} object.
-     */
-    public void setGroupRoleAttribute(String groupRoleAttribute) {
-        Assert.notNull(groupRoleAttribute, "groupRoleAttribute must not be null");
-        this.groupRoleAttribute = groupRoleAttribute;
-    }
-
-    /**
-     * <p>Setter for the field <code>groupSearchFilter</code>.</p>
-     *
-     * @param groupSearchFilter a {@link java.lang.String} object.
-     */
-    public void setGroupSearchFilter(String groupSearchFilter) {
-        Assert.notNull(groupSearchFilter, "groupSearchFilter must not be null");
-        this.groupSearchFilter = groupSearchFilter;
-    }
-
-    /**
-     * If set to true, a subtree scope search will be performed. If false a single-level search is used.
-     *
-     * @param searchSubtree set to true to enable searching of the entire tree below the <tt>groupSearchBase</tt>.
-     */
-    public void setSearchSubtree(boolean searchSubtree) {
-        int searchScope = searchSubtree ? SearchControls.SUBTREE_SCOPE : SearchControls.ONELEVEL_SCOPE;
-        searchControls.setSearchScope(searchScope);
-    }
+	/**
+	 * <p>This property contains a set of group to role mappings. Both values are specified
+	 * as string values.</p>
+	 * 
+	 * <p>An example Spring context that sets this property could be:</p>
+	 * 
+	 * <pre>
+	 * <code>
+	 * &lt;property xmlns="http://www.springframework.org/schema/beans" name="groupToRoleMap"&gt;
+	 *   &lt;map&gt;
+	 *     &lt;entry&gt;
+	 *       &lt;key&gt;&lt;value&gt;CompanyX_OpenNMS_User_Group&lt;/value&gt;&lt;/key&gt;
+	 *       &lt;list&gt;
+	 *         &lt;value&gt;ROLE_USER&lt;/value&gt;
+	 *       &lt;/list&gt;
+	 *     &lt;/entry&gt;
+	 *   &lt;/map&gt; 
+	 * &lt;/property&gt;
+	 * </code>
+	 * </pre>
+	 */
+	public void setGroupToRoleMap(Map<String, List<String>> groupToRoleMap) {
+		this.groupToRoleMap = groupToRoleMap;
+	}
 }
