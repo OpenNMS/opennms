2b9ff3e0ab1ea2d5c46b44608a7160bc440a02d3
Donald Desloge <desloge@opennms.com>
2011-02-03 15:56:54 -0500

    Update ColonSplitter to havndle paths for Windows

--- integrations/opennms-jasper-extensions/src/test/java/org/opennms/netmgt/jasper/jrobin/RrdXportCmdTest.java
+++ integrations/opennms-jasper-extensions/src/test/java/org/opennms/netmgt/jasper/jrobin/RrdXportCmdTest.java
@@ -1,5 +1,6 @@
 package org.opennms.netmgt.jasper.jrobin;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
@@ -22,5 +23,15 @@ public class RrdXportCmdTest {
 
     private String getQueryString() {
         return "--start 1287005100 --end 1287018990 DEF:xx=src/test/resources/http-8980.jrb:http-8980:AVERAGE DEF:zz=src/test/resources/ssh.jrb:ssh:AVERAGE XPORT:xx:HttpLatency XPORT:zz:SshLatency";
+    }
+    
+    private String getWindowsString() {
+        return "--start 1287005100 --end 1287018990 DEF:xx=\\jrbs\\http-8980.jrb:http-8980:AVERAGE DEF:zz=\\jrbs\\ssh.jrb:ssh:AVERAGE XPORT:xx:HttpLatency XPORT:zz:SshLatency";
+    }
+    
+    @Test
+    public void testColonSplitter() throws RrdException {
+        String[] splitter = new ColonSplitter("DEF:xx=C\\\\:\\jrbs\\http-8980.jrb:http-8980:AVERAGE").split();
+        assertEquals(4, splitter.length);
     }
 }
