ff3750ae2c2b1966d0d3aba42e0a176d7344b1b7
Benjamin Reed <ranger@opennms.org>
2011-02-09 12:32:10 -0500

    update build.bat to error out like build.sh, also add bamboo.pl to clean when building

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-set MAVEN_OPTS=-XX:PermSize=64M -XX:MaxPermSize=256M -Xmx1G
-maven\bin\mvn -Dmaven.test.skip.exec=true %*
+@echo "Don't use this script, use compile.pl and assemble.pl instead!"
+@exit 1
