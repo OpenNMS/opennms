1955bf481dfad2910920a15584537a8e92dd60b1
Benjamin Reed <ranger@opennms.org>
2011-03-25 10:47:48 -0400

    fix test to use the new infrastructure

--- opennms-model/src/main/java/org/opennms/netmgt/model/NetworkBuilder.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/NetworkBuilder.java
@@ -37,7 +37,7 @@ package org.opennms.netmgt.model;
 
 import java.util.Date;
 
-import org.opennms.core.utils.ThreadCategory;
+import org.opennms.core.utils.LogUtils;
 import org.opennms.netmgt.model.OnmsArpInterface.StatusType;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
@@ -51,7 +51,7 @@ import org.springframework.beans.BeansException;
  */
 public class NetworkBuilder {
 
-	OnmsDistPoller m_distPoller;
+	private final OnmsDistPoller m_distPoller;
 
 	OnmsNode m_currentNode;
 	
@@ -68,7 +68,7 @@ public class NetworkBuilder {
 	 *
 	 * @param distPoller a {@link org.opennms.netmgt.model.OnmsDistPoller} object.
 	 */
-	public NetworkBuilder(OnmsDistPoller distPoller) {
+	public NetworkBuilder(final OnmsDistPoller distPoller) {
 		m_distPoller = distPoller;
 	}
 	
@@ -78,7 +78,7 @@ public class NetworkBuilder {
 	 * @param name a {@link java.lang.String} object.
 	 * @param ipAddress a {@link java.lang.String} object.
 	 */
-	public NetworkBuilder(String name, String ipAddress) {
+	public NetworkBuilder(final String name, final String ipAddress) {
 	    m_distPoller = new OnmsDistPoller(name, ipAddress);
 	}
 
@@ -105,7 +105,7 @@ public class NetworkBuilder {
     public class NodeBuilder {
         OnmsNode m_node;
         
-        NodeBuilder(OnmsNode node) {
+        NodeBuilder(final OnmsNode node) {
             m_node = node;
         }
         
@@ -113,17 +113,17 @@ public class NetworkBuilder {
             return m_node;
         }
         
-        public NodeBuilder setId(Integer id) {
+        public NodeBuilder setId(final Integer id) {
             m_node.setId(id);
             return this;
         }
         
-        public NodeBuilder setForeignSource(String foreignSource) {
+        public NodeBuilder setForeignSource(final String foreignSource) {
             m_node.setForeignSource(foreignSource);
             return this;
         }
         
-        public NodeBuilder setForeignId(String foreignId) {
+        public NodeBuilder setForeignId(final String foreignId) {
             m_node.setForeignId(foreignId);
             return this;
         }
@@ -132,18 +132,18 @@ public class NetworkBuilder {
             return m_node.getAssetRecord();
         }
 
-        public NodeBuilder setLabelSource(String labelSource) {
+        public NodeBuilder setLabelSource(final String labelSource) {
             m_node.setLabelSource(labelSource);
             return this;
             
         }
 
-        public NodeBuilder setType(String type) {
+        public NodeBuilder setType(final String type) {
             m_node.setType(type);
             return this;
         }
         
-        public NodeBuilder setSysObjectId(String sysObjectId) {
+        public NodeBuilder setSysObjectId(final String sysObjectId) {
             m_node.setSysObjectId(sysObjectId);
             return this;
         }
@@ -157,27 +157,25 @@ public class NetworkBuilder {
      * @param ipAddr a {@link java.lang.String} object.
      * @return a {@link org.opennms.netmgt.model.NetworkBuilder.InterfaceBuilder} object.
      */
-    public InterfaceBuilder addInterface(String ipAddr) {
+    public InterfaceBuilder addInterface(final String ipAddr) {
 		m_currentIf = new OnmsIpInterface(ipAddr, m_currentNode);
-//        return m_currentIf;
         return new InterfaceBuilder(m_currentIf);
 	}
 
     public class InterfaceBuilder {
-		OnmsIpInterface m_iface;
+    	final OnmsIpInterface m_iface;
 
-		InterfaceBuilder(OnmsIpInterface iface) {
+		InterfaceBuilder(final OnmsIpInterface iface) {
 			m_iface = iface;
 		}
 
-		public InterfaceBuilder setIsManaged(String managed) {
+		public InterfaceBuilder setIsManaged(final String managed) {
 			m_iface.setIsManaged(managed);
 			return this;
 		}
 
-		public InterfaceBuilder setIsSnmpPrimary(String isSnmpPrimary) {
-			m_iface.setIsSnmpPrimary(OnmsIpInterface.PrimaryType
-					.get(isSnmpPrimary));
+		public InterfaceBuilder setIsSnmpPrimary(final String isSnmpPrimary) {
+			m_iface.setIsSnmpPrimary(OnmsIpInterface.PrimaryType.get(isSnmpPrimary));
 			return this;
 		}
 
@@ -185,8 +183,8 @@ public class NetworkBuilder {
 			return m_iface;
 		}
 
-		public SnmpInterfaceBuilder addSnmpInterface(int ifIndex) {
-		    OnmsSnmpInterface snmpIf = new OnmsSnmpInterface(m_currentNode, ifIndex);
+		public SnmpInterfaceBuilder addSnmpInterface(final int ifIndex) {
+			final OnmsSnmpInterface snmpIf = new OnmsSnmpInterface(m_currentNode, ifIndex);
 		    m_iface.setSnmpInterface(snmpIf);
             // TODO: Should this be done in setSnmpInterface?
             snmpIf.getIpInterfaces().add(m_iface);
@@ -194,30 +192,30 @@ public class NetworkBuilder {
 
 		}
 
-		public InterfaceBuilder setId(int id) {
+		public InterfaceBuilder setId(final int id) {
 		    m_iface.setId(id);
             return this;
 		}
 	}
     
     public class AtInterfaceBuilder {
-        OnmsArpInterface m_iface;
+    	final OnmsArpInterface m_iface;
 
-        AtInterfaceBuilder(OnmsArpInterface iface) {
+        AtInterfaceBuilder(final OnmsArpInterface iface) {
             m_iface = iface;
         }
 
-        public AtInterfaceBuilder setStatus(char managed) {
+        public AtInterfaceBuilder setStatus(final char managed) {
             m_iface.setStatus(StatusType.get(managed));
             return this;
         }
 
-        public AtInterfaceBuilder setIfIndex(int ifIndex) {
+        public AtInterfaceBuilder setIfIndex(final int ifIndex) {
             m_iface.setIfIndex(ifIndex);
             return this;
         }
 
-        public AtInterfaceBuilder setSourceNode(OnmsNode node) {
+        public AtInterfaceBuilder setSourceNode(final OnmsNode node) {
             m_iface.setSourceNode(node);
             return this;
         }
@@ -226,12 +224,12 @@ public class NetworkBuilder {
             return m_iface;
         }
 
-        public AtInterfaceBuilder setId(int id) {
+        public AtInterfaceBuilder setId(final int id) {
             m_iface.setId(id);
             return this;
         }
 
-        public AtInterfaceBuilder setLastPollTime(Date timestamp) {
+        public AtInterfaceBuilder setLastPollTime(final Date timestamp) {
             m_iface.setLastPoll(timestamp);
             return this;
         }
@@ -244,7 +242,7 @@ public class NetworkBuilder {
      * @param snmpInterface a {@link org.opennms.netmgt.model.OnmsSnmpInterface} object.
      * @return a {@link org.opennms.netmgt.model.NetworkBuilder.InterfaceBuilder} object.
      */
-    public InterfaceBuilder addInterface(String ipAddr, OnmsSnmpInterface snmpInterface) {
+    public InterfaceBuilder addInterface(final String ipAddr, final OnmsSnmpInterface snmpInterface) {
         m_currentIf = new OnmsIpInterface(ipAddr, m_currentNode);
         m_currentIf.setSnmpInterface(snmpInterface);
         return new InterfaceBuilder(m_currentIf);
@@ -252,7 +250,7 @@ public class NetworkBuilder {
 
     /**
      */
-    public AtInterfaceBuilder addAtInterface(OnmsNode sourceNode, String ipAddr, String physAddr) {
+    public AtInterfaceBuilder addAtInterface(final OnmsNode sourceNode, final String ipAddr, final String physAddr) {
         m_currentAtIf = new OnmsArpInterface(sourceNode, m_currentNode, ipAddr, physAddr);
         return new AtInterfaceBuilder(m_currentAtIf);
     }
@@ -264,9 +262,8 @@ public class NetworkBuilder {
 	 *
 	 * @return a {@link org.opennms.netmgt.model.SnmpInterfaceBuilder} object.
 	 */
-	public SnmpInterfaceBuilder addSnmpInterface(int ifIndex) {
-		OnmsSnmpInterface snmpIf = new OnmsSnmpInterface(m_currentNode, ifIndex);
-		return new SnmpInterfaceBuilder(snmpIf);
+	public SnmpInterfaceBuilder addSnmpInterface(final int ifIndex) {
+		return new SnmpInterfaceBuilder(new OnmsSnmpInterface(m_currentNode, ifIndex));
 
 	}
 
@@ -276,7 +273,7 @@ public class NetworkBuilder {
 	 * @param serviceType a {@link org.opennms.netmgt.model.OnmsServiceType} object.
 	 * @return a {@link org.opennms.netmgt.model.OnmsMonitoredService} object.
 	 */
-	public OnmsMonitoredService addService(OnmsServiceType serviceType) {
+	public OnmsMonitoredService addService(final OnmsServiceType serviceType) {
 	    if (m_currentIf != null) {
 	        m_currentMonSvc = new OnmsMonitoredService(m_currentIf, serviceType);
 	        return m_currentMonSvc;
@@ -291,7 +288,7 @@ public class NetworkBuilder {
 	 *
 	 * @param displayCategory a {@link java.lang.String} object.
 	 */
-	public void setDisplayCategory(String displayCategory) {
+	public void setDisplayCategory(final String displayCategory) {
 		m_currentNode.getAssetRecord().setDisplayCategory(displayCategory);
 	}
 	
@@ -300,7 +297,7 @@ public class NetworkBuilder {
 	 *
 	 * @param building a {@link java.lang.String} object.
 	 */
-	public void setBuilding(String building){
+	public void setBuilding(final String building) {
 	    m_currentNode.getAssetRecord().setBuilding(building);
 	}
 
@@ -318,7 +315,7 @@ public class NetworkBuilder {
      *
      * @param cat a {@link org.opennms.netmgt.model.OnmsCategory} object.
      */
-    public void addCategory(OnmsCategory cat) {
+    public void addCategory(final OnmsCategory cat) {
         m_currentNode.addCategory(cat);
     }
     
@@ -327,7 +324,7 @@ public class NetworkBuilder {
      *
      * @param categoryName a {@link java.lang.String} object.
      */
-    public void addCategory(String categoryName) {
+    public void addCategory(final String categoryName) {
         addCategory(new OnmsCategory(categoryName));
     }
 
@@ -345,7 +342,7 @@ public class NetworkBuilder {
      * @param serviceName a {@link java.lang.String} object.
      * @return a {@link org.opennms.netmgt.model.OnmsMonitoredService} object.
      */
-    public OnmsMonitoredService addService(String serviceName) {
+    public OnmsMonitoredService addService(final String serviceName) {
         return addService(new OnmsServiceType(serviceName));
     }
 
@@ -355,11 +352,11 @@ public class NetworkBuilder {
      * @param name a {@link java.lang.String} object.
      * @param value a {@link java.lang.String} object.
      */
-    public void setAssetAttribute(String name, String value) {
+    public void setAssetAttribute(final String name, final String value) {
         try {
             m_assetBean.setPropertyValue(name, value);
-        } catch (BeansException e) {
-            ThreadCategory.getInstance(this.getClass()).warn("Could not set property on asset: " + name, e);
+        } catch (final BeansException e) {
+        	LogUtils.warnf(this, e, "Could not set property '%v' on asset '%v'", value, name);
         }
     }
 
