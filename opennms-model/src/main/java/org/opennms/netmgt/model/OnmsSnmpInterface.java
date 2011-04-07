5d085bbf72647de2b0586af1c49800995e2b2dd6
Donald Desloge <desloge@opennms.com>
2011-04-07 15:06:39 -0400

    moved rrd label computation into util class

--- opennms-model/src/main/java/org/opennms/netmgt/model/OnmsSnmpInterface.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/OnmsSnmpInterface.java
@@ -60,6 +60,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
 import org.opennms.core.utils.AlphaNumeric;
+import org.opennms.core.utils.RrdLabelUtils;
 import org.opennms.core.utils.ThreadCategory;
 import org.springframework.core.style.ToStringCreator;
 
@@ -670,9 +671,7 @@ public class OnmsSnmpInterface extends OnmsEntity implements Serializable {
      * @return a {@link java.lang.String} object.
      */
     public String computeLabelForRRD() {
-        String name = computeNameForRRD();
-        String physAddrForRRD = computePhysAddrForRRD();
-        return (physAddrForRRD == null ? name : name + '-' + physAddrForRRD);
+        return RrdLabelUtils.computeLabelForRRD(getIfName(), getIfDescr(), getPhysAddr());
     }
 
     /**
