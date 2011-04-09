0a1d63c50cfdd4270bd3ea5bbe5a0b31a97eec00
Antonio <rssntn67@yahoo.it>
2011-04-09 10:28:00 +0200

    CEMUS-29 Finished implenting EventFilter interface

---
+++ opennms-tools/scriptd-event-proxy/src/main/java/org/opennms/netmgt/scriptd/helper/EventFilter.java
@@ -0,0 +1,34 @@
+package org.opennms.netmgt.scriptd.helper;
+
+import org.opennms.netmgt.xml.event.Event;
+/**
+ * An EventFilter is a filter of Events
+ * An implementation of this interface is a class
+ * where you have some criteria to decide if the Event
+ * pass the filter or not
+ * 
+ * @author antonio
+ *
+ */
+public interface EventFilter {
+
+	/**
+	 * Method to decide if the filter
+	 * must be applied
+	 * 
+	 * @param event
+	 * @return
+	 */
+	boolean match(Event event);
+
+	/**
+	 * Method to decide if the event 
+	 * should be filtered or not
+	 * 
+	 * @return event
+	 * the filtered Event
+	 * that can be null
+	 */
+	Event filter();
+
+}
