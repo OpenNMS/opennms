3f2f307d9021e478388b52f7240d490153875f4c
Antonio <rssntn67@yahoo.it>
2011-04-09 11:03:08 +0200

    CEMUS-39 created Abstract Class AbstractEventForwarder
    The Abstract class is a standard implementation for the
    EventForwarder Interface, also is a abstract method
    to expand the event definition

---
+++ opennms-tools/scriptd-event-proxy/src/main/java/org/opennms/netmgt/scriptd/helper/AbstractEventForwarder.java
@@ -0,0 +1,23 @@
+package org.opennms.netmgt.scriptd.helper;
+
+import org.opennms.netmgt.xml.event.Event;
+
+public abstract class AbstractEventForwarder implements EventForwarder {
+
+	public void addEventFilter(EventFilter filter) {
+		m_filters.add(filter);
+
+	}
+
+	public Event flushEvent(Event event) {
+		for (EventFilter filter: m_filters) {
+			if (filter.match(event))
+				event = filter.filter(event);
+		}
+		event = expand(event);
+		return event;
+	}
+
+	protected abstract Event expand(Event event);
+
+}
