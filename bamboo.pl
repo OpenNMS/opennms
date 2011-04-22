6cff9d5938e2f71474ec92b033f5041fde5e5a47
Benjamin Reed <ranger@opennms.org>
2011-04-22 15:11:14 -0400

    refactor the scripts to add a deploy script which handles nen cleanly

--- bamboo.pl
+++ bamboo.pl
@@ -10,23 +10,13 @@ use vars qw(
 $PREFIX = abs_path(dirname($0));
 require($PREFIX . "/functions.pl");
 
-my $git=`which git 2>/dev/null`;
-chomp($git);
-if ($git eq "" or not -x $git) {
-	error("Unable to locate git ($git)");
+if (not defined $GIT) {
 	exit 1;
 }
 
-my @command = ($git, "clean", "-fdx", ".");
+my @command = ($GIT, "clean", "-fdx", ".");
 info("running:", @command);
 handle_errors_and_exit_on_failure(system(@command));
-
-my $hostname = `/bin/hostname`;
-chomp($hostname);
-
-if ($hostname eq "nen") {
-	unshift(@ARGS, "-DaltDeploymentRepository=opennms-snapshot::default::file:///var/www/sites/opennms.org/site/repo/snapshots");
-}
 
 @command = ($MVN, @ARGS);
 info("running:", @command);
