9ce1d538f869dec7952590d12edccaa54430cc83
Benjamin Reed <ranger@opennms.org>
2011-04-26 08:40:32 -0400

    put it back the old way

--- deploy.pl
+++ deploy.pl
@@ -18,36 +18,16 @@ if (not defined $GIT) {
 
 clean_git();
 
-my @other_args = ();
-
 my $hostname = `hostname 2>/dev/null`;
 chomp($hostname);
 if ($hostname eq "nen") {
 	# special case, on nen we use the local repo
-	push(@other_args, "-DaltDeploymentRepository=opennms-snapshot::default::file:///var/www/sites/opennms.org/site/repo/snapshots");
+	unshift(@ARGS, "-DaltDeploymentRepository=opennms-snapshot::default::file:///var/www/sites/opennms.org/site/repo/snapshots");
 }
 
-my @command = ($MVN, '-Dmaven.test.skip.exec=true', 'install');
+my @command = ($MVN, '-Dmaven.test.skip.exec=true', @ARGS, 'install', 'deploy');
 info("running:", @command);
 handle_errors_and_exit_on_failure(system(@command));
-
-for my $module (@ARGS) {
-	if ($module =~ /^-/) {
-		push(@other_args, $module);
-		next;
-	}
-
-	my $moduledir = $PREFIX . "/" . $module;
-	if (-d $moduledir) {
-		chdir($moduledir);
-		@command = ($MVN, '-Dmaven.test.skip.exec=true', @other_args, 'deploy');
-		info("running:", @command);
-		handle_errors_and_exit_on_failure(system(@command));
-	} else {
-		error("directory $module does not exist in $PREFIX!");
-		exit 1;
-	}
-}
 
 clean_git();
 
