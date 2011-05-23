fa0bfec6c5429afae203cde2a55b7869af66a1aa
Donald Desloge <desloge@opennms.com>
2011-05-23 13:18:40 -0400

    Rename package

--- features/isoc-ipv6-gui/src/main/java/org/opennms/isoc/ipv6/gui/client/HostUpdateEventHandler.java
+++
@@ -1,7 +0,0 @@
-package org.opennms.isoc.ipv6.gui.client;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface HostUpdateEventHandler extends EventHandler {
-    void onHostUpdate(HostUpdateEvent event);
-}
