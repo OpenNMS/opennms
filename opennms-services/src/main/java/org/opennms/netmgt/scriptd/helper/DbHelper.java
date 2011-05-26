63928e1678722dd876207280ffdb69b39611cff9
Antonio <rssntn67@yahoo.it>
2011-05-26 11:49:49 +0200

    NMS-4721 DbHelper class now use nodeDao

--- opennms-services/src/main/java/org/opennms/netmgt/scriptd/helper/DbHelper.java
+++ opennms-services/src/main/java/org/opennms/netmgt/scriptd/helper/DbHelper.java
@@ -1,15 +1,20 @@
 package org.opennms.netmgt.scriptd.helper;
 
+import org.opennms.core.utils.BeanUtils;
 import org.opennms.netmgt.config.DataSourceFactory;
+import org.opennms.netmgt.dao.EventDao;
+import org.opennms.netmgt.dao.NodeDao;
 import org.opennms.netmgt.utils.SingleResultQuerier;
+import org.springframework.beans.factory.access.BeanFactoryReference;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class DbHelper {
 
-	private static String GETNODELABELSTRING = "select nodeLabel from node where nodeId = ?";
 	
 	public static String getNodeLabel(Integer nodeid) {
-		SingleResultQuerier querier = new SingleResultQuerier(DataSourceFactory.getInstance(), GETNODELABELSTRING);
-		querier.execute(nodeid);
-		return (String) querier.getResult();
+    	BeanFactoryReference bf = BeanUtils.getBeanFactory("daoContext");
+        return BeanUtils.getBean(bf,"nodeDao", NodeDao.class)
+        	.get(nodeid).getLabel();
 	}
+	
 }
