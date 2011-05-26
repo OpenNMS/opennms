2cce118f00af145ca65b0b5ecf9d7d6f02f951fd
Antonio <rssntn67@yahoo.it>
2011-05-26 12:03:31 +0200

    NMS-4721 Cleaned up the dependencies

--- opennms-services/src/main/java/org/opennms/netmgt/scriptd/helper/DbHelper.java
+++ opennms-services/src/main/java/org/opennms/netmgt/scriptd/helper/DbHelper.java
@@ -1,12 +1,8 @@
 package org.opennms.netmgt.scriptd.helper;
 
 import org.opennms.core.utils.BeanUtils;
-import org.opennms.netmgt.config.DataSourceFactory;
-import org.opennms.netmgt.dao.EventDao;
 import org.opennms.netmgt.dao.NodeDao;
-import org.opennms.netmgt.utils.SingleResultQuerier;
 import org.springframework.beans.factory.access.BeanFactoryReference;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class DbHelper {
 
