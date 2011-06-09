1ee139943cca038f81946e30c45fc5287be994fb
Seth Leger <seth@opennms.org>
2011-06-09 17:24:53 -0400

    Updated AuthorizationTest with JUnit annotations.

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/AuthorizationTest.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/AuthorizationTest.java
@@ -35,22 +35,79 @@
 //
 package org.opennms.netmgt.dao;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opennms.netmgt.dao.db.JUnitTemporaryDatabase;
+import org.opennms.netmgt.dao.db.OpenNMSConfigurationExecutionListener;
+import org.opennms.netmgt.dao.db.TemporaryDatabaseExecutionListener;
 import org.opennms.netmgt.dao.hibernate.AlarmDaoHibernate;
 import org.opennms.netmgt.model.OnmsAlarm;
 import org.opennms.netmgt.model.OnmsCategory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 
-public class AuthorizationTest extends AbstractTransactionalDaoTestCase {
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestExecutionListeners({
+    OpenNMSConfigurationExecutionListener.class,
+    TemporaryDatabaseExecutionListener.class,
+    DependencyInjectionTestExecutionListener.class,
+    DirtiesContextTestExecutionListener.class,
+    TransactionalTestExecutionListener.class
+})
+@ContextConfiguration(locations={
+        "classpath:/META-INF/opennms/applicationContext-dao.xml",
+        "classpath*:/META-INF/opennms/component-dao.xml",
+        "classpath:/META-INF/opennms/applicationContext-databasePopulator.xml",
+        "classpath:/META-INF/opennms/applicationContext-setupIpLike-enabled.xml"
+})
+@JUnitTemporaryDatabase
+public class AuthorizationTest implements InitializingBean {
+    
+    @Autowired
+    AlarmDao m_alarmDao;
 
-   public void testAuthorizedAlarms() {
+    @Autowired
+    CategoryDao m_categoryDao;
+
+    @Autowired
+    DatabasePopulator m_populator;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        assertNotNull(m_alarmDao);
+        assertNotNull(m_categoryDao);
+        assertNotNull(m_populator);
+    }
+
+    @Before
+    @Transactional
+    public void setUp() throws Exception {
+        m_populator.populateDatabase();
+    }
+
+    @Test
+    @Transactional
+    public void testAuthorizedAlarms() {
        
-       Collection<OnmsAlarm> matching = getAlarmDao().findAll();
+       Collection<OnmsAlarm> matching = m_alarmDao.findAll();
        
        assertNotNull(matching);
        assertEquals(1, matching.size());
@@ -59,7 +116,7 @@ public class AuthorizationTest extends AbstractTransactionalDaoTestCase {
        
        enableAuthorizationFilter("NonExistentGroup");
 
-       Collection<OnmsAlarm> matching2 = getAlarmDao().findAll();
+       Collection<OnmsAlarm> matching2 = m_alarmDao.findAll();
 
        assertNotNull(matching2);
        assertEquals(0, matching2.size());
@@ -68,7 +125,7 @@ public class AuthorizationTest extends AbstractTransactionalDaoTestCase {
 
        disableAuthorizationFilter();
 
-       Collection<OnmsAlarm> matching3 = getAlarmDao().findAll();
+       Collection<OnmsAlarm> matching3 = m_alarmDao.findAll();
 
        assertNotNull(matching3);
        assertEquals(1, matching3.size());
@@ -76,9 +133,11 @@ public class AuthorizationTest extends AbstractTransactionalDaoTestCase {
        System.err.println(matching3);
     }
    
+   @Test
+   @Transactional
    public void testGetCategoriesWithAuthorizedGroups() {
        
-       List<OnmsCategory> categories = getCategoryDao().getCategoriesWithAuthorizedGroup("RoutersGroup");
+       List<OnmsCategory> categories = m_categoryDao.getCategoriesWithAuthorizedGroup("RoutersGroup");
        
        assertNotNull(categories);
        assertEquals(1, categories.size());
@@ -97,7 +156,7 @@ public class AuthorizationTest extends AbstractTransactionalDaoTestCase {
            
        };
        
-       ((AlarmDaoHibernate)getAlarmDao()).getHibernateTemplate().execute(cb);
+       ((AlarmDaoHibernate)m_alarmDao).getHibernateTemplate().execute(cb);
    }
 
    public void disableAuthorizationFilter() {
@@ -111,6 +170,6 @@ public class AuthorizationTest extends AbstractTransactionalDaoTestCase {
            
        };
        
-       ((AlarmDaoHibernate)getAlarmDao()).getHibernateTemplate().execute(cb);
+       ((AlarmDaoHibernate)m_alarmDao).getHibernateTemplate().execute(cb);
    }
 }
