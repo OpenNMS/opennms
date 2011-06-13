88180db636c5d42740dfb0f22c82eabf03bd9de3
Seth Leger <seth@opennms.org>
2011-06-13 18:28:24 -0400

    Updated Drools tests and slightly reorganized the Spring contexts for the unit tests.

--- opennms-correlation/drools-correlation-engine/src/test/java/org/opennms/netmgt/correlation/drools/CorrelationRulesTestCase.java
+++ opennms-correlation/drools-correlation-engine/src/test/java/org/opennms/netmgt/correlation/drools/CorrelationRulesTestCase.java
@@ -17,6 +17,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={
+        "classpath:META-INF/opennms/applicationContext-daemon.xml",
+        "classpath:META-INF/opennms/mockEventIpcManager.xml",
         "classpath:test-context.xml",
         "classpath:META-INF/opennms/correlation-engine.xml"
 })
