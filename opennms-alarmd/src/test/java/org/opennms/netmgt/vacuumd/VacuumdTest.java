8033660f66b04a37bbb84266d8b94b3d84be8e6c
Benjamin Reed <ranger@opennms.org>
2011-06-21 16:59:31 -0500

    clean up licensing (opennms-ackd/ opennms-alarmd/ opennms-bootstrap/ opennms-config-tester/ opennms-config-utils/ opennms-dao/ opennms-model/ opennms-util/)

--- opennms-alarmd/src/test/java/org/opennms/netmgt/vacuumd/VacuumdTest.java
+++ opennms-alarmd/src/test/java/org/opennms/netmgt/vacuumd/VacuumdTest.java
@@ -1,42 +1,27 @@
-/*
+/*******************************************************************************
  * This file is part of the OpenNMS(R) Application.
- * 
- * OpenNMS(R) is Copyright (C) 2005 The OpenNMS Group, Inc.  All rights reserved.
- * OpenNMS(R) is a derivative work, containing both original code, included code and modified
- * code that was published under the GNU General Public License. Copyrights for modified 
- * and included code are below.
  *
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.  All rights reserved.
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
- * Modifications:
- *
- * 2008 Jan 26: Don't call methods directly on Eventd to send events
- *              (they are moving, anyway)--use EventIpcManager. - dj@opennms.org
- * 2007 Jun 10: Use SimpleJdbcTemplate for queries. - dj@opennms.org
- * 2007 Jun 09: Move the config into a test resource. - dj@opennms.org
- * 2007 Mar 13: VacuumdConfigFactory.setConfigReader(Reader) is gone.  Use new VacuumdConfigFactory(Reader) and setInstance, instead. - dj@opennms.org
- *
- * Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
- *
- *  This program is free software; you can redistribute it and/or modify
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * OpenNMS(R) is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.                                                            
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- *    
+ *     along with OpenNMS(R).  If not, see <http://www.gnu.org/licenses/>.
+ *
  * For more information contact: 
- *      OpenNMS Licensing       <license@opennms.org>
- *      http://www.opennms.org/
- *      http://www.opennms.com/
- */
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
 package org.opennms.netmgt.vacuumd;
 
 import static org.junit.Assert.assertEquals;
--- opennms-dao/src/main/java/org/opennms/netmgt/dao/support/NodeResourceType.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/support/NodeResourceType.java
@@ -1,39 +1,27 @@
-//
-// This file is part of the OpenNMS(R) Application.
-//
-// OpenNMS(R) is Copyright (C) 2006 The OpenNMS Group, Inc.  All rights reserved.
-// OpenNMS(R) is a derivative work, containing both original code, included code and modified
-// code that was published under the GNU General Public License. Copyrights for modified
-// and included code are below.
-//
-// OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
-//
-// Modifications:
-//
-// 2007 Apr 10: Store the entity. - dj@opennms.org
-// 2007 Apr 05: Remove getRelativePathForAttribute. - dj@opennms.org
-//
-// Original code base Copyright (C) 1999-2001 Oculan Corp.  All rights reserved.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-//
-// For more information contact:
-//      OpenNMS Licensing       <license@opennms.org>
-//      http://www.opennms.org/
-//      http://www.opennms.com/
-//
+/*******************************************************************************
+ * This file is part of the OpenNMS(R) Application.
+ *
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.  All rights reserved.
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ *     along with OpenNMS(R).  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information contact: 
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
 package org.opennms.netmgt.dao.support;
 
 import java.util.Collection;
@@ -50,12 +38,6 @@ import org.opennms.netmgt.model.OnmsNode;
 import org.opennms.netmgt.model.OnmsResource;
 import org.opennms.netmgt.model.OnmsResourceType;
 
-/**
- * <p>NodeResourceType class.</p>
- *
- * @author ranger
- * @version $Id: $
- */
 public class NodeResourceType implements OnmsResourceType {
     /** Constant <code>s_emptyAttributeSet</code> */
     protected static final Set<OnmsAttribute> s_emptyAttributeSet = Collections.unmodifiableSet(new HashSet<OnmsAttribute>());
