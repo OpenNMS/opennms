c2d01be56ece37ed7038134deed6ec0b94388840
Mike Huot <mhuot@opennms.org>
2011-06-23 20:42:48 -0500

    Converted Autoacknowledge, Autoaction, Correlation, Decode, Forward, and Logmsg

---
+++ opennms-config/src/test/java/org/opennms/netmgt/xml/eventconf/AutoacknowledgeTest.java
@@ -0,0 +1,33 @@
+package org.opennms.netmgt.xml.eventconf;
+
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+import org.opennms.core.test.xml.XmlTest;
+
+public class AutoacknowledgeTest extends XmlTest<Autoacknowledge> {
+
+	public AutoacknowledgeTest(final Autoacknowledge sampleObject, final String sampleXml,
+			final String schemaFile) {
+		super(sampleObject, sampleXml, schemaFile);
+	}
+
+	@Parameters
+	public static Collection<Object[]> data() throws ParseException {
+		Autoacknowledge autoacknowledge0 = new Autoacknowledge();
+		Autoacknowledge autoacknowledge1 = new Autoacknowledge();
+		autoacknowledge1.setContent("These are important data");
+		autoacknowledge1.setState("on");
+		return Arrays.asList(new Object[][] {
+				{autoacknowledge0,
+				"<autoacknowledge/>",
+				"target/classes/xsds/eventconf.xsd" },
+				{autoacknowledge1,
+				"<autoacknowledge state=\"on\">These are important data</autoacknowledge>",
+				"target/classes/xsds/eventconf.xsd" } 
+		});
+	}
+
+}
