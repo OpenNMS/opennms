072d176081998ba3d31be92dbc6d909dbb08c15e
Mike Huot <mhuot@opennms.org>
2011-06-25 21:50:37 -0500

    Converted global, tticket, varbindsdecode from castor to jaxb

---
+++ opennms-config/src/test/java/org/opennms/netmgt/xml/eventconf/TticketTest.java
@@ -0,0 +1,34 @@
+package org.opennms.netmgt.xml.eventconf;
+
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized.Parameters;
+import org.opennms.core.test.xml.XmlTest;
+
+public class TticketTest extends XmlTest<Tticket> {
+
+	public TticketTest(final Tticket sampleObject, final String sampleXml,
+			final String schemaFile) {
+		super(sampleObject, sampleXml, schemaFile);
+	}
+
+	@Parameters
+	public static Collection<Object[]> data() throws ParseException {
+		Tticket tticket0 = new Tticket();
+		tticket0.setContent("This is a test");
+		Tticket tticket1 = new Tticket();
+		tticket1.setContent("This is a test");
+		tticket1.setState("on");
+		return Arrays.asList(new Object[][] {
+				{tticket0,
+				"<tticket>This is a test</tticket>",
+				"target/classes/xsds/eventconf.xsd" },
+				{tticket1,
+				"<tticket state=\"on\">This is a test</tticket>",
+				"target/classes/xsds/eventconf.xsd" } 
+		});
+	}
+
+}
