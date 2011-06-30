3c861adaf4873637dd7cef636204998111c35f79
Donald Desloge <desloge@opennms.com>
2011-06-30 16:19:23 -0400

    adding a module for suggestion combobox

---
+++ features/gwt-suggestion-combobox/src/main/java/org/opennms/features/gwt/combobox/client/OnmsSuggestionCombobox.java
@@ -0,0 +1,18 @@
+package org.opennms.features.gwt.combobox.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class OnmsSuggestionCombobox implements EntryPoint {
+  
+
+  /**
+   * This is the entry point method.
+   */
+  public void onModuleLoad() {
+    Window.alert("your module has been run");
+  }
+}
