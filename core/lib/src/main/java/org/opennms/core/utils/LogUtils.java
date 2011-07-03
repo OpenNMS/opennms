78b29b5ef2addf5e4a7a6e797abb482ab2adef39
Matt Brozowski <brozow@opennms.org>
2011-07-02 20:37:48 -0400

    RSVI-9: Massive memory leak found here keeping a reference to every object that ever logs

--- core/lib/src/main/java/org/opennms/core/utils/LogUtils.java
+++ core/lib/src/main/java/org/opennms/core/utils/LogUtils.java
@@ -1,7 +1,5 @@
 package org.opennms.core.utils;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Properties;
 
 import org.apache.log4j.PropertyConfigurator;
@@ -14,21 +12,6 @@ import org.slf4j.Logger;
  * @version $Id: $
  */
 public class LogUtils {
-    private static class SizeLimitedHashMap extends LinkedHashMap<Object,Logger> {
-        private static final long serialVersionUID = 1L;
-        private int m_maxSize;
-        
-        public SizeLimitedHashMap(final int size) {
-            super(size, 0.75f, true);
-            m_maxSize = size;
-        }
-
-        public boolean removeEldestEntry(final Map.Entry<Object,Logger> entry) {
-            return size() > m_maxSize;
-        }
-    }
-
-    private static final SizeLimitedHashMap m_hot = new SizeLimitedHashMap(100);
 
     /**
      * <p>tracef</p>
@@ -258,12 +241,6 @@ public class LogUtils {
 
 	private static Logger getLogger(final Object logee) {
        Logger log;
-	    synchronized(m_hot) {
-	        log = m_hot.get(logee);
-	        if (log != null) {
-	            return log;
-	        }
-	    }
         if (logee instanceof Class<?>) {
             log = ThreadCategory.getSlf4jInstance((Class<?>)logee);
         } else if (logee instanceof String) {
@@ -271,7 +248,6 @@ public class LogUtils {
         } else {
             log = ThreadCategory.getSlf4jInstance(logee.getClass());
         }
-        m_hot.put(logee, log);
         return log;
     }
 
