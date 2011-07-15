088284238e101cd8736d250b4a0c0ce789977007
pdgrenon <pdgrenon@opennms.org>
2011-07-15 12:49:02 -0400

    Repaired broken unit tests in the ILR

--- features/instrumentationLogReader/src/test/java/org/opennms/util/ilr/FilterTest.java
+++ features/instrumentationLogReader/src/test/java/org/opennms/util/ilr/FilterTest.java
@@ -3,8 +3,10 @@ package org.opennms.util.ilr;
 import static org.junit.Assert.*;
 import static org.opennms.util.ilr.Filter.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
@@ -46,7 +48,7 @@ public class FilterTest {
     public void testCreateIntegerBasedFilter() {
         Predicate<Integer> predicate = filter.createIntegerBasedPredicate(3);
         List<Integer> list = Arrays.asList(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
-        Collection<Integer> actual = Filter.filter(list, predicate);
+        Collection<Integer> actual = filter(list, predicate);
         List<Integer> expected = Arrays.asList(new Integer[] {3});
         assertEquals(expected, actual);
     }
@@ -54,14 +56,14 @@ public class FilterTest {
     public void testCreateStringBasedFilter() {
         Predicate<String> predicate = filter.createStringBasedPredicate("This is a String");
         List<String> list = Arrays.asList(new String[] {"just", "to","let","you","know","This is a String"});
-        Collection<String> actual = Filter.filter(list, predicate);
+        Collection<String> actual = filter(list, predicate);
         List<String> expected = Arrays.asList(new String[] {"This is a String"});
         assertEquals(expected, actual);
     }
     @Test
     public void testFilterEvenNumbersOut() {
         List<Integer> list = Arrays.asList(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
-        Collection<Integer> oddNumbers = Filter.filter(list, new Predicate<Integer>() {
+        Collection<Integer> oddNumbers = filter(list, new Predicate<Integer>() {
             public boolean apply(Integer i) {
                 if (i % 2 != 0) {
                     return true;
@@ -72,30 +74,36 @@ public class FilterTest {
         List<Integer> expected = Arrays.asList(new Integer[] {1, 3, 5, 7, 9});
         assertEquals(expected, oddNumbers);
     }
-
-    @Test
-    public void testCompoundFilters() {
-        assertEquals(1,0);
-        //TODO create "and" for filters
-        //TODO create "or" for filters
-    }
-
-    @Test
-    public void testCustomFilters() {
-        assertEquals(1,0);
-        //TODO create a way to take take parameters as filter creation arguments
-    }
     
-  
     @Test
     public void testFilterServiceCollectorsByServiceID () {
         setup();
         
         final String serviceID = "0/3.3.3.3/SNMP";
         
-        Collection<ServiceCollector> filtered = Filter.filter(c.getServiceCollectors(), and(eq(serviceID(), serviceID), lessThan(collectionCount(), 2)));
+        Collection<ServiceCollector> filtered = filter(c.getServiceCollectors(), and(eq(serviceID(), serviceID), lessThan(collectionCount(), 2)));
         assertEquals(1, filtered.size());
         assertEquals(serviceID, filtered.iterator().next().getServiceID());
+        
+    }
+
+    @Test
+    public void testCompoundFilters() {
+        setup();
+        final String serviceID1 = "0/1.1.1.1/SNMP";
+        final String serviceID2 = "0/2.2.2.2/SNMP";
+        final String serviceID3 = "0/3.3.3.3/SNMP";
+        
+        Collection<ServiceCollector> compoundOrFilter = filter(c.getServiceCollectors(), or(byServiceID(serviceID2), byServiceID(serviceID3)));
+        assertEquals(2, compoundOrFilter.size());
+        assertEquals(serviceID2, compoundOrFilter.iterator().next().getServiceID());
+        
+        
+        Collection<ServiceCollector> compoundAndFilter = filter(c.getServiceCollectors(), and(byServiceID(serviceID1), byServiceID(serviceID2)));
+        assertEquals(0, compoundAndFilter.size());
+        assertFalse(compoundOrFilter.contains(serviceID1));
+        assertFalse(compoundOrFilter.contains(serviceID2));
+        assertFalse(compoundOrFilter.contains(serviceID3));
         
     }
 
@@ -103,7 +111,7 @@ public class FilterTest {
     @Test
     public void testFilterOddNumbersOut() {
         List<Integer> list = Arrays.asList(new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
-        Collection<Integer> evenNumbers = Filter.filter(list, new Predicate<Integer>() {
+        Collection<Integer> evenNumbers = filter(list, new Predicate<Integer>() {
             public boolean apply(Integer i) {
                 if (i % 2 == 0) {
                     return true;
