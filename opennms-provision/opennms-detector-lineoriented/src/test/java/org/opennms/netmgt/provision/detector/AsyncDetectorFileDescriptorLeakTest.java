442ce2345764ff413d631ae0f7cf232b23cb6c32
Benjamin Reed <ranger@opennms.org>
2011-10-06 15:40:16 -0400

    attempt to fix the intermittent async detector test failures

--- opennms-provision/opennms-detector-lineoriented/src/test/java/org/opennms/netmgt/provision/detector/AsyncDetectorFileDescriptorLeakTest.java
+++ opennms-provision/opennms-detector-lineoriented/src/test/java/org/opennms/netmgt/provision/detector/AsyncDetectorFileDescriptorLeakTest.java
@@ -99,7 +99,7 @@ public class AsyncDetectorFileDescriptorLeakTest implements ApplicationContextAw
             
         };
 
-        m_server.setTimeout(10000);
+        m_server.setTimeout(0);
         m_server.init();
         m_server.startServer();
     }
