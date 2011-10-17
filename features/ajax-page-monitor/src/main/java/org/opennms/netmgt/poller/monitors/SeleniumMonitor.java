aee9e79a7af12f8ac4e2faa6c6126d9f93c9f2ba
Donald Desloge <desloge@opennms.com>
2011-10-17 14:02:28 -0400

    Rename monitor to SeleniumMonitor

---
+++ features/ajax-page-monitor/src/main/java/org/opennms/netmgt/poller/monitors/SeleniumMonitor.java
@@ -0,0 +1,130 @@
+package org.opennms.netmgt.poller.monitors;
+
+import groovy.lang.GroovyClassLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.codehaus.groovy.control.CompilationFailedException;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import org.opennms.core.utils.TimeoutTracker;
+import org.opennms.netmgt.junit.runner.SeleniumComputer;
+import org.opennms.netmgt.model.PollStatus;
+import org.opennms.netmgt.poller.MonitoredService;
+
+public class SeleniumMonitor extends AbstractServiceMonitor {
+
+    public static class BaseUrlUtils{
+        private static Pattern s_ipAddrPattern = Pattern.compile("\\$\\{ipAddr\\}");
+        
+        
+        public static String replaceIpAddr(String baseUrl, String monSvcIpAddr) {
+            if(!baseUrl.contains("${ipAddr}")) {
+                return baseUrl;
+            }
+            
+            String finalUrl = "";
+            Matcher matcher = s_ipAddrPattern.matcher(baseUrl); 
+            finalUrl = matcher.replaceAll(monSvcIpAddr);
+            
+            return finalUrl;
+        }
+    }
+    
+    private static final int DEFAULT_SEQUENCE_RETRY = 0;
+	private static final int DEFAULT_TIMEOUT = 3000;
+	
+	@Override
+	public PollStatus poll(MonitoredService svc, Map<String, Object> parameters) 
+	{
+		PollStatus serviceStatus = PollStatus.unavailable("Poll not completed yet");
+		TimeoutTracker tracker = new TimeoutTracker(parameters, DEFAULT_SEQUENCE_RETRY, DEFAULT_TIMEOUT);
+	    
+		for(tracker.reset(); tracker.shouldRetry() && !serviceStatus.isAvailable(); tracker.nextAttempt()) {
+    		try {
+    	        
+                Map<String, Number> responseTimes = new HashMap<String, Number>();
+                responseTimes.put("response-time", Double.NaN);
+                
+                tracker.startAttempt();
+                Result result = runTest( getBaseUrl(parameters, svc), createGroovyClass( getGroovyFilename( parameters ) ) );
+                double responseTime = tracker.elapsedTimeInMillis();
+                responseTimes.put("response-time", responseTime);
+                
+                if(result.wasSuccessful()) {
+                    serviceStatus = PollStatus.available();
+                    serviceStatus.setProperties(responseTimes);
+                }else {
+                    serviceStatus = PollStatus.unavailable( getFailureMessage( result, svc ));
+                }
+            } catch (CompilationFailedException e) {
+                serviceStatus = PollStatus.unavailable("Selenium page sequence attempt on:" + svc.getIpAddr() + " failed : " + e.getMessage());
+            } catch (IOException e) {
+                serviceStatus = PollStatus.unavailable("Selenium page sequence attempt on " + svc.getIpAddr() + " failed: " + e.getMessage());
+            } catch (Exception e) {
+                serviceStatus = PollStatus.unavailable("Selenium page sequence attempt on " + svc.getIpAddr() + " failed: " + e.getMessage());
+            }
+		}
+	    
+		return serviceStatus;
+	}
+
+    private String getBaseUrl(Map<String, Object> parameters, MonitoredService svc)
+    {
+        if(parameters.containsKey("base-url")) {
+            String baseUrl = (String) parameters.get("base-url");
+            
+            if(!baseUrl.contains("http")) {
+                baseUrl = "http://" + baseUrl;
+            }
+            
+            if(baseUrl.contains("${ipAddr}")) {
+                return BaseUrlUtils.replaceIpAddr(baseUrl, svc.getIpAddr());
+            }
+            
+            return baseUrl;
+        }else {
+            return null;
+        }
+        
+    }
+
+    private String getFailureMessage(Result result, MonitoredService svc)
+    {
+        StringBuffer stringBuilder = new StringBuffer();
+        stringBuilder.append("Failed page sequence attempt on " + svc.getIpAddr() + " for node: " + svc.getNodeLabel() + " with service: " + svc.getSvcName());
+        for(Failure failure : result.getFailures()) { 
+            stringBuilder.append(failure.getMessage() + "\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    private Result runTest(String baseUrl, Class<?> clazz)
+    {
+        return JUnitCore.runClasses(new SeleniumComputer(baseUrl), clazz);
+    }
+
+    private String  getGroovyFilename(Map<String, Object> parameters) 
+    {
+        if(parameters.containsKey("selenium-test")) {
+            return (String) parameters.get("selenium-test");
+        }else {
+            return "";
+        }
+        
+    }
+
+    private Class<?> createGroovyClass(String string) throws CompilationFailedException, IOException 
+    {
+        GroovyClassLoader gcl = new GroovyClassLoader();
+        
+        return gcl.parseClass( new File( string ) );
+    }
+
+}
---
+++ features/ajax-page-monitor/src/test/java/org/opennms/netmgt/poller/monitor/SeleniumMonitorTest.java
@@ -0,0 +1,123 @@
+package org.opennms.netmgt.poller.monitor;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.opennms.core.utils.InetAddressUtils;
+import org.opennms.netmgt.model.PollStatus;
+import org.opennms.netmgt.poller.InetNetworkInterface;
+import org.opennms.netmgt.poller.MonitoredService;
+import org.opennms.netmgt.poller.NetworkInterface;
+import org.opennms.netmgt.poller.monitors.SeleniumMonitor;
+import org.opennms.netmgt.poller.monitors.SeleniumMonitor.BaseUrlUtils;
+
+
+public class SeleniumMonitorTest {
+	
+	
+	public static class MockMonService implements MonitoredService{
+	    
+	    private int m_nodeId;
+        private String m_nodeLabel;
+        private InetAddress m_inetAddr;
+        private String m_svcName;
+        private String m_ipAddr;
+
+        public MockMonService(int nodeId, String nodeLabel, InetAddress inetAddress, String svcName) throws UnknownHostException {
+	        m_nodeId = nodeId;
+	        m_nodeLabel = nodeLabel;
+	        m_inetAddr = inetAddress;
+	        m_svcName = svcName;
+	        m_ipAddr = InetAddressUtils.str(m_inetAddr);
+	    }
+	    
+        @Override
+        public String getSvcUrl() {
+            return null;
+        }
+
+        @Override
+        public String getSvcName() {
+            return m_svcName;
+        }
+
+        @Override
+        public String getIpAddr() {
+            return m_ipAddr;
+        }
+
+        @Override
+        public int getNodeId() {
+            return m_nodeId;
+        }
+
+        @Override
+        public String getNodeLabel() {
+            return m_nodeLabel;
+        }
+
+        @Override
+        public NetworkInterface<InetAddress> getNetInterface() {
+            return new InetNetworkInterface(m_inetAddr);
+        }
+
+        @Override
+        public InetAddress getAddress() {
+            return m_inetAddr;
+        }
+	    
+	}
+	
+	@Before
+	public void setup() throws Exception{
+		
+	}
+	
+	@Test
+	public void testPollStatusNotNull() throws UnknownHostException{
+	    MonitoredService monSvc = new MockMonService(1, "papajohns", InetAddress.getByName("http://www.papajohns.co.uk"), "PapaJohnsSite");
+	    
+	    System.out.println("monSvc.getSvcUrl: " + monSvc.getSvcUrl());
+	    
+	    Map<String, Object> params = new HashMap<String, Object>();
+	    params.put("selenium-test", "src/main/resources/groovy/SeleniumGroovyTest.groovy");
+	    params.put("base-url", "${ipAddr}");
+	    
+		SeleniumMonitor ajaxPSM = new SeleniumMonitor();
+		PollStatus pollStatus = ajaxPSM.poll(monSvc, params);
+		
+		assertNotNull("PollStatus must not be null", pollStatus);
+		
+		System.err.println("PollStatus message: " + pollStatus.getReason());
+		assertEquals(PollStatus.available(), pollStatus);
+		
+	}
+	
+	@Test
+	public void testBaseUrlUtils() 
+	{
+	    
+	    String baseUrl = "http://${ipAddr}:8080";
+	    String monSvcIpAddr = "192.168.1.1";
+	    String finalUrl = "";
+	    
+	    finalUrl = BaseUrlUtils.replaceIpAddr(baseUrl, monSvcIpAddr);
+	    
+	    assertEquals("http://192.168.1.1:8080", finalUrl);
+	}
+	
+	
+	
+	@After
+	public void tearDown(){
+		
+	}
+}
