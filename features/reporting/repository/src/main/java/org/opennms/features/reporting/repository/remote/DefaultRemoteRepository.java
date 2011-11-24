ddb621d9317148a36ea1d219971f01b1186c55c2
Markus Neumann <markus@opennms.com>
2011-11-24 10:22:28 +0100

    cleanup; remoterepository for remoterepository connected to rest-server

--- features/reporting/repository/src/main/java/org/opennms/features/reporting/repository/remote/DefaultRemoteRepository.java
+++ features/reporting/repository/src/main/java/org/opennms/features/reporting/repository/remote/DefaultRemoteRepository.java
@@ -6,42 +6,51 @@ import java.util.List;
 import org.opennms.features.reporting.model.Report;
 import org.opennms.features.reporting.repository.ReportRepository;
 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.GenericType;
+import com.sun.jersey.api.client.WebResource;
+
+//TODO Tak: SSL Auth and Session-Handling(create/destroy)
 public class DefaultRemoteRepository implements ReportRepository {
 
+    private String SERVER_URI = "http://192.168.30.176:8080/opennms/connect/rest/repo/";
+    private Client m_client = Client.create();
+    private WebResource m_webResource;
+
+    @Override
     public List<Report> getReports() {
-        // TODO Auto-generated method stub
-        return new ArrayList<Report>();
+        m_webResource = m_client.resource(SERVER_URI + "getReports");
+        return m_webResource.get(new GenericType<ArrayList<Report>>(){});
     }
 
+    @Override
     public List<Report> getOnlineReports() {
-        // TODO Auto-generated method stub
-        return new ArrayList<Report>();
+        m_webResource = m_client.resource(SERVER_URI + "getOnlineReports");
+        return m_webResource.get(new GenericType<ArrayList<Report>>(){});
     }
 
+    @Override
     public String getReportService(String id) {
-        // TODO Auto-generated method stub
-        return "";
+        m_webResource = m_client.resource(SERVER_URI + "getReportService/" + id);
+        return m_webResource.get(String.class);
     }
 
+    @Override
     public String getDisplayName(String id) {
-        // TODO Auto-generated method stub
-        return "";
+        m_webResource = m_client.resource(SERVER_URI + "getDisplayName/" + id);
+        return m_webResource.get(String.class);
     }
 
+    @Override
     public String getEngine(String id) {
-        // TODO Auto-generated method stub
-        return "";
-    }
-
-    public String getTemplateLocation(String id) {
-        // TODO Auto-generated method stub
-        return "";
+        m_webResource = m_client.resource(SERVER_URI + "getEngine/" + id);
+        return m_webResource.get(String.class);
     }
 
     @Override
     public InputStream getTemplateStream(String id) {
-        // TODO Auto-generated method stub
-        return null;
+        m_webResource = m_client.resource(SERVER_URI + "getTemplateStream/" + id);
+        return m_webResource.get(InputStream.class);
     }
 
 }
