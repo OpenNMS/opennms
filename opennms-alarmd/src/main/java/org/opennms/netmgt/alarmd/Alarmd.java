11b93eb3675d3a1ded5f2a13151a744b1781b92a
Benjamin Reed <ranger@opennms.org>
2011-12-06 10:46:15 -0500

    fix some spurious null-related errors

--- opennms-alarmd/src/main/java/org/opennms/netmgt/alarmd/Alarmd.java
+++ opennms-alarmd/src/main/java/org/opennms/netmgt/alarmd/Alarmd.java
@@ -132,8 +132,10 @@ public class Alarmd implements SpringServiceDaemon, DisposableBean {
      * @throws java.lang.Exception if any.
      */
     public void afterPropertiesSet() throws Exception {
-        for (Northbounder nb : getNorthboundInterfaces()) {
-            nb.init();
+        if (getNorthboundInterfaces() != null) {
+            for (Northbounder nb : getNorthboundInterfaces()) {
+                nb.init();
+            }
         }
     }
 
@@ -163,15 +165,11 @@ public class Alarmd implements SpringServiceDaemon, DisposableBean {
     }
 
     public List<Northbounder> getNorthboundInterfaces() {
-        synchronized (m_northboundInterfaces) {
-            return m_northboundInterfaces;
-        }
+        return m_northboundInterfaces;
     }
 
     public void setNorthboundInterfaces(List<Northbounder> northboundInterfaces) {
-        synchronized (northboundInterfaces) {
-            m_northboundInterfaces = northboundInterfaces;
-        }
+        m_northboundInterfaces = northboundInterfaces;
     }
 
 }
