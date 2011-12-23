d9bafcd91f4c992d1ba1cbe560d017afa6c525e0
OpenNMS Build Account <opennms@opennms.org>
2011-12-23 11:15:52 -0500

    fix up makerpm errors

--- makerpm.sh
+++ makerpm.sh
@@ -12,9 +12,13 @@ cd "$TOPDIR"
 #opennms-core-1.9.9-0.<datestamp>.rpm
 #opennms-core-<pom-version>-<release-major>.<release-minor>.<release-micro>
 
+function exists() {
+    which "$1" >/dev/null 2>&1
+}
+
 function run()
 {
-    if which -s $1; then
+    if exists $1; then
 	"$@"
     else
 	die "Command not found: $1"
@@ -47,7 +51,7 @@ function usage()
 
 function calcMinor()
 {
-    if which -s git; then
+    if exists git; then
 	git log --pretty='format:%cd' --date=short -1 | head -n 1 | sed -e 's,^Date: *,,' -e 's,-,,g'
     else
 	date '+%Y%m%d'
@@ -96,7 +100,7 @@ function setJavaHome()
 	    fi
 	done
     fi
-    
+
     if [ -z $JAVA_HOME ]; then
 	die "*** JAVA_HOME must be set ***"
     fi
@@ -115,19 +119,22 @@ function main()
     SIGN=false
     SIGN_PASSWORD=
     SIGN_ID=opennms@opennms.org
+    BUILD_RPM=true
 
     RELEASE_MAJOR=0
     local RELEASE_MINOR="$(calcMinor)"
     local RELEASE_MICRO=1
-    
 
-    while getopts as:g:M:m:u: OPT; do
+
+    while getopts ahrs:g:M:m:u: OPT; do
 	case $OPT in
 	    a)  ASSEMBLY_ONLY=true
 		;;
 	    s)  SIGN=true
 		SIGN_PASSWORD="$OPTARG"
 		;;
+            r)  BUILD_RPM=false
+                ;;
 	    g)  SIGN_ID="$OPTARG"
 		;;
 	    M)  RELEASE_MAJOR="$OPTARG"
@@ -152,57 +159,53 @@ function main()
 
     setJavaHome
 
-    
-    echo "==== Building OpenNMS RPMs ===="
-    echo
-    echo "Version: " $VERSION
-    echo "Release: " $RELEASE
-    echo
+    if $BUILD_RPM; then
+        echo "==== Building OpenNMS RPMs ===="
+        echo
+        echo "Version: " $VERSION
+        echo "Release: " $RELEASE
+        echo
 
-    echo "=== Creating Working Directories ==="
-    run install -d -m 755 "$WORKDIR/tmp/opennms-$VERSION-$RELEASE/source"
-    run install -d -m 755 "$WORKDIR"/{BUILD,RPMS/{i386,i686,noarch},SOURCES,SPECS,SRPMS}
-    
-    echo "=== Copying Source to Source Directory ==="
-    run rsync -aqr --exclude=.git --exclude=.svn --exclude=target --delete --delete-excluded "$TOPDIR/" "$WORKDIR/tmp/opennms-$VERSION-$RELEASE/source/"
-    
-    echo "=== Creating a tar.gz archive of the Source in /usr/src/redhat/SOURCES ==="
-    run tar zcf "$WORKDIR/SOURCES/opennms-source-$VERSION-$RELEASE.tar.gz" -C "$WORKDIR/tmp" "opennms-$VERSION-$RELEASE"
-    run tar zcf "$WORKDIR/SOURCES/centric-troubleticketer.tar.gz" -C "$WORKDIR/tmp/opennms-$VERSION-$RELEASE/source/opennms-tools" "centric-troubleticketer"
-    
-    echo "=== Building RPMs ==="
-    for spec in tools/packages/opennms/opennms.spec opennms-tools/centric-troubleticketer/src/main/rpm/opennms-plugin-ticketer-centric.spec
-    do
-	run rpmbuild -bb \
-	    --define "skip_compile $(skipCompile)" \
-	    --define "extrainfo $EXTRA_INFO" \
-	    --define "extrainfo2 $EXTRA_INFO2" \
-	    --define "_topdir $WORKDIR" \
-	    --define "_tmppath $WORKDIR/tmp" \
-	    --define "version $VERSION" \
-	    --define "releasenumber $RELEASE" \
-	    $spec
-    done
+        echo "=== Creating Working Directories ==="
+        run install -d -m 755 "$WORKDIR/tmp/opennms-$VERSION-$RELEASE/source"
+        run install -d -m 755 "$WORKDIR"/{BUILD,RPMS/{i386,i686,noarch},SOURCES,SPECS,SRPMS}
+
+        echo "=== Copying Source to Source Directory ==="
+        run rsync -aqr --exclude=.git --exclude=.svn --exclude=target --delete --delete-excluded "$TOPDIR/" "$WORKDIR/tmp/opennms-$VERSION-$RELEASE/source/"
+
+        echo "=== Creating a tar.gz archive of the Source in /usr/src/redhat/SOURCES ==="
+        run tar zcf "$WORKDIR/SOURCES/opennms-source-$VERSION-$RELEASE.tar.gz" -C "$WORKDIR/tmp" "opennms-$VERSION-$RELEASE"
+        run tar zcf "$WORKDIR/SOURCES/centric-troubleticketer.tar.gz" -C "$WORKDIR/tmp/opennms-$VERSION-$RELEASE/source/opennms-tools" "centric-troubleticketer"
+
+        echo "=== Building RPMs ==="
+        for spec in tools/packages/opennms/opennms.spec opennms-tools/centric-troubleticketer/src/main/rpm/opennms-plugin-ticketer-centric.spec
+        do
+    	run rpmbuild -bb \
+    	    --define "skip_compile $(skipCompile)" \
+    	    --define "extrainfo $EXTRA_INFO" \
+    	    --define "extrainfo2 $EXTRA_INFO2" \
+    	    --define "_topdir $WORKDIR" \
+    	    --define "_tmppath $WORKDIR/tmp" \
+    	    --define "version $VERSION" \
+    	    --define "releasenumber $RELEASE" \
+    	    $spec
+        done
+    fi
 
     if $SIGN; then
-	
-	RPMS="$WORKDIR"/RPMS/noarch/*.rpm
-	run rpm --define "_signature gpg" --define "_gpg_name $SIGN_ID" --resign "$RPMS"
-	
+
+	RPMS=$(echo "$WORKDIR"/RPMS/noarch/*.rpm)
+	#run rpm --define "_signature gpg" --define "_gpg_name $SIGN_ID" --resign "$RPMS"
+
 	run expect -c "set timeout -1; spawn rpm --define \"_signature gpg\" --define \"_gpg_name $SIGN_ID\" --resign $RPMS; match_max 100000; expect \"Enter pass phrase: \"; send -- \"${SIGN_PASSWORD}\r\"; expect eof" || \
 	    die "RPM signing failed for $(branch)"
-	
+
     fi
-    
+
     echo "==== OpenNMS RPM Build Finished ===="
-    
+
     echo ""
     echo "Your completed RPMs are in the $WORKDIR/RPMS/noarch directory."
 }
 
 main "$@"
-
-
-
-
-
