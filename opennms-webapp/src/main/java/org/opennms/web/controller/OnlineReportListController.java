807471178a2ce129b23ab8002f634a10f7a2322f
Ronny Trommer <ronny@opennms.org>
2011-12-29 15:16:22 +0100

    Optimize imports, removed a strange import against sun.com.tools.jdi

--- opennms-webapp/src/main/java/org/opennms/web/controller/OnlineReportListController.java
+++ opennms-webapp/src/main/java/org/opennms/web/controller/OnlineReportListController.java
@@ -28,10 +28,6 @@
 
 package org.opennms.web.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.sun.tools.jdi.*;
 import org.opennms.web.svclayer.DatabaseReportListService;
 import org.opennms.web.svclayer.support.DatabaseReportDescription;
 import org.opennms.web.svclayer.support.ReportRepositoryDescription;
@@ -40,8 +36,10 @@ import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-import java.util.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * <p>OnlineReportListController class.</p>
