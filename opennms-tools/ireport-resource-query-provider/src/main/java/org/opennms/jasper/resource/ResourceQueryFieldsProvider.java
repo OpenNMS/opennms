6c4371e9c88926d1352fcc0f998773ee36d0a87c
Donald Desloge <desloge@opennms.com>
2012-01-11 15:11:31 -0500

    add fields provider for ResourceQuery

---
+++ opennms-tools/ireport-resource-query-provider/src/main/java/org/opennms/jasper/resource/ResourceQueryFieldsProvider.java
@@ -0,0 +1,79 @@
+package org.opennms.jasper.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JRDataset;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRField;
+import net.sf.jasperreports.engine.design.JRDesignField;
+
+import com.jaspersoft.ireport.designer.FieldsProvider;
+import com.jaspersoft.ireport.designer.FieldsProviderEditor;
+import com.jaspersoft.ireport.designer.IReportConnection;
+import com.jaspersoft.ireport.designer.data.ReportQueryDialog;
+
+public class ResourceQueryFieldsProvider implements FieldsProvider{
+
+    public String designQuery(IReportConnection connection, String arg1, ReportQueryDialog arg2) throws JRException, UnsupportedOperationException {
+        return null;
+    }
+
+    public FieldsProviderEditor getEditorComponent(ReportQueryDialog arg0) {
+        return null;
+    }
+
+    public JRField[] getFields(IReportConnection irConn, JRDataset reportDataset, Map parameters) throws JRException, UnsupportedOperationException {
+        String query = "";
+        
+        if(reportDataset.getQuery() == null || reportDataset.getQuery().getText() == null || reportDataset.getQuery().getText().length() == 0) {
+            return new JRField[0];
+        }
+        
+        List<JRField> fields = new ArrayList<JRField>();
+        query = reportDataset.getQuery().getText();
+        
+        addPathColumn(fields);
+        if(query.contains("--dsNames")) {
+            String[] splitDsNames = query.split("--dsNames");
+            String dsNames = splitDsNames[splitDsNames.length - 1];
+            String[] dsNameList = dsNames.split(";");
+            for(String ds : dsNameList) {
+                JRDesignField field = new JRDesignField();
+                field.setName(ds.trim());
+                field.setDescription(ds.trim());
+                field.setValueClass(String.class);
+                fields.add(field);
+            }
+        }
+        
+        return fields.toArray(new JRField[fields.size()]);
+        
+    }
+
+    private void addPathColumn(List<JRField> fields) {
+        JRDesignField field = new JRDesignField();
+        field.setName("Path");
+        field.setDescription("Path");
+        field.setValueClass(String.class);
+        fields.add(field);
+    }
+
+    public boolean hasEditorComponent() {
+        return false;
+    }
+
+    public boolean hasQueryDesigner() {
+        return false;
+    }
+
+    public boolean supportsAutomaticQueryExecution() {
+        return false;
+    }
+
+    public boolean supportsGetFieldsOperation() {
+        return true;
+    }
+
+}
