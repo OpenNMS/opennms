e348e79f324a62b9d8f6bb9c221fe903e3db9383
Benjamin Reed <ranger@opennms.org>
2012-01-16 09:24:15 -0500

    add script for pushing the source to sourceforge

---
+++ bin/update-sourceforge-repo.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+use File::Spec;
+use Getopt::Long qw(:config gnu_getopt);
+
+use OpenNMS::Release::FilePackage 2.1;
+use OpenNMS::Release::SFTPRepo 2.1;
+
+my $help   = 0;
+my $result = GetOptions(
+	"h|help"     => \$help,
+);
+
+my $release = shift @ARGV;
+my @files   = @ARGV;
+
+if ($help or not @files) {
+	usage();
+}
+
+my $repo = OpenNMS::Release::SFTPRepo->new('frs.sourceforge.net', File::Spec->catdir('/home/frs/project/o/op/opennms/OpenNMS-Snapshots', $release))->begin();
+for my $file (@files) {
+	my $package = OpenNMS::Release::FilePackage->new(Cwd::abs_path($file));
+	print "- installing ", $package->to_string, "\n";
+	$repo->install_package($package);
+}
+$repo->commit();
+
+sub usage {
+	print STDERR <<END;
+usage: $0 <release> <tarball1..tarballN>
+
+	-h       This help.
+
+END
+	exit 1;
+}
