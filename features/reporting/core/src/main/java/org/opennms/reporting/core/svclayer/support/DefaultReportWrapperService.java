93d182e0ab69d147c55e8b12c4c9ad15dd34508e
Alejandro Galue <agalue@opennms.org>
2012-01-18 20:29:04 -0430

    Fix for Bug NMS-5111 - A better approach.
    The only piece of code needed is to catch the NullPointerException. It
    is not necesary to execute options.setMailTo(""); so I've added a
    warning when the user does not have configured an email.

--- features/reporting/core/src/main/java/org/opennms/reporting/core/svclayer/support/DefaultReportWrapperService.java
+++ features/reporting/core/src/main/java/org/opennms/reporting/core/svclayer/support/DefaultReportWrapperService.java
@@ -107,8 +107,8 @@ public class DefaultReportWrapperService implements ReportWrapperService {
         } catch (IOException e) {
             log.error("IO exception trying to set destination email address",
                       e);
-        } catch (NullPointerException e) {
-            options.setMailTo(""); // See NMS-5111 for more details.
+        } catch (NullPointerException e) { // See NMS-5111 for more details.
+            log.warn("the user " + userId + " does not have any email configured.");
         }
 
         options.setInstanceId(reportId + " " + userId);
