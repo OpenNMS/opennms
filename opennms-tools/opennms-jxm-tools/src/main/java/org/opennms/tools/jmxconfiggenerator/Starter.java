592c343742a22e4b5d1274c5ee57665e60d986c8
Markus Neumann <MarkusNeumannMarkus@gmail.com>
2012-02-06 00:59:50 +0100

    opennms-jmx-tools; cli tool for generating jmx-datacollection.xml and graph.properties by reading avalible mbeans from a system vi jmx-rmi

---
+++ opennms-tools/opennms-jxm-tools/src/main/java/org/opennms/tools/jmxconfiggenerator/Starter.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2007-2011 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.tools.jmxconfiggenerator;
+
+import java.io.IOException;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.opennms.tools.jmxconfiggenerator.graphs.SnmpGraphConfigGenerator;
+import org.opennms.tools.jmxconfiggenerator.jmxconfig.JmxDatacollectionConfigGenerator;
+
+public class Starter {
+
+	@Option(name = "-jmx", usage = "Generate jmx-datacollection.xml by reading JMX over RMI")
+	private boolean jmx = false;
+
+	@Option(name = "-service", usage = "Your optional service-name. Like cassandra, jboss, tomcat")
+	private String serviceName = "anyservice";
+
+	@Option(name = "-host", usage = "Hostname or IP-Adress of JMX-RMI host")
+	private String hostName;
+
+	@Option(name = "-port", usage = "Port of JMX-RMI service")
+	private String port;
+	
+	@Option(name = "-skipDefaultVM", usage = "set to process default JavaVM Beans.")
+	private boolean skipDefaultVM = false;
+	
+	@Option(name = "-runCompositeData", usage = "set to process CompositeData of Beans.")
+	private boolean runCompositeData = false;
+	
+	@Option(name = "-graph", usage = "Generate snmp-graph.properties linke file to out, by reading jmx-datacollection.xml like file from input")
+	private boolean graph = false;
+
+	@Option(name = "-input", usage = "Jmx-datacolletion.xml like file to parse")
+	private String inputFile;
+
+	@Option(name = "-out", usage = "File to write generated snmp-graph.properties linke content")
+	private String outFile;
+
+	public static void main(String[] args) throws IOException {
+		new Starter().doMain(args);
+	}
+
+	public void doMain(String[] args) {
+
+		CmdLineParser parser = new CmdLineParser(this);
+
+		parser.setUsageWidth(80);
+
+		try {
+			parser.parseArgument(args);
+			if (jmx && graph) {
+				throw new CmdLineException(parser, "jmx and graph is set. Just use on at a time.");
+			}
+			if (!jmx && !graph) {
+				throw new CmdLineException(parser, "set jmx or graph.");
+			}
+			if (jmx && hostName != null && port != null) {
+				JmxDatacollectionConfigGenerator.generateJmxConfig(serviceName, hostName, port, !skipDefaultVM, runCompositeData);
+				return;
+			}
+			if (graph && inputFile != null && outFile != null) {
+				SnmpGraphConfigGenerator.generateGraphs(serviceName, inputFile, outFile);
+				return;
+			}
+			throw new CmdLineException(parser, "no valid call found.");
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			System.err.println("JmxSaugBlaser [options...] arguments...");
+			parser.printUsage(System.err);
+			System.err.println();
+			// System.err.println("  Example: java -jar JmxSaugBlaser" +
+			// parser.printExample(ALL));
+			System.err.println("Use a call linke:");
+			System.err.println("  Example generation of jmx-datacollection.xml: java -jar JmxSaugBlaser.jar -jmx -host localhost -port 7199 [-service cassandra] [-runCompositeData] [-skipDefaultVM]");
+			System.err.println("  Example generation of  snmp-graph.properties: java -jar JmxSaugBlaser.jar -graph -input test.xml -out test.properies [-service cassandra]");
+			return;
+		}
+	}
+}
---
+++ opennms-tools/opennms-jxm-tools/src/main/java/org/opennms/tools/jmxconfiggenerator/jmxconfig/JmxDatacollectionConfigGenerator.java
@@ -0,0 +1,301 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2007-2011 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.tools.jmxconfiggenerator.jmxconfig;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+import javax.management.AttributeNotFoundException;
+import javax.management.InstanceNotFoundException;
+import javax.management.IntrospectionException;
+import javax.management.MBeanAttributeInfo;
+import javax.management.MBeanException;
+import javax.management.MBeanInfo;
+import javax.management.MBeanServerConnection;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+import javax.management.ReflectionException;
+import javax.management.openmbean.CompositeData;
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import javax.xml.bind.JAXB;
+
+import org.apache.commons.lang3.StringUtils;
+import org.opennms.xmlns.xsd.config.jmx_datacollection.Attrib;
+import org.opennms.xmlns.xsd.config.jmx_datacollection.CompAttrib;
+import org.opennms.xmlns.xsd.config.jmx_datacollection.CompMember;
+import org.opennms.xmlns.xsd.config.jmx_datacollection.JmxCollection;
+import org.opennms.xmlns.xsd.config.jmx_datacollection.JmxDatacollectionConfig;
+import org.opennms.xmlns.xsd.config.jmx_datacollection.Mbean;
+import org.opennms.xmlns.xsd.config.jmx_datacollection.ObjectFactory;
+import org.opennms.xmlns.xsd.config.jmx_datacollection.Rrd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JmxDatacollectionConfigGenerator {
+
+	private static Logger logger = LoggerFactory.getLogger(JmxDatacollectionConfigGenerator.class);
+	private static ObjectFactory xmlObjectFactory = new ObjectFactory();
+	
+	private static ArrayList<String> standardVmBeans = new ArrayList<String>();
+	private static ArrayList<String> ignores = new ArrayList<String>();
+	private static ArrayList<String> numbers = new ArrayList<String>();
+	private static ArrayList<String> justDo = new ArrayList<String>();
+
+	private static ArrayList<String> rras = new ArrayList<String>();
+
+	private static HashMap<String, Integer> aliasMap = new HashMap<String, Integer>();
+	private static ArrayList<String> aliasList = new ArrayList<String>();
+
+	private static Rrd rrd = new Rrd();
+	private static MBeanServerConnection jmxServerConnection;
+
+	static {
+
+		justDo.add("java.lang");
+
+		ignores.add("");
+
+		standardVmBeans.add("JMImplementation");
+		standardVmBeans.add("com.sun.management");
+		standardVmBeans.add("java.lang");
+		standardVmBeans.add("java.util.logging");
+
+		numbers.add("int");
+		numbers.add("long");
+		numbers.add("double");
+		numbers.add("float");
+		numbers.add("class java.lang.Long");
+
+		// rrd setup
+		rrd.setStep(300);
+		rras.add("RRA:AVERAGE:0.5:1:2016");
+		rras.add("RRA:AVERAGE:0.5:12:1488");
+		rras.add("RRA:AVERAGE:0.5:288:366");
+		rras.add("RRA:MAX:0.5:288:366");
+		rras.add("RRA:MIN:0.5:288:366");
+		rrd.getRra().addAll(rras);
+	}
+
+	public static void generateJmxConfig(String serviceName, String hostName, String port, Boolean runStandardVmBeans, Boolean runCompositeData) throws AttributeNotFoundException, MBeanException {
+	logger.debug("Startup values: \n serviceName: " + serviceName + "\n hostName: " + hostName + "\n port:" + port + "\n runStandardVmBeans: " + runStandardVmBeans + "\n runCompositeData: " + runCompositeData);
+		JMXServiceURL jmxServiceURL;
+		JmxDatacollectionConfig xmlJmxDatacollectionConfig = xmlObjectFactory.createJmxDatacollectionConfig();
+		JmxCollection xmlJmxCollection = xmlObjectFactory.createJmxCollection();
+
+		xmlJmxCollection.setName("JSR160-" + serviceName);
+		xmlJmxCollection.setRrd(rrd);
+		xmlJmxDatacollectionConfig.getJmxCollection().add(xmlJmxCollection);
+		xmlJmxCollection.setMbeans(xmlObjectFactory.createMbeans());
+
+		if (!runStandardVmBeans) {
+			ignores.addAll(standardVmBeans);
+		}
+		
+		try {
+			jmxServiceURL = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + hostName + ":" + port + "/jmxrmi");
+			JMXConnector jmxConnector = JMXConnectorFactory.connect(jmxServiceURL);
+			jmxConnector.connect();
+			jmxServerConnection = jmxConnector.getMBeanServerConnection();
+			logger.debug("count: " + jmxServerConnection.getMBeanCount());
+			for (String domainName : jmxServerConnection.getDomains()) {
+
+				// just domains that are relevant for the service
+				if (!ignores.contains(domainName)) {
+					logger.debug("domain: " + domainName);
+
+					// for all mBeans of the actual domain
+					for (ObjectInstance jmxObjectInstance : jmxServerConnection.queryMBeans(new ObjectName(domainName + ":*"), null)) {
+						Mbean xmlMbean = xmlObjectFactory.createMbean();
+						xmlMbean.setObjectname(jmxObjectInstance.getObjectName().toString());
+						String typeAndOthers = StringUtils.substringAfterLast(jmxObjectInstance.getObjectName().getCanonicalName(), "=");
+						xmlMbean.setName(domainName + "." + typeAndOthers);
+
+						logger.debug("\t" + jmxObjectInstance.getObjectName());
+						MBeanInfo jmxMbeanInfo = jmxServerConnection.getMBeanInfo(jmxObjectInstance.getObjectName());
+						logger.debug("--- Attributes for " + jmxObjectInstance.getObjectName());
+
+						for (MBeanAttributeInfo jmxBeanAttributeInfo : jmxMbeanInfo.getAttributes()) {
+
+							// process just readable and not writable mbeans
+							if (jmxBeanAttributeInfo.isReadable() && !jmxBeanAttributeInfo.isWritable()) {
+								logger.info("Add Elements for mBean: '{}'", jmxObjectInstance.getObjectName().toString());
+								
+								// just process CompositeData if activated
+								if ( runCompositeData && "javax.management.openmbean.CompositeData".equals(jmxBeanAttributeInfo.getType())) {
+									logger.error("actual mBean: '{}'", jmxObjectInstance.getObjectName());
+									CompAttrib compAttrib = createCompAttrib(jmxObjectInstance, jmxBeanAttributeInfo);
+									if (compAttrib != null) {
+										logger.debug("xmlMbean got CompAttrib");
+										xmlMbean.getCompAttrib().add(compAttrib);
+									}
+								}
+
+								if (numbers.contains(jmxBeanAttributeInfo.getType())) {
+									Attrib xmlJmxAttribute = createAttr(jmxBeanAttributeInfo);
+									// logger.info("\tAdded attribute: '{}' with alias: '{}'",
+									// xmlJmxAttribute.getName(),
+									// xmlJmxAttribute.getAlias());
+									xmlMbean.getAttrib().add(xmlJmxAttribute);
+								}
+							}
+						}
+
+						if (xmlMbean.getAttrib().size() > 0 || xmlMbean.getCompAttrib().size() > 0) {
+							xmlJmxCollection.getMbeans().getMbean().add(xmlMbean);
+						} else {
+							logger.debug("mbean: " + xmlMbean.getName() + " has no relavant attributes.");
+						}
+					}
+				} else {
+					logger.debug("ignored: " + domainName);
+				}
+			}
+
+			JAXB.marshal(xmlJmxDatacollectionConfig, new File("test.xml"));
+
+		} catch (MalformedURLException e) {
+			logger.error("MalformedURLException '{}'", e.getMessage());
+			e.printStackTrace();
+		} catch (IOException e) {
+			logger.error("IOException '{}'", e.getMessage());
+			e.printStackTrace();
+		} catch (MalformedObjectNameException e) {
+			logger.error("MalformedObjectNameException '{}'", e.getMessage());
+			e.printStackTrace();
+		} catch (InstanceNotFoundException e) {
+			logger.error("InstanceNotFoundException '{}'", e.getMessage());
+			e.printStackTrace();
+		} catch (IntrospectionException e) {
+			logger.error("IntrospectionException '{}'", e.getMessage());
+			e.printStackTrace();
+		} catch (ReflectionException e) {
+			logger.error("ReflectionException '{}'", e.getMessage());
+			e.printStackTrace();
+		}
+
+		logger.debug(xmlJmxDatacollectionConfig.toString());
+
+		logger.info("Thx for computing with us!");
+	}
+
+	private static CompAttrib createCompAttrib(ObjectInstance jmxObjectInstance, MBeanAttributeInfo jmxMBeanAttributeInfo) {
+
+		// <comp-attrib name="Usage" type="Composite" alias="EdenUsage">
+		// <comp-member name="init" type="gauge" alias="EdenUsage.init"/>
+		// <comp-member name="max" type="gauge" alias="EdenUsage.max"/>
+		// <comp-member name="used" type="gauge" alias="EdenUsage.used"/>
+		// <comp-member name="committed" type="gauge" alias="EdenUsg.cmmttd"/>
+		// </comp-attrib>
+
+		Boolean contentAdded = false;
+
+		CompAttrib xmlCompAttrib = xmlObjectFactory.createCompAttrib();
+		xmlCompAttrib.setName(jmxMBeanAttributeInfo.getName());
+		xmlCompAttrib.setType("Composite");
+		xmlCompAttrib.setAlias(jmxMBeanAttributeInfo.getName());
+
+		// logger.error("\t Description: '{}'",
+		// jmxMBeanAttributeInfo.getDescription());
+		// logger.error("\t Descriptor: '{}'",
+		// jmxMBeanAttributeInfo.getDescriptor());
+		// logger.error("\t\t Descriptor(): '{}'",
+		// jmxMBeanAttributeInfo.getDescriptor());
+		// logger.error("\t\t Type: '{}'", jmxMBeanAttributeInfo.getType());
+
+		// logger.error("found CompositeData at attrib: '{}'",
+		// jmxMBeanAttributeInfo.getName());
+
+		CompositeData compositeData;
+		try {
+			logger.error("Try to get data");
+			compositeData = (CompositeData) jmxServerConnection.getAttribute(jmxObjectInstance.getObjectName(), jmxMBeanAttributeInfo.getName());
+
+			logger.error("compositeData.getCompositeType: '{}'", compositeData.getCompositeType());
+
+			Set<String> keys = compositeData.getCompositeType().keySet();
+
+			for (String key : keys) {
+				Object compositeEntry = compositeData.get(key);
+				if (numbers.contains(compositeEntry.getClass().toString())) {
+					contentAdded = true;
+					CompMember xmlCompMember = xmlObjectFactory.createCompMember();
+					xmlCompMember.setName(key);
+					xmlCompMember.setAlias(key);
+					xmlCompMember.setType("gauge");
+					xmlCompAttrib.getCompMember().add(xmlCompMember);
+				}
+			}
+
+		} catch (Exception e) {
+			logger.error("killed in action: '{}'", e.getMessage());
+			e.printStackTrace();
+		}
+
+		if (contentAdded) {
+			logger.error("xmlCompAttrib returned by createCompAttrib it's '{}'", xmlCompAttrib);
+			return xmlCompAttrib;
+		}
+		return null;
+	}
+
+	private static Attrib createAttr(MBeanAttributeInfo jmxMBeanAttributeInfo) {
+		Attrib xmlJmxAttribute = xmlObjectFactory.createAttrib();
+		xmlJmxAttribute.setName(jmxMBeanAttributeInfo.getName());
+		xmlJmxAttribute.setType("gauge");
+		if (!aliasMap.containsKey(jmxMBeanAttributeInfo.getName())) {
+			xmlJmxAttribute.setAlias(0 + jmxMBeanAttributeInfo.getName());
+			aliasMap.put(jmxMBeanAttributeInfo.getName(), 0);
+		} else {
+			aliasMap.put(jmxMBeanAttributeInfo.getName(), aliasMap.get(jmxMBeanAttributeInfo.getName()) + 1);
+			xmlJmxAttribute.setAlias(aliasMap.get(jmxMBeanAttributeInfo.getName()).toString() + jmxMBeanAttributeInfo.getName());
+		}
+
+		//find alias crashes caused by cuting down alias length to 19 chars
+		if (aliasList.contains(StringUtils.substring(xmlJmxAttribute.getAlias(), 0, 19))) {
+			// logger.error("ALIAS CRASH AT :" + xmlJmxAttribute.getAlias() +
+			// "\t as: " + StringUtils.substring(xmlJmxAttribute.getAlias(), 0,
+			// 19));
+			xmlJmxAttribute.setAlias(xmlJmxAttribute.getAlias() + "_NAME_CRASH_AS_19_CHAR_VALUE");
+		} else {
+			aliasList.add(StringUtils.substring(xmlJmxAttribute.getAlias(), 0, 19));
+			// logger.debug("added alias    : " + xmlJmxAttribute.getAlias() +
+			// "\t as: " + StringUtils.substring(xmlJmxAttribute.getAlias(), 0,
+			// 19));
+		}
+
+		return xmlJmxAttribute;
+	}
+}
