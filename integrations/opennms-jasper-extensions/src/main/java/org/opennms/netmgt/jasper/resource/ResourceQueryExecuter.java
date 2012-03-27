e02dedc8c47ea04a231a4097b0a2731c8d85b98b
Seth Leger <seth@opennms.org>
2012-03-27 11:58:34 -0400

    Minor warning cleanup.

--- integrations/opennms-jasper-extensions/src/main/java/org/opennms/netmgt/jasper/resource/ResourceQueryExecuter.java
+++ integrations/opennms-jasper-extensions/src/main/java/org/opennms/netmgt/jasper/resource/ResourceQueryExecuter.java
@@ -9,6 +9,7 @@ import net.sf.jasperreports.engine.query.JRAbstractQueryExecuter;
 
 public class ResourceQueryExecuter extends JRAbstractQueryExecuter {
 
+    @SuppressWarnings("unchecked")
     protected ResourceQueryExecuter(JRDataset dataset, Map parametersMap) {
         super(dataset, parametersMap);
         parseQuery();
