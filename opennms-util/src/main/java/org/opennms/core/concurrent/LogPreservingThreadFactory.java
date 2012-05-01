5bc573a1abe9812f2a38479f446eb99910df7dbf
Seth Leger <seth@opennms.org>
2012-05-01 17:36:10 -0400

    Constant and javadoc cleanup.

--- opennms-util/src/main/java/org/opennms/core/concurrent/LogPreservingThreadFactory.java
+++ opennms-util/src/main/java/org/opennms/core/concurrent/LogPreservingThreadFactory.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2006-2011 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.
+ * Copyright (C) 2012 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2012 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
