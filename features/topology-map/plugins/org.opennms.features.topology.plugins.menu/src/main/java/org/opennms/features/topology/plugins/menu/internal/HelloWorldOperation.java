085e3647116f8b2c5872e20759bd38b0d61e67dc
Benjamin Reed <ranger@opennms.org>
2012-05-22 15:22:22 -0400

    create a sample operation

---
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.menu/src/main/java/org/opennms/features/topology/plugins/menu/internal/HelloWorldOperation.java
@@ -0,0 +1,27 @@
+package org.opennms.features.topology.plugins.menu.internal;
+
+import java.util.List;
+
+import org.opennms.features.topology.api.Operation;
+
+public class HelloWorldOperation implements Operation {
+
+	@Override
+	public Undoer execute(List<Object> targets) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean display(List<Object> targets) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean enabled(List<Object> targets) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
