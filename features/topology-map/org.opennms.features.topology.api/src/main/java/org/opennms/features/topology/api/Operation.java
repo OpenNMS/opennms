0677e82082df0ed554ca2e1f32d1927021940299
Donald Desloge <thedesloge@gmail.com>
2012-05-22 12:52:38 -0400

    added Operation interface

---
+++ features/topology-map/org.opennms.features.topology.api/src/main/java/org/opennms/features/topology/api/Operation.java
@@ -0,0 +1,14 @@
+package org.opennms.features.topology.api;
+
+import java.util.List;
+
+public interface Operation {
+    
+    public interface Undoer {
+        public void undo();
+    }
+    
+    public Undoer execute(List<Object> targets);
+    public boolean display(List<Object> targets);
+    public boolean enabled(List<Object> targets);
+}
