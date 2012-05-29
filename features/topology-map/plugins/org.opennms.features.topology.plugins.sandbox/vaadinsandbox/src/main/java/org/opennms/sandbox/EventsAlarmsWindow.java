bf97c01503f37c62b529c1a33cc6e09f3d18c7b0
Leonardo Bell <lmbell@opennms.org>
2012-05-29 10:38:43 -0400

    Added Sandbox directory with VaadinSandbox project concerned with Menus

---
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.sandbox/vaadinsandbox/src/main/java/org/opennms/sandbox/EventsAlarmsWindow.java
@@ -0,0 +1,54 @@
+package org.opennms.sandbox;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.vaadin.terminal.ExternalResource;
+import com.vaadin.ui.Embedded;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.TabSheet.SelectedTabChangeEvent;
+import com.vaadin.ui.TabSheet.SelectedTabChangeListener;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+public class EventsAlarmsWindow extends Window {
+
+	private final double sizePercentage = 0.80; // Window size proportionate to main window
+	private final int widthCushion = 50; //Border cushion for width of window;
+	private final int heightCushion = 110; //Border cushion for height of window
+	private final URL eventsURL = new URL("http://demo.opennms.org/opennms/event/list");
+	private final URL alarmsURL = new URL("http://demo.opennms.org/opennms/alarm/list.htm?display=long");
+	private Embedded eventsBrowser = new Embedded("", new ExternalResource(eventsURL));
+	private Embedded alarmsBrowser = new Embedded("", new ExternalResource(alarmsURL));
+
+	
+	public EventsAlarmsWindow(float width, float height) throws MalformedURLException {
+		
+		int browserWidth = (int)(sizePercentage * width), browserHeight = (int)(sizePercentage * height);
+		int windowWidth = browserWidth + widthCushion, windowHeight = browserHeight + heightCushion;
+		
+		setCaption("Events & Alarms");
+		setImmediate(true);
+		setResizable(false);
+		setWidth("" + windowWidth + "px");
+		setHeight("" + windowHeight + "px");
+		setPositionX((int)((1.0 - windowWidth/width)/2.0 * width));
+		setPositionY((int)((1.0 - windowHeight/height)/2.0 * height));
+		
+		VerticalLayout layout = new VerticalLayout();
+		TabSheet tabsheet = new TabSheet();
+		alarmsBrowser.setType(Embedded.TYPE_BROWSER);
+		alarmsBrowser.setWidth("" + browserWidth + "px");
+		alarmsBrowser.setHeight("" + browserHeight + "px");
+		eventsBrowser.setType(Embedded.TYPE_BROWSER);
+		eventsBrowser.setWidth("" + browserWidth + "px");
+		eventsBrowser.setHeight("" + browserHeight + "px");
+		tabsheet.addTab(eventsBrowser, "Events");
+		tabsheet.addTab(alarmsBrowser, "Alarms");
+		
+		layout.addComponent(tabsheet);
+		addComponent(layout);
+	}
+	
+}
---
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.sandbox/vaadinsandbox/src/main/java/org/opennms/sandbox/ResourceGraphsWindow.java
@@ -0,0 +1,41 @@
+package org.opennms.sandbox;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.vaadin.terminal.ExternalResource;
+import com.vaadin.ui.Embedded;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+public class ResourceGraphsWindow extends Window {
+
+	private final double sizePercentage = 0.80; // Window size proportionate to main window
+	private final int widthCushion = 50; //Border cushion for width of window;
+	private final int heightCushion = 110; //Border cushion for height of window
+	private final URL rgURL = new URL("http://demo.opennms.org/opennms/graph/index.jsp");
+	private Embedded rgBrowser = new Embedded("", new ExternalResource(rgURL));
+	
+	public ResourceGraphsWindow (float width, float height) throws MalformedURLException{
+		int browserWidth = (int)(sizePercentage * width), browserHeight = (int)(sizePercentage * height);
+		int windowWidth = browserWidth + widthCushion, windowHeight = browserHeight + heightCushion;
+		
+		setCaption("Resource Graphs");
+		setImmediate(true);
+		setResizable(false);
+		setWidth("" + windowWidth + "px");
+		setHeight("" + windowHeight + "px");
+		setPositionX((int)((1.0 - windowWidth/width)/2.0 * width));
+		setPositionY((int)((1.0 - windowHeight/height)/2.0 * height));
+		
+		VerticalLayout layout = new VerticalLayout();
+		rgBrowser.setType(Embedded.TYPE_BROWSER);
+		rgBrowser.setWidth("" + browserWidth + "px");
+		rgBrowser.setHeight("" + browserHeight + "px");
+		
+		layout.addComponent(rgBrowser);
+		addComponent(layout);
+	}
+}
