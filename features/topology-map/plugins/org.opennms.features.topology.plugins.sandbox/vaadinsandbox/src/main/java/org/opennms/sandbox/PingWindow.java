c16174c79284ef395ce28ba98095aeb13190b8db
Matt Brozowski <brozow@opennms.org>
2012-05-30 11:49:38 -0400

    Revert "IP dropdown bar works correctly, GWT optimization added."
    This reverts commit 0d597abdea54f608f46de9798214f671ae8ba4c3.

--- features/topology-map/plugins/org.opennms.features.topology.plugins.sandbox/vaadinsandbox/src/main/java/org/opennms/sandbox/PingWindow.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.sandbox/vaadinsandbox/src/main/java/org/opennms/sandbox/PingWindow.java
@@ -16,14 +16,10 @@ public class PingWindow extends Window{
 
    
     private final double sizePercentage = 0.80; // Window size proportionate to main window
-    private boolean numOutput = false;
-    private NativeSelect ipDropdown = null;
-    private NativeSelect packetSizeDropdown = null;
-    private Node testNode = null;
     public PingWindow (float width, float height){
         
         //Test Data
-        testNode = new Node("172.0.1.234","Test Node");
+        final Node testNode = new Node("172.0.1.234","Test Node");
         //End Test Data
         
         
@@ -39,10 +35,10 @@ public class PingWindow extends Window{
 
         VerticalLayout layout = new VerticalLayout();
         GridLayout grid = new GridLayout(2,5);
-        ipDropdown = new NativeSelect();
-        packetSizeDropdown = new NativeSelect();
-        CheckBox numericalDataCheckBox = new CheckBox("Use Numerical Node Names");
-        numericalDataCheckBox.setImmediate(true);
+        final NativeSelect ipDropdown = new NativeSelect();
+        
+        NativeSelect packetSizeDropdown = new NativeSelect();
+        final CheckBox numericalDataCheckBox = new CheckBox("Use Numerical Node Names");
         packetSizeDropdown.addItem("16");
         packetSizeDropdown.addItem("32");
         packetSizeDropdown.addItem("64");
@@ -70,11 +66,18 @@ public class PingWindow extends Window{
         grid.addComponent(numericalDataCheckBox);
         
         numericalDataCheckBox.addListener(new ValueChangeListener() {
-
-			public void valueChange(ValueChangeEvent event) {
-				switchNumOutput();
-			}
-        	
+            public void valueChange(ValueChangeEvent event) {
+               if(numericalDataCheckBox.getValue().equals(true)) {
+                 testNode.setDisplayedName(testNode.name);
+                 ipDropdown.removeAllItems();
+                 ipDropdown.addItem(testNode.getDisplayedName());
+                 
+               }else if(numericalDataCheckBox.getValue().equals(false)){
+                 testNode.setDisplayedName(testNode.ip);
+                 ipDropdown.removeAllItems();
+                 ipDropdown.addItem(testNode.getDisplayedName());
+               }
+            }
         });
        
         final Button pingButton = new Button("Ping");        
@@ -91,19 +94,4 @@ public class PingWindow extends Window{
         addComponent(layout);
 
     }
-    
-	private void switchNumOutput() {
-		ipDropdown.removeAllItems();
-		if (numOutput == false){
-			testNode.setDisplayedName(testNode.ip);
-	        ipDropdown.addItem(testNode.getDisplayedName());
-	        numOutput = true;
-		} else {
-			testNode.setDisplayedName(testNode.name);
-            ipDropdown.addItem(testNode.getDisplayedName());
-            numOutput = false;
-		}
-		ipDropdown.select(testNode.getDisplayedName());
-		ipDropdown.requestRepaint();
-	}
 }
