f75971e734ab81382d9ea7bfb8bdaef8684cbbf6
Seth Leger <seth@opennms.org>
2012-06-19 10:49:10 -0400

    Removed JUnitSnmpAgent that is binding to localhost which is probably causing intermittent failures in the unit tests.

--- integrations/opennms-snmp-asset-provisioning-adapter/src/test/java/org/opennms/netmgt/provision/SnmpAssetProvisioningAdapterIntegrationTest.java
+++ integrations/opennms-snmp-asset-provisioning-adapter/src/test/java/org/opennms/netmgt/provision/SnmpAssetProvisioningAdapterIntegrationTest.java
@@ -75,7 +75,6 @@ import org.springframework.util.Assert;
 @JUnitConfigurationEnvironment
 @JUnitTemporaryDatabase
 @JUnitSnmpAgents(value={
-		@JUnitSnmpAgent(resource = "classpath:snmpAssetTestData.properties"),
 		@JUnitSnmpAgent(host="192.168.1.1", resource = "classpath:snmpAssetTestData.properties"),
 		@JUnitSnmpAgent(host="172.20.1.201", resource = "classpath:snmpAssetTestData.properties"),
 		@JUnitSnmpAgent(host="172.20.1.204", resource = "classpath:snmpAssetTestData.properties")
