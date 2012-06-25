c7ee691193b9250530063d2f4aaba249f4f56f83
Seth Leger <seth@opennms.org>
2012-06-25 12:50:10 -0400

    Removed generics from class that overrides a Hibernate class (since Hibernate wasn't using generics). This fixes a compile problem on Java7.

--- opennms-model/src/main/java/org/opennms/netmgt/model/OnmsCriteria.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/OnmsCriteria.java
@@ -338,11 +338,13 @@ public class OnmsCriteria {
             return new OnmsCriteria.OnmsDetachedCriteria(entityName, alias);
         }
         
-        public static OnmsCriteria.OnmsDetachedCriteria forClass(Class<?> clazz) {
+        @SuppressWarnings("unchecked")
+        public static OnmsCriteria.OnmsDetachedCriteria forClass(Class clazz) {
             return new OnmsCriteria.OnmsDetachedCriteria(clazz.getName());
         }
         
-        public static OnmsCriteria.OnmsDetachedCriteria forClass(Class<?> clazz, String alias) {
+        @SuppressWarnings("unchecked")
+        public static OnmsCriteria.OnmsDetachedCriteria forClass(Class clazz, String alias) {
             return new OnmsCriteria.OnmsDetachedCriteria(clazz.getName() , alias);
         }
         
