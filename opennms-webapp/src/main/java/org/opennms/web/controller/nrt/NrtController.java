56b4e77efd393f52583ed68cba4cc8f827e8c6d9
Markus Neumann <MarkusNeumannMarkus@gmail.com>
2012-06-25 20:41:40 -0500

    controller for NearRealTime Collection

---
+++ opennms-webapp/src/main/java/org/opennms/web/controller/nrt/NrtController.java
@@ -0,0 +1,144 @@
+package org.opennms.web.controller.nrt;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+import org.opennms.netmgt.dao.GraphDao;
+import org.opennms.netmgt.dao.NodeDao;
+import org.opennms.netmgt.dao.ResourceDao;
+import org.opennms.netmgt.model.OnmsNode;
+import org.opennms.netmgt.model.OnmsResource;
+import org.opennms.netmgt.model.PrefabGraph;
+import org.opennms.netmgt.model.RrdGraphAttribute;
+import org.opennms.nrtcollector.api.model.CollectionJob;
+import org.opennms.nrtcollector.api.model.SnmpCollectionJob;
+import org.opennms.web.graph.Graph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Markus Neumann
+ */
+@Controller
+@RequestMapping("/nrt/starter.htm")
+public class NrtController {
+    private static Logger logger = LoggerFactory.getLogger(NrtController.class);
+
+    @Autowired
+    private GraphDao m_graphDao;
+    
+    @Autowired
+    private NodeDao m_nodeDao;
+    
+    @Autowired
+    private ResourceDao m_resourceDao;
+    
+    @RequestMapping(method = RequestMethod.GET, params = {"resourceId", "report"})
+    public ModelAndView nrtStart(String resourceId, String report) {
+        assert(resourceId != null);
+        logger.debug("resourceId: '{}'", resourceId);
+        
+        assert(report != null);
+        logger.debug("report: '{}'", report);
+        
+        OnmsResource reportResource = m_resourceDao.getResourceById(resourceId);
+        OnmsResource topResource = reportResource.getParent();
+        OnmsNode node = (OnmsNode)topResource.getEntity();
+        
+        PrefabGraph prefabGraph = m_graphDao.getPrefabGraph(report);
+        //TODO Tak graph service is able to check is a graph is propper for a given resource, check that later.
+        lookUpMetricsForColumnsOfPrefabGraphs(prefabGraph, reportResource);
+        
+        CollectionJob collectionJob = new SnmpCollectionJob();
+        collectionJob.setService("SNMP");
+
+        collectionJob.setNodeId(node.getId());
+        String netInterface = node.getPrimaryInterface().getIpAddress().getHostAddress();
+        logger.debug("netInterface '{}'", netInterface);
+        collectionJob.setNetInterface(netInterface);
+        
+        Set<String> resultDestinations = new HashSet<String>();
+        resultDestinations.add("NrtResults");
+        
+        for (int i = 0; i < prefabGraph.getColumns().length; i++) {
+            logger.debug("Adding Metric '{}' with MetricId '{}' to collectionJob", prefabGraph.getColumns()[i], prefabGraph.getMetricIds()[i]);
+            collectionJob.addMetric(prefabGraph.getColumns()[i], resultDestinations);
+        }
+        logger.debug("CollectionJob '{}'", collectionJob.toString());
+        
+        Map<String, String> model = new HashMap<String, String>();
+        model.put("collectionJob", collectionJob.toString());
+        return new ModelAndView("realtime", model);
+    }
+    
+        /**
+     * <p>getRequiredRrGraphdAttributes</p>
+     *
+     * @return a {@link java.util.Collection} object.
+     */
+    public Collection<RrdGraphAttribute> getRequiredRrdGraphAttributes(OnmsResource onmsResource, PrefabGraph prefabGraph) {
+        Map<String, RrdGraphAttribute> available = onmsResource.getRrdGraphAttributes();
+        Set<RrdGraphAttribute> reqAttrs = new LinkedHashSet<RrdGraphAttribute>();
+        for(String attrName : prefabGraph.getColumns()) {
+            RrdGraphAttribute attr = available.get(attrName);
+            if (attr != null) {
+                reqAttrs.add(attr);
+            }
+        }
+        return reqAttrs;
+    }
+    
+     /**
+     * Adds the Metrics corresponding to the columns into the prefabGraph. Based
+     * on a meta file lookup. The files to check for metric to column name
+     * mappings are provided by the filesToPromote. At the moment this method
+     * will check for the filenames in the list and expects to file a file with
+     * this name and a .meta ending.
+     *
+     * @param prefabGraph
+     * @param filesToPromoteForThisGraph
+     */
+    public void lookUpMetricsForColumnsOfPrefabGraphs(PrefabGraph prefabGraph, OnmsResource onmsResource) {
+        //Build a Hashmap with all columns to metrics from the files
+        Map<String, String> columnsToMetrics = new HashMap<String, String>();
+
+        //get all metrics to columns mappings from the files in to hashmap
+        for (RrdGraphAttribute attr : getRequiredRrdGraphAttributes(onmsResource, prefabGraph)) {
+            String fileName = null;
+            
+            try {
+                fileName = m_resourceDao.getRrdDirectory() + File.separator + attr.getRrdRelativePath();
+                
+                //get meta files instead of rrd or jrb
+                fileName = fileName.substring(0, fileName.lastIndexOf("."));
+                fileName = fileName.concat(".meta");
+                BufferedReader bf = new BufferedReader(new FileReader(fileName));
+
+                String mappingLine = "";
+                while (mappingLine != null) {
+                    mappingLine = bf.readLine();
+                    String metric = mappingLine.substring(0, mappingLine.lastIndexOf("="));
+                    String column = mappingLine.substring(mappingLine.lastIndexOf("=") + 1);
+                    columnsToMetrics.put(column, metric);
+                }
+            } catch (Exception ex) {
+                logger.error("Problem by looking up metrics for cloumns in context of prefabgraphs from meta file '{}' '{}'", fileName, ex.getMessage());
+            }
+        }
+        /**
+         * put the metrics from the columnsToMetrics map into the metrics array of the prefabGraph.
+         */
+        String[] metrics = new String[prefabGraph.getColumns().length];
+        for (int i = 0; i < prefabGraph.getColumns().length; i++) {
+            metrics[i] = columnsToMetrics.get(prefabGraph.getColumns()[i]);
+        }
+        prefabGraph.setMetricIds(metrics);
+    }
+}
