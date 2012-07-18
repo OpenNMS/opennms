5092c24a099a6b3996a11b5b86c7adad1cf2b737
Matt Brozowski <brozow@opennms.org>
2012-07-18 12:04:43 -0400

    make sure applicationContext-dao.xml references include applicationContext-soa.xml

--- opennms-services/src/test/java/org/opennms/netmgt/linkd/LinkdCapsdIntegrationTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/linkd/LinkdCapsdIntegrationTest.java
@@ -64,6 +64,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(OpenNMSJUnit4ClassRunner.class)
 @ContextConfiguration(locations={
         "classpath:/META-INF/opennms/mockEventIpcManager.xml",
+        "classpath:/META-INF/opennms/applicationContext-soa.xml",
         "classpath:/META-INF/opennms/applicationContext-dao.xml",
         "classpath:/META-INF/opennms/applicationContext-daemon.xml",
         "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
