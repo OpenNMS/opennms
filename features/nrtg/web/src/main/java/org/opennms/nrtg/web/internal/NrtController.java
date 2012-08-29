9b86b94188afd351e1e99f87b27655d9d726c177
Ronny Trommer <ronny@opennms.org>
2012-08-29 12:16:58 +0200

    Added GPLv3 licensing header in NRTG feature

--- features/nrtg/web/src/main/java/org/opennms/nrtg/web/internal/NrtController.java
+++ features/nrtg/web/src/main/java/org/opennms/nrtg/web/internal/NrtController.java
@@ -1,3 +1,31 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2010-2012 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2011 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
 package org.opennms.nrtg.web.internal;
 
 import org.opennms.netmgt.config.SnmpAgentConfigFactory;
@@ -26,12 +54,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Markus Neumann
@@ -132,16 +155,16 @@ public class NrtController {
 
         ModelAndView modelAndView = new ModelAndView("nrt/realtime");
         modelAndView.addObject("nrtCollectionTaskId", nrtCollectionTaskId);
-        
+
         modelAndView.addObject("graphTitle", prefabGraph.getTitle());
         modelAndView.addObject("graphName", prefabGraph.getName());
         modelAndView.addObject("graphDescription", prefabGraph.getDescription());
-        
+
         NrtRrdCommandFormatter rrdFormatter = new NrtRrdCommandFormatter(prefabGraph);
         modelAndView.addObject("rrdGraphString", rrdFormatter.getRrdGraphString());
         modelAndView.addObject("metricsMapping", rrdFormatter.getRrdMetricsMapping());
-        
-        
+
+
         return modelAndView;
     }
 
@@ -193,13 +216,13 @@ public class NrtController {
             } catch (Exception ex) {
                 logger.error("Problem by looking up metrics for cloumns in context of prefabgraphs from meta file '{}' '{}'", fileName, ex.getMessage());
             } finally {
-            	if(bf != null) {
+                if (bf != null) {
                     try {
                         bf.close();
                     } catch (IOException ex) {
                         logger.warn("problem by reader close", ex);
                     }
-            	}
+                }
             }
         }
         /**
