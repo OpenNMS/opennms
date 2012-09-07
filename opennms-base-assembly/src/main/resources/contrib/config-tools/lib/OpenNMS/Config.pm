e8cf9fb9a1d6fd27094835010f7d82d118f3b084
Benjamin Reed <ranger@opennms.org>
2012-09-07 17:00:05 -0400

    skeleton of a project for the git config script

---
+++ opennms-base-assembly/src/main/resources/contrib/config-tools/lib/OpenNMS/Config.pm
@@ -0,0 +1,36 @@
+package OpenNMS::Config;
+
+use 5.008008;
+use strict;
+use warnings;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our $VERSION = '0.1.0';
+
+1;
+__END__
+=head1 NAME
+
+OpenNMS::Config - OpenNMS configuration-management tools.
+
+=head1 SYNOPSIS
+
+  use OpenNMS::Config;
+
+=head1 AUTHOR
+
+Benjamin Reed E<lt>ranger@opennms.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by The OpenNMS Group, Inc.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
