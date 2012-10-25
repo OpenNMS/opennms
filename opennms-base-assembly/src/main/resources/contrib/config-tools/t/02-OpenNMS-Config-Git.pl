61db373d367e26892d7aed5e67d1de2212fb0b83
OpenNMS Maintainer <allofus@lists.opennms.com>
2012-10-25 12:07:42 -0400

    add more unit tests for the git config handling

--- opennms-base-assembly/src/main/resources/contrib/config-tools/t/02-OpenNMS-Config-Git.pl
+++ opennms-base-assembly/src/main/resources/contrib/config-tools/t/02-OpenNMS-Config-Git.pl
@@ -87,9 +87,9 @@ is($git->get_index_status('bar.txt'), 'deleted');
 
 rmtree("target");
 
-$git = OpenNMS::Config::Git->new("target/git-test/repo");
+$git = OpenNMS::Config::Git->new("$testdir");
 $git->init(branch_name => 'pristine');
-ok(-d "target/git-test/repo/.git");
+ok(-d "$testdir/.git");
 is($git->get_branch_name(), 'pristine');
 
 $git->author_name('git.t');
@@ -97,13 +97,13 @@ $git->author_email('t/git.t');
 is($git->author_name(), 'git.t');
 is($git->author_email(), 't/git.t');
 
-write_file('target/git-test/repo/new.txt', 'This file is new!');
+write_file("$testdir/new.txt", 'This file is new!');
 
 $git->add("new.txt");
 # new, modified, untracked, committed, deleted
 is($git->get_index_status("new.txt"), 'new');
 
-write_file('target/git-test/repo/untracked.txt', 'This file is untracked.');
+write_file("$testdir/untracked.txt", 'This file is untracked.');
 is($git->get_index_status('untracked.txt'), 'untracked');
 
 my @changes = $git->get_modifications();
@@ -132,10 +132,10 @@ $git->commit('add previously untracked file');
 
 $git->checkout('master');
 is($git->get_branch_name(), 'master');
-ok(! -e 'target/git-test/repo/untracked.txt');
+ok(! -e "$testdir/untracked.txt");
 
 $git->merge('pristine');
-ok(-e 'target/git-test/repo/untracked.txt');
+ok(-e "$testdir/untracked.txt");
 is($git->get_index_status('untracked.txt'), 'unchanged');
 
 $git->tag('bogus-tag');
@@ -145,8 +145,8 @@ eval {
 };
 ok($@);
 
-write_file('target/git-test/repo/added.txt');
-unlink('target/git-test/repo/untracked.txt');
+write_file("$testdir/added.txt");
+unlink("$testdir/untracked.txt");
 @changes = $git->get_modifications();
 is(scalar(@changes), 2);
 for my $change (@changes) {
@@ -156,11 +156,64 @@ $git->commit('add a file, remove a file');
 @changes = $git->get_modifications();
 is(scalar(@changes), 0);
 
-unlink('target/git-test/repo/added.txt');
-write_file('target/git-test/repo/monkey.txt');
-write_file('target/git-test/repo/new.txt', {append => 1}, "/nsomething else\n");
+unlink("$testdir/added.txt");
+write_file("$testdir/monkey.txt");
+write_file("$testdir/new.txt", {append => 1}, "\nsomething else\n");
 @changes = $git->get_modifications();
 is(scalar(@changes), 3);
 $git->commit_modifications('added a file, removed a file, modified a file');
 @changes = $git->get_modifications();
 is(scalar(@changes), 0);
+
+sub create_runtime_test_env {
+	rmtree('target');
+	$git->init(
+		author_name => 'Horatio',
+		author_email => 'ho@rat.io',
+		branch_name => 'pristine',
+	);
+	write_file("$testdir/foo.txt", "This is foo.\n");
+	$git->add('.');
+	$git->commit('add foo');
+	$git->create_branch('runtime', 'pristine');
+	$git->checkout('runtime');
+	write_file("$testdir/foo.txt", "Crikey!\n");
+	$git->add('.');
+	$git->commit('change runtime version of foo');
+	$git->checkout('pristine');
+	write_file("$testdir/foo.txt", "This is foo, suckas!\n");
+	$git->add('.');
+	$git->commit('change pristine version of foo');
+	$git->checkout('runtime');
+}
+
+create_runtime_test_env();
+
+eval { $git->merge(); };
+ok($@, 'Merge with no arguments should croak.');
+eval { $git->merge('pristine'); };
+is($git->get_index_status('foo.txt'), 'modified');
+my @mods = $git->get_modifications();
+is(scalar @mods, 1);
+
+create_runtime_test_env();
+eval { $git->merge_or_fail('pristine'); };
+is($git->get_index_status('foo.txt'), 'modified');
+@mods = $git->get_modifications();
+is(scalar @mods, 1);
+
+$git->reset('runtime');
+is($git->get_index_status('foo.txt'), 'unchanged');
+@mods = $git->get_modifications();
+is(scalar @mods, 0);
+is(read_file("$testdir/foo.txt"), "Crikey!\n");
+
+create_runtime_test_env();
+eval { $git->merge_or_fail('pristine'); };
+$git->reset('runtime');
+$git->save_changes_compared_to('pristine');
+@mods = $git->get_modifications();
+is(scalar @mods, 2);
+is(read_file("$testdir/foo.txt"), "This is foo, suckas!\n");
+ok(-e "$testdir/foo.txt.old");
+is(read_file("$testdir/foo.txt.old"), "Crikey!\n");
