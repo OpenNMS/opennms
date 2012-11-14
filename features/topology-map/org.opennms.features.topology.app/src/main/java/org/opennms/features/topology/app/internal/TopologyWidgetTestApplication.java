d84f681b37d703ff6151fd8174488991eecd2295
Donald Desloge <thedesloge@gmail.com>
2012-11-14 16:41:45 -0500

    Moved graphcontainer addlistener call due to some vaadin weirdness

--- features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/TopologyWidgetTestApplication.java
+++ features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/TopologyWidgetTestApplication.java
@@ -34,6 +34,8 @@ import java.util.List;
 import org.opennms.features.topology.api.DisplayState;
 import org.opennms.features.topology.api.GraphContainer;
 import org.opennms.features.topology.api.IViewContribution;
+import org.opennms.features.topology.api.SelectionManager;
+import org.opennms.features.topology.api.SelectionManager.SelectionListener;
 import org.opennms.features.topology.api.TopologyProvider;
 import org.opennms.features.topology.api.WidgetContext;
 import org.opennms.features.topology.api.support.FilterableHierarchicalContainer;
@@ -117,6 +119,13 @@ public class TopologyWidgetTestApplication extends Application implements Comman
 		m_graphContainer.setLayoutAlgorithm(new FRLayoutAlgorithm());
 
 		m_topologyComponent = new TopologyComponent(m_graphContainer);
+		m_graphContainer.getSelectionManager().addSelectionListener(new SelectionListener() {
+            
+            @Override
+            public void selectionChanged(SelectionManager selectionManager) {
+                m_topologyComponent.requestRepaint();
+            }
+        });
 		m_topologyComponent.setIconRepoManager(m_iconRepositoryManager);
 		m_topologyComponent.setSizeFull();
 		m_topologyComponent.addMenuItemStateListener(this);
