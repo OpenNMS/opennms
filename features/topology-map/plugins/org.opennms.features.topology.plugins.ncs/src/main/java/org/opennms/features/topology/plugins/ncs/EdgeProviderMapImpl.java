a83b44c0e0eb836158994c5999ea106da0903d11
Seth Leger <seth@opennms.org>
2012-11-14 10:01:57 -0500

    Added SelectionTreeFactory which will use the blueprint container to instantiate SelectionTree components. Separated EdgeProvider code out of NCSSelectionTree into its own class.

---
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.ncs/src/main/java/org/opennms/features/topology/plugins/ncs/EdgeProviderMapImpl.java
@@ -0,0 +1,63 @@
+package org.opennms.features.topology.plugins.ncs;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.opennms.features.topology.api.topo.Edge;
+import org.opennms.features.topology.api.topo.EdgeListener;
+import org.opennms.features.topology.api.topo.EdgeProvider;
+import org.opennms.features.topology.api.topo.EdgeRef;
+
+public class EdgeProviderMapImpl implements EdgeProvider {
+
+	private Map<String,Edge> m_edges = new HashMap<String,Edge>();
+
+	public EdgeProviderMapImpl() {
+	}
+
+	@Override
+	public void addEdgeListener(EdgeListener vertexListener) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Edge getEdge(String id) {
+		return m_edges.get(id);
+	}
+
+	@Override
+	public Edge getEdge(EdgeRef reference) {
+		return getEdge(reference.getId());
+	}
+
+	@Override
+	public List<? extends Edge> getEdges() {
+		return Collections.unmodifiableList(Collections.list(Collections.enumeration(m_edges.values())));
+	}
+
+	@Override
+	public List<? extends Edge> getEdges(Collection<? extends EdgeRef> references) {
+		List<Edge> retval = new ArrayList<Edge>();
+		for (EdgeRef reference : references) {
+			Edge edge = getEdge(reference);
+			if (edge != null) {
+				retval.add(edge);
+			}
+		}
+		return Collections.unmodifiableList(retval);
+	}
+
+	@Override
+	public String getNamespace() {
+		return "ncs";
+	}
+
+	@Override
+	public void removeEdgeListener(EdgeListener vertexListener) {
+		throw new UnsupportedOperationException();
+	}
+}
