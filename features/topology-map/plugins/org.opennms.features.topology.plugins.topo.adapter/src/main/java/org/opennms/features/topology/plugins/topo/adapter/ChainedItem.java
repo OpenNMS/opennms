4c50d8b0a07cca36821c8a0d4e140523bdd30b8c
Matt Brozowski <matt@brozowski.com>
2012-12-04 12:48:49 -0500

    SPC-537:  Make sure we check for vertices that have a parent of themselves.. how does this happen?

--- features/topology-map/plugins/org.opennms.features.topology.plugins.topo.adapter/src/main/java/org/opennms/features/topology/plugins/topo/adapter/ChainedItem.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.topo.adapter/src/main/java/org/opennms/features/topology/plugins/topo/adapter/ChainedItem.java
@@ -20,13 +20,15 @@ public class ChainedItem implements Item {
 	@Override
 	public Property getItemProperty(Object id) {
 		Property property = m_item == null ? null : m_item.getItemProperty(id);
-		return property != null ? property : m_next.getItemProperty(id);
+		return property != null ? property : m_next != null ? m_next.getItemProperty(id) : null;
 	}
 
 	@Override
 	public Collection<?> getItemPropertyIds() {
 		Set<Object> propertyIds = new LinkedHashSet<Object>(m_item.getItemPropertyIds());
-		propertyIds.addAll(m_next.getItemPropertyIds());
+		if (m_next != null) {
+			propertyIds.addAll(m_next.getItemPropertyIds());
+		}
 		return propertyIds;
 	}
 
