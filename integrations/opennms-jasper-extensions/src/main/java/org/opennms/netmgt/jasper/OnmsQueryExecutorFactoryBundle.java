1a9f8386236e783308baa6c76477a5a26ddf4f4e
Donald Desloge <thedesloge@gmail.com>
2012-12-21 11:20:15 -0500

    Update to Jasper reports, checks rrd strategy for language

--- integrations/opennms-jasper-extensions/src/main/java/org/opennms/netmgt/jasper/OnmsQueryExecutorFactoryBundle.java
+++ integrations/opennms-jasper-extensions/src/main/java/org/opennms/netmgt/jasper/OnmsQueryExecutorFactoryBundle.java
@@ -28,12 +28,10 @@
 
 package org.opennms.netmgt.jasper;
 
-import org.opennms.netmgt.jasper.jrobin.JRobinQueryExecutorFactory;
-import org.opennms.netmgt.jasper.rrdtool.RrdtoolQueryExecutorFactory;
-
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.query.JRQueryExecuterFactory;
 import net.sf.jasperreports.engine.query.QueryExecuterFactoryBundle;
+
 import org.opennms.netmgt.jasper.jrobin.JRobinQueryExecutorFactory;
 import org.opennms.netmgt.jasper.resource.ResourceQueryExecuterFactory;
 import org.opennms.netmgt.jasper.rrdtool.RrdtoolQueryExecutorFactory;
@@ -45,15 +43,34 @@ public class OnmsQueryExecutorFactoryBundle implements QueryExecuterFactoryBundl
     }
 
     public JRQueryExecuterFactory getQueryExecuterFactory(String language) throws JRException {
-        if("jrobin".equals(language)) {
+        String reportLanguage = checkReportLanguage(language);
+        
+        if("jrobin".equals(reportLanguage)) {
             return new JRobinQueryExecutorFactory();
-        } else if("rrdtool".equals(language)) {
+        } else if("rrdtool".equals(reportLanguage)) {
             return new RrdtoolQueryExecutorFactory();
-        } else if("resourceQuery".equals(language)) {
+        } else if("resourceQuery".equals(reportLanguage)) {
             return new ResourceQueryExecuterFactory();
         } else {
             return null;
         }
+    }
+
+    private String checkReportLanguage(String language) {
+        if(language.equals("resourceQuery")) return language;
+        
+        String strategy = System.getProperty("org.opennms.rrd.strategyClass");
+        
+        if(strategy == null) return language;
+        
+        String[] strategySplit = strategy.split("\\.");
+        String rrdStrategy = strategySplit[strategySplit.length - 1];
+        if(rrdStrategy.equals("JniRrdStrategy")) {
+            return "rrdtool";
+        } else if(rrdStrategy.equals("JRobinRrdStrategy")) {
+            return "jrobin";
+        }
+        return "";
     }
 
 }
