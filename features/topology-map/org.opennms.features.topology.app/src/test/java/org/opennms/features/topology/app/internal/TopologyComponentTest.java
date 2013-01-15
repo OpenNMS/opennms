7dfe4308522ef9818532d4fed9c3518ff19f7df8
Donald Desloge <desloge@opennms.com>
2013-01-15 11:15:55 -0500

    fix broken test

--- features/topology-map/org.opennms.features.topology.app/src/test/java/org/opennms/features/topology/app/internal/TopologyComponentTest.java
+++ features/topology-map/org.opennms.features.topology.app/src/test/java/org/opennms/features/topology/app/internal/TopologyComponentTest.java
@@ -276,12 +276,7 @@ public class TopologyComponentTest {
     
     private void mockGraphAttrs(PaintTarget target, int semanticZoomLevel, boolean fitToView) throws PaintException {
         target.addAttribute("scale", 1.0);
-        target.addAttribute("clientX", 0);
-        target.addAttribute("clientY", 0);
-        target.addAttribute("semanticZoomLevel", semanticZoomLevel);
         target.addAttribute("activeTool", "pan");
-        target.addAttribute("panToSelection", false);
-        target.addAttribute("fitToView", fitToView);
         
         target.addAttribute(EasyMock.eq("boundX"), EasyMock.anyInt());
         target.addAttribute(EasyMock.eq("boundY"), EasyMock.anyInt());
