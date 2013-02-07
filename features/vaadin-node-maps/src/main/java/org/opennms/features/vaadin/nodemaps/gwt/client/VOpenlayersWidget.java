60a8d06e7bcb86044aaf2cb6e990fb9d1cbb605f
Benjamin Reed <ranger@opennms.org>
2013-02-07 15:49:40 -0500

    major refactoring and cleanup; more objects, pass alarm data

--- features/vaadin-node-maps/src/main/java/org/opennms/features/vaadin/nodemaps/gwt/client/VOpenlayersWidget.java
+++ features/vaadin-node-maps/src/main/java/org/opennms/features/vaadin/nodemaps/gwt/client/VOpenlayersWidget.java
@@ -28,7 +28,11 @@
 
 package org.opennms.features.vaadin.nodemaps.gwt.client;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.opennms.features.vaadin.nodemaps.gwt.client.openlayers.FeatureCollection;
 import org.opennms.features.vaadin.nodemaps.gwt.client.openlayers.NodeFeature;
@@ -62,25 +66,36 @@ public class VOpenlayersWidget extends GWTOpenlayersWidget implements Paintable 
         final UIDL nodeUIDL = uidl.getChildByTagName("nodes");
 
         final FeatureCollection featureCollection = FeatureCollection.create();
-
         for (final Iterator<?> iterator = nodeUIDL.getChildIterator(); iterator.hasNext();) {
             final UIDL node = (UIDL) iterator.next();
-
-            final float latitude = node.getFloatAttribute("latitude");
-            final float longitude = node.getFloatAttribute("longitude");
-
-            final NodeFeature feature = NodeFeature.create(latitude, longitude).cast();
-
-            for (final String key : new String[] { "severityLabel", "nodeLabel", "foreignSource", "foreignId", "ipAddress", "severity", "nodeId", "unackedCount" }) {
-                if (node.hasAttribute(key)) feature.putProperty(key, node.getStringAttribute(key));
-            }
-
+            final NodeFeature feature = NodeFeature.create(node).cast();
             log(feature.asString());
-
             featureCollection.add(feature);
         }
+        setFeatures(featureCollection);
 
-        setFeatureCollection(featureCollection);
+        final UIDL alarmUIDL = uidl.getChildByTagName("alarms");
+
+        final Map<Integer,List<Alarm>> alarms = new HashMap<Integer,List<Alarm>>();
+        for (final Iterator<?> iterator = alarmUIDL.getChildIterator(); iterator.hasNext(); ) {
+            final UIDL uidlAlarm = (UIDL) iterator.next();
+            final Alarm alarm = Alarm.create(uidlAlarm);
+            addAlarm(alarms, alarm);
+        }
+        setAlarms(alarms);
+
         updateFeatureLayer();
+    }
+
+    private void addAlarm(final Map<Integer, List<Alarm>> alarms, final Alarm alarm) {
+        final List<Alarm> alarmList;
+        final Integer nodeId = alarm.getNodeId();
+        if (alarms.containsKey(nodeId)) {
+            alarmList = alarms.get(nodeId);
+        } else {
+            alarmList = new ArrayList<Alarm>();
+            alarms.put(nodeId, alarmList);
+        }
+        alarmList.add(alarm);
     }
 }
--- features/vaadin-node-maps/src/main/java/org/opennms/features/vaadin/nodemaps/gwt/client/openlayers/NodeFeature.java
+++ features/vaadin-node-maps/src/main/java/org/opennms/features/vaadin/nodemaps/gwt/client/openlayers/NodeFeature.java
@@ -1,48 +1,41 @@
 package org.opennms.features.vaadin.nodemaps.gwt.client.openlayers;
 
+import com.vaadin.terminal.gwt.client.UIDL;
+
 public class NodeFeature extends GeoJSONFeature {
     protected NodeFeature() {}
 
-    protected native final String getAttribute(final String key) /*-{
-        return this[key];
-    }-*/;
+    public static NodeFeature create(final UIDL node) {
+        final float latitude = node.getFloatAttribute("latitude");
+        final float longitude = node.getFloatAttribute("longitude");
 
-    protected native final void setAttribute(final String key, final String value) /*-{
-        this[key] = value;
-    }-*/;
+        final NodeFeature me = GeoJSONFeature.create(latitude, longitude).cast();
+
+        for (final String key : new String[] { "nodeLabel", "foreignSource", "foreignId", "ipAddress", "nodeId" }) {
+            if (node.hasAttribute(key)) me.putProperty(key, node.getStringAttribute(key));
+        }
+
+        return me;
+    }
 
     public final Integer getNodeId() {
-        final String id = getAttribute("nodeId");
+        final String id = getProperty("nodeId");
         return id == null? null : Integer.valueOf(id);
     }
 
     public final String getNodeLabel() {
-        return getAttribute("nodeLabel");
+        return getProperty("nodeLabel");
     }
 
     public final String getForeignSource() {
-        return getAttribute("foreignSource");
+        return getProperty("foreignSource");
     }
 
     public final String getForeignId() {
-        return getAttribute("foreignId");
+        return getProperty("foreignId");
     }
     
     public final String getIpAddress() {
-        return getAttribute("ipAddress");
-    }
-
-    public final String getSeverityLabel() {
-        return getAttribute("severityLabel");
-    }
-
-    public final Integer getSeverity() {
-        final String severity = getAttribute("severity");
-        return severity == null? null : Integer.valueOf(severity);
-    }
-
-    public final Integer getUnackedCount() {
-        final String count = getAttribute("unackedCount");
-        return count == null? 0 : Integer.valueOf(count);
+        return getProperty("ipAddress");
     }
 }
