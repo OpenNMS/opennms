4ae46c6a370c52e38aa809e218a7fc47b1f9d8a9
Seth Leger <seth@opennms.org>
2013-03-01 15:57:01 -0500

    Changed SelectionAwareTable so that it avoids circular registrations of SelectionListeners and SelectionNotifiers. Table column ClickEvent filtering works now!

--- features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/SelectionAwareTable.java
+++ features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/SelectionAwareTable.java
@@ -31,6 +31,7 @@ package org.opennms.features.topology.app.internal;
 import java.io.Serializable;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 import org.opennms.features.topology.api.SelectionContext;
 import org.opennms.features.topology.api.SelectionListener;
@@ -45,6 +46,7 @@ public class SelectionAwareTable extends Table implements SelectionListener, Sel
 	private static final long serialVersionUID = 2761774077365441249L;
 
 	private final OnmsDaoContainer<?,? extends Serializable> m_container;
+	private final Set<SelectionNotifier> m_selectionNotifiers = new CopyOnWriteArraySet();
 
 	/**
 	 *  Leave OnmsDaoContainer without generics; the Aries blueprint code cannot match up
@@ -73,6 +75,9 @@ public class SelectionAwareTable extends Table implements SelectionListener, Sel
 	@Override
 	public void addSelectionListener(SelectionListener listener) {
 		m_container.addSelectionListener(listener);
+		for (SelectionNotifier notifier : m_selectionNotifiers) {
+			notifier.addSelectionListener(listener);
+		}
 	}
 
 	/**
@@ -81,6 +86,9 @@ public class SelectionAwareTable extends Table implements SelectionListener, Sel
 	@Override
 	public void removeSelectionListener(SelectionListener listener) {
 		m_container.removeSelectionListener(listener);
+		for (SelectionNotifier notifier : m_selectionNotifiers) {
+			notifier.removeSelectionListener(listener);
+		}
 	}
 
 	/**
@@ -89,15 +97,23 @@ public class SelectionAwareTable extends Table implements SelectionListener, Sel
 	@Override
 	public void setSelectionListeners(Set<SelectionListener> listeners) {
 		m_container.setSelectionListeners(listeners);
+		for (SelectionNotifier notifier : m_selectionNotifiers) {
+			notifier.setSelectionListeners(listeners);
+		}
 	}
 
+	/**
+	 * Call this method before any of the {@link SelectionNotifier} methods to ensure
+	 * that the {@link SelectionListener} instances are registered with all of the
+	 * {@link ColumnGenerator} classes that also implement {@link SelectionNotifier}.
+	 */
 	public void setColumnGenerators(Map generators) {
 		for (Object key : generators.keySet()) {
 			super.addGeneratedColumn(key, (ColumnGenerator)generators.get(key));
 			// If any of the column generators are {@link SelectionNotifier} instances,
 			// then register this component as a listener for events that they generate.
 			try {
-				((SelectionNotifier)generators.get(key)).addSelectionListener(this);
+				m_selectionNotifiers.add((SelectionNotifier)generators.get(key));
 			} catch (ClassCastException e) {}
 		}
 	}
