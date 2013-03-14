65d58aff1621499a88df178f0765830ea6b57da0
Benjamin Reed <ranger@opennms.org>
2013-03-14 13:32:31 -0400

    fix handling of "enter" in search control

--- features/vaadin-node-maps/src/main/java/org/opennms/features/vaadin/nodemaps/internal/gwt/client/ui/controls/search/SearchControl.java
+++ features/vaadin-node-maps/src/main/java/org/opennms/features/vaadin/nodemaps/internal/gwt/client/ui/controls/search/SearchControl.java
@@ -51,7 +51,7 @@ public class SearchControl extends Control {
 
     private HTMLPanel m_container;
     private TextBox m_inputBox;
-    private HTML m_submitAnchor;
+    private HTML m_submitIcon;
 
     private SearchConsumer m_searchConsumer;
     private MarkerContainer m_markerContainer;
@@ -63,6 +63,7 @@ public class SearchControl extends Control {
     private Timer m_timer;
     private CellList<NodeMarker> m_autoComplete;
     private NodeMarker m_selected;
+    private boolean m_userSubmitted = false;
 
     protected SearchControl(final JSObject element) {
         super(element);
@@ -104,7 +105,7 @@ public class SearchControl extends Control {
         VConsole.log("onAdd() called");
         
         m_container.add(m_inputBox);
-        m_container.add(m_submitAnchor);
+        m_container.add(m_submitIcon);
         m_container.add(m_autoComplete);
 
         return m_container.getElement();
@@ -120,7 +121,7 @@ public class SearchControl extends Control {
         Scheduler.get().scheduleDeferred(new ScheduledCommand() {
             @Override public void execute() {
                 final List<NodeMarker> markers = m_markerContainer.getMarkers();
-                if (m_searchConsumer.isSearching()) {
+                if (m_searchConsumer.isSearching() && !m_userSubmitted) {
                     m_autoComplete.setVisible(true);
                     updateAutocompleteStyle(m_autoComplete);
                     if (markers.size() == 1) {
@@ -159,13 +160,16 @@ public class SearchControl extends Control {
     }
 
     protected void handleSearchEvent(final NativeEvent event) {
+        VConsole.log("handleSearchEvent: type = " + event.getType());
+
         final Element target = event.getEventTarget().cast();
-        if (target.equals(m_submitAnchor.getElement())) {
-            VConsole.log("event received on the anchor, preventing default");
+        if (target.equals(m_submitIcon.getElement())) {
             event.preventDefault();
+            m_userSubmitted = true;
         }
         if (event.getKeyCode() == KeyCodes.KEY_ESCAPE) {
             m_inputBox.setText("");
+            m_userSubmitted = false;
         } else if (event.getKeyCode() == KeyCodes.KEY_DOWN) {
             final List<NodeMarker> markers = m_markerContainer.getMarkers();
             if (markers.size() > 0) {
@@ -173,6 +177,8 @@ public class SearchControl extends Control {
             }
             m_autoComplete.setFocus(true);
             m_autoComplete.setVisible(true);
+        } else if (event.getKeyCode() == KeyCodes.KEY_ENTER) {
+            m_userSubmitted = true;
         }
         m_refreshSearch = true;
         if (!m_timerActive) {
@@ -214,12 +220,12 @@ public class SearchControl extends Control {
     }
 
     private void initializeSubmitWidget() {
-        m_submitAnchor = new HTML();
-        m_submitAnchor.addStyleName("search-button");
-        m_submitAnchor.setTitle("Search locations...");
+        m_submitIcon = new HTML();
+        m_submitIcon.addStyleName("search-button");
+        m_submitIcon.setTitle("Search locations...");
 
-        DomEvent.stopEventPropagation(m_submitAnchor);
-        DomEvent.addListener(new DomEventCallback("click", m_submitAnchor) {
+        DomEvent.stopEventPropagation(m_submitIcon);
+        DomEvent.addListener(new DomEventCallback("click", m_submitIcon) {
             @Override
             protected void onEvent(final NativeEvent event) {
                 m_inputBox.setFocus(true);
@@ -285,6 +291,7 @@ public class SearchControl extends Control {
                     if (event.getKeyCode() == KeyCodes.KEY_ENTER) {
                         setSearchString = true;
                     } else if (event.getKeyCode() == KeyCodes.KEY_ESCAPE) {
+                        m_userSubmitted = false;
                         m_inputBox.setText("");
                         setSearchString = false;
                         hideAutocomplete();
@@ -294,6 +301,7 @@ public class SearchControl extends Control {
                 if (setSearchString) {
                     Scheduler.get().scheduleDeferred(new ScheduledCommand() {
                         @Override public void execute() {
+                            m_userSubmitted = true;
                             m_autoComplete.getSelectionModel().setSelected(value, false);
                             hideAutocomplete();
                             m_inputBox.setFocus(true);
