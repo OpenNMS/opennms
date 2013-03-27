ac2a97ad196dbd090eb484415e20ee6246ab5596
Benjamin Reed <ranger@opennms.org>
2013-03-27 10:11:08 -0400

    fix missing import

--- integration-tests/src/test/java/org/opennms/netmgt/config/WillItUnmarshalTest.java
+++ integration-tests/src/test/java/org/opennms/netmgt/config/WillItUnmarshalTest.java
@@ -40,6 +40,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
