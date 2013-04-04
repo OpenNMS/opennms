7dbbb49a575c9da5c50cb69bdcc771a3d0ab8a77
Markus von Rüden <mvr@opennms.com>
2013-04-04 14:49:58 +0200

    Fixed error in Unit tests

--- protocols/psm-enhanced/src/test/java/org/opennms/netmgt/poller/monitors/PageSequenceMonitorEnhancedTest.java
+++ protocols/psm-enhanced/src/test/java/org/opennms/netmgt/poller/monitors/PageSequenceMonitorEnhancedTest.java
@@ -188,7 +188,7 @@ public class PageSequenceMonitorEnhancedTest {
         m_params.put("page-sequence", ""
                 + "<?xml version=\"1.0\"?>"
                 + "<page-sequence>\n"
-                + "  <page scheme=\"https\" path=\"/ws/eBayISAPI.dll\" query=\"RegisterEnterInfo\" port=\"443\" user-agent=\"Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0)\" successMatch=\"ebaystatic.com/\" virtual-host=\"scgi.ebay.com\" disable-host-verification=\"true\"/>\n"
+                + "  <page scheme=\"https\" path=\"/ws/eBayISAPI.dll\" query=\"RegisterEnterInfo\" port=\"443\" user-agent=\"Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0)\" successMatch=\"ebaystatic.com/\" virtual-host=\"scgi.ebay.com\" disable-ssl-verification=\"true\"/>\n"
                 + "</page-sequence>\n");
 
         try {
