4d33c8153fddba793e70693d1a9f9d616a8f10d1
Markus von Rüden <mvr@opennms.com>
2013-04-25 13:45:17 +0200

    fixed merge. :)

--- opennms-config/src/test/java/org/opennms/netmgt/config/SnmpEventInfoTest.java
+++ opennms-config/src/test/java/org/opennms/netmgt/config/SnmpEventInfoTest.java
@@ -31,7 +31,7 @@ package org.opennms.netmgt.config;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.opennms.core.test.xml.XmlTest.assertXmlEquals;
