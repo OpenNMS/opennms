75d551275421f5b904c19cdffc2bbd3e0316049b
YongHua Zhan <yhzhan@juniper.net>
2013-05-09 22:23:00 -0400

    JUNIPER-ONLY: PR880522 PL-791 Alarm Notification Feature - Reload feature, suppression feature

--- features/notifications/alarm-notification/src/main/etc/alarm-notification/scripts/REST_NA_Script.sh
+++
@@ -1,4 +0,0 @@
-echo "TCA alarm notification script started "
-xml=$1
-echo "Alarm xml being sent is " $xml
-curl -v -u super:juniper123 -X POST -H "Content-type:application/xml" -d "$xml" "http://localhost:8080/serviceui/resteasy/tc-alarms"
