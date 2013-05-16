a59e27f935aad70a7d9687a6eb7cbae0abb281bc
Markus von Rüden <mvr@opennms.com>
2013-05-16 11:48:55 +0200

    Fix of #SPC-602

--- features/topology-map/plugins/org.opennms.features.topology.plugins.browsers/src/main/java/org/opennms/features/topology/plugins/browsers/SelectionAwareTable.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.browsers/src/main/java/org/opennms/features/topology/plugins/browsers/SelectionAwareTable.java
@@ -29,6 +29,8 @@
 package org.opennms.features.topology.plugins.browsers;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
@@ -46,7 +48,8 @@ public class SelectionAwareTable extends Table implements SelectionListener, Sel
 
 	private final OnmsDaoContainer<?,? extends Serializable> m_container;
 	private final Set<SelectionNotifier> m_selectionNotifiers = new CopyOnWriteArraySet<SelectionNotifier>();
-
+	private List<String> nonCollapsibleColumns = new ArrayList<String>();
+	
 	/**
 	 *  Leave OnmsDaoContainer without generics; the Aries blueprint code cannot match up
 	 *  the arguments if you put the generic types in.
@@ -131,4 +134,24 @@ public class SelectionAwareTable extends Table implements SelectionListener, Sel
 		} catch (ClassCastException e) {}
 		super.setCellStyleGenerator(generator);
 	}
+	
+	/**
+	 * Sets the non collapsbile columns.
+	 * @param nonCollapsibleColumns
+	 */
+	public void setNonCollapsibleColumns(List<String> nonCollapsibleColumns) {
+	    // set all elements to collapsible
+	    for (Object eachPropertyId : m_container.getContainerPropertyIds()) {
+	        setColumnCollapsible(eachPropertyId,  true);
+	    }
+	    
+	    // set new value
+	    if (nonCollapsibleColumns == null) nonCollapsibleColumns = new ArrayList<String>();
+        this.nonCollapsibleColumns = nonCollapsibleColumns;
+        
+        // set non collapsible
+        for (Object eachPropertyId : this.nonCollapsibleColumns) {
+            setColumnCollapsible(eachPropertyId,  false);
+        }
+    }
 }
