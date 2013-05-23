df3f73368207c2608acca75f42f6b87d487df63b
Seth Leger <seth@opennms.org>
2013-05-23 11:44:58 -0400

    Added reversible sorting to the primaryInterface sorting in NodeTable.

--- features/topology-map/plugins/org.opennms.features.topology.plugins.browsers/src/main/java/org/opennms/features/topology/plugins/browsers/NodeTable.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.browsers/src/main/java/org/opennms/features/topology/plugins/browsers/NodeTable.java
@@ -36,47 +36,87 @@ import org.opennms.netmgt.model.OnmsNode;
 @SuppressWarnings("serial")
 public class NodeTable extends SelectionAwareTable {
 
+	private static class ReverseComparator<T> implements Comparator<T> {
+		private final Comparator<T> m_comparator;
+
+		public ReverseComparator(Comparator<T> comparator) {
+			m_comparator = comparator;
+		}
+
+		@Override
+		public int compare(T o1, T o2) {
+			int comparison = m_comparator.compare(o1, o2);
+			if (comparison == 0) {
+				return 0;
+			} else if (comparison < 0) {
+				return 1;
+			} else {
+				return -1;
+			}
+		}
+
+	}
+
+	private static class PrimaryInterfaceAddressComparator implements Comparator<OnmsNode> {
+		@Override
+		public int compare(OnmsNode o1, OnmsNode o2) {
+			if (o1 == null) {
+				if (o2 == null) {
+					return 0;
+				} else {
+					return -1;
+				}
+			} else {
+				if (o2 == null) {
+					return 1;
+				} else {
+					if (o1.getPrimaryInterface() == null) {
+						if (o2.getPrimaryInterface() == null) {
+							return 0;
+						} else {
+							return -1;
+						}
+					} else {
+						if (o2.getPrimaryInterface() == null) {
+							return 1;
+						} else {
+							return new InetAddressComparator().compare(o1.getPrimaryInterface().getIpAddress(), o2.getPrimaryInterface().getIpAddress());
+						}
+					}
+				}
+			}
+		}
+	}
+
 	@SuppressWarnings("unchecked") // Because Aries Blueprint cannot handle generics
 	public NodeTable(String caption, OnmsDaoContainer container) {
 		super(caption, container);
 
 		addListener(new HeaderClickListener() {
 			public void headerClick(HeaderClickEvent event) {
+				NodeDaoContainer nodeContainer = (NodeDaoContainer)getContainerDataSource();
 				String column = (String)event.getPropertyId();
 				if ("primaryInterface".equals(column)) {
-					((NodeDaoContainer)getContainerDataSource()).additionalSorting.add(new Comparator<OnmsNode>() {
-						@Override
-						public int compare(OnmsNode o1, OnmsNode o2) {
-							if (o1 == null) {
-								if (o2 == null) {
-									return 0;
-								} else {
-									return -1;
-								}
-							} else {
-								if (o2 == null) {
-									return 1;
-								} else {
-									if (o1.getPrimaryInterface() == null) {
-										if (o2.getPrimaryInterface() == null) {
-											return 0;
-										} else {
-											return -1;
-										}
-									} else {
-										if (o2.getPrimaryInterface() == null) {
-											return 1;
-										} else {
-											return new InetAddressComparator().compare(o1.getPrimaryInterface().getIpAddress(), o2.getPrimaryInterface().getIpAddress());
-										}
-									}
-								}
-							}
+					if (nodeContainer.additionalSorting.size() == 0) {
+						nodeContainer.additionalSorting.add(new PrimaryInterfaceAddressComparator());
+					} else if (nodeContainer.additionalSorting.size() == 1) {
+						Comparator<OnmsNode> comparator = nodeContainer.additionalSorting.get(0);
+						if (comparator instanceof PrimaryInterfaceAddressComparator) {
+							nodeContainer.additionalSorting.set(0, new ReverseComparator<OnmsNode>(comparator));
+						} else {
+							nodeContainer.additionalSorting.set(0, new PrimaryInterfaceAddressComparator());
 						}
-					});
+					} else {
+						// Unexpected number of comparators in the list...
+						nodeContainer.additionalSorting.clear();
+						nodeContainer.additionalSorting.add(new PrimaryInterfaceAddressComparator());
+					}
 					refreshRowCache();
 				} else {
-					((NodeDaoContainer)getContainerDataSource()).additionalSorting.clear();
+					nodeContainer.additionalSorting.clear();
+					// We need to refresh the rows here too because the sorting for the other columns seems to be applied
+					// before this listener is fired.
+					refreshRowCache();
 				}
 			}
 		});
