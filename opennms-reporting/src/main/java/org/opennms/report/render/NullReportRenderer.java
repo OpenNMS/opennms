4e5d2854946e38dd0927c93b2e6eaaf7c68c7ec9
Ronald Roskens <ronald.roskens@gmail.com>
2013-06-25 09:49:18 -0500

    opennms-reporting: fix wrong MDC import.

--- opennms-reporting/src/main/java/org/opennms/report/render/NullReportRenderer.java
+++ opennms-reporting/src/main/java/org/opennms/report/render/NullReportRenderer.java
@@ -31,11 +31,11 @@ package org.opennms.report.render;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import org.apache.log4j.MDC;
 import org.opennms.reporting.availability.render.ReportRenderException;
 import org.opennms.reporting.availability.render.ReportRenderer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.core.io.Resource;
 
 /**
