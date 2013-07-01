7ea3350192a05eb699ddd40ecc50f6c762761b3f
Eric Evans <eevans@sym-link.com>
2013-07-01 12:02:41 -0500

    update missed reference to discovery prefix/thread-category

--- opennms-services/src/main/java/org/opennms/netmgt/discovery/Discovery.java
+++ opennms-services/src/main/java/org/opennms/netmgt/discovery/Discovery.java
@@ -82,6 +82,8 @@ public class Discovery extends AbstractServiceDaemon {
      */
     private static final DiscoveryPingResponseCallback cb = new DiscoveryPingResponseCallback();
 
+    private static final String LOG4J_CATEGORY = "discover";
+
 
     private static final int PING_IDLE = 0;
     private static final int PING_RUNNING = 1;
@@ -156,7 +158,7 @@ public class Discovery extends AbstractServiceDaemon {
      * Constructs a new discovery instance.
      */
     public Discovery() {
-        super("discovery");
+        super(LOG4J_CATEGORY);
     }
 
     /**
@@ -469,4 +471,7 @@ public class Discovery extends AbstractServiceDaemon {
         LOG.debug("Added {} as discovered", iface);
     }
 
+    public static String getLoggingCategory() {
+        return LOG4J_CATEGORY;
+    }
 }
