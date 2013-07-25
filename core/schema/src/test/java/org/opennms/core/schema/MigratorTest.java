302286de00bd88f3b47064b546574a262a5cc52f
Benjamin Reed <ranger@opennms.org>
2013-07-25 11:54:39 -0400

    more refactoring of the migration test

--- core/schema/src/test/java/org/opennms/core/schema/MigratorTest.java
+++ core/schema/src/test/java/org/opennms/core/schema/MigratorTest.java
@@ -28,9 +28,11 @@
 
 package org.opennms.core.schema;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -42,7 +44,6 @@ import java.util.Set;
 
 import javax.sql.DataSource;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,7 +56,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 
 @RunWith(OpenNMSJUnit4ClassRunner.class)
@@ -86,7 +86,7 @@ public class MigratorTest {
         assertFalse(changelogExists());
 
         Resource aResource = null;
-        for (final Resource resource : m_context.getResources("classpath*:/changelog.xml")) {
+        for (final Resource resource : getTestResources()) {
             if (resource.getURI().toString().contains("test-api.schema.a")) {
                 aResource = resource;
             }
@@ -94,7 +94,7 @@ public class MigratorTest {
 
         Set<String> tables = getTables();
         assertFalse("must not contain table 'schematest'", tables.contains("schematest"));
-        
+
         final Migration migration = new Migration();
         migration.setAdminUser(System.getProperty(TemporaryDatabase.ADMIN_USER_PROPERTY, TemporaryDatabase.DEFAULT_ADMIN_USER));
         migration.setAdminPassword(System.getProperty(TemporaryDatabase.ADMIN_PASSWORD_PROPERTY, TemporaryDatabase.DEFAULT_ADMIN_PASSWORD));
@@ -124,16 +124,7 @@ public class MigratorTest {
     @Test
     @JUnitTemporaryDatabase(createSchema=false)
     public void testMultipleChangelogs() throws Exception {
-        // Make sure there is no databasechangelog table
-        Connection connection = m_dataSource.getConnection();
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT id FROM databasechangelog");
-            statement.execute();
-            Assert.fail("databasechangelog exists");
-        } catch (SQLException e) {
-        } finally {
-            connection.close();
-        }
+        assertFalse(changelogExists());
 
         final Migration migration = new Migration();
         migration.setAdminUser(System.getProperty(TemporaryDatabase.ADMIN_USER_PROPERTY, TemporaryDatabase.DEFAULT_ADMIN_USER));
@@ -151,29 +142,30 @@ public class MigratorTest {
 
         // Add a resource accessor to the migration so that it will load multiple changelog.xml files
         // from the classpath
-        for (final Resource resource : m_context.getResources("classpath*:/changelog.xml")) {
+        for (final Resource resource : getTestResources()) {
+            if (!resource.getURI().toString().contains("test-api.schema")) continue;
             LogUtils.infof(this, "=== found resource: " + resource + " ===");
             migration.setAccessor(new ExistingResourceAccessor(resource));
             m.migrate(migration);
         }
 
-        connection = m_dataSource.getConnection();
-        PreparedStatement statement = connection.prepareStatement("SELECT id FROM databasechangelog");
-        assertTrue(statement.execute());
-        ResultSet rs = statement.getResultSet();
-        List<String> ids = new ArrayList<String>();
-        while (rs.next()) {
-            ids.add(rs.getString(1));
-        }
+        final List<ChangelogEntry> ids = getChangelogEntries();
 
         assertTrue(ids.size() > 0);
-        assertTrue(ids.contains("test-api.schema.a"));
-        assertTrue(ids.contains("test-api.schema.b"));
+        assertEquals("test-api.schema.a", ids.get(0).getId());
+        assertEquals("test-api.schema.b", ids.get(1).getId());
     }
 
     @Test
     @JUnitTemporaryDatabase(createSchema=false)
     public void testUpdateTwice() throws Exception {
+        assertFalse(changelogExists());
+
+        doMigration();
+        doMigration();
+    }
+
+    private void doMigration() throws MigrationException, IOException {
         final Migration migration = new Migration();
         migration.setAdminUser(System.getProperty(TemporaryDatabase.ADMIN_USER_PROPERTY, TemporaryDatabase.DEFAULT_ADMIN_USER));
         migration.setAdminPassword(System.getProperty(TemporaryDatabase.ADMIN_PASSWORD_PROPERTY, TemporaryDatabase.DEFAULT_ADMIN_PASSWORD));
@@ -183,8 +175,11 @@ public class MigratorTest {
 
         final Migrator m = new Migrator();
         m.setDataSource(m_dataSource);
-        m.migrate(migration);
-        m.migrate(migration);
+
+        for (final Resource resource : getTestResources()) {
+            migration.setAccessor(new ExistingResourceAccessor(resource));
+            m.migrate(migration);
+        }
     }
 
     protected boolean changelogExists() throws SQLException {
@@ -227,5 +222,82 @@ public class MigratorTest {
             connection.close();
         }
         return tables;
+    }
+
+    private List<ChangelogEntry> getChangelogEntries() throws SQLException {
+        final Connection connection = m_dataSource.getConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT id, md5sum FROM databasechangelog order by id");
+            assertTrue(statement.execute());
+            ResultSet rs = statement.getResultSet();
+            final List<ChangelogEntry> entries = new ArrayList<ChangelogEntry>();
+            while (rs.next()) {
+                entries.add(new ChangelogEntry(rs.getString(1), rs.getString(2)));
+            }
+            return entries;
+        } finally {
+            connection.close();
+        }
+    }
+
+    private static class ChangelogEntry {
+        private final String m_id;
+        private final String m_md5sum;
+
+        public ChangelogEntry(final String id, final String md5sum) {
+            m_id = id;
+            m_md5sum = md5sum;
+        }
+
+        public String getId() {
+            return m_id;
+        }
+
+        @SuppressWarnings("unused")
+        public String getMd5sum() {
+            return m_md5sum;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((m_id == null) ? 0 : m_id.hashCode());
+            result = prime * result + ((m_md5sum == null) ? 0 : m_md5sum.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(final Object obj) {
+            if (this == obj) return true;
+            if (obj == null) return false;
+            if (getClass() != obj.getClass()) return false;
+            final ChangelogEntry other = (ChangelogEntry) obj;
+            if (m_id == null) {
+                if (other.m_id != null) return false;
+            } else if (!m_id.equals(other.m_id)) {
+                return false;
+            }
+            if (m_md5sum == null) {
+                if (other.m_md5sum != null) return false;
+            } else if (!m_md5sum.equals(other.m_md5sum)) {
+                return false;
+            }
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return m_id + "=" + m_md5sum;
+        }
+    }
+
+    private List<Resource> getTestResources() throws IOException {
+        final List<Resource> resources = new ArrayList<Resource>();
+        for (final Resource resource : m_context.getResources("classpath*:/changelog.xml")) {
+            if (!resource.getURI().toString().contains("test-api.schema")) continue;
+            resources.add(resource);
+        }
+        return resources;
     }
 }
