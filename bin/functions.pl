dca4ec6251f023e7acfe81ad89a83c945e04f2ff
Benjamin Reed <ranger@opennms.org>
2013-08-20 15:50:54 -0400

    get the branch hash if it is detached

--- bin/functions.pl
+++ bin/functions.pl
@@ -183,7 +183,7 @@ info("PATH = " . $ENV{'PATH'});
 info("MVN = $MVN");
 info("MAVEN_OPTS = $MAVEN_OPTS"); 
 
-chomp(my $git_branch=`$GIT symbolic-ref HEAD`);
+chomp(my $git_branch=`$GIT symbolic-ref HEAD 2>/dev/null || $GIT rev-parse HEAD 2>/dev/null`);
 $git_branch =~ s,^refs/heads/,,;
 info("Git Branch = $git_branch");
 
