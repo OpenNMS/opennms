9d22e170a007ab6834d9eec1ea32d0c7a6dad607
Antonio <rssntn67@yahoo.it>
2013-08-25 11:05:48 +0200

    NMS-5582: Build Isis test
    The IsIs discovery is tested for
    network nms0001

--- opennms-services/src/test/java/org/opennms/netmgt/linkd/Nms0001Test.java
+++ opennms-services/src/test/java/org/opennms/netmgt/linkd/Nms0001Test.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2011-2012 The OpenNMS Group, Inc.
+ * Copyright (C) 2012 The OpenNMS Group, Inc.
  * OpenNMS(R) is Copyright (C) 1999-2012 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
@@ -31,6 +31,7 @@ package org.opennms.netmgt.linkd;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Properties;
 
@@ -50,7 +51,7 @@ import org.opennms.netmgt.config.linkd.Package;
 import org.opennms.netmgt.dao.api.DataLinkInterfaceDao;
 import org.opennms.netmgt.dao.api.NodeDao;
 import org.opennms.netmgt.dao.api.SnmpInterfaceDao;
-import org.opennms.netmgt.linkd.nb.Nms4930NetworkBuilder;
+import org.opennms.netmgt.linkd.nb.Nms0001NetworkBuilder;
 import org.opennms.netmgt.model.DataLinkInterface;
 import org.opennms.netmgt.model.OnmsNode;
 import org.opennms.test.JUnitConfigurationEnvironment;
@@ -73,7 +74,7 @@ import org.springframework.test.context.ContextConfiguration;
 })
 @JUnitConfigurationEnvironment(systemProperties="org.opennms.provisiond.enableDiscovery=false")
 @JUnitTemporaryDatabase
-public class Nms4930Test extends Nms4930NetworkBuilder implements InitializingBean {
+public class Nms0001Test extends Nms0001NetworkBuilder implements InitializingBean {
 
     @Autowired
     private Linkd m_linkd;
@@ -82,21 +83,20 @@ public class Nms4930Test extends Nms4930NetworkBuilder implements InitializingBe
 
     @Autowired
     private NodeDao m_nodeDao;
-
+    
     @Autowired
     private SnmpInterfaceDao m_snmpInterfaceDao;
 
     @Autowired
     private DataLinkInterfaceDao m_dataLinkInterfaceDao;
-
+        
     @Override
     public void afterPropertiesSet() throws Exception {
         BeanUtils.assertAutowiring(this);
     }
 
-	@Before
+    @Before
     public void setUp() throws Exception {
-        // MockLogAppender.setupLogging(true);
         Properties p = new Properties();
         p.setProperty("log4j.logger.org.hibernate.SQL", "WARN");
         p.setProperty("log4j.logger.org.hibernate.cfg", "WARN");
@@ -106,19 +106,16 @@ public class Nms4930Test extends Nms4930NetworkBuilder implements InitializingBe
 
         super.setNodeDao(m_nodeDao);
         super.setSnmpInterfaceDao(m_snmpInterfaceDao);
- 
-        buildNetwork4005();
-        
     }
 
-	@Before
-	public void setUpLinkdConfiguration() throws Exception {
-	    LinkdConfigFactory.init();
-	    final Resource config = new ClassPathResource("etc/linkd-configuration.xml");
-	    final LinkdConfigFactory factory = new LinkdConfigFactory(-1L, config.getInputStream());
-	    LinkdConfigFactory.setInstance(factory);
-	    m_linkdConfig = LinkdConfigFactory.getInstance();
-	}
+    @Before
+    public void setUpLinkdConfiguration() throws Exception {
+        LinkdConfigFactory.init();
+        final Resource config = new ClassPathResource("etc/linkd-configuration.xml");
+        final LinkdConfigFactory factory = new LinkdConfigFactory(-1L, config.getInputStream());
+        LinkdConfigFactory.setInstance(factory);
+        m_linkdConfig = LinkdConfigFactory.getInstance();
+    }
 
     @After
     public void tearDown() throws Exception {
@@ -127,53 +124,93 @@ public class Nms4930Test extends Nms4930NetworkBuilder implements InitializingBe
         }
         m_nodeDao.flush();
     }
-
-    /*
-     * The main fact is that this devices have only the Bridge MIb walk
-     * dlink_DES has STP disabled
-     * dlink_DGS has STP enabled but root is itself
-     * no way to find links....
-     * Also there is no At interface information
-     * c2007db90010 --> 10.1.1.2  ---nothing in the bridge forwarding table...
-     * no way to get links...
-     * 
-     */
+    
     @Test
     @JUnitSnmpAgents(value={
-            @JUnitSnmpAgent(host="10.1.1.2", port=161, resource="classpath:linkd/nms4930/dlink_DES-3026.properties"),
-            @JUnitSnmpAgent(host="10.1.2.2", port=161, resource="classpath:linkd/nms4930/dlink_DGS-3612G.properties")
+            @JUnitSnmpAgent(host = FROH_IP, port = 161, resource = "classpath:linkd/nms0001/" + FROH_NAME + "-"+FROH_IP + "-walk.txt"),
+            @JUnitSnmpAgent(host = OEDIPUS_IP, port = 161, resource = "classpath:linkd/nms0001/" + OEDIPUS_NAME + "-"+OEDIPUS_IP + "-walk.txt"),
+            @JUnitSnmpAgent(host = SIEGFRIE_IP, port = 161, resource = "classpath:linkd/nms0001/" + SIEGFRIE_NAME + "-"+SIEGFRIE_IP + "-walk.txt")
     })
-    public void testNms4930Network() throws Exception {
+    public void testNetwork17216Links() throws Exception {
+        
+        m_nodeDao.save(getFroh());
+        m_nodeDao.save(getOedipus());
+        m_nodeDao.save(getSiegFrie());
+        m_nodeDao.flush();
 
         Package example1 = m_linkdConfig.getPackage("example1");
-        example1.setUseLldpDiscovery(false);
-        example1.setUseOspfDiscovery(false);
-        example1.setUseCdpDiscovery(false);
+        example1.setUseBridgeDiscovery(false);
         example1.setUseIpRouteDiscovery(false);
-        example1.setUseBridgeDiscovery(true);
-        example1.setUseIsisDiscovery(false);
-
-        example1.setSaveRouteTable(false);
         example1.setEnableVlanDiscovery(false);
+        example1.setUseOspfDiscovery(false);
+        example1.setUseLldpDiscovery(false);
         
-    	final OnmsNode cisco1 = m_nodeDao.findByForeignId("linkd", "cisco1");
-        final OnmsNode cisco2 = m_nodeDao.findByForeignId("linkd", "cisco2");
+        example1.setSaveStpInterfaceTable(false);
+        example1.setSaveRouteTable(false);
+        example1.setSaveStpNodeTable(false);
 
-        assertTrue(m_linkd.scheduleNodeCollection(cisco1.getId()));
-        assertTrue(m_linkd.scheduleNodeCollection(cisco2.getId()));
+        final OnmsNode froh = m_nodeDao.findByForeignId("linkd", FROH_NAME);
+        final OnmsNode oedipus = m_nodeDao.findByForeignId("linkd", OEDIPUS_NAME);
+        final OnmsNode siegfrie = m_nodeDao.findByForeignId("linkd", SIEGFRIE_NAME);
+        
+        assertTrue(m_linkd.scheduleNodeCollection(froh.getId()));
+        assertTrue(m_linkd.scheduleNodeCollection(oedipus.getId()));
+        assertTrue(m_linkd.scheduleNodeCollection(siegfrie.getId()));
 
-        assertTrue(m_linkd.runSingleSnmpCollection(cisco1.getId()));
-        assertTrue(m_linkd.runSingleSnmpCollection(cisco2.getId()));
+        assertTrue(m_linkd.runSingleSnmpCollection(froh.getId()));
+        assertTrue(m_linkd.runSingleSnmpCollection(oedipus.getId()));
+        assertTrue(m_linkd.runSingleSnmpCollection(siegfrie.getId()));
+       
+        assertEquals(0,m_dataLinkInterfaceDao.countAll());
+        
+        final Collection<LinkableNode> nodes = m_linkd.getLinkableNodesOnPackage("example1");
 
+        assertEquals(3, nodes.size());
+        
+        for (LinkableNode node: nodes) {
+            assertEquals(2, node.getIsisInterfaces().size());
+            switch(node.getNodeId()) {
+                case 1: assertEquals(FROH_ISIS_SYS_ID, node.getIsisSysId());
+                break;
+                case 2: assertEquals(OEDIPUS_ISIS_SYS_ID, node.getIsisSysId());
+                break;
+                case 3: assertEquals(SIEGFRIE_ISIS_SYS_ID, node.getIsisSysId());
+                break;
+                default: assertEquals(-1, node.getNodeId());
+                break;
+            }
+        }        
+        
         assertTrue(m_linkd.runSingleLinkDiscovery("example1"));
 
-        final List<DataLinkInterface> ifaces = m_dataLinkInterfaceDao.findAll();
-        for (final DataLinkInterface link: ifaces) {
-            printLink(link);
+        assertEquals(3,m_dataLinkInterfaceDao.countAll());
+        final List<DataLinkInterface> datalinkinterfaces = m_dataLinkInterfaceDao.findAll();
+
+        int start=getStartPoint(datalinkinterfaces);
+
+        /*
+         * 
+         * These are the links among the following nodes discovered using 
+         * only the isis protocol
+         * froh:ae1.0(599):10.1.3.6/30       <-->    oedipus:ae1.0(578):10.1.3.5/30
+         * froh:ae2.0(600):10.1.3.2/30       <-->    siegfrie:ae2.0(552):10.1.3.1/30
+         * oedipus:ae0.0(575):10.1.0.10/30   <-->    siegfrie:ae0.0(533):10.1.0.9/30
+         * 
+         */
+        for (final DataLinkInterface datalinkinterface: datalinkinterfaces) {
+            
+            Integer linkid = datalinkinterface.getId();
+            if ( linkid == start) {
+                checkLink(froh, oedipus, 599, 578, datalinkinterface);
+            } else if (linkid == start+1 ) {
+                checkLink(froh, siegfrie, 600, 552, datalinkinterface);
+            } else if (linkid == start+2) {
+                checkLink(oedipus, siegfrie, 575, 533, datalinkinterface);
+            } else {
+                // error
+                checkLink(froh,froh,-1,-1,datalinkinterface);
+            } 
+            
         }
-        
-        // Note By AR: I've inspected the snmp file, only the bridge mib are there
-        //             and no link is found
-        assertEquals("we should have found no links", 0, ifaces.size());
     }
 }
