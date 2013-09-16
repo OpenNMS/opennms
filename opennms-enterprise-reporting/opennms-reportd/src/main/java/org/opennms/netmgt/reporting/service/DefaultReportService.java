9396ee0011dcec8148896b69ce239def3e39c76b
Jeff Gehlbach <jeffg@opennms.org>
2013-09-16 16:25:48 -0400

    Fixes NMS-6113 / Cron-triggered reports in Reportd do not work Stop wrapping the report parameters in an UnmodifiableMap, since JasperReports is later going to try to put() into what we return.

--- opennms-enterprise-reporting/opennms-reportd/src/main/java/org/opennms/netmgt/reporting/service/DefaultReportService.java
+++ opennms-enterprise-reporting/opennms-reportd/src/main/java/org/opennms/netmgt/reporting/service/DefaultReportService.java
@@ -250,7 +250,7 @@ public class DefaultReportService implements ReportService,InitializingBean {
         for(Parameter parm : parameters)
             parmMap.put(parm.getName(), parm.getValue());
         
-        return Collections.unmodifiableMap(parmMap);
+        return parmMap;
     }
 
 }
