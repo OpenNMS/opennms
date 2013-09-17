220e8b4118b66bd65453285157e3ee95704fea08
Benjamin Reed <ranger@opennms.org>
2013-09-17 11:50:32 -0400

    bump selenium version, use phantomjs if it is found
    Conflicts:
    	smoke-test/src/test/java/org/opennms/smoketest/OpenNMSSeleniumTestCase.java

--- smoke-test/src/test/java/org/opennms/smoketest/OpenNMSSeleniumTestCase.java
+++ smoke-test/src/test/java/org/opennms/smoketest/OpenNMSSeleniumTestCase.java
@@ -38,6 +38,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverBackedSelenium;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 import com.thoughtworks.selenium.SeleneseTestBase;
 import com.thoughtworks.selenium.SeleniumException;
@@ -69,7 +70,12 @@ public class OpenNMSSeleniumTestCase extends SeleneseTestBase {
 
         // otherwise, Firefox
         if (driver == null) {
-            driver = new FirefoxDriver();
+            final File phantomJS = new File("/usr/local/bin/phantomjs");
+            if (phantomJS.exists()) {
+                driver = new PhantomJSDriver();
+            } else {
+                driver = new FirefoxDriver();
+            }
         }
 
         String baseUrl = "http://localhost:8980/";
