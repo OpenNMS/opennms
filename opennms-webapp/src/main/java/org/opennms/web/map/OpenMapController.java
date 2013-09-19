616e39239de3a2e108e371ea194a2702dbee891a
Antonio <rssntn67@yahoo.it>
2013-09-19 19:59:36 +0200

    Adjusted the map height and width for kk algorithm

--- opennms-webapp/src/main/java/org/opennms/web/map/OpenMapController.java
+++ opennms-webapp/src/main/java/org/opennms/web/map/OpenMapController.java
@@ -180,6 +180,11 @@ public class OpenMapController extends MapsLoggingController {
 		                }
 		                
 		                for (VElement ve : map.getElements().values()) {
+		                    LOG.debug("---------Element {}---------", ve.getLabel());
+		                    LOG.debug("dbcoor: X={} Y={}",ve.getX(),ve.getY());
+                                    LOG.debug("kkcoor: X={} Y={}",layout.getX(ve),layout.getY(ve));
+                                    LOG.debug("kkcoor: X={} Y={}",(int)layout.getX(ve),(int)layout.getY(ve));
+                                    LOG.debug("");
                                     ve.setX((int)layout.getX(ve));
                                     ve.setY((int)layout.getY(ve));
                                }
@@ -207,13 +212,16 @@ public class OpenMapController extends MapsLoggingController {
        }
         
         protected Dimension selectLayoutSize(VMap g) {
-            int vertexCount = g.getElements().size();
+            /*
+             int vertexCount = g.getElements().size();
             
-             double height = 1.5*Math.sqrt(vertexCount)*ELBOW_ROOM;
+             double height = 1.5*Math.sqrt(vertexCount)*g.getHeight();
              double width = height*16/9;
              
-             return new Dimension((int)width, (int)height);
-             
+             return new Dimension((int)width,(int)height);
+             */
+            
+            return new Dimension(g.getWidth(),g.getHeight());
             
       }
 
