35b19d061e3bbc7ce58cdfdfea3a33a584a85718
Seth Leger <seth@opennms.org>
2014-02-05 14:26:12 -0500

    Updated Atomikos to latest version. Added unit test to make sure that connections are recycled properly in the connection pool. Changed default opennms-datasources.xml settings so that the pool is set to a constant size of 50 database connections.

--- core/db/src/test/java/org/opennms/core/db/ConnectionFactoryTest.java
+++ core/db/src/test/java/org/opennms/core/db/ConnectionFactoryTest.java
@@ -50,86 +50,145 @@ import org.opennms.test.DaoTestConfigBean;
  * @author <a href="mailto:david@opennms.org">David Hustace</a>
  */
 public class ConnectionFactoryTest extends TestCase {
-    public void testMarshalDataSourceFromConfig() throws Exception {
-        DaoTestConfigBean bean = new DaoTestConfigBean();
-        bean.afterPropertiesSet();
+	public void testMarshalDataSourceFromConfig() throws Exception {
+		DaoTestConfigBean bean = new DaoTestConfigBean();
+		bean.afterPropertiesSet();
 
-        AtomikosDataSourceFactory factory1 = null;
-        AtomikosDataSourceFactory factory2 = null;
-        
-        try {
-        	factory1 = makeFactory("opennms");
-        	factory2 = makeFactory("opennms2");
+		AtomikosDataSourceFactory factory1 = null;
+		AtomikosDataSourceFactory factory2 = null;
 
-        	Connection conn = null;
-        	Statement s = null;
-        	try {
-        		conn = factory2.getConnection();
-        		s = conn.createStatement();
-        		s.execute("select * from pg_proc");
-        	} finally {
-        		if (s != null) {
-        			s.close();
-        		}
-        		if (conn != null) {
-        			conn.close();
-        		}
-        	}
-        } finally {
-        	Throwable t1 = null;
-        	Throwable t2 = null;
-        	
-    		if (factory1 != null) {
-    			try {
-    				factory1.close();
-    				factory1 = null;
-    			} catch (Throwable e1) {
-    				t1 = e1;
-    			}
-    		}
+		try {
+			factory1 = makeFactory("opennms");
+			factory1.setUniqueResourceName("opennms");
+			factory2 = makeFactory("opennms2");
+			factory2.setUniqueResourceName("opennms2");
 
-    		if (factory2 != null) {
-    			try {
-    				factory2.close();
-    				factory2 = null;
-    			} catch (Throwable e2) {
-    				t2 = e2;
-    			}
-    		}
-    		
-    		if (t1 != null || t2 != null) {
-    			StringBuffer message = new StringBuffer();
-    			message.append("Could not successfully close both C3P0 factories.  Future tests might fail.");
-    			
-    			Throwable choice;
-    			if (t1 != null) {
-    				message.append("  First factory failed with: " + t1.getMessage() + "; see stack back trace.");
-    				choice = t1;
-    				
-    				if (t2 != null) {
-    					System.err.println("  Both factories failed to close.  See stderr for second stack back trace.");
-    					t2.printStackTrace(System.err);
-    				}
-    			} else {
-    				choice = t2;
-    			}
-    			AssertionError e = new AssertionError(message.toString());
-    			e.initCause(choice);
-    			throw e;
-    		}
-        }
-    }
+			Connection conn = null;
+			Statement s = null;
+			try {
+				conn = factory2.getConnection();
+				s = conn.createStatement();
+				s.execute("select * from pg_proc");
+			} finally {
+				if (s != null) {
+					s.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			}
+		} finally {
+			Throwable t1 = null;
+			Throwable t2 = null;
 
-    private AtomikosDataSourceFactory makeFactory(String database) throws MarshalException, ValidationException, PropertyVetoException, SQLException, IOException, ClassNotFoundException {
-        InputStream stream1 = new ByteArrayInputStream(ConfigurationTestUtils.getConfigForResourceWithReplacements(this, ConfigFileConstants.getFileName(ConfigFileConstants.OPENNMS_DATASOURCE_CONFIG_FILE_NAME)).getBytes());
-        DataSourceFactory.setDataSourceConfigurationFactory(new DataSourceConfigurationFactory(stream1));
-        InputStream stream2 = new ByteArrayInputStream(ConfigurationTestUtils.getConfigForResourceWithReplacements(this, ConfigFileConstants.getFileName(ConfigFileConstants.OPENNMS_DATASOURCE_CONFIG_FILE_NAME)).getBytes());
-        XADataSourceFactory.setDataSourceConfigurationFactory(new DataSourceConfigurationFactory(stream2));
-        try {
-            return new AtomikosDataSourceFactory();
-        } finally {
-            IOUtils.closeQuietly(stream1);
-            IOUtils.closeQuietly(stream2);
-        }
-    }
+			if (factory1 != null) {
+				try {
+					factory1.close();
+					factory1 = null;
+				} catch (Throwable e1) {
+					t1 = e1;
+				}
+			}
+
+			if (factory2 != null) {
+				try {
+					factory2.close();
+					factory2 = null;
+				} catch (Throwable e2) {
+					t2 = e2;
+				}
+			}
+
+			if (t1 != null || t2 != null) {
+				StringBuffer message = new StringBuffer();
+				message.append("Could not successfully close both C3P0 factories.  Future tests might fail.");
+
+				Throwable choice;
+				if (t1 != null) {
+					message.append("  First factory failed with: " + t1.getMessage() + "; see stack back trace.");
+					choice = t1;
+
+					if (t2 != null) {
+						System.err.println("  Both factories failed to close.  See stderr for second stack back trace.");
+						t2.printStackTrace(System.err);
+					}
+				} else {
+					choice = t2;
+				}
+				AssertionError e = new AssertionError(message.toString());
+				e.initCause(choice);
+				throw e;
+			}
+		}
+	}
+
+	public void testPoolWithSqlExceptions() throws Exception {
+		DaoTestConfigBean bean = new DaoTestConfigBean();
+		bean.afterPropertiesSet();
+
+		AtomikosDataSourceFactory factory = makeFactory("opennms");
+		factory.afterPropertiesSet();
+		// Verify the default values
+		assertEquals(30, factory.poolAvailableSize());
+		assertEquals(30, factory.poolTotalSize());
+		// Close the factory so that we can reregister another factory with the same name
+		factory.close();
+
+		final AtomikosDataSourceFactory factory2 = makeFactory("opennms");
+		factory2.setPoolSize(50);
+		factory2.afterPropertiesSet();
+		// Verify the altered values
+		assertEquals(50, factory2.poolAvailableSize());
+		assertEquals(50, factory2.poolTotalSize());
+
+		// Spawn a bunch of threads that generate continuous SQLExceptions
+		for (int i = 0; i < 2000; i++) {
+			new Thread() {
+				public void run() {
+					Connection conn = null;
+					try {
+						assertEquals(50, factory2.poolTotalSize());
+
+						conn = factory2.getConnection();
+
+						// Make sure that the total size of the pool stays at 50
+						assertEquals(50, factory2.poolTotalSize());
+						assertTrue(factory2.poolAvailableSize() > 0);
+						// Fetching the current connection will push the available connections below 50
+						assertTrue(factory2.poolAvailableSize() < 50);
+
+						Statement stmt = conn.createStatement();
+						stmt.execute("SELECT * FROM doesnt_exist_in_the_database");
+					} catch (SQLException e) {
+						e.printStackTrace();
+					} finally {
+						if (conn != null) {
+							try {
+								conn.close();
+							} catch (SQLException e) {
+								fail("Exception thrown when trying to close connection");
+							}
+						}
+					}
+				}
+			}.start();
+
+			// Only sleep for a bit after spawning 10 threads so that we force some
+			// contention.
+			if (i % 10 == 0) Thread.sleep(200);
+		}
+	}
+
+	private AtomikosDataSourceFactory makeFactory(String database) throws MarshalException, ValidationException, PropertyVetoException, SQLException, IOException, ClassNotFoundException {
+		InputStream stream1 = new ByteArrayInputStream(ConfigurationTestUtils.getConfigForResourceWithReplacements(this, ConfigFileConstants.getFileName(ConfigFileConstants.OPENNMS_DATASOURCE_CONFIG_FILE_NAME)).getBytes());
+		DataSourceFactory.setDataSourceConfigurationFactory(new DataSourceConfigurationFactory(stream1));
+		InputStream stream2 = new ByteArrayInputStream(ConfigurationTestUtils.getConfigForResourceWithReplacements(this, ConfigFileConstants.getFileName(ConfigFileConstants.OPENNMS_DATASOURCE_CONFIG_FILE_NAME)).getBytes());
+		XADataSourceFactory.setDataSourceConfigurationFactory(new DataSourceConfigurationFactory(stream2));
+		try {
+			return new AtomikosDataSourceFactory();
+		} finally {
+			IOUtils.closeQuietly(stream1);
+			IOUtils.closeQuietly(stream2);
+		}
+	}
 }
