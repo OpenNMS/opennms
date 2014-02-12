b4cfdb6b86de227ea70f56af31debdb41369a0ad
Seth Leger <seth@opennms.org>
2014-02-12 17:24:13 -0500

    Turned off readOnly transaction semantics on DefaultCollectionAgentService because PostgreSQL doesn't support this.

--- opennms-services/src/main/java/org/opennms/netmgt/collectd/DefaultCollectionAgentService.java
+++ opennms-services/src/main/java/org/opennms/netmgt/collectd/DefaultCollectionAgentService.java
@@ -55,6 +55,9 @@ import org.springframework.transaction.interceptor.TransactionProxyFactoryBean;
  * @author ranger
  * @version $Id: $
  */
+// Eventually, we should be constructing these instances in the context and using
+// annotation-based transaction processing.
+//@Transactional(propagation=Propagation.REQUIRED)
 public class DefaultCollectionAgentService implements CollectionAgentService {
     
     private static final Logger LOG = LoggerFactory.getLogger(DefaultCollectionAgentService.class);
@@ -75,7 +78,7 @@ public class DefaultCollectionAgentService implements CollectionAgentService {
         bean.setTarget(agent);
         
         Properties props = new Properties();
-        props.put("*", "PROPAGATION_REQUIRED,readOnly");
+        props.put("*", "PROPAGATION_REQUIRED");
         
         bean.setTransactionAttributes(props);
         
