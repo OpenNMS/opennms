ea9ddad6ae2c3c7bed93ee01237cd0ec820ec29d
Benjamin Reed <ranger@opennms.org>
2014-02-19 12:03:31 -0500

    fix page sequence tests failing

--- opennms-config-jaxb/src/main/java/org/opennms/netmgt/config/pagesequence/Parameter.java
+++ opennms-config-jaxb/src/main/java/org/opennms/netmgt/config/pagesequence/Parameter.java
@@ -7,41 +7,26 @@
 
 package org.opennms.netmgt.config.pagesequence;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.io.Serializable;
-import java.io.Writer;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
-
-import org.exolab.castor.xml.MarshalException;
-import org.exolab.castor.xml.Marshaller;
-import org.exolab.castor.xml.Unmarshaller;
-import org.exolab.castor.xml.ValidationException;
-import org.exolab.castor.xml.Validator;
-import org.xml.sax.ContentHandler;
+import javax.xml.bind.annotation.XmlType;
 
 /**
  * Currently only used for HTTP form parameters.
  */
-
 @XmlRootElement(name="parameter")
-@XmlAccessorType(XmlAccessType.FIELD)
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlType(propOrder={"m_key","m_value"})
 public class Parameter implements Serializable {
-    private static final long serialVersionUID = 4430221176071592103L;
+    private static final long serialVersionUID = 5655167778463737674L;
 
-    /**
-     * Field m_key.
-     */
     @XmlAttribute(name="key")
     private String m_key;
 
-    /**
-     * Field m_value.
-     */
     @XmlAttribute(name="value")
     private String m_value;
 
@@ -49,168 +34,63 @@ public class Parameter implements Serializable {
         super();
     }
 
-
-    /**
-     * Overrides the Object.equals method.
-     * 
-     * @param obj
-     * @return true if the objects are equal.
-     */
-    @Override()
-    public boolean equals(final Object obj) {
-        if ( this == obj ) return true;
-        
-        if (obj instanceof Parameter) {
-            final Parameter temp = (Parameter)obj;
-            if (m_key != null) {
-                if (temp.m_key == null) {
-                    return false;
-                } else if (!(m_key.equals(temp.m_key))) {
-                    return false;
-                }
-            } else if (temp.m_key != null) {
-                return false;
-            }
-            if (m_value != null) {
-                if (temp.m_value == null) {
-                    return false;
-                } else if (!(m_value.equals(temp.m_value))) {
-                    return false;
-                }
-            } else if (temp.m_value != null) {
-                return false;
-            }
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Returns the value of field 'key'.
-     * 
-     * @return the value of field 'Key'.
-     */
     public String getKey() {
         return m_key;
     }
 
-    /**
-     * Returns the value of field 'value'.
-     * 
-     * @return the value of field 'Value'.
-     */
     public String getValue() {
         return m_value;
     }
 
-    /**
-     * Overrides the Object.hashCode method.
-     * <p>
-     * The following steps came from <b>Effective Java Programming
-     * Language Guide</b> by Joshua Bloch, Chapter 3
-     * 
-     * @return a hash code value for the object.
-     */
+    public void setKey(final String key) {
+        m_key = key == null? null : key.intern();
+    }
+
+    public void setValue(final String value) {
+        m_value = value == null? null : value.intern();
+    }
+
+    @Override
     public int hashCode() {
-        int result = 17;
-        
-        if (m_key != null) {
-           result = 37 * result + m_key.hashCode();
-        }
-        if (m_value != null) {
-           result = 37 * result + m_value.hashCode();
-        }
-        
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((m_key == null) ? 0 : m_key.hashCode());
+        result = prime * result + ((m_value == null) ? 0 : m_value.hashCode());
         return result;
     }
 
-    /**
-     * Method isValid.
-     * 
-     * @return true if this object is valid according to the schema
-     */
-    public boolean isValid() {
-        try {
-            validate();
-        } catch (final ValidationException vex) {
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Parameter)) {
+            return false;
+        }
+        final Parameter other = (Parameter) obj;
+        if (m_key == null) {
+            if (other.m_key != null) {
+                return false;
+            }
+        } else if (!m_key.equals(other.m_key)) {
+            return false;
+        }
+        if (m_value == null) {
+            if (other.m_value != null) {
+                return false;
+            }
+        } else if (!m_value.equals(other.m_value)) {
             return false;
         }
         return true;
     }
 
-    /**
-     * 
-     * 
-     * @param out
-     * @throws MarshalException if object is
-     * null or if any SAXException is thrown during marshaling
-     * @throws ValidationException if this
-     * object is an invalid instance according to the schema
-     */
-    public void marshal(final Writer out) throws MarshalException, ValidationException {
-        Marshaller.marshal(this, out);
-    }
-
-    /**
-     * 
-     * 
-     * @param handler
-     * @throws IOException if an IOException occurs during
-     * marshaling
-     * @throws ValidationException if this
-     * object is an invalid instance according to the schema
-     * @throws MarshalException if object is
-     * null or if any SAXException is thrown during marshaling
-     */
-    public void marshal(final ContentHandler handler) throws IOException, MarshalException, ValidationException {
-        Marshaller.marshal(this, handler);
-    }
-
-    /**
-     * Sets the value of field 'key'.
-     * 
-     * @param key the value of field 'key'.
-     */
-    public void setKey(final String key) {
-        m_key = key;
-    }
-
-    /**
-     * Sets the value of field 'value'.
-     * 
-     * @param value the value of field 'value'.
-     */
-    public void setValue(final String value) {
-        m_value = value;
-    }
-
-    /**
-     * Method unmarshal.
-     * 
-     * @param reader
-     * @throws MarshalException if object is
-     * null or if any SAXException is thrown during marshaling
-     * @throws ValidationException if this
-     * object is an invalid instance according to the schema
-     * @return the unmarshaled
-     * Parameter
-     */
-    public static Parameter unmarshal(final Reader reader) throws MarshalException, ValidationException {
-        return (Parameter) Unmarshaller.unmarshal(Parameter.class, reader);
-    }
-
-    /**
-     * 
-     * 
-     * @throws ValidationException if this
-     * object is an invalid instance according to the schema
-     */
-    public void validate() throws ValidationException {
-        new Validator().validate(this);
-    }
-
     @Override
     public String toString() {
-        return "Parameter[key=" + m_key + ",value=" + m_value + "]";
+        return "Parameter [key=" + m_key + ", value=" + m_value + "]";
     }
+
 }
--- opennms-config-jaxb/src/main/java/org/opennms/netmgt/config/pagesequence/SessionVariable.java
+++ opennms-config-jaxb/src/main/java/org/opennms/netmgt/config/pagesequence/SessionVariable.java
@@ -7,230 +7,93 @@
 
 package org.opennms.netmgt.config.pagesequence;
 
-  //---------------------------------/
- //- Imported classes and packages -/
-//---------------------------------/
-
-import java.io.IOException;
-import java.io.Reader;
 import java.io.Serializable;
-import java.io.Writer;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
-
-import org.exolab.castor.xml.MarshalException;
-import org.exolab.castor.xml.Marshaller;
-import org.exolab.castor.xml.Unmarshaller;
-import org.exolab.castor.xml.ValidationException;
-import org.exolab.castor.xml.Validator;
+import javax.xml.bind.annotation.XmlType;
 
 /**
- * Assign the value of a regex match group to a
- *  session variable with a user-defined name. The
- *  match group is identified by number and must
- *  be zero or greater.
+ * Assign the value of a regex match group to a session variable with a
+ * user-defined name. The match group is identified by number and must be zero
+ * or greater.
  */
 
 @XmlRootElement(name="session-variable")
-@XmlAccessorType(XmlAccessType.FIELD)
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlType(propOrder={"m_matchGroup", "m_name"})
 public class SessionVariable implements Serializable {
-    private static final long serialVersionUID = 5173622778411856405L;
+    private static final long serialVersionUID = 440115812786476754L;
 
-    /**
-     * Field m_matchGroup.
-     */
+    @XmlAttribute(name="match-group")
     private Integer m_matchGroup;
 
-    /**
-     * Field m_name.
-     */
+    @XmlAttribute(name="name")
     private String m_name;
 
     public SessionVariable() {
         super();
     }
 
-    public void deleteMatchGroup() {
-        m_matchGroup = null;
-    }
-
-    /**
-     * Overrides the Object.equals method.
-     * 
-     * @param obj
-     * @return true if the objects are equal.
-     */
-    @Override()
-    public boolean equals(
-            final Object obj) {
-        if ( this == obj )
-            return true;
-        
-        if (obj instanceof SessionVariable) {
-        
-            SessionVariable temp = (SessionVariable)obj;
-            if (m_matchGroup != null) {
-                if (temp.m_matchGroup == null) {
-                    return false;
-                } else if (!(m_matchGroup.equals(temp.m_matchGroup))) {
-                    return false;
-                }
-            } else if (temp.m_matchGroup != null) {
-                return false;
-            }
-            if (m_name != null) {
-                if (temp.m_name == null) {
-                    return false;
-                } else if (!(m_name.equals(temp.m_name))) {
-                    return false;
-                }
-            } else if (temp.m_name != null) {
-                return false;
-            }
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Returns the value of field 'matchGroup'.
-     * 
-     * @return the value of field 'MatchGroup'.
-     */
     public Integer getMatchGroup() {
         return m_matchGroup == null? 0 : m_matchGroup;
     }
 
-    /**
-     * Returns the value of field 'name'.
-     * 
-     * @return the value of field 'Name'.
-     */
     public String getName() {
         return m_name;
     }
 
-    /**
-     * Method hasMatchGroup.
-     * 
-     * @return true if at least one MatchGroup has been added
-     */
-    public boolean hasMatchGroup() {
-        return m_matchGroup != null;
+    public void setMatchGroup(final Integer matchGroup) {
+        m_matchGroup = matchGroup;
     }
 
-    /**
-     * Overrides the Object.hashCode method.
-     * <p>
-     * The following steps came from <b>Effective Java Programming
-     * Language Guide</b> by Joshua Bloch, Chapter 3
-     * 
-     * @return a hash code value for the object.
-     */
-    public int hashCode(
-    ) {
-        int result = 17;
-        
-        if (m_matchGroup != null) {
-            result = 37 * result + m_matchGroup.hashCode();
-         }
-        if (m_name != null) {
-           result = 37 * result + m_name.hashCode();
-        }
-        
+    public void setName(final String name) {
+        m_name = name == null? null : name.intern();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((m_matchGroup == null) ? 0 : m_matchGroup.hashCode());
+        result = prime * result + ((m_name == null) ? 0 : m_name.hashCode());
         return result;
     }
 
-    /**
-     * Method isValid.
-     * 
-     * @return true if this object is valid according to the schema
-     */
-    public boolean isValid(
-    ) {
-        try {
-            validate();
-        } catch (ValidationException vex) {
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof SessionVariable)) {
+            return false;
+        }
+        final SessionVariable other = (SessionVariable) obj;
+        if (m_matchGroup == null) {
+            if (other.m_matchGroup != null) {
+                return false;
+            }
+        } else if (!m_matchGroup.equals(other.m_matchGroup)) {
+            return false;
+        }
+        if (m_name == null) {
+            if (other.m_name != null) {
+                return false;
+            }
+        } else if (!m_name.equals(other.m_name)) {
             return false;
         }
         return true;
     }
 
-    /**
-     * 
-     * 
-     * @param out
-     * @throws MarshalException if object is
-     * null or if any SAXException is thrown during marshaling
-     * @throws ValidationException if this
-     * object is an invalid instance according to the schema
-     */
-    public void marshal(
-            final Writer out)
-    throws MarshalException, ValidationException {
-        Marshaller.marshal(this, out);
-    }
-
-    /**
-     * 
-     * 
-     * @param handler
-     * @throws IOException if an IOException occurs during
-     * marshaling
-     * @throws ValidationException if this
-     * object is an invalid instance according to the schema
-     * @throws MarshalException if object is
-     * null or if any SAXException is thrown during marshaling
-     */
-    public void marshal(
-            final org.xml.sax.ContentHandler handler)
-    throws IOException, MarshalException, ValidationException {
-        Marshaller.marshal(this, handler);
-    }
-
-    /**
-     * Sets the value of field 'matchGroup'.
-     * 
-     * @param matchGroup the value of field 'matchGroup'.
-     */
-    public void setMatchGroup(final Integer matchGroup) {
-        m_matchGroup = matchGroup;
-    }
-
-    /**
-     * Sets the value of field 'name'.
-     * 
-     * @param name the value of field 'name'.
-     */
-    public void setName(final String name) {
-        m_name = name;
-    }
-
-    /**
-     * Method unmarshal.
-     * 
-     * @param reader
-     * @throws MarshalException if object is
-     * null or if any SAXException is thrown during marshaling
-     * @throws ValidationException if this
-     * object is an invalid instance according to the schema
-     * @return the unmarshaled
-     * SessionVariable
-     */
-    public static SessionVariable unmarshal(final Reader reader) throws MarshalException, ValidationException {
-        return (SessionVariable) Unmarshaller.unmarshal(SessionVariable.class, reader);
-    }
-
-    /**
-     * 
-     * 
-     * @throws ValidationException if this
-     * object is an invalid instance according to the schema
-     */
-    public void validate() throws ValidationException {
-        new Validator().validate(this);
+    @Override
+    public String toString() {
+        return "SessionVariable [matchGroup=" + m_matchGroup + ", name=" + m_name + "]";
     }
 
 }
