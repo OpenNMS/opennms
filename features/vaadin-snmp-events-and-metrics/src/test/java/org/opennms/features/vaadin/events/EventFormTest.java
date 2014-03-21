872e86fb3ed15b454c7fdb50ed865bb0995997df
Alejandro Galue <agalue@opennms.org>
2014-03-21 15:40:29 -0400

    Fixing the junit test for EventForm

--- features/vaadin-snmp-events-and-metrics/src/test/java/org/opennms/features/vaadin/events/EventFormTest.java
+++ features/vaadin-snmp-events-and-metrics/src/test/java/org/opennms/features/vaadin/events/EventFormTest.java
@@ -74,7 +74,7 @@ public class EventFormTest {
         Assert.assertTrue(uei instanceof TextField);
         Assert.assertEquals("uei.opennms.org/newEvent", uei.getValue());
 
-        Field<?> logMsgDest = group.getField("logMsgDest");
+        Field<?> logMsgDest = group.getField("logmsg.dest");
         Assert.assertNotNull(logMsgDest);
         Assert.assertTrue(logMsgDest instanceof ComboBox);
         Assert.assertEquals("logndisplay", logMsgDest.getValue());
@@ -84,7 +84,7 @@ public class EventFormTest {
         Assert.assertNotNull(event);
 
         form.setEvent(event);
-        logMsgDest = group.getField("logMsgDest");
+        logMsgDest = group.getField("logmsg.dest");
         Assert.assertNotNull(logMsgDest);
         Assert.assertTrue(logMsgDest instanceof ComboBox);
         Assert.assertEquals(event.getLogmsg().getDest(), logMsgDest.getValue());
