7a6415fe59ecfba6aaa559219a91aeb40b4c4109
Ryan Lambeth <mrlambet@ncsu.edu>
2014-05-28 12:24:12 -0400

    Update commonConfigs, tests pass maven clean install

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/ServiceTypeDaoTest.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/ServiceTypeDaoTest.java
@@ -45,11 +45,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(OpenNMSJUnit4ClassRunner.class)
 @ContextConfiguration(locations={
-        "classpath:/META-INF/opennms/applicationContext-soa.xml",
+		"classpath:/META-INF/opennms/applicationContext-soa.xml",
         "classpath:/META-INF/opennms/applicationContext-dao.xml",
         "classpath*:/META-INF/opennms/component-dao.xml",
-        "classpath:/META-INF/opennms/applicationContext-minimal-conf.xml",
         "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml"
+        
 })
 @JUnitConfigurationEnvironment
 @JUnitTemporaryDatabase(reuseDatabase=false)
