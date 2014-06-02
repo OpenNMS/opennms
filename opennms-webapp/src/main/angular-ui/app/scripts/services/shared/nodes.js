2c8a6996460b99da2d6de0d82e298246477ccecb
Matt Raykowski <matt.raykowski@gmail.com>
2014-06-02 18:06:09 -0500

    Changed 'node factories' to NodeService, updated tests, using promises so added helper.

--- opennms-webapp/src/main/angular-ui/app/scripts/services/shared/nodes.js
+++ opennms-webapp/src/main/angular-ui/app/scripts/services/shared/nodes.js
@@ -1,35 +1,76 @@
 (function() {
   'use strict';
 
-  angular.module('opennms.services.shared.nodes', [])
-    .factory('nodeFactory', function($http) {
-      var nodes = [
-        {'id': 1, 'label': 'node8', 'foreignSource': 'bigreq', 'foreignId': 'node8'},
-        {'id': 2, 'label': 'node3'},
-        {'id': 3, 'label': 'node7'},
-        {'id': 4, 'label': 'node5'},
-        {'id': 5, 'label': 'node6'},
-        {'id': 6, 'label': 'node4'},
-        {'id': 7, 'label': 'node2'},
-        {'id': 8, 'label': 'node1'},
-        {'id': 9, 'label': 'node10'},
-        {'id': 10, 'label': 'node9'},
-        {'id': 11, 'label': 'localhost'},
-        {'id': 12, 'label': 'Test Node'},
-        {'id': 13, 'label': 'roskens-fedora'}
-      ];
-      var factory = {};
-      factory.getNodes = function() {
-        return nodes;
+  angular.module('opennms.services.shared.nodes', [
+    'opennms.services.shared.config'
+  ])
+
+    .factory('NodeService', ['$q', '$log', '$http', 'ConfigService', function($q, $log, $http, config) {
+      /* global X2JS: true */
+      var x2js = new X2JS();
+
+      var getNode = function(id) {
+        var nodeUrl = config.getRoot() + '/rest/nodes/'+ id;
+        $log.debug('getNode: GET ' + nodeUrl);
+
+        var deferred = $q.defer();
+        $http({
+          'method': 'GET',
+          'url': nodeUrl,
+          'headers': {
+            'Accept': 'application/xml'
+          }
+        }).success(function(data, status, headers, config) {
+          var results = x2js.xml_str2json(data);
+          var nodes = [];
+          if (results && results.nodes && results.nodes.node) {
+            nodes = results.nodes.node;
+          }
+          deferred.resolve(nodes);
+        }).error(function(data, status, headers, config) {
+          $log.error('GET ' + nodeUrl + ' failed:', data, status);
+          deferred.reject(status);
+        });
+        return deferred.promise;
       };
-      return factory;
-    })
-    .factory('nodeDetailFactory', function($http) {
-      var node = {'id': 1, 'label': 'node8', 'foreignSource': 'bigreq', 'foreignId': 'node8'};
-      var factory = {};
-      factory.getNode = function(id) {
-        return node;
+
+      var getNodes = function(offset, limit) {
+        if (limit === undefined) {
+          limit = 50;
+        }
+        if (offset === undefined) {
+          offset = 0;
+        }
+
+        var nodesUrl = config.getRoot() + '/rest/nodes?limit=' + limit + '&offset=' + offset;
+        $log.debug('getNodes: GET ' + nodesUrl);
+
+        var deferred = $q.defer();
+        $http({
+          'method': 'GET',
+          'url': nodesUrl,
+          'headers': {
+            'Accept': 'application/xml'
+          }
+        }).success(function(data, status, headers, config) {
+          var results = x2js.xml_str2json(data);
+          var nodes = [];
+          if (results && results.nodes && results.nodes.node) {
+            nodes = results.nodes.node;
+          }
+          deferred.resolve(nodes);
+        }).error(function(data, status, headers, config) {
+          $log.error('GET ' + nodesUrl + ' failed:', data, status);
+          deferred.reject(status);
+        });
+        return deferred.promise;
       };
-      return factory;
-    });
+
+      return {
+        'list': getNodes,
+        'get': getNode
+      };
+    }])
+
+  ;
 }());
