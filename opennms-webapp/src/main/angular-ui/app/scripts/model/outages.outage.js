cfc92c7a3ea8ba741a4f95f6cbd648d82a88e186
Ronald Roskens <ronald.roskens@gmail.com>
2014-06-03 16:33:21 -0500

    outage pages

--- opennms-webapp/src/main/angular-ui/app/scripts/model/outages.outage.js
+++ opennms-webapp/src/main/angular-ui/app/scripts/model/outages.outage.js
@@ -9,7 +9,9 @@ function Outage(outage) {
   self.nodeId = Number(outage['nodeId']);
   self.ipAddress = outage['ipAddress'];
   self.ifLostService = moment(outage['_ifLostService']);
-  self.ifRegainedService = moment(outage['_ifRegainedService']);
+  if (outage['_ifRegainedService']) {
+    self.ifRegainedService = moment(outage['_ifRegainedService']);
+  }
   self.monitoredService = new MonitoredService(outage['monitoredService']);
   self.serviceLostEvent = new Event(outage['serviceLostEvent']);
   //self.node = new Node(self.nodeId);
