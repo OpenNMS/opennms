5317f595be345261ac988d01cac75aaa2b28b13b
Ronald Roskens <ronald.roskens@gmail.com>
2014-06-05 10:55:21 -0500

    updates to the KSC report interface: export more ksc report info. (pt2)

--- features/vaadin-dashlets/dashlet-ksc/src/main/java/org/opennms/features/vaadin/dashboard/dashlets/KscDashlet.java
+++ features/vaadin-dashlets/dashlet-ksc/src/main/java/org/opennms/features/vaadin/dashboard/dashlets/KscDashlet.java
@@ -110,13 +110,13 @@ public class KscDashlet extends AbstractDashlet {
 
                     KSC_PerformanceReportFactory kscPerformanceReportFactory = KSC_PerformanceReportFactory.getInstance();
 
-                    Map<Integer, String> reportsMap = kscPerformanceReportFactory.getReportList();
+                    Map<Integer, Report> reportsMap = kscPerformanceReportFactory.getReportList();
 
                     int kscReportId = -1;
 
-                    for (Map.Entry<Integer, String> entry : reportsMap.entrySet()) {
+                    for (Map.Entry<Integer, Report> entry : reportsMap.entrySet()) {
 
-                        if (kscReportName.equals(entry.getValue())) {
+                        if (kscReportName.equals(entry.getValue().getTitle())) {
                             kscReportId = entry.getKey();
                             break;
                         }
@@ -293,13 +293,13 @@ public class KscDashlet extends AbstractDashlet {
 
                     KSC_PerformanceReportFactory kscPerformanceReportFactory = KSC_PerformanceReportFactory.getInstance();
 
-                    Map<Integer, String> reportsMap = kscPerformanceReportFactory.getReportList();
+                    Map<Integer, Report> reportsMap = kscPerformanceReportFactory.getReportList();
 
                     int kscReportId = -1;
 
-                    for (Map.Entry<Integer, String> entry : reportsMap.entrySet()) {
+                    for (Map.Entry<Integer, Report> entry : reportsMap.entrySet()) {
 
-                        if (kscReportName.equals(entry.getValue())) {
+                        if (kscReportName.equals(entry.getValue().getTitle())) {
                             kscReportId = entry.getKey();
                             break;
                         }
