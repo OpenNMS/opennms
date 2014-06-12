cb1e4fe3f2731fc2840e9575c5a0d8f4c7768c6b
Benjamin Reed <ranger@opennms.org>
2014-06-12 16:47:45 -0400

    initial mockup of the minion UI

---
+++ opennms-webapp/src/main/webapp/minion/minion.js
@@ -0,0 +1,35 @@
+(function() {
+	'use strict';
+
+	String.prototype.endsWith = function(suffix) {
+		return this.indexOf(suffix, this.length - suffix.length) !== -1;
+	};
+
+	angular.module('minion', [
+		'ng'
+	])
+
+	.controller('MinionListCtrl', ['$scope', '$log', function($scope, $log) {
+		$log.debug('MinionListCtrl Initialized.');
+		$scope.minions = [
+			{ uuid: '0f45fda0-b05e-4ae6-8dc9-981d5307aac1', location: 'Pittsboro' },
+			{ uuid: '4125bb00-7084-47a2-a7f3-95309fd578dc', location: 'San Francisco' }
+		];
+	}])
+
+	.run(['$rootScope', '$log', function($rootScope, $log) {
+		$log.debug('Finished initializing Minion.');
+		$rootScope.base = document.baseURI;
+		if (!$rootScope.base.endsWith('/')) {
+			$rootScope.base += '/';
+		}
+		$rootScope.base += 'minion';
+	}])
+
+	;
+
+	angular.element(document).ready(function() {
+		console.log('Bootstrapping minion UI.');
+		angular.bootstrap(document, ['minion']);
+	});
+}());
