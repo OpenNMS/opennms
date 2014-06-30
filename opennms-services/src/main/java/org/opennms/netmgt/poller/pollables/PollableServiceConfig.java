6cc7f4f80cf376c3e2315c9aa58612996bc2ac2a
Alejandro Galue <agalue@opennms.org>
2014-06-30 11:08:36 -0400

    Fix for Bug NMS-6603
    Pollerd stops working if a service is down and the downtime model is not
    correct.
    
    Now PollableServiceConfig.getInterval() will return -1 instead of
    throwing a RuntimeException when the downtime model is wrong, and it
    will log an error to poller.log

--- opennms-services/src/main/java/org/opennms/netmgt/poller/pollables/PollableServiceConfig.java
+++ opennms-services/src/main/java/org/opennms/netmgt/poller/pollables/PollableServiceConfig.java
@@ -218,8 +218,8 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
                 }
             }
             if (!matched) {
-                ThreadCategory.getInstance(getClass()).warn("getInterval: Could not locate downtime model, throwing runtime exception");
-                throw new RuntimeException("Downtime model is invalid, cannot schedule service " + m_service);
+                ThreadCategory.getInstance(getClass()).error("Downtime model is invalid on package " + m_pkg.getName() + ", cannot schedule service " + m_service);
+                return -1;
             }
         }
         
