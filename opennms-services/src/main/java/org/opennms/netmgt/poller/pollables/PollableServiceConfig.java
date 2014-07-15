50f47b5ff25bb86b25aeeebd41e76e6d878a95f6
Benjamin Reed <ranger@opennms.org>
2014-07-15 08:23:23 -0400

    PJSM-212 Fix embedded XML in poller config (PSM)
    When the Poller configuration was converted to JAXB, the config object
    now contains the actual objects embedded inside parameters, instead of
    an XML string that had to be hand-parsed.  However, the code inside
    PollableServiceConfig was calling .toString() on the embedded config.
    So, for example, an embedded page sequence would actually be returned
    to the PSM as the string representation "PageSequence [Page=foo]" and
    it would fail to monitor.  This likely affected other embedded XML like
    the MTM as well.
    
    Squashed commit of the following:
    
    commit b7e90c1ee2bd9c786eff45d8d0694336d80d0458
    Author: Benjamin Reed <ranger@opennms.org>
    Date:   Mon Jul 14 20:35:20 2014 -0400
    
        better mocking; move to a separate test
    
    commit ca1679f59b5af63fa8d39d877ddb4e599a483542
    Author: Benjamin Reed <ranger@opennms.org>
    Date:   Mon Jul 14 16:52:30 2014 -0400
    
        get rid of extraneous field in the test
    
    commit 6da4ce1fe063729ed836268cd057e4ca750f376a
    Author: Benjamin Reed <ranger@opennms.org>
    Date:   Mon Jul 14 16:34:35 2014 -0400
    
        PJSM-212 fix handling of objects passed as service parameters
    
    commit 8f9ba982b0874cf712ab0bc0bd16565165eda401
    Author: Benjamin Reed <ranger@opennms.org>
    Date:   Mon Jul 14 16:15:15 2014 -0400
    
        remove old useMoxy code, we use it by default now

--- opennms-services/src/main/java/org/opennms/netmgt/poller/pollables/PollableServiceConfig.java
+++ opennms-services/src/main/java/org/opennms/netmgt/poller/pollables/PollableServiceConfig.java
@@ -60,7 +60,7 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
     private Package m_pkg;
     private Timer m_timer;
     private Service m_configService;
-	private ServiceMonitor m_serviceMonitor;
+    private ServiceMonitor m_serviceMonitor;
 
     /**
      * <p>Constructor for PollableServiceConfig.</p>
@@ -78,7 +78,7 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
         m_pkg = pkg;
         m_timer = timer;
         m_configService = findService(pkg);
-        
+
         ServiceMonitor monitor = getServiceMonitor();
         monitor.initialize(m_service);
     }
@@ -95,7 +95,7 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
         }
 
         throw new RuntimeException("Service name not part of package!");
-        
+
     }
 
     /**
@@ -121,15 +121,19 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
         }
     }
 
-	private synchronized ServiceMonitor getServiceMonitor() {
-		if (m_serviceMonitor == null) {
-			ServiceMonitor monitor = m_pollerConfig.getServiceMonitor(m_service.getSvcName());
-			m_serviceMonitor = new LatencyStoringServiceMonitorAdaptor(monitor, m_pollerConfig, m_pkg);
-			
-		}
-		return m_serviceMonitor;
-	}
-    
+    private synchronized ServiceMonitor getServiceMonitor() {
+        if (m_serviceMonitor == null) {
+            ServiceMonitor monitor = m_pollerConfig.getServiceMonitor(m_service.getSvcName());
+            m_serviceMonitor = new LatencyStoringServiceMonitorAdaptor(monitor, m_pollerConfig, m_pkg);
+
+        }
+        return m_serviceMonitor;
+    }
+
+    void setServiceMonitor(final ServiceMonitor serviceMonitor) {
+        m_serviceMonitor = serviceMonitor;
+    }
+
     /**
      * Uses the existing package name to try and re-obtain the package from the poller config factory.
      * Should be called when the poller config has been reloaded.
@@ -143,9 +147,9 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
         m_pkg = newPkg;
         m_configService = findService(m_pkg);
         m_parameters = null;
-        
+
     }
-    
+
     /**
      * Should be called when thresholds configuration has been reloaded
      */
@@ -160,25 +164,24 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
      */
     private synchronized Map<String,Object> getParameters() {
         if (m_parameters == null) {
-            
-            m_parameters = createPropertyMap(m_configService);
+            m_parameters = createParameterMap(m_configService);
         }
         return m_parameters;
     }
 
-    private Map<String,Object> createPropertyMap(Service svc) {
-        Map<String,Object> m = new ConcurrentSkipListMap<String,Object>();
-        for (Parameter p : svc.getParameters()) {
-            String val = p.getValue();
+    private Map<String,Object> createParameterMap(final Service svc) {
+        final Map<String,Object> m = new ConcurrentSkipListMap<String,Object>();
+        for (final Parameter p : svc.getParameters()) {
+            Object val = p.getValue();
             if (val == null) {
-            	val = (p.getAnyObject() == null ? "" : p.getAnyObject().toString());
+                val = (p.getAnyObject() == null ? "" : p.getAnyObject());
             }
 
-           m.put(p.getKey(), val);
+            m.put(p.getKey(), val);
         }
         return m;
     }
-    
+
 
     /**
      * <p>getCurrentTime</p>
@@ -197,10 +200,10 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
      */
     @Override
     public synchronized long getInterval() {
-        
+
         if (m_service.isDeleted())
             return -1;
-        
+
         long when = m_configService.getInterval();
 
         if (m_service.getStatus().isDown()) {
@@ -229,14 +232,14 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
                 throw new RuntimeException("Downtime model is invalid, cannot schedule service " + m_service);
             }
         }
-        
+
         if (when < 0) {
             m_service.sendDeleteEvent();
         }
-        
+
         return when;
     }
-    
+
 
 
     /**
@@ -251,16 +254,16 @@ public class PollableServiceConfig implements PollConfig, ScheduleInterval {
             // Does the outage apply to the current time?
             if (m_pollOutagesConfig.isTimeInOutage(m_timer.getCurrentTime(), outageName)) {
                 // Does the outage apply to this interface?
-    
+
                 if (m_pollOutagesConfig.isNodeIdInOutage(nodeId, outageName) || 
-                    (m_pollOutagesConfig.isInterfaceInOutage(m_service.getIpAddr(), outageName)) || 
+                        (m_pollOutagesConfig.isInterfaceInOutage(m_service.getIpAddr(), outageName)) || 
                         (m_pollOutagesConfig.isInterfaceInOutage("match-any", outageName))) {
                     LOG.debug("scheduledOutage: configured outage '{}' applies, {} will not be polled.", outageName, m_configService);
                     return true;
                 }
             }
         }
-    
+
         //return outageFound;
         return false;
     }
