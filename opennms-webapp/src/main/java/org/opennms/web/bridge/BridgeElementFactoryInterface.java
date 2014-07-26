70edaf16c8e8c95762eb79c1be94f44cce65509d
Antonio <rssntn67@yahoo.it>
2014-07-26 21:02:06 +0200

    Added Methid to finf bridge links to BridgeElementFactory

--- opennms-webapp/src/main/java/org/opennms/web/bridge/BridgeElementFactoryInterface.java
+++ opennms-webapp/src/main/java/org/opennms/web/bridge/BridgeElementFactoryInterface.java
@@ -1,11 +1,12 @@
 package org.opennms.web.bridge;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface BridgeElementFactoryInterface {
 
 	List<BridgeElementNode> getBridgeElements(int nodeId);
 	
-	List<BridgeLinkNode> getBridgeLinks(int nodeId);
+	Collection<BridgeLinkNode> getBridgeLinks(int nodeId);
 	
 }
