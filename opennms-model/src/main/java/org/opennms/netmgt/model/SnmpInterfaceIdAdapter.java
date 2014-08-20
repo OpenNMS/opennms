b62a697ccea67bad406868776081183a1209f1ad
Seth Leger <seth@opennms.org>
2014-08-19 22:14:41 -0400

    Reverted one collection change in SnmpInterfaceIdAdapter that was causing JAXB problems.

--- opennms-model/src/main/java/org/opennms/netmgt/model/SnmpInterfaceIdAdapter.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/SnmpInterfaceIdAdapter.java
@@ -1,16 +1,15 @@
 package org.opennms.netmgt.model;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 
-public class SnmpInterfaceIdAdapter extends XmlAdapter<List<Integer>, Set<OnmsIpInterface>> {
+public class SnmpInterfaceIdAdapter extends XmlAdapter<ArrayList<Integer>, Set<OnmsIpInterface>> {
 
     @Override
-    public List<Integer> marshal(final Set<OnmsIpInterface> ifaces) throws Exception {
+    public ArrayList<Integer> marshal(final Set<OnmsIpInterface> ifaces) throws Exception {
         final ArrayList<Integer> ret = new ArrayList<Integer>();
         for (final OnmsIpInterface iface : ifaces) {
             ret.add(iface.getId());
@@ -19,7 +18,7 @@ public class SnmpInterfaceIdAdapter extends XmlAdapter<List<Integer>, Set<OnmsIp
     }
 
     @Override
-    public Set<OnmsIpInterface> unmarshal(final List<Integer> ids) throws Exception {
+    public Set<OnmsIpInterface> unmarshal(final ArrayList<Integer> ids) throws Exception {
         final Set<OnmsIpInterface> ret = new TreeSet<OnmsIpInterface>();
         for (final Integer id : ids) {
             final OnmsIpInterface iface = new OnmsIpInterface();
