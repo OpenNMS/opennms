4ff638e4cd2df55c316a518e44a82555672e6b08
Benjamin Reed <ranger@opennms.org>
2014-10-15 21:29:32 -0400

    use an available port instead

--- tests/mock-snmp-agent/src/test/java/org/opennms/mock/snmp/LLDPMibTest.java
+++ tests/mock-snmp-agent/src/test/java/org/opennms/mock/snmp/LLDPMibTest.java
@@ -139,7 +139,7 @@ public class LLDPMibTest  {
         m_usm = new USM(SecurityProtocols.getInstance(), new OctetString(MPv3.createLocalEngineID()), 0);
         SecurityModels.getInstance().addSecurityModel(m_usm);
 
-        m_agent = MockSnmpAgent.createAgentAndRun(classPathResource("penrose-lldp-mib.properties"), "127.0.0.1/1691");	
+        m_agent = MockSnmpAgent.createAgentAndRun(classPathResource("penrose-lldp-mib.properties"), "127.0.0.1/0");	
         
         m_requestedVarbinds = new ArrayList<AnticipatedRequest>();
     }
@@ -276,7 +276,7 @@ public class LLDPMibTest  {
 		PDU response;
 		CommunityTarget target = new CommunityTarget();
         target.setCommunity(new OctetString("public"));
-        target.setAddress(new UdpAddress(InetAddress.getByName("127.0.0.1"), 1691));
+        target.setAddress(new UdpAddress(InetAddress.getByName("127.0.0.1"), m_agent.getPort()));
 		target.setVersion(version);
 		if (m_timeout > 0) { 
 			target.setTimeout(m_timeout);
@@ -307,7 +307,7 @@ public class LLDPMibTest  {
         UserTarget target = new UserTarget();
         target.setSecurityLevel(SecurityLevel.AUTH_PRIV);
         target.setSecurityName(userId);
-        target.setAddress(new UdpAddress(InetAddress.getByName("127.0.0.1"), 1691));
+        target.setAddress(new UdpAddress(InetAddress.getByName("127.0.0.1"), m_agent.getPort()));
         target.setVersion(SnmpConstants.version3);
 		if (m_timeout > 0) { 
 			target.setTimeout(m_timeout);
