0c74451fe50214622e2ba7815455cde22d9e891b
Benjamin Reed <ranger@opennms.org>
2014-11-13 14:30:53 -0500

    allow specifying method, also fix append passthrough

--- opennms-bootstrap/src/main/java/org/opennms/bootstrap/InstallerBootstrap.java
+++ opennms-bootstrap/src/main/java/org/opennms/bootstrap/InstallerBootstrap.java
@@ -30,6 +30,6 @@ package org.opennms.bootstrap;
 
 public class InstallerBootstrap extends Bootstrap {
 	public static void main(String[] args) throws Exception {
-		executeClass("org.opennms.install.Installer", "main", args, false);
+		executeClass("org.opennms.install.Installer", "main", args, false, false);
 	}
 }
