38a785a706dd12ad0d48a4650d36847b087fe24d
Seth Leger <seth@opennms.org>
2014-12-03 11:44:39 -0500

    Removed extra javadoc.

--- opennms-webapp/src/main/java/org/opennms/web/svclayer/NodeListService.java
+++ opennms-webapp/src/main/java/org/opennms/web/svclayer/NodeListService.java
@@ -39,14 +39,6 @@ import org.springframework.transaction.annotation.Transactional;
  * @author <a href="mailto:brozow@opennms.org">Mathew Brozowski</a>
  * @author <a href="mailto:david@opennms.org">David Hustace</a>
  * @author <a href="mailto:dj@opennms.org">DJ Gregor</a>
- * @author <a href="mailto:brozow@opennms.org">Mathew Brozowski</a>
- * @author <a href="mailto:david@opennms.org">David Hustace</a>
- * @author <a href="mailto:dj@opennms.org">DJ Gregor</a>
- * @author <a href="mailto:brozow@opennms.org">Mathew Brozowski</a>
- * @author <a href="mailto:david@opennms.org">David Hustace</a>
- * @author <a href="mailto:dj@opennms.org">DJ Gregor</a>
- * @version $Id: $
- * @since 1.8.1
  */
 @Transactional(readOnly = true)
 public interface NodeListService {
