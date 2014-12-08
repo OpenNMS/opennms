716ae09a7ed513bd3087907a3248224848132619
Benjamin Reed <ranger@opennms.org>
2014-12-08 12:49:24 -0500

    allow logging a one-liner event summary at INFO
    if you enable it in eventd-configuration.xml with logEventSummaries="true"

--- opennms-services/src/main/java/org/opennms/netmgt/eventd/DefaultEventHandlerImpl.java
+++ opennms-services/src/main/java/org/opennms/netmgt/eventd/DefaultEventHandlerImpl.java
@@ -28,6 +28,7 @@
 
 package org.opennms.netmgt.eventd;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.opennms.netmgt.model.events.EventProcessor;
@@ -57,6 +58,8 @@ public final class DefaultEventHandlerImpl implements InitializingBean, EventHan
     private static final Logger LOG = LoggerFactory.getLogger(DefaultEventHandlerImpl.class);
     
     private List<EventProcessor> m_eventProcessors;
+
+    private boolean m_logEventSummaries;
 
     /**
      * <p>Constructor for DefaultEventHandlerImpl.</p>
@@ -100,6 +103,10 @@ public final class DefaultEventHandlerImpl implements InitializingBean, EventHan
             }
 
             for (final Event event : events.getEventCollection()) {
+                if (getLogEventSummaries() && LOG.isInfoEnabled()) {
+                    LOG.info("Received event: UEI={}, src={}, iface={}, svc={}, time={}, parms={}", event.getUei(), event.getSource(), event.getInterface(), event.getService(), event.getTime(), getPrettyParms(event));
+                }
+
                 if (LOG.isDebugEnabled()) {
                     // Log the uei, source, and other important aspects
                     final String uuid = event.getUuid();
@@ -108,8 +115,9 @@ public final class DefaultEventHandlerImpl implements InitializingBean, EventHan
                     LOG.debug("  uei   = {}", event.getUei());
                     LOG.debug("  src   = {}", event.getSource());
                     LOG.debug("  iface = {}", event.getInterface());
+                    LOG.debug("  svc   = {}", event.getService());
                     LOG.debug("  time  = {}", event.getTime());
-                    if (event.getParmCollection().size() > 0) {
+                    if (getPrettyParms(event).size() > 0) {
                         LOG.debug("  parms {");
                         for (final Parm parm : event.getParmCollection()) {
                             if ((parm.getParmName() != null) && (parm.getValue().getContent() != null)) {
@@ -133,6 +141,14 @@ public final class DefaultEventHandlerImpl implements InitializingBean, EventHan
                     }
                 }
             }
+        }
+
+        private List<String> getPrettyParms(final Event event) {
+            final List<String> parms = new ArrayList<>();
+            for (final Parm p : event.getParmCollection()) {
+                parms.add(p.getParmName() + "=" + p.getValue().getContent());
+            }
+            return parms;
         }
 
     }
@@ -164,5 +180,13 @@ public final class DefaultEventHandlerImpl implements InitializingBean, EventHan
      */
     public void setEventProcessors(List<EventProcessor> eventProcessors) {
         m_eventProcessors = eventProcessors;
+    }
+    
+    public boolean getLogEventSummaries() {
+        return m_logEventSummaries;
+    }
+    
+    public void setLogEventSummaries(final boolean logEventSummaries) {
+        m_logEventSummaries = logEventSummaries;
     }
 }
