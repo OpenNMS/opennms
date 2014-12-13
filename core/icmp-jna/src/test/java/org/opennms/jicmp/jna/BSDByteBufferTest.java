b389d5cc99b45f7bc5a82a0c56afb6789f9f5a61
Ronald Roskens <ronald.roskens@gmail.com>
2014-12-13 09:53:14 -0600

    NMS-7257: use bsd_sockaddr_in on BSD platforms

--- core/icmp-jna/src/test/java/org/opennms/jicmp/jna/BSDByteBufferTest.java
+++ core/icmp-jna/src/test/java/org/opennms/jicmp/jna/BSDByteBufferTest.java
@@ -45,124 +45,128 @@ import com.sun.jna.ptr.IntByReference;
 
 
 /**
- * ByteBufferTest
+ * BSDByteBufferTest
  *
  * @author brozow
  */
-public class ByteBufferTest {
-    
+public class BSDByteBufferTest {
+
     static {
         Native.register((String)null);
-    }     
+    }
 
     public native int socket(int domain, int type, int protocol) throws LastErrorException;
-    public native int sendto(int socket, Buffer buffer, int buflen, int flags, sockaddr_in dest_addr, int dest_addr_len) throws LastErrorException;
-    public native int recvfrom(int socket, Buffer buffer, int buflen, int flags, sockaddr_in in_addr, IntByReference in_addr_len) throws LastErrorException;
+    public native int sendto(int socket, Buffer buffer, int buflen, int flags, bsd_sockaddr_in dest_addr, int dest_addr_len) throws LastErrorException;
+    public native int recvfrom(int socket, Buffer buffer, int buflen, int flags, bsd_sockaddr_in in_addr, IntByReference in_addr_len) throws LastErrorException;
     //public native int close(int socket) throws LastErrorException;
 
-    
+
     public void printf(String fmt, Object... args) {
         System.err.print(String.format(fmt, args));
     }
 
-    
+
     @Test
     public void testWrap() throws Exception {
-        
+
         String msg = "OpenNMS!";
-        
+
         byte[] data = msg.getBytes("US-ASCII");
 
         ByteBuffer buf = ByteBuffer.wrap(data, 2, 4);
-        
+
         assertThat(buf.arrayOffset(), is(equalTo(0)));
         assertThat(buf.position(), is(equalTo(2)));
         assertThat(buf.limit(), is(equalTo(6)));
         assertThat(buf.capacity(), is(equalTo(data.length)));
-        
+
         assertThat(buf.get(0), is(equalTo((byte)'O')));
-        
+
     }
-    
+
     @Test
     public void testStringDecoding() {
-        
+
         /*
          *  attempt to decode a string from a byte buffer without
          *  accessing the byte array that may or may NOT be behind it
          */
-        
+
         Charset ascii = Charset.forName("US-ASCII");
 
         ByteBuffer buf = ascii.encode("OpenNMS!");
-        
+
         String decoded = ascii.decode(buf).toString();
-        
+
         assertThat(decoded, is(equalTo("OpenNMS!")));
-        
-        
-        
+
+
+
     }
-    
+
     @Test(timeout=30000)
     public void testPassing() throws Exception {
-        if (Platform.isMac() || Platform.isFreeBSD() || Platform.isOpenBSD()) {
+        if (!(Platform.isMac() || Platform.isFreeBSD() || Platform.isOpenBSD())) {
             printf("sockaddr_in is incompatible with bsd_sockaddr_in\n");
             return;
         }
+
         Server server = new Server(7777);
         server.start();
         server.waitForStart();
 
         String msg = "OpenNMS!";
 
-        
+
         int socket = -1;
         try {
-            
+
             byte[] data = msg.getBytes("US-ASCII");
             String sent = msg.substring(4,7);
             ByteBuffer buf = ByteBuffer.wrap(data, 4, 3).slice();
-            
+
+            printf("socket(%d, %d, %d);\n", NativeDatagramSocket.PF_INET, NativeDatagramSocket.SOCK_DGRAM, NativeDatagramSocket.IPPROTO_UDP);
             socket = socket(NativeDatagramSocket.PF_INET, NativeDatagramSocket.SOCK_DGRAM, NativeDatagramSocket.IPPROTO_UDP);
 
-            sockaddr_in destAddr = new sockaddr_in(InetAddress.getLocalHost(), 7777);
+            printf("sockaddr_in destAddr = new sockaddr_in('%s', 7777);\n", InetAddress.getLocalHost());
+            bsd_sockaddr_in destAddr = new bsd_sockaddr_in(InetAddress.getLocalHost(), 7777);
+            printf("sendto(socket, '%s', %d, 0, destAddr, %d);\n", buf, buf.remaining(), destAddr.size());
             sendto(socket, buf, buf.remaining(), 0, destAddr, destAddr.size());
 
-            
-            sockaddr_in in_addr = new sockaddr_in();
+
+            bsd_sockaddr_in in_addr = new bsd_sockaddr_in();
             IntByReference szRef = new IntByReference(in_addr.size());
-            
+
             ByteBuffer rBuf = ByteBuffer.allocate(128);
             int n = recvfrom(socket, rBuf, rBuf.remaining(), 0, in_addr, szRef);
             rBuf.limit(rBuf.position()+n);
-            
+
             assertThat(szRef.getValue(), is(equalTo(in_addr.size())));
             assertThat(rBuf.isDirect(), is(false));
             assertThat(rBuf.position(), is(equalTo(0)));
             assertThat(rBuf.limit(), is(equalTo(n)));
             assertThat(rBuf.capacity(), is(equalTo(128)));
-            
+
             byte[] b = new byte[rBuf.remaining()];
             rBuf.get(b);
-            
+
             String results = new String(b, "US-ASCII");
-            
+
             printf("Received: %s\n", results);
-            
+
             assertEquals(sent, results);
-            
-                
+
+
         } finally {
             // we leak this socket since close doesn't work on windows
             // it will go away when the test exits
             //if (socket != -1) close(socket);
-            
+
             server.stop();
 
         }
 
-        
+
     }
 
 }
