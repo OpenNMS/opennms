5fbfb1f05fc88f7878794913244c02e26764947f
Markus von Rüden <mvr@opennms.com>
2014-12-18 14:27:23 +0100

    NMS-7263: Add missing jaxb.properties and provide JaxbContextTest to verify JAXBContext.

---
+++ integrations/opennms-link-provisioning-adapter/src/test/java/org/opennms/netmgt/provision/JaxbContextTest.java
@@ -0,0 +1,14 @@
+package org.opennms.netmgt.provision;
+
+import org.junit.Test;
+import org.opennms.core.test.xml.JaxbTestUtils;
+
+import javax.xml.bind.JAXBException;
+
+public class JaxbContextTest {
+
+    @Test
+    public void testJaxbContext() throws JAXBException {
+        JaxbTestUtils.verifyJaxbContext(getClass().getPackage().getName());
+    }
+}
