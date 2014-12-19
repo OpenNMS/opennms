222e32c8ac80af2c66de18e284b123def9e6b54a
Alejandro Galue <agalue@opennms.org>
2014-12-19 13:31:33 -0500

    Adding @Qualifier(eventProxy) to all ReST classes that require EventProxy

--- opennms-webapp/src/main/java/org/opennms/web/rest/OnmsMonitoredServiceResource.java
+++ opennms-webapp/src/main/java/org/opennms/web/rest/OnmsMonitoredServiceResource.java
@@ -63,6 +63,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.PropertyAccessorFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -105,6 +106,7 @@ public class OnmsMonitoredServiceResource extends OnmsRestService {
     private ServiceTypeDao m_serviceTypeDao;
     
     @Autowired
+    @Qualifier("eventProxy")
     private EventProxy m_eventProxy;
 
     /**
