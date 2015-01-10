32d5884917aca6888bd25cd9d07cdebd793af873
Bobby Krupczak <rdk@krupczak.org>
2015-01-10 11:12:26 -0500

    Initial checkin of XmpDetector.

---
+++ protocols/xmp/src/main/java/org/opennms/netmgt/protocols/xmp/detector/XmpDetector.java
@@ -0,0 +1,217 @@
+/*
+* OCA CONTRIBUTION ACKNOWLEDGEMENT - NOT PART OF LEGAL BOILERPLATE
+* DO NOT DUPLICATE THIS COMMENT BLOCK WHEN CREATING NEW FILES!
+*
+* This file was contributed to the OpenNMS(R) project under the
+* terms of the OpenNMS Contributor Agreement (OCA).  For details on
+* the OCA, see http://www.opennms.org/index.php/Contributor_Agreement
+*
+* Contributed under the terms of the OCA by:
+*
+* Bobby Krupczak <rdk@krupczak.org>
+* THE KRUPCZAK ORGANIZATION, LLC
+* http://www.krupczak.org/
+*/
+
+/**
+ * OpenNMS XMP Detector allows for discovery of service/protocols via 
+ * provisiond.  Our detector is pretty simple right now.  All it does is
+ * attempt to establish an XmpSession with a system and if it succeeds,
+ * it queries a few core MIB variables and returns success.
+ * Future enhancements could include more capability information like
+ * supported MIBs, modules/plugins, and various dependencies to
+ * determine if something is a server for some service (noted by an
+ * inbound dependency relationship).
+ */
+
+package org.opennms.protocols.xmp.detector;
+
+import java.net.InetAddress;
+import org.opennms.netmgt.provision.support.BasicDetector;
+import org.opennms.netmgt.provision.ServiceDetector;
+import org.opennms.netmgt.provision.SyncServiceDetector;
+import org.krupczak.xmp.*;
+import org.opennms.netmgt.config.xmpConfig.XmpConfig;    
+import org.opennms.netmgt.protocols.xmp.config.XmpConfigFactory;
+import org.opennms.netmgt.protocols.xmp.collector.XmpCollector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author rdk
+ * @version $Id: $
+ *
+ */
+
+public class XmpDetector implements SyncServiceDetector  
+{
+    // class variables
+
+    private static final String DEFAULT_SERVICE_NAME = "XMP";
+    private static final Logger LOG = LoggerFactory.getLogger(XmpDetector.class);
+    // instance variables
+    SocketOpts sockopts;
+    XmpConfig protoConfig;
+    int xmpPort;
+    int xmpTimeout;
+    int xmpRetries;
+    String xmpAuthenUser;
+    String xmpServiceName;
+    String m_ipMatch;
+
+    public XmpDetector(String serviceName, int port)
+    {
+        // set default config
+        xmpPort = port;
+        xmpAuthenUser = new String("xmpUser"); 
+        xmpTimeout = 3000; /* millseconds */
+        xmpRetries = 1;
+
+        // get socket opts
+        sockopts = new SocketOpts();
+
+        xmpServiceName = new String(serviceName);
+
+	m_ipMatch = new String("");
+
+        System.out.println("XmpDetector created");
+
+    } /* XmpDetector */
+
+    public XmpDetector()
+    {
+	this(DEFAULT_SERVICE_NAME,Xmp.XMP_PORT);
+        
+    } /* XmpDetector */
+
+    public String getServiceName() { return xmpServiceName; }
+  
+    public void setServiceName(String newServiceName) 
+    {
+	xmpServiceName = new String(newServiceName);         
+    }      
+
+    public void init()
+    {
+        // try to get configuration
+        try { 
+            XmpConfig protoConfig;
+
+	    XmpConfigFactory.init();
+            protoConfig = XmpConfigFactory.getInstance().getXmpConfig();
+            if (protoConfig.hasPort())
+               xmpPort = protoConfig.getPort();
+            if (protoConfig.hasTimeout())
+               xmpTimeout = protoConfig.getTimeout();
+            if (protoConfig.getAuthenUser() != null)
+               xmpAuthenUser = protoConfig.getAuthenUser();
+
+        } catch (Throwable e) {
+            if (LOG != null)
+               LOG.error("XmpDetector: no config factory, using defaults");   
+            else
+	       System.out.println("XmpDetector: null LOG");
+        }
+    }
+
+    public int getPort() { return xmpPort; }
+
+    public void setPort(int newPort) { xmpPort = newPort; }
+
+    public void setIpMatch(String ipMatch) { m_ipMatch = ipMatch; }
+
+    public String getIpMatch() { return m_ipMatch; }
+
+    public int getTimeout() { return xmpTimeout; }
+
+    public void setTimeout(int newTimeout) 
+    { 
+        xmpTimeout = newTimeout; 
+        sockopts.setConnectTimeout(xmpTimeout);
+    }
+
+    public void dispose()
+    {
+	// dispose of anything like sessions, etc.
+        // no need to dispose SocketOpts
+    }
+
+    public boolean isServiceDetected(InetAddress address)
+    {
+        XmpSession aSession;
+        XmpMessage aReply;
+        XmpVar[] vars,replyVars;
+
+        if (LOG != null)
+           LOG.debug("XmpDetector: isServiceDetected checking out "+address);
+        else 
+	   System.out.println("XmpDetector: isServiceDetected starting with null LOG to query "+address);
+
+        // try to establish session
+        aSession = new XmpSession(sockopts,address,xmpPort,xmpAuthenUser);
+        if (aSession == null) {
+	   System.out.println("XmpDetector: null session to "+address);
+	   LOG.debug("XmpDetector: null session to "+address);
+	   return false;
+        }
+
+        if (LOG != null)
+           LOG.debug("XmpDetector: isServiceDetected session established with "+address);
+        else 
+           System.out.println("XmpDetector: isServiceDetected session established with "+address);
+
+        // query for core.sysName, core.sysDescr, 
+        // core.sysUpTime, core.xmpdVersion
+        vars = new XmpVar[] {
+	    new XmpVar("core","sysName","","",Xmp.SYNTAX_NULLSYNTAX),
+	    new XmpVar("core","sysDescr","","",Xmp.SYNTAX_NULLSYNTAX),
+	    new XmpVar("core","sysUpTime","","",Xmp.SYNTAX_NULLSYNTAX),
+	    new XmpVar("core","xmpdVersion","","",Xmp.SYNTAX_NULLSYNTAX),
+        };
+      
+        if ((aReply = aSession.queryVars(vars)) == null) {
+            if (LOG != null) {
+               LOG.debug("XmpDetector: isServiceDetected no vars from "+address);
+               LOG.debug("XmpDetector: isServiceDetected false for "+address);
+            }
+            else {
+               System.out.println("XmpDetector: isServiceDetected no vars from "+address);
+               System.out.println("XmpDetector: isServiceDetected false for "+address);
+            }
+	    aSession.closeSession();
+            return false;
+        }
+
+        aSession.closeSession();
+
+	// log what we retrieved
+        if ((replyVars = aReply.getMIBVars()) == null) {
+           if (LOG != null) {
+	      LOG.debug("XmpDetector: isServiceDetected no replyVars for "+address);
+   	   }
+           else {
+	      System.out.println("XmpDetector: isServiceDetected no replyVars for"+address);
+           }
+
+           return false;
+
+        } /* if replyVars == null */
+
+        if (LOG != null) {
+           LOG.debug("XmpDetector: isServiceDetected "+address+" reports "+
+                     replyVars[0].getValue()+","+
+                     replyVars[1].getValue());
+           LOG.debug("XmpDetector: isServiceDetected true for "+address);
+	}
+        else {
+           System.out.println("XmpDetector: isServiceDetected "+address+" reports "+
+                              replyVars[0].getValue()+","+
+                              replyVars[1].getValue());
+           System.out.println("XmpDetector: isServiceDetected true for "+address);
+        }
+
+	return true;
+
+    } /* isServiceDetected */
+
+} /* class XmpDetector */
