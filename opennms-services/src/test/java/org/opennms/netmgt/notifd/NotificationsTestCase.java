6fd1f4c176616bf6921a92099b0eab185c723211
Jesse White <jesse@opennms.org>
2015-02-09 15:09:18 -0500

    Convert notifd to a Spring-based daemon and implement the remaining calls in EventUtilDaoImpl using reflection.
    Squashed commit of the following:
    
    commit 9d4ced872bba02811a835363d2887b4222dc85cd
    Author: Jesse White <jesse@opennms.org>
    Date:   Mon Feb 9 09:29:49 2015 -0500
    
        Make sure all expandParms calls are captured in a transaction.
    
    commit fae8f09ec5ca00c0e7fc545d34d0709859dfd046
    Author: Jesse White <jesse@opennms.org>
    Date:   Mon Feb 9 09:29:27 2015 -0500
    
        Fix an issue with duplicate EventIPCManager beans.
    
    commit 4811a488f73b28b812d1e587ae636951865673d2
    Author: Jesse White <jesse@opennms.org>
    Date:   Sun Feb 8 18:24:44 2015 -0500
    
        Retrieve asset fields and hardware entity fields by name using reflection.
    
    commit ae2bd94e02b3fa157cf5a60a43aa97b80ce04dd6
    Author: Jesse White <jesse@opennms.org>
    Date:   Sun Feb 8 17:19:31 2015 -0500
    
        Fix an invalid test case, getAssetFieldValue() should not return the node label.
    
    commit 8534024f907193e712d15af609dc4c268f50f011
    Author: Jesse White <jesse@opennms.org>
    Date:   Sun Feb 8 16:54:35 2015 -0500
    
        Change proxy type to allow autowiring of EventUtilDaoImpl.
    
    commit bddcab04e5e15a622fcedd43775d18d7c6ddcb9a
    Author: Jesse White <jesse@opennms.org>
    Date:   Sun Feb 8 13:42:59 2015 -0500
    
        Convert notifd to a Spring-based daemon.
    
    commit c2a3b9be70820048fc362bd1c3606e13eb4eb8a7
    Author: Jesse White <jesse@opennms.org>
    Date:   Sun Feb 8 12:41:20 2015 -0500
    
        Make the expandParms() transactional in order to resolve issues
        with lazy loading.
    
    commit 0651939b8ea67d211d2e8c34bd83151ce176859d
    Author: Jesse White <jesse@opennms.org>
    Date:   Sat Feb 7 11:55:16 2015 -0500
    
        Use Spring and the Hibernate-based EventUtil implementation (instead of the JDBC implementation)
        in the base test case for notifd.
    
        With this change, some tests are now failing with org.hibernate.LazyInitializationException,
        as reported in NMS-7396.

--- opennms-services/src/test/java/org/opennms/netmgt/notifd/NotificationsTestCase.java
+++ opennms-services/src/test/java/org/opennms/netmgt/notifd/NotificationsTestCase.java
@@ -35,10 +35,15 @@ import java.util.Date;
 
 import org.exolab.castor.xml.MarshalException;
 import org.exolab.castor.xml.ValidationException;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
 import org.opennms.core.db.DataSourceFactory;
 import org.opennms.core.test.ConfigurationTestUtils;
 import org.opennms.core.test.MockLogAppender;
+import org.opennms.core.test.OpenNMSJUnit4ClassRunner;
 import org.opennms.core.test.db.MockDatabase;
+import org.opennms.core.test.db.TemporaryDatabaseAware;
+import org.opennms.core.test.db.annotations.JUnitTemporaryDatabase;
 import org.opennms.core.utils.TimeConverter;
 import org.opennms.netmgt.config.NotificationCommandManager;
 import org.opennms.netmgt.config.NotificationManager;
@@ -54,22 +59,43 @@ import org.opennms.netmgt.config.mock.MockUserManager;
 import org.opennms.netmgt.config.users.Contact;
 import org.opennms.netmgt.config.users.User;
 import org.opennms.netmgt.dao.mock.MockEventIpcManager;
-import org.opennms.netmgt.eventd.AbstractEventUtil;
-import org.opennms.netmgt.mock.EventUtilJdbcImpl;
+import org.opennms.netmgt.eventd.EventUtil;
 import org.opennms.netmgt.mock.MockNetwork;
 import org.opennms.netmgt.mock.MockNotification;
 import org.opennms.netmgt.mock.MockPollerConfig;
 import org.opennms.netmgt.mock.NotificationAnticipator;
 import org.opennms.test.DaoTestConfigBean;
+import org.opennms.test.JUnitConfigurationEnvironment;
 import org.opennms.test.mock.MockUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 
-/**
- * @deprecated Please develop new unit tests by using the Spring unit test
- * framework instead of this base class.
- */
-public class NotificationsTestCase {
+@RunWith(OpenNMSJUnit4ClassRunner.class)
+@ContextConfiguration(locations={
+        "classpath:/META-INF/opennms/applicationContext-soa.xml",
+        "classpath:/META-INF/opennms/applicationContext-dao.xml",
+        "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
+        "classpath*:/META-INF/opennms/component-dao.xml",
+        "classpath*:/META-INF/opennms/component-service.xml",
+        "classpath:/META-INF/opennms/mockEventIpcManager.xml",
+        "classpath:/META-INF/opennms/applicationContext-minimal-conf.xml",
+        // Notifd
+        "classpath:/META-INF/opennms/applicationContext-notifdTest.xml"
+})
+@JUnitConfigurationEnvironment
+@JUnitTemporaryDatabase(tempDbClass=MockDatabase.class,reuseDatabase=false)
+@Ignore
+public class NotificationsTestCase implements TemporaryDatabaseAware<MockDatabase> {
 
+    @Autowired
     protected Notifd m_notifd;
+
+    @Autowired
+    protected BroadcastEventProcessor m_eventProcessor;
+
+    @Autowired
+    protected EventUtil m_eventUtil;
+
     protected MockEventIpcManager m_eventMgr;
     protected MockNotifdConfigManager m_notifdConfig;
     protected MockGroupManager m_groupManager;
@@ -91,14 +117,13 @@ public class NotificationsTestCase {
         MockLogAppender.setupLogging();
         
         m_network = createMockNetwork();
-        
-        m_db = createDatabase(m_network);
-    
+
+        m_db.populate(m_network);
+        DataSourceFactory.setInstance(m_db);
+
         m_eventMgr = new MockEventIpcManager();
         m_eventMgr.setEventWriter(m_db);
-        
-        AbstractEventUtil.setInstance(new EventUtilJdbcImpl());
-        
+
         m_notifdConfig = new MockNotifdConfigManager(ConfigurationTestUtils.getConfigForResourceWithReplacements(this, "notifd-configuration.xml"));
         m_notifdConfig.setNextNotifIdSql(m_db.getNextNotifIdSql());
         m_notifdConfig.setNextUserNotifIdSql(m_db.getNextUserNotifIdSql());
@@ -113,17 +138,18 @@ public class NotificationsTestCase {
         
         m_anticipator = new NotificationAnticipator();
         MockNotificationStrategy.setAnticipator(m_anticipator);
-        
-        m_notifd = new Notifd();
-        m_notifd.setEventManager(m_eventMgr);
+
         m_notifd.setConfigManager(m_notifdConfig);
-        m_notifd.setGroupManager(m_groupManager);
-        m_notifd.setUserManager(m_userManager);
-        m_notifd.setDestinationPathManager(m_destinationPathManager);
-        m_notifd.setNotificationCommandManager(m_notificationCommandManger);
-        m_notifd.setNotificationManager(m_notificationManager);
-        m_notifd.setPollOutagesConfigManager(m_pollOutagesConfigManager);
-                
+
+        m_eventProcessor.setEventManager(m_eventMgr);
+        m_eventProcessor.setNotifdConfigManager(m_notifdConfig);
+        m_eventProcessor.setGroupManager(m_groupManager);
+        m_eventProcessor.setUserManager(m_userManager);
+        m_eventProcessor.setDestinationPathManager(m_destinationPathManager);
+        m_eventProcessor.setNotificationCommandManager(m_notificationCommandManger);
+        m_eventProcessor.setNotificationManager(m_notificationManager);
+        m_eventProcessor.setPollOutagesConfigManager(m_pollOutagesConfigManager);
+
         m_notifd.init();
         m_notifd.start();
         
@@ -140,13 +166,6 @@ public class NotificationsTestCase {
         MockUtil.println("################ Finish Setup ################");
 
     
-    }
-
-    protected MockDatabase createDatabase(MockNetwork network) throws Exception {
-        MockDatabase db = new MockDatabase();
-        DataSourceFactory.setInstance(db);
-        db.populate(network);
-        return db;
     }
 
     protected MockNetwork createMockNetwork() {
@@ -254,4 +273,8 @@ public class NotificationsTestCase {
         return TimeConverter.convertToMillis(interval);
     }
 
+    @Override
+    public void setTemporaryDatabase(MockDatabase database) {
+        m_db = database;
+    }
 }
