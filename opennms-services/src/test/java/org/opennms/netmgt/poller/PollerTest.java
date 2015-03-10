231ce8596dac6168600db176c1b0ea8ca40c4969
Jesse White <jesse@opennms.org>
2015-03-10 11:50:35 -0400

    Add a callback used to notify the event hook that it should unblock any pending events.

--- opennms-services/src/test/java/org/opennms/netmgt/poller/PollerTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/poller/PollerTest.java
@@ -1311,6 +1311,13 @@ public class PollerTest implements TemporaryDatabaseAware<MockDatabase> {
                 m_latch.countDown();
             }
         }
+
+        @Override
+        public void finishProcessingEvents() {
+            while(m_latch.getCount() > 0) {
+                m_latch.countDown();
+            }
+        }
     }
 
     /**
