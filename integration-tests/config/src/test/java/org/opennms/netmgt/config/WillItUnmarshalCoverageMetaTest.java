d6774d1c2f75a7129b7bdb8a5834b8438cfcdb1b
Jeff Gehlbach <jeffg@opennms.org>
2015-03-11 11:23:59 -0400

    NMS-7535: Relocate external web auth config to OPENNMS_HOME/etc
    - Create a NullAuthenticationProvider for use in the no-external-auth
      use case
    - Wire up this provider as the default externalAuthenticationProvider
      bean for the local-only default use case
    - Move the configuration of the externalAuthenticationProvider bean out
      of the WEB-INF/spring-security.d directory and into the main etc

--- integration-tests/config/src/test/java/org/opennms/netmgt/config/WillItUnmarshalCoverageMetaTest.java
+++ integration-tests/config/src/test/java/org/opennms/netmgt/config/WillItUnmarshalCoverageMetaTest.java
@@ -127,6 +127,7 @@ public class WillItUnmarshalCoverageMetaTest {
         ignoreFile(new File(getDaemonEtcDirectory(), "syslog/POSIX.syslog.xml"));
         ignoreFile(new File(getDaemonEtcDirectory(), "syslog/Sudo.syslog.xml"));
         ignoreFile(new File(getDaemonEtcDirectory(), "log4j2.xml"));
+        ignoreFile(new File(getDaemonEtcDirectory(), "external-auth-config.xml"));
         LOG.debug("FILES.size() = {}", FILES.size());
     }
     
