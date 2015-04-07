c360782732635d176da762a37a20769b0acdc6c8
Benjamin Reed <ranger@opennms.org>
2015-04-07 11:38:39 -0400

    audit detector tests for long timeouts and test-order issues

--- opennms-provision/opennms-detector-simple/src/test/java/org/opennms/netmgt/provision/detector/Win32ServiceDetectorTest.java
+++ opennms-provision/opennms-detector-simple/src/test/java/org/opennms/netmgt/provision/detector/Win32ServiceDetectorTest.java
@@ -72,12 +72,12 @@ public class Win32ServiceDetectorTest implements InitializingBean {
         m_detector.setWin32ServiceName("VMware Tools Service");
     }
     
-    @Test(timeout=90000)
+    @Test(timeout=20000)
     public void testDetectorSuccessful() throws UnknownHostException{
         assertTrue(m_detector.isServiceDetected(InetAddressUtils.addr(TEST_IP_ADDRESS)));
     }
 
-    @Test(timeout=90000)
+    @Test(timeout=20000)
     public void testDetectorFail() throws UnknownHostException{
         m_detector.setWin32ServiceName("This service does not exist");
         assertFalse(m_detector.isServiceDetected(InetAddressUtils.addr(TEST_IP_ADDRESS)));
