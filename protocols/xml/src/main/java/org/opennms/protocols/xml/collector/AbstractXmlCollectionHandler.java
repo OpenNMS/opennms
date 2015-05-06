240d48f92c15d2f5c91808130f526f7a010eaa5d
Alejandro Galue <agalue@opennms.org>
2015-05-06 13:53:32 -0400

    Fix for Bug NMS-7650
    XML data collection with HTTP POST requests is not working.
    
    The method parseRequest from the AbstractXmlCollectionHandler class was
    not passing the method (i.e. GET or POST, and it defaults to GET if it
    is configured) to the formatted version of the Request object, which was
    preventing sending an actual POST to the target device.
    
    A JUnit test has been added to verify the proper behavior of the POST
    request against a test web-app.

--- protocols/xml/src/main/java/org/opennms/protocols/xml/collector/AbstractXmlCollectionHandler.java
+++ protocols/xml/src/main/java/org/opennms/protocols/xml/collector/AbstractXmlCollectionHandler.java
@@ -189,9 +189,9 @@ public abstract class AbstractXmlCollectionHandler implements XmlCollectionHandl
             for (XmlSource source : collection.getXmlSources()) {
                 LOG.debug("collect: starting source url '{}' collection", source.getUrl());
                 String urlStr = parseUrl(source.getUrl(), agent, collection.getXmlRrd().getStep());
-                LOG.debug("collect: parsed url for source url '{}'", source.getUrl());
+                LOG.debug("collect: parsed url for source url '{}'", urlStr);
                 Request request = parseRequest(source.getRequest(), agent, collection.getXmlRrd().getStep());
-                LOG.debug("collect: parsed request for source url '{}'", source.getUrl());
+                LOG.debug("collect: parsed request for source url '{}' : {}", urlStr, request);
                 fillCollectionSet(urlStr, request, agent, collectionSet, source);
                 LOG.debug("collect: finished source url '{}' collection", source.getUrl());
             }
@@ -386,6 +386,7 @@ public abstract class AbstractXmlCollectionHandler implements XmlCollectionHandl
             return null;
         final OnmsNode node = getNodeDao().get(agent.getNodeId());
         final Request request = new Request();
+        request.setMethod(unformattedRequest.getMethod());
         for (Header header : unformattedRequest.getHeaders()) {
             request.addHeader(header.getName(), parseString(header.getName(), header.getValue(), node, agent.getHostAddress(), collectionStep));
         }
