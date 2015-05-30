7a08b9e7da41f78aba411b06fa223eaf72439801
Jesse White <jesse@opennms.org>
2015-05-30 17:19:02 -0400

    Add support for performing queries against multiple resource ids.

--- features/rest-measurements-api/src/main/java/org/opennms/web/rest/measurements/fetch/NewtsFetchStrategy.java
+++ features/rest-measurements-api/src/main/java/org/opennms/web/rest/measurements/fetch/NewtsFetchStrategy.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.opennms.netmgt.dao.api.ResourceDao;
 import org.opennms.netmgt.model.OnmsResource;
@@ -28,11 +29,14 @@ import org.slf4j.LoggerFactory;
 import com.codahale.metrics.MetricRegistry;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 public class NewtsFetchStrategy implements MeasurementFetchStrategy {
 
     private static final Logger LOG = LoggerFactory.getLogger(NewtsFetchStrategy.class);
+
+    private static final int STEP_LOWER_BOUND_IN_MS = 120 * 1000;
 
     private final ResourceDao m_resourceDao;
 
@@ -47,16 +51,18 @@ public class NewtsFetchStrategy implements MeasurementFetchStrategy {
     @Override
     public FetchResults fetch(long start, long end, long step, int maxrows,
             List<Source> sources) throws Exception {
-        
-        // Add a lower bound
-        step = Math.max(300 * 1000, step);
+        // Limit the step with a lower bound in order to prevent
+        // extremely large queries
+        step = Math.max(STEP_LOWER_BOUND_IN_MS, step);
 
         Optional<Timestamp> startTs = Optional.of(Timestamp.fromEpochMillis(start));
         Optional<Timestamp> endTs = Optional.of(Timestamp.fromEpochMillis(end));
 
-        ResultDescriptor resultDescriptor = new ResultDescriptor(step);
+        // Newts only allows to to perform a query using single resource id
+        // To work around this, we group the sources by resource id,
+        // perform multiple queries and aggregate the results
+        Map<String, List<Source>> sourcesByNewtsResourceId = Maps.newHashMap();
 
-        String resourceId = null;
         for (Source source : sources) {
             // Grab the resource
             final OnmsResource resource = m_resourceDao.getResourceById(source
@@ -73,52 +79,73 @@ public class NewtsFetchStrategy implements MeasurementFetchStrategy {
                 LOG.error("No attribute with name: {}", source.getAttribute());
             }
 
-            if (resourceId == null) {
-                resourceId = rrdGraphAttribute.getRrdRelativePath();
-            } else {
-                if (!resourceId.equals(rrdGraphAttribute.getRrdRelativePath())) {
-                    throw new RuntimeException("Newts queries cannot span multiple resource ids!");
-                }
+            final String newtsResourceId = rrdGraphAttribute.getRrdRelativePath();
+
+            List<Source> listOfSources = sourcesByNewtsResourceId.get(newtsResourceId);
+            // Create the list if it doesn't exist
+            if (listOfSources == null) {
+                listOfSources = Lists.newArrayList();
+                sourcesByNewtsResourceId.put(newtsResourceId, listOfSources);
             }
 
-            final String metricName = source.getAttribute();
-            final String name = source.getLabel();
-            final AggregationFunction fn = toAggregationFunction(source.getAggregation());
-            
-            resultDescriptor.datasource(name, metricName, 2*step, fn);
-            resultDescriptor.export(name);
+            listOfSources.add(source);
         }
 
-        // HACK - not sure where the / is coming from right meow
-        if (resourceId.startsWith("/")) {
-            resourceId = resourceId.substring(1, resourceId.length());
-        }
-
-        //LOG.debug("Samples: {}", getSampleRepository().select(new Resource("response:127.0.0.1:icmp"), startTs, endTs));
-
-        LOG.debug("Querying Newts for resource id {} with result descriptor: {}", resourceId, resultDescriptor);
-        Results<Measurement> results = getSampleRepository().select(new Resource(resourceId), startTs, endTs, resultDescriptor, Duration.millis(2*step));
-        Collection<Row<Measurement>> rows = results.getRows();
-        LOG.debug("Found {} rows.", rows.size());
-
-        final int N = rows.size();
-        final long[] timestamps = new long[N];
-        final Map<String, double[]> columns = Maps.newHashMapWithExpectedSize(N);
+        // Used to aggregate the results
+        long[] timestamps = null;
+        final Map<String, double[]> columns = Maps.newHashMap();
         final Map<String, Object> constants = Maps.newHashMap();
 
-        int k = 0;
-        for (Row<Measurement> row : results.getRows()) {
-            for (Measurement measurement : row.getElements()) {
-                if (k == 0) {
-                    columns.put(measurement.getName(), new double[N]);
-                }
-                columns.get(measurement.getName())[k] = measurement.getValue();
-                Map<String, String> attributes = measurement.getAttributes();
-                if (attributes != null) {
-                    constants.putAll(measurement.getAttributes());
-                }
+        for (Entry<String, List<Source>> entry : sourcesByNewtsResourceId.entrySet()) {
+            String newtsResourceId = entry.getKey();
+            List<Source> listOfSources = entry.getValue();
+
+            ResultDescriptor resultDescriptor = new ResultDescriptor(step);
+            for (Source source : listOfSources) {
+                final String metricName = source.getAttribute();
+                final String name = source.getLabel();
+                final AggregationFunction fn = toAggregationFunction(source.getAggregation());
+
+                resultDescriptor.datasource(name, metricName, 2*step, fn);
+                resultDescriptor.export(name);
             }
-            timestamps[k++] = row.getTimestamp().asMillis();
+
+            // HACK - not sure where the / is coming from right meow
+            if (newtsResourceId.startsWith("/")) {
+                newtsResourceId = newtsResourceId.substring(1, newtsResourceId.length());
+            }
+
+            LOG.debug("Querying Newts for resource id {} with result descriptor: {}", newtsResourceId, resultDescriptor);
+            Results<Measurement> results = getSampleRepository().select(new Resource(newtsResourceId), startTs, endTs, resultDescriptor, Duration.millis(2*step));
+            Collection<Row<Measurement>> rows = results.getRows();
+            LOG.debug("Found {} rows.", rows.size());
+
+            final int N = rows.size();
+            boolean setTimestamps = false;
+            if (timestamps == null) {
+                timestamps = new long[N];
+                setTimestamps = true;
+            }
+
+            int k = 0;
+            for (Row<Measurement> row : results.getRows()) {
+                for (Measurement measurement : row.getElements()) {
+                    if (k == 0) {
+                        columns.put(measurement.getName(), new double[N]);
+                    }
+                    columns.get(measurement.getName())[k] = measurement.getValue();
+                    Map<String, String> attributes = measurement.getAttributes();
+                    if (attributes != null) {
+                        constants.putAll(measurement.getAttributes());
+                    }
+                }
+
+                // Only set the timestamps when processing the first resultset
+                if (setTimestamps) {
+                    timestamps[k] = row.getTimestamp().asMillis();
+                }
+                k += 1;
+            }
         }
 
         FetchResults fetchResults = new FetchResults(timestamps, columns, step, constants);
