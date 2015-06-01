5e83e5b68d61e53ffaa336106452d884c55bbb3b
Jesse White <jesse@opennms.org>
2015-06-01 13:13:16 -0400

    Use conditional contexts to load the Newts beans instead of the RRD beans when needed.

---
+++ opennms-rrd/opennms-rrd-newts/src/main/java/org/opennms/netmgt/dao/support/ConditionalNewtsDaoContext.java
@@ -0,0 +1,25 @@
+package org.opennms.netmgt.dao.support;
+
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ConfigurationCondition;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+@Configuration
+@Conditional(ConditionalNewtsDaoContext.Condition.class)
+@ImportResource("/META-INF/opennms/component-dao-ext-newts.xml")
+public class ConditionalNewtsDaoContext {
+    static class Condition implements ConfigurationCondition {
+         @Override
+         public ConfigurationPhase getConfigurationPhase() {
+             return ConfigurationPhase.PARSE_CONFIGURATION;
+         }
+
+         @Override
+         public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+             return "org.opennms.netmgt.rrd.newts.NewtsRrdStrategy".equals(System.getProperty("org.opennms.rrd.strategyClass"));
+         }
+    }
+}
