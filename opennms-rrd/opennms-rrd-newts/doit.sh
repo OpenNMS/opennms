2b0b7c2813f6aa59ba5e89d8f47812d406ce947f
Jesse White <jesse@opennms.org>
2015-06-03 15:37:29 -0400

    This doesn't belong here.

--- opennms-rrd/opennms-rrd-newts/doit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-OPENMMS_HOME=~/git/opennms/target/opennms
-rm -rf ./target
-../../compile.pl
-sudo cp target/opennms-rrd-newts-17.0.0-SNAPSHOT.jar $OPENNMS_HOME/lib/
-sudo $OPENMMS_HOME/bin/opennms restart
