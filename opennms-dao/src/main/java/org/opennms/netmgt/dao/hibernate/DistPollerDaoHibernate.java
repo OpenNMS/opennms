2c1bbf9ce78b564b2b205c627e62c80f0d06a0fc
Seth Leger <seth@opennms.org>
2015-06-08 16:27:35 -0400

    Got rid of caching support on DistPollerDaoHibernate because it breaks lazy loading when OnmsDistPoller is a subclass of OnmsMonitoringSystem.

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/hibernate/DistPollerDaoHibernate.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/hibernate/DistPollerDaoHibernate.java
@@ -31,21 +31,12 @@ package org.opennms.netmgt.dao.hibernate;
 import org.opennms.netmgt.dao.api.DistPollerDao;
 import org.opennms.netmgt.model.OnmsDistPoller;
 
-public class DistPollerDaoHibernate extends AbstractCachingDaoHibernate<OnmsDistPoller, String, String> implements DistPollerDao {
+public class DistPollerDaoHibernate extends AbstractDaoHibernate<OnmsDistPoller, String> implements DistPollerDao {
 
     /**
      * <p>Constructor for DistPollerDaoHibernate.</p>
      */
     public DistPollerDaoHibernate() {
-		super(OnmsDistPoller.class, true);
-	}
-
-    /** {@inheritDoc} */
-    @Override
-    protected String getKey(OnmsDistPoller t) {
-        return t.getId();
+        super(OnmsDistPoller.class);
     }
-
-
-
 }
