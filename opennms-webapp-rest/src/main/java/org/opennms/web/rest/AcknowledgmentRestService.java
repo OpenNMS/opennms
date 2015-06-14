32b95a5f24918be8072c80ec0edef50736b621bf
Seth Leger <seth@opennms.org>
2015-06-14 07:53:33 -0400

    Changed UriInfo into a method parameter for better CXF compatibility. Added names to @Component annotations for REST services.

--- opennms-webapp-rest/src/main/java/org/opennms/web/rest/AcknowledgmentRestService.java
+++ opennms-webapp-rest/src/main/java/org/opennms/web/rest/AcknowledgmentRestService.java
@@ -37,7 +37,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 
 import org.opennms.core.criteria.CriteriaBuilder;
@@ -63,7 +62,7 @@ import com.sun.jersey.spi.resource.PerRequest;
  * @version $Id: $
  * @since 1.8.1
  */
-@Component
+@Component("acknowledgmentRestService")
 @PerRequest
 @Scope("prototype")
 @Path("acks")
@@ -76,12 +75,6 @@ public class AcknowledgmentRestService extends OnmsRestService {
     
     @Autowired
     private NotificationDao m_notificationDao;
-    
-    @Context 
-    UriInfo m_uriInfo;
-
-    @Context
-    SecurityContext m_securityContext;
     
     /**
      * <p>getAcknowledgment</p>
@@ -128,11 +121,11 @@ public class AcknowledgmentRestService extends OnmsRestService {
     @GET
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_ATOM_XML})
     @Transactional
-    public OnmsAcknowledgmentCollection getAcks() {
+    public OnmsAcknowledgmentCollection getAcks(@Context UriInfo uriInfo) {
         readLock();
         
         try {
-            final CriteriaBuilder builder = getQueryFilters(m_uriInfo.getQueryParameters());
+            final CriteriaBuilder builder = getQueryFilters(uriInfo.getQueryParameters());
             OnmsAcknowledgmentCollection coll = new OnmsAcknowledgmentCollection(m_ackDao.findMatching(builder.toCriteria()));
     
             //For getting totalCount
--- opennms-webapp-rest/src/main/java/org/opennms/web/rest/MinionRestService.java
+++ opennms-webapp-rest/src/main/java/org/opennms/web/rest/MinionRestService.java
@@ -41,7 +41,6 @@ import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.SecurityContext;
 import javax.ws.rs.core.UriInfo;
 
-import org.opennms.core.criteria.Alias.JoinType;
 import org.opennms.core.criteria.CriteriaBuilder;
 import org.opennms.netmgt.dao.api.MinionDao;
 import org.opennms.netmgt.model.OnmsMinionCollection;
@@ -53,16 +52,13 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.sun.jersey.spi.resource.PerRequest;
 
-@Component
+@Component("minionRestService")
 @PerRequest
 @Scope("prototype")
 @Path("minions")
 public class MinionRestService extends OnmsRestService {
     @Autowired
     private MinionDao m_minionDao;
-
-    @Context
-    UriInfo m_uriInfo;
 
     @Context
     HttpHeaders m_headers;
@@ -112,11 +108,11 @@ public class MinionRestService extends OnmsRestService {
     @GET
     @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON, MediaType.APPLICATION_ATOM_XML})
     @Transactional
-    public OnmsMinionCollection getMinions() throws ParseException {
+    public OnmsMinionCollection getMinions(@Context UriInfo uriInfo) throws ParseException {
         readLock();
 
         try {
-            final CriteriaBuilder builder = getCriteriaBuilder(m_uriInfo.getQueryParameters());
+            final CriteriaBuilder builder = getCriteriaBuilder(uriInfo.getQueryParameters());
             final OnmsMinionCollection coll = new OnmsMinionCollection(m_minionDao.findMatching(builder.toCriteria()));
             coll.setTotalCount(m_minionDao.countMatching(builder.clearOrder().toCriteria()));
 
