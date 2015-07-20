ca024b66aed899f5abcaf6d1e8e18b5b2279d6d5
Benjamin Reed <ranger@opennms.org>
2015-07-20 14:42:28 -0400

    refactor test to support multiple sources

--- opennms-rrd/opennms-rrd-newts/src/test/java/org/opennms/netmgt/measurements/impl/NewtsFetchStrategyTest.java
+++ opennms-rrd/opennms-rrd-newts/src/test/java/org/opennms/netmgt/measurements/impl/NewtsFetchStrategyTest.java
@@ -29,11 +29,14 @@
 package org.opennms.netmgt.measurements.impl;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 import java.util.Set;
 
 import org.easymock.EasyMock;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.opennms.netmgt.dao.api.ResourceDao;
 import org.opennms.netmgt.measurements.api.FetchResults;
@@ -55,51 +58,88 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 public class NewtsFetchStrategyTest {
+    private Context m_context;
+    private ResourceDao m_resourceDao;
+    private SampleRepository m_sampleRepository;
+
+    private NewtsFetchStrategy m_newtsFetchStrategy;
+
+    @Before
+    public void setUp() throws Exception {
+        m_context = new Context("test");
+        m_resourceDao = EasyMock.createNiceMock(ResourceDao.class);
+        m_sampleRepository = EasyMock.createNiceMock(SampleRepository.class);
+ 
+        m_newtsFetchStrategy = new NewtsFetchStrategy();
+        m_newtsFetchStrategy.setContext(m_context);
+        m_newtsFetchStrategy.setResourceDao(m_resourceDao);
+        m_newtsFetchStrategy.setSampleRepository(m_sampleRepository);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        EasyMock.verify(m_resourceDao, m_sampleRepository);
+    }
 
     @Test
-    public void fetchIt() throws Exception {
-        RrdGraphAttribute attribute = new RrdGraphAttribute("icmp", "", "response:127.0.0.1:icmp");
+    public void testFetch() throws Exception {
+        List<Source> sources = Lists.newArrayList(
+            createMockResource("icmplocalhost", "icmp", "127.0.0.1"),
+            createMockResource("snmplocalhost", "snmp", "127.0.0.1"),
+            createMockResource("snmp192", "snmp", "192.168.0.1")
+        );
+        EasyMock.replay(m_resourceDao, m_sampleRepository);
 
-        Set<OnmsAttribute> attributes = Sets.newHashSet(attribute);
+        FetchResults fetchResults = m_newtsFetchStrategy.fetch(1431047069000L - (60 * 60 * 1000), 1431047069000L, 300 * 1000, 0, sources);
+        assertEquals(3, fetchResults.getColumns().keySet().size());
+        assertTrue(fetchResults.getColumns().containsKey("icmplocalhost"));
+        assertTrue(fetchResults.getColumns().containsKey("snmplocalhost"));
+        assertTrue(fetchResults.getColumns().containsKey("snmp192"));
+        assertEquals(1, fetchResults.getTimestamps().length);
+    }
+
+    @Test
+    public void testFetchWithDuplicateResources() throws Exception {
+        List<Source> sources = Lists.newArrayList(
+            createMockResource("icmp", "icmp", "127.0.0.1"),
+            createMockResource("icmp", "icmp", "192.168.0.1")
+        );
+        EasyMock.replay(m_resourceDao, m_sampleRepository);
+
+        FetchResults fetchResults = m_newtsFetchStrategy.fetch(1431047069000L - (60 * 60 * 1000), 1431047069000L, 300 * 1000, 0, sources);
+        // It's not possible to fetch multiple resources with the same label, we should only get 1 ICMP result
+        assertEquals(1, fetchResults.getColumns().keySet().size());
+    }
+
+    public Source createMockResource(final String label, final String attr, final String node) {
         OnmsResourceType type = EasyMock.createNiceMock(OnmsResourceType.class);
-        OnmsResource resource = new OnmsResource("", "", type, attributes, ResourcePath.get("foo"));
 
-        ResourceDao resourceDao = EasyMock.createNiceMock(ResourceDao.class);
-        EasyMock.expect(resourceDao.getResourceById("nodeSource[NODES:1430502148137].responseTime[127.0.0.1]")).andReturn(resource);
+        final int nodeId = node.hashCode();
 
-        Context context = new Context("test");
+        final String newtsResourceId = "response:" + node + ":" + attr;
+        RrdGraphAttribute attribute = new RrdGraphAttribute(attr, "", newtsResourceId);
+        Set<OnmsAttribute> attributes = Sets.newHashSet(attribute);
+        OnmsResource resource = new OnmsResource(attr, label, type, attributes, ResourcePath.get("foo"));
+        final String resourceId = "nodeSource[NODES:" + nodeId + "].responseTime[" + node + "]";
+        EasyMock.expect(m_resourceDao.getResourceById(resourceId)).andReturn(resource);
 
-        SampleRepository sampleRepository = EasyMock.createNiceMock(SampleRepository.class);
         Results<Measurement> results = new Results<Measurement>();
-        Resource res = new Resource("");
+        Resource res = new Resource(newtsResourceId);
         Row<Measurement> row = new Row<Measurement>(Timestamp.fromEpochSeconds(0), res);
-        Measurement measurement = new Measurement(Timestamp.fromEpochSeconds(0), res, "", 0.0d);
+        Measurement measurement = new Measurement(Timestamp.fromEpochSeconds(0), res, label, 0.0d);
         row.addElement(measurement);
         results.addRow(row);
 
-        EasyMock.expect(sampleRepository.select(
-                EasyMock.eq(context), EasyMock.anyObject(), EasyMock.anyObject(), EasyMock.anyObject(), EasyMock.anyObject(), EasyMock.anyObject()
+        EasyMock.expect(m_sampleRepository.select(
+                EasyMock.eq(m_context), EasyMock.eq(res), EasyMock.anyObject(), EasyMock.anyObject(), EasyMock.anyObject(), EasyMock.anyObject()
                 )).andReturn(results);
 
-        EasyMock.replay(resourceDao, sampleRepository);
-
-        NewtsFetchStrategy nfs = new NewtsFetchStrategy();
-        nfs.setResourceDao(resourceDao);
-        nfs.setSampleRepository(sampleRepository);
-        nfs.setContext(context);
-
-        Source source = new Source();
+        final Source source = new Source();
         source.setAggregation("AVERAGE");
-        source.setAttribute("icmp");
-        source.setLabel("icmp");
-        source.setResourceId("nodeSource[NODES:1430502148137].responseTime[127.0.0.1]");
+        source.setAttribute(attr);
+        source.setLabel(label);
+        source.setResourceId(resourceId);
         source.setTransient(false);
-
-        List<Source> sources = Lists.newArrayList(source);
-
-        FetchResults fetchResults = nfs.fetch(1431047069000L - (60 * 60 * 1000), 1431047069000L, 300 * 1000, 0, sources);
-        assertEquals(1, fetchResults.getColumns().keySet().size());
-
-        EasyMock.verify(resourceDao, sampleRepository);
+        return source;
     }
 }
