7e01ad5c7afa7dfc7559843810bbceb9346c8001
Alejandro Galue <agalue@opennms.org>
2015-08-10 11:55:00 -0400

    Refactoring the JsonCollectorITCase class.
    Because the original implementation of JsonCollectorITCase was basically
    a copy/paste of XmlCollectorITCase with just a few modifications, I
    refactored JsonCollectorITCase to extends XmlCollectorITCase. The only
    difference was just one method.
    
    All the tests passes in Eclipse and from Command Line

--- protocols/xml/src/test/java/org/opennms/protocols/json/collector/JsonCollectorITCase.java
+++ protocols/xml/src/test/java/org/opennms/protocols/json/collector/JsonCollectorITCase.java
@@ -28,210 +28,21 @@
 
 package org.opennms.protocols.json.collector;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.io.FileUtils;
-import org.easymock.EasyMock;
-import org.jrobin.core.Datasource;
-import org.jrobin.core.RrdDb;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.TemporaryFolder;
-import org.opennms.core.test.MockLogAppender;
-import org.opennms.netmgt.collection.api.CollectionAgent;
-import org.opennms.netmgt.collection.api.CollectionSet;
-import org.opennms.netmgt.collection.api.CollectionSetVisitor;
-import org.opennms.netmgt.collection.api.ServiceCollector;
-import org.opennms.netmgt.collection.api.ServiceParameters;
-import org.opennms.netmgt.collection.persistence.rrd.RrdPersisterFactory;
-import org.opennms.netmgt.dao.support.FilesystemResourceStorageDao;
-import org.opennms.netmgt.events.api.EventProxy;
-import org.opennms.netmgt.rrd.RrdRepository;
-import org.opennms.netmgt.rrd.RrdStrategy;
-import org.opennms.netmgt.rrd.jrobin.JRobinRrdStrategy;
-import org.opennms.protocols.xml.collector.XmlCollector;
-import org.opennms.protocols.xml.config.XmlRrd;
-import org.opennms.protocols.xml.dao.jaxb.XmlDataCollectionConfigDaoJaxb;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
+import org.opennms.protocols.xml.collector.XmlCollectorITCase;
 
 /**
  * The Abstract Class for Testing the JSON Collector.
- * <p>This class is very similar to AbstractXmlCollectorTest.</p>
- * <p>TODO: refactor this class to extend AbstractXmlCollectorTest.</p>
  * 
- * @author <a href="mailto:ronald.roskens@gmail.com">Ronald Roskens</a>
  * @author <a href="mailto:agalue@opennms.org">Alejandro Galue</a>
  */
-public abstract class JsonCollectorITCase {
-
-    @Rule
-    public TemporaryFolder m_temporaryFolder = new TemporaryFolder();
-
-    /** The collection agent. */
-    private CollectionAgent m_collectionAgent;
-
-    /** The event proxy. */
-    private EventProxy m_eventProxy;
-
-    /** The XML collection DAO. */
-    private XmlDataCollectionConfigDaoJaxb m_xmlCollectionDao;
-
-    private RrdStrategy<?, ?> m_rrdStrategy;
-
-    private FilesystemResourceStorageDao m_resourceStorageDao;
-
-    private RrdPersisterFactory m_persisterFactory;
+public abstract class JsonCollectorITCase extends XmlCollectorITCase {
 
     /**
-     * Sets the up.
-     *
-     * @throws Exception the exception
+     * Initialize document builder.
      */
-    @Before
-    public void setUp() throws Exception {
-        MockLogAppender.setupLogging();
-
-        initializeRrdStrategy();
-
-        m_collectionAgent = EasyMock.createMock(CollectionAgent.class);
-        EasyMock.expect(m_collectionAgent.getNodeId()).andReturn(1).anyTimes();
-        EasyMock.expect(m_collectionAgent.getHostAddress()).andReturn("127.0.0.1").anyTimes();
-        EasyMock.expect(m_collectionAgent.getStorageDir()).andReturn(new File(String.valueOf(1))).anyTimes();
-        m_eventProxy = EasyMock.createMock(EventProxy.class);
-
-        m_xmlCollectionDao = new XmlDataCollectionConfigDaoJaxb();
-        Resource resource = new FileSystemResource(getJSONConfigFileName());
-        m_xmlCollectionDao.setConfigResource(resource);
-        m_xmlCollectionDao.afterPropertiesSet();
-        MockDocumentBuilder.setJSONFileName(getJSONSampleFileName());
-
-        EasyMock.replay(m_collectionAgent, m_eventProxy);
+    @Override
+    protected void initializeDocumentBuilder() {
+        MockDocumentBuilder.setJSONFileName(getSampleFileName());
     }
 
-    /**
-     * Initialize RRD strategy.
-     *
-     * @throws Exception the exception
-     */
-    protected void initializeRrdStrategy() throws Exception {
-        m_rrdStrategy = new JRobinRrdStrategy();
-        m_resourceStorageDao = new FilesystemResourceStorageDao();
-        m_resourceStorageDao.setRrdDirectory(m_temporaryFolder.getRoot());
-        m_temporaryFolder.newFolder("snmp");
-
-        m_persisterFactory = new RrdPersisterFactory();
-        m_persisterFactory.setResourceStorageDao(m_resourceStorageDao);
-        m_persisterFactory.setRrdStrategy(m_rrdStrategy);
-    }
-
-    /**
-     * Gets the RRD extension.
-     *
-     * @return the RRD extension
-     */
-    protected String getRrdExtension() {
-        return "jrb";
-    }
-
-    /**
-     * Gets the XML configuration DAO.
-     *
-     * @return the XML configuration DAO
-     */
-    public XmlDataCollectionConfigDaoJaxb getConfigDao() {
-        return m_xmlCollectionDao;
-    }
-
-    /**
-     * Gets the test JSON sample file name.
-     *
-     * @return the test JSON sample file name
-     */
-    public abstract String getJSONSampleFileName();
-
-    /**
-     * Gets the test JSON configuration file name.
-     *
-     * @return the test JSON configuration file name
-     */
-    public abstract String getJSONConfigFileName();
-
-    /**
-     * Tear down.
-     *
-     * @throws Exception the exception
-     */
-    @After
-    public void tearDown() throws Exception {
-        EasyMock.verify(m_collectionAgent, m_eventProxy);
-        MockLogAppender.assertNoWarningsOrGreater();
-    }
-
-    /**
-     * Executes collector test.
-     *
-     * @param parameters the parameters
-     * @param expectedFiles the expected amount of JRB files
-     * @throws Exception the exception
-     */
-    public void executeCollectorTest(Map<String, Object> parameters, int expectedFiles) throws Exception {
-        XmlCollector collector = new XmlCollector();
-        collector.setXmlCollectionDao(m_xmlCollectionDao);
-        collector.initialize(m_collectionAgent, parameters);
-        CollectionSet collectionSet = collector.collect(m_collectionAgent, m_eventProxy, parameters);
-        collector.release(m_collectionAgent);
-        Assert.assertEquals(ServiceCollector.COLLECTION_SUCCEEDED, collectionSet.getStatus());
-
-        ServiceParameters serviceParams = new ServiceParameters(new HashMap<String,Object>());
-        CollectionSetVisitor persister = m_persisterFactory.createGroupPersister(serviceParams, createRrdRepository((String)parameters.get("collection")), false, false);
-        collectionSet.visit(persister);
-
-        Assert.assertEquals(expectedFiles, FileUtils.listFiles(getSnmpRoot(), new String[] { getRrdExtension() }, true).size());
-    }
-
-    /**
-     * Validates a JRB.
-     * <p>It assumes storeByGroup=true</p>
-     * 
-     * @param file the JRB file instance
-     * @param dsnames the array of data source names
-     * @param dsvalues the array of data source values
-     * @throws Exception the exception
-     */
-    public void validateJrb(File file, String[] dsnames, Double[] dsvalues) throws Exception {
-        Assert.assertTrue(file.exists());
-        RrdDb jrb = new RrdDb(file);
-        Assert.assertEquals(dsnames.length, jrb.getDsCount());
-        for (int i = 0; i < dsnames.length; i++) {
-            Datasource ds = jrb.getDatasource(dsnames[i]);
-            Assert.assertNotNull(ds);
-            Assert.assertEquals(dsvalues[i], Double.valueOf(ds.getLastValue()));
-        }
-    }
-
-    /**
-     * Creates the RRD repository.
-     *
-     * @return the RRD repository
-     * @throws IOException Signals that an I/O exception has occurred.
-     */
-    private RrdRepository createRrdRepository(String collection) throws IOException {
-        XmlRrd rrd = m_xmlCollectionDao.getDataCollectionByName(collection).getXmlRrd();
-        RrdRepository repository = new RrdRepository();
-        repository.setRrdBaseDir(getSnmpRoot());
-        repository.setHeartBeat(rrd.getStep() * 2);
-        repository.setStep(rrd.getStep());
-        repository.setRraList(rrd.getXmlRras());
-        return repository;
-    }
-
-    public File getSnmpRoot() {
-        return new File(m_temporaryFolder.getRoot(), "snmp");
-    }
 }
