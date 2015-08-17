82cd8f6a9838ec4cc1b5c2dc17ee3b4e456defcd
Benjamin Reed <ranger@opennms.org>
2015-08-17 11:25:44 -0400

    HZN-381 default graph engine is "auto", uses backshift if backend is "newts"

--- features/measurements/impl/src/main/java/org/opennms/netmgt/measurements/impl/JRrd2FetchStrategyProvider.java
+++ features/measurements/impl/src/main/java/org/opennms/netmgt/measurements/impl/JRrd2FetchStrategyProvider.java
@@ -28,15 +28,15 @@
 
 package org.opennms.netmgt.measurements.impl;
 
+import org.opennms.core.utils.TimeSeries;
 import org.opennms.netmgt.measurements.api.MeasurementFetchStrategy;
 import org.opennms.netmgt.measurements.api.MeasurementFetchStrategyProvider;
-import org.opennms.netmgt.rrd.RrdConfig;
 import org.opennms.netmgt.rrd.rrdtool.MultithreadedJniRrdStrategy;
 
 public class JRrd2FetchStrategyProvider implements MeasurementFetchStrategyProvider {
     @Override
     public Class<? extends MeasurementFetchStrategy> getStrategyClass(String timeSeriesStrategyName, String rrdStrategyClass) {
-        if(!RrdConfig.RRD_TIME_SERIES_STRATEGY_NAME.equalsIgnoreCase(timeSeriesStrategyName) ||
+        if(!TimeSeries.RRD_TIME_SERIES_STRATEGY_NAME.equalsIgnoreCase(timeSeriesStrategyName) ||
                 !MultithreadedJniRrdStrategy.class.getCanonicalName().equals(rrdStrategyClass)) {
             return null;
         }
