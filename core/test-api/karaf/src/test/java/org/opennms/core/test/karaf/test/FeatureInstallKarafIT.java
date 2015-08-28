55358cdfd15f13d178c10a49b3440f14b1b8350e
Seth Leger <seth@opennms.org>
2015-08-28 12:19:42 -0400

    Added all OpenNMS-related Karaf features to the test repo in core/test-api/karaf and added integration tests for installing most of the OpenNMS features. There are still some dependency issues with web-related features and more OpenNMS stuff has yet to be added.

--- core/test-api/karaf/src/test/java/org/opennms/core/test/karaf/test/FeatureInstallKarafIT.java
+++ core/test-api/karaf/src/test/java/org/opennms/core/test/karaf/test/FeatureInstallKarafIT.java
@@ -28,6 +28,8 @@
 
 package org.opennms.core.test.karaf.test;
 
+import static org.ops4j.pax.exam.CoreOptions.maven;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.core.test.karaf.KarafTestCase;
@@ -35,46 +37,138 @@ import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerMethod;
 
-/**
- * This tests checks if features are defined properly and can be installed
- * without any other features/bundles installed by default.
- */
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerMethod.class)
 public class FeatureInstallKarafIT extends KarafTestCase {
 
+    /**
+     * This test attempts to install all features from the standard Karaf
+     * features.xml.
+     */
     @Test
     public void testInstallAllStandardFeatures() {
         installFeature("admin");
         installFeature("aries-blueprint");
         installFeature("aries-proxy");
-        //installFeature("blueprint-web");
+        installFeature("blueprint-web");
         installFeature("config");
         installFeature("deployer");
         installFeature("diagnostic");
         installFeature("eventadmin");
         installFeature("features");
-        //installFeature("framework-security");
-        //installFeature("http");
-        //installFeature("http-whiteboard");
+        installFeature("http");
+        installFeature("http-whiteboard");
         installFeature("jaas");
-        //installFeature("jasypt-encryption");
-        //installFeature("jetty");
+        installFeature("jasypt-encryption");
+        installFeature("jetty");
         installFeature("karaf-framework");
         installFeature("kar");
         installFeature("management");
-        //installFeature("obr");
+        installFeature("obr");
         //installFeature("scr");
         installFeature("service-security");
         installFeature("service-wrapper");
         installFeature("shell");
         installFeature("ssh");
-        //installFeature("war");
-        //installFeature("webconsole");
+        installFeature("war");
+        installFeature("webconsole");
         installFeature("wrap");
         installFeature("wrapper");
 
         System.out.println(executeCommand("features:list"));
     }
 
+    /**
+     * This test attempts to install all features from the Spring Karaf
+     * features.xml.
+     */
+    @Test
+    public void testInstallAllSpringFeatures() {
+        installFeature("spring");
+        installFeature("spring-aspects");
+        installFeature("spring-instrument");
+        installFeature("spring-jdbc");
+        installFeature("spring-jms");
+        //installFeature("spring-test");
+        installFeature("spring-orm");
+        installFeature("spring-oxm");
+        installFeature("spring-tx");
+        //installFeature("spring-web");
+        //installFeature("spring-web-portlet");
+        //installFeature("spring-websocket");
+        //installFeature("spring-security");
+
+        System.out.println(executeCommand("features:list"));
+    }
+
+    /**
+     * This test attempts to install all features from the OpenNMS
+     * features.xml.
+     */
+    @Test
+    public void testInstallAllOpenNMSFeatures() {
+        addFeaturesUrl(maven().groupId("org.opennms.container").artifactId("karaf").version("17.0.0-SNAPSHOT").type("xml").classifier("features").getURL());
+        addFeaturesUrl(maven().groupId("org.opennms.karaf").artifactId("opennms").version("17.0.0-SNAPSHOT").type("xml").classifier("features").getURL());
+
+        installFeature("atomikos");
+        installFeature("batik");
+        installFeature("c3p0");
+        installFeature("castor");
+        installFeature("commons-beanutils");
+        installFeature("commons-cli");
+        installFeature("commons-codec");
+        installFeature("commons-collections");
+        //installFeature("commons-configuration");
+        installFeature("commons-digester");
+        installFeature("commons-exec");
+        installFeature("commons-io");
+        installFeature("commons-jexl");
+        installFeature("commons-lang");
+        installFeature("commons-net");
+        installFeature("dnsjava");
+        installFeature("fop");
+        installFeature("guava");
+        installFeature("hibernate36");
+        installFeature("hibernate-validator41");
+        installFeature("jaxb");
+        installFeature("jersey-client");
+        installFeature("jfreechart");
+        //installFeature("jolokia");
+        installFeature("jrobin");
+        installFeature("json-lib");
+        installFeature("opennms-activemq-config");
+        //installFeature("opennms-activemq");
+        installFeature("opennms-activemq-dispatcher-config");
+        //installFeature("opennms-activemq-dispatcher");
+        //installFeature("opennms-activemq-event-forwarder");
+        //installFeature("opennms-activemq-event-receiver");
+        installFeature("opennms-collection-api");
+        installFeature("opennms-collection-persistence-rrd");
+        installFeature("opennms-config-api");
+        installFeature("opennms-config");
+        installFeature("opennms-config-jaxb");
+        installFeature("opennms-core-daemon");
+        installFeature("opennms-core-db");
+        installFeature("opennms-core");
+        installFeature("opennms-core-web");
+        installFeature("opennms-dao-api");
+        installFeature("opennms-dao");
+        installFeature("opennms-events-api");
+        installFeature("opennms-events-daemon");
+        installFeature("opennms-events-traps");
+        installFeature("opennms-icmp-api");
+        //installFeature("opennms-javamail");
+        installFeature("opennms-model");
+        installFeature("opennms-poller-api");
+        //installFeature("opennms-provisioning");
+        //installFeature("opennms-reporting");
+        installFeature("opennms-rrd-api");
+        installFeature("opennms-rrd-jrobin");
+        installFeature("opennms-snmp");
+        //installFeature("opennms-webapp");
+        installFeature("org.json");
+        installFeature("postgresql");
+
+        System.out.println(executeCommand("features:list"));
+    }
 }
