13a0df5690a283dbdeeee94c49112a3947d62acc
Jesse White <jesse@opennms.org>
2015-09-02 17:28:00 -0400

    Make the EventWriter actually send events.

--- features/discovery/src/test/java/org/opennms/netmgt/discovery/DiscoveryRoutingTest.java
+++ features/discovery/src/test/java/org/opennms/netmgt/discovery/DiscoveryRoutingTest.java
@@ -1,6 +1,7 @@
 package org.opennms.netmgt.discovery;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.util.ArrayList;
 
 import org.apache.camel.builder.AdviceWithRouteBuilder;
@@ -12,8 +13,15 @@ import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.core.test.OpenNMSJUnit4ClassRunner;
+import org.opennms.core.utils.InetAddressUtils;
 import org.opennms.netmgt.discovery.messages.DiscoveryJob;
 import org.opennms.netmgt.discovery.messages.DiscoveryResults;
+import org.opennms.netmgt.events.api.EventConstants;
+import org.opennms.netmgt.events.api.EventIpcManagerFactory;
+import org.opennms.netmgt.icmp.EchoPacket;
+import org.opennms.netmgt.model.events.EventBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 
 import com.google.common.collect.Lists;
@@ -34,11 +42,32 @@ public class DiscoveryRoutingTest extends CamelTestSupport
 
     static public class EventWriter
     {
-        public void writeEvent( DiscoveryResults results )
+        private static final Logger LOG = LoggerFactory.getLogger(EventWriter.class);
+
+        public void sendEvents( DiscoveryResults results )
         {
-            System.out.println( results );
+            results.getResponses().entrySet()
+                .forEach(e -> sendNewSuspectEvent(e.getKey(), e.getValue(),results.getForeignSource()));
         }
 
+        private void sendNewSuspectEvent(InetAddress address, EchoPacket response, String foreignSource) {
+            EventBuilder eb = new EventBuilder(EventConstants.NEW_SUSPECT_INTERFACE_EVENT_UEI, "OpenNMS.Discovery");
+            eb.setInterface(address);
+            eb.setHost(InetAddressUtils.getLocalHostName());
+
+            eb.addParam("RTT", response.getReceivedTimeNanos() - response.getSentTimeNanos());
+
+            if (foreignSource != null) {
+                eb.addParam("foreignSource", foreignSource);
+            }
+
+            try {
+                EventIpcManagerFactory.getIpcManager().sendNow(eb.getEvent());
+                LOG.debug("Sent event: {}", EventConstants.NEW_SUSPECT_INTERFACE_EVENT_UEI);
+            } catch (Throwable t) {
+                LOG.warn("run: unexpected throwable exception caught during send to middleware", t);
+            }
+        }
     }
 
     @Override
