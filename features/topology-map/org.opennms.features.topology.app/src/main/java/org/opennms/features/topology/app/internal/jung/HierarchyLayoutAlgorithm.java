04072353020061f5736e777531ef7f2f65581ada
Murat Yildiz <murat_yildiz@gmx.net>
2015-11-05 12:10:54 +0100

    BSM-51: Implement hierarchy layout for topology map

---
+++ features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/jung/HierarchyLayoutAlgorithm.java
@@ -0,0 +1,89 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ * <p>
+ * Copyright (C) 2015 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2015 The OpenNMS Group, Inc.
+ * <p>
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ * <p>
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * <p>
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ * http://www.gnu.org/licenses/
+ * <p>
+ * For more information contact:
+ * OpenNMS(R) Licensing <license@opennms.org>
+ * http://www.opennms.org/
+ * http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.features.topology.app.internal.jung;
+
+
+import java.awt.geom.Point2D;
+import java.util.Collection;
+
+import edu.uci.ics.jung.algorithms.layout.TreeLayout;
+import edu.uci.ics.jung.algorithms.shortestpath.MinimumSpanningForest;
+import edu.uci.ics.jung.graph.DelegateForest;
+import edu.uci.ics.jung.graph.Forest;
+import edu.uci.ics.jung.graph.SparseGraph;
+import org.opennms.features.topology.api.Graph;
+import org.opennms.features.topology.api.GraphContainer;
+import org.opennms.features.topology.api.Layout;
+import org.opennms.features.topology.api.topo.Edge;
+import org.opennms.features.topology.api.topo.Vertex;
+import org.opennms.features.topology.api.topo.VertexRef;
+
+public class HierarchyLayoutAlgorithm extends AbstractLayoutAlgorithm {
+    private static final int HORIZONTAL_SPACING = 60;
+    private static final int VERTICAL_SPACING = 60;
+
+    @Override
+    public void updateLayout(final GraphContainer graphContainer) {
+        final Graph g = graphContainer.getGraph();
+        final Layout graphLayout = g.getLayout();
+        final edu.uci.ics.jung.algorithms.layout.Layout<VertexRef, Edge> treeLayout = createTreeLayout(g);
+
+        treeLayout.setInitializer(initializer(g.getLayout()));
+
+        transformLayout(g.getDisplayVertices(), treeLayout, graphLayout);
+    }
+
+    private edu.uci.ics.jung.graph.Graph<VertexRef, Edge> convert(final Graph g) {
+        final SparseGraph<VertexRef, Edge> sparseGraph = new SparseGraph<VertexRef, Edge>();
+
+        for(VertexRef v : g.getDisplayVertices()) {
+            sparseGraph.addVertex(v);
+        }
+        for(Edge e : g.getDisplayEdges()) {
+            sparseGraph.addEdge(e, e.getSource().getVertex(), e.getTarget().getVertex());
+        }
+        return sparseGraph;
+    }
+
+    private void transformLayout(final Collection<? extends Vertex> vertices, final edu.uci.ics.jung.algorithms.layout.Layout<VertexRef, Edge> layout, final Layout graphLayout) {
+        Point2D p;
+        for(VertexRef v : vertices) {
+            p = (Point2D) layout.transform(v);
+            graphLayout.setLocation(v, p.getX(), p.getY());
+        }
+    }
+
+    public Forest createMinForest(final Graph g) {
+        return new MinimumSpanningForest(convert(g), new DelegateForest(), null).getForest();
+    }
+
+    public edu.uci.ics.jung.algorithms.layout.Layout<VertexRef, Edge> createTreeLayout(final Graph g) {
+        return new TreeLayout<VertexRef, Edge>(createMinForest(g));
+    }
+}
