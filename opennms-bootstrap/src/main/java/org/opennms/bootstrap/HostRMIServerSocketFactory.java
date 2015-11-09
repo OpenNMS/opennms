bef3c67a647d2412733e3f00052720a3c1351297
Benjamin Reed <ranger@opennms.org>
2015-11-09 11:35:58 -0500

    NMS-7971 only listen for RMI on localhost by default
    This can be overridden by setting the property
    opennms.poller.server.serverHost=<hostname or IP>
    
    The old behavior can be restored by setting
    opennms.poller.server.serverHost=0.0.0.0

---
+++ opennms-bootstrap/src/main/java/org/opennms/bootstrap/HostRMIServerSocketFactory.java
@@ -0,0 +1,43 @@
+package org.opennms.bootstrap;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.rmi.server.RMISocketFactory;
+
+public class HostRMIServerSocketFactory extends RMISocketFactory implements RMIServerSocketFactory, RMIClientSocketFactory {
+    private String m_host;
+
+    public HostRMIServerSocketFactory() {
+    }
+
+    public HostRMIServerSocketFactory(final String host) {
+        m_host = host;
+    }
+
+    @Override
+    public ServerSocket createServerSocket(final int port) throws IOException {
+        final InetAddress address;
+        if ("localhost".equals(m_host)) {
+            address = InetAddress.getLoopbackAddress();
+        } else {
+            address = InetAddress.getByName(m_host);
+        }
+        return new ServerSocket(port, -1, address);
+    }
+
+    @Override
+    public Socket createSocket(final String host, final int port) throws IOException {
+        return RMISocketFactory.getDefaultSocketFactory().createSocket(host, port);
+    }
+
+    public String getHost() {
+        return m_host;
+    }
+    public void setHost(final String host) {
+        m_host = host;
+    }
+}
