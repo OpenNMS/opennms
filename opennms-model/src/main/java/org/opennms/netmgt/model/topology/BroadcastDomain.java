084dfd09b951a99cdabf29f5c21bf54301c5cb13
Antonio <rssntn67@yahoo.it>
2015-12-03 21:38:31 +0100

    NMS-7918: slow topology bridge discover
     Created the BridgeTopolgy Object Model
     Fixed a typo in applicationContext:
     the reference to TopologyBridgeDao
     was wrong

---
+++ opennms-model/src/main/java/org/opennms/netmgt/model/topology/BroadcastDomain.java
@@ -0,0 +1,10 @@
+package org.opennms.netmgt.model.topology;
+
+import java.util.List;
+import java.util.Set;
+
+public class BroadcastDomain {
+
+    List<Bridge> m_bridges;
+    Set<String> m_macsOnDomain;
+}
---
+++ opennms-model/src/main/java/org/opennms/netmgt/model/topology/SharedSegment.java
@@ -0,0 +1,11 @@
+package org.opennms.netmgt.model.topology;
+
+import java.util.List;
+import java.util.Set;
+
+public class SharedSegment {
+
+    List<BridgePort> m_bridgePortsOnSegment;
+    Set<String> m_macsOnSegment;
+    
+}
