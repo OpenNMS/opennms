98d47e049b388d623df3a4470a9c823e18e21f7d
Benjamin Reed <ranger@opennms.org>
2015-12-17 11:42:13 -0500

    separate authentication from general gui-ness

--- features/remote-poller/src/main/java/org/opennms/poller/remote/GroovyGui.java
+++ features/remote-poller/src/main/java/org/opennms/poller/remote/GroovyGui.java
@@ -35,16 +35,5 @@ package org.opennms.poller.remote;
  * @version $Id: $
  */
 public interface GroovyGui {
-
-    /**
-     * <p>getAuthenticationBean</p>
-     *
-     * @return a {@link org.opennms.poller.remote.AuthenticationBean} object.
-     */
-    public AuthenticationBean getAuthenticationBean();
-    /**
-     * <p>createAndShowGui</p>
-     */
     public void createAndShowGui();
-
 }
