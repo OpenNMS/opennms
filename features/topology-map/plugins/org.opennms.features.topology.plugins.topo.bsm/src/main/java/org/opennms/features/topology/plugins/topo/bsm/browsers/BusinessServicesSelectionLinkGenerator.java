5fbcc44119d272014835e9c5d2705c74613f7c41
Jesse White <jesse@opennms.org>
2016-01-11 16:25:47 -0500

    Cleanup the business service selection table code.

--- features/topology-map/plugins/org.opennms.features.topology.plugins.topo.bsm/src/main/java/org/opennms/features/topology/plugins/topo/bsm/browsers/BusinessServicesSelectionLinkGenerator.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.topo.bsm/src/main/java/org/opennms/features/topology/plugins/topo/bsm/browsers/BusinessServicesSelectionLinkGenerator.java
@@ -32,11 +32,7 @@ import org.opennms.features.topology.api.GraphContainer;
 import org.opennms.features.topology.api.WidgetContext;
 import org.opennms.features.topology.plugins.browsers.ToStringColumnGenerator;
 import org.opennms.features.topology.plugins.topo.bsm.BusinessServiceCriteria;
-import org.opennms.netmgt.bsm.persistence.api.BusinessService;
-import org.opennms.netmgt.bsm.persistence.api.BusinessServiceDao;
 import org.opennms.netmgt.bsm.service.BusinessServiceManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.vaadin.data.Property;
 import com.vaadin.ui.Button;
@@ -44,71 +40,70 @@ import com.vaadin.ui.Table;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.themes.BaseTheme;
 
+/**
+ * TODO: MVR this is (almost) the same code as ApplicationSElectionLinkGenerator
+ * -> generalize
+ */
 public class BusinessServicesSelectionLinkGenerator implements Table.ColumnGenerator {
     private static final long serialVersionUID = 1L;
-    private static final Logger LOG = LoggerFactory.getLogger(BusinessServicesSelectionLinkGenerator.class);
 
-    private BusinessServiceDao m_businessServiceDao;
     private BusinessServiceManager m_businessServiceManager;
 
     private final String m_idPropertyName;
+    private final String m_labelPropertyName;
     private final Table.ColumnGenerator m_columnGenerator;
 
-    public BusinessServicesSelectionLinkGenerator(String idPropertyName) {
-		m_idPropertyName = idPropertyName;
-		m_columnGenerator = new ToStringColumnGenerator();
-	}
+    public BusinessServicesSelectionLinkGenerator(String idPropertyName, String labelPropertyName) {
+        m_idPropertyName = idPropertyName;
+        m_labelPropertyName = labelPropertyName;
+        m_columnGenerator = new ToStringColumnGenerator();
+    }
 
-	@Override
-	public Object generateCell(final Table source, final Object itemId, Object columnId) {
-		// TODO MVR this is (almost) the same code as ApplicationSElectionLinkGenerator -> generalize
-		@SuppressWarnings("unchecked")
-		final Property<Long> idProperty = source.getContainerProperty(itemId, m_idPropertyName);
-		Object cellValue = m_columnGenerator.generateCell(source, itemId, columnId);
-		if (cellValue == null) {
-			return null;
-		} else {
-			if (idProperty.getValue() == null) {
-				return cellValue;
-			} else {
-				Button button = new Button(cellValue.toString());
-				button.setStyleName(BaseTheme.BUTTON_LINK);
-				button.setDescription(idProperty.getValue().toString());
-				button.addClickListener(new Button.ClickListener() {
-				private static final long serialVersionUID = 1L;
+    @Override
+    @SuppressWarnings("unchecked")
+    public Object generateCell(final Table source, final Object itemId, Object columnId) {
+        final Property<Long> idProperty = source.getContainerProperty(itemId, m_idPropertyName);
+        final Property<String> labelProperty = source.getContainerProperty(itemId, m_labelPropertyName);
 
-					@Override
-					public void buttonClick(Button.ClickEvent event) {
-					    // Retrieve the associated business service, we need it's name
-					    // TODO: Can we get this directly from the table instead of issuing another DAO call?
-					    String businessServiceId = String.valueOf(idProperty.getValue());
-                        BusinessService businessService = m_businessServiceDao.get(Long.parseLong(businessServiceId));
-                        if (businessService == null) {
-                            LOG.warn("Could not find business service with id: {}", businessServiceId);
-                        }
+        final Object cellValue = m_columnGenerator.generateCell(source, itemId, columnId);
+        if (cellValue == null) {
+            return null;
+        } else {
+            if (idProperty.getValue() == null) {
+                return cellValue;
+            } else {
+                Button button = new Button(cellValue.toString());
+                button.setStyleName(BaseTheme.BUTTON_LINK);
+                button.setDescription(idProperty.getValue().toString());
+                button.addClickListener(new Button.ClickListener() {
+                    private static final long serialVersionUID = 1L;
 
-						// Retrieve the graph container associated with the current application context
-						UI ui = UI.getCurrent();
-						WidgetContext context = (WidgetContext)ui;
-						GraphContainer graphContainer = context.getGraphContainer();
+                    @Override
+                    public void buttonClick(Button.ClickEvent event) {
+                        String businessServiceId = String.valueOf(idProperty.getValue());
+                        String businessServiceLabel = labelProperty.getValue();
 
-						// Add criteria use to filter for the selected application
-						BusinessServiceCriteria businessServiceCriteria = new BusinessServiceCriteria(businessServiceId, businessService.getName(), m_businessServiceManager);
-						graphContainer.addCriteria(businessServiceCriteria);
+                        // Retrieve the graph container associated with the
+                        // current application context
+                        UI ui = UI.getCurrent();
+                        WidgetContext context = (WidgetContext) ui;
+                        GraphContainer graphContainer = context.getGraphContainer();
+
+                        // Add criteria use to filter for the selected
+                        // application
+                        BusinessServiceCriteria businessServiceCriteria = new BusinessServiceCriteria(businessServiceId,
+                                businessServiceLabel, m_businessServiceManager);
+                        graphContainer.addCriteria(businessServiceCriteria);
                         graphContainer.setDirty(true);
                         graphContainer.redoLayout();
-					}
-				});
-				return button;
-			}
-		}
-	}
+                    }
+                });
+                return button;
+            }
+        }
+    }
 
-	public void setBusinessServiceDao(BusinessServiceDao businessServiceDao) {
-	    m_businessServiceDao = businessServiceDao;
-	}
-
-	public void setBusinessServiceManager(BusinessServiceManager businessServiceManager) {
-	    m_businessServiceManager = businessServiceManager;
-	}
+    public void setBusinessServiceManager(BusinessServiceManager businessServiceManager) {
+        m_businessServiceManager = businessServiceManager;
+    }
 }
