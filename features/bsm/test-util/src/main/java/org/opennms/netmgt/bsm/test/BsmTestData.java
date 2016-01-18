99003b183db2c001475e3b2fa193e33dfd26c36b
Markus von Rüden <mvr@opennms.com>
2016-01-18 11:16:27 -0500

    BSM-61: Consider the hierarchy in the Business State Machine

---
+++ features/bsm/test-util/src/main/java/org/opennms/netmgt/bsm/test/BsmTestData.java
@@ -0,0 +1,86 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ * <p>
+ * Copyright (C) 2016 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2016 The OpenNMS Group, Inc.
+ * <p>
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ * <p>
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * <p>
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ * http://www.gnu.org/licenses/
+ * <p>
+ * For more information contact:
+ * OpenNMS(R) Licensing <license@opennms.org>
+ * http://www.opennms.org/
+ * http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.netmgt.bsm.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+import org.opennms.core.utils.InetAddressUtils;
+import org.opennms.netmgt.bsm.persistence.api.BusinessServiceEntity;
+import org.opennms.netmgt.model.OnmsMonitoredService;
+
+public class BsmTestData {
+
+    private List<BusinessServiceEntity> businessServices = new ArrayList<>();
+
+    public BsmTestData(BusinessServiceEntity... entities) {
+        if (entities != null) {
+            for (BusinessServiceEntity eachEntity : entities) {
+                businessServices.add(eachEntity);
+            }
+        }
+    }
+
+    public int getServiceCount() {
+        return businessServices.size();
+    }
+
+    public List<BusinessServiceEntity> getServices() {
+        return Collections.unmodifiableList(businessServices);
+    }
+
+    public BusinessServiceEntity findByName(final String name) {
+        List<BusinessServiceEntity> entities = businessServices.stream().filter(s -> name.equals(s.getName())).collect(Collectors.toList());
+        if (entities.isEmpty()) {
+            throw new NoSuchElementException("No Business Service with name " + name + " found in Test Data");
+        }
+        if (entities.size() > 1) {
+            throw new IllegalArgumentException("More than one Business Service with name " + name + " found in Test Data");
+        }
+        return entities.get(0);
+    }
+
+    public OnmsMonitoredService findIpService(String ipAddress, String serviceName) {
+        List<OnmsMonitoredService> entities = businessServices
+                .stream()
+                .flatMap(eachEntity -> eachEntity.getIpServices().stream())
+                .filter(eachService -> InetAddressUtils.addr(ipAddress).equals(eachService.getIpAddress()) && eachService.getServiceName().equals(serviceName))
+                .collect(Collectors.toList());
+        if (entities.isEmpty()) {
+            throw new NoSuchElementException("No IpService with criteria found");
+        }
+        if (entities.size() > 1) {
+            throw new IllegalArgumentException("More than one IpService with criteria found");
+        }
+        return entities.get(0);
+    }
+}
---
+++ features/bsm/test-util/src/main/java/org/opennms/netmgt/bsm/test/BsmTestUtils.java
@@ -0,0 +1,145 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ * <p>
+ * Copyright (C) 2016 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2016 The OpenNMS Group, Inc.
+ * <p>
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ * <p>
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ * <p>
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ * http://www.gnu.org/licenses/
+ * <p>
+ * For more information contact:
+ * OpenNMS(R) Licensing <license@opennms.org>
+ * http://www.opennms.org/
+ * http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.netmgt.bsm.test;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.opennms.core.xml.JaxbUtils;
+import org.opennms.netmgt.bsm.persistence.api.BusinessServiceEntity;
+import org.opennms.netmgt.events.api.EventConstants;
+import org.opennms.netmgt.model.OnmsAlarm;
+import org.opennms.netmgt.model.OnmsMonitoredService;
+import org.opennms.netmgt.model.OnmsSeverity;
+import org.opennms.web.rest.v2.bsm.model.BusinessServiceRequestDTO;
+
+import com.google.common.base.Throwables;
+
+/**
+ * Helper class to create all kinds of objects for writing BSM tests.
+ */
+public class BsmTestUtils {
+
+    public static BusinessServiceRequestDTO toRequestDto(BusinessServiceEntity input) {
+        BusinessServiceRequestDTO request = new BusinessServiceRequestDTO();
+        request.setName(input.getName());
+        request.setAttributes(new HashMap<>(input.getAttributes()));
+        request.setChildServices(input.getChildServices().stream().map(s -> s.getId()).collect(Collectors.toSet()));
+        request.setIpServices(input.getIpServices().stream().map(s -> s.getId()).collect(Collectors.toSet()));
+        return request;
+    }
+
+    // convert to json
+    public static String toJson(BusinessServiceRequestDTO request) {
+        try {
+            return new ObjectMapper().writeValueAsString(request);
+        } catch (IOException io) {
+            throw Throwables.propagate(io);
+        }
+    }
+
+    // convert to xml
+    public static String toXml(BusinessServiceRequestDTO request) {
+        return JaxbUtils.marshal(request);
+    }
+
+    public static OnmsAlarm createAlarm(OnmsMonitoredService monitoredService, OnmsSeverity severity) {
+        return createAlarm(
+                Objects.requireNonNull(monitoredService.getNodeId()),
+                Objects.requireNonNull(monitoredService.getIpAddress().toString()).substring(1),
+                Objects.requireNonNull(monitoredService.getServiceName()),
+                Objects.requireNonNull(severity));
+    }
+
+    private static OnmsAlarm createAlarm(int nodeId, String ip, String service, OnmsSeverity severity) {
+        OnmsAlarm alarm = new OnmsAlarm();
+        alarm.setUei(EventConstants.NODE_LOST_SERVICE_EVENT_UEI);
+        alarm.setSeverity(severity);
+        alarm.setReductionKey(String.format("%s::%s:%s:%s", EventConstants.NODE_LOST_SERVICE_EVENT_UEI, nodeId, ip, service));
+        return alarm;
+    }
+
+    public static OnmsMonitoredService createService(final int nodeId, final String ipAddress, final String serviceName) {
+        return new OnmsMonitoredService() {
+            private static final long serialVersionUID = 8510675581667310365L;
+
+            public Integer getNodeId() {
+                return nodeId;
+            }
+
+            public InetAddress getIpAddress() {
+                try {
+                    return InetAddress.getByName(ipAddress);
+                } catch (UnknownHostException e) {
+                    throw Throwables.propagate(e);
+                }
+            }
+
+            public String getServiceName() {
+                return serviceName;
+            }
+
+            public String toString() {
+                return getServiceName();
+            }
+        };
+    }
+
+    // creates a simple hierarchy with 1 parent and 2 childs.
+    // Each children has one ip service atached. The parent has not.
+    public static BsmTestData createSimpleHierarchy() {
+        // Create a simple hierarchy
+        BusinessServiceEntity child1 = new BusinessServiceEntityBuilder()
+                .name("Child 1")
+                .addIpService(createService(1, "192.168.1.1", "ICMP"))
+                .toEntity();
+
+        BusinessServiceEntity child2 = new BusinessServiceEntityBuilder()
+                .name("Child 2")
+                .addIpService(createService(2, "192.168.1.2", "SNMP"))
+                .toEntity();
+
+        BusinessServiceEntity root = new BusinessServiceEntityBuilder()
+                .name("Parent")
+                .addChildren(child1)
+                .addChildren(child2)
+                .toEntity();
+
+        child1.getParentServices().add(root);
+        child2.getParentServices().add(root);
+
+        return new BsmTestData(child1, child2, root);
+    }
+
+}
