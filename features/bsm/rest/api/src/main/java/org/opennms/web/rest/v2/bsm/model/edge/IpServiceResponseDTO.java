58e61ee80beb15910bf4d4bbfae70047fdcc9e74
Markus von Rüden <mvr@opennms.com>
2016-02-02 15:05:29 +0100

    BSM-24: Fix license header formatting

--- features/bsm/rest/api/src/main/java/org/opennms/web/rest/v2/bsm/model/edge/IpServiceResponseDTO.java
+++ features/bsm/rest/api/src/main/java/org/opennms/web/rest/v2/bsm/model/edge/IpServiceResponseDTO.java
@@ -18,7 +18,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with OpenNMS(R).  If not, see:
- * http://www.gnu.org/licenses/
+ *      http://www.gnu.org/licenses/
  *
  * For more information contact:
  *     OpenNMS(R) Licensing <license@opennms.org>
