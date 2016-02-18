7bdc0640f6b23e7ca8131fd3bde848d922153e6c
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2016-02-18 09:47:23 +0100

    BSM-145: Changed input validators for key

--- features/bsm/vaadin-adminpage/src/main/java/org/opennms/netmgt/bsm/vaadin/adminpage/BusinessServiceEditWindow.java
+++ features/bsm/vaadin-adminpage/src/main/java/org/opennms/netmgt/bsm/vaadin/adminpage/BusinessServiceEditWindow.java
@@ -386,7 +386,7 @@ public class BusinessServiceEditWindow extends Window {
                     .withKeyFieldName("Key")
                     .withValueFieldName("Value")
                     .withCaption("Attribute")
-                    .withKey("Key")
+                    .withKey("")
                     .withValue("")
                     .withOkAction(new KeyValueInputDialogWindow.Action() {
                         @Override
@@ -395,10 +395,16 @@ public class BusinessServiceEditWindow extends Window {
                             refreshAttributes();
                         }
                     })
-                    .withKeyValidator(new AbstractStringValidator("String must not be empty") {
+                    .withKeyValidator(new AbstractStringValidator("Key must not be empty") {
                         @Override
                         protected boolean isValidValue(String value) {
-                            return !Strings.isNullOrEmpty(value) && !m_businessService.getAttributes().containsKey(value);
+                            return !Strings.isNullOrEmpty(value);
+                        }
+                    })
+                    .withKeyValidator(new AbstractStringValidator("Key must be unique") {
+                        @Override
+                        protected boolean isValidValue(String value) {
+                            return !m_businessService.getAttributes().containsKey(value);
                         }
                     });
             this.getUI().addWindow(keyValueInputDialogWindow);
@@ -424,12 +430,6 @@ public class BusinessServiceEditWindow extends Window {
                         public void execute(KeyValueInputDialogWindow window) {
                             m_businessService.getAttributes().put(window.getKey(), window.getValue());
                             refreshAttributes();
-                        }
-                    })
-                    .withKeyValidator(new AbstractStringValidator("String must not be empty") {
-                        @Override
-                        protected boolean isValidValue(String value) {
-                            return !Strings.isNullOrEmpty(value);
                         }
                     });
             this.getUI().addWindow(keyValueInputDialogWindow);
