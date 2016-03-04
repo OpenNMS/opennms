dec72d664bcdb42e5a6bebbd9da8b09fb6f51f37
Benjamin Reed <ranger@opennms.org>
2016-03-04 10:33:07 -0500

    17.1.1 branch prepped from last master release.  17.1.0 -> 17.1.1

--- opennms-webapp/gulpfile.js
+++ opennms-webapp/gulpfile.js
@@ -8,7 +8,7 @@ var paths = {
 	'jsp': 'src/main/webapp/**/*.jsp'
 };
 
-var opennmsHome = process.env.OPENNMS_HOME || '../target/opennms-17.1.0';
+var opennmsHome = process.env.OPENNMS_HOME || '../target/opennms-17.1.1-SNAPSHOT';
 gutil.log('gulp', 'OpenNMS Home: ' + opennmsHome);
 
 gulp.task('default', ['sass', 'jsp']);
