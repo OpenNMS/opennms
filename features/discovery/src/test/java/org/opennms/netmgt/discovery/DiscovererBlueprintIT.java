9dee0fe5c3dfe01f00a559ad335818a40cebb2e0
Seth Leger <seth@opennms.org>
2016-03-08 15:23:46 -0500

    Split the Discoverer blueprint out of the Discovery blueprint. Wrote a DistPollerDao that can provide the ID and location of the platform on Minion. Added integration tests for all blueprints.

--- features/discovery/src/test/java/org/opennms/netmgt/discovery/DiscovererBlueprintIT.java
+++ features/discovery/src/test/java/org/opennms/netmgt/discovery/DiscovererBlueprintIT.java
@@ -28,21 +28,11 @@
 
 package org.opennms.netmgt.discovery;
 
-import java.net.InetAddress;
-import java.util.Collections;
 import java.util.Dictionary;
 import java.util.Map;
 import java.util.Properties;
 
 import org.apache.activemq.broker.BrokerService;
-import org.apache.activemq.camel.component.ActiveMQComponent;
-import org.apache.camel.CamelContext;
-import org.apache.camel.Exchange;
-import org.apache.camel.Message;
-import org.apache.camel.Processor;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.impl.SimpleRegistry;
 import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
 import org.apache.camel.util.KeyValueHolder;
 import org.junit.AfterClass;
@@ -50,29 +40,22 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.core.test.OpenNMSJUnit4ClassRunner;
-import org.opennms.core.utils.InetAddressUtils;
 import org.opennms.netmgt.config.DiscoveryConfigFactory;
 import org.opennms.netmgt.config.api.DiscoveryConfigurationFactory;
 import org.opennms.netmgt.config.discovery.DiscoveryConfiguration;
 import org.opennms.netmgt.config.discovery.IncludeRange;
-import org.opennms.netmgt.config.discovery.Specific;
 import org.opennms.netmgt.dao.api.DistPollerDao;
-import org.opennms.netmgt.dao.mock.EventAnticipator;
 import org.opennms.netmgt.dao.mock.MockEventIpcManager;
-import org.opennms.netmgt.discovery.messages.DiscoveryJob;
-import org.opennms.netmgt.discovery.messages.DiscoveryResults;
-import org.opennms.netmgt.events.api.EventConstants;
 import org.opennms.netmgt.events.api.EventForwarder;
 import org.opennms.netmgt.events.api.EventIpcManager;
 import org.opennms.netmgt.icmp.Pinger;
-import org.opennms.netmgt.model.events.EventBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 
 @RunWith( OpenNMSJUnit4ClassRunner.class )
 @ContextConfiguration( locations = { "classpath:/META-INF/opennms/emptyContext.xml" } )
-public class DiscoveryBlueprintIT extends CamelBlueprintTestSupport {
+public class DiscovererBlueprintIT extends CamelBlueprintTestSupport {
 
     private static final Logger LOG = LoggerFactory.getLogger(DiscoveryBlueprintIT.class );
     private static final MockEventIpcManager IPC_MANAGER_INSTANCE = new MockEventIpcManager();
@@ -156,7 +139,7 @@ public class DiscoveryBlueprintIT extends CamelBlueprintTestSupport {
     @Override
     protected String getBlueprintDescriptor()
     {
-        return "file:blueprint-discovery.xml";
+        return "file:blueprint-discoverer.xml";
     }
 
     @BeforeClass
@@ -174,74 +157,6 @@ public class DiscoveryBlueprintIT extends CamelBlueprintTestSupport {
     }
 
     @Test
-    public void testDiscover() throws Exception {
-
-        /*
-         * Create a Camel listener for the location queue that will respond with
-         * {@link DiscoveryResult} objects.
-         */
-        SimpleRegistry registry = new SimpleRegistry();
-        CamelContext mockDiscoverer = new DefaultCamelContext(registry);
-        mockDiscoverer.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://127.0.0.1:61616"));
-        mockDiscoverer.addRoutes(new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
-                String from = String.format("activemq:Location-%s", LOCATION);
-
-                from(from)
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        DiscoveryJob job = exchange.getIn().getBody(DiscoveryJob.class);
-                        String foreignSource = job.getForeignSource();
-                        String location = job.getLocation();
-
-                        Message out = exchange.getOut();
-                        DiscoveryResults results = new DiscoveryResults(
-                            Collections.singletonMap(InetAddressUtils.addr("4.2.2.2"), 1000L),
-                            foreignSource,
-                            location
-                        );
-                        out.setBody(results);
-                    }
-                });
-            }
-        });
-
-        mockDiscoverer.start();
-
-        final String ipAddress = "4.2.2.2";
-        final String foreignSource = "Bogus FS";
-        final String location = LOCATION;
-
-        EventAnticipator anticipator = IPC_MANAGER_INSTANCE.getEventAnticipator();
-
-        EventBuilder eb = new EventBuilder( EventConstants.NEW_SUSPECT_INTERFACE_EVENT_UEI, "OpenNMS.Discovery" );
-        eb.setInterface( InetAddress.getByName( ipAddress ) );
-        eb.setHost( InetAddressUtils.getLocalHostName() );
-
-        eb.addParam( "RTT", 0 );
-        eb.addParam( "foreignSource", foreignSource );
-
-        anticipator.anticipateEvent( eb.getEvent() );
-
-        // Create the config aka job
-        Specific specific = new Specific();
-        specific.setContent( ipAddress );
-
-        DiscoveryConfiguration config = new DiscoveryConfiguration();
-        config.addSpecific( specific );
-        config.setForeignSource( foreignSource );
-        config.setTimeout( 3000 );
-        config.setRetries( 2 );
-        config.setLocation( location );
-
-        // Execute the job
-        template.requestBody( "direct:submitDiscoveryTask", config );
-
-        Thread.sleep( 1000 );
-        anticipator.verifyAnticipated();
-
-        mockDiscoverer.stop();
+    public void testDiscoverer() throws Exception {
     }
 }
