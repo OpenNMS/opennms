5edd9c9cc60e6e94616d72b87bcc0b0c19e95214
Markus von Rüden <mvr@opennms.com>
2016-03-08 18:18:40 +0100

    BSM-38: Update javadoc for the IconManager

--- features/topology-map/org.opennms.features.topology.api/src/main/java/org/opennms/features/topology/api/IconManager.java
+++ features/topology-map/org.opennms.features.topology.api/src/main/java/org/opennms/features/topology/api/IconManager.java
@@ -34,19 +34,51 @@ import org.opennms.features.topology.api.topo.Vertex;
 
 public interface IconManager {
 
+    /**
+     * Returns the first {@link IconRepository} which has the provided <code>iconKey</code> mapping defined.
+     *
+     * @param iconKey the <code>iconKey</code> to look up
+     * @return the first {@link IconRepository} which has the provided <code>iconKey</code> mapping defined, or <code>null</code> if no {@link IconRepository} exists with the provided <code>iconKey</code>
+     */
     IconRepository findRepositoryByIconKey(String iconKey);
 
     /**
-     * Returns the list of available svg-files, e.g. 'theme://svg/file.svg'
+     * Returns the list of available svg-files, e.g. 'theme://svg/file.svg'.
+     *
      * @return the list of available svg-files, e.g. 'theme://svg/file.svg'
      */
     List<String> getSVGIconFiles();
 
+    /**
+     * Returns the icon id assigned to the provided <code>iconKey</code>.
+     *
+     * @param iconKey the <code>iconKey</code> to look up
+     * @return the icon id assigned to the provided <code>iconKey</code>
+     */
     String getSVGIconId(String iconKey);
 
+    /**
+     * Returns the icon id assigned to the provided {@link Vertex}.
+     *
+     * @param vertex the vertex to get the icon id for
+     * @return the icon id assigned to the provided {@link Vertex}
+     */
     String getSVGIconId(Vertex vertex);
 
+    /**
+     * Sets a new icon mapping from the {@link Vertex} to the <code>newIconId</code>.
+     *
+     * @param vertex the vertex to map
+     * @param newIconId the icon id to map the vertex to
+     * @return the icon key of the vertex if this {@link IconManager} was able to save the mapping, null otherwise
+     */
     String setIconMapping(Vertex vertex, String newIconId);
 
+    /**
+     * Removes the icon mapping for the {@link Vertex} if defined.
+     *
+     * @param vertex the {@link Vertex} to remove the icon mapping for
+     * @return <code>true</code> if the mapping was removed, <code>false</code> if no icon mapping was found for the provided {@link Vertex} and therefore could not be removed
+     */
     boolean removeIconMapping(Vertex vertex);
 }
