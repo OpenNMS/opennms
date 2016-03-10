3d9ca245b8db94e1b96fe0cb34cfac1a8589bd5a
Benjamin Reed <ranger@opennms.org>
2016-03-10 12:33:49 -0500

    fix number of tests

--- smoke-test/src/test/javascript/tests/navigation/menu.js
+++ smoke-test/src/test/javascript/tests/navigation/menu.js
@@ -169,7 +169,7 @@ var expected = {
 	}
 };
 
-casper.test.begin('OpenNMS Nav Bar Menu', 59, {
+casper.test.begin('OpenNMS Nav Bar Menu', 58, {
 	setUp: function() {
 		opennms.initialize();
 		opennms.login();
