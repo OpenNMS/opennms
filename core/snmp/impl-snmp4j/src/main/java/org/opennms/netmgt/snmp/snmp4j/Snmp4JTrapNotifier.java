3d898b450a70ff64086cf870385353a9957ec2b0
Pradeep Srivatsa <pradeep.s1@cerner.com>
2016-03-16 11:38:33 -0400

    HZN-606 : Added junits to test whether TrapNotification is serializable.

--- core/snmp/impl-snmp4j/src/main/java/org/opennms/netmgt/snmp/snmp4j/Snmp4JTrapNotifier.java
+++ core/snmp/impl-snmp4j/src/main/java/org/opennms/netmgt/snmp/snmp4j/Snmp4JTrapNotifier.java
@@ -28,6 +28,7 @@
 
 package org.opennms.netmgt.snmp.snmp4j;
 
+import java.io.Serializable;
 import java.net.InetAddress;
 
 import org.opennms.netmgt.snmp.SnmpObjId;
@@ -66,11 +67,13 @@ public class Snmp4JTrapNotifier implements CommandResponder {
         m_trapProcessorFactory = processorFactory;
     }
     
-    public static class Snmp4JV1TrapInformation extends TrapInformation {
+    public static class Snmp4JV1TrapInformation extends TrapInformation implements Serializable {
+
+        private static final long serialVersionUID = -5130592972765964504L;
 
         private PDUv1 m_pdu;
 
-        protected Snmp4JV1TrapInformation(InetAddress agent, String community, PDUv1 pdu, TrapProcessor trapProcessor) {
+        public Snmp4JV1TrapInformation(InetAddress agent, String community, PDUv1 pdu, TrapProcessor trapProcessor) {
             super(agent, community, trapProcessor);
             m_pdu = pdu;
         }
@@ -112,7 +115,10 @@ public class Snmp4JTrapNotifier implements CommandResponder {
         }
     }
 
-    public static class Snmp4JV2TrapInformation extends TrapInformation {
+    public static class Snmp4JV2TrapInformation extends TrapInformation implements Serializable {
+
+        private static final long serialVersionUID = 2961743197092484906L;
+
         /**
          * The received PDU
          */
