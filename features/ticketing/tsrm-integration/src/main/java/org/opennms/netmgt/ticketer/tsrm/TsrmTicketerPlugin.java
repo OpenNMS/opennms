17da68375960d365f5f36b546dc61691a60adfe4
Jesse White <jesse@opennms.org>
2016-03-17 08:49:50 -0700

    HZN-645: Create project and compile WSDLs for the TSRM ticketing integration.

---
+++ features/ticketing/tsrm-integration/src/main/java/org/opennms/netmgt/ticketer/tsrm/TsrmTicketerPlugin.java
@@ -0,0 +1,20 @@
+package org.opennms.netmgt.ticketer.tsrm;
+
+import org.opennms.api.integration.ticketing.Plugin;
+import org.opennms.api.integration.ticketing.PluginException;
+import org.opennms.api.integration.ticketing.Ticket;
+
+public class TsrmTicketerPlugin implements Plugin {
+
+    @Override
+    public Ticket get(String ticketId) throws PluginException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void saveOrUpdate(Ticket ticket) throws PluginException {
+        // TODO Auto-generated method stub
+        
+    }
+}
