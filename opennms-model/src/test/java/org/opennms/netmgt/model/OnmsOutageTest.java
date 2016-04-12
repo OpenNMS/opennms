cbe379fb3e470997a4d2c04fe33c4c00c013c159
Jesse White <jesse@opennms.org>
2016-04-12 10:53:27 -0400

    Update JSON string to relect output rendered by the REST API.

--- opennms-model/src/test/java/org/opennms/netmgt/model/OnmsOutageTest.java
+++ opennms-model/src/test/java/org/opennms/netmgt/model/OnmsOutageTest.java
@@ -46,7 +46,6 @@ public class OnmsOutageTest {
         JsonTest.assertJsonEquals(
                 "{\"id\" : 1,\n" +
                 "  \"monitoredService\" : {\n" +
-                "    \"id\" : 1,\n" +
                 "    \"applications\" : [ {\n" +
                 "      \"name\" : \"Dummy\",\n" +
                 "      \"id\" : 100\n" +
