409e4c9c9e2e785fd5774e3ccf1837276509f75b
Antonio <rssntn67@yahoo.it>
2016-04-20 17:57:50 +0200

    NMS-8041: Refactor Enhanced Linkd Topology
    Fixed Unit Tests applicationContext
    Remove unused Applicationcontext for test removed LinkdTopologyProvider
    Added new EdgeStatusProvider

--- features/topology-map/plugins/org.opennms.features.topology.plugins.topo.linkd/src/main/java/org/opennms/features/topology/plugins/topo/linkd/internal/LinkdEdgeStatusProvider.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.topo.linkd/src/main/java/org/opennms/features/topology/plugins/topo/linkd/internal/LinkdEdgeStatusProvider.java
@@ -1,7 +1,6 @@
 package org.opennms.features.topology.plugins.topo.linkd.internal;
 
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -19,37 +18,6 @@ import org.opennms.netmgt.model.OnmsSeverity;
 
 public class LinkdEdgeStatusProvider implements EdgeStatusProvider {
 
-    public static class LinkdEdgeStatus implements Status{
-
-        private final String m_status;
-
-        public LinkdEdgeStatus(String status) {
-            m_status = status;
-        }
-
-        public LinkdEdgeStatus(OnmsAlarm summary) {
-            m_status = summary.getUei().equals(EventConstants.TOPOLOGY_LINK_DOWN_EVENT_UEI) ? "down" : "up";
-        }
-
-        @Override
-        public String computeStatus() {
-            return m_status.toLowerCase();
-        }
-
-        @Override
-        public Map<String, String> getStatusProperties() {
-            Map<String, String> statusMap = new LinkedHashMap<String, String>();
-            statusMap.put("status", m_status);
-
-            return statusMap;
-        }
-
-        @Override
-        public String toString() {
-            return "LinkdEdgeStatus[" + m_status + "]";
-        }
-    }
-
     private AlarmDao m_alarmDao;
 
     @Override
@@ -60,31 +28,8 @@ public class LinkdEdgeStatusProvider implements EdgeStatusProvider {
     @Override
     public Map<EdgeRef, Status> getStatusForEdges(EdgeProvider edgeProvider,
             Collection<EdgeRef> edges, Criteria[] criteria) {
-        Map<EdgeRef, Status> retVal = new LinkedHashMap<EdgeRef, Status>();
-EDGES:        for (EdgeRef edgeRef : edges) {
-                LinkdEdge edge = (LinkdEdge) edgeProvider.getEdge(edgeRef);
-                for (OnmsAlarm alarm: getLinkdEdgeDownAlarms()) {
-                    if (alarm.getNode().getId() == null)
-                        continue;
-                    if (alarm.getIfIndex() == null)
-                        continue;
-                    int alarmnodeid = alarm.getNode().getId().intValue();
-                    if ( edge.getSourceNodeid() != null && edge.getSourceNodeid().intValue() == alarmnodeid
-                            && edge.getSourceEndPoint() != null
-                            && edge.getSourceEndPoint().equals(String.valueOf(alarm.getIfIndex()))) {
-                        retVal.put(edgeRef, new LinkdEdgeStatus(alarm));
-                        continue EDGES;
-                    }
-                    if ( edge.getTargetNodeid() != null && edge.getTargetNodeid().intValue() == alarmnodeid
-                            && edge.getTargetEndPoint() != null
-                            && edge.getTargetEndPoint().equals(String.valueOf(alarm.getIfIndex()))) {
-                        retVal.put(edgeRef, new LinkdEdgeStatus(alarm));
-                        continue EDGES;
-                    }                
-              }
-              retVal.put(edgeRef, new LinkdEdgeStatus("up"));
-        }
-        return retVal;
+        // TODO Auto-generated method stub
+        return null;
     }
 
     @Override
@@ -96,7 +41,7 @@ EDGES:        for (EdgeRef edgeRef : edges) {
         return m_alarmDao;
     }
 
-    protected List<OnmsAlarm> getLinkdEdgeDownAlarms() {
+    protected List<OnmsAlarm> getLinkDownAlarms() {
         org.opennms.core.criteria.Criteria criteria = new org.opennms.core.criteria.Criteria(OnmsAlarm.class);
         criteria.addRestriction(new EqRestriction("uei", EventConstants.TOPOLOGY_LINK_DOWN_EVENT_UEI));
         criteria.addRestriction(new NeRestriction("severity", OnmsSeverity.CLEARED));
