dc1adf2dd416fc678d2eadccb644580d79bb68e0
Markus von Rüden <mvr@opennms.com>
2016-04-25 11:12:10 +0200

    NMS-8371: Fix info panel not updated on (auto) refresh

--- features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/TopologyUI.java
+++ features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/TopologyUI.java
@@ -34,7 +34,6 @@ import static org.opennms.features.topology.app.internal.operations.TopologySele
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -94,7 +93,6 @@ import org.opennms.osgi.locator.OnmsServiceManagerLocator;
 import org.opennms.web.api.OnmsHeaderProvider;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -345,7 +343,7 @@ public class TopologyUI extends UI implements CommandUpdateListener, MenuItemUpd
     /**
      * Helper class to load components to show in the info panel.
      */
-    public class InfoPanelItemProvider implements SelectionListener, MenuItemUpdateListener {
+    public class InfoPanelItemProvider implements SelectionListener, MenuItemUpdateListener, GraphContainer.ChangeListener {
 
         // Panel Item to visualize the selection context
         private final InfoPanelItem selectionContextPanelItem = new InfoPanelItem() {
@@ -485,6 +483,11 @@ public class TopologyUI extends UI implements CommandUpdateListener, MenuItemUpd
 
         @Override
         public void updateMenuItems() {
+            refreshInfoPanel();
+        }
+
+        @Override
+        public void graphChanged(GraphContainer graphContainer) {
             refreshInfoPanel();
         }
     }
@@ -630,6 +633,7 @@ public class TopologyUI extends UI implements CommandUpdateListener, MenuItemUpd
         final InfoPanelItemProvider infoPanelItemProvider = new InfoPanelItemProvider();
         m_selectionManager.addSelectionListener(infoPanelItemProvider);
         m_commandManager.addMenuItemUpdateListener(infoPanelItemProvider);
+        m_graphContainer.addChangeListener(infoPanelItemProvider);
     }
 
     private boolean noAdditionalFocusCriteria() {
