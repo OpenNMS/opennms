b27cb9f88d2f1a66740ecf48230036c6a54fbab3
Jesse White <jesse@opennms.org>
2016-05-02 15:45:58 -0400

    HZN-735: Refactor common SearchProvider logic into the SimpleSearchProvider.

--- features/topology-map/plugins/org.opennms.features.topology.plugins.topo.graphml/src/main/java/org/opennms/features/topology/plugins/topo/graphml/GraphMLSearchProvider.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.topo.graphml/src/main/java/org/opennms/features/topology/plugins/topo/graphml/GraphMLSearchProvider.java
@@ -29,34 +29,21 @@
 package org.opennms.features.topology.plugins.topo.graphml;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import org.opennms.features.topology.api.GraphContainer;
-import org.opennms.features.topology.api.support.VertexHopGraphProvider.DefaultVertexHopCriteria;
-import org.opennms.features.topology.api.topo.AbstractSearchProvider;
-import org.opennms.features.topology.api.topo.DefaultVertexRef;
-import org.opennms.features.topology.api.topo.SearchProvider;
 import org.opennms.features.topology.api.topo.SearchQuery;
-import org.opennms.features.topology.api.topo.SearchResult;
+import org.opennms.features.topology.api.topo.SimpleSearchProvider;
 import org.opennms.features.topology.api.topo.VertexRef;
 import org.opennms.features.topology.plugins.topo.graphml.model.GraphML;
 import org.opennms.features.topology.plugins.topo.graphml.model.GraphMLNode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
+public class GraphMLSearchProvider extends SimpleSearchProvider {
 
-/**
- * TODO: Most of this is copied from the BusinessServiceSearchProvider
- * 
- * @author jwhite
- */
-public class GraphMLSearchProvider extends AbstractSearchProvider implements SearchProvider {
-    private static final Logger LOG = LoggerFactory.getLogger(GraphMLSearchProvider.class);
+    private Optional<GraphML> getGraph() {
+        return Optional.ofNullable(GraphMLTopologyProvider.lastGraph);
+    }
 
     @Override
     public String getSearchProviderNamespace() {
@@ -64,23 +51,7 @@ public class GraphMLSearchProvider extends AbstractSearchProvider implements Sea
     }
 
     @Override
-    public boolean contributesTo(String namespace) {
-        final String graphNamespace = getSearchProviderNamespace();
-        return graphNamespace != null ? graphNamespace.equalsIgnoreCase(namespace) : false;
-    }
-
-    @Override
-    public boolean supportsPrefix(String searchPrefix) {
-        final String graphNamespace = getSearchProviderNamespace();
-        final String graphPrefix = graphNamespace != null ? graphNamespace : "";
-        return supportsPrefix(graphPrefix + "=", searchPrefix);
-    }
-
-    @Override
-    public List<SearchResult> query(SearchQuery searchQuery, GraphContainer container) {
-        LOG.info("GraphMLSearchProvider->query: called with search query: '{}'", searchQuery);
-        final List<SearchResult> results = Lists.newArrayList();
-
+    public List<? extends VertexRef> queryVertices(SearchQuery searchQuery, GraphContainer container) {
         // Search all of the nodes on all the graphs
         final List<GraphMLVertex> matchingVertices = new ArrayList<>();
         getGraph().ifPresent(d -> {
@@ -92,17 +63,7 @@ public class GraphMLSearchProvider extends AbstractSearchProvider implements Sea
                 .map(n -> new GraphMLVertex(n))
                 .forEach(v -> matchingVertices.add(v));
         });
-
-        // Build search results from the matching vertices
-        matchingVertices.stream().forEach(v -> {
-            SearchResult searchResult = new SearchResult(v);
-            searchResult.setCollapsed(false);
-            searchResult.setCollapsible(true);
-            results.add(searchResult);
-        });
-
-        LOG.info("GraphMLSearchProvider->query: found {} results: {}", results.size(), results);
-        return results;
+        return matchingVertices;
     }
 
     /**
@@ -118,36 +79,5 @@ public class GraphMLSearchProvider extends AbstractSearchProvider implements Sea
             }
         }
         return graphMLNode.getId().toLowerCase().contains(qs);
-    }
-
-    @Override
-    public Set<VertexRef> getVertexRefsBy(SearchResult searchResult, GraphContainer container) {
-        VertexRef vertexToFocus = new DefaultVertexRef(searchResult.getNamespace(), searchResult.getId(), searchResult.getLabel());
-        return Sets.newHashSet(vertexToFocus);
-    }
-
-    @Override
-    public void addVertexHopCriteria(SearchResult searchResult, GraphContainer container) {
-        LOG.debug("GraphMLSearchProvider->addVertexHopCriteria: called with search result: '{}'", searchResult);
-
-        DefaultVertexHopCriteria criterion = new DefaultVertexHopCriteria(new DefaultVertexRef(searchResult.getNamespace(), searchResult.getId(), searchResult.getLabel()));
-        container.addCriteria(criterion);
-
-        LOG.debug("GraphMLSearchProvider->addVertexHop: adding hop criteria {}.", criterion);
-        LOG.debug("GraphMLSearchProvider->addVertexHop: current criteria {}.", Arrays.toString(container.getCriteria()));
-    }
-
-    @Override
-    public void removeVertexHopCriteria(SearchResult searchResult, GraphContainer container) {
-        LOG.debug("GraphMLSearchProvider->removeVertexHopCriteria: called with search result: '{}'", searchResult);
-
-        DefaultVertexHopCriteria criterion = new DefaultVertexHopCriteria(new DefaultVertexRef(searchResult.getNamespace(), searchResult.getId(), searchResult.getLabel()));
-        container.removeCriteria(criterion);
-
-        LOG.debug("GraphMLSearchProvider->removeVertexHopCriteria: current criteria {}.", Arrays.toString(container.getCriteria()));
-    }
-
-    private Optional<GraphML> getGraph() {
-        return Optional.ofNullable(GraphMLTopologyProvider.lastGraph);
     }
 }
