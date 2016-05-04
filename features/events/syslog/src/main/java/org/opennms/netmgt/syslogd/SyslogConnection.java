6c87e5597923902fba707459095fbad2937ade26
Seth Leger <seth@opennms.org>
2016-05-03 23:41:05 -0400

    Changed <bean> tag to more precise <process> tag in blueprint-syslog-handler-default.xml. Fixed problem with ByteBuffer copy in test.

--- features/events/syslog/src/main/java/org/opennms/netmgt/syslogd/SyslogConnection.java
+++ features/events/syslog/src/main/java/org/opennms/netmgt/syslogd/SyslogConnection.java
@@ -122,6 +122,7 @@ public class SyslogConnection implements Callable<Callable<?>> {
 
     @XmlAttribute
     public byte[] getBytes() {
+        m_bytes.rewind();
         byte[] retval = new byte[m_bytes.remaining()];
         m_bytes.get(retval);
         m_bytes.rewind();
