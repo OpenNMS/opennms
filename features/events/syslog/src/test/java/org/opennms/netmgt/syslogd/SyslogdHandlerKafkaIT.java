be063428a8dc707028941b8d2a87190fd5f4c08e
Pradeep S <pradeep.s1@cerner.com>
2016-05-12 19:39:18 +0530

    Reverted the additional route

--- features/events/syslog/src/test/java/org/opennms/netmgt/syslogd/SyslogdHandlerKafkaIT.java
+++ features/events/syslog/src/test/java/org/opennms/netmgt/syslogd/SyslogdHandlerKafkaIT.java
@@ -168,48 +168,7 @@ public class SyslogdHandlerKafkaIT extends CamelBlueprintTestSupport {
             }
         });
             
-        
-        syslogd.addRoutes(new RouteBuilder(){
-
-			@Override
-			public void configure() throws Exception {
-				from("kafka:localhost:9092?topic=syslog&zookeeperHost=localhost&zookeeperPort=2181&groupId=group1")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange)
-                            throws Exception {
-                        String messageKey = "";
-                        if (exchange.getIn() != null) {
-                            Message message = exchange.getIn();
-                            Integer partitionId = (Integer) message
-                                    .getHeader(KafkaConstants.PARTITION);
-                            String topicName = (String) message
-                                    .getHeader(KafkaConstants.TOPIC);
-                            if (message.getHeader(KafkaConstants.KEY) != null)
-                                messageKey = (String) message
-                                        .getHeader(KafkaConstants.KEY);
-                            Object data = message.getBody();
-
-
-                            System.out.println("topicName :: "
-                                    + topicName + " partitionId :: "
-                                    + partitionId + " messageKey :: "
-                                    + messageKey + " message :: "
-                                    + data + "\n");
-                            
-                        }
-                    }
-                }).to("log:input");
-				
-			}
-        	
-        });
-        
         syslogd.start();
-        ProducerTemplate template = syslogd.createProducerTemplate();
-        SyslogConnection connection = new SyslogConnection();
-        connection.setSourceAddress(InetAddress.getByName("www.opennms.com"));
-        template.sendBody( "seda:handleMessage", connection);
         
 	}
 	
