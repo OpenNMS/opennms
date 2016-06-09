964dd7e958b1f6b395b4f28e55ba58148e2bfd08
Jesse White <jesse@opennms.org>
2016-06-09 11:42:19 -0600

    Add support for varying the string attribute values.

--- features/collection/shell-commands/src/main/java/org/opennms/netmgt/collection/commands/StressCommand.java
+++ features/collection/shell-commands/src/main/java/org/opennms/netmgt/collection/commands/StressCommand.java
@@ -102,6 +102,9 @@ public class StressCommand extends OsgiCommandSupport {
     @Option(name="-t", aliases="--threads", description="number of threads that will be used to generate and persist collection sets, defaults to 1", required=false, multiValued=false)
     int numberOfGeneratorThreads = 1;
 
+    @Option(name="-z", aliases="--string-variation-factor", description="when set, every n-th group will use unique string attribute values in each batch, defaults to 0", required=false, multiValued=false)
+    int stringVariationFactor = 0;
+
     private final AtomicBoolean abort = new AtomicBoolean(false);
 
     private final MetricRegistry metrics = new MetricRegistry();
@@ -111,6 +114,8 @@ public class StressCommand extends OsgiCommandSupport {
     private final Meter numericAttributesGenerated = metrics.meter("numeric-attributes-generated");
 
     private final Meter stringAttributesGenerated = metrics.meter("string-attributes-generated");
+
+    private Meter stringAttributesVaried;
 
     /**
      * Used to calculate non-constant, but predictable values stored in numeric attributes.
@@ -128,6 +133,10 @@ public class StressCommand extends OsgiCommandSupport {
         numberOfStringAttributesPerGroup = Math.max(0, numberOfStringAttributesPerGroup);
         reportIntervalInSeconds = Math.max(1, reportIntervalInSeconds);
         numberOfGeneratorThreads = Math.max(1, numberOfGeneratorThreads);
+        stringVariationFactor = Math.max(0, stringVariationFactor);
+        if (stringVariationFactor > 0) {
+            stringAttributesVaried = metrics.meter("string-attributes-varied");
+        }
 
         // Display the effective settings and rates
         double attributesPerSecond = (1 / (double)intervalInSeconds) * numberOfGroupsPerInterface
@@ -139,6 +148,9 @@ public class StressCommand extends OsgiCommandSupport {
         System.out.printf("\t with %d numeric attributes\n", numberOfNumericAttributesPerGroup);
         System.out.printf("\t with %d string attributes\n", numberOfStringAttributesPerGroup);
         System.out.printf("Across %d threads\n", numberOfGeneratorThreads);
+        if (stringVariationFactor > 0) {
+            System.out.printf("With string variation factor %d\n", stringVariationFactor);
+        }
         System.out.printf("Which will yield an effective\n");
         System.out.printf("\t %.2f numeric attributes per second\n", numberOfNumericAttributesPerGroup * attributesPerSecond);
         System.out.printf("\t %.2f string attributes per second\n", numberOfStringAttributesPerGroup * attributesPerSecond);
@@ -230,7 +242,7 @@ public class StressCommand extends OsgiCommandSupport {
                         InterfaceLevelResource interfaceResource = new InterfaceLevelResource(nodeResource, "tap" + interfaceId);
 
                         // Generate the collection set
-                        CollectionSet collectionSet = generateCollectionSet(agent, interfaceResource);
+                        CollectionSet collectionSet = generateCollectionSet(agent, nodeId, interfaceId, interfaceResource);
 
                         // Persist
                         collectionSet.visit(persister);
@@ -241,7 +253,7 @@ public class StressCommand extends OsgiCommandSupport {
         };
     }
 
-    private CollectionSet generateCollectionSet(CollectionAgent agent, Resource resource) {
+    private CollectionSet generateCollectionSet(CollectionAgent agent, int nodeId, int interfaceId, Resource resource) {
         CollectionSetBuilder builder = new CollectionSetBuilder(agent);
         for (int groupId = 0; groupId < numberOfGroupsPerInterface; groupId++) {
             String groupName = "group" + groupId;
@@ -253,10 +265,20 @@ public class StressCommand extends OsgiCommandSupport {
                 numericAttributesGenerated.mark();
             }
 
+            String stringAttributeValueSuffix = "";
+            int groupInstance = (nodeId + 1) * (interfaceId + 1) * (groupId + 1); // Add 1 to each of these to make sure they are > 0
+            if (stringVariationFactor > 0 && groupInstance % stringVariationFactor == 0) {
+                stringAttributeValueSuffix = String.format("-%d-varied", groupInstance);
+                stringAttributesVaried.mark(numberOfStringAttributesPerGroup);
+            }
+
             // String attributes
             for (int stringAttributeId = 0; stringAttributeId < numberOfStringAttributesPerGroup; stringAttributeId++) {
-                // Use constant values for the string attributes
-                builder.withStringAttribute(resource, groupName, "tag" + stringAttributeId, "key" + stringAttributeId);
+                // String attributes are stored at the resource level, and not at the group level, so we prefix
+                // the keys with the group name to make sure these don't collide
+                String key = String.format("%s-key-%d", groupName, stringAttributeId);
+                String value = String.format("%s-value-%d%s", groupName, stringAttributeId, stringAttributeValueSuffix);
+                builder.withStringAttribute(resource, groupName, key, value);
                 stringAttributesGenerated.mark();
             }
         }
