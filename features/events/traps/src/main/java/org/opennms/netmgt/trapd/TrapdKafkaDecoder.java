67f2f1ddd63f2d595fa880abbc51dd27404d96b6
DP044946 <deepak.patel@cerner.com>
2016-06-23 16:09:42 +0530

    formatting the code & removing loggers

--- features/events/traps/src/main/java/org/opennms/netmgt/trapd/TrapdKafkaDecoder.java
+++ features/events/traps/src/main/java/org/opennms/netmgt/trapd/TrapdKafkaDecoder.java
@@ -3,11 +3,16 @@ package org.opennms.netmgt.trapd;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.json.JSONObject;
 import org.opennms.core.utils.InetAddressUtils;
 import org.opennms.netmgt.snmp.BasicTrapProcessor;
 import org.opennms.netmgt.snmp.SnmpObjId;
@@ -20,6 +25,7 @@ import org.opennms.netmgt.snmp.snmp4j.Snmp4JTrapNotifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snmp4j.PDU;
+import org.snmp4j.PDUv1;
 import org.snmp4j.mp.SnmpConstants;
 import org.snmp4j.smi.IpAddress;
 import org.snmp4j.smi.OID;
@@ -32,85 +38,26 @@ import kafka.serializer.Decoder;
 public class TrapdKafkaDecoder implements Decoder<Object>{
 	
 	public static final Logger LOG = LoggerFactory.getLogger(TrapdKafkaDecoder.class);
-
-//	public TrapdKafkaDecoder(VerifiableProperties verifiableProperties){
-//		
-//	}
-	
 	
     @Override
     public Object fromBytes(byte[] bytes) {
         ObjectMapper objectMapper = new ObjectMapper();
         TrapNotification snmp4JV2cTrap = null;
-        try {
-        	//System.out.println("######################################################## bytes is : "+bytes);
-        	//Snmp4JTrapNotifier.Snmp4JV2TrapInformation v2Trap = objectMapper.readValue(bytes, Snmp4JTrapNotifier.Snmp4JV2TrapInformation.class);
-        	//System.out.println("####################################################### v2Trap is : "+v2Trap);
-        	//System.out.println("####################################################### v2Trap.toString is : "+v2Trap.toString());
-        	//objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES,false);
-
-        	//Object result = objectMapper.readValue(bytes, Snmp4JTrapNotifier.Snmp4JV2TrapInformation.class);
-
-        	
+        try {        	
         	
         	JsonNode result = objectMapper.readTree(bytes);
-        	System.out.println("result is : "+result); 
-
+        	System.out.println("result is : "+result);
+        	
         	String version = result.findValue("version").asText();
-        	System.out.println("version is : "+version); 
 
-        	String trapAddress = result.findValue("trapAddress").asText();
-        	System.out.println("trapAddress : "+trapAddress);
 
-        	JsonNode trapProcessorRoot = result.findValue("trapProcessor");
-        	System.out.println("trapProcessorRoot : "+trapProcessorRoot);
-        	        	
-        	TrapProcessor trapProcessor = new BasicTrapProcessor();
-			trapProcessor.setAgentAddress(InetAddressUtils.getInetAddress(trapProcessorRoot.findValue("agentAddress").asText()));
-			trapProcessor.setCommunity(trapProcessorRoot.findValue("community").asText());
-			trapProcessor.setTimeStamp(trapProcessorRoot.findValue("timeStamp").asLong());
-			trapProcessor.setVersion(trapProcessorRoot.findValue("version").asText());
-			trapProcessor.setTrapAddress(InetAddressUtils.getInetAddress(trapProcessorRoot.findValue("trapAddress").asText()));
-
-			// Setting TrapIdentity {trapIdentityRoot is : {"generic":6,"specific":0,"enterpriseId":".1.3.6.1.2.1.1.3"}
-			JsonNode trapIdentityRoot = trapProcessorRoot.findValue("trapIdentity");
-			System.out.println("trapIdentityRoot is : "+trapIdentityRoot);
-			int[] ids = convertStringToInts(trapIdentityRoot.findValue("enterpriseId").asText()); 
-			SnmpObjId entId = new SnmpObjId(ids, false);
-			System.out.println("entId is : "+entId);
-			int generic = trapIdentityRoot.findValue("generic").asInt();
-			int specific = trapIdentityRoot.findValue("specific").asInt();
-			TrapIdentity trapIdentity = new TrapIdentity(entId, generic, specific);
-			trapProcessor.setTrapIdentity(trapIdentity);
-			
-			// Setting VarBind {VarBindRoot is : {"generic":6,"specific":0,"enterpriseId":".1.3.6.1.2.1.1.3"}
-			JsonNode varBindRoot = trapProcessorRoot.findValue("varBinds");
-			System.out.println("varBindRoot is : "+varBindRoot);         
-			String value = new String(Base64.getDecoder().decode(varBindRoot.findValue("value").asText()));
-			System.out.println("value is :"+value);
-	
-			PDU snmp4JV2cTrapPdu = new PDU();
-			OID oid = new OID(trapIdentityRoot.findValue("enterpriseId").asText());
-			snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.sysUpTime, new TimeTicks(5000)));   
-			snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapOID, new OID(oid)));
-			snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapAddress,
-					new IpAddress(trapAddress)));
-
-			snmp4JV2cTrapPdu.add(new VariableBinding(new OID(oid), new OctetString(value)));
-			snmp4JV2cTrapPdu.setType(varBindRoot.findValue("type").asInt()); // what should be the type?
-			System.out.println("type is : "+varBindRoot.findValue("type").asInt());
-			
-			snmp4JV2cTrap = new Snmp4JTrapNotifier.Snmp4JV2TrapInformation(
-			InetAddressUtils.getInetAddress(trapAddress), trapProcessorRoot.findValue("community").asText(),
-			snmp4JV2cTrapPdu, trapProcessor);
 			
         	
-//        	if(version.equalsIgnoreCase("v2") || version.equalsIgnoreCase("v3")){
-//        		return parseV2Information(result);
-//        	}else if(version.equalsIgnoreCase("v1")){
-//        		return parseV1Information(result);
-//        	}
-			System.out.println("snmp4JV2cTrap is : "+snmp4JV2cTrap);
+        	if(version.equalsIgnoreCase("v2") || version.equalsIgnoreCase("v3")){
+        		snmp4JV2cTrap = parseV2Information(result);
+        	}else if(version.equalsIgnoreCase("v1")){
+        		//snmp4JV2cTrap =  parseV1Information(result);
+        	}
             
         } catch (IOException e) {
         	//System.out.println("e is : "+e);
@@ -119,6 +66,119 @@ public class TrapdKafkaDecoder implements Decoder<Object>{
         }
         return snmp4JV2cTrap;
         	
+    }
+    
+    public TrapInformation parseV2Information(JsonNode result){
+    	
+    	String trapAddress = result.findValue("trapAddress").asText();
+
+    	JsonNode trapProcessorRoot = result.findValue("trapProcessor");
+    	        	
+    	TrapProcessor trapProcessor = new BasicTrapProcessor();
+		trapProcessor.setAgentAddress(InetAddressUtils.getInetAddress(trapProcessorRoot.findValue("agentAddress").asText()));
+		trapProcessor.setCommunity(trapProcessorRoot.findValue("community").asText());
+		trapProcessor.setTimeStamp(trapProcessorRoot.findValue("timeStamp").asLong());
+		trapProcessor.setVersion(trapProcessorRoot.findValue("version").asText());
+		trapProcessor.setTrapAddress(InetAddressUtils.getInetAddress(trapProcessorRoot.findValue("trapAddress").asText()));
+
+		// Setting TrapIdentity {trapIdentityRoot is : {"generic":6,"specific":0,"enterpriseId":".1.3.6.1.2.1.1.3"}
+		JsonNode trapIdentityRoot = trapProcessorRoot.findValue("trapIdentity");
+		int[] ids = convertStringToInts(trapIdentityRoot.findValue("enterpriseId").asText()); 
+		SnmpObjId entId = new SnmpObjId(ids, false);
+		int generic = trapIdentityRoot.findValue("generic").asInt();
+		int specific = trapIdentityRoot.findValue("specific").asInt();
+		TrapIdentity trapIdentity = new TrapIdentity(entId, generic, specific);
+		trapProcessor.setTrapIdentity(trapIdentity);
+		
+		// Setting VarBind 
+		PDU snmp4JV2cTrapPdu = new PDU();
+		
+		JsonNode varBindRoot = trapProcessorRoot.findValue("varBinds");
+		System.out.println("varBindRoot is : "+varBindRoot);
+		
+		String varBindValue = "";
+	    Iterator<Map.Entry<String,JsonNode>> fieldsIterator = varBindRoot.getFields();
+	    	while (fieldsIterator.hasNext()) {
+
+	           Map.Entry<String,JsonNode> field = fieldsIterator.next();
+	           System.out.println("Key: " + field.getKey() + "\tValue:" + field.getValue());
+	           varBindValue = field.getKey();
+	    	}
+		
+		String bytesValue = new String(Base64.getDecoder().decode(varBindRoot.findValue("value").asText()));
+		System.out.println("bytesValue is : "+bytesValue);
+		System.out.println("varBindValue is : "+varBindValue);
+		OID oid = new OID(varBindValue);
+		snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.sysUpTime, new TimeTicks(5000)));   
+		snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapOID, new OID(oid)));
+		snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapAddress,
+				new IpAddress(trapAddress)));
+
+		snmp4JV2cTrapPdu.add(new VariableBinding(new OID(oid), new OctetString(bytesValue)));
+		
+		System.out.println("type is : "+varBindRoot.findValue("type"));
+		
+		//snmp4JV2cTrapPdu.setType(varBindRoot.findValue("type").asInt()); // what should be the type? 
+		snmp4JV2cTrapPdu.setType(PDU.NOTIFICATION);
+		
+		Snmp4JTrapNotifier.Snmp4JV2TrapInformation snmp4JV2cTrap = new Snmp4JTrapNotifier.Snmp4JV2TrapInformation(
+		InetAddressUtils.getInetAddress(trapAddress), trapProcessorRoot.findValue("community").asText(),
+		snmp4JV2cTrapPdu, trapProcessor);
+		
+		return snmp4JV2cTrap;
+    }
+    
+    public TrapInformation parseV1Information(JsonNode result){
+    	
+    	String trapAddress = result.findValue("trapAddress").asText();
+
+    	JsonNode trapProcessorRoot = result.findValue("trapProcessor");
+    	        	
+    	TrapProcessor trapProcessor = new BasicTrapProcessor();
+		trapProcessor.setAgentAddress(InetAddressUtils.getInetAddress(trapProcessorRoot.findValue("agentAddress").asText()));
+		trapProcessor.setCommunity(trapProcessorRoot.findValue("community").asText());
+		trapProcessor.setTimeStamp(trapProcessorRoot.findValue("timeStamp").asLong());
+		trapProcessor.setVersion(trapProcessorRoot.findValue("version").asText());
+		trapProcessor.setTrapAddress(InetAddressUtils.getInetAddress(trapProcessorRoot.findValue("trapAddress").asText()));
+
+		// Setting TrapIdentity {trapIdentityRoot is : {"generic":6,"specific":0,"enterpriseId":".1.3.6.1.2.1.1.3"}
+		JsonNode trapIdentityRoot = trapProcessorRoot.findValue("trapIdentity");
+		int[] ids = convertStringToInts(trapIdentityRoot.findValue("enterpriseId").asText()); 
+		SnmpObjId entId = new SnmpObjId(ids, false);
+		int generic = trapIdentityRoot.findValue("generic").asInt();
+		int specific = trapIdentityRoot.findValue("specific").asInt();
+		TrapIdentity trapIdentity = new TrapIdentity(entId, generic, specific);
+		trapProcessor.setTrapIdentity(trapIdentity);
+		
+		// Setting VarBind 
+		PDUv1 snmp4JV1cTrapPdu = new PDUv1();
+		
+		JsonNode varBindRoot = trapProcessorRoot.findValue("varBinds");
+		System.out.println(varBindRoot);
+		
+		String bytesValue = new String(Base64.getDecoder().decode(varBindRoot.findValue("value").asText()));
+		System.out.println("bytesValue is : "+bytesValue);
+		
+		OID oid = new OID(trapIdentityRoot.findValue("enterpriseId").asText());
+		snmp4JV1cTrapPdu.add(new VariableBinding(SnmpConstants.sysUpTime, new TimeTicks(5000)));   
+		snmp4JV1cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapOID, new OID(oid)));
+		snmp4JV1cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapAddress,
+				new IpAddress(trapAddress)));
+
+		snmp4JV1cTrapPdu.add(new VariableBinding(new OID(oid), new OctetString(bytesValue))); // we should read this from object
+		//snmp4JV2cTrapPdu.add(new VariableBinding(new OID(oid2), new OctetString(value2))); // we should read this from object
+		
+		System.out.println("type is : "+varBindRoot.findValue("type"));
+		
+		//snmp4JV2cTrapPdu.setType(varBindRoot.findValue("type").asInt()); // what should be the type? 
+		snmp4JV1cTrapPdu.setType(PDU.NOTIFICATION);
+		
+		
+		Snmp4JTrapNotifier.Snmp4JV1TrapInformation snmp4JV1cTrap = new Snmp4JTrapNotifier.Snmp4JV1TrapInformation(
+		InetAddressUtils.getInetAddress(trapAddress), trapProcessorRoot.findValue("community").asText(),
+		snmp4JV1cTrapPdu, trapProcessor);
+		
+		return snmp4JV1cTrap;
     }
     
     private static int[] convertStringToInts(String oid) {
--- features/events/traps/src/test/java/org/opennms/netmgt/trapd/TrapdEncoderDecoderTest.java
+++ features/events/traps/src/test/java/org/opennms/netmgt/trapd/TrapdEncoderDecoderTest.java
@@ -1,23 +1,18 @@
 package org.opennms.netmgt.trapd;
 
-import static org.junit.Assert.*;
-import kafka.serializer.StringEncoder;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.opennms.core.camel.JaxbUtilsMarshalProcessor;
 import org.opennms.core.utils.InetAddressUtils;
-import org.opennms.core.xml.JaxbUtils;
 import org.opennms.netmgt.snmp.BasicTrapProcessor;
-import org.opennms.netmgt.snmp.TrapInformation;
 import org.opennms.netmgt.snmp.TrapNotification;
 import org.opennms.netmgt.snmp.snmp4j.Snmp4JTrapNotifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snmp4j.PDU;
+import org.snmp4j.PDUv1;
 import org.snmp4j.mp.SnmpConstants;
 import org.snmp4j.smi.IpAddress;
 import org.snmp4j.smi.OID;
@@ -48,21 +43,21 @@ public class TrapdEncoderDecoderTest {
 	}
 
 	@Test
-	public void testTrapdSnmp4jObj(){
+	public void testTrapdSnmp4jV2Trap(){
 		
 		PDU snmp4JV2cTrapPdu = new PDU();
+
 		OID oid = new OID(".1.3.6.1.2.1.1.3.0");
 		snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.sysUpTime, new TimeTicks(5000)));
 		snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapOID, new OID(oid)));
-		snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapAddress,
-				new IpAddress("127.0.0.1")));
+		//snmp4JV2cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapAddress,new IpAddress("127.0.0.1")));
 
-		snmp4JV2cTrapPdu.add(new VariableBinding(new OID(oid), new OctetString("TESTING TRAPS MSGS")));
+		snmp4JV2cTrapPdu.add(new VariableBinding(new OID(oid), new OctetString("Trap Msg v2")));
 		snmp4JV2cTrapPdu.setType(PDU.NOTIFICATION);
-		
+
 		TrapNotification snmp4JV2cTrap = new Snmp4JTrapNotifier.Snmp4JV2TrapInformation(
-		InetAddressUtils.ONE_TWENTY_SEVEN, "public",
-		snmp4JV2cTrapPdu, new BasicTrapProcessor());
+				InetAddressUtils.ONE_TWENTY_SEVEN, "public",
+				snmp4JV2cTrapPdu, new BasicTrapProcessor());
 		
 		TrapdKafkaEncoder encoder = new TrapdKafkaEncoder();
 		byte[] testArray = encoder.toBytes(snmp4JV2cTrap);
@@ -74,5 +69,34 @@ public class TrapdEncoderDecoderTest {
 		System.out.println("snmp4JV2cTrap1 is : "+snmp4JV2cTrap1);
 		System.out.println("snmp4JV2cTrap1.toString() is : "+snmp4JV2cTrap1.toString());
 	}
+	
+//	@Test
+//	public void testTrapdSnmp4jV1Trap(){
+//		
+//		PDUv1 snmp4JV1cTrapPdu = new PDUv1();
+//
+//		OID oid = new OID(".1.3.6.1.2.1.1.3.0");
+//		snmp4JV1cTrapPdu.add(new VariableBinding(SnmpConstants.sysUpTime, new TimeTicks(5000)));
+//		snmp4JV1cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapOID, new OID(oid)));
+//		snmp4JV1cTrapPdu.add(new VariableBinding(SnmpConstants.snmpTrapAddress,
+//				new IpAddress("127.0.0.1")));
+//
+//		snmp4JV1cTrapPdu.add(new VariableBinding(new OID(oid), new OctetString("Trap Msg v1")));
+//		snmp4JV1cTrapPdu.setType(PDU.NOTIFICATION);
+//
+//		TrapNotification snmp4JV1cTrap = new Snmp4JTrapNotifier.Snmp4JV1TrapInformation(
+//				InetAddressUtils.ONE_TWENTY_SEVEN, "public",
+//				snmp4JV1cTrapPdu, new BasicTrapProcessor());
+//		
+//		TrapdKafkaEncoder encoder = new TrapdKafkaEncoder();
+//		byte[] testArray = encoder.toBytes(snmp4JV1cTrap);
+//
+//		System.out.println("testArray is : "+testArray);
+//		
+//		TrapdKafkaDecoder decoder = new TrapdKafkaDecoder();
+//		TrapNotification snmp4JV1cTrap1 = (TrapNotification) decoder.fromBytes(testArray);
+//		System.out.println("snmp4JV1cTrap1 is : "+snmp4JV1cTrap1);
+//		System.out.println("snmp4JV1cTrap1.toString() is : "+snmp4JV1cTrap1.toString());
+//	}
 
 }
