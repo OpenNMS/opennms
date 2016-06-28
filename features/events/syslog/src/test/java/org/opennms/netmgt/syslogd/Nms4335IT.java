5c31131b99bcd185942682e08142f06a35c7978d
Seth Leger <seth@opennms.org>
2016-06-27 22:48:18 -0400

    Moved applicationContext-databasePopulator.xml into opennms-dao's main source to reduce contents of test-jar artifact. Got rid of unnecessary applicationContext-setupIpLike-enabled.xml.

--- features/events/syslog/src/test/java/org/opennms/netmgt/syslogd/Nms4335IT.java
+++ features/events/syslog/src/test/java/org/opennms/netmgt/syslogd/Nms4335IT.java
@@ -70,7 +70,6 @@ import org.springframework.transaction.annotation.Transactional;
         "classpath:/META-INF/opennms/applicationContext-soa.xml",
         "classpath:/META-INF/opennms/applicationContext-dao.xml",
         "classpath:/META-INF/opennms/applicationContext-daemon.xml",
-        "classpath:/META-INF/opennms/applicationContext-setupIpLike-enabled.xml",
         "classpath:/META-INF/opennms/applicationContext-databasePopulator.xml",
         "classpath:/META-INF/opennms/mockEventIpcManager.xml"
 })
