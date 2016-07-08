4232fe86a64cc502394c03c054d84a8c9e262edb
Malatesh Sudarshan <Malatesh.Sudarshan@cerner.com>
2016-07-08 12:33:39 +0530

    Setting Agent address inorder to show Interface at Web Console

--- features/events/traps/src/main/java/org/opennms/netmgt/trapd/TrapdKafkaDecoder.java
+++ features/events/traps/src/main/java/org/opennms/netmgt/trapd/TrapdKafkaDecoder.java
@@ -166,7 +166,7 @@ public class TrapdKafkaDecoder implements Decoder<Object>{
 		
 		// Setting VarBind 
 		PDUv1 snmp4JV1cTrapPdu = new PDUv1();
-		snmp4JV1cTrapPdu.setAgentAddress(new IpAddress(trapAddress));
+		snmp4JV1cTrapPdu.setAgentAddress(new IpAddress(agentAddress));
 		
 		JsonNode varBindRoot = trapProcessorRoot.findValue("varBinds");
 		LOG.debug("varBindRoot is : "+varBindRoot);
