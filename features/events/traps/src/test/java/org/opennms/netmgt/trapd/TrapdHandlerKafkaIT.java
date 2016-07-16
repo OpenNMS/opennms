f0a8e1df89f042bf3be7af65771cf5ff4e5d6b49
Seth Leger <seth@opennms.org>
2016-07-16 17:12:20 -0400

    Cleaned up features and Karaf ITs. Added random port allocation and Kafka shutdown to Trapd Kafka tests.

--- features/events/traps/src/test/java/org/opennms/netmgt/trapd/TrapdHandlerKafkaIT.java
+++ features/events/traps/src/test/java/org/opennms/netmgt/trapd/TrapdHandlerKafkaIT.java
@@ -28,8 +28,8 @@
 
 package org.opennms.netmgt.trapd;
 
+import java.net.ServerSocket;
 import java.util.Dictionary;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
@@ -45,7 +45,6 @@ import org.apache.camel.component.kafka.KafkaComponent;
 import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.SimpleRegistry;
-import org.apache.camel.test.blueprint.CamelBlueprintTestSupport;
 import org.apache.camel.util.KeyValueHolder;
 import org.apache.curator.test.TestingServer;
 import org.junit.After;
@@ -53,6 +52,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.core.test.OpenNMSJUnit4ClassRunner;
+import org.opennms.core.test.camel.CamelBlueprintTest;
 import org.opennms.netmgt.snmp.TrapNotification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,66 +60,66 @@ import org.springframework.test.context.ContextConfiguration;
 
 @RunWith(OpenNMSJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/META-INF/opennms/emptyContext.xml" })
-public class TrapdHandlerKafkaIT extends CamelBlueprintTestSupport {
+public class TrapdHandlerKafkaIT extends CamelBlueprintTest {
 
 	private static final Logger LOG = LoggerFactory.getLogger(TrapdHandlerKafkaIT.class);
 
 	private static KafkaConfig kafkaConfig;
 	private KafkaServer kafkaServer;
-	private  TestingServer zkTestServer;
-	
-	/**
-	 * Use Aries Blueprint synchronous mode to avoid a blueprint deadlock bug.
-	 * 
-	 * @see https://issues.apache.org/jira/browse/ARIES-1051
-	 * @see https://access.redhat.com/site/solutions/640943
-	 */
+	private TestingServer zkTestServer;
+
+	private int kafkaPort;
+
+	private int zookeeperPort;
+
+	private static int getAvailablePort(int min, int max) {
+		for (int i = min; i <= max; i++) {
+			try (ServerSocket socket = new ServerSocket(i)) {
+				return socket.getLocalPort();
+			} catch (Throwable e) {}
+		}
+		throw new IllegalStateException("Can't find an available network port");
+	}
+
 	@Override
 	public void doPreSetup() throws Exception {
-		System.setProperty("org.apache.aries.blueprint.synchronous", Boolean.TRUE.toString());
-		System.setProperty("de.kalpatec.pojosr.framework.events.sync", Boolean.TRUE.toString());
-		
-		zkTestServer = new TestingServer(2181);
-    	Properties properties = new Properties();
-    	properties.put("port", "9092");
-    	properties.put("host.name", "localhost");
-    	properties.put("broker.id", "5001");
-    	properties.put("enable.zookeeper", "false");
-    	properties.put("zookeeper.connect",zkTestServer.getConnectString());
-    	try{
-    		kafkaConfig = new KafkaConfig(properties);
-    		kafkaServer = new KafkaServer(kafkaConfig, null);
-    		kafkaServer.startup();
-    	}
-    	catch(Exception e){
-    		e.printStackTrace();
-    	}
+		super.doPreSetup();
+
+		zkTestServer = new TestingServer(zookeeperPort);
+		Properties properties = new Properties();
+		properties.put("port", String.valueOf(kafkaPort));
+		properties.put("host.name", "localhost");
+		properties.put("broker.id", "5001");
+		properties.put("enable.zookeeper", "false");
+		properties.put("zookeeper.connect",zkTestServer.getConnectString());
+		try{
+			kafkaConfig = new KafkaConfig(properties);
+			kafkaServer = new KafkaServer(kafkaConfig, null);
+			kafkaServer.startup();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
 	}
-	
-    @BeforeClass
-    public static void startKafka() throws Exception {
 
-    }
-	
-    @BeforeClass
-    public static void startActiveMQ() throws Exception {
+	@BeforeClass
+	public static void startKafka() throws Exception {
 
-    }
+	}
 
-	@Override
-	public boolean isUseAdviceWith() {
-		return true;
+	@BeforeClass
+	public static void startActiveMQ() throws Exception {
+
 	}
 
 	@Override
-	public boolean isUseDebugger() {
-		// must enable debugger
-		return true;
-	}
+	protected String setConfigAdminInitialConfiguration(Properties props) {
+		zookeeperPort = getAvailablePort(2181, 2281);
+		kafkaPort = getAvailablePort(9092, 9192);
 
-	@Override
-	public String isMockEndpoints() {
-		return "*";
+		props.put("zookeeperport", String.valueOf(zookeeperPort));
+		props.put("kafkaport", String.valueOf(kafkaPort));
+		return "org.opennms.netmgt.trapd.handler.kafka";
 	}
 
 	@SuppressWarnings("rawtypes")
@@ -127,15 +127,15 @@ public class TrapdHandlerKafkaIT extends CamelBlueprintTestSupport {
 	protected void addServicesOnStartup(Map<String, KeyValueHolder<Object, Dictionary>> services) {
 		// Register any mock OSGi services here
 		services.put( TrapNotificationHandler.class.getName(), new KeyValueHolder<Object, Dictionary>(new TrapNotificationHandlerCamelImpl("seda:handleMessage"), new Properties()));
-		
+
 		//creating kafka component
 		Properties props = new Properties();
 		props.setProperty("alias", "opennms.broker");
-		
+
 		KafkaComponent kafka = new KafkaComponent();
-		kafka.createComponentConfiguration().setBaseUri("kafka://localhost:9092");
-        services.put( Component.class.getName(),
-                new KeyValueHolder<Object, Dictionary>( kafka, props ) );
+		kafka.createComponentConfiguration().setBaseUri("kafka://localhost:" + kafkaPort);
+		services.put( Component.class.getName(),
+				new KeyValueHolder<Object, Dictionary>( kafka, props ) );
 	}
 
 	// The location of our Blueprint XML files to be used for testing
@@ -146,30 +146,28 @@ public class TrapdHandlerKafkaIT extends CamelBlueprintTestSupport {
 
 	@Test
 	public void testTrapd() throws Exception {
-		
-        Map<String, Object> parms = new HashMap<String, Object>();
-        parms.put("port",61616);	
-        SimpleRegistry registry = new SimpleRegistry();
-        CamelContext trapd = new DefaultCamelContext(registry);
-        trapd.addComponent("kafka", new KafkaComponent());
-        trapd.addRoutes(new RouteBuilder() {
-            @Override
-            public void configure() throws Exception {
 
-                from("seda:handleMessage").convertBodyTo(TrapNotification.class).process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        exchange.getIn().setBody("Test Message from Camel Kafka Component Final",String.class);
-                        exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY,simple("${body.hostname}"));
-                    }
-                }).log("address:${body.sourceAddress}").log("port: ${body.port}").transform(simple("${body.byteBuffer}")).convertBodyTo(String.class).log(body().toString()).to("kafka:localhost:9092?topic=trapd;serializerClass=kafka.serializer.StringEncoder");
-            
-            }
-        });
-        
-        trapd.start();
+		SimpleRegistry registry = new SimpleRegistry();
+		CamelContext trapd = new DefaultCamelContext(registry);
+		trapd.addComponent("kafka", new KafkaComponent());
+		trapd.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+
+				from("seda:handleMessage").convertBodyTo(TrapNotification.class).process(new Processor() {
+					@Override
+					public void process(Exchange exchange) throws Exception {
+						exchange.getIn().setBody("Test Message from Camel Kafka Component Final",String.class);
+						exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY,simple("${body.hostname}"));
+					}
+				}).log("address:${body.sourceAddress}").log("port: ${body.port}").transform(simple("${body.byteBuffer}")).convertBodyTo(String.class).log(body().toString()).to("kafka:localhost:" + kafkaPort + "?topic=trapd;serializerClass=kafka.serializer.StringEncoder");
+
+			}
+		});
+
+		trapd.start();
 	}
-	
+
 	@After
 	public void shutDownKafka(){
 		kafkaServer.shutdown();
