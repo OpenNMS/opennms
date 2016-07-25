9b4b949a9c3d2a7ae2516579e970baf5f9fc151d
DJ Gregor <dj@gregor.com>
2016-07-24 20:00:05 -0500

    Make the NorthboundAlarmTest when you're not in the US/Eastern timezone

--- opennms-alarms/api/src/test/java/org/opennms/netmgt/alarmd/api/NorthboundAlarmTest.java
+++ opennms-alarms/api/src/test/java/org/opennms/netmgt/alarmd/api/NorthboundAlarmTest.java
@@ -38,7 +38,12 @@ import org.opennms.netmgt.alarmd.api.NorthboundAlarm.AlarmType;
 import org.opennms.netmgt.model.OnmsSeverity;
 import org.opennms.netmgt.model.TroubleTicketState;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class NorthboundAlarmTest extends XmlTestNoCastor<NorthboundAlarm> {
+
+    private static final String FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"; // 1969-12-31T19:00:00.001-05:00 -- note the timezone needs the ":" added after SimpleDateFormat formats it
 
     public NorthboundAlarmTest(NorthboundAlarm sampleObject, Object sampleXml) {
         super(sampleObject, sampleXml, null);
@@ -47,6 +52,13 @@ public class NorthboundAlarmTest extends XmlTestNoCastor<NorthboundAlarm> {
     @Override
     protected String getSchemaFile() {
         return "target/classes/xsds/northbound-alarm.xsd";
+    }
+
+    private static String fixDate(Date d) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(FORMAT);
+        StringBuffer b = new StringBuffer(dateFormat.format(d));
+        b.insert(b.length() - 2, ':');
+        return b.toString();
     }
 
     @Parameters
@@ -61,16 +73,16 @@ public class NorthboundAlarmTest extends XmlTestNoCastor<NorthboundAlarm> {
                      "<node-sysobjectid>.1.3.6</node-sysobjectid>\n" +
                      "<node-foreignsource>fs</node-foreignsource>\n" +
                      "<node-foreignid>fid</node-foreignid>\n" +
-                     "<ack-time>1969-12-31T19:00:00.001-05:00</ack-time>\n" +
+                     "<ack-time>" + fixDate(new Date(1)) + "</ack-time>\n" +
                      "<ack-user>admin</ack-user>\n" +
                      "<alarm-type>PROBLEM</alarm-type>\n" +
                      "<app-dn>some-app-dn</app-dn>\n" +
                      "<clear-key>some-clear-key</clear-key>\n" +
                      "<count>1</count>\n" +
                      "<description>some description</description>\n" +
-                     "<first-occurrence>1969-12-31T19:00:00.002-05:00</first-occurrence>\n" +
+                     "<first-occurrence>" + fixDate(new Date(2)) + "</first-occurrence>\n" +
                      "<ip-address>127.0.0.127</ip-address>\n" +
-                     "<last-occurrence>1969-12-31T19:00:00.004-05:00</last-occurrence>\n" +
+                     "<last-occurrence>" + fixDate(new Date(4)) + "</last-occurrence>\n" +
                      "<log-messsage>logmsg</log-messsage>\n" +
                      "<object-instance>instance</object-instance>\n" +
                      "<object-type>type</object-type>\n" +
@@ -80,8 +92,8 @@ public class NorthboundAlarmTest extends XmlTestNoCastor<NorthboundAlarm> {
                      "<alarm-key>key</alarm-key>\n" +
                      "<service>service</service>\n" +
                      "<severity>CRITICAL</severity>\n" +
-                     "<suppressed>1969-12-31T19:00:00.005-05:00</suppressed>\n" +
-                     "<suppressed-until>1969-12-31T19:00:00.006-05:00</suppressed-until>\n" +
+                     "<suppressed>" + fixDate(new Date(5)) + "</suppressed>\n" +
+                     "<suppressed-until>" + fixDate(new Date(6)) + "</suppressed-until>\n" +
                      "<suppressed-by>me</suppressed-by>\n" +
                      "<ticket-id>NMS-8068</ticket-id>\n" +
                      "<ticket-state>OPEN</ticket-state>\n" +
