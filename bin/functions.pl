6c9abad36432005ad9240448ca75b5e8c2feef60
Benjamin Reed <ranger@opennms.org>
2016-07-28 09:22:31 -0500

    bump memory up a little bit

--- bin/functions.pl
+++ bin/functions.pl
@@ -85,11 +85,7 @@ delete $ENV{'M2_HOME'};
 # maven options
 $MAVEN_OPTS = $ENV{'MAVEN_OPTS'};
 if (not defined $MAVEN_OPTS or $MAVEN_OPTS eq '') {
-	if (defined $TESTS) {
-		$MAVEN_OPTS = "-Xmx2048m -XX:ReservedCodeCacheSize=512m";
-	} else {
-		$MAVEN_OPTS = "-Xmx1536m -XX:ReservedCodeCacheSize=512m";
-	}
+	$MAVEN_OPTS = "-Xmx2048m -XX:ReservedCodeCacheSize=512m";
 }
 
 if (not $MAVEN_OPTS =~ /TieredCompilation/) {
