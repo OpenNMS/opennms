711134f9b53aba9fe2fe08c617c6bf88021291e3
Seth Leger <seth@opennms.org>
2016-09-13 16:58:04 -0400

    Updated SyslogdBroadcastEventProcessorTest to work correctly with the InterfaceToNodeCache event listener.

--- features/events/syslog/src/test/java/org/opennms/netmgt/syslogd/SyslogdBroadcastEventProcessorTest.java
+++ features/events/syslog/src/test/java/org/opennms/netmgt/syslogd/SyslogdBroadcastEventProcessorTest.java
@@ -34,9 +34,11 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.DatagramPacket;
+import java.net.InetAddress;
 
 import org.exolab.castor.xml.MarshalException;
 import org.exolab.castor.xml.ValidationException;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -48,6 +50,7 @@ import org.opennms.core.utils.InetAddressUtils;
 import org.opennms.netmgt.config.SyslogdConfigFactory;
 import org.opennms.netmgt.dao.DatabasePopulator;
 import org.opennms.netmgt.dao.api.DistPollerDao;
+import org.opennms.netmgt.dao.api.InterfaceToNodeCache;
 import org.opennms.netmgt.dao.api.MonitoringLocationDao;
 import org.opennms.netmgt.dao.support.InterfaceToNodeCacheEventProcessor;
 import org.opennms.netmgt.events.api.EventConstants;
@@ -74,9 +77,14 @@ import org.springframework.transaction.annotation.Transactional;
 @JUnitConfigurationEnvironment
 @JUnitTemporaryDatabase
 public class SyslogdBroadcastEventProcessorTest {
+
     private static final Logger LOG = LoggerFactory.getLogger(SyslogdBroadcastEventProcessorTest.class);
-    private static final String s_systemId = DistPollerDao.DEFAULT_DIST_POLLER_ID;
-    private static final String s_location = MonitoringLocationDao.DEFAULT_MONITORING_LOCATION_ID;
+
+    private static final String SYSTEM_ID = DistPollerDao.DEFAULT_DIST_POLLER_ID;
+    private static final String LOCATION = MonitoringLocationDao.DEFAULT_MONITORING_LOCATION_ID;
+
+    @Autowired
+    private InterfaceToNodeCache m_cache;
 
     @Autowired
     private InterfaceToNodeCacheEventProcessor m_processor;
@@ -87,8 +95,15 @@ public class SyslogdBroadcastEventProcessorTest {
     @Before
     public void setUp() throws Exception {
         MockLogAppender.setupLogging(true, "DEBUG");
-        loadSyslogConfiguration("/etc/syslogd-rfc-configuration.xml");
+
+        // Populate the database but don't sync the InterfaceToNodeCache until we're
+        // inside each test so that we can test different behaviors
         m_databasePopulator.populateDatabase();
+    }
+
+    @After
+    public void tearDown() {
+        m_cache.clear();
     }
 
     private SyslogdConfigFactory loadSyslogConfiguration(final String configuration) throws IOException, MarshalException, ValidationException {
@@ -99,45 +114,101 @@ public class SyslogdBroadcastEventProcessorTest {
 
     @Test
     @Transactional
-    public void testUpdateSyslogIPMgr() throws Exception {
-        final byte[] bytes = "<34>1 2010-08-19T22:14:15.000Z localhost - - - - BOMfoo0: load test 0 on tty1\0".getBytes();
-        final DatagramPacket pkt = new DatagramPacket(bytes, bytes.length, InetAddressUtils.ONE_TWENTY_SEVEN, SyslogClient.PORT);
-        final InterfaceToNodeCacheEventProcessor eventProcessor = m_processor;
+    public void testUpdateInterfaceToNodeCache() throws Exception {
+
+        // The cache has not been sync'd with the database yet
+        assertEquals(0, m_cache.size());
+
         final Integer nodeId = m_databasePopulator.getNode1().getId();
+        // One of the interfaces on node1
+        final InetAddress addr = InetAddressUtils.addr("192.168.1.3");
+
+        final byte[] bytes = ("<34>1 2010-08-19T22:14:15.000Z " + InetAddressUtils.str(addr) + " - - - - BOMfoo0: load test 0 on tty1\0").getBytes();
+        final DatagramPacket pkt = new DatagramPacket(bytes, bytes.length, addr, SyslogClient.PORT);
 
         // Create a mock SyslogdConfig
         SyslogdConfigFactory config = loadSyslogConfiguration("/etc/syslogd-rfc-configuration.xml");
 
         // Create a new SyslogConnection and call it to create the processed event
-        SyslogConnection connection = new SyslogConnection(pkt, config, s_systemId, s_location);
+        SyslogConnection connection = new SyslogConnection(pkt, config, SYSTEM_ID, LOCATION);
         SyslogProcessor processor = connection.call();
         // The node is not present so nodeID should be blank
         Long foundid = processor.getEvent().getNodeid();
-        LOG.debug("found id: {}", foundid);
+        LOG.debug("Found node ID: {}", foundid);
         assertTrue("Node ID was unexpectedly present: " + processor.getEvent().getNodeid(), foundid < 1);
 
         // Simulate a nodeGainedInterface event
         EventBuilder builder = new EventBuilder(EventConstants.NODE_GAINED_INTERFACE_EVENT_UEI, getClass().getSimpleName());
         builder.setNodeid(nodeId);
-        builder.setInterface(InetAddressUtils.ONE_TWENTY_SEVEN);
-        eventProcessor.handleNodeGainedInterface(builder.getEvent());
+        builder.setInterface(addr);
+        m_processor.handleNodeGainedInterface(builder.getEvent());
 
-        connection = new SyslogConnection(pkt, config, s_systemId, s_location);
+        // The entry was added to the cache
+        assertEquals(1, m_cache.size());
+
+        connection = new SyslogConnection(pkt, config, SYSTEM_ID, LOCATION);
         processor = connection.call();
         // Assert that the event was associated with the node correctly
         foundid = processor.getEvent().getNodeid();
-        LOG.debug("found id: {}", foundid);
+        LOG.debug("Found node ID: {}", foundid);
         assertEquals("Node ID was not present: " + processor.getEvent().getNodeid(), Long.valueOf(nodeId.longValue()), foundid);
 
         // Simulate an interfaceDeleted event
         builder = new EventBuilder(EventConstants.INTERFACE_DELETED_EVENT_UEI, getClass().getSimpleName());
         builder.setNodeid(nodeId);
-        builder.setInterface(InetAddressUtils.ONE_TWENTY_SEVEN);
-        eventProcessor.handleInterfaceDeleted(builder.getEvent());
+        builder.setInterface(addr);
+        m_processor.handleInterfaceDeleted(builder.getEvent());
 
-        connection = new SyslogConnection(pkt, config, s_systemId, s_location);
+        // The entry was removed from the cache
+        assertEquals(0, m_cache.size());
+
+        connection = new SyslogConnection(pkt, config, SYSTEM_ID, LOCATION);
         processor = connection.call();
         // Assert that the event is no longer associated with the node
         assertTrue("Node ID was unexpectedly present: " + processor.getEvent().getNodeid(), processor.getEvent().getNodeid() < 1);
+    }
+
+    @Test
+    @Transactional
+    public void testSyncWithDatabaseThenClear() throws Exception {
+
+        // The cache has not been sync'd with the database yet
+        assertEquals(0, m_cache.size());
+
+        final Integer nodeId = m_databasePopulator.getNode1().getId();
+        // One of the interfaces on node1
+        final InetAddress addr = InetAddressUtils.addr("192.168.1.3");
+
+        final byte[] bytes = ("<34>1 2010-08-19T22:14:15.000Z " + InetAddressUtils.str(addr) + " - - - - BOMfoo0: load test 0 on tty1\0").getBytes();
+        final DatagramPacket pkt = new DatagramPacket(bytes, bytes.length, addr, SyslogClient.PORT);
+
+        // Create a mock SyslogdConfig
+        SyslogdConfigFactory config = loadSyslogConfiguration("/etc/syslogd-rfc-configuration.xml");
+
+        // Sync the cache with the database
+        m_cache.dataSourceSync();
+
+        // The cache has entries from the database
+        assertTrue(0 < m_cache.size());
+
+        // Create a new SyslogConnection and call it to create the processed event
+        SyslogConnection connection = new SyslogConnection(pkt, config, SYSTEM_ID, LOCATION);
+        SyslogProcessor processor = connection.call();
+        // The node is in the database so it should already be in the cache
+        Long foundid = processor.getEvent().getNodeid();
+        LOG.debug("Found node ID: {}", foundid);
+        assertEquals("Node ID was not present: " + processor.getEvent().getNodeid(), Long.valueOf(nodeId.longValue()), foundid);
+
+        // Clear the cache
+        m_cache.clear();
+        assertEquals(0, m_cache.size());
+
+        // Create a new SyslogConnection and call it to create the processed event
+        connection = new SyslogConnection(pkt, config, SYSTEM_ID, LOCATION);
+        processor = connection.call();
+        // The node is in the database so it should already be in the cache
+        foundid = processor.getEvent().getNodeid();
+        LOG.debug("Found node ID: {}", foundid);
+        assertTrue("Node ID was unexpectedly present: " + processor.getEvent().getNodeid(), foundid < 1);
     }
 }
