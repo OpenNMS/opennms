94330e7456f66759bc5ea5ce57975079dcf4f127
Chandra Gorantla <cgorantla@raindanceit.com>
2016-10-17 12:43:40 -0500

    NMS-8812: Consider location=Default and no location as same.

--- opennms-config-jaxb/src/main/java/org/opennms/netmgt/config/snmp/AddressSnmpConfigVisitor.java
+++ opennms-config-jaxb/src/main/java/org/opennms/netmgt/config/snmp/AddressSnmpConfigVisitor.java
@@ -72,14 +72,19 @@ public class AddressSnmpConfigVisitor extends AbstractSnmpConfigVisitor implemen
     private String m_location = null;
     private Definition m_definitionWithLocation = null;
     private Definition m_prevMatchedDefinition = null;
+    static private final String DEFAULT_LOCATION ="Default";
 
     public AddressSnmpConfigVisitor(final InetAddress addr) {
         m_address = addr;
+        m_location = DEFAULT_LOCATION;
     }
 
     public AddressSnmpConfigVisitor(final InetAddress addr, String location) {
         m_address = addr;
         m_location = location;
+        if (StringUtils.isBlank(m_location)) {
+            m_location = DEFAULT_LOCATION;
+        }
     }
     public void visitSnmpConfig(final SnmpConfig config) {
         m_currentConfig = config;
@@ -149,6 +154,9 @@ public class AddressSnmpConfigVisitor extends AbstractSnmpConfigVisitor implemen
         if (m_matchedDefinition != null && Objects.equals(m_currentDefinition, m_matchedDefinition)) {
 
             String location = m_matchedDefinition.getLocation();
+            if (StringUtils.isBlank(location)) {
+                location = DEFAULT_LOCATION;
+            }
             // If location doesn't match, it's not a matched definition
             if (!Objects.equals(location, m_location)) {
                 // Retrieve if there is a matched Definition before
