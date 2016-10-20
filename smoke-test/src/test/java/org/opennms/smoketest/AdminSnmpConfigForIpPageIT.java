94818d6dc4b11f03f43f6150080ffd957c3edef7
Benjamin Reed <ranger@opennms.org>
2016-10-20 16:59:21 -0400

    Revert "fix smoke test failure after NMS-8789 changes"
    This reverts commit 4d789bfd1339c26ac35db1be9c40a4314b06fcee.

--- smoke-test/src/test/java/org/opennms/smoketest/AdminSnmpConfigForIpPageIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/AdminSnmpConfigForIpPageIT.java
@@ -122,6 +122,7 @@ public class AdminSnmpConfigForIpPageIT extends OpenNMSSeleniumTestCase {
     @Test
     public void testIntegerValidation() {
         final String defaultValidationErrorTemplate = "%s is not a valid %s. Please enter a number greater than 0 or leave it empty.";
+        final String geZeroValidationErrorTemplate = "%s is not a valid %s. Please enter a number greater than or equal to 0, or leave it empty.";
         final String maxRequestSizeErrorTemplate = "%s is not a valid %s. Please enter a number greater or equal than 484 or leave it empty.";
         final String[] integerFields = new String[]{
                 "timeout", 
@@ -139,7 +140,7 @@ public class AdminSnmpConfigForIpPageIT extends OpenNMSSeleniumTestCase {
         "Max Request Size"};
         final String[] errorMessages = new String[]{
                 defaultValidationErrorTemplate, 
-                defaultValidationErrorTemplate,
+                geZeroValidationErrorTemplate, 
                 defaultValidationErrorTemplate, 
                 defaultValidationErrorTemplate, 
                 defaultValidationErrorTemplate,
