6ccf486ab05fab24e2877d78a5ef3adb4310e238
Seth Leger <seth@opennms.org>
2016-12-01 11:56:21 -0500

    HZN-800: Added embedded Elasticsearch and Kafka to SyslogTest.

--- smoke-test/src/test/java/org/opennms/smoketest/minion/SyslogTest.java
+++ smoke-test/src/test/java/org/opennms/smoketest/minion/SyslogTest.java
@@ -28,11 +28,13 @@
 package org.opennms.smoketest.minion;
 
 import static com.jayway.awaitility.Awaitility.await;
+import static com.jayway.awaitility.Awaitility.with;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
@@ -42,17 +44,24 @@ import java.net.Inet4Address;
 import java.net.InetSocketAddress;
 import java.util.Date;
 
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.Assume;
 import org.junit.Before;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.opennms.core.camel.IndexNameFunction;
 import org.opennms.core.criteria.Criteria;
 import org.opennms.core.criteria.CriteriaBuilder;
+import org.opennms.core.test.elasticsearch.JUnitElasticsearchServer;
+import org.opennms.core.test.kafka.JUnitKafkaServer;
 import org.opennms.netmgt.dao.api.EventDao;
 import org.opennms.netmgt.dao.hibernate.EventDaoHibernate;
 import org.opennms.netmgt.dao.hibernate.MinionDaoHibernate;
 import org.opennms.netmgt.dao.hibernate.MonitoredServiceDaoHibernate;
 import org.opennms.netmgt.dao.hibernate.NodeDaoHibernate;
+import org.opennms.netmgt.events.api.EventConstants;
 import org.opennms.netmgt.model.OnmsEvent;
 import org.opennms.netmgt.model.OnmsNode;
 import org.opennms.netmgt.model.minion.OnmsMinion;
@@ -71,6 +80,8 @@ import org.opennms.test.system.api.TestEnvironmentBuilder;
  * @author jwhite
  */
 public class SyslogTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SyslogTest.class);
 
     private static TestEnvironment minionSystem;
 
@@ -97,6 +108,12 @@ public class SyslogTest {
         }
     }
 
+    @ClassRule
+    public static final JUnitElasticsearchServer ELASTICSEARCH = new JUnitElasticsearchServer();
+
+    @ClassRule
+    public static final JUnitKafkaServer KAFKA = new JUnitKafkaServer();
+
     @Before
     public void checkForDocker() {
         Assume.assumeTrue(OpenNMSSeleniumTestCase.isDockerEnabled());
@@ -114,7 +131,7 @@ public class SyslogTest {
         final Date startOfTest = new Date();
 
         // Send a syslog packet to the Minion syslog listener
-        this.sendMessage("myhost");
+        SyslogTest.sendMessage("myhost");
 
         // Parsing the message correctly relies on the customized syslogd-configuration.xml that is part of the OpenNMS image
         final EventDao eventDao = this.daoFactory.getDao(EventDaoHibernate.class);
@@ -144,7 +161,7 @@ public class SyslogTest {
                       is(1));
 
         // Send the initial message
-        this.sendMessage(sender);
+        SyslogTest.sendMessage(sender);
 
         // Wait for the syslog message
         await().atMost(1, MINUTES).pollInterval(5, SECONDS)
@@ -185,7 +202,7 @@ public class SyslogTest {
                       notNullValue());
 
         // Send the second message
-        this.sendMessage(sender);
+        SyslogTest.sendMessage(sender);
 
         // Wait for the second message with the node assigned
         await().atMost(1, MINUTES).pollInterval(5, SECONDS)
@@ -198,7 +215,42 @@ public class SyslogTest {
                       is(1));
     }
 
-    private void sendMessage(final String host) throws IOException {
+    /**
+     * This test will send syslog messages and verify that they have been processed into
+     * Elasticsearch records.
+     */
+    @Test
+    @Ignore
+    public void testElasticsearchSyslogs() throws Exception {
+        Date date = new Date();
+
+        with().pollInterval(1, SECONDS).await().atMost(30, SECONDS).until(() -> {
+            try {
+                // Refresh the "opennms-2011.01" index
+                ELASTICSEARCH.getClient().admin().indices().prepareRefresh(new IndexNameFunction().apply("opennms", date)).execute().actionGet();
+
+                // Search for all entries in the index
+                SearchResponse response = ELASTICSEARCH.getClient()
+                    // Search the index that the event above created
+                    .prepareSearch(new IndexNameFunction().apply("opennms", date)) // opennms-2011.01
+                    .setQuery(QueryBuilders.matchAllQuery())
+                    .execute()
+                    .actionGet();
+
+                LOG.debug("RESPONSE: {}", response.toString());
+
+                assertEquals("ES search hits was not equal to 1", 1, response.getHits().totalHits());
+                assertEquals("Event UEI did not match", EventConstants.NEW_SUSPECT_INTERFACE_EVENT_UEI, response.getHits().getAt(0).getSource().get("eventuei"));
+                assertEquals("Event IP address did not match", "4.2.2.2", response.getHits().getAt(0).getSource().get("ipaddr"));
+            } catch (Throwable e) {
+                LOG.warn(e.getMessage(), e);
+                return false;
+            }
+            return true;
+        });
+    }
+
+    private static void sendMessage(final String host) throws IOException {
         final InetSocketAddress syslogAddr = minionSystem.getServiceAddress(ContainerAlias.MINION, 1514, "udp");
         byte[] message = ("<190>Mar 11 08:35:17 " + host + " 30128311: Mar 11 08:35:16.844 CST: %SEC-6-IPACCESSLOGP: list in110 denied tcp 192.168.10.100(63923) -> 192.168.11.128(1521), 1 packet\n").getBytes();
         DatagramPacket packet = new DatagramPacket(message, message.length, syslogAddr.getAddress(), syslogAddr.getPort());
