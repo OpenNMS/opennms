820e0f603ec39b776d06d3b8d793d213af08c0c1
cernerpradeep <pradeep.srivatsa@cerner.com>
2016-12-09 13:30:01 +0530

    Removed complete processing from Camel routes in kafka blueprint. Moved all the code to CustomSylogHandler

--- features/events/syslog/src/main/java/org/opennms/netmgt/syslogd/CustomSyslogHandler.java
+++ features/events/syslog/src/main/java/org/opennms/netmgt/syslogd/CustomSyslogHandler.java
@@ -1,8 +1,15 @@
 package org.opennms.netmgt.syslogd;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import kafka.Kafka;
+
 import org.apache.camel.Endpoint;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.kafka.KafkaConstants;
+import org.opennms.core.camel.RandomPartitionKeyGenerator;
 import org.opennms.core.xml.JaxbUtils;
 
 public class CustomSyslogHandler implements SyslogDTOHandler{
@@ -13,11 +20,26 @@ public class CustomSyslogHandler implements SyslogDTOHandler{
     @EndpointInject(uri = "seda:handleMessage", context = "syslogdHandlerKafkaContext")
     private Endpoint endpoint;
     
+    private RandomPartitionKeyGenerator randomPartitionKeyGenerator;
+    
 
 	@Override
 	public void handleSyslogDTO(SyslogDTO message) {
-		template.sendBody(endpoint,JaxbUtils.marshal(message));
+		Map<String, Object> headers = new HashMap<>();
+        headers.put(KafkaConstants.PARTITION_KEY, randomPartitionKeyGenerator.getPartitionKey());
+		template.sendBodyAndHeader(endpoint,JaxbUtils.marshal(message),headers);
 		
+	}
+
+
+	public RandomPartitionKeyGenerator getRandomPartitionKeyGenerator() {
+		return randomPartitionKeyGenerator;
+	}
+
+
+	public void setRandomPartitionKeyGenerator(
+			RandomPartitionKeyGenerator randomPartitionKeyGenerator) {
+		this.randomPartitionKeyGenerator = randomPartitionKeyGenerator;
 	}
 
 }
