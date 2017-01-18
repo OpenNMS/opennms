b6715dcc38f2b366a6f06547b81f03167f364af1
Markus von Rüden <mvr@opennms.com>
2017-01-18 17:09:23 +0100

    NMS-8978: Make threshold configurable

--- features/bsm/service/impl/src/main/java/org/opennms/netmgt/bsm/service/internal/AlarmProviderImpl.java
+++ features/bsm/service/impl/src/main/java/org/opennms/netmgt/bsm/service/internal/AlarmProviderImpl.java
@@ -39,9 +39,19 @@ import org.opennms.netmgt.bsm.service.AlarmProvider;
 import org.opennms.netmgt.bsm.service.model.AlarmWrapper;
 import org.opennms.netmgt.dao.api.AlarmDao;
 import org.opennms.netmgt.model.OnmsAlarm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class AlarmProviderImpl implements AlarmProvider {
+
+    private static final Logger LOG = LoggerFactory.getLogger(AlarmProviderImpl.class);
+
+    private static final long DEFAULT_THRESHOLD = 1000;
+
+    private static final String THRESHOLD_KEY = "org.opennms.features.bsm.reductionKeyFullLoadThreshold";
+
+    private final long threshold = getThreshold();
 
     @Autowired
     private AlarmDao alarmDao;
@@ -51,13 +61,29 @@ public class AlarmProviderImpl implements AlarmProvider {
         if (reductionKeys == null || reductionKeys.isEmpty()) {
             return new HashMap<>();
         }
-        if (reductionKeys.size() <= 1000) {
+        if (reductionKeys.size() <= threshold) {
             List<OnmsAlarm> alarms = alarmDao.findMatching(new CriteriaBuilder(OnmsAlarm.class).in("reductionKey", reductionKeys).toCriteria());
             return alarms.stream().collect(Collectors.toMap(alarm -> alarm.getReductionKey(), alarm -> new AlarmWrapperImpl(alarm)));
         } else {
             return alarmDao.findAll().stream()
                     .filter(a -> reductionKeys.contains(a.getReductionKey()))
                     .collect(Collectors.toMap(alarm -> alarm.getReductionKey(), alarm -> new AlarmWrapperImpl(alarm)));
+        }
+    }
+
+    private static long getThreshold() {
+        String thresholdProperty = System.getProperty(THRESHOLD_KEY, Long.toString(DEFAULT_THRESHOLD));
+        try {
+            long threshold = Long.valueOf(thresholdProperty);
+            if (threshold <= 0) {
+                LOG.warn("Defined threshold must be greater than 0, but was {}. Falling back to default: {}", threshold, DEFAULT_THRESHOLD);
+                return DEFAULT_THRESHOLD;
+            }
+            LOG.debug("Using threshold {}", threshold);
+            return threshold;
+        } catch (Exception ex) {
+            LOG.warn("The defined threshold {} could not be interpreted as long value. Falling back to default: {}", thresholdProperty, DEFAULT_THRESHOLD);
+            return DEFAULT_THRESHOLD;
         }
     }
 }
