eb7304c58d655d8c5ef703a7e622dcd8725bcc18
Jesse White <jesse@opennms.org>
2017-03-06 15:49:10 -0500

    NMS-9186: Validate that the Drools examples load correctly.

---
+++ opennms-correlation/drools-correlation-engine/src/test/java/org/opennms/netmgt/correlation/drools/DroolsCorrelationEngineBuilderExamplesIT.java
@@ -0,0 +1,74 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2017-2017 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.netmgt.correlation.drools;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opennms.core.test.OpenNMSJUnit4ClassRunner;
+import org.opennms.core.test.db.annotations.JUnitTemporaryDatabase;
+import org.opennms.netmgt.correlation.CorrelationEngineRegistrar;
+import org.opennms.test.JUnitConfigurationEnvironment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(OpenNMSJUnit4ClassRunner.class)
+@ContextConfiguration(locations={
+        "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
+        "classpath:/META-INF/opennms/applicationContext-minimal-conf.xml",
+        "classpath:META-INF/opennms/applicationContext-soa.xml",
+        "classpath:META-INF/opennms/applicationContext-mockDao.xml",
+        "classpath*:/META-INF/opennms/component-dao.xml",
+        "classpath:META-INF/opennms/applicationContext-daemon.xml",
+        "classpath:META-INF/opennms/mockEventIpcManager.xml",
+        "classpath:META-INF/opennms/applicationContext-correlator.xml",
+        "classpath:test-context-examples.xml"
+})
+@JUnitConfigurationEnvironment
+@JUnitTemporaryDatabase
+public class DroolsCorrelationEngineBuilderExamplesIT {
+
+    @Autowired
+    private CorrelationEngineRegistrar mockCorrelator;
+
+    /**
+     * This test is used to verify that the correlation engines we provide
+     * in the example directory load successfully.
+     *
+     * The @JUnitConfigurationEnvironment sets the 'opennms.home' system
+     * property to the examples directory in the 'opennms-base-assembly'
+     * project, which is then referenced in 'test-context-examples.xml'.
+     */
+    @Test
+    public void didLoadCorrelationEngines() {
+        assertTrue("Expected at least 1 correlation engine to be registered.",
+                mockCorrelator.getEngines().size() > 0);
+    }
+}
