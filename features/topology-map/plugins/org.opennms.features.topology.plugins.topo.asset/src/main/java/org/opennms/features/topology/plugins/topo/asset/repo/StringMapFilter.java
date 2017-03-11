1ea71c4d0cf2d1c59d237ee3515cb6dd8aca9d08
cgallen <cgallen@opennms.org>
2017-03-11 10:53:24 +0000

    NMS-9128: minor documentation

--- features/topology-map/plugins/org.opennms.features.topology.plugins.topo.asset/src/main/java/org/opennms/features/topology/plugins/topo/asset/repo/StringMapFilter.java
+++ features/topology-map/plugins/org.opennms.features.topology.plugins.topo.asset/src/main/java/org/opennms/features/topology/plugins/topo/asset/repo/StringMapFilter.java
@@ -16,12 +16,13 @@ public class StringMapFilter {
 	}
 
 	/**
-	 * returns filtered node info repository filter name value pair filter =
-	 * null do not filter results key1=value1&key1=value2 (additive i.e. OR of
-	 * key 1 values key1=value1,value2,value3 (additive i.e. OR of key 1 values)
-	 * alternative) key1=value1,key2=value2,value3 etc (AND of keys - both must
-	 * apply) key1=!value1 (negation - key NOT value1) key1=~regex (regex match
-	 * of key) or key1=!~regex
+	 * returns filtered node info repository filter 
+	 * filter = null do not filter results
+	 * key1=value1&key1=value2 (additive i.e. OR of key 1 values
+	 * key1=value1,value2,value3 (additive i.e. OR of key 1 values) alternative)
+	 * key1=value1,key2=value2,value3 etc (AND of keys - both must apply)
+	 * key1=!value1 (negation - key NOT value1)
+	 * key1=~regex (regex match of key) or key1=!~regex
 	 * 
 	 * @param filter
 	 *            filter can be derived from url query
