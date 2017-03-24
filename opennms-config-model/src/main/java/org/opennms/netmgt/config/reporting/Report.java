dd3e657fc22764089c87fd70a284f705eb1ac0e0
Benjamin Reed <ranger@opennms.org>
2017-03-24 11:02:37 -0400

    add Optional where appropriate in reporting config

--- opennms-config-model/src/main/java/org/opennms/netmgt/config/reporting/Report.java
+++ opennms-config-model/src/main/java/org/opennms/netmgt/config/reporting/Report.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  * 
- * Copyright (C) 2017-2017 The OpenNMS Group, Inc.
+ * Copyright (C) 2017 The OpenNMS Group, Inc.
  * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
  * 
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
@@ -29,254 +29,154 @@
 package org.opennms.netmgt.config.reporting;
 
 
+import java.io.Serializable;
 import java.util.Objects;
+import java.util.Optional;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- * Class Report.
- * 
- * @version $Revision$ $Date$
- */
+import org.opennms.core.xml.ValidateUsing;
+import org.opennms.netmgt.config.utils.ConfigUtils;
+
 @XmlRootElement(name = "report")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Report implements java.io.Serializable {
-    private static final long serialVersionUID = 1L;
+@ValidateUsing("reporting.xsd")
+public class Report implements Serializable {
+    private static final long serialVersionUID = 2L;
 
     /**
      * the name of this report
      */
     @XmlAttribute(name = "id", required = true)
-    private String id;
+    private String m_id;
 
     /**
      * type of this report (calendar/classic)
      */
     @XmlAttribute(name = "type", required = true)
-    private String type;
+    private String m_type;
 
     @XmlElement(name = "parameters")
-    private Parameters parameters;
+    private Parameters m_parameters;
 
     /**
      * template to convert to display the report
      *  in PDF format
      */
     @XmlElement(name = "pdf-template")
-    private String pdfTemplate;
+    private String m_pdfTemplate;
 
     /**
      * template to convert to display the report
      *  in PDF format with embedded SVG
      */
     @XmlElement(name = "svg-template")
-    private String svgTemplate;
+    private String m_svgTemplate;
 
     /**
      * template to convert to display the report
      *  in HTML format
      */
     @XmlElement(name = "html-template")
-    private String htmlTemplate;
+    private String m_htmlTemplate;
 
     /**
      * path to the logo file
      */
     @XmlElement(name = "logo", required = true)
-    private String logo;
+    private String m_logo;
 
     public Report() {
     }
 
-    /**
-     * Overrides the Object.equals method.
-     * 
-     * @param obj
-     * @return true if the objects are equal.
-     */
+    public String getId() {
+        return m_id;
+    }
+
+    public void setId(final String id) {
+        m_id = ConfigUtils.assertNotEmpty(id, "id");
+    }
+
+    public String getType() {
+        return m_type;
+    }
+
+    public void setType(final String type) {
+        m_type = ConfigUtils.assertNotEmpty(type, "type");
+    }
+
+    public Optional<Parameters> getParameters() {
+        return Optional.ofNullable(m_parameters);
+    }
+
+    public void setParameters(final Parameters parameters) {
+        m_parameters = parameters;
+    }
+
+    public Optional<String> getPdfTemplate() {
+        return Optional.ofNullable(m_pdfTemplate);
+    }
+
+    public void setPdfTemplate(final String pdfTemplate) {
+        m_pdfTemplate = ConfigUtils.normalizeString(pdfTemplate);
+    }
+
+    public Optional<String> getSvgTemplate() {
+        return Optional.ofNullable(m_svgTemplate);
+    }
+
+    public void setSvgTemplate(final String svgTemplate) {
+        m_svgTemplate = ConfigUtils.normalizeString(svgTemplate);
+    }
+
+    public Optional<String> getHtmlTemplate() {
+        return Optional.ofNullable(m_htmlTemplate);
+    }
+
+    public void setHtmlTemplate(final String htmlTemplate) {
+        m_htmlTemplate = ConfigUtils.normalizeString(htmlTemplate);
+    }
+
+    public String getLogo() {
+        return m_logo;
+    }
+
+    public void setLogo(final String logo) {
+        m_logo = ConfigUtils.assertNotEmpty(logo, "logo");
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(m_id, 
+                            m_type, 
+                            m_parameters, 
+                            m_pdfTemplate, 
+                            m_svgTemplate, 
+                            m_htmlTemplate, 
+                            m_logo);
+    }
+
     @Override
     public boolean equals(final Object obj) {
         if ( this == obj ) {
             return true;
         }
-        
+
         if (obj instanceof Report) {
-            Report temp = (Report)obj;
-            boolean equals = Objects.equals(temp.id, id)
-                && Objects.equals(temp.type, type)
-                && Objects.equals(temp.parameters, parameters)
-                && Objects.equals(temp.pdfTemplate, pdfTemplate)
-                && Objects.equals(temp.svgTemplate, svgTemplate)
-                && Objects.equals(temp.htmlTemplate, htmlTemplate)
-                && Objects.equals(temp.logo, logo);
-            return equals;
+            final Report that = (Report)obj;
+            return Objects.equals(this.m_id, that.m_id)
+                    && Objects.equals(this.m_type, that.m_type)
+                    && Objects.equals(this.m_parameters, that.m_parameters)
+                    && Objects.equals(this.m_pdfTemplate, that.m_pdfTemplate)
+                    && Objects.equals(this.m_svgTemplate, that.m_svgTemplate)
+                    && Objects.equals(this.m_htmlTemplate, that.m_htmlTemplate)
+                    && Objects.equals(this.m_logo, that.m_logo);
         }
         return false;
-    }
-
-    /**
-     * Returns the value of field 'htmlTemplate'. The field 'htmlTemplate' has the
-     * following description: template to convert to display the report
-     *  in HTML format
-     * 
-     * @return the value of field 'HtmlTemplate'.
-     */
-    public String getHtmlTemplate() {
-        return this.htmlTemplate;
-    }
-
-    /**
-     * Returns the value of field 'id'. The field 'id' has the following
-     * description: the name of this report
-     * 
-     * @return the value of field 'Id'.
-     */
-    public String getId() {
-        return this.id;
-    }
-
-    /**
-     * Returns the value of field 'logo'. The field 'logo' has the following
-     * description: path to the logo file
-     * 
-     * @return the value of field 'Logo'.
-     */
-    public String getLogo() {
-        return this.logo;
-    }
-
-    /**
-     * Returns the value of field 'parameters'.
-     * 
-     * @return the value of field 'Parameters'.
-     */
-    public Parameters getParameters() {
-        return this.parameters;
-    }
-
-    /**
-     * Returns the value of field 'pdfTemplate'. The field 'pdfTemplate' has the
-     * following description: template to convert to display the report
-     *  in PDF format
-     * 
-     * @return the value of field 'PdfTemplate'.
-     */
-    public String getPdfTemplate() {
-        return this.pdfTemplate;
-    }
-
-    /**
-     * Returns the value of field 'svgTemplate'. The field 'svgTemplate' has the
-     * following description: template to convert to display the report
-     *  in PDF format with embedded SVG
-     * 
-     * @return the value of field 'SvgTemplate'.
-     */
-    public String getSvgTemplate() {
-        return this.svgTemplate;
-    }
-
-    /**
-     * Returns the value of field 'type'. The field 'type' has the following
-     * description: type of this report (calendar/classic)
-     * 
-     * @return the value of field 'Type'.
-     */
-    public String getType() {
-        return this.type;
-    }
-
-    /**
-     * Method hashCode.
-     * 
-     * @return a hash code value for the object.
-     */
-    @Override
-    public int hashCode() {
-        int hash = Objects.hash(
-            id, 
-            type, 
-            parameters, 
-            pdfTemplate, 
-            svgTemplate, 
-            htmlTemplate, 
-            logo);
-        return hash;
-    }
-
-    /**
-     * Sets the value of field 'htmlTemplate'. The field 'htmlTemplate' has the
-     * following description: template to convert to display the report
-     *  in HTML format
-     * 
-     * @param htmlTemplate the value of field 'htmlTemplate'.
-     */
-    public void setHtmlTemplate(final String htmlTemplate) {
-        this.htmlTemplate = htmlTemplate;
-    }
-
-    /**
-     * Sets the value of field 'id'. The field 'id' has the following description:
-     * the name of this report
-     * 
-     * @param id the value of field 'id'.
-     */
-    public void setId(final String id) {
-        this.id = id;
-    }
-
-    /**
-     * Sets the value of field 'logo'. The field 'logo' has the following
-     * description: path to the logo file
-     * 
-     * @param logo the value of field 'logo'.
-     */
-    public void setLogo(final String logo) {
-        this.logo = logo;
-    }
-
-    /**
-     * Sets the value of field 'parameters'.
-     * 
-     * @param parameters the value of field 'parameters'.
-     */
-    public void setParameters(final Parameters parameters) {
-        this.parameters = parameters;
-    }
-
-    /**
-     * Sets the value of field 'pdfTemplate'. The field 'pdfTemplate' has the
-     * following description: template to convert to display the report
-     *  in PDF format
-     * 
-     * @param pdfTemplate the value of field 'pdfTemplate'.
-     */
-    public void setPdfTemplate(final String pdfTemplate) {
-        this.pdfTemplate = pdfTemplate;
-    }
-
-    /**
-     * Sets the value of field 'svgTemplate'. The field 'svgTemplate' has the
-     * following description: template to convert to display the report
-     *  in PDF format with embedded SVG
-     * 
-     * @param svgTemplate the value of field 'svgTemplate'.
-     */
-    public void setSvgTemplate(final String svgTemplate) {
-        this.svgTemplate = svgTemplate;
-    }
-
-    /**
-     * Sets the value of field 'type'. The field 'type' has the following
-     * description: type of this report (calendar/classic)
-     * 
-     * @param type the value of field 'type'.
-     */
-    public void setType(final String type) {
-        this.type = type;
     }
 
 }
