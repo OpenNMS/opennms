c0235202d14980d1403f49b1a499aad276d02e45
Benjamin Reed <ranger@opennms.org>
2017-03-31 11:47:49 -0400

    add Optional/clean up wmi-config

--- opennms-config-model/src/main/java/org/opennms/netmgt/config/wmi/agent/package-info.java
+++ opennms-config-model/src/main/java/org/opennms/netmgt/config/wmi/agent/package-info.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2017-2017 The OpenNMS Group, Inc.
+ * Copyright (C) 2017 The OpenNMS Group, Inc.
  * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
@@ -27,12 +27,12 @@
  *******************************************************************************/
 
 @XmlSchema(
-    namespace = "http://xmlns.opennms.org/xsd/config/wmi",
-    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
-    xmlns={
-        @XmlNs(prefix="", namespaceURI="http://xmlns.opennms.org/xsd/config/wmi"),
-    }
-)
+           namespace = "http://xmlns.opennms.org/xsd/config/wmi",
+           elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
+           xmlns={
+                   @XmlNs(prefix="", namespaceURI="http://xmlns.opennms.org/xsd/config/wmi"),
+           }
+        )
 package org.opennms.netmgt.config.wmi.agent;
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlSchema;
