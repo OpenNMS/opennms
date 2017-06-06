4122bb2fb27fa4a0248824dbe186202b3729ff9f
Jesse White <jesse@opennms.org>
2017-06-06 12:06:29 -0400

    NMS-9407: Preserve the ordering of the latency metrics when marshaling these to and from XML.

--- features/poller/client-rpc/src/test/java/org/opennms/netmgt/poller/client/rpc/PollerResponseDTOTest.java
+++ features/poller/client-rpc/src/test/java/org/opennms/netmgt/poller/client/rpc/PollerResponseDTOTest.java
@@ -28,6 +28,7 @@
 
 package org.opennms.netmgt.poller.client.rpc;
 
+import java.math.BigDecimal;
 import java.net.UnknownHostException;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
@@ -49,12 +50,22 @@ public class PollerResponseDTOTest extends XmlTestNoCastor<PollerResponseDTO> {
     @Parameters
     public static Collection<Object[]> data() throws Exception {
         return Arrays.asList(new Object[][] {
+            // NOTE: In order for the latency metrics to be persisted properly
+            // it is important the the properties are unmarshaled in the same order
+            // as they appear in the source PollStatus object
             {
                 getPollerResponse(),
                 "<?xml version=\"1.0\"?>\n" +
                 "<poller-response>" +
-                    "<poll-status code=\"1\" name=\"Up\" time=\"" + StringUtils.iso8601OffsetString(new Date(0), ZoneId.systemDefault(), ChronoUnit.SECONDS) + "\"/>" +
-                 "</poller-response>"
+                "   <poll-status code=\"1\" name=\"Up\" time=\"" + StringUtils.iso8601OffsetString(new Date(0), ZoneId.systemDefault(), ChronoUnit.SECONDS) + "\">\n" +
+                "      <properties>\n" +
+                "         <property key=\"ping1\">55</property>\n" +
+                "         <property key=\"ping2\">61</property>\n" +
+                "         <property key=\"ping3\">67</property>\n" +
+                "         <property key=\"median\">98</property>\n" +
+                "      </properties>\n" +
+                "   </poll-status>\n" +
+                "</poller-response>"
             }
         });
     }
@@ -63,6 +74,10 @@ public class PollerResponseDTOTest extends XmlTestNoCastor<PollerResponseDTO> {
         PollerResponseDTO dto = new PollerResponseDTO();
         PollStatus pollStatus = PollStatus.available();
         pollStatus.setTimestamp(new Date(0));
+        pollStatus.setProperty("ping1", BigDecimal.valueOf(55));
+        pollStatus.setProperty("ping2", BigDecimal.valueOf(61));
+        pollStatus.setProperty("ping3", BigDecimal.valueOf(67));
+        pollStatus.setProperty("median", BigDecimal.valueOf(98));
         dto.setPollStatus(pollStatus);
         return dto;
     }
