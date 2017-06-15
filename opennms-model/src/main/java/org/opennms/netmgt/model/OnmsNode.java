d2f5b4a5369279f0e459966985374d43d3c4c495
Benjamin Reed <ranger@opennms.org>
2017-06-15 17:26:56 -0400

    OnmsNode.id should be a number in JSON format

--- opennms-model/src/main/java/org/opennms/netmgt/model/OnmsNode.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/OnmsNode.java
@@ -75,6 +75,7 @@ import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonValue;
 import org.codehaus.jackson.map.annotate.JsonDeserialize;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
@@ -110,6 +111,7 @@ public class OnmsNode extends OnmsEntity implements Serializable, Comparable<Onm
     private static final Logger LOG = LoggerFactory.getLogger(OnmsNode.class);
 
     /** identifier field */
+    @JsonProperty("id")
     private Integer m_id;
 
     /** persistent field */
@@ -258,6 +260,7 @@ public class OnmsNode extends OnmsEntity implements Serializable, Comparable<Onm
     @XmlID
     @XmlAttribute(name="id", required=true)
     @Transient
+    @JsonIgnore
     public String getNodeId() {
         if (getId() != null) {
             return getId().toString();
