bc8238d0e4705a534b4263a88858652b44711b7e
Benjamin Reed <ranger@opennms.org>
2017-06-16 09:20:47 -0400

    fix test failure from updated service model

--- opennms-model/src/test/java/org/opennms/netmgt/model/OnmsOutageTest.java
+++ opennms-model/src/test/java/org/opennms/netmgt/model/OnmsOutageTest.java
@@ -76,6 +76,7 @@ public class OnmsOutageTest {
         JsonTest.assertJsonEquals(
                 "{\"id\" : 1,\n" +
                 "  \"monitoredService\" : {\n" +
+                "    \"id\" : 1,\n" +
                 "    \"applications\" : [ {\n" +
                 "      \"name\" : \"Dummy\",\n" +
                 "      \"id\" : 100\n" +
