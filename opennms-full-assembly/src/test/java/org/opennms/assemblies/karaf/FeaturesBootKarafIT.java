d6ec32b0ccf060ea4884c10739c9e8d3e495d847
Atlassian Bamboo <bamboo@opennms.org>
2017-07-10 10:24:54 -0400

    OpenNMS Horizon 20.0.1

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/FeaturesBootKarafIT.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/FeaturesBootKarafIT.java
@@ -50,8 +50,8 @@ public class FeaturesBootKarafIT extends OnmsKarafTestCase {
 	 */
 	@Test
 	public void testInstallAllOpenNMSFeatures() {
-		addFeaturesUrl(maven().groupId("org.opennms.container").artifactId("org.opennms.container.karaf").version("20.0.1-SNAPSHOT").type("xml").classifier("features").getURL());
-		addFeaturesUrl(maven().groupId("org.opennms.karaf").artifactId("opennms").version("20.0.1-SNAPSHOT").type("xml").classifier("features").getURL());
+		addFeaturesUrl(maven().groupId("org.opennms.container").artifactId("org.opennms.container.karaf").version("20.0.1").type("xml").classifier("features").getURL());
+		addFeaturesUrl(maven().groupId("org.opennms.karaf").artifactId("opennms").version("20.0.1").type("xml").classifier("features").getURL());
 
 		for (String feature : new String[] {
 			"karaf-framework",
