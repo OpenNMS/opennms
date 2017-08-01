24929763d560a7fcb0602365565802b2e0a34b94
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2017-08-01 21:06:29 +0200

    HELM-4: Fixed duplicate key errors

--- core/upgrade/src/main/java/org/opennms/upgrade/implementations/EventParameterMigratorOffline.java
+++ core/upgrade/src/main/java/org/opennms/upgrade/implementations/EventParameterMigratorOffline.java
@@ -34,6 +34,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.opennms.core.db.DataSourceFactory;
 import org.opennms.netmgt.events.api.EventParameterUtils;
@@ -79,23 +82,26 @@ public class EventParameterMigratorOffline extends AbstractOnmsUpgrade {
     public void execute() throws OnmsUpgradeException {
         long eventCount = 0, parameterCount = 0;
 
-        try (final Connection connection = DataSourceFactory.getInstance().getConnection())
-        {
+        try (final Connection connection = DataSourceFactory.getInstance().getConnection()) {
             connection.setAutoCommit(false);
             try (final Statement selectStatement = connection.createStatement();
                  final PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO event_parameters (eventid, name, value, type) VALUES  (?,?,?,?)");
                  final PreparedStatement nullifyStatement = connection.prepareStatement("UPDATE events SET eventparms=NULL WHERE eventid=?")) {
-                try (final ResultSet resultSet = selectStatement.executeQuery("SELECT eventid, eventparms FROM events")) {
+                try (final ResultSet resultSet = selectStatement.executeQuery("SELECT eventid, eventparms FROM events WHERE eventparms IS NOT NULL")) {
                     while (resultSet.next()) {
                         final Integer eventId = resultSet.getInt("eventid");
                         final String eventParms = resultSet.getString("eventparms");
                         final List<Parm> parmList = EventParameterUtils.decode(eventParms);
+
                         if (parmList != null) {
-                            for (Parm parm : parmList) {
+                            Map<String, Parm> parmMap = parmList.stream().collect(
+                                    Collectors.toMap(Parm::getParmName, Function.identity(), (p1,p2)->p1));
+
+                            for (Map.Entry<String, Parm> entry : parmMap.entrySet()) {
                                 insertStatement.setInt(1, eventId);
-                                insertStatement.setString(2, parm.getParmName());
-                                insertStatement.setString(3, parm.getValue().getContent());
-                                insertStatement.setString(4, parm.getValue().getType());
+                                insertStatement.setString(2, entry.getValue().getParmName());
+                                insertStatement.setString(3, entry.getValue().getValue().getContent());
+                                insertStatement.setString(4, entry.getValue().getValue().getType());
                                 insertStatement.execute();
 
                                 nullifyStatement.setInt(1, eventId);
@@ -107,11 +113,11 @@ public class EventParameterMigratorOffline extends AbstractOnmsUpgrade {
                         eventCount++;
 
                         if (eventCount % 10000 == 0) {
-                            log("Processed %d event(s) and inserted %d event parameter(s)...\n", eventCount, parameterCount);
+                            log("Processed %d eventparms entries, %d event parameters inserted...\n", eventCount, parameterCount);
                         }
                     }
 
-                    log("Processed %d event(s) and inserted %d event parameter(s)...\n", eventCount, parameterCount);
+                    log("Processed %d eventparms entries, %d event parameters inserted...\n", eventCount, parameterCount);
                 }
             } catch(SQLException e) {
                 connection.rollback();
@@ -122,7 +128,7 @@ public class EventParameterMigratorOffline extends AbstractOnmsUpgrade {
             connection.commit();
             connection.setAutoCommit(true);
         } catch (Throwable e) {
-            throw new OnmsUpgradeException("Can't move event parameters to table " + e.getMessage(), e);
+            throw new OnmsUpgradeException("Can't move event parameters to table: " + e.getMessage(), e);
         }
 /*
         try {
