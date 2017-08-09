6c7aab8ec219c184b3f8c5b61ac36819523f8198
Benjamin Reed <ranger@opennms.org>
2017-08-09 13:21:18 -0400

    20.0.2 branch prepped from last release.  20.0.1 -> 20.0.2

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
@@ -59,7 +59,7 @@ public class OnmsKarafTestCase extends KarafTestCase {
 				.groupId("org.opennms.container")
 				.artifactId("org.opennms.container.karaf")
 				.type("tar.gz")
-				.version("20.0.1");
+				.version("20.0.2-SNAPSHOT");
 	}
 
 	/**
