32423e8e113334e5eebb3accd0a8f1debb254e88
Benjamin Reed <ranger@opennms.org>
2017-08-29 12:17:23 -0400

    fix formatting

--- opennms-web-api/src/main/java/org/opennms/web/api/ISO8601DateEditor.java
+++ opennms-web-api/src/main/java/org/opennms/web/api/ISO8601DateEditor.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2008-2014 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2014 The OpenNMS Group, Inc.
+ * Copyright (C) 2008-2017 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -38,51 +38,46 @@ import org.joda.time.format.ISODateTimeFormat;
 /**
  * PropertyEditor suitable for use by BeanWrapperImpl, so that we can accept xsd:datetime formatted dates
  * in query strings.
+ *
  * Also handles "epoch" style dates, if they exist.  Could be extended to guess the date format and do something
  * useful with it
  *
  * @author miskellc
- * @version $Id: $
- * @since 1.8.1
  */
 public class ISO8601DateEditor extends PropertyEditorSupport {
     static final DateTimeFormatter m_formatter = ISODateTimeFormat.dateTime().withZone(DateTimeZone.UTC);
-    
-    /**
-     * <p>Constructor for ISO8601DateEditor.</p>
-     */
+
     public ISO8601DateEditor() {
         super();
-        
     }
-	/** {@inheritDoc} */
-	@Override
-	public String getAsText() {
-		Date date=(Date)super.getValue();
-		return m_formatter.print(date.getTime());
-	}
 
-	/** {@inheritDoc} */
-	@Override
-	public void setAsText(String text) throws IllegalArgumentException {
-		Date date;
-		try {
-			long epoch=Long.parseLong(text);
-			date=new Date(epoch);
-		} catch (NumberFormatException e) {
-		    date = new Date(m_formatter.parseMillis(text));
-		}
-		super.setValue(date);
-	}
+    /** {@inheritDoc} */
+    @Override
+    public String getAsText() {
+        return m_formatter.print(((Date)super.getValue()).getTime());
+    }
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * No, we don't do GUIs.  Sod off
-	 */
-	@Override
-	public boolean isPaintable() {
-		return false;
-	}
+    /** {@inheritDoc} */
+    @Override
+    public void setAsText(final String text) throws IllegalArgumentException {
+        Date date;
+        try {
+            final long epoch = Long.parseLong(text);
+            date=new Date(epoch);
+        } catch (final NumberFormatException e) {
+            date = new Date(m_formatter.parseMillis(text));
+        }
+        super.setValue(date);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * No, we don't do GUIs.  Sod off
+     */
+    @Override
+    public boolean isPaintable() {
+        return false;
+    }
 
 }
