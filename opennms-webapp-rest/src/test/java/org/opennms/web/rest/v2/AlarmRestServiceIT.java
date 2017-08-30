3b674914f3dfd3bb40eb1902979ff91b25fd52b5
Seth Leger <seth@opennms.org>
2017-08-29 23:59:17 -0400

    HZN-1098: Add limit param to the search property value endpoint

--- opennms-webapp-rest/src/test/java/org/opennms/web/rest/v2/AlarmRestServiceIT.java
+++ opennms-webapp-rest/src/test/java/org/opennms/web/rest/v2/AlarmRestServiceIT.java
@@ -33,6 +33,7 @@ import static org.junit.Assert.fail;
 import static org.opennms.web.svclayer.support.DefaultTroubleTicketProxy.createEventBuilder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -40,6 +41,7 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
@@ -320,6 +322,59 @@ public class AlarmRestServiceIT extends AbstractSpringJerseyRestTestCase {
     public void testLocationFiltering() throws Exception {
         executeQueryAndVerify("_s=location.locationName==Default", 7);
         executeQueryAndVerify("_s=location.locationName!=Default", 0);
+    }
+
+    /**
+     * Test metadata autocompletion.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testNodeLabelPropertyValues() throws Exception {
+        JSONObject object = new JSONObject(sendRequest(GET, "/alarms/properties/node.label", Collections.emptyMap(), 200));
+        Assert.assertEquals(2, object.getInt("totalCount"));
+        JSONArray values = object.getJSONArray("value");
+        // Values should be sorted alphabetically so this order is deterministic
+        assertEquals("server01", values.getString(0));
+        assertEquals("server02", values.getString(1));
+
+        object = new JSONObject(sendRequest(GET, "/alarms/properties/node.label", Collections.singletonMap("limit", "1"), 200));
+        Assert.assertEquals(1, object.getInt("totalCount"));
+        values = object.getJSONArray("value");
+        assertEquals("server01", values.getString(0));
+
+        // Using a limit less than 1 should result in an unlimited return value
+        object = new JSONObject(sendRequest(GET, "/alarms/properties/node.label", Collections.singletonMap("limit", "0"), 200));
+        Assert.assertEquals(2, object.getInt("totalCount"));
+        values = object.getJSONArray("value");
+        assertEquals("server01", values.getString(0));
+        assertEquals("server02", values.getString(1));
+        object = new JSONObject(sendRequest(GET, "/alarms/properties/node.label", Collections.singletonMap("limit", "-2"), 200));
+        Assert.assertEquals(2, object.getInt("totalCount"));
+        values = object.getJSONArray("value");
+        assertEquals("server01", values.getString(0));
+        assertEquals("server02", values.getString(1));
+
+        // Test a query
+        object = new JSONObject(sendRequest(GET, "/alarms/properties/node.label", Collections.singletonMap("q", "02"), 200));
+        Assert.assertEquals(1, object.getInt("totalCount"));
+        values = object.getJSONArray("value");
+        assertEquals("server02", values.getString(0));
+
+        object = new JSONObject(sendRequest(GET, "/alarms/properties/node.label", Collections.singletonMap("q", "server"), 200));
+        Assert.assertEquals(2, object.getInt("totalCount"));
+        values = object.getJSONArray("value");
+        assertEquals("server01", values.getString(0));
+        assertEquals("server02", values.getString(1));
+
+        // Test a query with a limit
+        object = new JSONObject(sendRequest(GET, "/alarms/properties/node.label", ImmutableMap.of(
+            "q", "server",
+            "limit", "1"
+        ), 200));
+        Assert.assertEquals(1, object.getInt("totalCount"));
+        values = object.getJSONArray("value");
+        assertEquals("server01", values.getString(0));
     }
 
     @Test
