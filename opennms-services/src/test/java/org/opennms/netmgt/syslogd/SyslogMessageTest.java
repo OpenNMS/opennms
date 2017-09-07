b2f6edfe3b50072858fb71d51eed121e3b6d02a5
Seth Leger <seth@opennms.org>
2017-09-07 10:53:54 -0400

    Revert "NMS-7934: Fix enum so that syslog 'Info' events match events in the OpenNMS"
    This reverts commit e99b93fce781a006edc807ce234d9a0f30033feb. This matches
    the config changes made in NMS-8106.

--- opennms-services/src/test/java/org/opennms/netmgt/syslogd/SyslogMessageTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/syslogd/SyslogMessageTest.java
@@ -77,7 +77,7 @@ public class SyslogMessageTest {
         final SyslogMessage message = parser.parse();
 
         assertEquals(SyslogFacility.KERNEL, message.getFacility());
-        assertEquals(SyslogSeverity.INFO, message.getSeverity());
+        assertEquals(SyslogSeverity.INFORMATIONAL, message.getSeverity());
         assertEquals("test", message.getMessageID());
         assertEquals("127.0.0.1", message.getHostName());
         assertEquals("OpenNMS", message.getProcessName());
@@ -194,7 +194,7 @@ public class SyslogMessageTest {
         final Date date = calendar.getTime();
 
         assertEquals(SyslogFacility.KERNEL, message.getFacility());
-        assertEquals(SyslogSeverity.INFO, message.getSeverity());
+        assertEquals(SyslogSeverity.INFORMATIONAL, message.getSeverity());
         assertEquals("test", message.getMessageID());
         assertEquals(date, message.getDate());
         assertEquals("127.0.0.1", message.getHostName());
@@ -219,7 +219,7 @@ public class SyslogMessageTest {
         final Date date = calendar.getTime();
 
         assertEquals(SyslogFacility.KERNEL, message.getFacility());
-        assertEquals(SyslogSeverity.INFO, message.getSeverity());
+        assertEquals(SyslogSeverity.INFORMATIONAL, message.getSeverity());
         assertEquals("test", message.getMessageID());
         assertEquals(date, message.getDate());
         assertEquals("127.0.0.1", message.getHostName());
