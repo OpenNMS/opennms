3124aa0dd132b6c79474e3fe376e52977b70d29c
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2017-10-11 19:38:09 +0200

    NMS-9613: Stale IP Address Cache (#1699)
    * NMS-9613: Allow multiple nodes with same interface
    
    This makes the interface to node chache aware of multiple nodes having
    the same interface and retracts interfaces from cache if node losts
    interface.
    
    * NMS-9613: Added tests
    
    * NMS-9613: Fixed insertion
    
    * NMS-9613: Rethink cache concept
    
    Now, the cache stores the nodes for each IP sorted by the interface
    primary type. getFirstNodeId will return the node where the IP is used
    as primary interface, if any.
    
    * NMS-9613: Fixed test
    
    * NMS-9613: Moved test method to own IT class
    
    * NMS-9613: Some review changes

--- features/events/traps/src/test/java/org/opennms/netmgt/trapd/TrapHandlerITCase.java
+++ features/events/traps/src/test/java/org/opennms/netmgt/trapd/TrapHandlerITCase.java
@@ -32,6 +32,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.opennms.core.utils.InetAddressUtils.str;
 
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.util.Collection;
 import java.util.Iterator;
@@ -55,6 +56,9 @@ import org.opennms.netmgt.dao.api.InterfaceToNodeCache;
 import org.opennms.netmgt.dao.api.MonitoringLocationDao;
 import org.opennms.netmgt.dao.mock.MockEventIpcManager;
 import org.opennms.netmgt.events.api.EventConstants;
+import org.opennms.netmgt.model.OnmsIpInterface;
+import org.opennms.netmgt.model.OnmsNode;
+import org.opennms.netmgt.model.PrimaryType;
 import org.opennms.netmgt.model.events.EventBuilder;
 import org.opennms.netmgt.snmp.SnmpInstId;
 import org.opennms.netmgt.snmp.SnmpObjId;
@@ -65,11 +69,14 @@ import org.opennms.netmgt.snmp.SnmpValue;
 import org.opennms.netmgt.snmp.SnmpValueFactory;
 import org.opennms.netmgt.xml.event.Event;
 import org.opennms.netmgt.xml.event.Parm;
+import org.opennms.netmgt.xml.event.Value;
 import org.opennms.test.JUnitConfigurationEnvironment;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
+
+import com.google.common.collect.Lists;
 
 @RunWith(OpenNMSJUnit4ClassRunner.class)
 @ContextConfiguration(locations={
@@ -108,7 +115,7 @@ public class TrapHandlerITCase implements InitializingBean {
 
     private static final InetAddress m_ip = InetAddressUtils.ONE_TWENTY_SEVEN;
     
-    private static final int m_nodeId = 1;
+    private static int m_nodeId;
 
     @BeforeClass
     public static void setUpLogging() {
@@ -122,6 +129,14 @@ public class TrapHandlerITCase implements InitializingBean {
 
     @Before
     public void setUp() throws Exception {
+        final OnmsNode node = new OnmsNode(m_dbPopulator.getMonitoringLocationDao().getDefaultLocation(), "test123");
+        final OnmsIpInterface iface = new OnmsIpInterface();
+        iface.setIpAddress(m_ip);
+        iface.setIsSnmpPrimary(PrimaryType.PRIMARY);
+        node.addIpInterface(iface);
+
+        m_nodeId = m_dbPopulator.getNodeDao().save(node);
+
         m_cache.setNodeId(MonitoringLocationDao.DEFAULT_MONITORING_LOCATION_ID, m_ip, m_nodeId);
 
         m_trapd.start();
@@ -410,13 +425,26 @@ public class TrapHandlerITCase implements InitializingBean {
     @Test
     @DirtiesContext
     public void testInterfaceReparentedModifiesIpMgr() throws Exception {
-        long nodeId = 1;
+        final OnmsNode node = new OnmsNode(m_dbPopulator.getMonitoringLocationDao().getDefaultLocation(), "test123");
+        final OnmsIpInterface iface = new OnmsIpInterface();
+        iface.setIpAddress(m_ip);
+        iface.setIsSnmpPrimary(PrimaryType.PRIMARY);
+        node.addIpInterface(iface);
+        int nodeId = m_dbPopulator.getNodeDao().save(node);
 
         anticipateEvent("uei.opennms.org/default/trap", m_ip, nodeId);
 
         Event event =
             anticipateEvent(EventConstants.INTERFACE_REPARENTED_EVENT_UEI,
                     m_ip, nodeId);
+        event.addParm(new Parm() {{
+            this.setParmName(EventConstants.PARM_OLD_NODEID);
+            this.setValue(new Value(Integer.toString(m_nodeId)));
+        }});
+        event.addParm(new Parm() {{
+            this.setParmName(EventConstants.PARM_NEW_NODEID);
+            this.setValue(new Value(Integer.toString(nodeId)));
+        }});
         m_eventMgr.sendNow(event);
 
         try {
