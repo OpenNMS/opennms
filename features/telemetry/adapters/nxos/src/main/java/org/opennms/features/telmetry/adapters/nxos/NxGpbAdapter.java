e2cd218fd13a101cb15249881fd6723bdc197225
Chandra Gorantla <chandra@opennms.com>
2017-10-23 14:15:48 -0400

    HZN-1177: Add proto files & compiled java files

---
+++ features/telemetry/adapters/nxos/src/main/java/org/opennms/features/telmetry/adapters/nxos/NxGpbAdapter.java
@@ -0,0 +1,150 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2017-2017 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.features.telmetry.adapters.nxos;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Optional;
+
+import org.opennms.features.telmetry.adapters.nxos.proto.TelemetryBis;
+import org.opennms.netmgt.collection.api.CollectionAgent;
+import org.opennms.netmgt.collection.api.CollectionAgentFactory;
+import org.opennms.netmgt.collection.api.CollectionSet;
+import org.opennms.netmgt.dao.api.InterfaceToNodeCache;
+import org.opennms.netmgt.dao.api.NodeDao;
+import org.opennms.netmgt.model.OnmsIpInterface;
+import org.opennms.netmgt.model.OnmsNode;
+import org.opennms.netmgt.telemetry.adapters.collection.AbstractPersistingAdapter;
+import org.opennms.netmgt.telemetry.adapters.collection.CollectionSetWithAgent;
+import org.opennms.netmgt.telemetry.adapters.collection.ScriptedCollectionSetBuilder;
+import org.opennms.netmgt.telemetry.ipc.TelemetryMessageDTO;
+import org.opennms.netmgt.telemetry.ipc.TelemetryMessageLogDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import com.google.common.collect.Iterables;
+import com.google.protobuf.ExtensionRegistry;
+
+public class NxGpbAdapter extends AbstractPersistingAdapter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NxGpbAdapter.class);
+
+    private static final ExtensionRegistry s_registry = ExtensionRegistry.newInstance();
+
+    static {
+        TelemetryBis.registerAllExtensions(s_registry);
+    }
+
+    @Autowired
+    private CollectionAgentFactory collectionAgentFactory;
+
+    @Autowired
+    private InterfaceToNodeCache interfaceToNodeCache;
+
+    @Autowired
+    private NodeDao nodeDao;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    private String script;
+
+    private final ThreadLocal<ScriptedCollectionSetBuilder> scriptedCollectionSetBuilders = new ThreadLocal<ScriptedCollectionSetBuilder>() {
+        @Override
+        protected ScriptedCollectionSetBuilder initialValue() {
+            try {
+                return new ScriptedCollectionSetBuilder(new File(script));
+            } catch (Exception e) {
+                LOG.error("Failed to create builder for script '{}'.", script, e);
+                return null;
+            }
+        }
+    };
+
+    public String getScript() {
+        return script;
+    }
+
+    public void setScript(String script) {
+        this.script = script;
+    }
+    
+    @Override
+    public Optional<CollectionSetWithAgent> handleMessage(TelemetryMessageDTO message, TelemetryMessageLogDTO messageLog) throws Exception {
+    
+        final TelemetryBis.Telemetry msg = TelemetryBis.Telemetry.parseFrom(message.getBytes().array(), s_registry);
+
+        CollectionAgent agent = null;
+        try {
+            // Attempt to resolve the systemId to an InetAddress
+            final InetAddress inetAddress = InetAddress.getByName(msg.getNodeIdStr());
+            final Optional<Integer> nodeId = interfaceToNodeCache.getFirstNodeId(messageLog.getLocation(), inetAddress);
+            if (nodeId.isPresent()) {
+                // NOTE: This will throw a IllegalArgumentException if the nodeId/inetAddress pair does not exist in the database
+                agent = collectionAgentFactory.createCollectionAgent(Integer.toString(nodeId.get()), inetAddress);
+            }
+        } catch (UnknownHostException e) {
+            LOG.debug("Could not convert system id to address: {}", msg.getNodeIdStr());
+        }
+
+        if (agent == null) {
+            // We were unable to build the agent by resolving the systemId, try finding
+            // a node with a matching label
+            agent = transactionTemplate.execute(new TransactionCallback<CollectionAgent>() {
+                @Override
+                public CollectionAgent doInTransaction(TransactionStatus status) {
+                    final OnmsNode node = Iterables.getFirst(nodeDao.findByLabel(msg.getNodeIdStr()), null);
+                    if (node != null) {
+                        final OnmsIpInterface primaryInterface = node.getPrimaryInterface();
+                        return collectionAgentFactory.createCollectionAgent(primaryInterface);
+                    }
+                    return null;
+                }
+            });
+        }
+
+        if (agent == null) {
+            LOG.warn("Unable to find node and inteface for system id: {}", msg.getNodeIdStr());
+            return Optional.empty();
+        }
+
+        final ScriptedCollectionSetBuilder builder = scriptedCollectionSetBuilders.get();
+        if (builder == null) {
+            throw new Exception(String.format("Error compiling script '%s'. See logs for details.", script));
+        }
+        final CollectionSet collectionSet = builder.build(agent, msg);
+        return Optional.of(new CollectionSetWithAgent(agent, collectionSet));
+    }
+
+}
---
+++ features/telemetry/adapters/nxos/src/main/java/org/opennms/features/telmetry/adapters/nxos/NxTelmetryClient.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2017-2017 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.features.telmetry.adapters.nxos;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Date;
+
+import org.opennms.core.utils.InetAddressUtils;
+import org.opennms.features.telmetry.adapters.nxos.proto.TelemetryBis;
+
+public class NxTelmetryClient {
+
+    private static TelemetryBis.Telemetry buildMessage(String nodeId, String subsriptionId) {
+
+        // Set Telemetry fields
+        TelemetryBis.TelemetryField field = TelemetryBis.TelemetryField.newBuilder()
+                                                .setName("ProtoName")
+                                                .setStringValue("ipv4")
+                                                .setTimestamp(1508775858).build();
+        
+        final TelemetryBis.Telemetry telemetrymsg = TelemetryBis.Telemetry.newBuilder()
+                                                        .setNodeIdStr(nodeId)
+                                                        .addDataGpbkv(field)
+                                                        .setSubscriptionIdStr(subsriptionId)
+                                                        .setCollectionId(4)
+                                                        .setCollectionStartTime(1508775858)
+                                                        .setCollectionEndTime(1508775875)
+                                                        .setMsgTimestamp(new Date().getTime())
+                                                        .build();
+        
+
+        return telemetrymsg;
+
+    }
+    
+    public static void main(String... args) throws IOException {
+        // Not sure what could be nodeId and subscription Id. This is just assumption
+        TelemetryBis.Telemetry jtiMsg = buildMessage("192.168.2.1", "eth0");
+        byte[] jtiMsgBytes = jtiMsg.toByteArray();
+
+        InetAddress address = InetAddressUtils.getLocalHostAddress();
+        DatagramPacket packet = new DatagramPacket(jtiMsgBytes, jtiMsgBytes.length, address, 50000);
+        DatagramSocket socket = new DatagramSocket();
+        socket.send(packet);
+    }
+
+}
