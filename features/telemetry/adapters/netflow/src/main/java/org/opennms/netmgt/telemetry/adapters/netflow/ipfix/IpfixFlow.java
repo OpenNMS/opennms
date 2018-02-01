ee852df2e1538bdf8719dec7b380b7020a23272d
Dustin Frisch <fooker@lab.sh>
2018-02-01 16:53:47 +0100

    HZN-1180: Fixed protocol version for IPFIX

--- features/telemetry/adapters/netflow/src/main/java/org/opennms/netmgt/telemetry/adapters/netflow/ipfix/IpfixFlow.java
+++ features/telemetry/adapters/netflow/src/main/java/org/opennms/netmgt/telemetry/adapters/netflow/ipfix/IpfixFlow.java
@@ -264,7 +264,7 @@ class IpfixFlow implements Flow {
 
     @Override
     public NetflowVersion getNetflowVersion() {
-        return NetflowVersion.V9;
+        return NetflowVersion.IPFIX;
     }
 
     @Override
