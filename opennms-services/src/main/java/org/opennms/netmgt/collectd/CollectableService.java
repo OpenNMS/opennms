c1e06b9ec192fb8441adbb42efd9cf1980c1567a
Benjamin Reed <ranger@opennms.org>
2018-02-07 11:01:41 -0500

    remove extraneous import

--- opennms-services/src/main/java/org/opennms/netmgt/collectd/CollectableService.java
+++ opennms-services/src/main/java/org/opennms/netmgt/collectd/CollectableService.java
@@ -35,7 +35,6 @@ import java.util.Date;
 import org.opennms.core.logging.Logging;
 import org.opennms.core.utils.InetAddressUtils;
 import org.opennms.netmgt.collectd.Collectd.SchedulingCompletedFlag;
-import org.opennms.netmgt.collectd.DefaultSnmpCollectionAgent;
 import org.opennms.netmgt.collection.api.AttributeGroup;
 import org.opennms.netmgt.collection.api.CollectionAgent;
 import org.opennms.netmgt.collection.api.CollectionAttribute;
