c0fd94526e346d3f18ac5c1399c7f2cb474807da
Markus von Rüden <mvr@opennms.com>
2018-02-12 10:35:34 +0100

    HZN-1209: Proper trigger csv download

--- core/web-assets/src/main/assets/js/apps/onms-classifications/index.js
+++ core/web-assets/src/main/assets/js/apps/onms-classifications/index.js
@@ -156,7 +156,7 @@ const importModalTemplate  = require('./views/modals/import-modal.html');
             };
             $scope.refresh();
         }])
-        .controller('ClassificationGroupController', ['$scope', '$stateParams', '$uibModal', '$log', 'ClassificationRuleService', 'ClassificationGroupService', function($scope, $stateParams, $uibModal, $log, ClassificationRuleService, ClassificationGroupService) {
+        .controller('ClassificationGroupController', ['$scope', '$stateParams', '$uibModal', '$log', '$http', '$window','ClassificationRuleService', 'ClassificationGroupService', function($scope, $stateParams, $uibModal, $log, $http, $window,ClassificationRuleService, ClassificationGroupService) {
             // Defaults
             $scope.rules = [];
             $scope.query = {
@@ -171,8 +171,19 @@ const importModalTemplate  = require('./views/modals/import-modal.html');
             $scope.findGroup = function(groupId) {
                 return ClassificationGroupService.get({id: groupId}, function(response) {
                     $scope.group = response;
-                    $scope.csv_link = 'rest/classifications/groups/' + $scope.group.id;
                     $scope.refresh();
+                });
+            };
+            $scope.download = function() {
+                return $http({
+                    method: 'GET',
+                    url: 'rest/classifications/groups/' + $scope.group.id,
+                    headers: {
+                        'Accept': 'text/comma-separated-values'
+                    }
+                }).then(function(response) {
+                    var uri = 'data:text/csv;charset=UTF-8,' + encodeURIComponent(response.data);
+                    $window.location = uri;
                 });
             };
 
