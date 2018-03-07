4fa2d108fe0982894c2e4a94e8b381fc542bdcfb
Chandra Gorantla <chandra@opennms.com>
2018-03-06 20:22:57 -0500

    NMS-9861: Fix test

--- smoke-test/src/test/java/org/opennms/smoketest/StatisticsReportsIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/StatisticsReportsIT.java
@@ -1,3 +1,31 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2018 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2018 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
 package org.opennms.smoketest;
 
 import static com.jayway.awaitility.Awaitility.await;
@@ -6,7 +34,6 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertNotNull;
 
-import java.net.InetSocketAddress;
 import java.util.Date;
 
 import org.junit.Before;
@@ -21,21 +48,20 @@ import org.opennms.netmgt.model.StatisticsReport;
 import org.opennms.netmgt.model.StatisticsReportData;
 import org.opennms.smoketest.utils.DaoUtils;
 import org.opennms.smoketest.utils.HibernateDaoFactory;
-import org.opennms.test.system.api.NewTestEnvironment.ContainerAlias;
 
-public class StatsIT extends OpenNMSSeleniumTestCase {
-    
+public class StatisticsReportsIT extends OpenNMSSeleniumTestCase {
+
     @Before
     public void setUp() throws Exception {
         m_driver.get(getBaseUrl() + "opennms/statisticsReports/index.htm");
+
     }
 
     @Test
     public void hasReportLinkThatMatchDescription() throws Exception {
-        
         Date startOfTest = new Date();
-        InetSocketAddress pgsql = getTestEnvironment().getServiceAddress(ContainerAlias.POSTGRES, 5432);
-        HibernateDaoFactory daoFactory = new HibernateDaoFactory(pgsql);
+
+        HibernateDaoFactory daoFactory = new HibernateDaoFactory(getPostgresService());
         ResourceReferenceDao resourceReferenceDao = daoFactory.getDao(ResourceReferenceDaoHibernate.class);
         StatisticsReportDao statisticsReportDao = daoFactory.getDao(StatisticsReportDaoHibernate.class);
 
@@ -60,8 +86,10 @@ public class StatsIT extends OpenNMSSeleniumTestCase {
 
         statisticsReportDao.save(report);
         await().atMost(1, MINUTES).pollInterval(5, SECONDS)
-        .until(DaoUtils.findMatchingCallable(statisticsReportDao, new CriteriaBuilder(StatisticsReport.class)
-                .ge("createTime", startOfTest).toCriteria()), notNullValue());
+                .until(DaoUtils.findMatchingCallable(statisticsReportDao,
+                        new CriteriaBuilder(StatisticsReport.class).ge("startDate", startOfTest).toCriteria()),
+                        notNullValue());
+        m_driver.navigate().refresh();
 
         assertNotNull(findElementByLink("Hourly Top 10 responses across all nodes"));
     }
