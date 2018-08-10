3be05c1e1658c54543150e513e6c9eb277f23f25
Malatesh <Malatesh.Sudarshan@cerner.com>
2018-08-11 00:24:55 +0530

    Adding opennms copyright header

--- core/ipc/sink/kafka/itests/src/test/java/org/opennms/core/ipc/sink/kafka/itests/EventSinkConsumerIT.java
+++ core/ipc/sink/kafka/itests/src/test/java/org/opennms/core/ipc/sink/kafka/itests/EventSinkConsumerIT.java
@@ -1,21 +1,45 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2016 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2016 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+
 package org.opennms.core.ipc.sink.kafka.itests;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.Hashtable;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,7 +47,6 @@ import org.opennms.core.ipc.sink.api.MessageConsumer;
 import org.opennms.core.ipc.sink.api.MessageDispatcherFactory;
 import org.opennms.core.ipc.sink.api.SinkModule;
 import org.opennms.core.ipc.sink.api.SyncDispatcher;
-import org.opennms.core.ipc.sink.common.ThreadLockingMessageConsumer;
 import org.opennms.core.ipc.sink.kafka.client.KafkaRemoteMessageDispatcherFactory;
 import org.opennms.core.ipc.sink.kafka.common.KafkaSinkConstants;
 import org.opennms.core.ipc.sink.kafka.server.KafkaMessageConsumerManager;
@@ -37,12 +60,6 @@ import org.opennms.test.JUnitConfigurationEnvironment;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-
-import com.codahale.metrics.Meter;
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Timer;
-import com.codahale.metrics.Timer.Context;
-import com.google.common.util.concurrent.RateLimiter;
 
 @RunWith(OpenNMSJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -126,96 +143,4 @@ public class EventSinkConsumerIT {
 
     }
 
-    @Test(timeout = 60000)
-    @Ignore("flapping")
-    public void canConsumeMessagesInParallel() throws Exception {
-        final int NUM_CONSUMER_THREADS = 7;
-
-        final EventsModule parallelEventsModule = new EventsModule(m_config) {
-            @Override
-            public int getNumConsumerThreads() {
-                return NUM_CONSUMER_THREADS;
-            }
-        };
-
-        final ThreadLockingMessageConsumer<Event, Log> consumer = new ThreadLockingMessageConsumer<>(parallelEventsModule);
-
-        final CompletableFuture<Integer> future = consumer.waitForThreads(NUM_CONSUMER_THREADS);
-
-        try {
-            consumerManager.registerConsumer(consumer);
-
-            final SyncDispatcher<Event> dispatcher = remoteMessageDispatcherFactory.createSyncDispatcher(new EventsModule(m_config));
-
-            final EventGenerator generator = new EventGenerator(dispatcher,
-                                                                        100.0);
-            generator.start();
-
-            // Wait until we have NUM_CONSUMER_THREADS locked
-            future.get();
-
-            // Take a snooze
-            Thread.sleep(TimeUnit.SECONDS.toMillis(5));
-
-            // Verify that there aren't more than NUM_CONSUMER_THREADS waiting
-            assertEquals(0, consumer.getNumExtraThreadsWaiting());
-
-            generator.stop();
-        } finally {
-            consumerManager.unregisterConsumer(consumer);
-        }
-    }
-
-    public static class EventGenerator {
-        Thread thread;
-
-        final SyncDispatcher<Event> dispatcher;
-        final double rate;
-        final AtomicBoolean stopped = new AtomicBoolean(false);
-        private final Meter sentMeter;
-        private final Timer sendTimer;
-
-        public EventGenerator(SyncDispatcher<Event> dispatcher, double rate) {
-            this.dispatcher = dispatcher;
-            this.rate = rate;
-            MetricRegistry metrics = new MetricRegistry();
-            this.sentMeter = metrics.meter("sent");
-            this.sendTimer = metrics.timer("send");
-        }
-
-        public EventGenerator(SyncDispatcher<Event> dispatcher, double rate,
-                Meter sentMeter, Timer sendTimer) {
-            this.dispatcher = dispatcher;
-            this.rate = rate;
-            this.sentMeter = sentMeter;
-            this.sendTimer = sendTimer;
-        }
-
-        public synchronized void start() {
-            stopped.set(false);
-            final RateLimiter rateLimiter = RateLimiter.create(rate);
-            thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-
-                    while (!stopped.get()) {
-                        rateLimiter.acquire();
-                        try (Context ctx = sendTimer.time()) {
-                            dispatcher.send(new Event());
-                            sentMeter.mark();
-                        }
-                    }
-                }
-            });
-            thread.start();
-        }
-
-        public synchronized void stop() throws InterruptedException {
-            stopped.set(true);
-            if (thread != null) {
-                thread.join();
-                thread = null;
-            }
-        }
-    }
 }
