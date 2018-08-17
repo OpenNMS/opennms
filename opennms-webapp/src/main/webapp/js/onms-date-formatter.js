a0802c3450f06fc78ec883475ebc2e05f4e303e4
Benjamin Reed <ranger@opennms.org>
2018-08-17 10:37:36 -0400

    NMS-10232: add a filter, clean up factory

--- opennms-webapp/src/main/webapp/js/onms-date-formatter.js
+++ opennms-webapp/src/main/webapp/js/onms-date-formatter.js
@@ -12,8 +12,13 @@ OnmsDateFormatter.prototype.init = function init(readyCallback) {
 			if (xhr.readyState === XMLHttpRequest.DONE) {
 				if (xhr.status === 200) {
 					var config = JSON.parse(xhr.responseText);
-					window._onmsDateTimeFormat = config.datetimeformat || defaultFormat;
-					window._onmsZoneId = config.zoneId;
+					if (config.datetimeformatConfig && config.datetimeformatConfig.datetimeformat) {
+						window._onmsDateTimeFormat = config.datetimeformatConfig.datetimeformat || defaultFormat;
+						window._onmsZoneId = config.datetimeformatConfig.zoneId;
+					} else {
+						console.log('Error: datetimeformatConfig property not found:',config);
+						window._onmsDateTimeFormat = defaultFormat;
+					}
 				} else {
 					console.log('Error: failed to request format info: ' + xhr.status + ' ' + xhr.statusText);
 					window._onmsDateTimeFormat = defaultFormat;
@@ -33,6 +38,7 @@ OnmsDateFormatter.prototype.init = function init(readyCallback) {
 
 OnmsDateFormatter.prototype.assertInitialized = function assertInitialized() {
 	if (!window._onmsDateTimeFormat) {
+		console.log('OnmsDateFormatter.init() must complete before using!');
 		throw 'OnmsDateFormatter.init() must complete before using!';
 	}
 };
@@ -56,7 +62,7 @@ OnmsDateFormatter.prototype.getZoneId = function getZoneId() {
 			try {
 				self._zoneId = JSJoda.ZoneId.of(window._onmsZoneId);
 			} catch (err) {
-				console.log('Unhandled zone ID ' + this.config.zoneId + ': ' + err);
+				console.log('Unhandled zone ID ' + window._onmsZoneId + ': ' + err);
 				console.log('Falling back to default browser zone.');
 				self._zoneId = JSJoda.ZoneId.SYSTEM;
 			}
@@ -71,13 +77,14 @@ OnmsDateFormatter.prototype.format = function format(date) {
 	var self = this;
 	self.assertInitialized();
 
-	if (!date) {
-		throw 'OnmsDateFormatter.format: No date supplied!';
+	if (date === undefined || date === null) {
+		return date;
 	}
 
+	//console.log('Formatting "' + date + '" with formatter "' + window._onmsDateTimeFormat + '" and zone ID "' + window._onmsZoneId + '"');
 	var jodaDate = JSJoda.ZonedDateTime
-		.from(JSJoda.nativeJs(moment(date)))
-		.withZoneSameLocal(self.getZoneId());
+	.from(JSJoda.nativeJs(moment(date)))
+	.withZoneSameLocal(self.getZoneId());
 
 	return self.getFormatter().format(jodaDate);
 };
@@ -86,28 +93,70 @@ window.OnmsDateFormatter = OnmsDateFormatter;
 
 (function() {
 	'use strict';
+	var f = new OnmsDateFormatter();
+	f.init(function() {
+		window._onmsFormatter = f;
+	});
+})();
+
+(function() {
+	'use strict';
 
 	if (window.angular) {
-		angular.module('onmsDateFormatter', ['ng']).factory('onmsDateFormatterFactory', ['$q', function onmsDateFormatterService($q) {
-			console.log('Initializing onmsDateFormatterFactory');
+		angular.module('onmsDateFormatter', ['ng']).factory('DateFormatterService', ['$interval', '$q', function DateFormatterService($interval, $q) {
+			console.log('Initializing DateFormatterService');
 
 			var deferred = $q.defer();
-			var formatter = new OnmsDateFormatter();
-			formatter.init(function() {
-				deferred.resolve(formatter);
-			});
 
-			return function() {
-				return deferred.promise;
+			var count = 0;
+			var i = $interval(function() {
+				if (window._onmsFormatter) {
+					console.log('Global formatter found: ' + window._onmsDateTimeFormat);
+					deferred.resolve(window._onmsFormatter);
+					$interval.cancel(i);
+				}
+				if (count++ > 300) {
+					console.log('DateFormatterService: giving up waiting for global formatter.');
+					$interval.cancel(i);
+				}
+			}, 100);
+
+			return {
+				raw: window._onmsFormatter,
+				formatter: deferred.promise,
+				format: function(date) {
+					return deferred.promise.then(function(f) {
+						return f.format(date);
+					});
+				}
 			}
 		}]);
 
-		angular.module('onmsDateFormatter').directive('onmsDate', ['onmsDateFormatterFactory', function(formatterFactory) {
+		angular.module('onmsDateFormatter').filter('onmsDate', ['$filter', 'DateFormatterService', function($filter, DateFormatterService) {
+			return function onmsDate(input, ifEmpty) {
+				var ret;
+				if (window._onmsFormatter) {
+					// If the formatter has finished initializing, use it
+					ret = window._onmsFormatter.format(input);
+					//console.log('onmsDate: formatter returned: ' + ret);
+				} else {
+					// Otherwise, use ISO format
+					ret = $filter('date')(input, 'yyyy-MM-ddTHH:mm:ss.sssZ');
+					//console.log('onmsDate: $filter(date) returned: ' + ret);
+				}
+				if (ret !== undefined && ret !== null) {
+					return ret;
+				} else {
+					return ifEmpty;
+				}
+			}
+		}]);
+
+		angular.module('onmsDateFormatter').directive('onmsDate', ['DateFormatterService', function(dateFormatterService) {
 			return {
 				restrict: 'E',
 				compile: function(element) {
-					formatterFactory().then(function(formatter) {
-						var formatted = formatter.format(element.text());
+					dateFormatterService.format(element.text()).then(function(formatted) {
 						element.replaceWith(formatted);
 					}).catch(function(e) {
 						console.error('Failed to format ' + element.text(), e);
@@ -115,5 +164,7 @@ window.OnmsDateFormatter = OnmsDateFormatter;
 				}
 			};
 		}]);
+	} else {
+		console.log('Angular not found.  Not initializing OnmsDateFormatter Angular components.');
 	}
 })();
