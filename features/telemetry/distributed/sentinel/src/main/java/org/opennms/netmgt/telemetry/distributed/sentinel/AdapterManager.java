15d277ee491789fd42acc01ea872216e17af963e
Markus von Rüden <mvr@opennms.com>
2018-08-28 12:19:58 +0200

    HZN-1373: Update javadoc

--- features/telemetry/distributed/sentinel/src/main/java/org/opennms/netmgt/telemetry/distributed/sentinel/AdapterManager.java
+++ features/telemetry/distributed/sentinel/src/main/java/org/opennms/netmgt/telemetry/distributed/sentinel/AdapterManager.java
@@ -41,7 +41,6 @@ import org.opennms.netmgt.telemetry.config.api.Adapter;
 import org.opennms.netmgt.telemetry.config.api.Protocol;
 import org.opennms.netmgt.telemetry.daemon.TelemetryMessageConsumer;
 import org.opennms.netmgt.telemetry.distributed.common.MapBasedAdapterDef;
-import org.opennms.netmgt.telemetry.distributed.common.MapBasedListenerDef;
 import org.opennms.netmgt.telemetry.distributed.common.MapBasedProtocolDef;
 import org.opennms.netmgt.telemetry.distributed.common.MapUtils;
 import org.opennms.netmgt.telemetry.ipc.TelemetrySinkModule;
@@ -53,15 +52,14 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Lists;
 
-// TODO MVR ...
 /**
  * This {@link ManagedServiceFactory} for service pids that contain
- * telemetry listener definitions and manages their lifecycle by starting/updating
+ * telemetry adapter definitions and manages their lifecycle by starting/updating
  * and stopping them accordingly.
  *
- * See {@link MapBasedListenerDef} for a list of supported properties.
+ * See {@link MapBasedAdapterDef} for a list of supported properties.
  *
- * @author jwhite
+ * @author mvrueden
  */
 public class AdapterManager implements ManagedServiceFactory {
     private static final Logger LOG = LoggerFactory.getLogger(AdapterManager.class);
@@ -79,7 +77,7 @@ public class AdapterManager implements ManagedServiceFactory {
 
     @Override
     public String getName() {
-        return "Manages telemetry listener lifecycle.";
+        return "Manages telemetry adapter lifecycle.";
     }
 
     @Override
