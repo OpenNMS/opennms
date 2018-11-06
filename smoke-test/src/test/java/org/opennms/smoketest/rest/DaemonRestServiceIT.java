4eb670312111122dbc0cdd2964cba6d7f686aafc
Sebastian Grund <grund92@gmx.de>
2018-11-06 18:44:20 +0100

    NMS-10427: More Tests on the DaemonRestService

--- smoke-test/src/test/java/org/opennms/smoketest/rest/DaemonRestServiceIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/rest/DaemonRestServiceIT.java
@@ -34,15 +34,29 @@ import static io.restassured.RestAssured.preemptive;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 import org.opennms.netmgt.daemon.DaemonReloadState;
+import org.opennms.netmgt.dao.api.DistPollerDao;
+import org.opennms.netmgt.dao.api.EventDao;
+import org.opennms.netmgt.dao.hibernate.EventDaoHibernate;
+import org.opennms.netmgt.events.api.EventConstants;
+import org.opennms.netmgt.model.OnmsDistPoller;
+import org.opennms.netmgt.model.OnmsEvent;
+import org.opennms.netmgt.model.OnmsEventParameter;
+import org.opennms.netmgt.model.OnmsSeverity;
 import org.opennms.smoketest.OpenNMSSeleniumTestCase;
+import org.opennms.smoketest.utils.HibernateDaoFactory;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -126,66 +140,128 @@ public class DaemonRestServiceIT extends OpenNMSSeleniumTestCase {
 
     @Test
     public void verifyDaemonReload() {
+        final InetSocketAddress pgsql = this.getPostgresService();
+        final HibernateDaoFactory daoFactory = new HibernateDaoFactory(pgsql);
+        final EventDao eventDao = daoFactory.getDao(EventDaoHibernate.class);
+
         // Reload a non Valid DaemonName
-        given().body("").post("/reload/WrongDaemonName/").then()
-                .assertThat().statusCode(404);
-        given().get("/reload/WrongDaemonName/").then()
-                .assertThat().statusCode(404);
+        reload("WrongDaemonName", 404);
+        check("WrongDaemonName", 404, null);
+
 
         // Reload of a non reloadable and internal Daemon
-        given().body("").post("/reload/manager/").then()
-                .assertThat().statusCode(400);
-
         // Verify reload state is unknown, as the daemon is not reloadable
-        await().atMost(5, TimeUnit.SECONDS)
-               .pollDelay(1, TimeUnit.SECONDS)
-                .until(() ->  given().get("/reload/manager/").then()
-                .assertThat().statusCode(200)
-                .assertThat()
-                .body("reloadState", equalTo(DaemonReloadState.Unknown.toString())));
+        reload("manager", 400);
+        check("manager", 200, DaemonReloadState.Unknown);
 
+        // Reload a reloadable but not enabled Daemon
+        reload("snmppoller", 400);
+        check("snmppoller", 200, DaemonReloadState.Reloading);
+
+        // Reload of a non reloadable, not enabled Daemon
+        reload("correlator", 400);
+        check("correlator", 200, DaemonReloadState.Unknown);
+
+        // Reload of a non reloadable but enabled Daemon
+        reload("ticketer", 400);
+        check("ticketer", 200, DaemonReloadState.Unknown);
+
+        // Reload a reloadable and enabled Daemon
+        // Verify reloading workeds
+        reload("eventd", 204);
+        check("eventd", 200, DaemonReloadState.Success);
+
+        //Verify Reloading and Failure State with the reloadable+enabled Daemon
+        verifyReloadingState(eventDao);
+        verifyFailureState(eventDao);
+    }
+
+    private void reload(String daemonName, int reloadHTTPStatusCode) {
+        given().body("").post("/reload/" + daemonName + "/").then()
+                .assertThat().statusCode(reloadHTTPStatusCode);
+
+    }
+
+    private void check(String daemonName, int checkHTTPStatusCode, DaemonReloadState state) {
+        if (state != null) {
+            await().atMost(5, TimeUnit.SECONDS)
+                    .pollDelay(1, TimeUnit.SECONDS)
+                    .until(() -> given().get("/reload/" + daemonName + "/").then()
+                            .assertThat().statusCode(checkHTTPStatusCode)
+                            .assertThat()
+                            .body("reloadState", equalTo(state.toString())));
+        } else {
+            await().atMost(5, TimeUnit.SECONDS)
+                    .pollDelay(1, TimeUnit.SECONDS)
+                    .until(() -> given().get("/reload/" + daemonName + "/").then()
+                            .assertThat().statusCode(checkHTTPStatusCode)
+                    );
+        }
+    }
+
+    private void verifyReloadingState(EventDao eventDao) {
+        Date time = new Date();
         // Reload a reloadable and enabled Daemon
         given().body("").post("/reload/eventd/").then()
                 .assertThat().statusCode(204);
 
-        // Verify reloading worked
-        await().atMost(5, TimeUnit.SECONDS)
-                .pollDelay(1, TimeUnit.SECONDS)
-                .until(() -> given().get("/reload/eventd/").then()
-                        .assertThat().statusCode(200)
-                        .assertThat()
-                        .body("reloadState", equalTo(DaemonReloadState.Success.toString())));
+        List<String> al = new ArrayList<>();
+        al.add(EventConstants.RELOAD_DAEMON_CONFIG_SUCCESSFUL_UEI);
+        List<OnmsEvent> successfulEventsAfterReload = eventDao.getEventsAfterDate(al, time);
 
-        // Reload a reloadable but not enabled Daemon
-        given().body("").post("/reload/snmppoller/").then()
-                .assertThat().statusCode(400);
-        sleep(1000);
-        given().get("/reload/snmppoller/").then()
-                .assertThat().statusCode(200)
-                .assertThat()
-                .body("reloadState", equalTo(DaemonReloadState.Unknown.toString()));
+        Assert.assertThat(successfulEventsAfterReload, hasSize(1));
 
-        // Reload of a non reloadable, not enabled Daemon
-        given().body("").post("/reload/correlator/").then()
-                .assertThat().statusCode(400);
-        sleep(1000);
-        given().get("/reload/correlator/").then()
-                .assertThat().statusCode(200)
-                .assertThat()
-                .body("reloadState", equalTo(DaemonReloadState.Unknown.toString()));
+        if (successfulEventsAfterReload.size() == 1) {
+            eventDao.delete(successfulEventsAfterReload.get(0));
+            check("eventd", 200, DaemonReloadState.Reloading);
+        }
+    }
 
-        // Reload of a non reloadable but enabled Daemon
-        given().body("").post("/reload/ticketer/").then()
-                .assertThat().statusCode(400);
-        sleep(1000);
-        given().get("/reload/ticketer/").then()
-                .assertThat().statusCode(200)
-                .assertThat()
-                .body("reloadState", equalTo(DaemonReloadState.Unknown.toString()));
+    private void verifyFailureState(EventDao eventDao) {
+        OnmsEvent onmsEvent = new OnmsEvent();
 
-        // TODO SG verify daemon state via event (FAILED)
-        // TODO SG: optinally verify reloading
+        OnmsDistPoller distPoller = new OnmsDistPoller(DistPollerDao.DEFAULT_DIST_POLLER_ID);
+        onmsEvent.setDistPoller(distPoller);
 
+        Date time = new Date();
+        onmsEvent.setEventTime(time);
+        onmsEvent.setEventCreateTime(time);
+
+        onmsEvent.setEventLog("Y");
+        onmsEvent.setEventDisplay("Y");
+        onmsEvent.setEventDescr("Failed Reload Event");
+        onmsEvent.setEventLogMsg("Test log message");
+
+        onmsEvent.setEventSeverity(OnmsSeverity.MAJOR.getId());
+
+        onmsEvent.setEventUei(EventConstants.RELOAD_DAEMON_CONFIG_FAILED_UEI);
+        onmsEvent.setEventSource("Daemon Rest Service IT");
+
+
+        OnmsEventParameter ep1 = new OnmsEventParameter(
+                onmsEvent,
+                EventConstants.PARM_REASON,
+                "Its needed for the DaemonRestService IT test to have a Failed Reload Event",
+                "string");
+        OnmsEventParameter ep2 = new OnmsEventParameter(
+                onmsEvent,
+                EventConstants.PARM_DAEMON_NAME,
+                "eventd",
+                "string");
+
+
+        onmsEvent.addEventParameter(ep1);
+        onmsEvent.addEventParameter(ep2);
+
+        // Add Failed Event
+        eventDao.save(onmsEvent);
+        eventDao.flush();
+
+        check("eventd",200, DaemonReloadState.Failed);
+
+        // Remove Failed Event
+        eventDao.delete(onmsEvent);
+        eventDao.flush();
     }
 
     @Test
