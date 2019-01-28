a05defcba5a5a82df616b871ddc6670f0df84c49
Benjamin Reed <ranger@opennms.org>
2019-01-28 15:34:52 -0500

    HZN-457: upgrade Liquibase to version 3
    * Liquibase generic type `DATETIME` now creates TIMESTAMP WITHOUT TIME
      ZONE, rather than TIMESTAMP WITH TIME ZONE, so update all references
      in `changelog.xml` files and set `<validCheckSum/>` tags to make sure
      old (pre-this-patch) databases accept that they're OK.
    * convert Migrator to use the `SpringLiquibase` class for doing
      resource-based loading
    * port our liquibase plugins to the 3 API
      * the `createType` custom plugin was only used in one place and was
        creating problems; I switched it to be a normal <sql> tag
      * sub-tags are no longer supported by liquibase's extension parser,
        convert `setSequence` to take a single table + column + sequence
        and make multiple `setSequence` calls instead

--- core/schema/src/main/java/liquibase/ext/opennms/createindex/CreateIndexWithWhereGenerator.java
+++ core/schema/src/main/java/liquibase/ext/opennms/createindex/CreateIndexWithWhereGenerator.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2011-2014 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2014 The OpenNMS Group, Inc.
+ * Copyright (C) 2011-2019 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2019 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -29,38 +29,41 @@
 package liquibase.ext.opennms.createindex;
 
 import liquibase.database.Database;
-import liquibase.database.structure.DatabaseObject;
-import liquibase.logging.LogFactory;
+import liquibase.logging.LogService;
+import liquibase.logging.Logger;
 import liquibase.sql.Sql;
 import liquibase.sql.UnparsedSql;
 import liquibase.sqlgenerator.SqlGeneratorChain;
 import liquibase.sqlgenerator.core.CreateIndexGenerator;
 import liquibase.statement.core.CreateIndexStatement;
+import liquibase.structure.DatabaseObject;
 
 public class CreateIndexWithWhereGenerator extends CreateIndexGenerator {
+    private static final Logger LOG = LogService.getLog(CreateIndexWithWhereGenerator.class);
 
-        @Override
-	public int getPriority() {
-		return super.getPriority() + 1;
-	}
+    @Override
+    public int getPriority() {
+        return super.getPriority() + 1;
+    }
 
-        @Override
+    @Override
+    @SuppressWarnings("rawtypes")
     public Sql[] generateSql(final CreateIndexStatement statement, final Database database, final SqlGeneratorChain sqlGeneratorChain) {
-    	final Sql[] superSql = super.generateSql(statement, database, sqlGeneratorChain);
+        final Sql[] superSql = super.generateSql(statement, database, sqlGeneratorChain);
 
-    	if (statement instanceof CreateIndexWithWhereStatement) {
-    		if (superSql.length != 1) {
-    			LogFactory.getLogger().warning("expected 1 create index statement, but got " + superSql.length);
-            	return superSql;
-    		}
-    		
-    		return new Sql[] {
-    				new UnparsedSql(superSql[0].toSql() + " WHERE " + ((CreateIndexWithWhereStatement)statement).getWhere(),
-    						superSql[0].getEndDelimiter(), superSql[0].getAffectedDatabaseObjects().toArray(new DatabaseObject[0]))
-    		};
-    	} else {
-    		return superSql;
-    	}
+        if (statement instanceof CreateIndexWithWhereStatement) {
+            if (superSql.length != 1) {
+                LOG.warning("expected 1 create index statement, but got " + superSql.length);
+                return superSql;
+            }
+
+            return new Sql[] {
+                    new UnparsedSql(superSql[0].toSql() + " WHERE " + ((CreateIndexWithWhereStatement)statement).getWhere(),
+                                    superSql[0].getEndDelimiter(), superSql[0].getAffectedDatabaseObjects().toArray(new DatabaseObject[0]))
+            };
+        } else {
+            return superSql;
+        }
     }
 
 }
