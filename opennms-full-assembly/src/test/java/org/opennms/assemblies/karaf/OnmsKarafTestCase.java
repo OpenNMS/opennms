a109e3092f0665f226342880c598b4f2a38ab8f0
Atlassian Bamboo <bamboo@opennms.org>
2019-02-21 12:52:12 -0500

    OpenNMS Horizon 23.0.3

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
@@ -59,7 +59,7 @@ public class OnmsKarafTestCase extends KarafTestCase {
 				.groupId("org.opennms.container")
 				.artifactId("org.opennms.container.karaf")
 				.type("tar.gz")
-				.version("23.0.3-SNAPSHOT");
+				.version("23.0.3");
 	}
 
 	/**
