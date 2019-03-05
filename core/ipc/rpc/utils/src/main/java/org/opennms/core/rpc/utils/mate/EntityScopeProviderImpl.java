79b445080cb2d513c61ced38d27cccecbaaaabbb
Dustin Frisch <fooker@lab.sh>
2019-03-05 21:30:05 +0100

    NMS-10598: Add scope for node/interface/service details

--- core/ipc/rpc/utils/src/main/java/org/opennms/core/rpc/utils/mate/EntityScopeProviderImpl.java
+++ core/ipc/rpc/utils/src/main/java/org/opennms/core/rpc/utils/mate/EntityScopeProviderImpl.java
@@ -31,19 +31,22 @@ package org.opennms.core.rpc.utils.mate;
 import java.net.InetAddress;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.opennms.core.utils.InetAddressUtils;
 import org.opennms.netmgt.dao.api.IpInterfaceDao;
 import org.opennms.netmgt.dao.api.MonitoredServiceDao;
 import org.opennms.netmgt.dao.api.NodeDao;
 import org.opennms.netmgt.model.OnmsIpInterface;
+import org.opennms.netmgt.model.OnmsMetaData;
 import org.opennms.netmgt.model.OnmsMonitoredService;
 import org.opennms.netmgt.model.OnmsNode;
-import org.opennms.netmgt.model.OnmsMetaData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
 
 public class EntityScopeProviderImpl implements EntityScopeProvider {
 
@@ -71,7 +74,21 @@ public class EntityScopeProviderImpl implements EntityScopeProvider {
                 return EmptyScope.EMPTY;
             }
 
-            return new SimpleScope(transform(node.getMetaData()));
+            return new FallbackScope(transform(node.getMetaData()),
+                    new ObjectScope<>(node)
+                            .map("node", "label", (n) -> Optional.of(n.getLabel()))
+                            .map("node", "foreign-source", (n) -> Optional.of(n.getForeignSource()))
+                            .map("node", "foreign-id", (n) -> Optional.of(n.getForeignId()))
+                            .map("node", "netbios-domain", (n) -> Optional.of(n.getNetBiosDomain()))
+                            .map("node", "netbios-name", (n) -> Optional.of(n.getNetBiosName()))
+                            .map("node", "os", (n) -> Optional.of(n.getOperatingSystem()))
+                            .map("node", "sys-name", (n) -> Optional.of(n.getSysName()))
+                            .map("node", "sys-location", (n) -> Optional.of(n.getSysLocation()))
+                            .map("node", "sys-contact", (n) -> Optional.of(n.getSysContact()))
+                            .map("node", "sys-description", (n) -> Optional.of(n.getSysDescription()))
+                            .map("node", "location", (n) -> Optional.of(n.getLocation().getLocationName()))
+                            .map("node", "area", (n) -> Optional.of(n.getLocation().getMonitoringArea()))
+            );
         });
 
         return metaDataScope;
@@ -89,12 +106,20 @@ public class EntityScopeProviderImpl implements EntityScopeProvider {
                 return EmptyScope.EMPTY;
             }
 
-            return new SimpleScope(transform(ipInterface.getMetaData()));
+            return new FallbackScope(transform(ipInterface.getMetaData()),
+                    new ObjectScope<>(ipInterface)
+                            .map("interface", "hostname", (i) -> Optional.of(i.getIpHostName()))
+                            .map("interface", "address", (i) -> Optional.of(i.getIpAddress()).map(InetAddressUtils::toIpAddrString))
+                            .map("interface", "netmask", (i) -> Optional.of(i.getNetMask()).map(InetAddressUtils::toIpAddrString))
+                            .map("interface", "if-index", (i) -> Optional.ofNullable(i.getIfIndex()).map(Object::toString))
+                            .map("interface", "if-alias", (i) -> Optional.of(i.getSnmpInterface().getIfAlias()))
+                            .map("interface", "if-description", (i) -> Optional.of(i.getSnmpInterface().getIfDescr()))
+                            .map("interface", "phy-addr", (i) -> Optional.of(i.getSnmpInterface().getPhysAddr()))
+            );
         });
 
         return metaDataScope;
     }
-
 
 
     @Override
@@ -109,15 +134,22 @@ public class EntityScopeProviderImpl implements EntityScopeProvider {
                 return EmptyScope.EMPTY;
             }
 
-            return new SimpleScope(transform(monitoredService.getMetaData()));
+            return new FallbackScope(transform(monitoredService.getMetaData()),
+                    new ObjectScope<>(monitoredService)
+                            .map("service", "name", (s) -> Optional.of(s.getServiceName()))
+                            .map("service", "type", (s) -> Optional.of(s.getServiceType().getName()))
+                            .map("service", "qualifier", (s) -> Optional.of(s.getQualifier()))
+                            .map("service", "source", (s) -> Optional.of(s.getSource()))
+                            .map("service", "notify", (s) -> Optional.of(s.getNotify()))
+            );
         });
 
         return metaDataScope;
     }
 
-    private static Map<ContextKey, String> transform(Collection<OnmsMetaData> metaData) {
-        return metaData.stream().collect(Collectors.toMap(
-                        e -> new ContextKey(e.getContext(), e.getKey()),
-                        e -> e.getValue()));
+    private static MapScope transform(Collection<OnmsMetaData> metaData) {
+        final Map<ContextKey, String> map = metaData.stream()
+                .collect(Collectors.toMap(e -> new ContextKey(e.getContext(), e.getKey()), e -> e.getValue()));
+        return new MapScope(map);
     }
 }
