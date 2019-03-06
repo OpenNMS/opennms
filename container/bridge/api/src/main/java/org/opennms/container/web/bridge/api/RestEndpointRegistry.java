b27b4a3aa714615453d7e12d9a25f8758f6bef16
Markus von Rüden <mvr@opennms.com>
2019-03-06 15:46:27 +0100

    NMS-10539: Add comment

--- container/bridge/api/src/main/java/org/opennms/container/web/bridge/api/RestEndpointRegistry.java
+++ container/bridge/api/src/main/java/org/opennms/container/web/bridge/api/RestEndpointRegistry.java
@@ -30,6 +30,14 @@ package org.opennms.container.web.bridge.api;
 
 import java.util.List;
 
+/**
+ * The Rest Endpoints are provided by the jax-rs-connector, which already provides access to those.
+ * As those need to be made available to the Jetty Classpath, it would either mean copy over
+ * some jax-rs-connector jars to the ${OPENNMS_HOME}/lib directory and hack the custom.properties, or provide a wrapper implementation.
+ * As the last was easier, this is what was used.
+ *
+ * @author mvrueden
+ */
 public interface RestEndpointRegistry {
     List<String> getRestEndpoints();
 }
