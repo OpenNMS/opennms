6daa58526ddc94a106caa18190a40ffc83a1a41d
David Smith <smith@opennms.com>
2019-04-29 13:10:42 -0400

    NMS-9811: Add the Category to the filter, check afirm and negate scenarios

--- opennms-services/src/test/java/org/opennms/netmgt/collectd/ThresholdIT.java
+++ opennms-services/src/test/java/org/opennms/netmgt/collectd/ThresholdIT.java
@@ -30,6 +30,7 @@ package org.opennms.netmgt.collectd;
 
 import static com.jayway.awaitility.Awaitility.await;
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertEquals;
 import static org.opennms.core.utils.InetAddressUtils.addr;
 
 import java.io.File;
@@ -61,6 +62,7 @@ import org.opennms.netmgt.collection.support.builder.NodeLevelResource;
 import org.opennms.netmgt.config.CollectdConfigFactory;
 import org.opennms.netmgt.config.ThreshdConfigFactory;
 import org.opennms.netmgt.config.ThresholdingConfigFactory;
+import org.opennms.netmgt.dao.api.CategoryDao;
 import org.opennms.netmgt.dao.api.IpInterfaceDao;
 import org.opennms.netmgt.dao.api.NodeDao;
 import org.opennms.netmgt.dao.api.SessionUtils;
@@ -99,6 +101,9 @@ import com.google.common.collect.Lists;
 public class ThresholdIT implements TemporaryDatabaseAware<MockDatabase> {
 
     private CollectdConfigFactory collectdConfigFactory;
+
+    @Autowired
+    private CategoryDao categoryDao;
 
     @Autowired
     private IpInterfaceDao ifaceDao;
@@ -170,10 +175,10 @@ public class ThresholdIT implements TemporaryDatabaseAware<MockDatabase> {
         transTemplate.execute(status -> {
             OnmsNode node = nodeDao.get(1);
             node.setSysObjectId(".1.3.6.1.4.1.8072.3.2.10");
+            node.addCategory(categoryDao.findByName("Development"));
             nodeDao.update(node);
             return node;
         });
-
 
         // Anticipate the high threshold event
         EventBuilder threshBldr = new EventBuilder(EventConstants.HIGH_THRESHOLD_EVENT_UEI, "Test");
@@ -182,10 +187,6 @@ public class ThresholdIT implements TemporaryDatabaseAware<MockDatabase> {
         threshBldr.setService("Mock");
         EventAnticipator eventAnticipator = mockEventIpcManager.getEventAnticipator();
         eventAnticipator.anticipateEvent(threshBldr.getEvent());
-
-        // We added a node, so we need to rebuild the map
-        // FIXME: This should not be necessary either
-        ThreshdConfigFactory.getInstance().rebuildPackageIpListMap();
 
         // Let's send a nodeGainedService event
         EventBuilder bldr = new EventBuilder(EventConstants.NODE_GAINED_SERVICE_EVENT_UEI, "Test");
@@ -201,6 +202,20 @@ public class ThresholdIT implements TemporaryDatabaseAware<MockDatabase> {
 
         // Wait until our threshold was triggered - the anticipator will remove the event from the list once received
         await().atMost(30, TimeUnit.SECONDS).until(eventAnticipator::getAnticipatedEvents, hasSize(0));
+
+        eventAnticipator.reset();
+
+        // Remove the 'Development' category
+        transTemplate.execute(status -> {
+            OnmsNode node = nodeDao.get(1);
+            node.removeCategory(categoryDao.findByName("Development"));
+            nodeDao.update(node);
+            return node;
+        });
+
+        // Assert Threshold is no longer triggered
+        Thread.sleep(30000);
+        assertEquals(0, eventAnticipator.getUnanticipatedEvents().size());
 
         // Stop collectd gracefully so we don't keep trying to collect during the tear down
         collectd.stop();
