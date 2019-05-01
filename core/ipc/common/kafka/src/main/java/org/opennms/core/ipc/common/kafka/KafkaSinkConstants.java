6e6a19964ac7997501a690b56af7292721348f49
Chandra Gorantla <chandra@opennms.com>
2019-05-01 12:29:22 -0400

    HZN-1531: Move cache to google cache so that messages can be expired

--- core/ipc/common/kafka/src/main/java/org/opennms/core/ipc/common/kafka/KafkaSinkConstants.java
+++ core/ipc/common/kafka/src/main/java/org/opennms/core/ipc/common/kafka/KafkaSinkConstants.java
@@ -38,7 +38,13 @@ public interface KafkaSinkConstants {
 
     String KAFKA_CONFIG_SYS_PROP_PREFIX = KAFKA_CONFIG_PID + ".";
 
-    public static final String MAX_BUFFER_SIZE_PROPERTY = "max.buffer.size";
+    // Configurable max buffer size for kafka that should be less than 900KB.
+    String MAX_BUFFER_SIZE_PROPERTY = "max.buffer.size";
     //By default, kafka allows 1MB buffer sizes, here message (content in sink-message.proto) is limited to 900KB.
-    public static final int DEFAULT_MAX_BUFFER_SIZE = 921600;
+    int DEFAULT_MAX_BUFFER_SIZE = 921600;
+
+    // Configurable messageId cache config to specify number of messages and time to expire.
+    String MESSAGEID_CACHE_CONFIG = "messageId.cache.config";
+    // Default to 1000 messages (large) in 10 minute interval.
+    String DEFAULT_MESSAGEID_CONFIG = "maximumSize=1000,expireAfterWrite=10m";
 }
