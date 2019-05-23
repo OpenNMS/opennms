1061b2c6608efdc96a206378e7de73914df3e555
Benjamin Reed <ranger@opennms.org>
2019-05-23 15:52:52 -0400

    2016.1.21 -> 2016.1.22-SNAPSHOT

--- opennms-webapp/gulpfile.js
+++ opennms-webapp/gulpfile.js
@@ -8,7 +8,7 @@ var paths = {
 	'jsp': 'src/main/webapp/**/*.jsp'
 };
 
-var opennmsHome = process.env.OPENNMS_HOME || '../target/opennms-2016.1.21';
+var opennmsHome = process.env.OPENNMS_HOME || '../target/opennms-2016.1.22-SNAPSHOT';
 gutil.log('gulp', 'OpenNMS Home: ' + opennmsHome);
 
 gulp.task('default', ['sass', 'jsp']);
