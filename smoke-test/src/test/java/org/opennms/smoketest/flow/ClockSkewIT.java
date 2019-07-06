5d573d4225475113f09647547ba8260f0728dab4
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2019-07-05 22:15:32 -0400

    NMS-11993: Working smoke-test

--- smoke-test/src/test/java/org/opennms/smoketest/flow/ClockSkewIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/flow/ClockSkewIT.java
@@ -32,7 +32,6 @@ import static com.jayway.awaitility.Awaitility.await;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertNotNull;
 
 import java.net.InetSocketAddress;
 
@@ -41,16 +40,25 @@ import org.junit.Test;
 import org.opennms.core.criteria.CriteriaBuilder;
 import org.opennms.netmgt.dao.api.AlarmDao;
 import org.opennms.netmgt.dao.api.EventDao;
+import org.opennms.netmgt.dao.api.NodeDao;
 import org.opennms.netmgt.dao.hibernate.AlarmDaoHibernate;
 import org.opennms.netmgt.dao.hibernate.EventDaoHibernate;
 import org.opennms.netmgt.model.OnmsAlarm;
 import org.opennms.netmgt.model.OnmsEvent;
+import org.opennms.netmgt.model.OnmsNode;
+import org.opennms.netmgt.model.PrimaryType;
+import org.opennms.netmgt.provision.persist.requisition.Requisition;
+import org.opennms.netmgt.provision.persist.requisition.RequisitionInterface;
+import org.opennms.netmgt.provision.persist.requisition.RequisitionNode;
 import org.opennms.smoketest.stacks.NetworkProtocol;
 import org.opennms.smoketest.stacks.OpenNMSStack;
 import org.opennms.smoketest.stacks.StackModel;
 import org.opennms.smoketest.telemetry.FlowTestBuilder;
 import org.opennms.smoketest.telemetry.FlowTester;
 import org.opennms.smoketest.utils.DaoUtils;
+import org.opennms.smoketest.utils.RestClient;
+
+import com.google.common.collect.ImmutableList;
 
 public class ClockSkewIT {
     @ClassRule
@@ -61,38 +69,73 @@ public class ClockSkewIT {
 
     @Test
     public void verifyClockSkewDetection() throws Exception {
+        // setting up endpoint addresses
         final InetSocketAddress flowTelemetryAddress = stack.minion().getNetworkProtocolAddress(NetworkProtocol.FLOWS);
         final InetSocketAddress opennmsWebAddress = stack.opennms().getWebAddress();
         final InetSocketAddress elasticRestAddress = InetSocketAddress.createUnresolved(
                 stack.elastic().getContainerIpAddress(), stack.elastic().getMappedPort(9200));
 
-        final FlowTester tester = new FlowTestBuilder()
+        // we need the host ip address in the docker network to be used as the exporter address
+        final String localAddress = stack.minion()
+                .getCurrentContainerInfo()
+                .getNetworkSettings()
+                .getNetworks()
+                .entrySet()
+                .stream()
+                .findFirst()
+                .get()
+                .getValue()
+                .getGateway();
+
+        // create node with ip address
+        final RestClient restClient = stack.opennms().getRestClient();
+        final Requisition requisition = new Requisition("TestForeignSource");
+
+        final RequisitionNode requisitionNode = new RequisitionNode();
+        requisitionNode.setNodeLabel("ExporterNode");
+        requisitionNode.setLocation(stack.minion().getLocation());
+
+        final RequisitionInterface requisitionInterface = new RequisitionInterface();
+        requisitionInterface.setIpAddr(localAddress);
+        requisitionInterface.setManaged(true);
+        requisitionInterface.setSnmpPrimary(PrimaryType.PRIMARY);
+        requisitionNode.setInterfaces(ImmutableList.of(requisitionInterface));
+        requisitionNode.setForeignId("ExporterNode");
+        requisition.insertNode(requisitionNode);
+        restClient.addOrReplaceRequisition(requisition);
+        restClient.importRequisition("TestForeignSource");
+
+        // wait till the node is created
+        final NodeDao nodeDao = stack.postgres().dao(org.opennms.netmgt.dao.hibernate.NodeDaoHibernate.class);
+        final OnmsNode onmsNode = await()
+                .atMost(2, MINUTES).pollInterval(5, SECONDS)
+                .until(DaoUtils.findMatchingCallable(nodeDao,
+                        new CriteriaBuilder(OnmsNode.class)
+                                .eq("label", "ExporterNode")
+                                .toCriteria()),
+                        notNullValue());
+
+        // now send Netflow v5 packet
+        final FlowTester flowTester = new FlowTestBuilder()
                 .withNetflow5Packet(flowTelemetryAddress)
                 .verifyOpennmsRestEndpoint(opennmsWebAddress)
                 .build(elasticRestAddress);
-        tester.verifyFlows();
 
+        // and verify
+        flowTester.verifyFlows();
+
+        // check for the event to appear
         final EventDao eventDao = stack.postgres().getDaoFactory().getDao(EventDaoHibernate.class);
-        final AlarmDao alarmDao = stack.postgres().getDaoFactory().getDao(AlarmDaoHibernate.class);
-
-        for(OnmsEvent e : eventDao.findAll()) {
-            System.out.println(e);
-        }
-
         final OnmsEvent onmsEvent = await().atMost(2, MINUTES).pollInterval(10, SECONDS)
                 .until(DaoUtils.findMatchingCallable(eventDao, new CriteriaBuilder(OnmsEvent.class)
                         .eq("eventUei", "uei.opennms.org/internal/telemetry/clockSkewDetected")
                         .toCriteria()), notNullValue());
 
-        for(OnmsAlarm a : alarmDao.findAll()) {
-            System.out.println(a);
-        }
-
+        // check for the alarm to appear
+        final AlarmDao alarmDao = stack.postgres().getDaoFactory().getDao(AlarmDaoHibernate.class);
         final OnmsAlarm onmsAlarm = await().atMost(2, MINUTES).pollInterval(10, SECONDS)
                 .until(DaoUtils.findMatchingCallable(alarmDao, new CriteriaBuilder(OnmsAlarm.class)
                         .eq("uei", "uei.opennms.org/translator/telemetry/clockSkewDetected")
                         .toCriteria()), notNullValue());
-
-        assertNotNull("The clockSkewDetected event was not received at OpenNMS", onmsAlarm);
     }
 }
