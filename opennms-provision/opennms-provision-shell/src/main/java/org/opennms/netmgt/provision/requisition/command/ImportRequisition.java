d050584ea4549b2eeac870c217d27b55ac92416b
Chandra Gorantla <chandra@opennms.com>
2019-07-23 17:19:47 -0400

    NMS-12135: Build url using httpclient

--- opennms-provision/opennms-provision-shell/src/main/java/org/opennms/netmgt/provision/requisition/command/ImportRequisition.java
+++ opennms-provision/opennms-provision-shell/src/main/java/org/opennms/netmgt/provision/requisition/command/ImportRequisition.java
@@ -28,9 +28,11 @@
 
 package org.opennms.netmgt.provision.requisition.command;
 
+import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Argument;
 import org.apache.karaf.shell.api.action.Command;
@@ -49,6 +51,7 @@ import com.google.common.base.Strings;
 public class ImportRequisition implements Action {
 
     public static final String EVENT_SOURCE = "karaf-shell";
+    public static final String URI_SCHEME = "requisition";
 
     @Reference
     private EventForwarder eventForwarder;
@@ -71,18 +74,20 @@ public class ImportRequisition implements Action {
         return sendImportRequisitionEvent(eventForwarder, type, parameters, rescanExisting);
     }
 
-    public static Object sendImportRequisitionEvent(EventForwarder eventForwarder, String type, List<String> parameters, String rescanExisting) {
+    public static Object sendImportRequisitionEvent(EventForwarder eventForwarder, String type, List<String> parameters, String rescanExisting) throws URISyntaxException {
         EventBuilder eventBuilder = new EventBuilder(EventConstants.RELOAD_IMPORT_UEI, EVENT_SOURCE);
 
         if((!Strings.isNullOrEmpty(type)) && !parameters.isEmpty()) {
-            String url = String.format("requisition://%s?%s", type, String.join("&", parameters));
+            URIBuilder builder = new URIBuilder().setScheme(URI_SCHEME).setHost(type);
+            parse(parameters, builder);
+            String url = builder.build().toString();
             eventBuilder.addParam(EventConstants.PARM_URL, url);
             if (!Strings.isNullOrEmpty(rescanExisting)) {
                 List<String> validValues = Arrays.asList("yes", "dbonly", "no");
                 if(validValues.contains(rescanExisting)) {
                     eventBuilder.addParam(EventConstants.PARM_IMPORT_RESCAN_EXISTING, rescanExisting);
                 } else {
-                    System.out.printf("Not a valid rescanExisting value, valid values are : 'yes', 'no', 'dbonly'\n");
+                    System.out.printf("Not a valid rescanExisting value, valid values are : %s \n", validValues);
                     return null;
                 }
             }
@@ -93,5 +98,23 @@ public class ImportRequisition implements Action {
         }
         return null;
     }
+
+    private static void parse(List<String> attributeList, URIBuilder builder) {
+        if (attributeList != null) {
+            for (String keyValue : attributeList) {
+                int splitAt = keyValue.indexOf("=");
+                if (splitAt <= 0) {
+                    throw new IllegalArgumentException("Invalid property " + keyValue);
+                } else {
+                    String key = keyValue.substring(0, splitAt);
+                    String value = keyValue.substring(splitAt + 1, keyValue.length());
+                    builder.addParameter(key, value);
+                }
+            }
+        }
+    }
+
+
+
 
 }
