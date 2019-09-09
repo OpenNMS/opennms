c8534bae0253194b9622196e7736f92f45832584
Chandra Gorantla <chandra@opennms.com>
2019-09-09 10:39:11 -0400

    NMS-12256: Resource Graphs : Fix long URL issues with Graph Selected (#2679)
    * NMS-12256: Add Rest service to generate Id for resources
    
    * NMS-12256: Handle graphs with different time periods
    
    * NMS-12256: Fix tests
    
    * NMS-12256: Handle review comments
    
    * NMS-12256: Keep the jsonStore name as it is.
    
    * NMS-12256: Remove `resourceId` from the payload

--- opennms-web-api/src/main/java/org/opennms/web/svclayer/support/DefaultGraphResultsService.java
+++ opennms-web-api/src/main/java/org/opennms/web/svclayer/support/DefaultGraphResultsService.java
@@ -36,7 +36,9 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
+import org.opennms.features.distributed.kvstore.api.JsonStore;
 import org.opennms.netmgt.dao.api.GraphDao;
 import org.opennms.netmgt.dao.api.NodeDao;
 import org.opennms.netmgt.dao.api.ResourceDao;
@@ -62,6 +64,7 @@ import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
 
 import com.google.common.base.Strings;
+import com.google.gson.Gson;
 
 /**
  * <p>DefaultGraphResultsService class.</p>
@@ -73,6 +76,8 @@ public class DefaultGraphResultsService implements GraphResultsService, Initiali
 	
 	private static final Logger LOG = LoggerFactory.getLogger(DefaultGraphResultsService.class);
 
+	public static final String RESOURCE_IDS_CONTEXT = "resourceIds";
+
     private ResourceDao m_resourceDao;
 
     private GraphDao m_graphDao;
@@ -82,6 +87,10 @@ public class DefaultGraphResultsService implements GraphResultsService, Initiali
     private RrdDao m_rrdDao;
 
     private EventProxy m_eventProxy;
+
+    private JsonStore m_jsonStore;
+
+    private final Gson m_gson = new Gson();
 
     private RelativeTimePeriod[] m_periods;
 
@@ -94,7 +103,7 @@ public class DefaultGraphResultsService implements GraphResultsService, Initiali
     }
 
     @Override
-    public GraphResults findResults(ResourceId[] resourceIds, String[] reports, String nodeCriteria, long start, long end, String relativeTime) {
+    public GraphResults findResults(ResourceId[] resourceIds, String[] reports, String generatedId, String nodeCriteria, long start, long end, String relativeTime) {
         if (reports == null) {
             throw new IllegalArgumentException("reports argument cannot be null");
         }
@@ -108,8 +117,9 @@ public class DefaultGraphResultsService implements GraphResultsService, Initiali
         graphResults.setRelativeTime(relativeTime);
         graphResults.setRelativeTimePeriods(m_periods);
         graphResults.setReports(reports);
+        graphResults.setNodeCriteria(nodeCriteria);
+        graphResults.setGeneratedId(generatedId);
 
-        // ResourceIds can be empty in case of GraphAll where resources are pulled from node.
         HashMap<ResourceId, List<OnmsResource>> resourcesMap = new HashMap<>();
         for (ResourceId resourceId : resourceIds) {
             LOG.debug("findResults: parent, childType, childName = {}, {}, {}", resourceId.parent, resourceId.type, resourceId.name);
@@ -138,11 +148,12 @@ public class DefaultGraphResultsService implements GraphResultsService, Initiali
             }
         }
 
-        if(!Strings.isNullOrEmpty(nodeCriteria)) {
+        // GraphAll case where all resources are fetched from node.
+        if (!Strings.isNullOrEmpty(nodeCriteria)) {
             OnmsNode node = m_nodeDao.get(nodeCriteria);
-            if(node != null) {
+            if (node != null) {
                 OnmsResource nodeResource = m_resourceDao.getResourceForNode(node);
-                if(nodeResource != null) {
+                if (nodeResource != null) {
                     List<OnmsResource> childResources = nodeResource.getChildResources();
                     for (OnmsResource resource : childResources) {
                         try {
@@ -151,6 +162,27 @@ public class DefaultGraphResultsService implements GraphResultsService, Initiali
                             LOG.warn(e.getMessage(), e);
                         }
                     }
+                }
+            }
+        }
+        // GraphSelected case where all resources are fetched from generatedId
+        if (!Strings.isNullOrEmpty(generatedId)) {
+            Optional<String> result = m_jsonStore.get(generatedId, RESOURCE_IDS_CONTEXT);
+            if (result.isPresent()) {
+                try {
+                    String[] resourceArray = m_gson.fromJson(result.get(), String[].class);
+                    for (String resourceId : resourceArray) {
+                        try {
+                            OnmsResource resource = m_resourceDao.getResourceById(ResourceId.fromString(resourceId));
+                            if(resource != null) {
+                                graphResults.addGraphResultSet(createGraphResultSet(null, resource, reports, graphResults));
+                            }
+                        } catch (IllegalArgumentException e) {
+                            LOG.warn(e.getMessage(), e);
+                        }
+                    }
+                } catch (Exception e) {
+                    LOG.warn("Exception while parsing json string {}", result.get());
                 }
             }
         }
@@ -266,6 +298,7 @@ public class DefaultGraphResultsService implements GraphResultsService, Initiali
         Assert.state(m_resourceDao != null, "resourceDao property has not been set");
         Assert.state(m_graphDao != null, "graphDao property has not been set");
         Assert.state(m_rrdDao != null, "rrdDao property has not been set");
+        Assert.state(m_jsonStore != null, "jsonStore property has not been set");
     }
 
     /**
@@ -349,5 +382,14 @@ public class DefaultGraphResultsService implements GraphResultsService, Initiali
     public void setEventProxy(EventProxy eventProxy) {
         m_eventProxy = eventProxy;
     }
-    
+
+
+    /**
+     * <p>setJsonStore</p>
+     *
+     * @param jsonStore a @{@link JsonStore} object.
+     */
+    public void setJsonStore(JsonStore jsonStore) {
+        m_jsonStore = jsonStore;
+    }
 }
