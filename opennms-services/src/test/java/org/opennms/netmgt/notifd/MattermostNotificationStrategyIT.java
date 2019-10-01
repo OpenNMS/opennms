ebfe4a56488484761a05be46540dd59358028242
Markus von Rüden <mvr@opennms.com>
2019-10-01 15:50:46 +0200

    NMS-10552: Cleanup

--- opennms-services/src/test/java/org/opennms/netmgt/notifd/MattermostNotificationStrategyIT.java
+++ opennms-services/src/test/java/org/opennms/netmgt/notifd/MattermostNotificationStrategyIT.java
@@ -31,11 +31,9 @@ package org.opennms.netmgt.notifd;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.json.simple.JSONObject;
 import org.junit.Test;
@@ -73,59 +71,75 @@ public class MattermostNotificationStrategyIT {
     public void testSendValidJustArgs() {
         final int port = JUnitHttpServerExecutionListener.getPort();
         assertTrue(port > 0);
-        try {
 
-            final NotificationStrategy ns = new MattermostNotificationStrategy();
-            final List<Argument> arguments = new ArrayList<Argument>();
+        final NotificationStrategy ns = new MattermostNotificationStrategy();
+        final List<Argument> arguments = new ArrayList<Argument>();
 
-            // Set these properties. We will override them with Args on the first run.
-            System.setProperty("org.opennms.netmgt.notifd.mattermost.webhookURL", "http://localhost:" + port + "/hooks/abunchofstuffthatidentifiesawebhook");
-            System.setProperty("org.opennms.netmgt.notifd.mattermost.channel", "integrationtestsXX");
-            System.setProperty("org.opennms.netmgt.notifd.mattermost.iconURL", "http://opennms.org/logo.pngXX");
-            System.setProperty("org.opennms.netmgt.notifd.mattermost.iconEmoji", ":shipitXX:");
-            System.setProperty("org.opennms.netmgt.notifd.mattermost.username", "opennmsXX");
+        // Set these properties. We will override them with Args on the first run.
+        System.setProperty("org.opennms.netmgt.notifd.mattermost.webhookURL", "http://localhost:" + port + "/hooks/abunchofstuffthatidentifiesawebhook");
+        System.setProperty("org.opennms.netmgt.notifd.mattermost.channel", "integrationtestsXX");
+        System.setProperty("org.opennms.netmgt.notifd.mattermost.iconURL", "http://opennms.org/logo.pngXX");
+        System.setProperty("org.opennms.netmgt.notifd.mattermost.iconEmoji", ":shipitXX:");
+        System.setProperty("org.opennms.netmgt.notifd.mattermost.username", "opennmsXX");
 
-            arguments.add(new Argument("-url", null, "http://localhost:" + port + "/hooks/abunchofstuffthatidentifiesawebhook", false));
-            arguments.add(new Argument("-channel", null, "integrationtests", false));
-            arguments.add(new Argument("-username", null, "opennms", false));
-            arguments.add(new Argument("-iconurl", null, "http://opennms.org/logo.png", false));
-            arguments.add(new Argument("-iconemoji", null, ":shipit:", false));
-            arguments.add(new Argument("-subject", null, "Test", false));
-            arguments.add(new Argument("-tm", null, "This is only a test", false));
-            
-            int statusCode = ns.send(arguments);
-            assertEquals(0, statusCode);
-            
-            JSONObject inputJson = MattermostNotificationStrategyTestServlet.getInputJson();
-            assertNotNull(inputJson);
-            assertEquals("opennms", inputJson.get("username"));
-            assertEquals("#### Test\nThis is only a test", inputJson.get("text"));
-            assertEquals("integrationtests", inputJson.get("channel"));
-            assertEquals("http://opennms.org/logo.png", inputJson.get("icon_url"));
-            assertEquals(":shipit:", inputJson.get("icon_emoji"));
-            assertEquals(5, inputJson.size());
-            
-            // Now do it again, without the Args, and verify that the property values come out
-            arguments.clear();
-            arguments.add(new Argument("-subject", null, "Test again", false));
-            arguments.add(new Argument("-tm", null, "This is only a second test", false));
-            
-            statusCode = ns.send(arguments);
-            assertEquals(0, statusCode);
-            
-            inputJson = MattermostNotificationStrategyTestServlet.getInputJson();
-            assertNotNull(inputJson);
-            assertEquals("opennmsXX", inputJson.get("username"));
-            assertEquals("#### Test again\nThis is only a second test", inputJson.get("text"));
-            assertEquals("integrationtestsXX", inputJson.get("channel"));
-            assertEquals("http://opennms.org/logo.pngXX", inputJson.get("icon_url"));
-            assertEquals(":shipitXX:", inputJson.get("icon_emoji"));
-            assertEquals(5, inputJson.size());
+        arguments.add(new Argument("-url", null, "http://localhost:" + port + "/hooks/abunchofstuffthatidentifiesawebhook", false));
+        arguments.add(new Argument("-channel", null, "integrationtests", false));
+        arguments.add(new Argument("-username", null, "opennms", false));
+        arguments.add(new Argument("-iconurl", null, "http://opennms.org/logo.png", false));
+        arguments.add(new Argument("-iconemoji", null, ":shipit:", false));
+        arguments.add(new Argument("-subject", null, "Test", false));
+        arguments.add(new Argument("-tm", null, "This is only a test", false));
 
-        } catch (Throwable e) {
-            e.printStackTrace();
-            fail("Caught Exception: " + e.getMessage());
-        }
+        int statusCode = ns.send(arguments);
+        assertEquals(0, statusCode);
+
+        JSONObject inputJson = MattermostNotificationStrategyTestServlet.getInputJson();
+        assertNotNull(inputJson);
+        assertEquals("opennms", inputJson.get("username"));
+        assertEquals("#### Test\nThis is only a test", inputJson.get("text"));
+        assertEquals("integrationtests", inputJson.get("channel"));
+        assertEquals("http://opennms.org/logo.png", inputJson.get("icon_url"));
+        assertEquals(":shipit:", inputJson.get("icon_emoji"));
+        assertEquals(5, inputJson.size());
+
+        // Now do it again, without the Args, and verify that the property values come out
+        arguments.clear();
+        arguments.add(new Argument("-subject", null, "Test again", false));
+        arguments.add(new Argument("-tm", null, "This is only a second test", false));
+
+        statusCode = ns.send(arguments);
+        assertEquals(0, statusCode);
+
+        inputJson = MattermostNotificationStrategyTestServlet.getInputJson();
+        assertNotNull(inputJson);
+        assertEquals("opennmsXX", inputJson.get("username"));
+        assertEquals("#### Test again\nThis is only a second test", inputJson.get("text"));
+        assertEquals("integrationtestsXX", inputJson.get("channel"));
+        assertEquals("http://opennms.org/logo.pngXX", inputJson.get("icon_url"));
+        assertEquals(":shipitXX:", inputJson.get("icon_emoji"));
+        assertEquals(5, inputJson.size());
+
+        // Now do it again without the leading - in switch names
+        arguments.clear();
+        arguments.add(new Argument("url", null, "http://localhost:" + port + "/hooks/abunchofstuffthatidentifiesawebhook", false));
+        arguments.add(new Argument("channel", null, "integrationtests", false));
+        arguments.add(new Argument("username", null, "opennms", false));
+        arguments.add(new Argument("iconurl", null, "http://opennms.org/logo.png", false));
+        arguments.add(new Argument("iconemoji", null, ":shipit:", false));
+        arguments.add(new Argument("subject", null, "Test", false));
+        arguments.add(new Argument("tm", null, "This is only a test", false));
+
+        statusCode = ns.send(arguments);
+        assertEquals(0, statusCode);
+
+        inputJson = MattermostNotificationStrategyTestServlet.getInputJson();
+        assertNotNull(inputJson);
+        assertEquals("opennms", inputJson.get("username"));
+        assertEquals("#### Test\nThis is only a test", inputJson.get("text"));
+        assertEquals("integrationtests", inputJson.get("channel"));
+        assertEquals("http://opennms.org/logo.png", inputJson.get("icon_url"));
+        assertEquals(":shipit:", inputJson.get("icon_emoji"));
+        assertEquals(5, inputJson.size());
     }
 
 }
--- opennms-services/src/test/java/org/opennms/netmgt/notifd/SlackCompatibleNotificationStrategyTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/notifd/SlackCompatibleNotificationStrategyTest.java
@@ -28,21 +28,44 @@
 
 package org.opennms.netmgt.notifd;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.opennms.netmgt.model.notifd.Argument;
 
 import com.google.common.collect.Lists;
 
+@RunWith(Parameterized.class)
 public class SlackCompatibleNotificationStrategyTest {
 
+    @Parameterized.Parameters
+    public static Collection<Object[]> params() {
+        return Arrays.asList(
+            new Object[][] {
+                { new MattermostNotificationStrategy(), "org.opennms.netmgt.notifd.mattermost" },
+                { new SlackNotificationStrategy(), "org.opennms.netmgt.notifd.slack" }
+        });
+    }
+
+    private final AbstractSlackCompatibleNotificationStrategy strategy;
+    private final String systemPropertiesPrefix;
+
+    public SlackCompatibleNotificationStrategyTest(final AbstractSlackCompatibleNotificationStrategy strategy, final String systemPropertiesPrefix) {
+        this.strategy = strategy;
+        this.systemPropertiesPrefix = systemPropertiesPrefix;
+    }
+
     @Test
-    public void verifyArguments() {
+    public void verifyArgumentsWorkWithAndWithoutSlash() {
         final List<Argument> arguments = Lists.newArrayList();
-        arguments.add(new Argument("-url", null, "url", false));
+        arguments.add(new Argument("-url", null, "http://localhost:1234/hooks/hooky", false));
         arguments.add(new Argument("-channel", null, "channel", false));
         arguments.add(new Argument("-username", null, "opennms", false));
         arguments.add(new Argument("-iconurl", null, "http://opennms.org/logo.png", false));
@@ -50,11 +73,46 @@ public class SlackCompatibleNotificationStrategyTest {
         arguments.add(new Argument("-subject", null, "Test", false));
         arguments.add(new Argument("-tm", null, "This is only a test", false));
 
-        final AbstractSlackCompatibleNotificationStrategy mattermostNotificationStrategy = new MattermostNotificationStrategy();
-        Assert.assertThat(mattermostNotificationStrategy.getUrl(), Matchers.is("url"));
-        mattermostNotificationStrategy.getChannel();
-        mattermostNotificationStrategy.getIconEmoji();
-        mattermostNotificationStrategy.getIconUrl();
-        mattermostNotificationStrategy.getUsername();
+        strategy.setArguments(arguments);
+        assertThat(strategy.getValue("url"), is("http://localhost:1234/hooks/hooky"));
+        assertThat(strategy.getValue("-url"), is("http://localhost:1234/hooks/hooky"));
+
+        assertThat(strategy.getValue("channel"), is("channel"));
+        assertThat(strategy.getValue("-channel"), is("channel"));
+
+        assertThat(strategy.getValue("username"), is("opennms"));
+        assertThat(strategy.getValue("-username"), is("opennms"));
+
+        assertThat(strategy.getValue("iconurl"), is("http://opennms.org/logo.png"));
+        assertThat(strategy.getValue("-iconurl"), is("http://opennms.org/logo.png"));
+
+        assertThat(strategy.getValue("iconemoji"), is(":shipit:"));
+        assertThat(strategy.getValue("-iconemoji"), is(":shipit:"));
+
+        assertThat(strategy.getValue("subject"), is("Test"));
+        assertThat(strategy.getValue("-subject"), is("Test"));
+
+        assertThat(strategy.getValue("tm"), is("This is only a test"));
+        assertThat(strategy.getValue("-tm"), is("This is only a test"));
+    }
+
+    @Test
+    public void verifyArgumentsFallbackToSystemProperty() {
+        System.setProperty(getPropertyName("webhookURL"), "http://localhost:4321/hooks/abunchofstuffthatidentifiesawebhook");
+        System.setProperty(getPropertyName("channel"), "integrationtestsXX");
+        System.setProperty(getPropertyName("iconURL"), "http://opennms.org/logo.pngXX");
+        System.setProperty(getPropertyName("iconEmoji"), ":shipitXX:");
+        System.setProperty(getPropertyName("username"), "opennmsXX");
+
+        final AbstractSlackCompatibleNotificationStrategy strategy = new MattermostNotificationStrategy();
+        assertThat(strategy.getValue("-url", getPropertyName("webhookURL")), is("http://localhost:4321/hooks/abunchofstuffthatidentifiesawebhook"));
+        assertThat(strategy.getValue("-channel", getPropertyName("channel")), is("integrationtestsXX"));
+        assertThat(strategy.getValue("-iconurl", getPropertyName("iconURL")), is("http://opennms.org/logo.pngXX"));
+        assertThat(strategy.getValue("-iconemoji", getPropertyName("iconEmoji")), is(":shipitXX:"));
+        assertThat(strategy.getValue("-username", getPropertyName("username")), is("opennmsXX"));
+    }
+
+    private String getPropertyName(String name) {
+        return String.format("%s.%s", systemPropertiesPrefix, name);
     }
 }
