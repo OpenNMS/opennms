810a84dca74ec71d3063044f9ef934567f19fd5a
Benjamin Reed <ranger@opennms.org>
2019-10-03 10:21:08 -0400

    fix downloads when auth is required

--- opennms-container/fetch-build-rpms.sh
+++ opennms-container/fetch-build-rpms.sh
@@ -3,27 +3,36 @@
 BAMBOO_HOST="https://bamboo.opennms.org"
 
 # We need a url
-if [ "${#}" -ne 1 ]; then
+if [ "${#}" -lt 1 ]; then
     echo ""
     echo "Script to download RPM artifacts from OpenNMS Bamboo system."
     echo "The argument is a Bamboo URL from a specific build. The rpms"
     echo "will be downloaded into an rpms subdirectory."
     echo ""
+    echo "If you are downloading Meridian RPMs, you will need to pass"
+    echo "the auth parameter as well."
+    echo ""
     echo "Example:"
     echo "  url: https://${BAMBOO_HOST}/browse/OPENNMS-ONMS2852-6"
     echo ""
-    echo "Usage: $0 <url>"
+    echo "Usage: $0 <url> [user:pass]"
     exit 1
 fi
 
 # PARSE URL
 URL=$1
+AUTH=$2
 BUILD=$(echo "${URL}" | awk -F'/' '{ print $NF }')
 PLAN_KEY=$(echo "${BUILD}" | awk -F'-' '{ print $(NF-2) "-" $(NF-1) }')
 BUILD_ID=$(echo "${BUILD}" | awk -F'-' '{ print $NF }')
 
+CURL_CMD=(curl -s -L)
+if [ -n "$AUTH" ]; then
+    CURL_CMD+=(-u "$AUTH")
+fi
+
 # Figure out RPM_VERSION
-RPM_VERSION=$(curl -s "${BAMBOO_HOST}/artifact/${PLAN_KEY}/shared/build-${BUILD_ID}/RPMs/" | grep -i meridian-core | sed -E 's/(.*>)(meridian-core-)(.*)\.noarch.rpm<\/a>.*/\3/g')
+RPM_VERSION=$("${CURL_CMD[@]}" "${BAMBOO_HOST}/artifact/${PLAN_KEY}/shared/build-${BUILD_ID}/RPMs/" | grep -i meridian-core | sed -E 's/(.*>)(meridian-core-)(.*)\.noarch.rpm<\/a>.*/\3/g')
 
 RPMS_MERIDIAN=("meridian-core-${RPM_VERSION}.noarch.rpm"
               "meridian-webapp-jetty-${RPM_VERSION}.noarch.rpm"
@@ -49,20 +58,32 @@ if [ -z "${BUILD}" ] || [ -z "${PLAN_KEY}" ] || [ -z "${BUILD_ID}" ] || [ -z "${
     exit 2
 fi
 
+pushd horizon/rpms || exit 1
 for RPM in ${RPMS_MERIDIAN[*]}; do
     echo "RPM: ${RPM}"
-    wget --no-clobber "${BAMBOO_HOST}/artifact/${PLAN_KEY}/shared/build-${BUILD_ID}/RPMs/${RPM}" -P meridian/rpms
+    if [ ! -f "${RPM}" ]; then
+        "${CURL_CMD[@]}" -O "${BAMBOO_HOST}/artifact/${PLAN_KEY}/shared/build-${BUILD_ID}/RPMs/${RPM}"
+    fi
 done
+popd || exit 1
 
+pushd minion/rpms || exit 1
 for RPM in ${RPMS_MINION[*]}; do
     echo "RPM: ${RPM}"
-    wget --no-clobber "${BAMBOO_HOST}/artifact/${PLAN_KEY}/shared/build-${BUILD_ID}/RPMs/${RPM}" -P minion/rpms
+    if [ ! -f "${RPM}" ]; then
+        "${CURL_CMD[@]}" -O "${BAMBOO_HOST}/artifact/${PLAN_KEY}/shared/build-${BUILD_ID}/RPMs/${RPM}"
+    fi
 done
+popd || exit 1
 
+pushd sentinel/rpms || exit 1
 for RPM in ${RPMS_SENTINEL[*]}; do
     echo "RPM: ${RPM}"
-    wget --no-clobber "${BAMBOO_HOST}/artifact/${PLAN_KEY}/shared/build-${BUILD_ID}/RPMs/${RPM}" -P sentinel/rpms
+    if [ ! -f "${RPM}" ]; then
+        "${CURL_CMD[@]}" -O "${BAMBOO_HOST}/artifact/${PLAN_KEY}/shared/build-${BUILD_ID}/RPMs/${RPM}"
+    fi
 done
+popd || exit 1
 
 cat <<END
 
