9a38a008d904c00ac234381f64094f660876b7ca
Benjamin Reed <ranger@opennms.org>
2019-10-15 11:30:45 -0400

    Revert "NMS-12307: Add ability to update definitions whenever SNMP profile changes (#2738)"
    This reverts commit e3834d4a7e1dcdc6aacb39fcce84930b231ecb96.

--- core/snmp/profile-mapper/src/test/java/org/opennms/core/snmp/profile/mapper/SnmpProfileMapperIT.java
+++ core/snmp/profile-mapper/src/test/java/org/opennms/core/snmp/profile/mapper/SnmpProfileMapperIT.java
@@ -35,9 +35,7 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
@@ -45,6 +43,7 @@ import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -52,15 +51,16 @@ import org.opennms.core.snmp.profile.mapper.impl.SnmpProfileMapperImpl;
 import org.opennms.core.test.OpenNMSJUnit4ClassRunner;
 import org.opennms.core.test.snmp.ProxySnmpAgentConfigFactory;
 import org.opennms.core.test.snmp.annotations.JUnitSnmpAgent;
+import org.opennms.netmgt.config.SnmpPeerFactory;
 import org.opennms.netmgt.config.snmp.Definition;
+import org.opennms.netmgt.config.snmp.SnmpProfile;
+import org.opennms.netmgt.config.snmp.SnmpProfiles;
 import org.opennms.netmgt.filter.api.FilterDao;
 import org.opennms.netmgt.snmp.SnmpAgentConfig;
-import org.opennms.netmgt.snmp.SnmpProfileMapper;
 import org.opennms.netmgt.snmp.proxy.LocationAwareSnmpClient;
 import org.opennms.test.JUnitConfigurationEnvironment;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 
 
@@ -68,33 +68,63 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration(locations = {
         "classpath:/META-INF/opennms/applicationContext-soa.xml",
         "classpath:/META-INF/opennms/applicationContext-mockDao.xml",
-        "classpath:/META-INF/opennms/applicationContext-rpc-client-mock.xml",
-        "classpath:/META-INF/opennms/applicationContext-rpc-snmp.xml"
+        "classpath:/META-INF/opennms/applicationContext-proxy-snmp.xml",
 })
 @JUnitConfigurationEnvironment
-@JUnitSnmpAgent(host = "192.0.1.206", resource = "classpath:/snmpProfileTestData.properties")
+@JUnitSnmpAgent(host = "192.0.1.206", resource = "classpath:/snmpTestData1.properties")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class SnmpProfileMapperIT {
 
-    private static final Logger LOG = LoggerFactory.getLogger(SnmpProfileMapperIT.class);
+    @Autowired
+    private SnmpPeerFactory snmpPeerFactory;
 
     @Autowired
     private LocationAwareSnmpClient locationAwareSnmpClient;
 
-    private SnmpProfileMapper profileMapper;
+    private FilterDao filterDao;
 
-    private ProxySnmpAgentConfigFactory snmpPeerFactory;
+    private SnmpProfileMapperImpl profileMapper;
 
 
-    @Test(timeout = 30000)
+    @Before
+    public void setup() throws IOException {
+        SnmpPeerFactory.setInstance(snmpPeerFactory);
+        assertTrue(snmpPeerFactory instanceof ProxySnmpAgentConfigFactory);
+        filterDao = Mockito.mock(FilterDao.class);
+        when(filterDao.isValid(Mockito.anyString(), Mockito.contains("IPLIKE"))).thenReturn(true);
+        profileMapper = new SnmpProfileMapperImpl(filterDao, snmpPeerFactory, locationAwareSnmpClient);
+    }
+
+    @Test
     public void testSnmpProfileMapper() throws UnknownHostException {
 
-        // timeout and ttl from first profile.
-        int timeout = 5000;
-        long ttl = 10000;
-        try (InputStream configStream = getClass().getResource("/snmp-config1.xml").openStream()) {
-            setUpProfileMapper(configStream);
-            CompletableFuture<Optional<SnmpAgentConfig>> future = profileMapper.getAgentConfigFromProfiles(InetAddress.getByName("192.0.1.206"),
-                    null);
+        int timeout = 4000;
+        long ttl = 6000;
+        SnmpProfiles snmpProfiles = new SnmpProfiles();
+        SnmpProfile snmpProfile = new SnmpProfile();
+        snmpProfile = new SnmpProfile();
+        snmpProfile.setLabel("profile1");
+        snmpProfile.setVersion("v1");
+        snmpProfile.setTimeout(timeout);
+        snmpProfile.setTTL(ttl);
+        snmpProfiles.addSnmpProfile(snmpProfile);
+        snmpProfile = new SnmpProfile();
+        snmpProfile.setLabel("profile2");
+        snmpProfile.setVersion("v1");
+        snmpProfile.setFilterExpression("IPADDR IPLIKE 192.0.*.*");
+        snmpProfile.setTimeout(10000);
+        snmpProfile.setTTL(ttl);
+        snmpProfiles.addSnmpProfile(snmpProfile);
+        snmpProfile = new SnmpProfile();
+        snmpProfile.setLabel("profile3");
+        snmpProfile.setVersion("v2c");
+        snmpProfile.setTimeout(12000);
+        snmpProfile.setTTL(ttl);
+        snmpProfiles.addSnmpProfile(snmpProfile);
+        snmpPeerFactory.getSnmpConfig().setSnmpProfiles(snmpProfiles);
+        CompletableFuture<Optional<SnmpAgentConfig>> future = profileMapper.getAgentConfigFromProfiles(InetAddress.getByName("192.0.1.206"),
+                null);
+        try {
             Optional<SnmpAgentConfig> agentConfig = future.get();
             assertTrue(agentConfig.isPresent());
             assertEquals(ttl, agentConfig.get().getTTL().longValue());
@@ -103,21 +133,29 @@ public class SnmpProfileMapperIT {
             List<Definition> definitions = snmpPeerFactory.getSnmpConfig().getDefinitions();
             Optional<Definition> definition = definitions.stream().filter(def -> def.getTimeout() == timeout).findFirst();
             assertTrue(definition.isPresent());
-        } catch (InterruptedException | ExecutionException | IOException e) {
+        } catch (InterruptedException | ExecutionException e) {
             fail();
         }
     }
 
-    @Test(timeout = 30000)
-    public void testSnmpProfileMapperWithCustomOID() {
+    @Test
+    public void testSnmpProfileMapperWithCustomOID() throws UnknownHostException {
 
-        // timeout and ttl from profile.
+        // Add profile with custom timeout and ttl
         int timeout = 6000;
         long ttl = 7000;
-        try (InputStream configStream = getClass().getResource("/snmp-config2.xml").openStream()) {
-            setUpProfileMapper(configStream);
-            CompletableFuture<Optional<SnmpAgentConfig>> future = profileMapper.getAgentConfigFromProfiles(InetAddress.getByName("192.0.1.206"),
-                    "Minion", ".1.3.6.1.2.1.1.3.0");
+        SnmpProfiles snmpProfiles = new SnmpProfiles();
+        SnmpProfile snmpProfile = new SnmpProfile();
+        snmpProfile.setLabel("profile1");
+        snmpProfile.setVersion("v1");
+        snmpProfile.setTimeout(timeout);
+        snmpProfile.setTTL(ttl);
+        snmpProfiles.addSnmpProfile(snmpProfile);
+        snmpPeerFactory.getSnmpConfig().setSnmpProfiles(snmpProfiles);
+        //Profile without filter expression should always match and that would be set.
+        CompletableFuture<Optional<SnmpAgentConfig>> future = profileMapper.getAgentConfigFromProfiles(InetAddress.getByName("192.0.1.206"),
+                "Minion", ".1.3.6.1.2.1.1.3.0");
+        try {
             Optional<SnmpAgentConfig> agentConfig = future.get();
             assertTrue(agentConfig.isPresent());
             assertEquals(agentConfig.get().getTTL().longValue(), ttl);
@@ -126,24 +164,30 @@ public class SnmpProfileMapperIT {
             List<Definition> definitions = snmpPeerFactory.getSnmpConfig().getDefinitions();
             Optional<Definition> definition = definitions.stream().filter(def -> def.getTimeout() == timeout).findFirst();
             assertTrue(definition.isPresent());
-            assertEquals("profile1", definition.get().getProfileLabel());
-        } catch (InterruptedException | ExecutionException | IOException e) {
+        } catch (InterruptedException | ExecutionException e) {
             fail();
         }
     }
 
 
-    @Test(timeout = 30000)
+    @Test
     public void testFitProfile() throws UnknownHostException {
 
-        // timeout and ttl from profile.
-        int timeout = 6000;
-        long ttl = 7000;
+        // Add profile with custom timeout and ttl
+        int timeout = 10000;
+        long ttl = 8000;
         SnmpAgentConfig snmpAgentConfig = null;
-        try (InputStream configStream = getClass().getResource("/snmp-config2.xml").openStream()) {
-            setUpProfileMapper(configStream);
-            //Profile without filter expression should always match and that would be set.
-            CompletableFuture<Optional<SnmpAgentConfig>> future = profileMapper.fitProfile("profile1", InetAddress.getByName("192.0.1.206"), "Minion", ".1.3.6.1.2.1.1.3.0");
+        SnmpProfiles snmpProfiles = new SnmpProfiles();
+        SnmpProfile snmpProfile = new SnmpProfile();
+        snmpProfile.setLabel("profile1");
+        snmpProfile.setVersion("v1");
+        snmpProfile.setTimeout(timeout);
+        snmpProfile.setTTL(ttl);
+        snmpProfiles.addSnmpProfile(snmpProfile);
+        snmpPeerFactory.getSnmpConfig().setSnmpProfiles(snmpProfiles);
+        //Profile without filter expression should always match and that would be set.
+        CompletableFuture<Optional<SnmpAgentConfig>> future = profileMapper.fitProfile("profile1", InetAddress.getByName("192.0.1.206"), "Minion", ".1.3.6.1.2.1.1.3.0");
+        try {
             Optional<SnmpAgentConfig> agentConfig = future.get();
             if (agentConfig.isPresent()) {
                 snmpAgentConfig = agentConfig.get();
@@ -151,20 +195,13 @@ public class SnmpProfileMapperIT {
             assertNotNull(snmpAgentConfig);
             assertEquals(snmpAgentConfig.getTTL().longValue(), ttl);
             assertEquals(snmpAgentConfig.getTimeout(), timeout);
-            // Check that fit profile doesn't actually save definitions in the config.
             List<Definition> definitions = snmpPeerFactory.getSnmpConfig().getDefinitions();
             Optional<Definition> definition = definitions.stream().filter(def -> def.getTimeout() == timeout).findFirst();
             assertFalse(definition.isPresent());
-        } catch (InterruptedException | ExecutionException | IOException e) {
+        } catch (InterruptedException | ExecutionException e) {
             fail();
         }
-    }
 
-    private void setUpProfileMapper(InputStream configStream) throws FileNotFoundException {
-        snmpPeerFactory = new ProxySnmpAgentConfigFactory(configStream);
-        FilterDao filterDao = Mockito.mock(FilterDao.class);
-        when(filterDao.isValid(Mockito.anyString(), Mockito.contains("IPLIKE"))).thenReturn(true);
-        profileMapper = new SnmpProfileMapperImpl(filterDao, snmpPeerFactory, locationAwareSnmpClient);
     }
 
 
