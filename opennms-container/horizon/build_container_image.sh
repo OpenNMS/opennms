1a70ea0425cff12d464485e488435f8650adf648
Ronny Trommer <ronny@opennms.org>
2019-10-21 12:36:19 +0200

    NMS-12354: Set a fixed dependency for OpenJDK 11 to a fixed build artifact

--- opennms-container/horizon/build_container_image.sh
+++ opennms-container/horizon/build_container_image.sh
@@ -22,12 +22,18 @@ done
 docker build -t horizon \
   --build-arg BUILD_DATE="$(date -u +\"%Y-%m-%dT%H:%M:%S%z\")" \
   --build-arg BASE_IMAGE="opennms/openjdk" \
-  --build-arg BASE_IMAGE_VERSION="11.0.4.11" \
+  --build-arg BASE_IMAGE_VERSION="11.0.4.11-b2418" \
   --build-arg VERSION="${VERSION}" \
+  --build-arg SOURCE="${CIRCLE_REPOSITORY_URL}" \
+  --build-arg REVISION="$(git describe --always)" \
+  --build-arg BUILD_JOB_ID="${CIRCLE_WORKFLOW_JOB_ID}" \
   --build-arg BUILD_NUMBER="${CIRCLE_BUILD_NUM}" \
   --build-arg BUILD_URL="${CIRCLE_BUILD_URL}" \
   --build-arg BUILD_BRANCH="${CIRCLE_BRANCH}" \
-  --build-arg BUILD_SHA1="${CIRCLE_SHA1}" \
   .
+
+if [ -n "${CIRCLE_BUILD_NUM}" ]; then
+  IMAGE_VERSION+=("${BASE_IMAGE_VERSION}-b${CIRCLE_BUILD_NUM}")
+fi
 
 docker image save horizon -o images/container.oci
