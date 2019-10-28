c47220c345c0ba9e1cc25b9b0807072a09abee2a
Jesse White <jesse@opennms.org>
2019-10-28 14:51:49 -0400

    NMS-12356: Move Karaf health monitoring to new module * Also migrate bundle-refresher to this new module

--- features/karaf-health/src/main/java/org/opennms/features/karaf/health/bundle/Activator.java
+++ features/karaf-health/src/main/java/org/opennms/features/karaf/health/bundle/Activator.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2017-2017 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
+ * Copyright (C) 2019 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2019 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -26,7 +26,8 @@
  *     http://www.opennms.com/
  *******************************************************************************/
 
-package org.opennms.features.vaadin.components.bundlerefresher;
+package org.opennms.features.karaf.health.bundle;
+
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,7 +39,6 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.wiring.FrameworkWiring;
-
 
 public class Activator implements BundleActivator {
 
