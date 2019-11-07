c25dad9ca1fec7cdad0460430ace52f313fb4df8
Benjamin Reed <ranger@opennms.org>
2019-11-07 11:28:00 -0500

    ITREFIT-720: print to output whether signing is happening

--- .circleci/scripts/makerpm.sh
+++ .circleci/scripts/makerpm.sh
@@ -12,8 +12,10 @@ fi
 SPECFILE="$1"
 
 if [ -n "$GPG_SECRET_KEY" ] && [ -n "$GPG_PASSPHRASE" ]; then
+	echo "PGP key found... signing RPMs"
 	. "$MYDIR/configure-signing.sh"
 	./makerpm.sh -a -d -s '***REDACTED***' -S "$SPECFILE"
 else
+	echo "PGP key not found... skipping RPM signing"
 	./makerpm.sh -a -d -S "$SPECFILE"
 fi
