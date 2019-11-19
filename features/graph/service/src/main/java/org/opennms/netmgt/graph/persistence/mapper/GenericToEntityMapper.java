810c8debf0a6f8744df25ee75cac303219443862
Markus von Rüden <mvr@opennms.com>
2019-11-19 14:39:31 +0100

    Remove System.out.println(...)

--- features/graph/service/src/main/java/org/opennms/netmgt/graph/persistence/mapper/GenericToEntityMapper.java
+++ features/graph/service/src/main/java/org/opennms/netmgt/graph/persistence/mapper/GenericToEntityMapper.java
@@ -48,7 +48,6 @@ import org.opennms.netmgt.graph.api.generic.GenericGraphContainer;
 import org.opennms.netmgt.graph.api.generic.GenericVertex;
 import org.opennms.netmgt.graph.persistence.converter.ConverterService;
 
-// TODO MVR remove System.out.println
 public class GenericToEntityMapper {
 
     private ConverterService converterService = new ConverterService();
@@ -68,18 +67,12 @@ public class GenericToEntityMapper {
         graphEntity.setProperties(convertToPropertyEntities(genericGraph.getProperties()));
 
         // Map Vertices
-        long start = System.currentTimeMillis();
-        System.out.println("\tConverting vertices");
         final List<VertexEntity> vertexEntities = genericGraph.getVertices().stream().map(genericVertex -> toEntity(genericVertex)).collect(Collectors.toList());
         graphEntity.addRelations(vertexEntities);
-        System.out.println("\tDONE. Took " + (System.currentTimeMillis() - start) + "ms");
 
         // Map Edges
-        start = System.currentTimeMillis();
-        System.out.println("\tConverting edges");
         final List<EdgeEntity> edgeEntities = genericGraph.getEdges().stream().map(genericEdge -> toEntity(genericEdge, graphEntity)).collect(Collectors.toList());
         graphEntity.addRelations(edgeEntities);
-        System.out.println("\tDONE. Took " + (System.currentTimeMillis() - start) + "ms");
 
         // Map Focus
         final Focus focus = genericGraph.getDefaultFocus();
