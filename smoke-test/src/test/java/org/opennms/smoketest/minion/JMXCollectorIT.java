c2b44982646fa0074e103b920028f3d3a0dfc0b7
Patrick Schweizer <patrick@schweizer-ing.com>
2019-11-20 13:38:06 -0400

    NMS-10413: add missing prefixes

--- smoke-test/src/test/java/org/opennms/smoketest/minion/JMXCollectorIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/minion/JMXCollectorIT.java
@@ -65,7 +65,7 @@ public class JMXCollectorIT {
         try (final SshClient sshClient = new SshClient(sshAddr, "admin", "admin")) {
             // Perform an adhoc collection from against the Minion JVM
             final PrintStream pipe = sshClient.openShell();
-            pipe.println("collection:collect -l MINION org.opennms.netmgt.collectd.Jsr160Collector 127.0.0.1 port=18980");
+            pipe.println("opennms-collection:collect -l MINION org.opennms.netmgt.collectd.Jsr160Collector 127.0.0.1 port=18980");
             pipe.println("logout");
             await().atMost(1, MINUTES).until(sshClient.isShellClosedCallable());
 
