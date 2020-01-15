4aaac60a1566bfc0920685e3a0114c5412641006
Pierre Bouffard <pierre@opennms.com>
2020-01-15 12:03:17 -0500

    NMS-10720: Make Events immutable (avoid CMEs and fix non-deterministic behavior) [Part 2]
    -Add explanation for 'has...()' methods on interfaces that have them.

--- features/events/api/src/main/java/org/opennms/netmgt/events/api/model/IAlarmData.java
+++ features/events/api/src/main/java/org/opennms/netmgt/events/api/model/IAlarmData.java
@@ -32,6 +32,9 @@ import java.util.List;
 
 /**
  * A definition corresponding to POJO '{@link org.opennms.netmgt.xml.event.AlarmData}'.
+ *
+ * The 'has...()' methods exist since the corresponding 'get...()' methods will return a default value if null.
+ * Using the 'has...()' method is the only means to determine if the backing value is null.
  */
 public interface IAlarmData {
     Integer getAlarmType();
