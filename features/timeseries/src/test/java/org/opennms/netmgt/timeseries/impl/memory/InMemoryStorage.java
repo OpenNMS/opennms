a4cbefff4bb748f06b8016dc5376396a5552e99f
Patrick Schweizer <patrick@schweizer-ing.com>
2020-02-15 14:08:11 -0500

    NMS-12383: introduced TimeseriesStorageManager

--- features/timeseries/src/test/java/org/opennms/netmgt/timeseries/impl/memory/InMemoryStorage.java
+++ features/timeseries/src/test/java/org/opennms/netmgt/timeseries/impl/memory/InMemoryStorage.java
@@ -36,12 +36,12 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
-import org.opennms.integration.api.v1.timeseries.TimeSeriesStorage;
 import org.opennms.integration.api.v1.timeseries.Aggregation;
 import org.opennms.integration.api.v1.timeseries.Metric;
 import org.opennms.integration.api.v1.timeseries.Sample;
 import org.opennms.integration.api.v1.timeseries.Tag;
 import org.opennms.integration.api.v1.timeseries.TimeSeriesFetchRequest;
+import org.opennms.integration.api.v1.timeseries.TimeSeriesStorage;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
@@ -54,7 +54,7 @@ public class InMemoryStorage implements TimeSeriesStorage {
 
     private final ConcurrentMap<Metric, Collection<Sample>> data;
 
-    public InMemoryStorage () {
+    public InMemoryStorage() {
         Cache<Metric, Collection<Sample>> cache = CacheBuilder.newBuilder().maximumSize(10000).build();
         data = cache.asMap();
     }
