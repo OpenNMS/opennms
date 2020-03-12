1a62f9d9cc0c173c4ddadb43d778acd84ddd8998
Ronny Trommer <ronny@opennms.org>
2020-03-12 14:03:22 +0100

    NMS-12483: Migrate to use docker-buildx instead of docker build

--- opennms-container/minion/build_container_image.sh
+++ opennms-container/minion/build_container_image.sh
@@ -12,7 +12,13 @@ source ../registry-config.sh
 # shellcheck source=opennms-container/version-n-tags.sh
 source ../version-tags.sh
 
-docker build -t minion \
+DOCKERX_INSTANCE="multiarch-env-minion"
+DOCKER_FLAGS="--output=type=docker,dest=images/container.oci"
+DOCKER_ARCH="linux/amd64"
+
+docker-buildx create --name ${DOCKERX_INSTANCE}
+docker-buildx use ${DOCKERX_INSTANCE}
+docker-buildx build --platform=${DOCKER_ARCH} \
   --build-arg BUILD_DATE="$(date -u +\"%Y-%m-%dT%H:%M:%S%z\")" \
   --build-arg VERSION="${VERSION}" \
   --build-arg SOURCE="${CIRCLE_REPOSITORY_URL}" \
@@ -21,6 +27,6 @@ docker build -t minion \
   --build-arg BUILD_NUMBER="${CIRCLE_BUILD_NUM}" \
   --build-arg BUILD_URL="${CIRCLE_BUILD_URL}" \
   --build-arg BUILD_BRANCH="${CIRCLE_BRANCH}" \
+  --tag minion \
+  ${DOCKER_FLAGS} \
   .
-
-docker image save minion -o images/container.oci
