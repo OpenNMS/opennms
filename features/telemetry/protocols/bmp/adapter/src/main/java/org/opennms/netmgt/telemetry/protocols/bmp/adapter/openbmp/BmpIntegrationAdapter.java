6f208c4ac8321a9baae2dbc08b83f7f12e7feab7
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2020-03-18 09:34:37 +0100

    NMS-12571: Parse BGP capabilities

--- features/telemetry/protocols/bmp/adapter/src/main/java/org/opennms/netmgt/telemetry/protocols/bmp/adapter/openbmp/BmpIntegrationAdapter.java
+++ features/telemetry/protocols/bmp/adapter/src/main/java/org/opennms/netmgt/telemetry/protocols/bmp/adapter/openbmp/BmpIntegrationAdapter.java
@@ -28,6 +28,10 @@
 
 package org.opennms.netmgt.telemetry.protocols.bmp.adapter.openbmp;
 
+import static org.opennms.netmgt.telemetry.listeners.utils.BufferUtils.repeatRemaining;
+import static org.opennms.netmgt.telemetry.listeners.utils.BufferUtils.skip;
+import static org.opennms.netmgt.telemetry.listeners.utils.BufferUtils.uint16;
+import static org.opennms.netmgt.telemetry.listeners.utils.BufferUtils.uint8;
 import static org.opennms.netmgt.telemetry.protocols.bmp.adapter.BmpAdapterTools.address;
 import static org.opennms.netmgt.telemetry.protocols.bmp.adapter.BmpAdapterTools.addressAsStr;
 import static org.opennms.netmgt.telemetry.protocols.bmp.adapter.BmpAdapterTools.asAttr;
@@ -50,8 +54,10 @@ import org.opennms.netmgt.telemetry.api.adapter.TelemetryMessageLog;
 import org.opennms.netmgt.telemetry.api.adapter.TelemetryMessageLogEntry;
 import org.opennms.netmgt.telemetry.config.api.AdapterDefinition;
 import org.opennms.netmgt.telemetry.protocols.bmp.adapter.BmpAdapterTools;
+import org.opennms.netmgt.telemetry.protocols.bmp.adapter.openbmp.proto.AddressFamilyIdentifier;
 import org.opennms.netmgt.telemetry.protocols.bmp.adapter.openbmp.proto.Message;
 import org.opennms.netmgt.telemetry.protocols.bmp.adapter.openbmp.proto.Record;
+import org.opennms.netmgt.telemetry.protocols.bmp.adapter.openbmp.proto.SubsequentAddressFamilyIdentifier;
 import org.opennms.netmgt.telemetry.protocols.bmp.adapter.openbmp.proto.Type;
 import org.opennms.netmgt.telemetry.protocols.bmp.adapter.openbmp.proto.records.BaseAttribute;
 import org.opennms.netmgt.telemetry.protocols.bmp.adapter.openbmp.proto.records.Collector;
@@ -69,7 +75,11 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 
 public class BmpIntegrationAdapter extends AbstractAdapter {
 
@@ -301,8 +311,8 @@ public class BmpIntegrationAdapter extends AbstractAdapter {
         peer.localPort = peerUp.getLocalPort();
         peer.localBgpId = address(peerUp.getSendMsg().getId());
         peer.infoData = peerUp.getMessage();
-        peer.advertisedCapabilities = ""; // TODO: Not parsed right now (see https://issues.opennms.org/browse/NMS-12571)
-        peer.receivedCapabilities = ""; // TODO: Not parsed right now (see https://issues.opennms.org/browse/NMS-12571)
+        peer.advertisedCapabilities = peerUp.getSendMsg().getCapabilities().getCapabilityList().stream().map(c -> generateCapabilityMessage(c.getCode(), c.getValue())).collect(Collectors.joining(","));
+        peer.receivedCapabilities = peerUp.getRecvMsg().getCapabilities().getCapabilityList().stream().map(c -> generateCapabilityMessage(c.getCode(), c.getValue())).collect(Collectors.joining(","));
         peer.remoteHolddown = uint32(peerUp.getRecvMsg().getHoldTime());
         peer.advertisedHolddown = uint32(peerUp.getSendMsg().getHoldTime());
         peer.bmpReason = null;
@@ -713,6 +723,96 @@ public class BmpIntegrationAdapter extends AbstractAdapter {
 
         public String getRouterHash() {
             return Record.hash(sourceAddress.getHostAddress(), Integer.toString(sourcePort), collectorHashId);
+        }
+    }
+
+    private String generateCapabilityMessage(final int code, final ByteString byteString) {
+        final ByteBuf value = Unpooled.wrappedBuffer(byteString.toByteArray());
+        if (code == 72 || (code >= 10 && code <= 63) || (code >= 74 && code <= 127)) {
+            return String.format("Unassigned (%d)", code);
+        }
+
+        if (code >= 128 && code <= 255) {
+            return String.format("Reserved for Private Use", code);
+        }
+        switch (code) {
+            case 0:
+                return "Reserved";
+            case 1: {
+                int afi = uint16(value);
+                skip(value, 1);
+                int safi = uint8(value);
+
+                return String.format("Multiprotocol Extensions for BGP-4 (1): afi=%d safi=%d: %s %s",
+                        code,
+                        afi,
+                        safi,
+                        AddressFamilyIdentifier.from(afi).getDescription(),
+                        SubsequentAddressFamilyIdentifier.from(safi).getDescription());
+            }
+            case 2:
+                return "Route Refresh Capability for BGP-4 (2)";
+            case 3:
+                return "Outbound Route Filtering Capability (3)";
+            case 4:
+                return "Multiple routes to a destination capability (deprecated) (4)";
+            case 5:
+                return "Extended Next Hop Encoding (5)";
+            case 6:
+                return "BGP Extended Message (6)";
+            case 7:
+                return "BGPsec Capability (7)";
+            case 8:
+                return "BGP Role (TEMPORARY - registered 2018-03-29, extension registered 2019-03-18, expires 2020-03-29) (8)";
+            case 9:
+                return "Multiple Labels Capability (9)";
+            case 64:
+                return "Graceful Restart Capability (64)";
+            case 65:
+                return "Support for 4-octet AS number capability (65)";
+            case 66:
+                return "Deprecated (2003-03-06) (66)";
+            case 67:
+                return "Support for Dynamic Capability (capability specific) (67)";
+            case 68:
+                return "Multisession BGP Capability (68)";
+            case 69: {
+                final List<String> strings = repeatRemaining(value, b -> {
+                    int afi = uint16(b);
+                    int safi = uint8(b);
+                    int sendReceive = uint8(b);
+
+                    return String.format("afi=%d safi=%d send/receive=%d: %s %s %s",
+                            afi,
+                            safi,
+                            sendReceive,
+                            AddressFamilyIdentifier.from(afi),
+                            SubsequentAddressFamilyIdentifier.from(safi),
+                            parseSendReceive(sendReceive));
+                });
+
+                return String.format("ADD-PATH Capability (69): %s", String.join(", ", strings));
+            }
+            case 70:
+                return "Enhanced Route Refresh Capability (70)";
+            case 71:
+                return "Long-Lived Graceful Restart (71)";
+            case 73:
+                return "FQDN Capability (73)";
+        }
+        return String.format("Unknown capability (%d)", code);
+    }
+
+    private String parseSendReceive(final int sendReceive) {
+        switch (sendReceive) {
+            case 1:
+                return "Receive";
+            case 2:
+                return "Send";
+            case 3:
+                return "Send/Receive";
+            default:
+                return "unknown";
         }
     }
 }
