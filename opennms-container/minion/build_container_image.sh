2a6cf3597e4b8257275593f04a757690aed581d6
Benjamin Reed <ranger@opennms.org>
2020-03-20 10:51:16 -0400

    put build script back for bamboo

---
+++ opennms-container/minion/build_container_image.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Exit script if a statement returns a non-true return value.
+set -o errexit
+
+# Use the error status of the first failure, rather than that of the last item in a pipeline.
+set -o pipefail
+
+# shellcheck source=registry-config.sh
+source ../registry-config.sh
+
+# shellcheck source=opennms-container/version-n-tags.sh
+source ../version-tags.sh
+
+# OpenNMS Minion packages
+MINION_PACKAGES="opennms-minion"
+
+for PKG in ${MINION_PACKAGES}; do 
+  cp ../../target/rpm/RPMS/noarch/"${PKG}"*.rpm rpms
+done
+
+docker build -t minion \
+  --build-arg BUILD_DATE="$(date -u +\"%Y-%m-%dT%H:%M:%S%z\")" \
+  --build-arg VERSION="${VERSION}" \
+  --build-arg SOURCE="${CIRCLE_REPOSITORY_URL}" \
+  --build-arg REVISION="$(git describe --always)" \
+  --build-arg BUILD_JOB_ID="${CIRCLE_WORKFLOW_JOB_ID}" \
+  --build-arg BUILD_NUMBER="${CIRCLE_BUILD_NUM}" \
+  --build-arg BUILD_URL="${CIRCLE_BUILD_URL}" \
+  --build-arg BUILD_BRANCH="${CIRCLE_BRANCH}" \
+  .
+
+docker image save minion -o images/container.oci
