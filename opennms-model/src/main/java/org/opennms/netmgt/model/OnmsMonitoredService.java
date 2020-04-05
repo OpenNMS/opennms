3591dc7bb1ec6d1521eab92a28eafe6f5d40e141
Patrick Schweizer <patrick@schweizer-ing.com>
2020-04-05 12:18:07 -0500

    NMS-12625: Cannot create monitored-service with JSON via ReST: cleanup

--- opennms-model/src/main/java/org/opennms/netmgt/model/OnmsMonitoredService.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/OnmsMonitoredService.java
@@ -569,7 +569,6 @@ public class OnmsMonitoredService extends OnmsEntity implements Serializable, Co
     )
     @XmlElementWrapper(name="applications")
     @XmlElement(name="application")
-    // @JsonManagedReference
     public Set<OnmsApplication> getApplications() {
         return m_applications;
     }
