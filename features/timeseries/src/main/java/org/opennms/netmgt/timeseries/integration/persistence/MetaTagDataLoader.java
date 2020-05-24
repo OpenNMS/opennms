a5449bfbe4b996de47c6cc5ebb064ad00aeb9775
Patrick Schweizer <patrick@schweizer-ing.com>
2020-05-24 14:24:34 -0500

    NMS-12730 Meta-data tag enhancements to Time Series Storage API: map more attributes

--- features/timeseries/src/main/java/org/opennms/netmgt/timeseries/integration/persistence/MetaTagDataLoader.java
+++ features/timeseries/src/main/java/org/opennms/netmgt/timeseries/integration/persistence/MetaTagDataLoader.java
@@ -30,12 +30,12 @@ package org.opennms.netmgt.timeseries.integration.persistence;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Supplier;
 
 import javax.inject.Inject;
 
@@ -79,17 +79,18 @@ public class MetaTagDataLoader extends CacheLoader<CollectionResource, Map<Strin
     public Map<String, String> load(final CollectionResource resource) {
         return sessionUtils.withReadOnlyTransaction(() -> {
             final Map<String, String> tags = new HashMap<>();
-            addTag(tags, MetaTagKey.resourceLabel, () -> Optional.ofNullable(resource.getInterfaceLabel()).orElse(null));
+            addTag(tags, MetaTagKey.resourceLabel, Optional.ofNullable(resource.getInterfaceLabel()).orElse(null));
 
             if (resource.getResourceTypeName().equals(CollectionResource.RESOURCE_TYPE_NODE)) {
                 String nodeCriteria = getNodeCriteriaFromResource(resource);
                 mapNode(tags, nodeCriteria);
             } else if (resource.getResourceTypeName().equals(CollectionResource.RESOURCE_TYPE_IF)) {
-
                 String nodeCriteria = getNodeCriteriaFromResource(resource);
                 if (!Strings.isNullOrEmpty(nodeCriteria)) {
                     mapNode(tags, nodeCriteria);
                 }
+                addTag(tags, MetaTagKey.ifDescr, resource.getInterfaceLabel()); // TODO: Patrick: is that correct?
+                // addTag(tags, MetaTagKey.ifAlias, ); // TODO: Patrick: from where do we get that attribute?
             } else if (resource.getResourceTypeName().equals(CollectionResource.RESOURCE_TYPE_LATENCY)) {
                 mapResponseTimeResource(resource, tags);
             } else {
@@ -105,13 +106,12 @@ public class MetaTagDataLoader extends CacheLoader<CollectionResource, Map<Strin
 
     private void mapNode(final Map<String, String> tags, final String nodeCriteria) {
         Optional<OnmsNode> node = getNode(nodeCriteria);
-        addTag(tags, MetaTagKey.nodeCriteria, () -> nodeCriteria);
-        addTag(tags, MetaTagKey.nodeLabel, () -> node.map(OnmsNode::getLabel).orElse(null));
-        addTag(tags, MetaTagKey.location, () -> node.map(OnmsNode::getLocation).map(OnmsMonitoringLocation::getLocationName).orElse(null));
-        addTag(tags, MetaTagKey.sysObjectID, () -> node.map(OnmsNode::getSysObjectId).orElse(null));
-        addTag(tags, MetaTagKey.foreignSource, () -> node.map(OnmsNode::getForeignSource).orElse(null));
-        addTag(tags, MetaTagKey.foreignId, () -> node.map(OnmsNode::getForeignId).orElse(null));
-        // TODO Patrick: map rest of attributes
+        addTag(tags, MetaTagKey.nodeLabel, node.map(OnmsNode::getLabel).orElse(null));
+        addTag(tags, MetaTagKey.location, node.map(OnmsNode::getLocation).map(OnmsMonitoringLocation::getLocationName).orElse(null));
+        addTag(tags, MetaTagKey.sysObjectID, node.map(OnmsNode::getSysObjectId).orElse(null));
+        addTag(tags, MetaTagKey.foreignSource, node.map(OnmsNode::getForeignSource).orElse(null));
+        addTag(tags, MetaTagKey.foreignId, node.map(OnmsNode::getForeignId).orElse(null));
+        addTag(tags, MetaTagKey.nodeCriteria, nodeCriteria);
 
         // categories
         if(node.isPresent()) {
@@ -128,26 +128,89 @@ public class MetaTagDataLoader extends CacheLoader<CollectionResource, Map<Strin
         if(node.map(OnmsNode::getAssetRecord).isPresent()) {
             mapAssets(tags, node.get().getAssetRecord());
         }
+
+        // Meta-Data DSL
+        // TODO Patrick: how will that work?
     }
 
     private void mapAssets(final Map<String, String> tags, final OnmsAssetRecord assets) {
         Objects.requireNonNull(assets);
-        addTag(tags, AssetTagKey.additionalHardware, assets::getAdditionalhardware);
-        addTag(tags, AssetTagKey.admin, assets::getAdmin);
-        addTag(tags, AssetTagKey.assetNumber, assets::getAssetNumber);
-        // TODO: Patrick add rest of assets
+
+        addTag(tags, AssetTagKey.id, Optional.ofNullable(assets.getId()).map(Object::toString).orElse(null));
+        addTag(tags, AssetTagKey.category, assets.getCategory());
+        addTag(tags, AssetTagKey.manufacturer, assets.getManufacturer());
+        addTag(tags, AssetTagKey.vendor, assets.getVendor());
+        addTag(tags, AssetTagKey.modelNumber, assets.getModelNumber());
+        addTag(tags, AssetTagKey.serialNumber, assets.getSerialNumber());
+        addTag(tags, AssetTagKey.description, assets.getDescription());
+        addTag(tags, AssetTagKey.circuitId, assets.getCircuitId());
+        addTag(tags, AssetTagKey.assetNumber, assets.getAssetNumber());
+        addTag(tags, AssetTagKey.operatingSystem, assets.getOperatingSystem());
+        addTag(tags, AssetTagKey.rack, assets.getRack());
+        addTag(tags, AssetTagKey.slot, assets.getSlot());
+        addTag(tags, AssetTagKey.port, assets.getPort());
+        addTag(tags, AssetTagKey.region, assets.getRegion());
+        addTag(tags, AssetTagKey.division, assets.getDivision());
+        addTag(tags, AssetTagKey.department, assets.getDepartment());
+        addTag(tags, AssetTagKey.building, assets.getBuilding());
+        addTag(tags, AssetTagKey.floor, assets.getFloor());
+        addTag(tags, AssetTagKey.room, assets.getRoom());
+        addTag(tags, AssetTagKey.vendorPhone, assets.getVendorPhone());
+        addTag(tags, AssetTagKey.vendorFax, assets.getVendorFax());
+        addTag(tags, AssetTagKey.vendorAssetNumber, assets.getVendorAssetNumber());
+        addTag(tags, AssetTagKey.username, assets.getUsername());
+        addTag(tags, AssetTagKey.password, assets.getPassword());
+        addTag(tags, AssetTagKey.enable, assets.getEnable());
+        addTag(tags, AssetTagKey.connection, assets.getConnection());
+        addTag(tags, AssetTagKey.autoenable, assets.getAutoenable());
+        addTag(tags, AssetTagKey.lastModifiedBy, assets.getLastModifiedBy());
+        addTag(tags, AssetTagKey.lastModifiedDate, Optional.ofNullable(assets.getLastModifiedDate()).map(Date::toString).orElse(null));
+        addTag(tags, AssetTagKey.dateInstalled, assets.getDateInstalled());
+        addTag(tags, AssetTagKey.lease, assets.getLease());
+        addTag(tags, AssetTagKey.leaseExpires, assets.getLeaseExpires());
+        addTag(tags, AssetTagKey.supportPhone, assets.getSupportPhone());
+        addTag(tags, AssetTagKey.maintcontract, assets.getMaintcontract());
+        addTag(tags, AssetTagKey.maintContractExpiration, assets.getMaintContractExpiration());
+        addTag(tags, AssetTagKey.displayCategory, assets.getDisplayCategory());
+        addTag(tags, AssetTagKey.notifyCategory, assets.getNotifyCategory());
+        addTag(tags, AssetTagKey.pollerCategory, assets.getPollerCategory());
+        addTag(tags, AssetTagKey.thresholdCategory, assets.getThresholdCategory());
+        addTag(tags, AssetTagKey.comment, assets.getComment());
+        addTag(tags, AssetTagKey.cpu, assets.getCpu());
+        addTag(tags, AssetTagKey.ram, assets.getRam());
+        addTag(tags, AssetTagKey.storagectrl, assets.getStoragectrl());
+        addTag(tags, AssetTagKey.hdd1, assets.getHdd1());
+        addTag(tags, AssetTagKey.hdd2, assets.getHdd2());
+        addTag(tags, AssetTagKey.hdd3, assets.getHdd3());
+        addTag(tags, AssetTagKey.hdd4, assets.getHdd4());
+        addTag(tags, AssetTagKey.hdd5, assets.getHdd5());
+        addTag(tags, AssetTagKey.hdd6, assets.getHdd6());
+        addTag(tags, AssetTagKey.numpowersupplies, assets.getNumpowersupplies());
+        addTag(tags, AssetTagKey.inputpower, assets.getInputpower());
+        addTag(tags, AssetTagKey.additionalhardware, assets.getAdditionalhardware());
+        addTag(tags, AssetTagKey.admin, assets.getAdmin());
+        addTag(tags, AssetTagKey.snmpcommunity, assets.getSnmpcommunity());
+        addTag(tags, AssetTagKey.rackunitheight, assets.getRackunitheight());
+        addTag(tags, AssetTagKey.managedObjectType, assets.getManagedObjectType());
+        addTag(tags, AssetTagKey.managedObjectInstance, assets.getManagedObjectInstance());
+        addTag(tags, AssetTagKey.geolocation, Optional.ofNullable(assets.getGeolocation()).map(Object::toString).orElse(null));
+        addTag(tags, AssetTagKey.vmwareManagedObjectId, assets.getVmwareManagedObjectId());
+        addTag(tags, AssetTagKey.vmwareManagedEntityType, assets.getVmwareManagedEntityType());
+        addTag(tags, AssetTagKey.vmwareManagementServer, assets.getVmwareManagementServer());
+        addTag(tags, AssetTagKey.vmwareTopologyInfo, assets.getVmwareTopologyInfo());
+        addTag(tags, AssetTagKey.vmwareState, assets.getVmwareState());
     }
 
-    private void addTag(final Map<String, String> tags, final MetaTagKey key, final Supplier<String> valueSupplier){
-        Optional.ofNullable(valueSupplier.get())
-                .filter((value) -> this.config.isEnabled(key))
-                .ifPresent((value) -> tags.put(key.name(), value));
+    private void addTag(final Map<String, String> tags, final MetaTagKey key, final String value) {
+        Optional.ofNullable(value)
+                .filter((v) -> this.config.isEnabled(key))
+                .ifPresent((v) -> tags.put(key.name(), v));
     }
 
-    private void addTag(final Map<String, String> tags, final AssetTagKey key, final Supplier<String> valueSupplier){
-        Optional.ofNullable(valueSupplier.get())
-                .filter((value) -> this.config.isEnabled(key))
-                .ifPresent((value) -> tags.put(key.name(), value));
+    private void addTag(final Map<String, String> tags, final AssetTagKey key, final String value){
+        Optional.ofNullable(value)
+                .filter((v) -> this.config.isEnabled(key))
+                .ifPresent((v) -> tags.put(key.name(), v));
     }
 
     private Optional<OnmsNode> getNode(String nodeCriteria) {
@@ -186,7 +249,7 @@ public class MetaTagDataLoader extends CacheLoader<CollectionResource, Map<Strin
         }
     }
 
-    private void mapResponseTimeResource(CollectionResource resource, Map<String, String> tags) {
+    private void mapResponseTimeResource(final CollectionResource resource, final Map<String, String> tags) {
         boolean validIp = false;
         // Check if resource parent is an IpAddress.
         if (resource.getParent() != null && resource.getParent().elements().length == 1) {
@@ -205,6 +268,9 @@ public class MetaTagDataLoader extends CacheLoader<CollectionResource, Map<Strin
                 tags.put(MetaTagKey.ipAddress.name(), resourcePathArray[0]);
             }
         }
+
+        // TODO: Patrick: from where do we get the service?
+        // tags.put(MetaTagKey.service.name(), );
     }
 
     private boolean checkForValidIpAddress(String resourcePath) {
