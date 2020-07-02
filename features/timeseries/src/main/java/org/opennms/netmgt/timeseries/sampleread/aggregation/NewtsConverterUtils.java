519ac9294d5bafbc9c548c67c2df695dbb4d0598
Patrick Schweizer <patrick@schweizer-ing.com>
2020-07-02 11:56:49 -0500

    NMS-12731 Optimize Performance of Timeseries Integration Layer: address PR comments

--- features/timeseries/src/main/java/org/opennms/netmgt/timeseries/sampleread/aggregation/NewtsConverterUtils.java
+++ features/timeseries/src/main/java/org/opennms/netmgt/timeseries/sampleread/aggregation/NewtsConverterUtils.java
@@ -28,20 +28,19 @@
 
 package org.opennms.netmgt.timeseries.sampleread.aggregation;
 
-import static org.opennms.netmgt.timeseries.util.Utils.asMap;
-
 import java.time.Instant;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.opennms.integration.api.v1.timeseries.IntrinsicTagNames;
 import org.opennms.integration.api.v1.timeseries.Metric;
 import org.opennms.integration.api.v1.timeseries.Sample;
+import org.opennms.integration.api.v1.timeseries.Tag;
 import org.opennms.integration.api.v1.timeseries.immutables.ImmutableSample;
-import org.opennms.integration.api.v1.timeseries.IntrinsicTagNames;
-import org.opennms.netmgt.measurements.model.Source;
 import org.opennms.newts.api.Context;
 import org.opennms.newts.api.Counter;
 import org.opennms.newts.api.Gauge;
@@ -57,7 +56,7 @@ import com.google.common.base.Optional;
 /** Provides methods to convert between the Timeseries API world and Newts. */
 public class NewtsConverterUtils {
 
-    public static Iterator<Results.Row<org.opennms.newts.api.Sample>> samplesToNewtsRowIterator(List<Sample> allSamples, final List<Source> listOfSources) {
+    public static Iterator<Results.Row<org.opennms.newts.api.Sample>> samplesToNewtsRowIterator(List<Sample> allSamples) {
         return allSamples
                 .stream()
                 .map(NewtsConverterUtils::sampleToRow)
@@ -70,9 +69,9 @@ public class NewtsConverterUtils {
         Optional<Map<String, String>> resourceAttributes = sample.getMetric().getMetaTags().isEmpty() ?
                 Optional.absent() : Optional.of(asMap(sample.getMetric().getMetaTags()));
 
-        final Timestamp timestamp = Timestamp.fromEpochMillis(sample.getTime().toEpochMilli());;
+        final Timestamp timestamp = Timestamp.fromEpochMillis(sample.getTime().toEpochMilli());
         final Context context = new Context("not relevant");
-        final Resource resource = new Resource(sample.getMetric().getFirstTagByKey(IntrinsicTagNames.resourceId).getValue(), resourceAttributes);;
+        final Resource resource = new Resource(sample.getMetric().getFirstTagByKey(IntrinsicTagNames.resourceId).getValue(), resourceAttributes);
         final String name = sample.getMetric().getFirstTagByKey(IntrinsicTagNames.name).getValue();
         final MetricType type = toNewts(Metric.Mtype.valueOf(sample.getMetric().getFirstTagByKey(IntrinsicTagNames.mtype).getValue()));
         final ValueType<?> value = toNewtsValue(sample);
@@ -127,5 +126,13 @@ public class NewtsConverterUtils {
 
     public static Instant toInstant(final Timestamp timestamp) {
         return Instant.ofEpochMilli(timestamp.asMillis());
+    }
+
+    public static Map<String, String> asMap (Collection<Tag> tags) {
+        Map<String, String> map = new HashMap<>();
+        for (Tag tag : tags) {
+            map.put(tag.getKey(), tag.getValue());
+        }
+        return map;
     }
 }
