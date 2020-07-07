2944f7f0887c87cb28800c0801980d8acb713c30
Benjamin Reed <ranger@opennms.org>
2020-07-07 11:42:23 -0400

    OpenNMS Meridian 2018.1.20

--- opennms-webapp/gulp.config.js
+++ opennms-webapp/gulp.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Destinations
   LIB_DEST: '/lib',
-  OPENNMS_HOME: process.env.OPENNMS_HOME || '../target/opennms-2018.1.20-SNAPSHOT',
+  OPENNMS_HOME: process.env.OPENNMS_HOME || '../target/opennms-2018.1.20',
   JETTY_WEBAPP: '/jetty-webapps/opennms',
 
   // Sources.
