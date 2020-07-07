0d7784cbe608487dab2156187c2b65a1ceb313a4
Benjamin Reed <ranger@opennms.org>
2020-07-07 09:48:19 -0400

    OpenNMS Meridian 2016.1.24

--- opennms-webapp/gulpfile.js
+++ opennms-webapp/gulpfile.js
@@ -8,7 +8,7 @@ var paths = {
 	'jsp': 'src/main/webapp/**/*.jsp'
 };
 
-var opennmsHome = process.env.OPENNMS_HOME || '../target/opennms-2016.1.24-SNAPSHOT';
+var opennmsHome = process.env.OPENNMS_HOME || '../target/opennms-2016.1.24';
 gutil.log('gulp', 'OpenNMS Home: ' + opennmsHome);
 
 gulp.task('default', ['sass', 'jsp']);
