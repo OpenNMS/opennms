c47efb46a39f603255f5b7654eed33372558d403
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2020-07-09 11:10:10 +0200

    NMS-12792: Fixed OutageRestServiceIT

--- opennms-model/src/main/java/org/opennms/netmgt/model/OnmsOutage.java
+++ opennms-model/src/main/java/org/opennms/netmgt/model/OnmsOutage.java
@@ -481,7 +481,7 @@ public class OnmsOutage implements Serializable {
     @JsonSerialize(using=MonitoringLocationJsonSerializer.class)
     @JsonDeserialize(using=MonitoringLocationJsonDeserializer.class)
     @XmlElement(name="perspective")
-    @ManyToOne(optional=false, fetch=FetchType.LAZY)
+    @ManyToOne(optional=false, fetch=FetchType.EAGER)
     @JoinColumn(name="perspective")
     @XmlJavaTypeAdapter(MonitoringLocationIdAdapter.class)
     public OnmsMonitoringLocation getPerspective() {
