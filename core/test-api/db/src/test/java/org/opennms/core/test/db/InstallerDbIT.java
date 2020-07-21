72cf00317394cec81081abef88206ca5b8317f17
Naicisum <Naicisum@users.noreply.github.com>
2020-07-21 18:42:59 -0500

    Revert "NMS-12704: Update tests to handle PostgreSQL 12"
    This reverts commit 9f5858261ef8b777c7670d4694fbe2475c1870cd.

--- core/test-api/db/src/test/java/org/opennms/core/test/db/InstallerDbIT.java
+++ core/test-api/db/src/test/java/org/opennms/core/test/db/InstallerDbIT.java
@@ -1654,7 +1654,7 @@ public class InstallerDbIT extends InstallerDbITCase {
     @Test
     public void testUpgradeAddCheckConstraint() throws Exception {
        	final String cname="setfilter_type_valid";
-        final String checkexpression="(((type >= 0) AND (type <= 2)))";
+    	final String checkexpression="(((type >= 0) AND (type <= 2)))";
         final String sql_start = "create table setFilter ( id integer, type integer);\n";
         executeSQL(sql_start);
         
@@ -1669,16 +1669,9 @@ public class InstallerDbIT extends InstallerDbITCase {
     	assertEquals(1, constraints.size());
     	Constraint constraint=constraints.get(0);
     	assertEquals(cname, constraint.getName());
-        switch (getInstallerDb().getDbMajorVersion()) {
-            case 12:
-                final String checkexpression12="(CHECK (((type >= 0) AND (type <= 2))))";
-                assertEquals(checkexpression12, "(" + constraint.getCheckExpression() + ")");
-                break;
-            default:
-                // postgresql8.2 has quotes in the resulting expression
-                // postgresql8.3 has none... remove the quotes (if there are any) before comparing
-                assertEquals(checkexpression.replaceAll("\"type\"", "type"), "(" + constraint.getCheckExpression().replaceAll("\"type\"", "type") + ")");
-        }
+        // postgresql8.2 has quotes in the resulting expression
+        // postgresql8.3 has none... remove the quotes (if there are any) before comparing
+        assertEquals(checkexpression.replaceAll("\"type\"", "type"), "("+constraint.getCheckExpression().replaceAll("\"type\"", "type")+")");
     }
 
     public void addTableFromSQL(String tableName) throws SQLException {
