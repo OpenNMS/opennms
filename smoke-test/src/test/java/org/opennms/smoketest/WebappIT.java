bd2daf3163768818614c1fc29757088304db377d
Benjamin Reed <ranger@opennms.org>
2020-08-04 10:49:38 -0400

    OpenNMS Meridian 2019.1.10

--- smoke-test/src/test/java/org/opennms/smoketest/WebappIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/WebappIT.java
@@ -83,7 +83,7 @@ public class WebappIT {
         .extract().response().body().print();
 
     // The expected payload looks like:
-    //  {"packageDescription":"OpenNMS","displayVersion":"2019.1.10-SNAPSHOT","packageName":"opennms","version":"2019.1.10", "ticketerConfig":{"enabled":false, "plugin": null}}
+    //  {"packageDescription":"OpenNMS","displayVersion":"2019.1.10","packageName":"opennms","version":"2019.1.10", "ticketerConfig":{"enabled":false, "plugin": null}}
     final ObjectMapper mapper = new ObjectMapper();
     final JsonNode infoObject = mapper.readTree(json);
 
