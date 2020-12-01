fb67b07dfd766d7d5dc1e4698ace35007ea0c55b
Benjamin Reed <ranger@opennms.org>
2020-12-01 12:13:02 -0500

    OpenNMS Horizon 27.0.1

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
@@ -59,7 +59,7 @@ public class OnmsKarafTestCase extends KarafTestCase {
 				.groupId("org.opennms.container")
 				.artifactId("org.opennms.container.karaf")
 				.type("tar.gz")
-				.version("27.0.1-SNAPSHOT");
+				.version("27.0.1");
 	}
 
 	/**
