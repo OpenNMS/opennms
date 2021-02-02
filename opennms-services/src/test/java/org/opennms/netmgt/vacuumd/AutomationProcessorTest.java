96478a8ea5df397a97b9e520f328fca71e544911
Jane Hou <jhou@opennms.com>
2021-02-02 14:29:26 -0500

    NMS-13098: update license year

--- opennms-services/src/test/java/org/opennms/netmgt/vacuumd/AutomationProcessorTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/vacuumd/AutomationProcessorTest.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2007-2014 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2014 The OpenNMS Group, Inc.
+ * Copyright (C) 2003-2021 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 2021 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
