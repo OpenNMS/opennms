9ea8e4c1def86a7147bda0a5236a9f9e27f7a8fd
Benjamin Reed <ranger@opennms.org>
2021-03-04 09:50:30 -0500

    27.1.0 -> 27.1.1-SNAPSHOT

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
@@ -59,7 +59,7 @@ public class OnmsKarafTestCase extends KarafTestCase {
 				.groupId("org.opennms.container")
 				.artifactId("org.opennms.container.karaf")
 				.type("tar.gz")
-				.version("27.1.0");
+				.version("27.1.1-SNAPSHOT");
 	}
 
 	/**
