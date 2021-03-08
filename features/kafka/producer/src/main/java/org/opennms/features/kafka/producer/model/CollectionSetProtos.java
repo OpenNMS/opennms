199e09d9574abf6f43860a22ad12480529b25a12
Chandra Gorantla <chandra@opennms.com>
2021-03-08 12:54:06 -0500

    NMS-13185: Update header

--- features/kafka/producer/src/main/java/org/opennms/features/kafka/producer/model/CollectionSetProtos.java
+++ features/kafka/producer/src/main/java/org/opennms/features/kafka/producer/model/CollectionSetProtos.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2021 The OpenNMS Group, Inc.
+ * Copyright (C) 2018-2021 The OpenNMS Group, Inc.
  * OpenNMS(R) is Copyright (C) 1999-2021 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
