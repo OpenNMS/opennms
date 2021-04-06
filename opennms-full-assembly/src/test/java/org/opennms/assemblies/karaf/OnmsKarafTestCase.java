0711fca249bf3483fd612398392338908fb5c114
Benjamin Reed <ranger@opennms.org>
2021-04-06 10:40:57 -0400

    2019.1.17 -> 2019.1.18-SNAPSHOT

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
@@ -59,7 +59,7 @@ public class OnmsKarafTestCase extends KarafTestCase {
 				.groupId("org.opennms.container")
 				.artifactId("org.opennms.container.karaf")
 				.type("tar.gz")
-				.version("2019.1.17");
+				.version("2019.1.18-SNAPSHOT");
 	}
 
 	/**
