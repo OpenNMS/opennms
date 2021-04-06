399ed865e66ec3cb60674c3e7ec8403309754dfb
Benjamin Reed <ranger@opennms.org>
2021-04-06 12:27:48 -0400

    OpenNMS Horizon 27.1.1

--- smoke-test/src/test/java/org/opennms/smoketest/WebappIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/WebappIT.java
@@ -83,7 +83,7 @@ public class WebappIT {
         .extract().response().body().print();
 
     // The expected payload looks like:
-    //  {"packageDescription":"OpenNMS","displayVersion":"27.1.1-SNAPSHOT","packageName":"opennms","version":"27.1.1", "ticketerConfig":{"enabled":false, "plugin": null}}
+    //  {"packageDescription":"OpenNMS","displayVersion":"27.1.1","packageName":"opennms","version":"27.1.1", "ticketerConfig":{"enabled":false, "plugin": null}}
     final ObjectMapper mapper = new ObjectMapper();
     final JsonNode infoObject = mapper.readTree(json);
 
