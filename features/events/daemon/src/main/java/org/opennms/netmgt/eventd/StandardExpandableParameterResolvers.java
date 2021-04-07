b9683500599c89099464434f34db26459afa1851
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2021-04-07 11:18:04 +0200

    NMS-13210: Clear %dpname% variable

--- features/events/daemon/src/main/java/org/opennms/netmgt/eventd/StandardExpandableParameterResolvers.java
+++ features/events/daemon/src/main/java/org/opennms/netmgt/eventd/StandardExpandableParameterResolvers.java
@@ -93,8 +93,6 @@ public enum StandardExpandableParameterResolvers implements ExpandableParameterR
     },
 
     DPNAME {
-        final boolean clearDpName = Boolean.getBoolean("org.opennms.netmgt.eventd.cleardpname");
-
         @Override
         public boolean matches(String parm) {
             return AbstractEventUtil.TAG_DPNAME.equals(parm);
@@ -102,10 +100,7 @@ public enum StandardExpandableParameterResolvers implements ExpandableParameterR
 
         @Override
         public String getValue(String parm, String parsedParm, Event event, EventUtil eventUtil) {
-            if (clearDpName) {
-                return "";
-            }
-            return event.getDistPoller();
+            return "";
         }
     },
 
