423960e2bd783c756ba63a190fe470a8ef922c28
Benjamin Reed <ranger@opennms.org>
2021-07-21 15:47:25 -0400

    actually, make no localhost the default

--- smoke-test/src/test/java/org/opennms/smoketest/TopologyIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/TopologyIT.java
@@ -893,7 +893,7 @@ public class TopologyIT extends OpenNMSSeleniumIT {
         public void send() throws InterruptedException, IOException {
             final EventBuilder builder = new EventBuilder(EventConstants.RELOAD_TOPOLOGY_UEI, getClass().getSimpleName());
             builder.setParam(EventConstants.PARAM_TOPOLOGY_NAMESPACE, "all");
-            stack.opennms().getRestClient().sendEvent(builder.getEvent());
+            OpenNMSSeleniumIT.stack.opennms().getRestClient().sendEvent(builder.getEvent());
             Thread.sleep(5000); // Wait to allow the event to be processed
         }
     }
