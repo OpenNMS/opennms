ef967d68ac86cd8682f9123c629f12d4a3544bdb
mike <mikewrosey@gmail.com>
2021-08-11 15:00:31 -0400

    add sass loader, axios, env files, base urls

---
+++ web/src/services/axiosInstances.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const v2 = axios.create({
+  baseURL: process.env.VITE_BASE_V2_URL || '/opennms/api/v2',
+  withCredentials: true
+})
+
+const rest = axios.create({
+  baseURL: process.env.VITE_BASE_REST_URL || '/opennms/rest',
+  withCredentials: true
+})
+
+export {
+  v2,
+  rest
+}
