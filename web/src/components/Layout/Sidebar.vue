bffc01f65fab6ecf685f3be84e5f73ac2a9b9c30
mike <mikewrosey@gmail.com>
2021-08-12 11:27:50 -0400

    add to readme, separate app sidebar/menubar components

---
+++ web/src/components/Layout/Sidebar.vue
@@ -0,0 +1,25 @@
+  
+<template>
+  <div class="sidebar">
+    <Link to="/">Nodes</Link>
+  </div>
+</template>
+    
+<script setup lang="ts">
+  import Link from './Link.vue'
+</script>
+    
+<style lang="scss">
+$menuColor: #0a0c1b;
+$linkColor: #14d1df;
+.sidebar {
+  height: 100vh;
+  width: 230px;
+  background: grey;
+  position: relative;
+  float: left;
+  background: $menuColor;
+  color: rgba($color: $linkColor, $alpha: 0.8);
+}
+</style>
+  
