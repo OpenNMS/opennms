6ca306ebd109a6f8405550f332f6910b985aab61
Mike <mikewrosey@gmail.com>
2021-08-16 14:21:44 -0400

    add primeflex, base url

--- web/src/main.ts
+++ web/src/main.ts
@@ -8,6 +8,7 @@ import Button from 'primevue/button'
 import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 
 createApp({
   render: () => h(App)
