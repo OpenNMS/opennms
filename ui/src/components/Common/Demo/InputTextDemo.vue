49693e3aff1249d0458835148cdbaf4a1f97e3d6
Sagar Salunkhe <SagarS16@citiustech.com>
2021-08-19 20:50:51 +0530

    PE-28-create-input-component

---
+++ ui/src/components/Common/Demo/InputTextDemo.vue
@@ -0,0 +1,19 @@
+<template>
+            <InputText
+              cmclass="sm"
+              type="text"
+              placeholder="Enter text"
+              v-model="show"
+            />
+            <p>Input Text :: {{show}}</p>
+
+</template>
+
+<script setup lang="ts">
+
+import {ref} from 'vue'
+import InputText from '../InputText.vue'
+
+const show = ref('');
+
+</script>
