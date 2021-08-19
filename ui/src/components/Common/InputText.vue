01611ce6c96919c10bb00dfa6f4344c6adf2e936
Sagar Salunkhe <SagarS16@citiustech.com>
2021-08-19 20:50:51 +0530

    PE-28-create-input-component

---
+++ ui/src/components/Common/InputText.vue
@@ -0,0 +1,33 @@
+<template>
+    <InputText
+        :type="type"
+        :placeholder="placeholder"
+        :class="cmclass === 'sm' ? 'p-inputtext-sm' : (cmclass === 'lg' ? 'p-inputtext-lg' : 'p-inputtext')"
+    />
+</template> 
+
+<script setup lang="ts">
+
+import InputText from 'primevue/inputtext';
+import { defineProps, defineEmits } from "vue";
+
+let enteredText;
+
+const props = defineProps({
+    type: {
+        type: String,
+        required: true
+    },
+    placeholder: {
+        type: String,
+        required: false
+    },
+    cmclass: {
+        type: String,
+        required: false
+    }
+});
+
+</script>
+
+
