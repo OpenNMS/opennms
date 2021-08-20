2a5fb1a67e5730be9feaf2f23c96c0612ed75cb8
tbansal-opennms <tbansal@nanthealth.com>
2021-08-20 18:55:13 +0530

    adding fieldSet component

---
+++ ui/src/components/Common/FieldSet.vue
@@ -0,0 +1,11 @@
+<template>
+	<Fieldset>		
+		<Slot></Slot>
+	</Fieldset>
+</template>
+<script setup lang="ts">	
+	import FieldSet from 'primevue/fieldset'	
+</script>
+
+<style lang="scss">
+</style>
