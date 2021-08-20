81466b8ad84619df3e1176e0aa9fc33da9f0be41
Tripti Bansal <TriptiB@citiustech.com>
2021-08-20 13:54:37 +0530

    Filedset component using primevue lib

--- ui/src/components/Common/FieldSet.vue
+++ ui/src/components/Common/FieldSet.vue
@@ -1,16 +1,10 @@
 <template>
 	<Fieldset>		
-		{{props.content}}
+		<Slot></Slot>
 	</Fieldset>
 </template>
 <script setup lang="ts">	
-	import FieldSet from 'primevue/fieldset'
-	const props = defineProps({
-		 content: {
-			type: String,
-			required: false
-		}
-  })
+	import FieldSet from 'primevue/fieldset'	
 </script>
 
 <style lang="scss">
