7f5c31c1f05c631a62ba5f70ac5a7ece783ea118
Sagar Salunkhe <SagarS16@citiustech.com>
2021-08-25 17:43:21 +0530

    PE-31 - Time-Picker Component created

--- ui/src/components/Common/Demo/Demo.vue
+++ ui/src/components/Common/Demo/Demo.vue
@@ -8,14 +8,20 @@
     primeClass="lg"
     v-model="showInputText"
   />
-
+  <hr />
+  <p>TimePicker Demo</p>
+  <p>Selected Time :: {{ showTimePicker }}</p>
+  <TimePicker id="timepicker01" v-model="showTimePicker" />
+  <hr />
 </template>
 
 <script setup lang="ts">
 
 import { ref } from 'vue'
 import InputText from '../InputText.vue'
+import TimePicker from '../TimePicker.vue'
 
 const showInputText = ref('');
+const showTimePicker = ref('');
 
 </script>
