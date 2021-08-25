b04c50273eac2e3ac2641b8e37e0954c8a59e552
Sagar Salunkhe <SagarS16@citiustech.com>
2021-08-25 17:43:21 +0530

    PE-31 - Time-Picker Component created

---
+++ ui/src/components/Common/TimePicker.vue
@@ -0,0 +1,22 @@
+<template>
+    <Calendar :id="id" :showTime="showTime" :timeOnly="timeOnly" :hourFormat="hourFormat" />
+</template>
+
+<script setup lang="ts">
+
+import Calendar from 'primevue/calendar';
+
+interface ItimePickerProps {
+    id?: string
+    showTime?: boolean
+    timeOnly?: boolean
+    hourFormat?: string
+}
+
+const props = withDefaults(defineProps<ItimePickerProps>(), {
+    showTime: true,
+    timeOnly: true,
+    hourFormat: "24", // value - 12 or 24 supported 
+})
+
+</script>
