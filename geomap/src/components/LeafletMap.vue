fd8f5a9103134361930c7508a1736bdafdc38580
jhou <jadyhou@gmail.com>
2021-08-26 14:44:09 -0400

    add leaflet package

--- geomap/src/components/LeafletMap.vue
+++ geomap/src/components/LeafletMap.vue
@@ -1,9 +1,35 @@
 <template>
   <div class="leaflet">
-    <h4>This is the leaflet page</h4>
+    <div class="geo-map">
+    <l-map
+      v-model:zoom="zoom"
+      :zoomAnimation="true"
+      :center="openNMSHeadQuarter"
+    >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      ></l-tile-layer>
+      <l-control-layers />
+    </l-map>
+  </div>
   </div>
 </template>
-<script lang="ts"></script>
+<script setup lang="ts">
+import { reactive, ref } from "vue";
+import {
+  LMap,
+  LTileLayer,
+  // LMarker,
+  // LControlLayers,
+  // LTooltip,
+  // LPopup,
+  // LPolyline,
+} from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
+const openNMSHeadQuarter = ref([35.849613, -78.794882])
+const zoom = ref(4)
+</script>
 
 <style scoped>
 .geo-map {
