17697449ec8d412a401ec959c55dba6c1d445014
Sagar Salunkhe <SagarS16@citiustech.com>
2021-08-27 21:17:42 +0530

    Mockup data & apiService setup

---
+++ ui/src/components/Common/DataTable.vue
@@ -0,0 +1,17 @@
+<template>
+<p>Data Table Comp</p>
+
+</template>
+
+<script setup lang="ts">
+
+import nodeData from './Demo/apiService';
+
+const nodeDetails = nodeData.then((response) => {
+       console.log("nodeData ::",response.data.response)
+    })
+    .catch((err) => {
+        console.error(err);
+    });
+
+</script>
