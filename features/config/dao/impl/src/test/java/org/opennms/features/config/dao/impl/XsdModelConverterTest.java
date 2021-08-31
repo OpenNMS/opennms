88038323b1ab9c5375c6650546b2549734c72535
Freddy Chu <freddy@Freddys-MacBook-Pro.local>
2021-08-31 12:45:11 -0400

    PE-23: fix pull request comments
    1. fix doc
    2. fix serviceName > configName
    3. better error messages
    4. fix isPrimitiveType

--- features/config/dao/impl/src/test/java/org/opennms/features/config/dao/impl/XsdModelConverterTest.java
+++ features/config/dao/impl/src/test/java/org/opennms/features/config/dao/impl/XsdModelConverterTest.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2019-2019 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2019 The OpenNMS Group, Inc.
+ * Copyright (C) 2021 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2021 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -25,6 +25,7 @@
  *     http://www.opennms.org/
  *     http://www.opennms.com/
  *******************************************************************************/
+
 package org.opennms.features.config.dao.impl;
 
 import org.junit.Test;
