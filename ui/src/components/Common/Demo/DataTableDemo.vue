dde285402e10a35177184cee70504a7e6fadbfbf
Sagar Salunkhe <SagarS16@citiustech.com>
2021-08-31 16:44:52 +0530

    DataTable comp changes

--- ui/src/components/Common/Demo/DataTableDemo.vue
+++ ui/src/components/Common/Demo/DataTableDemo.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <DataTable tableHeader="Node Table" :value="nodeDataValue" :customData="customData" />
+        <DataTable 
+        tableHeader="Node Table" 
+        :value="nodeDataValue" 
+        :customData="customData" 
+        :rowsPerPageOptions = '[5,3,1]'
+        />
     </div>
 </template>
 
@@ -11,7 +16,7 @@ import nodeData from '../Demo/apiService';
 import DataTable from '../DataTable.vue';
 
 let nodeDataValue:any = ref([]);
-let customData = ['edit','delete']; //custome data
+let customData = ['edit','delete']; //custom data
 
 onMounted(() => {
     //service call for data
@@ -19,8 +24,7 @@ onMounted(() => {
         //data come form api
         let dataLen = response.data.response.length;
         if (dataLen > 0) {
-            let data = response.data.response
-            nodeDataValue.value = data;
+            nodeDataValue.value = response.data.response;
         }
     }).catch((err) => {
         console.error("error ==>", err);
