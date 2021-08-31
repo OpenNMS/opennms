096a1a1e0013cbf647a68fd5ecc847b62835a1e9
Chandra Gorantla <chandra@opennms.com>
2021-08-31 12:43:51 -0400

    NMS-13422:  Add full trapoid for snmp trap events (#3612)
    * NMS-13422: Add full trapoid for snmp trap events (#3579)
    
    * NMS-13422: Add full trapoid for snmp trap events.
    
    * NMS-13422: Add trapoid in ImmutableEvent as well
    
    * NMS-13422: Add trapoid in kafka producer proto as well
    
    * NMS-13422: Fix trap-oid element in TrapIdentityDTO
    
    * NMS-13422: Add trapoid to ExpandableParameters

--- features/kafka/producer/src/main/java/org/opennms/features/kafka/producer/ProtobufMapper.java
+++ features/kafka/producer/src/main/java/org/opennms/features/kafka/producer/ProtobufMapper.java
@@ -265,6 +265,7 @@ public class ProtobufMapper {
             snmpInfoBuilder.setGeneric(snmp.getGeneric());
         }
         getString(snmp.getCommunity()).ifPresent(snmpInfoBuilder::setCommunity);
+        getString(snmp.getTrapOID()).ifPresent(snmpInfoBuilder::setTrapOid);
         return snmpInfoBuilder;
     }
 
