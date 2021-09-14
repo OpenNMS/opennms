259011ca71b748770665b9223934371594e7820a
Sagar Salunkhe <SagarS16@citiustech.com>
2021-09-14 19:58:46 +0530

    PE:57 - form UI layout

--- ui/src/components/Configuration/ReqDefinitionForm.vue
+++ ui/src/components/Configuration/ReqDefinitionForm.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="container">
         <p class="title">New Requisition Definition</p>
-        <div class="card">
+        <div class="form">
             <div class="p-fluid">
                 <div class="p-field">
-                    <label for="name">Name</label>
+                    <label for="name" class="required">Name</label>
                     <InputText id="name" v-model="nameVal" />
                 </div>
+
                 <div class="p-field">
-                    <label for="type">Type</label>
+                    <label for="type" class="required">Type</label>
                     <DropDown
                         v-model="selectedType"
                         :options="types"
@@ -17,78 +18,77 @@
                         :filter="true"
                     ></DropDown>
                 </div>
+
                 <div class="p-field">
-                    <label for="host">Host</label>
+                    <label for="host" class="required">Host</label>
                     <InputText id="host" v-model="hostVal" />
                 </div>
+
                 <div class="p-field">
-                    <label for="foreignSource">Foreign Source</label>
+                    <label for="foreignSource" class="required">Foreign Source</label>
                     <InputText id="foreignSource" v-model="foreignSourceVal" />
                 </div>
+
                 <div class="p-field">
                     <label for="advOps">Advanced Options</label>
-
-                    <div class="inline-display" v-for="(add,index) in addAnotherArr">
+                    <div class="inline-display" v-for="add in addAnotherArr">
                         <span
                             v-if="add.id !== 0"
                             class="pi pi-times width100 align-R"
+                            style="font-size: 12px;"
                             @click="closeIcon(add.id)"
                         ></span>
                         <DropDown
                             :id="add.id"
-                            v-model="add.name"
+                            v-model="add.dropdownVal"
                             :options="advancedDropdown"
                             optionLabel="name"
                             optionValue="value"
                             :filter="true"
                         ></DropDown>
                         <p>
-                            <InputText v-model="add.advText" />
+                            <InputText
+                                v-model="add.advTextVal"
+                                @focusout="advParam(add.dropdownVal, add.advTextVal)"
+                            />
                         </p>
                     </div>
+                    <label class="width100">
+                        <span @click="addAnother">
+                            <i class="pi pi-plus" style="font-size: 12px;margin: 2px;"></i>Add Another
+                        </span>
+                        <a class="viewDoc">View Documentation</a>
+                    </label>
+                </div>
 
-                    <!-- <DropDown
-                        id="0"
-                        v-model="selectedAdvDropdown"
-                        :options="advancedDropdown"
+                <div class="p-field">
+                    <p class="generatedUrl">URL : {{ generatedURL }}</p>
+                    <Button label="Generate URL" @click="generateURL" :disabled="disableBtn"></Button>
+                </div>
+
+                <div class="p-field">
+                    <label for="type" class="required">Schedule Period</label>
+                    <DropDown
+                        v-model="selectedPeriod"
+                        :options="schedulePeriod"
                         optionLabel="name"
                         optionValue="value"
                         :filter="true"
                     ></DropDown>
                     <p>
-                        <InputText id="advDropdownText" v-model="advDropdownText" />
+                        Every
+                        <span>
+                            <InputNumber
+                                class="inputNumberSection"
+                                showButtons
+                                v-model="selectedInputNumber"
+                                :min="minVal"
+                            />
+                        </span>
+                        {{ selectedPeriod }}
                     </p>
-                    <div
-                        id="addDrop"
-                        ref="addDrop"
-                        v-for="(add, index) in addAnotherArr"
-                        :key="add.id"
-                    >
-                        <div v-html="add.value"></div>
-                    </div>-->
+                </div>
 
-                    <label class="width100">
-                        <span @click="addAnother">Add Another</span>
-                        <span class="viewDoc">View Documentation</span>
-                    </label>
-                </div>
-                <div class="p-field">
-                    <label for="url">URL</label>
-                    <p
-                        class="generatedUrl"
-                    >{{ selectedType + '://' + hostVal + '/' + foreignSourceVal }}</p>
-                </div>
-                <div class="p-field">
-                    <label for="type">Schedule Period</label>
-                    <DropDown
-                        v-model="selectedPeriod"
-                        :options="schedulePeriod"
-                        optionLabel="name"
-                        optionValue="name"
-                        :filter="true"
-                    ></DropDown>
-                    <p>Display selection</p>
-                </div>
                 <div class="p-field">
                     <Button label="Save" icon="pi pi-save" @click="onSave"></Button>
                 </div>
@@ -104,28 +104,27 @@ import { apiTypes, apiPeriod, apiAdvDropdown } from '../Common/Demo/apiService'
 import InputText from '../Common/InputText.vue'
 import DropDown from '../Common/DropDown.vue'
 import Button from '../Common/Button.vue'
+import InputNumber from '../Common/InputNumber.vue'
 
-const addDrop = ref('');
-const runTimeHtml = ref('');
+const minVal = ref(1);
+const disableBtn = ref(true);
 
 const nameVal = ref('');
 const hostVal = ref('');
 const foreignSourceVal = ref('');
 
-// const url = ref(`${nameVal.value}://${hostVal.value}/${foreignSourceVal.value}`);
-// const url = ref('');
-// url.value = nameVal.value + '://' + hostVal.value + '/' + foreignSourceVal.value
-
 const selectedType = ref('');
-const selectedAdvDropdown0 = ref('');
-const selectedAdvDropdown1 = ref('');
-const advDropdownText = ref('');
 const selectedPeriod = ref('');
+const selectedInputNumber = ref('');
+
 const types: any = ref([]);
 const schedulePeriod: any = ref([]);
 const advancedDropdown: any = ref([]);
 const count = ref(0);
-const addAnotherArr: any = ref([{ "id": count.value, "name": `selectedAdvDropdown${count.value}`, "advText": '' }]);
+const addAnotherArr: any = ref([{ "id": count.value, "dropdownVal": `selectedAdvDropdown${count.value}`, "advTextVal": '' }]);
+
+const paramUrl = ref('');
+const generatedURL = ref('');
 
 onMounted(() => {
     //service call for data
@@ -172,34 +171,17 @@ onMounted(() => {
     }
 });
 
-const advDropdownText0 = ref('');
-const advDropdownText1 = ref('');
-
 const addAnother = () => {
     console.log('add another clicked');
-    let counterVal = ++count.value;
-    let addObj = { "id": counterVal, "name": `selectedAdvDropdown${counterVal}`, "advText": `Enter param ${counterVal}` };
-    addAnotherArr.value.push(addObj);
-    console.log("Add ID addAnotherArr", addAnotherArr.value);
+    if (addAnotherArr.value.length < 5) {
+        let counterVal = ++count.value;
+        let addObj = { "id": counterVal, "dropdownVal": `selectedAdvDropdown${counterVal}`, "advTextVal": `Enter param ${counterVal}` };
+        addAnotherArr.value.push(addObj);
+        console.log("Add ID addAnotherArr", addAnotherArr.value);
+    } else {
+        alert(`Max allowed limit is ${count.value}`);
+    }
 
-    // console.log("v-model", selectedAdvDropdown0.value, "ref", advDropdownText0.value);
-    // console.log("v-model", selectedAdvDropdown1.value, "ref", advDropdownText1.value);
-    // const runTimeHtml =
-    //     `<span class="pi pi-times width100 align-R" @click="closeIcon(${counterVal})"></span>
-    //  <DropDown
-    //  :id="${counterVal}"
-    //  v-model=selectedAdvDropdown${counterVal}
-    //  :options="advancedDropdown"
-    //   optionLabel="name"
-    //  optionValue="value"
-    // :filter="true"
-    // ></DropDown>
-    // <p> <InputText id="advDropdownText" v-model="advDropdownText${counterVal}" /> </p>`
-
-    // console.log(runTimeHtml);
-    // addAnotherArr.value.push({ "id": counterVal, "value": runTimeHtml });
-    // console.log("Add ID addAnotherArr", addAnotherArr.value);
-    // addDrop[innerHTML] = runTimeHtml;
 }
 
 const closeIcon = (id: any) => {
@@ -209,7 +191,28 @@ const closeIcon = (id: any) => {
     console.log("Remove ID from addAnotherArr ::", addAnotherArr.value);
 }
 
+const advParam = (paramDrop: any, paramText: any) => {
+    console.log(paramDrop, paramText);
+    let text = paramText;
+    if (text === '') {
+        paramUrl.value += `?${paramDrop}=${text}`;
+    } else {
+        console.log(text)
+        paramUrl.value += `?${paramDrop}=${text}`;
+    }
+}
+
+const generateURL = () => {
+    let mandateCondition = nameVal.value !== '' && hostVal.value !== '' && foreignSourceVal.value !== '' && selectedType.value !== '';
+    if (mandateCondition) {
+        disableBtn.value = false;
+    } else {
+        disableBtn.value = true;
+    }
+}
+
 const onSave = () => {
+    console.log("add another array value", addAnotherArr.value);
     // console.log(
     //     `{
     //     Name: ${nameVal.value},
@@ -258,5 +261,16 @@ $title-font: 18px;
 
 .align-R {
     text-align: right;
+}
+
+.inputNumberSection {
+    width: 30%;
+    height: 30%;
+    margin: 1%;
+}
+
+.required:after {
+    content: " *";
+    color: red;
 }
 </style>
