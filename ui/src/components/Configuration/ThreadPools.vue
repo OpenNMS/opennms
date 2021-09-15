25ce257ad0c1bc20f26d3392d092bc80f84f0dd4
tbansal-opennms <tbansal@nanthealth.com>
2021-09-15 17:43:40 +0530

    PE-67:Threadpools page layout

--- ui/src/components/Configuration/ThreadPools.vue
+++ ui/src/components/Configuration/ThreadPools.vue
@@ -1,9 +1,59 @@
 <template>
-<p>ThreadPool</p>
+  <br />
+    <label class="p-col-9" for="import">Import</label>
+    <br />
+    <InputNumber
+      id="import"
+      class="p-col-9"
+      mode="decimal"
+      />
+    <br />
+
+    <label class="p-col-9" for="scan">Scan</label>
+    <br />
+    <InputNumber
+      id="scan"
+      class="p-col-9"
+      mode="decimal"
+    />
+    <br />
+
+    <label class="p-col-9" for="rescan">Rescan</label>
+    <br />
+    <InputNumber
+    class="p-col-9"
+      mode="decimal"
+    />
+    <br />
+
+    <label class="p-col-9" for="write">Write</label>
+    <br />
+    <InputNumber
+    class="p-col-9"
+      mode="decimal"      
+    />    
+    <br />
+  <br />
+  <Button label="Save" ></Button>
 </template>
 
 <script setup lang="ts">
+import {reactive, toRef} from 'vue'
+import InputNumber from '../Common/InputNumber.vue'
+import Button from '../Common/Button.vue'
 
+interface ThreadPoolProps {
+  import?: number,
+  scan?: number,
+  rescan?: number,
+  write?: number
+}
 
+const threadpool = reactive({
+  import: 0,
+  scan: 0,
+  rescan: 0,
+  write: 0
+});
 
 </script>
