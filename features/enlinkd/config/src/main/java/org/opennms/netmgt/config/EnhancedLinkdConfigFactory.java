9ad02408fc634dea1be2983a8a17947798a5bfd5
Freddy Chu <fchu@opennms.com>
2021-09-22 23:23:04 -0400

    PE-75: add mock cm for test cases

--- features/enlinkd/config/src/main/java/org/opennms/netmgt/config/EnhancedLinkdConfigFactory.java
+++ features/enlinkd/config/src/main/java/org/opennms/netmgt/config/EnhancedLinkdConfigFactory.java
@@ -47,6 +47,8 @@ import org.opennms.netmgt.config.enlinkd.EnlinkdConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+
 /**
  * This is the singleton class used to load the configuration for the OpenNMS
  * enhanced linkd service from the enlinkd-configuration xml file.
@@ -64,7 +66,12 @@ public final class EnhancedLinkdConfigFactory extends EnhancedLinkdConfigManager
     private static final String CONFIG_NAME = "enlinkd";
     private static final String DEFAULT_CONFIG_ID = "default";
     
-    public EnhancedLinkdConfigFactory() throws IOException {
+    public EnhancedLinkdConfigFactory() {
+        // move to postConstruct to prevent dao bean not ready
+    }
+
+    @PostConstruct
+    public void postConstruct() throws IOException {
         reload();
     }
 
@@ -115,6 +122,6 @@ public final class EnhancedLinkdConfigFactory extends EnhancedLinkdConfigManager
 
     @Override
     public void reload() throws IOException {
-        this.loadConfig(this.getDefaultConfigId());
+        m_config = this.loadConfig(this.getDefaultConfigId());
     }
 }
