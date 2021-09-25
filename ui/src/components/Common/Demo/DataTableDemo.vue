168a6472fd3d272e5e9ec0a4103cb90a83aa8d1b
freddy61025 <freddy.chc@gmail.com>
2021-09-24 21:01:43 -0400

    PE-103: merge CM-Phase2 < CM-Phase1 (#3675)
    * PE-57:Structure for mockData & Form layout
    
    * PE:57 - form UI layout
    
    * PE-57:ReqDef UI Layout
    
    * PE-57:Final Form Layout
    
    * PE-57:thread pool CSS changed
    
    * PE-59-ThreadPool validations
    
    * "skipLibCheck": true
    
    * PE-97: Enhance ConfigurationMangerServiceMock for easier integration (#3674)
    
    * PE-97: enhance cm mock test
    
    * PE-97: minor fix for test cases
    
    * PE-97: fix opennms-dao test
    
    * PE-97: fix opennms-dao circular dependency
    
    Co-authored-by: Sagar Salunkhe <SagarS16@citiustech.com>
    Co-authored-by: tbansal-opennms <tbansal@nanthealth.com>
    Co-authored-by: Tripti Bansal <88324290+tbansal-opennms@users.noreply.github.com>
    Co-authored-by: ssalunkhe-opennms <88327383+ssalunkhe-opennms@users.noreply.github.com>

--- ui/src/components/Common/Demo/DataTableDemo.vue
+++ ui/src/components/Common/Demo/DataTableDemo.vue
@@ -1,25 +1,21 @@
 <template>
     <div>
-        <DataTable 
-        :value="nodeDataValue" 
-        :customData="customData" 
-        :rowsPerPageOptions = '[5,3,1]'
-        />
+        <DataTable :value="nodeDataValue" :customData="customData" :rowsPerPageOptions="[5, 3, 1]" />
     </div>
 </template>
 
 <script setup lang="ts">
 
 import { onMounted, ref } from 'vue';
-import nodeData from '../Demo/apiService';
+import { nodeData } from '../Demo/apiService';
 import DataTable from '../DataTable.vue';
 
-let nodeDataValue:any = ref([]);
-let customData = ['edit','delete']; //custom data
+let nodeDataValue: any = ref([]);
+let customData = ['edit', 'delete']; //custom data
 
 onMounted(() => {
     //service call for data
-    nodeData.then((response) => {
+    nodeData.then((response: any) => {
         //data come form api
         let dataLen = response.data.response.length;
         if (dataLen > 0) {
--- ui/src/shims-vue.d.ts
+++ ui/src/shims-vue.d.ts
@@ -3,4 +3,7 @@ declare module '*.vue' {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
+  
 }
+declare module '@vuelidate/core'
+declare module '@vuelidate/validators'
