9d1700cf05ca85fa75996a7e6cc253c1466d4def
fahmad-opennms <fahmad@opennms.com>
2021-09-28 09:28:44 +0000

    Added popup, tile layer and basic changes

--- geomap/src/components/LeafletMap.vue
+++ geomap/src/components/LeafletMap.vue
@@ -1,61 +1,99 @@
 <template>
   <div class="leaflet">
     <div class="geo-map">
-      <LMap
+      <l-map
         ref="map"
         :max-zoom="19"
         v-model="zoom"
         :zoomAnimation="true"
-        :center="{ lat: 51.289404225298256, lng: 9.697202050919614 }"
+        :center="center"
         @ready="onLeafletReady"
       >
-        <!-- <l-tile-layer 
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      ></l-tile-layer>
-      <l-marker :lat-lng="markerLatLng"></l-marker>
-      <l-marker :lat-lng="markerLatLng1"></l-marker> -->
-        <!-- <l-control-layers /> -->
         <template v-if="leafletReady">
-          <l-tile-layer url="https://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+          <l-control-layers />
+          <l-tile-layer
+            v-for="tileProvider in tileProviders"
+            :key="tileProvider.name"
+            :name="tileProvider.name"
+            :visible="tileProvider.visible"
+            :url="tileProvider.url"
+            :attribution="tileProvider.attribution"
+            layer-type="base"
+          />
           <marker-cluster
             :options="{ showCoverageOnHover: false, chunkedLoading: true }"
           >
-            <l-marker :lat-lng="[47.7515953048815, 8.757179159967961]" />
-            <l-marker :lat-lng="[54.379448751829784, 8.890621239746661]" />
-            <l-marker :lat-lng="[48.41432462648719, 11.172363685423019]" />
-            <l-marker :lat-lng="[54.34757868763789, 11.410597389004957]" />
-            <l-marker :lat-lng="[51.741295879474464, 13.693138753473695]" />
-            <l-marker :lat-lng="[53.574845165295145, 6.875185458821902]" />
-            <l-marker :lat-lng="[51.42494690949777, 6.901031944520698]" />
+            <l-marker
+              v-for="coordinate in coordinates"
+              :key="coordinate"
+              :lat-lng="coordinate.latlng"
+            >
+              <l-popup>{{ coordinate.popmessage }}</l-popup>
+            </l-marker>
           </marker-cluster>
         </template>
-      </LMap>
+      </l-map>
     </div>
   </div>
 </template>
 <script setup lang ="ts">
-import { reactive, onMounted, ref, nextTick } from "vue";
+import { ref, nextTick } from "vue";
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LPopup,
+  LControlLayers,
+} from "@vue-leaflet/vue-leaflet";
 import MarkerCluster from "./MarkerCluster.vue";
 import { Vue } from "vue-class-component";
 
 const zoom = ref(7);
+const center = ref({ lat: 51.289404225298256, lng: 9.697202050919614 });
 let leafletReady = ref(false);
-let leafletObject = ref('');
+let leafletObject = ref("");
 let visible = ref(false);
 let map: any = ref();
+
+/*****Multiple Markers*****/
+
+const coordinates = [
+  { latlng: [47.7515953048815, 8.757179159967961], popmessage: "Street 01" },
+  { latlng: [54.379448751829784, 8.890621239746661], popmessage: "Street 02" },
+  { latlng: [48.41432462648719, 11.172363685423019], popmessage: "Street 03" },
+  { latlng: [54.34757868763789, 11.410597389004957], popmessage: "Street 04" },
+  { latlng: [51.741295879474464, 13.693138753473695], popmessage: "Street 05" },
+  { latlng: [53.574845165295145, 6.875185458821902], popmessage: "Street 06" },
+  { latlng: [51.42494690949777, 6.901031944520698], popmessage: "Street 07" },
+];
 
 async function onLeafletReady() {
   await nextTick();
   leafletObject.value = map.value.leafletObject;
-  leafletReady.value = true;
+  if(leafletObject.value != undefined && leafletObject.value != null){
+    leafletReady.value = true;
+  }
 }
 
-//  const openNMSHeadQuarter = ref([27.175014, -78.042152])
-//  const markerLatLng = [47.7515953048815, 8.757179159967961];
-//  const markerLatLng1 = [54.379448751829784, 8.890621239746661];
+/*****Tile Layer*****/
+
+const tileProviders = [
+  {
+    name: "OpenStreetMap",
+    visible: true,
+    attribution:
+      '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  },
+  {
+    name: "OpenTopoMap",
+    visible: false,
+    url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
+    attribution:
+      'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+  },
+];
 </script>
 
 <style scoped>
