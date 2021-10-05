8c9dcbcb144f9b7ae5abe6a03b3b2db3060ebd9e
tbansal-opennms <tbansal@nanthealth.com>
2021-10-05 12:19:37 +0530

    Get API related changes

--- ui/src/components/Common/Demo/apiInterceptor.ts
+++ ui/src/components/Common/Demo/apiInterceptor.ts
@@ -1,25 +1,31 @@
 import axios from 'axios'
-import {BASE_REST_URL, BASE_REST_AUTH} from '../../../../devConfig'
+import { BASE_REST_URL, BASE_REST_AUTH } from '../../../../devConfig'
 
-export const axiosAuth = axios.create();
+export const axiosAuth = axios.create()
 
 axiosAuth.defaults.baseURL = BASE_REST_URL
-axiosAuth.defaults.headers.common['Authorization'] = "Basic " + BASE_REST_AUTH
+axiosAuth.defaults.headers.common['Authorization'] = 'Basic ' + BASE_REST_AUTH
 
-axiosAuth.interceptors.request.use(function (config) {
-    return config;
-  }, function (error) {
+axiosAuth.interceptors.request.use(
+  function (config) {
+    return config
+  },
+  function (error) {
     // Do something with request error
-    return Promise.reject(error);
-  });
+    return Promise.reject(error)
+  }
+)
 
 // Add a response interceptor
-axiosAuth.interceptors.response.use(function (response) {
+axiosAuth.interceptors.response.use(
+  function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
-    return response;
-  }, function (error) {
+    return response
+  },
+  function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
-  });
+    return Promise.reject(error)
+  }
+)
--- ui/src/components/Common/Demo/apiService.ts
+++ ui/src/components/Common/Demo/apiService.ts
@@ -1,14 +1,13 @@
 import axios from 'axios'
 import { axiosAuth } from './apiInterceptor'
 
-
 let url = 'src/components/Common/Demo/MockupData/nodeData.json'
 let typesUrl = 'src/components/Common/Demo/MockupData/types.json'
 let periodUrl = 'src/components/Common/Demo/MockupData/schedulePeriod.json'
 let advDropdownUrl = 'src/components/Common/Demo/MockupData/advancedDropdown.json'
 let getProvisionD = '/opennms/rest/cm/provisiond/default'
 const nodeData = axios.get(url)
-const apigetProvisionD =  axiosAuth.get(getProvisionD)
+const apigetProvisionD = axiosAuth.get(getProvisionD)
 
 const getDropdownTypes = axios
   .get(typesUrl)
@@ -46,4 +45,4 @@ const getAdvancedDropdown = axios
     console.error('apiAdvDropdown Error ==>', err)
   })
 
- export { nodeData, getDropdownTypes, getSchedulePeriod, getAdvancedDropdown, apigetProvisionD }
+export { nodeData, getDropdownTypes, getSchedulePeriod, getAdvancedDropdown, apigetProvisionD }
