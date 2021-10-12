3c63e5997fa1724d7087298bc653d5f1a60f5c80
Benjamin Reed <ranger@opennms.org>
2021-10-12 14:40:00 -0400

    backport build improvements to foundation 2019

--- .circleci/scripts/itest.sh
+++ .circleci/scripts/itest.sh
@@ -5,7 +5,7 @@ find_tests()
     # Generate surefire & failsafe test list based on current
     # branch and the list of files changed
     # (The format of the output files contains the canonical class names i.e. org.opennms.core.soa.filter.FilterTest)
-    pyenv local 3.5.2
+    pyenv local 3.8.5
     python3 .circleci/scripts/find-tests/find-tests.py generate-test-lists \
       --changes-only="${CCI_CHANGES_ONLY:-true}" \
       --output-unit-test-classes=surefire_classnames \
@@ -36,6 +36,9 @@ if [ ! -s /tmp/this_node_projects ]; then
   exit 0
 fi
 
+echo "#### Set loopback to 127.0.0.1"
+sudo sed -i 's/127.0.1.1/127.0.0.1/g' /etc/hosts
+
 echo "#### Allowing non-root ICMP"
 sudo sysctl net.ipv4.ping_group_range='0 429496729'
 
@@ -46,17 +49,39 @@ cd ~/project
 echo "#### Installing other dependencies"
 # limit the sources we need to update
 sudo rm -f /etc/apt/sources.list.d/*
-# limit more sources and add mirrors
-echo 'deb mirror://mirrors.ubuntu.com/mirrors.txt trusty main restricted universe multiverse
-deb http://archive.ubuntu.com/ubuntu/ trusty main restricted
-deb-src http://archive.ubuntu.com/ubuntu/ trusty main restricted' | sudo tee /etc/apt/sources.list
 
 # kill other apt-gets first to avoid problems locking /var/lib/apt/lists/lock - see https://discuss.circleci.com/t/could-not-get-lock-var-lib-apt-lists-lock/28337/6
-sudo killall -9 apt-get || true && \
-            sudo apt-get update && \
-            sudo apt-get -y install debconf-utils && \
+sudo killall -9 apt || true && \
+            sudo apt update && \
+            sudo env DEBIAN_FRONTEND=noninteractive apt -y --no-install-recommends install \
+                ca-certificates \
+                tzdata \
+                software-properties-common \
+                debconf-utils
+
+# install some keys
+curl -sSf https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+curl -sSf https://debian.opennms.org/OPENNMS-GPG-KEY | sudo tee -a /etc/apt/trusted.gpg.d/opennms_key.asc
+
+# limit more sources and add mirrors
+echo "deb mirror://mirrors.ubuntu.com/mirrors.txt $(lsb_release -cs) main restricted universe multiverse
+deb http://archive.ubuntu.com/ubuntu/ $(lsb_release -cs) main restricted" | sudo tee -a /etc/apt/sources.list
+sudo add-apt-repository 'deb http://debian.opennms.org stable main'
+
+# add the R repository
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+
+sudo apt-get update && \
+            RRDTOOL_VERSION=$(apt-cache show rrdtool | grep Version: | grep -v opennms | awk '{ print $2 }') && \
             echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections && \
-            sudo env DEBIAN_FRONTEND=noninteractive apt-get install -f nsis R-base rrdtool || exit 1
+            sudo env DEBIAN_FRONTEND=noninteractive apt -f --no-install-recommends install \
+                nsis \
+                r-base \
+                "rrdtool=$RRDTOOL_VERSION" \
+                jrrd2 \
+                jicmp \
+                jicmp6 \
+            || exit 1
 
 echo "#### Building Assembly Dependencies"
 ./compile.pl install -P'!checkstyle' \
