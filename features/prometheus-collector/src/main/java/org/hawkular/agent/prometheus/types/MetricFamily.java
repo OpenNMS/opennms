9576e6f14ad915a072a3114732bb76d4bc36a085
Dino <dino2gnt@gmail.com>
2021-10-18 15:33:39 -0500

    Revert "Revert "NMS-12717: Prometheus collector won't process untyped metrics""
    This reverts commit 58e88f6188f00b346e140a43c41063dce6b7eac3.

--- features/prometheus-collector/src/main/java/org/hawkular/agent/prometheus/types/MetricFamily.java
+++ features/prometheus-collector/src/main/java/org/hawkular/agent/prometheus/types/MetricFamily.java
@@ -87,6 +87,10 @@ public class MetricFamily {
             case HISTOGRAM:
                 expectedMetricClassType = Histogram.class;
                 break;
+            case UNTYPED:
+	        //treat untyped metrics as gauge
+                expectedMetricClassType = Gauge.class;
+                break;
             default:
                 throw new IllegalArgumentException("Invalid type: " + builder.type);
         }
