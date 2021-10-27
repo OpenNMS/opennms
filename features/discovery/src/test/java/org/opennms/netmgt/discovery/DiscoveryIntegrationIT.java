6d36537c93c133ce7f0cd6e7b96242ea36b3173c
Freddy Chu <fchu@opennms.com>
2021-10-27 17:48:22 -0400

    PE-66: try to fix test and align xml files

--- features/discovery/src/test/java/org/opennms/netmgt/discovery/DiscoveryIntegrationIT.java
+++ features/discovery/src/test/java/org/opennms/netmgt/discovery/DiscoveryIntegrationIT.java
@@ -62,10 +62,10 @@ import static org.opennms.core.utils.LocationUtils.DEFAULT_LOCATION_NAME;
 @RunWith(OpenNMSJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
         "classpath:/META-INF/opennms/applicationContext-soa.xml",
-        "classpath*:/META-INF/opennms/component-dao.xml",
         "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
         "classpath:/META-INF/opennms/applicationContext-minimal-conf.xml",
         "classpath:/META-INF/opennms/applicationContext-dao.xml",
+        "classpath*:/META-INF/opennms/component-dao.xml",
         "classpath:/META-INF/opennms/applicationContext-daemon.xml",
         "classpath:/META-INF/opennms/applicationContext-pinger.xml",
         "classpath:/META-INF/opennms/mockEventIpcManager.xml",
