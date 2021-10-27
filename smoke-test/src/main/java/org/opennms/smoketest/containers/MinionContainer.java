d954ba3bcad4729ae706c456f3c6a17e93aea188
Chandra Gorantla <chandra@opennms.com>
2021-10-27 09:42:54 -0400

    NMS-13160: Add twin strategy to OpenNMS container in smoke tests

--- smoke-test/src/main/java/org/opennms/smoketest/containers/MinionContainer.java
+++ smoke-test/src/main/java/org/opennms/smoketest/containers/MinionContainer.java
@@ -184,6 +184,19 @@ public class MinionContainer extends GenericContainer implements KarafContainer,
                     "\t}\n" +
                     "}";
             OverlayUtils.writeYaml(minionConfigYaml, jsonMapper.readValue(kafkaSink, Map.class));
+            
+            String kafkaTwin = "{\n" +
+                    "\t\"ipc\": {\n" +
+                    "\t\t\"twin\": {\n" +
+                    "\t\t\t\"kafka\": {\n" +
+                    "\t\t\t\t\"bootstrap.servers\": \""+ OpenNMSContainer.KAFKA_ALIAS +":9092\",\n" +
+                    "\t\t\t\t\"acks\": 1,\n" +
+                    "\t\t\t\t\"compression.type\": \""+ model.getKafkaCompressionStrategy().getCodec() +"\"\n" +
+                    "\t\t\t}\n" +
+                    "\t\t}\n" +
+                    "\t}\n" +
+                    "}";
+            OverlayUtils.writeYaml(minionConfigYaml, jsonMapper.readValue(kafkaTwin, Map.class));
         } else if (IpcStrategy.GRPC.equals(model.getIpcStrategy())) {
             String grpc = "{\n" +
                     "\t\"ipc\": {\n" +
