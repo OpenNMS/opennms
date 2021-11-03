dc3a9aefdda8c8027cd92b5d3f8ff0309975bff6
Antonio Russo <rssntn67@yahoo.it>
2021-11-03 19:52:05 +0100

    Update features/enlinkd/tests/src/test/java/org/opennms/netmgt/enlinkd/EnLinkdSnmpIT.java
    Co-authored-by: Christian Pape <Christian.Pape@informatik.hs-fulda.de>

--- features/enlinkd/tests/src/test/java/org/opennms/netmgt/enlinkd/EnLinkdSnmpIT.java
+++ features/enlinkd/tests/src/test/java/org/opennms/netmgt/enlinkd/EnLinkdSnmpIT.java
@@ -905,7 +905,7 @@ public class EnLinkdSnmpIT extends NmsNetworkBuilder implements InitializingBean
                     .execute()
                     .get();
         } catch (final InterruptedException e) {
-            LOG.info("run: collection interrupted, exiting {}",e.getMessage());
+            LOG.info("run: collection interrupted, exiting {}", e.getMessage());
         }
 
         assertNull(lldpLocalGroup02.getLldpLocChassisid());
