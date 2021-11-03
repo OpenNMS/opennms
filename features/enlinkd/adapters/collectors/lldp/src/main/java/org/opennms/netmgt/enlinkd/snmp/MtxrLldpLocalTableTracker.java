dd6403797d497f050a25d007c0ac5fa4535081dc
Antonio Russo <rssntn67@yahoo.it>
2021-11-03 18:51:13 +0100

    Update features/enlinkd/adapters/collectors/lldp/src/main/java/org/opennms/netmgt/enlinkd/snmp/MtxrLldpLocalTableTracker.java
    Co-authored-by: Christian Pape <Christian.Pape@informatik.hs-fulda.de>

--- features/enlinkd/adapters/collectors/lldp/src/main/java/org/opennms/netmgt/enlinkd/snmp/MtxrLldpLocalTableTracker.java
+++ features/enlinkd/adapters/collectors/lldp/src/main/java/org/opennms/netmgt/enlinkd/snmp/MtxrLldpLocalTableTracker.java
@@ -86,6 +86,7 @@ public class MtxrLldpLocalTableTracker extends TableTracker {
 	    public String getLldpLocPortDesc() {
 	    	if (getValue(LldpLocPortGetter.LLDP_LOC_DESCR) != null) {
 	    		return getValue(LldpLocPortGetter.LLDP_LOC_DESCR).toDisplayString();
+	    	}
 	    	return "";
 	    }
     }
