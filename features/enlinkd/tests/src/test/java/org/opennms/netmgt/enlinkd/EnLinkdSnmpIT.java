ca70ad4899766bfcf3b61b9b7d32e013e8000b53
Antonio Russo <rssntn67@yahoo.it>
2021-11-03 19:29:23 +0100

    Update features/enlinkd/tests/src/test/java/org/opennms/netmgt/enlinkd/EnLinkdSnmpIT.java
    Co-authored-by: Christian Pape <Christian.Pape@informatik.hs-fulda.de>

--- features/enlinkd/tests/src/test/java/org/opennms/netmgt/enlinkd/EnLinkdSnmpIT.java
+++ features/enlinkd/tests/src/test/java/org/opennms/netmgt/enlinkd/EnLinkdSnmpIT.java
@@ -746,7 +746,7 @@ public class EnLinkdSnmpIT extends NmsNetworkBuilder implements InitializingBean
                     assertEquals(74, link.getLldpLocalPortNum().intValue());
                     assertEquals("gi5", link.getLldpPortId());
                     assertEquals("GigabitEthernet5", link.getLldpPortDescr());
-                    assertEquals(LldpPortIdSubType.LLDP_PORTID_SUBTYPE_INTERFACENAME,link.getLldpPortIdSubType());
+                    assertEquals(LldpPortIdSubType.LLDP_PORTID_SUBTYPE_INTERFACENAME, link.getLldpPortIdSubType());
                     assertEquals(LldpChassisIdSubType.LLDP_CHASSISID_SUBTYPE_MACADDRESS,link.getLldpRemChassisIdSubType());
                     assertEquals(MKTROUTER2_ETHER1_MAC,link.getLldpRemChassisId());
                     assertEquals(LldpPortIdSubType.LLDP_PORTID_SUBTYPE_INTERFACENAME,link.getLldpRemPortIdSubType());
