ad3ebbb9aa918dc424e584ca34addf10d05bdb92
Jesse White <jesse@opennms.org>
2021-11-04 11:37:38 -0400

    add XML validation support

--- opennms-webapp-rest/src/main/java/org/opennms/web/rest/v1/FilesystemRestService.java
+++ opennms-webapp-rest/src/main/java/org/opennms/web/rest/v1/FilesystemRestService.java
@@ -39,6 +39,7 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -48,14 +49,163 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.cxf.jaxrs.ext.multipart.Attachment;
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
+import org.opennms.core.daemon.DaemonReloadEnum;
+import org.opennms.netmgt.events.api.EventConstants;
+import org.opennms.netmgt.events.api.EventIpcManager;
+import org.opennms.netmgt.model.events.EventBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 @Component
 @Path("filesystem")
 public class FilesystemRestService {
+
+    @Autowired
+    private EventIpcManager eventForwarder;
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<String> getFiles() {
+        return FILES;
+    }
+
+    @GET
+    @Path("/help")
+    @Produces("text/markdown")
+    public InputStream getFileHelp(@QueryParam("f") String fileName) throws IOException {
+        if (!FILES.contains(fileName)) {
+            throw new RuntimeException("Unsupported filename: '" + fileName + "'");
+        }
+        return this.getClass().getResourceAsStream("/help/" + fileName + ".md");
+    }
+
+    @GET
+    @Path("/contents")
+    public Response getFileContents(@QueryParam("f") String fileName) throws IOException {
+        if (!FILES.contains(fileName)) {
+            throw new RuntimeException("Unsupported filename: '" + fileName + "'");
+        }
+        final File file = Paths.get(System.getProperty("opennms.home"), "etc", fileName).toFile();
+        final String mimeType = Files.probeContentType(file.toPath());
+        return streamAll(file, mimeType, fileName);
+    }
+
+    @POST
+    @Path("/contents")
+    @Produces(MediaType.TEXT_HTML)
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public String uploadFile(@QueryParam("f") String fileName,
+                             @Multipart("upload") Attachment attachment) throws IOException {
+        if (!FILES.contains(fileName)) {
+            throw new RuntimeException("Unsupported filename: '" + fileName + "'");
+        }
+
+        // Write the contents a temporary file
+        final File tempFile = File.createTempFile("upload-",fileName);
+        try {
+            tempFile.deleteOnExit();
+            final InputStream in = attachment.getObject(InputStream.class);
+            Files.copy(in, tempFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+            // Validate it
+            maybeValidateXml(tempFile);
+
+            // Copy it to the right place
+            final java.nio.file.Path targetPath = Paths.get(System.getProperty("opennms.home"), "etc", fileName);
+            Files.copy(tempFile.toPath(), targetPath, StandardCopyOption.REPLACE_EXISTING);
+
+            // Reload the associated daemon
+            boolean didReloadDaemon = maybeReloadDaemon(fileName);
+
+            // Build our message
+            String message = String.format("Successfully wrote to '%s'.", targetPath);
+            if (didReloadDaemon) {
+                message += " The associated daemon was also reloaded.";
+            }
+
+            return message;
+        } finally {
+            // Delete the temporary file
+            tempFile.delete();
+        }
+    }
+
+    public static Response streamAll(final File file, String mimeType, String fileName) {
+        return Response.ok(file)
+                .type(mimeType)
+                .header(HttpHeaders.CONTENT_DISPOSITION, fileName)
+                .header(HttpHeaders.CONTENT_LENGTH, file.length())
+                .header(HttpHeaders.LAST_MODIFIED, new Date(file.lastModified()))
+                .build();
+    }
+
+    private boolean maybeReloadDaemon(String fileName) {
+        if (POLLER_CONFIGURATION_XML.equals(fileName)) {
+            EventBuilder eventBuilder = new EventBuilder(EventConstants.RELOAD_DAEMON_CONFIG_UEI, FilesystemRestService.class.getName());
+            eventBuilder.addParam(EventConstants.PARM_DAEMON_NAME, DaemonReloadEnum.POLLERD.getDaemonName());
+            eventForwarder.sendNow(eventBuilder.getEvent());
+            return true;
+        }
+        return false;
+    }
+
+    private void maybeValidateXml(File file) {
+        if (!file.getName().endsWith(".xml")) {
+            return;
+        }
+
+        final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setValidating(false);
+        factory.setNamespaceAware(true);
+
+        final CapturingErrorHandler errorHandler = new CapturingErrorHandler();
+        try {
+            final DocumentBuilder builder = factory.newDocumentBuilder();
+            builder.setErrorHandler(errorHandler);
+            builder.parse(file);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new BadRequestException(Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Validation failed: " + e.getMessage()).build());
+        }
+    }
+
+    public static class CapturingErrorHandler implements ErrorHandler {
+        final StringBuilder sb = new StringBuilder();
+
+        public void warning(SAXParseException e) {
+            sb.append("WARNING: ");
+            sb.append(e.getMessage());
+            sb.append("\n");
+        }
+
+        public void error(SAXParseException e) {
+            sb.append("ERROR: ");
+            sb.append(e.getMessage());
+            sb.append("\n");
+        }
+
+        public void fatalError(SAXParseException e) {
+            sb.append("FATAL ERROR: ");
+            sb.append(e.getMessage());
+            sb.append("\n");
+        }
+
+        public String toString() {
+            return sb.toString();
+        }
+    }
+
+    public static final String POLLER_CONFIGURATION_XML = "poller-configuration.xml";
 
     private static final List<String> FILES = Arrays.asList(
             "availability-reports.xml",
@@ -66,7 +216,7 @@ public class FilesystemRestService {
             "http-datacollection-config.xml",
             "jdbc-datacollection-config.xml",
             "log4j2.xml",
-            "poller-configuration.xml",
+            POLLER_CONFIGURATION_XML,
             "provisiond-configuration.xml",
             "rancid-configuration.xml",
             "snmp-asset-adapter-configuration.xml",
@@ -146,58 +296,4 @@ public class FilesystemRestService {
     ); static {
         FILES.sort(Comparator.naturalOrder());
     }
-
-    @GET
-    @Path("/")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<String> getFiles() {
-        return FILES;
-    }
-
-    @GET
-    @Path("/help")
-    @Produces("text/markdown")
-    public InputStream getFileHelp(@QueryParam("f") String fileName) throws IOException {
-        if (!FILES.contains(fileName)) {
-            throw new RuntimeException("Unsupported filename: '" + fileName + "'");
-        }
-        return this.getClass().getResourceAsStream("/help/" + fileName + ".md");
-    }
-
-    @GET
-    @Path("/contents")
-    public Response getFileContents(@QueryParam("f") String fileName) throws IOException {
-        if (!FILES.contains(fileName)) {
-            throw new RuntimeException("Unsupported filename: '" + fileName + "'");
-        }
-        final File file = Paths.get(System.getProperty("opennms.home"), "etc", fileName).toFile();
-        final String mimeType = Files.probeContentType(file.toPath());
-        return streamAll(file, mimeType, fileName);
-    }
-
-    @POST
-    @Path("/contents")
-    @Produces(MediaType.TEXT_HTML)
-    @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public String uploadFile(@QueryParam("f") String fileName,
-                             @Multipart("upload") Attachment attachment) throws IOException {
-        if (!FILES.contains(fileName)) {
-            throw new RuntimeException("Unsupported filename: '" + fileName + "'");
-        }
-
-        final java.nio.file.Path path = Paths.get(System.getProperty("opennms.home"), "etc", fileName);
-        InputStream in = attachment.getObject(InputStream.class);
-        Files.copy(in, path, StandardCopyOption.REPLACE_EXISTING);
-        return "uploaded " + fileName;
-    }
-
-    private Response streamAll(final File file, String mimeType, String fileName) {
-        return Response.ok(file)
-                .type(mimeType)
-                .header(HttpHeaders.CONTENT_DISPOSITION, fileName)
-                .header(HttpHeaders.CONTENT_LENGTH, file.length())
-                .header(HttpHeaders.LAST_MODIFIED, new Date(file.lastModified()))
-                .build();
-    }
-
 }
--- opennms-webapp-rest/src/main/resources/help/poller-configuration.xml.md
+++ opennms-webapp-rest/src/main/resources/help/poller-configuration.xml.md
@@ -1,24 +1,19 @@
 # Poller Configuration
 
-Hi, here's a snippet to monitor something:
-```
-  <more></xml>
-```
-
 ## Website monitoring
 
 ```
-<service name="Web-Site-Monitor" interval="300000" user-defined="true" status="on"> (1)
-   <pattern><![CDATA[^Web:.*$]]></pattern> (2)
+<service name="Web-Site-Monitor" interval="300000" user-defined="true" status="on">
+   <pattern><![CDATA[^Web:.*$]]></pattern>
    <parameter key="retry" value="1" />
    <parameter key="timeout" value="3000" />
-   <parameter key="port" value="${requisition:port|443}" /> (3)
-   <parameter key="host-name" value="${requisition:vhost}" /> (4)
-   <parameter key="url" value="${requisition:path|/}" /> (5)
-   <parameter key="response" value="${requisition:response|200}" /> (6)
+   <parameter key="port" value="${requisition:port|443}" />
+   <parameter key="host-name" value="${requisition:vhost}" />
+   <parameter key="url" value="${requisition:path|/}" />
+   <parameter key="response" value="${requisition:response|200}" />
    <parameter key="rrd-repository" value="/opt/opennms/share/rrd/response" />
-   <parameter key="rrd-base-name" value="http-${requisition:vhost}" /> (7)
-   <parameter key="ds-name" value="${service:name}" /> (8)
+   <parameter key="rrd-base-name" value="http-${requisition:vhost}" />
+   <parameter key="ds-name" value="${service:name}" />
 </service>
 
 <monitor service="Web-Site-Monitor" class-name="org.opennms.netmgt.poller.monitors.HttpsMonitor"/>(9)
