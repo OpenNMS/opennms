5f1481e9999a9f95373d6089c6902f410338cac8
Sean <Naicisum@users.noreply.github.com>
2021-11-13 00:35:07 +0800

    NMS-13716: Update Kafka dependencies to 3.0.0 (#3839)
    * NMS-13716: Update Kafka dependencies to 3.0.0
    
    * Update smoke test
    
    Kafka 2.8 no longer honors acks=1, it's acks=all by default
    
    * NMS-13716: Update smoke tests to use new version
    
    Co-authored-by: Chandra Gorantla <chandra@opennms.com>

--- features/kafka/producer/src/test/java/org/opennms/features/kafka/producer/KafkaForwarderIT.java
+++ features/kafka/producer/src/test/java/org/opennms/features/kafka/producer/KafkaForwarderIT.java
@@ -48,6 +48,7 @@ import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -573,7 +574,7 @@ public class KafkaForwarderIT implements TemporaryDatabaseAware<MockDatabase> {
                     }
                 }
             }
-            consumer.close(1, TimeUnit.MINUTES);
+            consumer.close(Duration.ofMinutes(1));
         }
 
         public AtomicInteger getNumRecordsConsumed() {
