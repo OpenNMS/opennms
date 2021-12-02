0db036dd284c4b235ee25a9e6acee341f86da5b2
psuthar-opennms <psuthar@opennms.com>
2021-12-02 17:51:43 +0000

    PE-147:[wmi-config.xml] - migrate config file to new CM

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/AlarmDaoIT.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/AlarmDaoIT.java
@@ -82,9 +82,9 @@ import com.google.common.collect.Sets;
 @ContextConfiguration(locations={
 		"classpath:/META-INF/opennms/applicationContext-soa.xml",
 		"classpath:/META-INF/opennms/applicationContext-dao.xml",
-		"classpath:/META-INF/opennms/applicationContext-mockConfigManager.xml",
+		"classpath:/META-INF/opennms/applicationContext-mockWmiConfigManager.xml",
 		"classpath:/META-INF/opennms/applicationContext-databasePopulator.xml",
-		"classpath*:/META-INF/opennms/component-dao.xml",
+		//"classpath*:/META-INF/opennms/component-dao.xml",
 		"classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
 		"classpath:/META-INF/opennms/applicationContext-minimal-conf.xml"
 })
