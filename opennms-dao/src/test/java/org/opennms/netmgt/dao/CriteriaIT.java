21bd0b182c759f179ec322a8b36cf80ac2e716f6
Upendra Guggilam <UGuggilam@nanthealth.com>
2021-12-02 20:17:38 -0500

    PE-147: Fixed dao openms-dao test cases

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/CriteriaIT.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/CriteriaIT.java
@@ -58,7 +58,7 @@ import org.springframework.transaction.annotation.Transactional;
         "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
         "classpath:/META-INF/opennms/applicationContext-minimal-conf.xml",
         "classpath:/META-INF/opennms/applicationContext-mockConfigManager.xml",
-        "classpath:/META-INF/opennms/applicationContext-mockWmiConfigManager.xml"
+        "classpath:/META-INF/opennms/applicationContext-mockWmiConfigManager.xml",
 })
 @JUnitConfigurationEnvironment
 @JUnitTemporaryDatabase(dirtiesContext=false)
