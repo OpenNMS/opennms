7501cc752681107abcb626de4109dbb6ea991ca7
Upendra Guggilam <UGuggilam@nanthealth.com>
2021-12-03 01:51:54 -0500

    PE-147 : Reverted addition of wmi-config.xml

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/support/JdbcFilterDaoIT.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/support/JdbcFilterDaoIT.java
@@ -49,7 +49,6 @@ import javax.sql.DataSource;
 import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.core.network.IPAddress;
@@ -87,7 +86,7 @@ import org.springframework.transaction.support.TransactionTemplate;
 @ContextConfiguration(locations={
         "classpath:/META-INF/opennms/applicationContext-soa.xml",
         "classpath:/META-INF/opennms/applicationContext-dao.xml",
-        "classpath:/META-INF/opennms/applicationContext-mockWmiConfigManager.xml",
+        "classpath:/META-INF/opennms/applicationContext-mockConfigManager.xml",
         "classpath:/META-INF/opennms/applicationContext-databasePopulator.xml",
         "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
         "classpath:/META-INF/opennms/applicationContext-minimal-conf.xml"
