2f4c43ea89aaa4e2aa49aa69c74dcf485eecd37c
Upendra Guggilam <UGuggilam@nanthealth.com>
2021-12-03 01:51:54 -0500

    PE-147 : Reverted addition of wmi-config.xml

--- integrations/opennms-rancid-provisioning-adapter/src/test/java/org/opennms/netmgt/provision/RancidProvisioningAdapterIntegrationIT.java
+++ integrations/opennms-rancid-provisioning-adapter/src/test/java/org/opennms/netmgt/provision/RancidProvisioningAdapterIntegrationIT.java
@@ -58,7 +58,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RunWith(OpenNMSJUnit4ClassRunner.class)
 @ContextConfiguration(locations= {
 		"classpath:/META-INF/opennms/applicationContext-soa.xml",
-        "classpath:/META-INF/opennms/applicationContext-mockWmiConfigManager.xml",
+        "classpath:/META-INF/opennms/applicationContext-mockConfigManager.xml",
 		"classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
 		"classpath:/META-INF/opennms/applicationContext-minimal-conf.xml",
 		"classpath:/META-INF/opennms/applicationContext-mockDao.xml",
