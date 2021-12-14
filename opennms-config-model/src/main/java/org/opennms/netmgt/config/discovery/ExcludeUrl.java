49e5b7d74a79aff8191708fb2384fbaeeaa7a173
Freddy Chu <fchu@opennms.com>
2021-12-14 16:22:17 -0500

    PE-188: handle new Discovery ExcludeUrl in CM

--- opennms-config-model/src/main/java/org/opennms/netmgt/config/discovery/ExcludeUrl.java
+++ opennms-config-model/src/main/java/org/opennms/netmgt/config/discovery/ExcludeUrl.java
@@ -1,25 +1,25 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
- * 
+ *
  * Copyright (C) 2017-2021 The OpenNMS Group, Inc.
  * OpenNMS(R) is Copyright (C) 1999-2021 The OpenNMS Group, Inc.
- * 
+ *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
- * 
+ *
  * OpenNMS(R) is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License,
  * or (at your option) any later version.
- * 
+ *
  * OpenNMS(R) is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with OpenNMS(R).  If not, see:
  *     http://www.gnu.org/licenses/
- * 
+ *
  * For more information contact:
  *     OpenNMS(R) Licensing <license@opennms.org>
  *     http://www.opennms.org/
@@ -29,18 +29,11 @@
 package org.opennms.netmgt.config.discovery;
 
 
+import org.opennms.netmgt.config.utils.ConfigUtils;
+
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.Optional;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlValue;
-
-import org.opennms.core.xml.ValidateUsing;
-import org.opennms.netmgt.config.utils.ConfigUtils;
 
 /**
  * A file URL holding specific addresses to be excluded. Each
@@ -49,65 +42,59 @@ import org.opennms.netmgt.config.utils.ConfigUtils;
  *  "#<comments>". Lines starting with a '#' are ignored and so are
  *  characters after a '<space>#' in a line.
  */
-@XmlRootElement(name = "exclude-url")
-@XmlAccessorType(XmlAccessType.FIELD)
-@ValidateUsing("discovery-configuration.xsd")
 public class ExcludeUrl implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
      * inner value
      */
-    @XmlValue
-    private String m_url;
+    private String url;
 
     /**
      * The monitoring location where this include URL
      *  will be executed.
      */
-    @XmlAttribute(name = "location")
-    private String m_location;
+    private String location;
 
-    @XmlAttribute(name = "foreign-source")
-    private String m_foreignSource;
+    private String foreignSource;
 
     public ExcludeUrl() {
     }
 
     public ExcludeUrl(final String url) {
-        m_url = url;
+        this.url = url;
     }
 
     public String getUrl() {
-        return m_url;
+        return url;
     }
 
     public void setUrl(final String url) {
-        m_url = ConfigUtils.assertNotEmpty(url, "URL");
+        this.url = ConfigUtils.assertNotEmpty(url, "URL");
     }
 
     public Optional<String> getLocation() {
-        return Optional.ofNullable(m_location);
+        return Optional.ofNullable(location);
     }
 
     public void setLocation(final String location) {
-        m_location = ConfigUtils.normalizeString(location);
-    }  
+        this.location = ConfigUtils.normalizeString(location);
+    }
 
     public Optional<String> getForeignSource() {
-        return Optional.ofNullable(m_foreignSource);
+        return Optional.ofNullable(foreignSource);
     }
 
     public void setForeignSource(final String foreignSource) {
-        m_foreignSource = ConfigUtils.normalizeString(foreignSource);
+        this.foreignSource = ConfigUtils.normalizeString(foreignSource);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(
-                            m_url, 
-                            m_location, 
-                            m_foreignSource);
+                            url,
+                            location,
+                            foreignSource);
     }
 
     @Override
@@ -118,17 +105,16 @@ public class ExcludeUrl implements Serializable {
 
         if (obj instanceof ExcludeUrl) {
             final ExcludeUrl temp = (ExcludeUrl)obj;
-            return Objects.equals(temp.m_url, m_url)
-                    && Objects.equals(temp.m_location, m_location)
-                    && Objects.equals(temp.m_foreignSource, m_foreignSource);
+            return Objects.equals(temp.url, url)
+                    && Objects.equals(temp.location, location)
+                    && Objects.equals(temp.foreignSource, foreignSource);
         }
         return false;
     }
 
     @Override
     public String toString() {
-        return "ExcludeUrl [value=" + m_url + ", location="
-                + m_location + ", foreignSource=" + m_foreignSource + "]";
+        return "ExcludeUrl [value=" + url + ", location="
+                + location + ", foreignSource=" + foreignSource + "]";
     }
-
 }
