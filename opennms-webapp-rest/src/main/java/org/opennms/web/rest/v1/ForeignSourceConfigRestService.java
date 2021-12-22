a02695783ab85db19af684b503463c8ea3913b9b
Yang Li <yali@opennms.com>
2021-12-22 13:39:08 -0500

    NMS-13745: enable detector plugin on the opennms web page (Default Foreign Source Definition FS Definition)

--- opennms-webapp-rest/src/main/java/org/opennms/web/rest/v1/ForeignSourceConfigRestService.java
+++ opennms-webapp-rest/src/main/java/org/opennms/web/rest/v1/ForeignSourceConfigRestService.java
@@ -106,6 +106,7 @@ public class ForeignSourceConfigRestService extends OnmsRestService implements I
 
         // The following is required, otherwise getWrappers() throws a NPE
         m_foreignSourceService.getPolicyTypes();
+        //m_foreignSourceService.getDetectorTypes();
     }
 
     /**
@@ -298,7 +299,7 @@ public class ForeignSourceConfigRestService extends OnmsRestService implements I
             SimplePluginConfig cfg = createPluginConfig(serviceName, clazz.getCanonicalName(), wrapper);
             plugins.add(cfg);
         }
-        plugins.getPlugins().sort((cfg0, cfg1) -> cfg0.name.compareTo(cfg1.name));
+        plugins.getPlugins().sort(Comparator.comparing(cfg0 -> cfg0.name));
         return plugins;
     }
 
@@ -310,7 +311,7 @@ public class ForeignSourceConfigRestService extends OnmsRestService implements I
             final Boolean required = paramEntry.getValue();
             final String paramName = paramEntry.getKey();
             final Set<String> options = required ? wrapper.getRequiredItems().get(paramName) : wrapper.getOptionalItems().get(paramName);
-            final List<String> optionList = new ArrayList<String>(options);
+            final List<String> optionList = new ArrayList<>(options);
             Collections.sort(optionList);
             SimplePluginParameter param = new SimplePluginParameter(paramName, required, optionList);
             if (required) {
