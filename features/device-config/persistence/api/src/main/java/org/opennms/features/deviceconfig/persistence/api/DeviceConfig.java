d15ca009bba0d74ede44def6a4a73740b3608037
Chandra Gorantla <chandra@opennms.com>
2022-01-05 09:53:22 -0500

    NMS-13775: Add device config table and DAO layer

---
+++ features/device-config/persistence/api/src/main/java/org/opennms/features/deviceconfig/persistence/api/DeviceConfig.java
@@ -0,0 +1,160 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.features.deviceconfig.persistence.api;
+
+import org.opennms.netmgt.model.OnmsIpInterface;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "device_config")
+public class DeviceConfig implements Serializable {
+
+    private static final long serialVersionUID = 1078656993339537763L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "deviceConfigSequence")
+    @SequenceGenerator(name = "deviceConfigSequence", sequenceName = "deviceconfignxtid")
+    @Column(name="id", nullable = false)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "interface_id")
+    private OnmsIpInterface ipInterface;
+
+    @Column(name = "version", nullable = false)
+    private Integer version;
+
+    @Lob
+    @Column(name = "config", nullable = false)
+    private byte[] config;
+
+    @Column(name = "encoding", nullable = false)
+    private String encoding;
+
+    @Column(name = "device_type")
+    private String deviceType;
+
+    @Column(name = "created_time", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public OnmsIpInterface getIpInterface() {
+        return ipInterface;
+    }
+
+    public void setIpInterface(OnmsIpInterface ipInterface) {
+        this.ipInterface = ipInterface;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public byte[] getConfig() {
+        return config;
+    }
+
+    public void setConfig(byte[] config) {
+        this.config = config;
+    }
+
+    public String getEncoding() {
+        return encoding;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DeviceConfig)) return false;
+        DeviceConfig that = (DeviceConfig) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(ipInterface, that.ipInterface) &&
+                Objects.equals(version, that.version) &&
+                Arrays.equals(config, that.config) &&
+                Objects.equals(encoding, that.encoding) &&
+                Objects.equals(deviceType, that.deviceType) &&
+                Objects.equals(createdTime, that.createdTime);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, ipInterface, version, encoding, deviceType, createdTime);
+        result = 31 * result + Arrays.hashCode(config);
+        return result;
+    }
+}
---
+++ features/device-config/persistence/impl/src/test/java/org/opennms/features/deviceconfig/persistence/DeviceConfigDaoIT.java
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.features.deviceconfig.persistence;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opennms.core.test.OpenNMSJUnit4ClassRunner;
+import org.opennms.core.test.db.annotations.JUnitTemporaryDatabase;
+import org.opennms.features.deviceconfig.persistence.api.DeviceConfig;
+import org.opennms.features.deviceconfig.persistence.api.DeviceConfigDao;
+import org.opennms.netmgt.dao.api.NodeDao;
+import org.opennms.netmgt.model.NetworkBuilder;
+import org.opennms.netmgt.model.OnmsIpInterface;
+import org.opennms.netmgt.model.OnmsNode;
+import org.opennms.test.JUnitConfigurationEnvironment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@RunWith(OpenNMSJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
+        "classpath:/META-INF/opennms/applicationContext-minimal-conf.xml",
+        "classpath:/META-INF/opennms/applicationContext-soa.xml",
+        "classpath:/META-INF/opennms/applicationContext-dao.xml",
+        "classpath*:/META-INF/opennms/component-dao.xml",
+        "classpath:/META-INF/opennms/mockEventIpcManager.xml"})
+@JUnitConfigurationEnvironment
+@JUnitTemporaryDatabase(reuseDatabase = false)
+public class DeviceConfigDaoIT {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private NodeDao nodeDao;
+
+    @Autowired
+    private DeviceConfigDao deviceConfigDao;
+
+    private OnmsIpInterface ipInterface;
+
+    @Before
+    public void init() {
+        ipInterface = populateIpInterfaceAndGet();
+    }
+
+    @Test
+    @Transactional
+    public void testPersistenceOfDeviceConfig() {
+        DeviceConfig deviceConfig = new DeviceConfig();
+        deviceConfig.setIpInterface(ipInterface);
+        deviceConfig.setConfig(UUID.randomUUID().toString().getBytes(StandardCharsets.UTF_8));
+        deviceConfig.setEncoding("ASCII");
+        deviceConfig.setCreatedTime(Date.from(Instant.now()));
+        deviceConfig.setDeviceType("Cisco-IOS");
+        deviceConfig.setVersion(1);
+        deviceConfigDao.saveOrUpdate(deviceConfig);
+        List<DeviceConfig> configs = deviceConfigDao.findAll();
+        Assert.assertThat(configs, Matchers.not(Matchers.empty()));
+        DeviceConfig retrieved = configs.get(0);
+        Assert.assertNotNull(retrieved);
+        Assert.assertEquals(ipInterface, retrieved.getIpInterface());
+        Assert.assertEquals(deviceConfig.getConfig(), retrieved.getConfig());
+    }
+
+    private OnmsIpInterface populateIpInterfaceAndGet() {
+        NetworkBuilder builder = new NetworkBuilder();
+        builder.addNode("node2").setForeignSource("imported:").setForeignId("2").setType(OnmsNode.NodeType.ACTIVE);
+        builder.addInterface("192.168.2.1").setIsManaged("M").setIsSnmpPrimary("P");
+        nodeDao.saveOrUpdate(builder.getCurrentNode());
+        Assert.assertThat(builder.getCurrentNode().getIpInterfaces(), Matchers.hasSize(1));
+        Set<OnmsIpInterface> ipInterfaces = builder.getCurrentNode().getIpInterfaces();
+        OnmsIpInterface ipInterface = ipInterfaces.iterator().next();
+        Assert.assertNotNull(ipInterface);
+        return ipInterface;
+    }
+
+}
