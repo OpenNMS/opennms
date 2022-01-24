c18959c88e50a034998e8431d48fd19fbf609e91
Stefan Wachter <stefan.wachter@gmx.de>
2022-01-24 20:39:59 +0100

    still not there

--- features/device-config/rest/api/src/main/java/org/opennms/features/deviceconfig/rest/api/DeviceConfigRestService.java
+++ features/device-config/rest/api/src/main/java/org/opennms/features/deviceconfig/rest/api/DeviceConfigRestService.java
@@ -28,20 +28,15 @@
 
 package org.opennms.features.deviceconfig.rest.api;
 
-import java.util.List;
-
 import javax.ws.rs.DELETE;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/device-config")
-@Produces(MediaType.APPLICATION_JSON)
 public interface DeviceConfigRestService {
 
     // paging, filter by ipaddressId, created time, device type
