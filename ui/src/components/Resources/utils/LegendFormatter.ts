053e476200664b1e5d7a2e2ae6e2946a54b548cc
Mike <mikewrosey@gmail.com>
2022-01-25 14:49:08 -0500

    custom time controls

--- ui/src/components/Resources/utils/LegendFormatter.ts
+++ ui/src/components/Resources/utils/LegendFormatter.ts
@@ -27,11 +27,15 @@ const formatTimestamp = (timestamp: number, formatStr: string) => {
 
   switch (formatStr) {
     case 'minutes':
-      return format(date, 'HH:mm')
+      return format(date, 'HH:mm:ss')
     case 'hours':
       return format(date, 'HH:mm')
     case 'days':
       return format(date, 'dd/MMM HH:mm')
+    case 'months':
+      return format(date, 'dd/MMM')
+    case 'years':
+      return format(date, 'MMM/y')
     default:
       return format(date, 'dd/MMM :HH:mm')
   }
