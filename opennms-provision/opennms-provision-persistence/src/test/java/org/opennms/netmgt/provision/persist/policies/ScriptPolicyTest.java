35d1673f34255f45612f27ce12837682c30d33eb
Benjamin Reed <ranger@opennms.org>
2022-01-31 11:49:38 -0500

    FixMethodOrder on ScriptPolicyTest

--- opennms-provision/opennms-provision-persistence/src/test/java/org/opennms/netmgt/provision/persist/policies/ScriptPolicyTest.java
+++ opennms-provision/opennms-provision-persistence/src/test/java/org/opennms/netmgt/provision/persist/policies/ScriptPolicyTest.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2018 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2018 The OpenNMS Group, Inc.
+ * Copyright (C) 2018-2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -40,7 +40,9 @@ import java.nio.file.Paths;
 import java.util.Collections;
 
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 import org.mockito.Mockito;
 import org.opennms.netmgt.dao.api.NodeDao;
 import org.opennms.netmgt.dao.mock.MockSessionUtils;
@@ -49,6 +51,7 @@ import org.opennms.netmgt.model.OnmsNode;
 import org.opennms.netmgt.model.PrimaryType;
 import org.opennms.netmgt.model.monitoringLocations.OnmsMonitoringLocation;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ScriptPolicyTest {
 
     @BeforeClass
