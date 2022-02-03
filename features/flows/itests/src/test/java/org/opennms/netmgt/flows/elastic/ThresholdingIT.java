a3744f5496892cfaf932552d73829e2f83d7c406
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2022-02-03 08:16:30 +0100

    NMS-13647: Fixed tests

--- features/flows/itests/src/test/java/org/opennms/netmgt/flows/elastic/ThresholdingIT.java
+++ features/flows/itests/src/test/java/org/opennms/netmgt/flows/elastic/ThresholdingIT.java
@@ -94,6 +94,7 @@ import io.searchbox.client.JestClient;
         "classpath:/META-INF/opennms/applicationContext-daemon.xml",
         "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
         "classpath:/META-INF/opennms/applicationContext-dao.xml",
+        "classpath:/META-INF/opennms/applicationContext-mockConfigManager.xml",
         "classpath:/META-INF/opennms/applicationContext-databasePopulator.xml",
         "classpath:/META-INF/opennms/applicationContext-shared.xml",
         "classpath:/META-INF/opennms/mockEventIpcManager.xml",
