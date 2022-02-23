f04df53140c01a1d1ae1af654e55acb301bdd394
yangli2014 <yangli2004@gmail.com>
2022-02-23 16:29:25 -0500

    NMS-13746: Allow OIA plugins to add menu items and components to the new Vue3 UI (#4270)
    * clean those code not ready to merge back
    
    * remove plugin mgmt, bump packages & test
    
    * Remove enable/disable ui extension feature for now
    
    * Made change based on review comments
    
    * include ui-extension bundle in the api-layer feature
    
    Co-authored-by: mikewrosey <mrose@nanthealth.com>

--- ui/src/store/index.ts
+++ ui/src/store/index.ts
@@ -16,6 +16,7 @@ import helpModule from './help'
 import notificationModule from './notification'
 import resourceModule from './resource'
 import graphModule from './graph'
+import pluginModule from './plugin'
 
 export default createStore({
   modules: {
@@ -33,6 +34,7 @@ export default createStore({
     helpModule,
     notificationModule,
     resourceModule,
-    graphModule
+    graphModule,
+    pluginModule
   }
 })
