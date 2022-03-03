869e81cadb813c3bdf81f41350dfb5442ab47d85
Alex May <amay@opennms.com>
2022-03-03 10:01:07 -0500

    NMS-13806: Removing uneccessary method per PR feedback

--- opennms-webapp-rest/src/main/java/org/opennms/web/rest/v1/RequisitionRestService.java
+++ opennms-webapp-rest/src/main/java/org/opennms/web/rest/v1/RequisitionRestService.java
@@ -452,7 +452,7 @@ public class RequisitionRestService extends OnmsRestService {
             LOG.debug("error validating incoming requisition with foreign source '{}'", requisition.getForeignSource(), e);
             throw getException(Status.BAD_REQUEST, e.getMessage());
         }
-        debug("addOrReplaceRequisition: Adding requisition {} (containing {} nodes)", requisition.getForeignSource(), requisition.getNodeCount());
+        LOG.debug("addOrReplaceRequisition: Adding requisition {} (containing {} nodes)", requisition.getForeignSource(), requisition.getNodeCount());
         m_accessService.addOrReplaceRequisition(requisition);
         return Response.accepted().header("Location", getRedirectUri(uriInfo, requisition.getForeignSource())).build();
     }
@@ -475,7 +475,7 @@ public class RequisitionRestService extends OnmsRestService {
             LOG.debug("error validating incoming node '{}'", node, e);
             throw getException(Status.BAD_REQUEST, e.getMessage());
         }
-        debug("addOrReplaceNode: Adding node {} to requisition {}", node.getForeignId(), foreignSource);
+        LOG.debug("addOrReplaceNode: Adding node {} to requisition {}", node.getForeignId(), foreignSource);
         m_accessService.addOrReplaceNode(foreignSource, node);
         return Response.accepted().header("Location", getRedirectUri(uriInfo, node.getForeignId())).build();
     }
@@ -500,7 +500,7 @@ public class RequisitionRestService extends OnmsRestService {
             LOG.debug("error validating incoming interface '{}'", iface, e);
             throw getException(Status.BAD_REQUEST, e.getMessage());
         }
-        debug("addOrReplaceInterface: Adding interface {} to node {}/{}", iface, foreignSource, foreignId);
+        LOG.debug("addOrReplaceInterface: Adding interface {} to node {}/{}", iface, foreignSource, foreignId);
         m_accessService.addOrReplaceInterface(foreignSource, foreignId, iface);
         return Response.accepted().header("Location", getRedirectUri(uriInfo, InetAddressUtils.str(iface.getIpAddr()))).build();
     }
@@ -525,7 +525,7 @@ public class RequisitionRestService extends OnmsRestService {
             LOG.debug("error validating incoming service '{}'", service, e);
             throw getException(Status.BAD_REQUEST, e.getMessage());
         }
-        debug("addOrReplaceService: Adding service {} to node {}/{}, interface {}", service.getServiceName(), foreignSource, foreignId, ipAddress);
+        LOG.debug("addOrReplaceService: Adding service {} to node {}/{}, interface {}", service.getServiceName(), foreignSource, foreignId, ipAddress);
         m_accessService.addOrReplaceService(foreignSource, foreignId, ipAddress, service);
         return Response.accepted().header("Location", getRedirectUri(uriInfo, service.getServiceName())).build();
     }
@@ -549,7 +549,7 @@ public class RequisitionRestService extends OnmsRestService {
             LOG.debug("error validating incoming category '{}'", category, e);
             throw getException(Status.BAD_REQUEST, e.getMessage());
         }
-        debug("addOrReplaceNodeCategory: Adding category {} to node {}/{}", category.getName(), foreignSource, foreignId);
+        LOG.debug("addOrReplaceNodeCategory: Adding category {} to node {}/{}", category.getName(), foreignSource, foreignId);
         m_accessService.addOrReplaceNodeCategory(foreignSource, foreignId, category);
         return Response.accepted().header("Location", getRedirectUri(uriInfo, category.getName())).build();
     }
@@ -573,7 +573,7 @@ public class RequisitionRestService extends OnmsRestService {
             LOG.debug("error validating incoming asset '{}'", asset, e);
             throw getException(Status.BAD_REQUEST, e.getMessage());
         }
-        debug("addOrReplaceNodeCategory: Adding asset {} to node {}/{}", asset.getName(), foreignSource, foreignId);
+        LOG.debug("addOrReplaceNodeCategory: Adding asset {} to node {}/{}", asset.getName(), foreignSource, foreignId);
         m_accessService.addOrReplaceNodeAssetParameter(foreignSource, foreignId, asset);
         return Response.accepted().header("Location", getRedirectUri(uriInfo, asset.getName())).build();
     }
@@ -588,7 +588,7 @@ public class RequisitionRestService extends OnmsRestService {
     @Path("{foreignSource}/import")
     @Transactional
     public Response importRequisition(@Context final UriInfo uriInfo, @PathParam("foreignSource") final String foreignSource, @QueryParam("rescanExisting") final String rescanExisting) {
-        debug("importRequisition: Importing requisition for foreign source {}", foreignSource);
+        LOG.debug("importRequisition: Importing requisition for foreign source {}", foreignSource);
         m_accessService.importRequisition(foreignSource, rescanExisting);
         return Response.accepted().header("Location", uriInfo.getBaseUriBuilder().path(this.getClass()).path(this.getClass(), "getRequisition").build(foreignSource)).build();
     }
@@ -750,9 +750,5 @@ public class RequisitionRestService extends OnmsRestService {
     public Response deleteAssetParameter(@PathParam("foreignSource") final String foreignSource, @PathParam("foreignId") final String foreignId, @PathParam("parameter") final String parameter) {
         m_accessService.deleteAssetParameter(foreignSource, foreignId, parameter);
         return Response.accepted().build();
-    }
-
-    void debug(final String format, final Object... values) {
-        LOG.debug(format, values);
     }
 }
