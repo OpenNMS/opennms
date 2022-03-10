5b67f532fc0243fd9fe9827cfe4c629eb9c5372f
Scott Theleman <synqotik@users.noreply.github.com>
2022-03-10 10:38:31 -0500

    NMS-13990: DeviceConfigBackup Rest API for downloading config data (#4341)
    * NMS-13990: Rest API for downloading device config data
    
    * increasing ulimit for tests
    
    Co-authored-by: Maxim Brener <mbrener@opennms.com>

---
+++ features/device-config/api/src/main/java/org/opennms/features/deviceconfig/service/DeviceConfigUtil.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.features.deviceconfig.service;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+
+import com.google.common.base.Strings;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class DeviceConfigUtil {
+
+    public static byte[] decompressGzipToBytes(byte[] source) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        try (GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(source))) {
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = gis.read(buffer)) > 0) {
+                output.write(buffer, 0, len);
+            }
+        }
+
+        return output.toByteArray();
+    }
+
+    public static byte[] tarGzipMultipleFiles(Map<String, byte[]> fileNameToDataMap) throws IOException {
+        if (fileNameToDataMap == null || fileNameToDataMap.isEmpty()) {
+            return null;
+        }
+
+        byte[] tarBytes = tarMultipleFiles(fileNameToDataMap);
+        var byteOutputStream = new ByteArrayOutputStream();
+
+        try (var gzipOut = new GzipCompressorOutputStream(byteOutputStream)) {
+            gzipOut.write(tarBytes);
+        }
+
+        return byteOutputStream.toByteArray();
+    }
+
+    public static Map<String, byte[]> unTarGzipMultipleFiles(byte[] tarGzipData) throws IOException {
+        if (tarGzipData == null) {
+            return new HashMap<>();
+        }
+
+        var result = new HashMap<String, byte[]>();
+        var byteArrayInputStream = new ByteArrayInputStream(tarGzipData);
+        var gzipInputStream = new GZIPInputStream(byteArrayInputStream);
+
+        try (var tarInputStream = new TarArchiveInputStream(gzipInputStream)) {
+            TarArchiveEntry tarEntry = null;
+            final int BUFLEN = 1024;
+            byte[] buffer = new byte[BUFLEN];
+
+            while ((tarEntry = tarInputStream.getNextTarEntry()) != null) {
+                if (tarEntry.isFile()) {
+                    String fileName = tarEntry.getName();
+
+                    var byteOutput = new ByteArrayOutputStream();
+
+                    try (var bufOut = new BufferedOutputStream(byteOutput)) {
+                        int len = 0;
+
+                        while ((len = tarInputStream.read(buffer)) != -1) {
+                            bufOut.write(buffer, 0, len);
+                        }
+                    }
+
+                    byte[] bytesRead = byteOutput.toByteArray();
+                    result.put(fileName, bytesRead);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static byte[] tarMultipleFiles(Map<String, byte[]> fileNameToDataMap) throws IOException {
+        var byteOutputStream = new ByteArrayOutputStream();
+
+        try (var tarOut = new TarArchiveOutputStream(byteOutputStream)) {
+            for (Map.Entry<String, byte[]> entry : fileNameToDataMap.entrySet()) {
+                if (!Strings.isNullOrEmpty(entry.getKey()) && entry.getValue() != null) {
+                    var archiveEntry = new TarArchiveEntry(entry.getKey());
+                    archiveEntry.setName(entry.getKey());
+                    archiveEntry.setSize(entry.getValue().length);
+
+                    tarOut.putArchiveEntry(archiveEntry);
+                    tarOut.write(entry.getValue());
+                    tarOut.closeArchiveEntry();
+                }
+            }
+        }
+
+        return byteOutputStream.toByteArray();
+    }
+}
--- features/device-config/rest/src/main/java/org/opennms/features/deviceconfig/rest/impl/DefaultDeviceConfigRestService.java
+++ features/device-config/rest/src/main/java/org/opennms/features/deviceconfig/rest/impl/DefaultDeviceConfigRestService.java
@@ -28,16 +28,26 @@
 
 package org.opennms.features.deviceconfig.rest.impl;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import javax.ws.rs.PathParam;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.google.common.base.Strings;
 import org.apache.commons.lang3.StringUtils;
@@ -51,6 +61,7 @@ import org.opennms.features.deviceconfig.rest.BackupRequestDTO;
 import org.opennms.features.deviceconfig.rest.api.DeviceConfigDTO;
 import org.opennms.features.deviceconfig.rest.api.DeviceConfigRestService;
 import org.opennms.features.deviceconfig.service.DeviceConfigService;
+import org.opennms.features.deviceconfig.service.DeviceConfigUtil;
 import org.opennms.netmgt.dao.api.MonitoredServiceDao;
 import org.opennms.netmgt.model.OnmsIpInterface;
 import org.opennms.netmgt.model.OnmsMetaData;
@@ -97,6 +108,26 @@ public class DefaultDeviceConfigRestService implements DeviceConfigRestService {
         this.deviceConfigService = deviceConfigService;
     }
 
+    /** {@inheritDoc} */
+    @Override
+    public Response getDeviceConfig(long id) {
+        final var dc = deviceConfigDao.get(id);
+
+        if (dc == null) {
+            return Response.noContent().build();
+        }
+
+        DeviceConfigDTO dto = createDeviceConfigDto(dc);
+        final List<DeviceConfigDTO> dtos = List.of(dto);
+
+        final Map<Integer, List<OnmsMonitoredService>> serviceMap = getServiceMapForDTOs(dtos);
+        populateScheduleInfo(dtos, serviceMap);
+        dto = dtos.get(0);
+
+        return Response.ok(dto).build();
+    }
+
+    /** {@inheritDoc} */
     @Override
     public Response getDeviceConfigs(
             Integer limit,
@@ -113,21 +144,12 @@ public class DefaultDeviceConfigRestService implements DeviceConfigRestService {
         var criteria = getCriteria(limit, offset, orderBy, order, deviceName, ipAddress, ipInterfaceId, configType, createdAfter, createdBefore);
 
         // find DeviceConfig items in device_config database as per filter/sort criteria
-        List<DeviceConfig> deviceConfigs = deviceConfigDao.findMatching(criteria);
+        final List<DeviceConfig> deviceConfigs = deviceConfigDao.findMatching(criteria);
 
         // do initial conversion to DTO with DeviceConfig data
-        List<DeviceConfigDTO> dtos = deviceConfigs.stream().map(DefaultDeviceConfigRestService::createDeviceConfigDto).collect(Collectors.toList());
+        final List<DeviceConfigDTO> dtos = deviceConfigs.stream().map(DefaultDeviceConfigRestService::createDeviceConfigDto).collect(Collectors.toList());
 
-        // Get ipInterface Ids for current set of device configs
-        List<Integer> ipInterfaceIds = dtos.stream().map(d -> d.getIpInterfaceId()).distinct().collect(Collectors.toList());
-
-        // Get the services matching the above device config records
-        List<OnmsMonitoredService> services = getDeviceConfigServices(ipInterfaceIds);
-
-        // Get a list of services per ipInterfaceId so we can correlate with DeviceConfig data
-        Map<Integer, List<OnmsMonitoredService>> serviceMap =
-            services.stream().collect(Collectors.groupingBy(OnmsMonitoredService::getIpInterfaceId));
-
+        final Map<Integer, List<OnmsMonitoredService>> serviceMap = getServiceMapForDTOs(dtos);
         populateScheduleInfo(dtos, serviceMap);
 
         if (limit != null || offset != null) {
@@ -145,11 +167,162 @@ public class DefaultDeviceConfigRestService implements DeviceConfigRestService {
         }
     }
 
+    /** {@inheritDoc} */
+    @Override
+    public void deleteDeviceConfig(long id) {
+        deviceConfigDao.delete(id);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Response downloadDeviceConfig(String id) {
+        LOG.debug("In downloadDeviceConfig");
+
+        if (Strings.isNullOrEmpty(id)) {
+            LOG.debug("empty or null id supplied by request");
+            return Response.noContent().build();
+        }
+
+        final String idParamPattern = "\\d+(, ?\\d+)*";
+        var pattern = Pattern.compile(idParamPattern);
+        var matcher = pattern.matcher(id);
+
+        if (!matcher.matches()) {
+            LOG.debug("invalid id param supplied by request");
+            return Response.status(Status.BAD_REQUEST).entity("Invalid 'id' parameter").build();
+        }
+
+        List<Long> ids = Arrays.stream(id.split(","))
+            .filter(s -> !Strings.isNullOrEmpty(s))
+            .map(s -> Long.parseLong(s))
+            .collect(Collectors.toList());
+
+        if (ids.isEmpty()) {
+            LOG.debug("no ids supplied by request");
+            return Response.noContent().build();
+        }
+
+        if (ids.size() == 1) {
+            return downloadSingleDeviceConfig(ids.get(0));
+        } else {
+            return downloadMultipleDeviceCofigs(ids);
+        }
+    }
+
+    private Response downloadSingleDeviceConfig(Long id) {
+        final DeviceConfig dc = deviceConfigDao.get(id);
+
+        if (dc == null || dc.getConfig() == null || dc.getConfig().length == 0) {
+            LOG.debug("could not find device config data for id: {}", id);
+            return Response.noContent().build();
+        }
+
+        final var outputStream = new ByteArrayOutputStream();
+        outputStream.write(dc.getConfig(), 0, dc.getConfig().length);
+
+        final String fileName = createDownloadFileName(dc);
+
+        // TODO: Update MIME type, charset based on 'encoding'
+        return Response.ok().type("text/plain;charset=UTF-8")
+            .header("Content-Disposition", "inline; filename=" + fileName)
+            .header("Pragma", "public")
+            .header("Cache-Control", "cache")
+            .header("Cache-Control", "must-revalidate")
+            .entity(outputStream.toByteArray()).build();
+    }
+
+    private Response downloadMultipleDeviceCofigs(List<Long> ids) {
+        final Map<String, byte[]> fileNameToDataMap = ids.stream()
+            .map(id -> deviceConfigDao.get(id))
+            .filter(dc -> dc != null && dc.getConfig() != null && dc.getConfig().length > 0)
+            .collect(Collectors.toMap(dc -> createDownloadFileName(dc), dc -> dc.getConfig()));
+
+        if (fileNameToDataMap.isEmpty()) {
+            LOG.debug("no config data found for request");
+            return Response.noContent().build();
+        }
+
+        byte[] gzipBytes = null;
+
+        try {
+            gzipBytes = DeviceConfigUtil.tarGzipMultipleFiles(fileNameToDataMap);
+        } catch (IOException e) {
+            var message = "Error compressing multiple files for download.";
+            LOG.error(message, e);
+            throw getException(Status.INTERNAL_SERVER_ERROR, message);
+        }
+
+        // TODO: better file name; check if all same node then use common nodelabel; add timestamp
+        final var currentTime = new Date();
+        final var formatter = new SimpleDateFormat("YYYYMMdd-HHmmss");
+        final String timestamp = formatter.format(currentTime);
+        final String fileName = "device-configs-" + timestamp + ".tar.gz";
+
+        return Response.ok().type("application/gzip")
+            .header("Content-Disposition", "inline; filename=" + fileName)
+            .header("Pragma", "public")
+            .header("Cache-Control", "cache")
+            .header("Cache-Control", "must-revalidate")
+            .entity(gzipBytes).build();
+    }
+
+    public static Optional<String> getScheduleValue(final List<OnmsMonitoredService> serviceList, final String serviceName) {
+        final var matchingService =
+            serviceList.stream()
+                .filter(x -> x.getServiceName().equalsIgnoreCase(serviceName))
+                .findFirst();
+
+        if (matchingService.isPresent()) {
+            return getScheduleMetaDataValue(matchingService.get().getMetaData());
+        }
+
+        return Optional.empty();
+    }
+
+    public static Optional<String> getScheduleMetaDataValue(final List<OnmsMetaData> metaData) {
+        final var entry = metaData.stream()
+            .filter(m -> m.getContext().equals(REQUISITION_CONTEXT) && m.getKey().equals((SCHEDULE_METADATA_KEY)))
+            .map(m -> m.getValue())
+            .findFirst();
+
+        return entry;
+    }
+
+    public static String createDownloadFileName(
+        final String deviceName, String ipAddress, String configType, Date createdTime) {
+        final var formatter = new SimpleDateFormat("YYYYMMdd-HHmmss");
+
+        final var items = List.of(
+            !Strings.isNullOrEmpty(deviceName) ? deviceName : "device",
+            !Strings.isNullOrEmpty(ipAddress) ? ipAddress.replace('.', '_') : "ipaddress",
+            !Strings.isNullOrEmpty(configType) ? configType : "default",
+            formatter.format(createdTime)
+        );
+
+        final String fileName = String.join("-", items) + ".cfg";
+
+        return fileName;
+    }
+
     /** Get DeviceConfig-related OnmsMonitoredServices for the given ip interface ids. */
     private List<OnmsMonitoredService> getDeviceConfigServices(List<Integer> ipInterfaceIds) {
-        List<OnmsMonitoredService> services = monitoredServiceDao.findByServiceTypeAndIpInterfaceId(DEVICE_CONFIG_SERVICE_PREFIX, ipInterfaceIds);
+        final List<OnmsMonitoredService> services = monitoredServiceDao.findByServiceTypeAndIpInterfaceId(DEVICE_CONFIG_SERVICE_PREFIX, ipInterfaceIds);
 
         return services;
+    }
+
+    private Map<Integer, List<OnmsMonitoredService>> getServiceMapForDTOs(List<DeviceConfigDTO> dtos) {
+        // Get ipInterface Ids for DTOs
+        final List<Integer> ipInterfaceIds = dtos.stream().map(d -> d.getIpInterfaceId()).distinct().collect(Collectors.toList());
+
+        // Get the services matching the above device config records
+        final List<OnmsMonitoredService> services = getDeviceConfigServices(ipInterfaceIds);
+
+        // Get a list of services per ipInterfaceId so we can correlate with DeviceConfig data
+        final Map<Integer, List<OnmsMonitoredService>> serviceMap =
+            services.stream().collect(Collectors.groupingBy(OnmsMonitoredService::getIpInterfaceId));
+
+        return serviceMap;
     }
 
     /**
@@ -178,56 +351,12 @@ public class DefaultDeviceConfigRestService implements DeviceConfigRestService {
         }
     }
 
-    public static Optional<String> getScheduleValue(final List<OnmsMonitoredService> serviceList, final String serviceName) {
-        final var matchingService =
-            serviceList.stream()
-            .filter(x -> x.getServiceName().equalsIgnoreCase(serviceName))
-            .findFirst();
-
-        if (matchingService.isPresent()) {
-            return getScheduleMetaDataValue(matchingService.get().getMetaData());
-        }
-
-        return Optional.empty();
-    }
-
-    public static Optional<String> getScheduleMetaDataValue(final List<OnmsMetaData> metaData) {
-        final var entry = metaData.stream()
-            .filter(m -> m.getContext().equals(REQUISITION_CONTEXT) && m.getKey().equals((SCHEDULE_METADATA_KEY)))
-            .map(m -> m.getValue())
-            .findFirst();
-
-        return entry;
-    }
-
     private ScheduleInfo getScheduleInfo(Date current, String schedulePattern) {
         // TODO: parse out cron-style schedulePattern and determine values
-
         final Date nextScheduledBackup = Date.from(current.toInstant().plus(Duration.ofHours(25)));
         final String scheduleInterval = schedulePattern;
 
         return new ScheduleInfo(nextScheduledBackup, scheduleInterval);
-    }
-
-    @Override
-    public DeviceConfigDTO getDeviceConfig(long id) {
-        var dc = deviceConfigDao.get(id);
-        return createDeviceConfigDto(dc);
-    }
-
-    @Override
-    public void deleteDeviceConfig(long id) {
-        deviceConfigDao.delete(id);
-    }
-
-    @Override
-    public Response downloadDeviceConfig(@PathParam("id") long id) {
-        throw new UnsupportedOperationException("downloadDeviceConfig not yet implemented.");
-    }
-
-    @Override
-    public Response downloadDeviceConfigs() {
-        throw new UnsupportedOperationException("downloadDeviceConfigs not yet implemented.");
     }
 
     @Override
@@ -306,6 +435,10 @@ public class DefaultDeviceConfigRestService implements DeviceConfigRestService {
         return criteriaBuilder.toCriteria();
     }
 
+    private WebApplicationException getException(final Status status, final String message) {
+        return new WebApplicationException(Response.status(status).type(MediaType.TEXT_PLAIN).entity(message).build());
+    }
+
     private static DeviceConfigDTO createDeviceConfigDto(DeviceConfig deviceConfig) {
         var dto = new DeviceConfigDTO(
             deviceConfig.getId(),
@@ -321,8 +454,13 @@ public class DefaultDeviceConfigRestService implements DeviceConfigRestService {
             deviceConfig.getFailureReason()
         );
 
-        OnmsIpInterface ipInterface = deviceConfig.getIpInterface();
-        OnmsNode node = ipInterface.getNode();
+        // determine backup status, not handling all cases for now
+        boolean backupSuccess = determineBackupSuccess(deviceConfig);
+        dto.setIsSuccessfulBackup(backupSuccess);
+        dto.setBackupStatus(backupSuccess ? "success" : "failure");
+
+        final OnmsIpInterface ipInterface = deviceConfig.getIpInterface();
+        final OnmsNode node = ipInterface.getNode();
 
         dto.setNodeId(node.getId());
         dto.setNodeLabel(node.getLabel());
@@ -331,5 +469,20 @@ public class DefaultDeviceConfigRestService implements DeviceConfigRestService {
         dto.setOperatingSystem(node.getOperatingSystem());
 
         return dto;
+    }
+
+    private static boolean determineBackupSuccess(DeviceConfig deviceConfig) {
+        return
+            deviceConfig.getLastSucceeded() != null &&
+            (deviceConfig.getLastSucceeded().getTime() == deviceConfig.getLastUpdated().getTime() ||
+             deviceConfig.getLastSucceeded().after(deviceConfig.getLastUpdated()));
+    }
+
+    private static String createDownloadFileName(DeviceConfig dc) {
+        OnmsIpInterface ipInterface = dc.getIpInterface();
+        String ipAddress = InetAddressUtils.str(ipInterface.getIpAddress());
+        String deviceName = ipInterface.getNode().getLabel();
+
+        return createDownloadFileName(deviceName, ipAddress, dc.getConfigType(), dc.getCreatedTime());
     }
 }
