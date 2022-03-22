3df3fc6bb91e1df0bcd523d056ed47b8e47db8ed
chinh le <cle@opennms.com>
2022-03-22 08:58:45 -0400

    PE-208 Typo - cleanup-work#23

--- ui/src/components/Configuration/copy/requisitionTypes.ts
+++ ui/src/components/Configuration/copy/requisitionTypes.ts
@@ -84,7 +84,7 @@ export const requisitionTypeList = [
   },
   /**
    * Cleanup-work #23 - Remove all Requisition Plugin items from the UI for H30
-   * Remove temporary the Requisition Plugin form the list. It will be put back
+   * Remove temporary the Requisition Plugin from the list. It will be put back
    * once the plugins are ready to real use.
    */
   /* {
