50728683deafba77a051c09a4d84b899c8312d18
Dustin Frisch <dustin@opennms.com>
2022-04-06 00:34:45 +0200

    NMS-14012: Fix service name assignment

--- features/device-config/rest/src/main/java/org/opennms/features/deviceconfig/rest/impl/DefaultDeviceConfigRestService.java
+++ features/device-config/rest/src/main/java/org/opennms/features/deviceconfig/rest/impl/DefaultDeviceConfigRestService.java
@@ -443,8 +443,7 @@ public class DefaultDeviceConfigRestService implements DeviceConfigRestService {
 
         var dto = new DeviceConfigDTO();
         dto.setId(queryResult.getId());
-        dto.setIpInterfaceId(queryResult.getIpInterfaceId());
-        dto.setMonitoredServiceId(queryResult.getMonitoredServiceId());
+        dto.setServiceName(queryResult.getServiceName());
         dto.setIpAddress(queryResult.getIpAddr());
         dto.setLastBackupDate(queryResult.getCreatedTime());
         dto.setLastUpdatedDate(queryResult.getLastUpdated());
@@ -479,8 +478,7 @@ public class DefaultDeviceConfigRestService implements DeviceConfigRestService {
 
         var dto = new DeviceConfigDTO();
         dto.setId(deviceConfig.getId());
-        dto.setIpInterfaceId(deviceConfig.getIpInterface().getId());
-        dto.setMonitoredServiceId(deviceConfig.getIpInterface().getMonitoredServiceByServiceType(deviceConfig.getServiceName()).getId());
+        dto.setServiceName(deviceConfig.getServiceName());
         dto.setIpAddress(InetAddressUtils.str(deviceConfig.getIpInterface().getIpAddress()));
         dto.setLastBackupDate(deviceConfig.getCreatedTime());
         dto.setLastUpdatedDate(deviceConfig.getLastUpdated());
