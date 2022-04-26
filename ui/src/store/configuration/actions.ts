63840ee6f9ecd82b0b865e2cc17e2971df4bcdd2
mikewrosey <mrose@nanthealth.com>
2022-04-26 15:06:57 -0400

    add missing parentheses

--- ui/src/store/configuration/actions.ts
+++ ui/src/store/configuration/actions.ts
@@ -2,7 +2,7 @@ import { VuexContext } from '@/types'
 import API from '@/services'
 
 const getProvisionDService = async (context: VuexContext) => {
-  const resp = await API.getProvisionDService
+  const resp = await API.getProvisionDService()
   if (resp) {
     context.commit('SAVE_PROVISION_SERVICE', resp)
   }
