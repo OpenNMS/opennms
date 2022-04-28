299d6c26c8b2f1e371437f160ba57ad45b6af4c6
Alexander Chadfield <achadfield@nanthealth.com>
2022-04-28 15:09:36 -0400

    test of different requests

--- smoke-test/src/test/java/org/opennms/smoketest/HttpsIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/HttpsIT.java
@@ -28,20 +28,49 @@
 
 package org.opennms.smoketest;
 
+import io.restassured.RestAssured;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Call;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.opennms.smoketest.containers.OpenNMSContainer;
+import org.opennms.smoketest.selenium.AbstractOpenNMSSeleniumHelper;
+import org.opennms.smoketest.stacks.MinionProfile;
 import org.opennms.smoketest.stacks.OpenNMSProfile;
 import org.opennms.smoketest.stacks.OpenNMSStack;
 import org.opennms.smoketest.stacks.StackModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import okhttp3.Call;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
@@ -50,10 +79,13 @@ import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.security.KeyManagementException;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeUnit;
+
+import static io.restassured.RestAssured.given;
 
 
 public class HttpsIT {
@@ -74,6 +106,9 @@ public class HttpsIT {
     @Before
     public void setUp() {
         LOG.info("Set up of the test");
+        RestAssured.baseURI = STACK.opennms().getBaseUrlExternal().toString();
+        RestAssured.port = STACK.opennms().getWebPort();
+        RestAssured.basePath = "/opennms/";
     }
 
 
@@ -86,6 +121,84 @@ public class HttpsIT {
      * This test will open the page over HTTPS accepting any self-signed certificate which will allow us to verify the page is opened.
      * Test will confirm response 200, Username and Password fields.
      */
+    @Test
+    public void verifyHTTPSConnection() {
+        LOG.info("Verify that the test itself works fine. Empty body.");
+
+        try {
+            // This TrustStrategy will make sure that we do not care about Self Signed Cert (true)
+            TrustStrategy acceptingTrustStrategy = (x509Certificates, s) -> true;
+            LOG.info("Trust Strategy is ready");
+
+            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext,
+                    NoopHostnameVerifier.INSTANCE);
+
+            LOG.info("Before Registry");
+            Registry<ConnectionSocketFactory> socketFactoryRegistry =
+                    RegistryBuilder.<ConnectionSocketFactory>create()
+                            .register("https", sslsf)
+                            .register("http", new PlainConnectionSocketFactory())
+                            .build();
+
+            LOG.info("Before BasicHTTP");
+            BasicHttpClientConnectionManager connectionManager =
+                    new BasicHttpClientConnectionManager(socketFactoryRegistry);
+            CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf)
+                    .setConnectionManager(connectionManager).build();
+
+            LOG.info("Before getting a response");
+            HttpComponentsClientHttpRequestFactory requestFactory =
+                    new HttpComponentsClientHttpRequestFactory(httpClient);
+
+            LOG.info("Getting response from the server");
+            ResponseEntity<String> response = new RestTemplate(requestFactory)
+                    .exchange(urlOverHttps, HttpMethod.GET, null, String.class);
+
+            LOG.info("Before assert");
+            Assert.assertEquals(response.getStatusCode().value(), 200);
+            Assert.assertTrue(response.getBody().contains("Username"));
+            Assert.assertTrue(response.getBody().contains("Password"));
+
+            LOG.info("we have reached the end of the test");
+
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            LOG.error(e.toString());
+        }
+
+    }
+
+    @Test
+    public void verifyHttp() {
+        String json  = given()
+                .auth().basic(AbstractOpenNMSSeleniumHelper.BASIC_AUTH_USERNAME, AbstractOpenNMSSeleniumHelper.BASIC_AUTH_PASSWORD)
+                .get("rest/info")
+                .then().statusCode(200)
+                .extract().response().body().print();
+
+        LOG.info(json.toString());
+    }
+
+    @Test
+    public void simpleCode() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("http://localhost:8980").get().build();
+        okhttp3.Response resp = client.newCall(request).execute();
+        LOG.info(resp.body().toString());
+    }
+
+    @Test
+    public void simpleHTTPSCode() throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url("https://localhost:8443").get().build();
+        okhttp3.Response resp = client.newCall(request).execute();
+        LOG.info(resp.body().toString());
+    }
+
     @Test
     public void okHttpTest() throws KeyManagementException, NoSuchAlgorithmException, IOException {
         OkHttpClient client = new OkHttpClient();
