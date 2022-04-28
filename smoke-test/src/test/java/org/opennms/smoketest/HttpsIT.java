50e6e84630ebe8b482b600da05e5ecfa26762fbd
Alexander Chadfield <achadfield@nanthealth.com>
2022-04-28 10:58:04 -0400

    test changed to use okhttp3 instead of spring

--- smoke-test/src/test/java/org/opennms/smoketest/HttpsIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/HttpsIT.java
@@ -28,17 +28,9 @@
 
 package org.opennms.smoketest;
 
-import org.apache.http.config.Registry;
-import org.apache.http.config.RegistryBuilder;
-import org.apache.http.conn.socket.ConnectionSocketFactory;
-import org.apache.http.conn.socket.PlainConnectionSocketFactory;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.BasicHttpClientConnectionManager;
-import org.apache.http.ssl.SSLContexts;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Call;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -50,15 +42,18 @@ import org.opennms.smoketest.stacks.OpenNMSStack;
 import org.opennms.smoketest.stacks.StackModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.io.IOException;
 import java.security.KeyManagementException;
-import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.concurrent.TimeUnit;
 
 
 public class HttpsIT {
@@ -92,50 +87,54 @@ public class HttpsIT {
      * Test will confirm response 200, Username and Password fields.
      */
     @Test
-    public void verifyHTTPSConnection() {
-        LOG.info("Verify that the test itself works fine. Empty body.");
+    public void okHttpTest() throws KeyManagementException, NoSuchAlgorithmException, IOException {
+        OkHttpClient client = new OkHttpClient();
+        OkHttpClient.Builder clientBuilder = client.newBuilder().readTimeout(6000, TimeUnit.SECONDS);
 
-        try {
-            // This TrustStrategy will make sure that we do not care about Self Signed Cert (true)
-            TrustStrategy acceptingTrustStrategy = (x509Certificates, s) -> true;
-            LOG.info("Trust Strategy is ready");
+        LOG.info("**** Allow untrusted SSL connection ****");
+        final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                X509Certificate[] cArrr = new X509Certificate[0];
+                return cArrr;
+            }
 
-            SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
-            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext,
-                    NoopHostnameVerifier.INSTANCE);
+            @Override
+            public void checkServerTrusted(final X509Certificate[] chain,
+                                           final String authType) throws CertificateException {
+            }
 
-            LOG.info("Before Registry");
-            Registry<ConnectionSocketFactory> socketFactoryRegistry =
-                    RegistryBuilder.<ConnectionSocketFactory>create()
-                            .register("https", sslsf)
-                            .register("http", new PlainConnectionSocketFactory())
-                            .build();
+            @Override
+            public void checkClientTrusted(final X509Certificate[] chain,
+                                           final String authType) throws CertificateException {
+            }
+        }};
 
-            LOG.info("Before BasicHTTP");
-            BasicHttpClientConnectionManager connectionManager =
-                    new BasicHttpClientConnectionManager(socketFactoryRegistry);
-            CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf)
-                    .setConnectionManager(connectionManager).build();
+        SSLContext sslContext = SSLContext.getInstance("SSL");
 
-            LOG.info("Before getting a response");
-            HttpComponentsClientHttpRequestFactory requestFactory =
-                    new HttpComponentsClientHttpRequestFactory(httpClient);
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        clientBuilder.sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager) trustAllCerts[0]);
 
-            LOG.info("Getting response from the server");
-            ResponseEntity<String> response = new RestTemplate(requestFactory)
-                    .exchange(urlOverHttps, HttpMethod.GET, null, String.class);
+        HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
 
-            LOG.info("Before assert");
-            Assert.assertEquals(response.getStatusCode().value(), 200);
-            Assert.assertTrue(response.getBody().contains("Username"));
-            Assert.assertTrue(response.getBody().contains("Password"));
+        clientBuilder.hostnameVerifier(hostnameVerifier);
 
-            LOG.info("we have reached the end of the test");
+        LOG.info("Before getting a request");
+        Request request = new Request.Builder().url(urlOverHttps).get().build();
 
-        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
-            LOG.error(e.toString());
-        }
+        final Call call = clientBuilder.build().newCall(request);
+        LOG.info("Before getting a response");
+        okhttp3.Response response = call.execute().networkResponse();
 
+        LOG.info("Before assert");
+        Assert.assertEquals(response.code(), 200);
+
+        LOG.info("we have reached the end of the test");
     }
 
 }
