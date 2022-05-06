e6cac419a788a9b5df1a58d526aa76d5e33a2a2f
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2022-05-06 11:31:24 +0200

    NMS-14244: Added DCB to RTC dashboard

--- opennms-services/src/test/java/org/opennms/netmgt/rtc/AvailabilityServiceIT.java
+++ opennms-services/src/test/java/org/opennms/netmgt/rtc/AvailabilityServiceIT.java
@@ -34,6 +34,9 @@ import static org.mockito.Mockito.when;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -162,6 +165,85 @@ public class AvailabilityServiceIT implements TemporaryDatabaseAware<MockDatabas
         // or the number of services in the category changes
         assertEquals(RTCUtils.getOutagePercentage(1800000, 86400000, 6), category.getCatvalue(), 0.0001);
         assertEquals(2, category.getNode().size());
+    }
+
+    @Test
+    @Transactional
+    public void compareServiceNamesAndRegExp() {
+        final MockNetwork mockNetwork = new MockNetwork();
+        mockNetwork.createStandardNetwork();
+        m_mockDatabase.populate(mockNetwork);
+
+        final RTCCategory rtcCat1 = mock(RTCCategory.class);
+        when(rtcCat1.getLabel()).thenReturn("NOC1");
+        when(rtcCat1.getServices()).thenReturn(Lists.newArrayList("SMTP", "HTTP"));
+        when(rtcCat1.getNodes()).thenReturn(Lists.newArrayList(1, 2));
+
+        final RTCCategory rtcCat2 = mock(RTCCategory.class);
+        when(rtcCat2.getLabel()).thenReturn("NOC2");
+        when(rtcCat2.getServices()).thenReturn(Lists.newArrayList("~[HS].TP"));
+        when(rtcCat2.getNodes()).thenReturn(Lists.newArrayList(1, 2));
+
+        EuiLevel euiLevel1 = m_availabilityService.getEuiLevel(rtcCat1);
+        EuiLevel euiLevel2 = m_availabilityService.getEuiLevel(rtcCat2);
+
+        assertEquals(1, euiLevel1.getCategory().size());
+        assertEquals(100.0, euiLevel1.getCategory().get(0).getCatvalue(), 0.001);
+        assertEquals(2, euiLevel1.getCategory().get(0).getNode().size());
+
+        compareEuiLevel(euiLevel1, euiLevel2);
+
+        final Date now = new Date();
+        final Date oneHourAgo = new Date(now.getTime() - (60 * 60 * 1000));
+        final Date thirtyMinutesAgo = new Date(now.getTime() - (30 * 60 * 1000));
+        final Date fifteenMinutesAgo = new Date(now.getTime() - (15 * 60 * 1000));
+
+        final OnmsMonitoredService smtpService = toMonitoredService(mockNetwork.getService(1, "192.168.1.2", "SMTP"));
+        final OnmsOutage outage1 = new OnmsOutage();
+        outage1.setMonitoredService(smtpService);
+        outage1.setIfLostService(oneHourAgo);
+        outage1.setIfRegainedService(thirtyMinutesAgo);
+        m_outageDao.save(outage1);
+        m_outageDao.flush();
+
+        final OnmsMonitoredService httpService = toMonitoredService(mockNetwork.getService(2, "192.168.1.3", "HTTP"));
+        final OnmsOutage outage2 = new OnmsOutage();
+        outage2.setMonitoredService(httpService);
+        outage2.setIfLostService(fifteenMinutesAgo);
+        outage2.setIfRegainedService(now);
+        m_outageDao.save(outage2);
+        m_outageDao.flush();
+
+        euiLevel1 = m_availabilityService.getEuiLevel(rtcCat1);
+        euiLevel2 = m_availabilityService.getEuiLevel(rtcCat2);
+
+        compareEuiLevel(euiLevel1, euiLevel2);
+    }
+
+    private void compareEuiLevel(final EuiLevel e1, final EuiLevel e2) {
+        assertEquals(1, e1.getCategory().size());
+        assertEquals(1, e2.getCategory().size());
+
+        final Category c1 = e1.getCategory().get(0);
+        final Category c2 = e2.getCategory().get(0);
+
+        assertEquals(c1.getCatvalue(), c2.getCatvalue(), 0.0);
+        assertEquals("NOC1", c1.getCatlabel());
+        assertEquals("NOC2", c2.getCatlabel());
+        assertEquals(c1.getNode().size(), c2.getNode().size());
+        assertEquals(c1.getNode().size(), c2.getNode().size());
+
+        final Map<Integer, Node> nodes1 = c1.getNode().stream().collect(Collectors.toMap(node -> (int) node.getNodeid(), Function.identity()));
+        final Map<Integer, Node> nodes2 = c2.getNode().stream().collect(Collectors.toMap(node -> (int) node.getNodeid(), Function.identity()));
+
+        assertEquals(nodes1.get(1).getNodesvccount() , nodes2.get(1).getNodesvccount());
+        assertEquals(nodes1.get(2).getNodesvccount() , nodes2.get(2).getNodesvccount());
+
+        assertEquals(nodes1.get(1).getNodesvcdowncount(), nodes2.get(1).getNodesvcdowncount());
+        assertEquals(nodes1.get(2).getNodesvcdowncount() , nodes2.get(2).getNodesvcdowncount());
+
+        assertEquals(nodes1.get(1).getNodevalue() , nodes2.get(1).getNodevalue(),0.0);
+        assertEquals(nodes1.get(2).getNodevalue() , nodes2.get(2).getNodevalue(),0.0);
     }
 
     // See NMS-10458
