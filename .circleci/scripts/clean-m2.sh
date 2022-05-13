067ef3248695d2d4795dfb51a582c0a7e347b2e4
Freddy Chu <fchu@opennms.com>
2022-05-13 09:29:31 -0400

    OIA-37: try fix m2 caching in ci

--- .circleci/scripts/clean-m2.sh
+++ .circleci/scripts/clean-m2.sh
@@ -3,6 +3,9 @@
 # keep anything downloaded in the last 7 days
 KEEP="+7"
 
+# remove directory by path (space separated)
+PATH_TO_DELETE="org/opennms/integration/api"
+
 if [ -d "$HOME/.m2/repository" ]; then
   # delete anything older than $KEEP days
   find "$HOME"/.m2/repository -depth -ctime $KEEP -type f -exec rm {} \; >/dev/null
@@ -10,5 +13,13 @@ if [ -d "$HOME/.m2/repository" ]; then
   # delete any directories that are empty (if a directory is not empty, silently continue0
   find "$HOME"/.m2/repository -depth -type d -print | while read -r DIR; do
     rmdir "$DIR" 2>/dev/null || :
+  done
+
+  # delete dependencies that are highly possible to change
+  for P in `echo "$PATH_TO_DELETE"`
+  do
+    if [ -n "${P}"  ]; then
+      find "$HOME"/.m2/repository -path "*${P}" -type d -exec rm -rf {} \;  >/dev/null
+    fi
   done
 fi
