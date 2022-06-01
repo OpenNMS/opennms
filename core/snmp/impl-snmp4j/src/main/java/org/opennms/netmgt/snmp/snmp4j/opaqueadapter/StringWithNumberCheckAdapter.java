020f1fa99dc697456b921e363d5e8c208f9e18d9
Dmitri Herdt <dmitri@herdt.online>
2022-06-01 14:17:43 +0200

    NMS-14018 Opaque data type - fix review issues 2

--- core/snmp/impl-snmp4j/src/main/java/org/opennms/netmgt/snmp/snmp4j/opaqueadapter/StringWithNumberCheckAdapter.java
+++ core/snmp/impl-snmp4j/src/main/java/org/opennms/netmgt/snmp/snmp4j/opaqueadapter/StringWithNumberCheckAdapter.java
@@ -31,35 +31,43 @@ package org.opennms.netmgt.snmp.snmp4j.opaqueadapter;
 import java.util.Objects;
 import org.opennms.netmgt.snmp.snmp4j.OpaqueValueType;
 
-
+/**
+ * This adapter is used to collect numeric data which is transfered as string.
+ * To improove performance the content of the string is checked only onece at creation.
+ */
 public class StringWithNumberCheckAdapter implements OpaqueTypeAdapter {
     
     String stringValue;
-    Long longValue = null;
-    Double doubleValue = null;
-    OpaqueValueType valueType = OpaqueValueType.STRING;
+    Long longValue;
+    Double doubleValue;
+    OpaqueValueType valueType;
 
     public StringWithNumberCheckAdapter(String value) {
         this.stringValue = Objects.requireNonNull(value);
         
-        //Try to extract a long value
+        //Trying to get an integer value (inl, long, ...) and store it as long. (Long is more perfomant as double)
         try {
             this.longValue = Long.parseLong(value);
             this.doubleValue = (double)this.longValue;
             this.valueType = OpaqueValueType.LONG;
             return;
         } catch (NumberFormatException ex) {
-            //do nothing
+            //do nothing -> thy to convert to double in the next step
         }
         
-        //Try to extract a double value
+        //Checking if we have a double value e.g. "+123e-45"
         try {
             this.doubleValue = Double.valueOf(value);
             this.longValue = this.doubleValue.longValue();
             this.valueType = OpaqueValueType.DOUBLE;
+            return;
         } catch (NumberFormatException ex) {
             //do nothing
         }
+
+        this.doubleValue = null;
+        this.longValue = null;
+        this.valueType = OpaqueValueType.STRING;
     }
     
     @Override
@@ -74,12 +82,12 @@ public class StringWithNumberCheckAdapter implements OpaqueTypeAdapter {
 
     @Override
     public String getString() {
-        return stringValue;
+        return this.stringValue;
     }
 
     @Override
     public OpaqueValueType getValueType() {
-        return valueType;
+        return this.valueType;
     }
     
 }
