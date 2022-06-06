27c6ffc2d19032de82bfc1bfc961958f6e2bcedb
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-06-06 16:06:23 -0400

    fix spacing in process_mapping

--- .circleci/pyscripts/process_mapping.py
+++ .circleci/pyscripts/process_mapping.py
@@ -1,90 +1,90 @@
 #!/usr/bin/env python3
 
-            import json
-            import os
-            import re
-            import subprocess
+import json
+import os
+import re
+import subprocess
 
-            def checkout(revision):
-              """
-              Helper function for checking out a branch
+def checkout(revision):
+    """
+    Helper function for checking out a branch
 
-              :param revision: The revision to checkout
-              :type revision: str
-              """
-              subprocess.run(
-                ['git', 'checkout', revision],
-                check=True
-              )
+    :param revision: The revision to checkout
+    :type revision: str
+    """
+    subprocess.run(
+    ['git', 'checkout', revision],
+    check=True
+    )
 
-            output_path = os.environ.get('OUTPUT_PATH')
-            head = os.environ.get('CIRCLE_SHA1')
-            base_revision = os.environ.get('BASE_REVISION')
-            
-            checkout(base_revision)  # Checkout base revision to make sure it is available for comparison
-            checkout(head)  # return to head commit
+output_path = os.environ.get('OUTPUT_PATH')
+head = os.environ.get('CIRCLE_SHA1')
+base_revision = os.environ.get('BASE_REVISION')
 
-            base = subprocess.run(
-              ['git', 'merge-base', head, 'HEAD~1'],
-              check=True,
-              capture_output=True
-            ).stdout.decode('utf-8').strip()
+checkout(base_revision)  # Checkout base revision to make sure it is available for comparison
+checkout(head)  # return to head commit
 
-            if head == base:
-              try:
-                # If building on the same branch as BASE_REVISION, we will get the
-                # current commit as merge base. In that case try to go back to the
-                # first parent, i.e. the last state of this branch before the
-                # merge, and use that as the base.
-                base = subprocess.run(
-                  ['git', 'rev-parse', 'HEAD~1'], # FIXME this breaks on the first commit, fallback to something
-                  check=True,
-                  capture_output=True
-                ).stdout.decode('utf-8').strip()
-              except:
-                # This can fail if this is the first commit of the repo, so that
-                # HEAD~1 actually doesn't resolve. In this case we can compare
-                # against this magic SHA below, which is the empty tree. The diff
-                # to that is just the first commit as patch.
-                base = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+base = subprocess.run(
+    ['git', 'merge-base', head, 'HEAD~1'],
+    check=True,
+    capture_output=True
+).stdout.decode('utf-8').strip()
 
-            print('Comparing {}...{}'.format(base, head))
-            changes = subprocess.run(
-              ['git', 'diff', '--name-only', base, head],
-              check=True,
-              capture_output=True
-            ).stdout.decode('utf-8').splitlines()
+if head == base:
+    try:
+        # If building on the same branch as BASE_REVISION, we will get the
+        # current commit as merge base. In that case try to go back to the
+        # first parent, i.e. the last state of this branch before the
+        # merge, and use that as the base.
+        base = subprocess.run(
+            ['git', 'rev-parse', 'HEAD~1'], # FIXME this breaks on the first commit, fallback to something
+            check=True,
+            capture_output=True
+        ).stdout.decode('utf-8').strip()
+    except:
+        # This can fail if this is the first commit of the repo, so that
+        # HEAD~1 actually doesn't resolve. In this case we can compare
+        # against this magic SHA below, which is the empty tree. The diff
+        # to that is just the first commit as patch.
+        base = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
 
-            print("Changed files between "+str(base)+" and "+str(head))
-            print(changes)
-            print("End of changed files")
+print('Comparing {}...{}'.format(base, head))
+changes = subprocess.run(
+    ['git', 'diff', '--name-only', base, head],
+    check=True,
+    capture_output=True
+).stdout.decode('utf-8').splitlines()
 
-            mappings = [
-              m.split() for m in
-              os.environ.get('MAPPING').splitlines()
-            ]
+print("Changed files between "+str(base)+" and "+str(head))
+print(changes)
+print("End of changed files")
 
-            def check_mapping(m):
-              if 3 != len(m):
-                raise Exception("Invalid mapping")
-              path, param, value = m
-              regex = re.compile(r'^' + path + r'$')
-              for change in changes:
-                if regex.match(change):
-                  return True
-              return False
+mappings = [
+    m.split() for m in
+    os.environ.get('MAPPING').splitlines()
+]
 
-            def convert_mapping(m):
-              return [m[1], json.loads(m[2])]
+def check_mapping(m):
+    if 3 != len(m):
+        raise Exception("Invalid mapping")
+    path, param, value = m
+    regex = re.compile(r'^' + path + r'$')
+    for change in changes:
+        if regex.match(change):
+            return True
+    return False
 
-            print("---ENV---")
-            print(os.environ.get('MAPPING'))
-            print("---END ENV---")
-            mappings = filter(check_mapping, mappings)
-            mappings = map(convert_mapping, mappings)
-            mappings = dict(mappings)
+def convert_mapping(m):
+    return [m[1], json.loads(m[2])]
 
-            print(mappings)
+print("---ENV---")
+print(os.environ.get('MAPPING'))
+print("---END ENV---")
+mappings = filter(check_mapping, mappings)
+mappings = map(convert_mapping, mappings)
+mappings = dict(mappings)
 
-            with open(output_path, 'w') as fp:
-              fp.write(json.dumps(mappings))
+print(mappings)
+
+with open(output_path, 'w') as fp:
+    fp.write(json.dumps(mappings))
