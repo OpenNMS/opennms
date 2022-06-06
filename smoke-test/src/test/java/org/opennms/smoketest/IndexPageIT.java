3eff1c6123e3d0da4360b16f066a3ec082ee79b9
Benjamin Reed <ranger@opennms.org>
2022-06-06 13:03:56 -0400

    sync "flaky" list with current flakes

--- smoke-test/src/test/java/org/opennms/smoketest/IndexPageIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/IndexPageIT.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2016-2017 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
+ * Copyright (C) 2016-2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -50,6 +50,7 @@ import com.google.common.base.Predicate;
  * 
  * @author <a href="mailto:agalue@opennms.org">Alejandro Galue</a>
  */
+@org.junit.experimental.categories.Category(org.opennms.smoketest.junit.FlakyTests.class)
 public class IndexPageIT extends OpenNMSSeleniumIT {
 
     private static final Logger LOG = LoggerFactory.getLogger(IndexPageIT.class);
