c0f9a71c5706c82b7b12444873fda040f70b5223
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-06-13 16:16:33 -0400

    test modifying parameters json file dynamically

--- .circleci/pyscripts/testarea.py
+++ .circleci/pyscripts/testarea.py
@@ -1,5 +1,5 @@
 from library import tests
-
+import json 
 x=tests.tests(True)
 
 print("Flaky testcases:")
@@ -10,6 +10,15 @@ print("===========")
 print("Smoke testcases (without Flaky):")
 for t in x.retrieveSmokeTests():
     print("\t",t)
+
+_data={}
+with open("/tmp/pipeline-parameters.json","r") as f:
+    _data=json.load(f)
+
+_data["pytest"]="worked"
+
+with open("/tmp/pipeline-parameters.json","w") as f:
+    json.dump(_data,f)
 
 #x.retrieveSmokeTests()
 #print(x.retrieveIntegrationTests())
