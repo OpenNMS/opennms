872414df2d7220d79423ddbad6fe3d7703104bde
mikewrosey <mrose@nanthealth.com>
2022-06-13 14:52:58 -0400

    let UI extensions tap into state mngment, update all pckgs

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import VueDiff from 'vue-diff'
+import { createPinia } from 'pinia'
 
 import '@featherds/styles'
 import '@featherds/styles/themes/open-light.css'
@@ -17,5 +18,6 @@ createApp({
   .use(VueDiff)
   .use(router)
   .use(store)
+  .use(createPinia())
   .directive('date', dateFormatDirective)
   .mount('#app')
