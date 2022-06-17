d4d31cd12ab3ff2c43b3f41ae700d913e3c0764a
chinh le <cle@opennms.com>
2022-06-17 09:24:40 -0400

    NMS-14344 Hack checkbox to behave as radio

--- ui/src/components/Topology/ViewSelect.vue
+++ ui/src/components/Topology/ViewSelect.vue
@@ -9,36 +9,43 @@
         >View</FeatherButton
       >
     </template>
-
     <!-- Views -->
+    <!--
+      Requires custom CSS for FeatherCheckbox to behave as radio
+        - add 'disabled' attribute to checkbox element: true when checked, hence preventing it from being deselected
+        - set disabled checkbox element to 'feather-primary' color (see style section at bottom)
+      i.e. FeatherRadioGroup/FeatherRadio is not working well with FeatherDropdown (@0.10.12)
+    -->
     <FeatherDropdownItem
       v-for="({type, label}) in Views"
       :key="type"
+      class="view-select-layout-dropdown-item"
     >
       <FeatherCheckbox
         @update:modelValue="selectView(type)"
         v-model="views[type]"
+        :disabled="views[type]"
         >{{label}}</FeatherCheckbox
       >
     </FeatherDropdownItem>
-
     <!-- Displays -->
     <div v-if="isTopologyView && graphs.length">
       <hr />
       <!-- <FeatherDropdownItem>
-        <FeatherCheckbox
-          @update:modelValue="selectDisplay('linkd')"
-          v-model="displays['linkd']"
-        >Enhanced Linkd</FeatherCheckbox>
-      </FeatherDropdownItem> -->
-
+              <FeatherCheckbox
+                @update:modelValue="selectDisplay('linkd')"
+                v-model="displays['linkd']"
+              >Enhanced Linkd</FeatherCheckbox>
+            </FeatherDropdownItem> -->
       <FeatherDropdownItem
         v-for="({id = '', label}) in graphs"
         :key="id"
+        class="view-select-display-dropdown-item"
       >
         <FeatherCheckbox
           @update:modelValue="selectDisplay(id)"
           v-model="displays[DisplayType[id]]"
+          :disabled="displays[DisplayType[id]]"
           >{{label}}</FeatherCheckbox
         >
       </FeatherDropdownItem>
@@ -89,9 +96,25 @@ const selectDisplay = (display: string) => {
 </style>
 <style lang="scss">
 @import "@featherds/dropdown/scss/mixins";
+@import "@featherds/styles/themes/variables";
 
 body > .feather-menu-dropdown > .feather-dropdown {
   @include dropdown-menu-height(8); // to have the view dropdown list of 8 items
+  // custom CSS for FeatherCheckbox to behave as radio; the disabled checked item (checkbox and label) have same color than the other checkable items
+  .view-select-layout-dropdown-item,
+  .view-select-display-dropdown-item {
+    .layout-container {
+      .feather-checkbox[aria-disabled="true"] {
+        > .checkbox > .box {
+          border-color: var(--feather-primary);
+          background-color: var(--feather-primary);
+        }
+        > label {
+          color: var(--feather-primary-text-on-surface);
+        }
+      }
+    }
+  }
 }
 </style>
 
