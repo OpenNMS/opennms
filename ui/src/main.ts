86a005cc8602ef781d5e5305d8f015e3afc4a9a8
mikewrosey <mrose@nanthealth.com>
2022-06-22 11:07:34 -0400

    let plugins change routes

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -4,6 +4,13 @@ import router from './router'
 import store from './store'
 import VueDiff from 'vue-diff'
 import { createPinia } from 'pinia'
+import API from '@/services'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import * as Vue from 'vue/dist/vue.esm-bundler'
+import * as Pinia from 'pinia'
+import * as Vuex from 'vuex'
 
 import '@featherds/styles'
 import '@featherds/styles/themes/open-light.css'
@@ -11,6 +18,22 @@ import '@featherds/styles/themes/open-light.css'
 import 'vue-diff/dist/index.css'
 
 import dateFormatDirective from './directives/v-date'
+import { externalComponent, getJSPath } from './components/Plugin/utils'
+
+// let plugins use state mngmnt / router
+(window as any).Vue = Vue;
+(window as any).Pinia = Pinia;
+(window as any).Vuex = Vuex;
+(window as any)['VRouter'] = router
+
+// plugin scripts must be loaded before app to use their routes
+const baseUrl = import.meta.env.VITE_BASE_REST_URL
+const plugins = await API.getPlugins()
+
+for (const plugin of plugins) {
+  const js = getJSPath(baseUrl, plugin.extensionId, plugin.resourceRootPath, plugin.moduleFileName)
+  await externalComponent(js)
+}
 
 createApp({
   render: () => h(App)
