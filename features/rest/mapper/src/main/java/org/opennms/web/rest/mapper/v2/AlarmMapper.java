9c801a27b66c9e194cf27d7c781ee5f722b99930
aramosvizcarra <aramosvizcarra@gmail.com>
2022-07-03 10:07:03 -0400

    Fix null pointer exception in api/v2/alarms rest point when lasteventid in alarms are null

--- features/rest/mapper/src/main/java/org/opennms/web/rest/mapper/v2/AlarmMapper.java
+++ features/rest/mapper/src/main/java/org/opennms/web/rest/mapper/v2/AlarmMapper.java
@@ -144,7 +144,10 @@ public abstract class AlarmMapper {
 
     @AfterMapping
     protected void mapEventLabel(@MappingTarget AlarmSummaryDTO summaryDTO) {
-        summaryDTO.setLabel(eventConfDao.getEventLabel(summaryDTO.getUei()));
+        //there are cases when lasteventid in alarms is null, making api/v2/alarms throw a null pointer exception
+        if(summaryDTO.getUei() != null) {
+            summaryDTO.setLabel(eventConfDao.getEventLabel(summaryDTO.getUei()));
+        }
     }
 
     public void setEventConfDao(EventConfDao eventConfDao) {
