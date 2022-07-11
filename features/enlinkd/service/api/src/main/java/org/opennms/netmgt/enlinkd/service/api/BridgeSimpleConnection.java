80ff599a6b6545260dbe9995d5e3a27dcec70e95
Antonio Russo <rssntn67@yahoo.it>
2022-07-11 08:43:24 +0200

    Update features/enlinkd/service/api/src/main/java/org/opennms/netmgt/enlinkd/service/api/BridgeSimpleConnection.java
    Co-authored-by: Christian Pape <Christian.Pape@informatik.hs-fulda.de>

--- features/enlinkd/service/api/src/main/java/org/opennms/netmgt/enlinkd/service/api/BridgeSimpleConnection.java
+++ features/enlinkd/service/api/src/main/java/org/opennms/netmgt/enlinkd/service/api/BridgeSimpleConnection.java
@@ -243,7 +243,7 @@ public class BridgeSimpleConnection implements Topology {
         // condition 2 XThenY
         if (m_yxPort == null && m_xyPort != null) {
             if (LOG.isDebugEnabled()) {
-                LOG.debug("findSimpleConnection: try condition 2 XThenY {} -> {}", m_xyPort.printTopology(),m_yBridge.getNodeId());
+                LOG.debug("findSimpleConnection: try condition 2 XThenY {} -> {}", m_xyPort.printTopology(), m_yBridge.getNodeId());
             }
             m_yxPort = findPortUsingSimpleConnectionAlgorithm(m_yBridge,m_xBridge,m_xyPort);
             if (m_yxPort != null) {
