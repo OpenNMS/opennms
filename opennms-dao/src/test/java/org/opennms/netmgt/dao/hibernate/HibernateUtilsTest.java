c9403725d45cd1a0c1e215c2c7f46b32a7de7fe1
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2022-07-11 17:25:58 +0200

    NMS-14233: Fixed tests

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/hibernate/HibernateUtilsTest.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/hibernate/HibernateUtilsTest.java
@@ -48,8 +48,10 @@ import static org.junit.Assert.assertTrue;
 @RunWith(OpenNMSJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
         "classpath:/META-INF/opennms/applicationContext-soa.xml",
-        "classpath:/META-INF/opennms/mockEventIpcManager.xml",
         "classpath:/META-INF/opennms/applicationContext-dao.xml",
+        "classpath:/META-INF/opennms/applicationContext-mockConfigManager.xml",
+        "classpath*:/META-INF/opennms/component-dao.xml",
+        "classpath:/META-INF/opennms/applicationContext-daemon.xml",
         "classpath:/META-INF/opennms/applicationContext-databasePopulator.xml",
         "classpath*:/META-INF/opennms/component-dao.xml",
         "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
