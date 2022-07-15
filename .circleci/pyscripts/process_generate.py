054af329b6b551621461c61975a79b488138da0c
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-07-15 10:31:39 -0400

    update build mappings in both files

--- .circleci/pyscripts/process_generate.py
+++ .circleci/pyscripts/process_generate.py
@@ -93,6 +93,7 @@ print("Git Keywords:",git_keywords)
 if "circleci_configuration" in What_to_build and len(What_to_build) == 1 :
     #if circleci_configuration is the only entry in the list we don't want to trigger a buildss.
     mappings["trigger-build"]=False
+    build_mappings["trigger-build"]=False
 
 if "smoke" in git_keywords or "Smoke_tests" in What_to_build:   
     build_mappings["trigger-smoke"]=True
