738f5702e9a161981f9c119e6532b9b084b879b9
mike <mikewrosey@gmail.com>
2022-07-18 16:42:41 -0400

    rm unused feather var import

--- ui/src/assets/Logo.vue
+++ ui/src/assets/Logo.vue
@@ -9,7 +9,6 @@ import Logo from './OpenNMS_Horizontal-Logo_Light-BG.svg'
 </script>
 
 <style scoped lang="scss">
-@import "@featherds/styles/themes/variables";
 .logo {
   width: 8em !important;
   color: #FFF !important;
