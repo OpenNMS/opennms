0a5b45b73bf62836abd2485c15a6b4e3f02e1d7b
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-07-19 14:21:00 -0400

    tweak initial processing

--- .circleci/pyscripts/process_generate.py
+++ .circleci/pyscripts/process_generate.py
@@ -153,6 +153,9 @@ if "integration" in git_keywords or "Integration_tests" in What_to_build:
 #else:
 #    build_mappings["tests"]["integration"]=False
 
+if "build" in What_to_build:
+    build_mappings["build"]["build"]=True
+
 if "doc" in git_keywords or "doc" in What_to_build:
     build_mappings["build"]["docs"]=True
 #else:
