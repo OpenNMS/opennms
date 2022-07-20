5ce108ccd3517bd30406d1f3c02544ddc2da59f3
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-07-20 16:32:20 -0400

    expand the keywords for #docs

--- .circleci/pyscripts/process_generate.py
+++ .circleci/pyscripts/process_generate.py
@@ -166,7 +166,7 @@ if "integration" in git_keywords or "Integration_tests" in What_to_build:
 if "build" in What_to_build:
     build_mappings["build"]["build"]=True
 
-if "doc" in git_keywords or "doc" in What_to_build:
+if git_keywords in ["doc","docs"] or "doc" in What_to_build:
     build_mappings["build"]["docs"]=True
 #else:
 #    build_mappings["build"]["docs"]=False
