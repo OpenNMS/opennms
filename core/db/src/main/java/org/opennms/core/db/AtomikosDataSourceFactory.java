53d489cc1b638298347b0999fe8989b0db4e0c55
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-07-25 08:55:54 -0400

    test a simple change to a source code

--- core/db/src/main/java/org/opennms/core/db/AtomikosDataSourceFactory.java
+++ core/db/src/main/java/org/opennms/core/db/AtomikosDataSourceFactory.java
@@ -64,7 +64,7 @@ public class AtomikosDataSourceFactory extends AtomikosDataSourceBean implements
 	public AtomikosDataSourceFactory() {
 		super.setUniqueResourceName("opennms");
 		super.setXaDataSource(XADataSourceFactory.getInstance());
-		super.setPoolSize(30);
+		super.setPoolSize(20);
 
 		// Automatically rollback the connection on borrow to avoid a problem where
 		// Atomikos will reuse database connections that contain aborted transactions, 
