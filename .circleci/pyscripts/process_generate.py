35f560f56a18745d935d210ad8fd0eed0774639a
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-07-29 14:04:02 -0400

    tweak #smoke:flaky

--- .circleci/pyscripts/process_generate.py
+++ .circleci/pyscripts/process_generate.py
@@ -133,7 +133,7 @@ if "circleci_configuration" in What_to_build and len(What_to_build) == 1 and not
 if "smoke" in git_keywords or "Smoke_tests" in What_to_build:   
     print("Keywords::",str(git_keywords))
     print("len:",str(len(git_keywords)))
-    if "flaky" in git_keywords:
+    if "flaky" in str(git_keywords):
         print("Found Flaky")
         build_mappings["tests"]["smoke-flaky"]=True
     else:
