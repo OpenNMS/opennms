2c101c6b507b79f2743e1e658b448adf74ed03af
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-07-29 14:15:47 -0400

    change the mapping for filters

--- .circleci/pyscripts/process_generate.py
+++ .circleci/pyscripts/process_generate.py
@@ -135,7 +135,7 @@ if "smoke" in git_keywords or "Smoke_tests" in What_to_build:
         build_mappings["tests"]["smoke-flaky"]=True
     else:
         build_mappings["tests"]["smoke"]=True
-    build_mappings["tests"]["filters"]=False
+    build_mappings["filters"]["enabled"]=False
 
 if "oci" in git_keywords:
     build_mappings["build"]["build"]=True
