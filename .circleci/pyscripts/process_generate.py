24cfede71a94efdbfd08ac5a1926eb2a6d664a5a
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-07-29 13:59:32 -0400

    testing #smoke:flaky

--- .circleci/pyscripts/process_generate.py
+++ .circleci/pyscripts/process_generate.py
@@ -133,13 +133,10 @@ if "circleci_configuration" in What_to_build and len(What_to_build) == 1 and not
 if "smoke" in git_keywords or "Smoke_tests" in What_to_build:   
     print("Keywords::",str(git_keywords))
     print("len:",str(len(git_keywords)))
-    if len(git_keywords)>=1:
-        for v in git_keywords["smoke"]:
-            print("Key",v)
-            if v in ["flaky"]:
-                build_mappings["tests"]["smoke-flaky"]=True
-            else:
-                build_mappings["tests"]["smoke"]=True
+    if "flaky" in git_keywords:
+        build_mappings["tests"]["smoke-flaky"]=True
+    else:
+        build_mappings["tests"]["smoke"]=True
 
 if "oci" in git_keywords:
     build_mappings["build"]["build"]=True
