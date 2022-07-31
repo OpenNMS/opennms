52fdb10f1ecb02e231eb8f068bf6a84aaf8e96e4
DJ Gregor <dj@opennms.org>
2022-07-31 17:35:10 -0400

    EXPERIMENTAL, INCOMPLETE: Use opentelemetry-opentracing-shim instead of Jaeger client library
    Configuration is currently only done through the OpenTelemetry auto-configuration
    environment variables / Java system properties as documented here:
    https://github.com/open-telemetry/opentelemetry-java/tree/main/sdk-extensions/autoconfigure
    
    Previous configuration options for Jaeger and sampling aren't supported right now.
    
    Note: the Jaeger configuration had logging of spans enabled
    ("withLogSpans(true)"). This is not included here as this functionality
    probably isn't desired to be enabled all of the time.

--- core/tracing/jaeger-tracer/src/main/java/org/opennms/core/tracing/jaeger/JaegerTracerWrapper.java
+++ core/tracing/jaeger-tracer/src/main/java/org/opennms/core/tracing/jaeger/JaegerTracerWrapper.java
@@ -33,10 +33,22 @@ import org.opennms.core.sysprops.SystemProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.jaegertracing.Configuration;
-import io.jaegertracing.internal.samplers.ConstSampler;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.extension.trace.propagation.JaegerPropagator;
+import io.opentelemetry.opentracingshim.OpenTracingShim;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
+
+
 
 /**
  * Provides Jaeger Tracer, Configuration options for Jaeger client are through system properties specified in
@@ -47,25 +59,79 @@ public class JaegerTracerWrapper implements TracerWrapper {
 
     private static final Logger LOG = LoggerFactory.getLogger(JaegerTracerWrapper.class);
 
-    private static final String JAEGER_SAMPLER_PARAM_PROPERTY = "JAEGER_SAMPLER_PARAM";
-    private static final int JAEGER_SAMPLER_PARAM_VALUE = SystemProperties.getInteger(JAEGER_SAMPLER_PARAM_PROPERTY, 1);
-    private static final String JAEGER_SAMPLER_TYPE_PROPERTY = "JAEGER_SAMPLER_TYPE";
-    private static final String JAEGER_SAMPLER_TYPE_VALUE = System.getProperty(JAEGER_SAMPLER_TYPE_PROPERTY, ConstSampler.TYPE);
+    // XXX need to convert
+    //private static final String JAEGER_SAMPLER_PARAM_PROPERTY = "JAEGER_SAMPLER_PARAM";
+    //private static final int JAEGER_SAMPLER_PARAM_VALUE = SystemProperties.getInteger(JAEGER_SAMPLER_PARAM_PROPERTY, 1);
+    //private static final String JAEGER_SAMPLER_TYPE_PROPERTY = "JAEGER_SAMPLER_TYPE";
+    //private static final String JAEGER_SAMPLER_TYPE_VALUE = System.getProperty(JAEGER_SAMPLER_TYPE_PROPERTY, ConstSampler.TYPE);
     
     @Override
     public Tracer init(String serviceName) {
-        Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv()
-                .withType(JAEGER_SAMPLER_TYPE_VALUE)
-                .withParam(JAEGER_SAMPLER_PARAM_VALUE);
+        if (GlobalTracer.isRegistered()) {
+            LOG.info("Returning already existing OpenTracing GlobalTracer");
+            return GlobalTracer.get();
+        }
 
-        Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv()
-                .withLogSpans(true);
+// XXX need to convert
+//        Configuration.SamplerConfiguration samplerConfig = Configuration.SamplerConfiguration.fromEnv()
+//                .withType(JAEGER_SAMPLER_TYPE_VALUE)
+//                .withParam(JAEGER_SAMPLER_PARAM_VALUE);
+//
+//        Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv()
+//                .withLogSpans(true);
+//
+//        Configuration config = new Configuration(serviceName)
+//                .withSampler(samplerConfig)
+//                .withReporter(reporterConfig);
+//        GlobalTracer.get();
 
-        Configuration config = new Configuration(serviceName)
-                .withSampler(samplerConfig)
-                .withReporter(reporterConfig);
-        GlobalTracer.get();
-        LOG.info("Jaeger tracer initialized with serviceName = {}", serviceName);
-        return config.getTracer();
+// XXX need to convert??? or can we continue to use autoconfiguration?
+// https://opentelemetry.io/docs/migration/opentracing/
+// https://medium.com/jaegertracing/migrating-from-jaeger-client-to-opentelemetry-sdk-bd337d796759
+//
+//        ManagedChannel jaegerChannel = ManagedChannelBuilder
+//            .forAddress("localhost", 14250)
+//            .usePlaintext()
+//            .build();
+//
+//        JaegerGrpcSpanExporter jaegerExporter = JaegerGrpcSpanExporter.builder()
+//            .setChannel(jaegerChannel)
+//            .setTimeout(1, TimeUnit.SECONDS)
+//            .build();
+//
+// XXX need to convert??? or can we continue to use autoconfiguration?
+//        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+//            //.addSpanProcessor(SimpleSpanProcessor.create(jaegerExporter))
+//            .setResource(Resource.getDefault().merge(serviceNameResource))
+//            .build();
+//
+//        OpenTelemetrySdk openTelemetry = OpenTelemetrySdk.builder()
+//            .setPropagators(ContextPropagators.create(
+//                TextMapPropagator.composite(
+//                    W3CTraceContextPropagator.getInstance(),
+//                    JaegerPropagator.getInstance()
+//                )
+//            ))
+//            .setTracerProvider(tracerProvider)
+//            .buildAndRegisterGlobal();
+//
+//
+//        Runtime.getRuntime().addShutdownHook(new Thread(tracerProvider::close));
+
+        // This will do auto-configuration.
+        OpenTelemetry otel = GlobalOpenTelemetry.get();
+
+        if (otel == OpenTelemetry.noop()) {
+            LOG.info("OpenTelemetry SDK initialized with no-op implementation");
+        } else {
+            LOG.info("OpenTelemetry SDK initialized");
+        }
+
+        // https://github.com/open-telemetry/opentelemetry-java/blob/main/opentracing-shim/README.md
+        GlobalTracer.registerIfAbsent(OpenTracingShim.createTracerShim(GlobalOpenTelemetry.get()));
+
+        LOG.info("OpenTracing->OpenTelmetry shim registered");
+
+        return GlobalTracer.get();
     }
 }
