c5ce78c401a54c1ef09c3fe202ea4dedf8f5f653
Atlassian Bamboo <bamboo@opennms.org>
2022-08-10 18:32:30 +0000

    30.0.2 -> 30.0.3-SNAPSHOT

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
@@ -59,7 +59,7 @@ public class OnmsKarafTestCase extends KarafTestCase {
 				.groupId("org.opennms.container")
 				.artifactId("org.opennms.container.karaf")
 				.type("tar.gz")
-				.version("30.0.2");
+				.version("30.0.3-SNAPSHOT");
 	}
 
 	/**
