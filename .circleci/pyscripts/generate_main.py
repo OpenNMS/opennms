2e9ae4e81178c495166a2aae2f8b475bdae2184b
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2022-08-23 13:02:01 -0400

    code clean up !doc !smoke !smoke-flaky !ui

--- .circleci/pyscripts/generate_main.py
+++ .circleci/pyscripts/generate_main.py
@@ -184,16 +184,6 @@ for e in main_yml_content:
                 )
                 workflow_path = append_to_sample_workflow(workflow_path, workflow)
 
-            # if build_components["integration"]:
-            #    print(
-            #        "tests > integration:",
-            #        circleCI.get_Workflow_dependency("integration-test"),
-            #    )
-            #    workflow = circleCI.get_Workflow_yaml(
-            #        "integration-test", level, enable_filters=filters_enabled
-            #    )
-            #    workflow_path = append_to_sample_workflow(workflow_path, workflow)
-
             if build_components["smoke"]:
                 print("tests > smoke:", circleCI.get_Workflow_dependency("smoke"))
 
