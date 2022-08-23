a7633ad06c83f32d90fe6d2c136a6a132ab971d9
Benjamin Reed <ranger@opennms.org>
2022-08-23 16:37:38 -0400

    NMS-14675: use new ubuntu 22 deploy base

--- .circleci/scripts/itest.sh
+++ .circleci/scripts/itest.sh
@@ -13,7 +13,8 @@ find_tests()
     # Generate surefire & failsafe test list based on current
     # branch and the list of files changed
     # (The format of the output files contains the canonical class names i.e. org.opennms.core.soa.filter.FilterTest)
-    pyenv local 3.8.5
+    SYSTEM_PYTHON="$(python3 --version | sed -e 's,^Python *,,')"
+    pyenv local "${SYSTEM_PYTHON}"
     python3 .circleci/scripts/find-tests/find-tests.py generate-test-lists \
       --changes-only="${CCI_CHANGES_ONLY:-true}" \
       --output-unit-test-classes=surefire_classnames \
