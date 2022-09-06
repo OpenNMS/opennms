61c620730252eb0f641f61df5ed14035d5d9c009
Benjamin Reed <ranger@opennms.org>
2022-09-06 09:44:47 -0400

    reformat the datachoices test JSON so it is easier to resolve conflicts

--- features/datachoices/src/test/java/org/opennms/features/datachoices/internal/UsageStatisticsReportDTOTest.java
+++ features/datachoices/src/test/java/org/opennms/features/datachoices/internal/UsageStatisticsReportDTOTest.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2016 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2016 The OpenNMS Group, Inc.
+ * Copyright (C) 2016-2022 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2022 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -68,7 +68,60 @@ public class UsageStatisticsReportDTOTest {
         usageStatisticsReport.setGroups(0);
         String actualJson = usageStatisticsReport.toJson();
         System.err.println(actualJson);
-        String expectedJson = "{\"alarms\":0,\"availableProcessors\":null,\"businessEdgeCount\":0,\"coreFlowsPersisted\":0,\"coreNewtsSamplesInserted\":0,\"coreQueuedUpdatesCompleted\":0,\"databaseProductName\":null,\"databaseProductVersion\":null,\"destinationPathCount\":-1,\"eventLogsProcessed\":0,\"events\":0,\"freePhysicalMemorySize\":null,\"groups\":0,\"installedFeatures\":null,\"installedOIAPlugins\":null,\"ipInterfaces\":0,\"minions\":0,\"monitoredServices\":0,\"monitoringLocations\":0,\"nodes\":0,\"nodesBySysOid\":{\".1.2.3.4\":2,\".1.2.3.5\":6},\"nodesWithDeviceConfigBySysOid\":{},\"notificationEnablementStatus\":null,\"notifications\":0,\"onCallRoleCount\":1,\"osArch\":null,\"osName\":null,\"osVersion\":null,\"outages\":0,\"packageName\":\"opennms\",\"pollsCompleted\":0,\"provisiondImportThreadPoolSize\":0,\"provisiondRequisitionSchemeCount\":{\"file\":4,\"vmware\":3},\"provisiondRescanThreadPoolSize\":0,\"provisiondScanThreadPoolSize\":0,\"provisiondWriteThreadPoolSize\":0,\"requisitionCount\":0,\"requisitionWithChangedFSCount\":0,\"rpcStrategy\":null,\"services\":{\"Provisiond\":true,\"Telemetryd\":false},\"sinkStrategy\":null,\"situations\":0,\"snmpInterfaces\":0,\"snmpInterfacesWithFlows\":0,\"systemId\":\"aae3fdeb-3014-47b4-bb13-c8aa503fccb7\",\"totalPhysicalMemorySize\":null,\"tssStrategies\":null,\"users\":0,\"version\":\"10.5.7\"}";
+        String expectedJson = "{" +
+            "\"alarms\":0," +
+            "\"availableProcessors\":null," +
+            "\"businessEdgeCount\":0," +
+            "\"coreFlowsPersisted\":0," +
+            "\"coreNewtsSamplesInserted\":0," +
+            "\"coreQueuedUpdatesCompleted\":0," +
+            "\"databaseProductName\":null," +
+            "\"databaseProductVersion\":null," +
+            "\"destinationPathCount\":-1," +
+            "\"eventLogsProcessed\":0," +
+            "\"events\":0," +
+            "\"freePhysicalMemorySize\":null," +
+            "\"groups\":0," +
+            "\"installedFeatures\":null," +
+            "\"installedOIAPlugins\":null," +
+            "\"ipInterfaces\":0," +
+            "\"minions\":0," +
+            "\"monitoredServices\":0," +
+            "\"monitoringLocations\":0," +
+            "\"nodes\":0," +
+            "\"nodesBySysOid\":{\".1.2.3.4\":2," +
+            "\".1.2.3.5\":6}," +
+            "\"nodesWithDeviceConfigBySysOid\":{}," +
+            "\"notificationEnablementStatus\":null," +
+            "\"notifications\":0," +
+            "\"onCallRoleCount\":1," +
+            "\"osArch\":null," +
+            "\"osName\":null," +
+            "\"osVersion\":null," +
+            "\"outages\":0," +
+            "\"packageName\":\"opennms\"," +
+            "\"pollsCompleted\":0," +
+            "\"provisiondImportThreadPoolSize\":0," +
+            "\"provisiondRequisitionSchemeCount\":{\"file\":4," +
+            "\"vmware\":3}," +
+            "\"provisiondRescanThreadPoolSize\":0," +
+            "\"provisiondScanThreadPoolSize\":0," +
+            "\"provisiondWriteThreadPoolSize\":0," +
+            "\"requisitionCount\":0," +
+            "\"requisitionWithChangedFSCount\":0," +
+            "\"rpcStrategy\":null," +
+            "\"services\":{\"Provisiond\":true," +
+            "\"Telemetryd\":false}," +
+            "\"sinkStrategy\":null," +
+            "\"situations\":0," +
+            "\"snmpInterfaces\":0," +
+            "\"snmpInterfacesWithFlows\":0," +
+            "\"systemId\":\"aae3fdeb-3014-47b4-bb13-c8aa503fccb7\"," +
+            "\"totalPhysicalMemorySize\":null," +
+            "\"tssStrategies\":null," +
+            "\"users\":0," +
+            "\"version\":\"10.5.7\"" +
+        "}";
         assertEquals(expectedJson, actualJson);
     }
 }
