fcd181935c77a34420ad5aff78a0c1f38e172dae
Benjamin Reed <ranger@opennms.org>
2022-09-13 10:19:19 -0400

    try doing incremental-only builds

--- .circleci/scripts/sonar.sh
+++ .circleci/scripts/sonar.sh
@@ -21,6 +21,25 @@ generate_junit_report_names()
   echo "$(generate_report_names 'target/surefire-reports-'),$(generate_report_names 'target/failsafe-reports-')"
 }
 
+find_tests()
+{
+    # Generate surefire & failsafe test list based on current
+    # branch and the list of files changed
+    # (The format of the output files contains the canonical class names i.e. org.opennms.core.soa.filter.FilterTest)
+    SYSTEM_PYTHON="$(python3 --version | sed -e 's,^Python *,,')"
+    pyenv local "${SYSTEM_PYTHON}"
+    python3 .circleci/scripts/find-tests/find-tests.py generate-test-lists \
+      --changes-only="${CCI_CHANGES_ONLY:-true}" \
+      --output-unit-test-classes=surefire_classnames \
+      --output-integration-test-classes=failsafe_classnames \
+      .
+
+    # Now determine the Maven modules related to the tests we need to run
+    cat /tmp/*_classnames | python3 .circleci/scripts/find-tests/find-tests.py generate-test-modules \
+      --output=/tmp/this_node_projects \
+      .
+}
+
 dnf -y remove npm nodejs-full-i18n
 dnf module reset -y nodejs
 dnf module enable -y nodejs:14
@@ -36,4 +55,6 @@ cd ~/project
   -Dsonar.login="$SONARCLOUD_LOGIN" \
   -Dsonar.branch.name="$CIRCLE_BRANCH" \
   -Dsonar.coverage.jacoco.xmlReportPaths="$(generate_jacoco_report_names)" \
-  -Dsonar.junit.reportPaths="$(generate_junit_report_names)"
+  -Dsonar.junit.reportPaths="$(generate_junit_report_names)" \
+  --also-make \
+  --projects "$(< /tmp/this_node_projects paste -s -d, -)"
