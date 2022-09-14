7a324d568ed188b565361e96433e0c7e816a0c75
Ivan Trechekas <trechekas.ivan@gmail.com>
2022-09-14 13:56:17 -0400

    Sonar: remove redundant null check

--- core/db-install/src/main/java/org/opennms/core/db/install/Constraint.java
+++ core/db-install/src/main/java/org/opennms/core/db/install/Constraint.java
@@ -496,7 +496,7 @@ public class Constraint {
      * @return a boolean.
      */
     public boolean equals(final Object other_o, boolean ignoreFdelType) {
-    	if (other_o == null || !(other_o instanceof Constraint)) return false;
+    	if (!(other_o instanceof Constraint)) return false;
     	final Constraint other = (Constraint) other_o;
 
         if ((m_name == null && other.getName() != null) || (m_name != null && other.getName() == null)) {
