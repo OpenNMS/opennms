96c2a7599c63379f612a25f46062a4aa46b02a82
Benjamin Reed <ranger@opennms.org>
2022-09-16 10:25:35 -0400

    optimize directory scan a tiny bit

--- .circleci/scripts/codecoverage-restore.sh
+++ .circleci/scripts/codecoverage-restore.sh
@@ -6,7 +6,7 @@ for file in ~/code-coverage/target-*.zip; do
     NUMBER="$(echo "$FILENAME" | cut -d. -f1 | cut -d- -f2-)"
     echo "* unpacking coverage reports $NUMBER:"
     unzip -qo "$file"
-    find . -type d -name failsafe-reports -o -name surefire-reports | while read -r DIR; do
+    find . -type d '!' -path './.git/*' -name failsafe-reports -o -name surefire-reports | while read -r DIR; do
       mkdir -p "${DIR}-${NUMBER}"
       mv "${DIR}"/* "${DIR}-${NUMBER}/"
       rm -rf "${DIR}"
