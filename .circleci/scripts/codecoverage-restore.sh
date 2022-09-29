9a38a8bae22694a713b6ce83b13b98a79de43a48
Benjamin Reed <ranger@opennms.org>
2022-09-29 11:34:38 -0400

    NMS-14759: enable (optimized) sonar in Foundation 2022

--- .circleci/scripts/codecoverage-restore.sh
+++ .circleci/scripts/codecoverage-restore.sh
@@ -1,11 +1,15 @@
-#!/bin/sh -e
+#!/bin/bash
+
+set -e
+set -o pipefail
+
 cd ~/project
 for file in ~/code-coverage/target-*.zip; do
     FILENAME="$(basename "$file")"
     NUMBER="$(echo "$FILENAME" | cut -d. -f1 | cut -d- -f2-)"
     echo "* unpacking coverage reports $NUMBER:"
     unzip -qo "$file"
-    find . -type d -name failsafe-reports -o -name surefire-reports | while read DIR; do
+    find . -type d '!' -path './.git/*' -name failsafe-reports -o -name surefire-reports | while read -r DIR; do
       mkdir -p "${DIR}-${NUMBER}"
       mv "${DIR}"/* "${DIR}-${NUMBER}/"
       rm -rf "${DIR}"
