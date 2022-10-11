69b38fffc6048e1e0eae239355d38e164cfc533a
Alexander Chadfield <achadfield@nanthealth.com>
2022-10-11 16:00:55 -0400

    NMS-14180 test rewritten and corrected based on the comments in PR.

--- smoke-test/src/test/java/org/opennms/smoketest/dcb/DcbMultipleNodesIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/dcb/DcbMultipleNodesIT.java
@@ -29,18 +29,12 @@
 package org.opennms.smoketest.dcb;
 
 import com.google.common.collect.Iterables;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
-import org.opennms.core.criteria.CriteriaBuilder;
 import org.opennms.features.deviceconfig.persistence.impl.DeviceConfigDaoImpl;
-import org.opennms.netmgt.dao.api.IpInterfaceDao;
-import org.opennms.netmgt.dao.api.MonitoredServiceDao;
 import org.opennms.netmgt.dao.api.NodeDao;
-import org.opennms.netmgt.dao.hibernate.IpInterfaceDaoHibernate;
-import org.opennms.netmgt.dao.hibernate.MonitoredServiceDaoHibernate;
 import org.opennms.netmgt.dao.hibernate.NodeDaoHibernate;
 import org.opennms.netmgt.model.OnmsIpInterface;
 import org.opennms.netmgt.model.OnmsMonitoredService;
@@ -54,34 +48,20 @@ import org.opennms.netmgt.provision.persist.requisition.RequisitionNode;
 import org.opennms.smoketest.stacks.OpenNMSProfile;
 import org.opennms.smoketest.stacks.OpenNMSStack;
 import org.opennms.smoketest.stacks.StackModel;
-import org.opennms.smoketest.utils.DaoUtils;
-import org.opennms.smoketest.utils.KarafShell;
 import org.opennms.smoketest.utils.RestClient;
 import org.opennms.smoketest.utils.TestContainerUtils;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.Network;
 import org.testcontainers.images.builder.ImageFromDockerfile;
 
-import javax.validation.constraints.AssertTrue;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.jayway.awaitility.Awaitility.await;
-import static com.spotify.hamcrest.jackson.JsonMatchers.jsonArray;
-import static com.spotify.hamcrest.jackson.JsonMatchers.jsonBoolean;
-import static com.spotify.hamcrest.jackson.JsonMatchers.jsonInt;
-import static com.spotify.hamcrest.jackson.JsonMatchers.jsonNull;
-import static com.spotify.hamcrest.jackson.JsonMatchers.jsonObject;
-import static com.spotify.hamcrest.jackson.JsonMatchers.jsonText;
 import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.equalTo;
 
 public class DcbMultipleNodesIT {
 
@@ -93,11 +73,12 @@ public class DcbMultipleNodesIT {
 
     private static final int SSH_PORT = 2222;
 
+    private static final int NUMBER_OF_NODES = 100;
+
     private static final String FOREIGN_SOURCE = "SmokeTests";
 
     private static final OpenNMSProfile OPENNMS_PROFILE = OpenNMSProfile.newBuilder()
             .withFile("device-config/test.dcb", "etc/device-config/" + DCB_SCRIPT_NAME + ".dcb")
-            .withFile("device-config/SmokeTests.xml", "etc/foreign-sources/pending/SmokeTests.xml")
             .build();
 
     @ClassRule
@@ -121,10 +102,6 @@ public class DcbMultipleNodesIT {
 
     private static String targetAddress;
 
-    private static OnmsNode localNode;
-    private static OnmsIpInterface localInterface;
-    private static OnmsMonitoredService localService;
-
     private static RestClient restClient;
 
     @BeforeClass
@@ -140,26 +117,25 @@ public class DcbMultipleNodesIT {
         LocalTime time = LocalTime.now().plusMinutes(2);
         String schedule = String.format("0 %d %d ? * *", time.getMinute(), time.getHour());
 
-        // Node in default location for triggering backup from core
-        // TODO make a loop for creation of at least 1000 nodes
-        for (int i = 1; i < 101; i++) {
-            final List<RequisitionMonitoredService> monitoredServiceList = new ArrayList<>();
-            monitoredServiceList.add(new RequisitionMonitoredService(DCB_SVC_NAME));
+        final List<RequisitionMonitoredService> monitoredServiceList = new ArrayList<>();
+        monitoredServiceList.add(new RequisitionMonitoredService(DCB_SVC_NAME));
 
-            final RequisitionInterface requisitionInterface = new RequisitionInterface();
-            requisitionInterface.setIpAddr(targetAddress);
-            requisitionInterface.setManaged(true);
-            requisitionInterface.setSnmpPrimary(PrimaryType.PRIMARY);
-            requisitionInterface.setMonitoredServices(monitoredServiceList);
+        final RequisitionInterface requisitionInterface = new RequisitionInterface();
+        requisitionInterface.setIpAddr(targetAddress);
+        requisitionInterface.setManaged(true);
+        requisitionInterface.setSnmpPrimary(PrimaryType.PRIMARY);
+        requisitionInterface.setMonitoredServices(monitoredServiceList);
 
-            final List<RequisitionMetaData> metaDataList = new ArrayList<>();
-            metaDataList.add(new RequisitionMetaData("requisition", "dcb:config-type", DCB_CONFIG_TYPE));
-            metaDataList.add(new RequisitionMetaData("requisition", "dcb:username", DCB_USERNAME));
-            metaDataList.add(new RequisitionMetaData("requisition", "dcb:password", DCB_PASSWORD));
-            metaDataList.add(new RequisitionMetaData("requisition", "dcb:script-file", DCB_SCRIPT_NAME));
-            metaDataList.add(new RequisitionMetaData("requisition", "dcb:ssh-port", Integer.toString((SSH_PORT))));
-            metaDataList.add(new RequisitionMetaData("requisition", "dcb:schedule", schedule));
+        final List<RequisitionMetaData> metaDataList = new ArrayList<>();
+        metaDataList.add(new RequisitionMetaData("requisition", "dcb:config-type", DCB_CONFIG_TYPE));
+        metaDataList.add(new RequisitionMetaData("requisition", "dcb:username", DCB_USERNAME));
+        metaDataList.add(new RequisitionMetaData("requisition", "dcb:password", DCB_PASSWORD));
+        metaDataList.add(new RequisitionMetaData("requisition", "dcb:script-file", DCB_SCRIPT_NAME));
+        metaDataList.add(new RequisitionMetaData("requisition", "dcb:ssh-port", Integer.toString((SSH_PORT))));
+        metaDataList.add(new RequisitionMetaData("requisition", "dcb:schedule", schedule));
 
+        // Nodes in default location for triggering backup from core
+        for (int i = 1; i <= NUMBER_OF_NODES; i++) {
             final RequisitionNode requisitionNode = new RequisitionNode();
             requisitionNode.setForeignId("local_" + i);
             requisitionNode.setNodeLabel("local_" + i);
@@ -174,17 +150,10 @@ public class DcbMultipleNodesIT {
         restClient.importRequisition(FOREIGN_SOURCE);
 
         final NodeDao nodeDao = STACK.postgres().dao(NodeDaoHibernate.class);
-        final IpInterfaceDao ipInterfaceDao = STACK.postgres().dao(IpInterfaceDaoHibernate.class);
-        final MonitoredServiceDao monitoredServiceDao = STACK.postgres().dao(MonitoredServiceDaoHibernate.class);
-
-        localNode = await()
-                .atMost(3, MINUTES)
+        // wait until all nodes are created
+        await().atMost(5, MINUTES)
                 .pollInterval(30, SECONDS)
-                .until(DaoUtils.findMatchingCallable(nodeDao, new CriteriaBuilder(OnmsNode.class).eq("foreignId", "local_1").toCriteria()), notNullValue());
-
-        localInterface = ipInterfaceDao.findPrimaryInterfaceByNodeId(localNode.getId());
-
-        localService = monitoredServiceDao.getPrimaryService(localNode.getId(), "DeviceConfig");
+                .until(() -> nodeDao.findByForeignSource(FOREIGN_SOURCE).size() == 100);
     }
 
     @Before
@@ -205,16 +174,10 @@ public class DcbMultipleNodesIT {
     }
 
     @Test
-    public void testGetConfigOnSchedule() throws Exception {
-        KarafShell karafShell=new KarafShell(STACK.opennms().getSshAddress());
-        karafShell.runCommand("opennms:sync-node-cache");
-
-        await().atMost(2, MINUTES)
-                .until(restClient::getBackups, is(nullValue()));
-        // Waiting for the Backup to complete.
-        Thread.sleep(5000 * 60);
-
-        Assert.assertEquals(restClient.getBackups().size(), 100);
+    public void testGetConfigOnSchedule() {
+        // Waiting for the Backups to appear.
+        await().atMost(6, MINUTES)
+                .pollInterval(30, SECONDS)
+                .until(restClient::getBackups, equalTo(NUMBER_OF_NODES));
     }
 }
-
