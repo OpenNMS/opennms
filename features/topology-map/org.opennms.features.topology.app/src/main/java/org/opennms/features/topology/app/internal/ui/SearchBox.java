adbbb977c420bdec305a6bf6f269b923a4f5d52a
Antonio Russo <rssntn67@yahoo.it>
2022-10-19 08:57:09 +0200

    Update features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/ui/SearchBox.java
    Co-authored-by: aramosvizcarra <aramosvizcarra@gmail.com>

--- features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/ui/SearchBox.java
+++ features/topology-map/org.opennms.features.topology.app/src/main/java/org/opennms/features/topology/app/internal/ui/SearchBox.java
@@ -308,7 +308,7 @@ public class SearchBox extends AbstractComponent implements SelectionListener, G
                 if (provider.supportsPrefix(query)) {
                     // If there is an '=' divider, strip it off. Otherwise, use an empty query string
                     String queryOnly = query.indexOf('=') > 0 ? query.substring(query.indexOf('=') + 1) : "";
-                    List<SearchResult> q = provider.    query(getSearchQuery(queryOnly), m_operationContext.getGraphContainer());
+                    List<SearchResult> q = provider.query(getSearchQuery(queryOnly), m_operationContext.getGraphContainer());
                     results.addAll(q);
 
                     if (m_suggestionMap.containsKey(provider)) {
