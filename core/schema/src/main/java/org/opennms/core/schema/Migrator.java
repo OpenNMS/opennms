a77280a6a5cccc5eda66f3f63a5108a521ae2d4b
Alex May <amay@opennms.com>
2022-10-24 13:03:46 -0400

    NMS-14839: Move update to an OnmsUpgrade

--- core/schema/src/main/java/org/opennms/core/schema/Migrator.java
+++ core/schema/src/main/java/org/opennms/core/schema/Migrator.java
@@ -50,7 +50,6 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -1036,8 +1035,6 @@ public class Migrator {
                 LOG.info("- Running migration for changelog: {}", resource.getDescription());
                 migrate(resource);
             }
-
-            updateSystemId();
         }
 
         if (vacuum) {
@@ -1046,55 +1043,6 @@ public class Migrator {
 
         if (iplike) {
             updateIplike();
-        }
-    }
-
-    /**
-     * Do this here to avoid installing an extension to the database
-     */
-    private void updateSystemId() throws MigrationException {
-        Connection c = null;
-        Statement st = null;
-        ResultSet rs = null;
-        try {
-            c = m_dataSource.getConnection();
-            st = c.createStatement();
-            rs = st.executeQuery("SELECT id FROM monitoringsystems WHERE location='Default' AND type='OpenNMS'");
-            if (rs.next()) {
-                String systemId = rs.getString("id");
-                if (systemId != null && systemId.equals("00000000-0000-0000-0000-000000000000")) {
-                    String newUUID = UUID.randomUUID().toString();
-                    LOG.info("Updating systemId to {}", newUUID);
-                    Statement statement = null;
-                    try {
-                        c.setAutoCommit(false);
-                        statement = c.createStatement();
-                        // For existing databases, we need to temporarily alter a foreign key
-                        // constraint to cascade updates into the alarm table
-                        statement.addBatch("ALTER TABLE alarms DROP CONSTRAINT fk_alarms_systemid");
-                        statement.addBatch("ALTER TABLE alarms ADD CONSTRAINT fk_alarms_systemid FOREIGN KEY (systemId) REFERENCES monitoringsystems (id) ON UPDATE CASCADE ON DELETE CASCADE");
-                        statement.addBatch("UPDATE monitoringsystems SET id='" + newUUID + "' WHERE id='00000000-0000-0000-0000-000000000000' AND location='Default' AND type='OpenNMS'");
-                        statement.addBatch("ALTER TABLE alarms DROP CONSTRAINT fk_alarms_systemid");
-                        statement.addBatch("ALTER TABLE alarms ADD CONSTRAINT fk_alarms_systemid FOREIGN KEY (systemId) REFERENCES monitoringsystems (id) ON DELETE CASCADE");
-                        statement.addBatch("UPDATE events SET systemid='" + newUUID + "' WHERE systemid = '00000000-0000-0000-0000-000000000000'");
-                        statement.executeBatch();
-                        c.commit();
-                    }
-                    catch (SQLException e) {
-                        c.rollback();
-                        throw e;
-                    }
-                    finally {
-                        cleanUpDatabase(null, null, statement, null);
-                    }
-                }
-            }
-        }
-        catch (SQLException e) {
-            throw new MigrationException("unable to update systemId", e);
-        }
-        finally {
-            cleanUpDatabase(c, null, st, rs);
         }
     }
 
