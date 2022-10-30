14753aa2429364f5aac315b70ce9325af81b4c50
DJ Gregor <dj@opennms.org>
2022-10-30 04:34:15 -0400

    Enable recording when there is a test failure
    Add support for using alternate Docker images in
    OpenNMSImageNameSubstitutor. It is enabled by configuration in the
    testcontainers.properties file. Use a system property to get the
    seleniarm version.
    
    Work around an issue where org.testcontainers doesn't stop recording
    when the test is done.
    
    Since we use BrowserWebDriverContainer as a @ClassRule, not a @Rule,
    add a @Rule to keep track when tests fail so we can properly tell
    BrowserWebDriverContainer.afterTest if there were failures. This lets
    it properly decide whether to save the recording.
    
    Use mp4 since pretty much everything supports it well.

--- smoke-test/src/test/java/org/opennms/smoketest/OpenNMSSeleniumIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/OpenNMSSeleniumIT.java
@@ -30,13 +30,18 @@ package org.opennms.smoketest;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.attribute.PosixFilePermissions;
+import java.util.Optional;
 
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 import org.opennms.smoketest.containers.OpenNMSContainer;
 import org.opennms.smoketest.selenium.AbstractOpenNMSSeleniumHelper;
 import org.opennms.smoketest.stacks.OpenNMSStack;
@@ -45,8 +50,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.BrowserWebDriverContainer;
+import org.testcontainers.containers.DefaultRecordingFileFactory;
 import org.testcontainers.containers.Network;
+import org.testcontainers.containers.VncRecordingContainer;
+import org.testcontainers.lifecycle.TestDescription;
 
 import com.github.dockerjava.api.command.CreateContainerCmd;
 
@@ -54,13 +64,35 @@ import com.github.dockerjava.api.command.CreateContainerCmd;
  * Base class for Selenium based testing of the OpenNMS web application.
  */
 public class OpenNMSSeleniumIT extends AbstractOpenNMSSeleniumHelper {
+    private static final Logger LOG = LoggerFactory.getLogger(OpenNMSSeleniumIT.class);
 
-    public static final BrowserWebDriverContainer firefox = (BrowserWebDriverContainer) new BrowserWebDriverContainer()
+    /**
+     * When to save web browser video recordings.
+     * The default, RECORD_FAILING will only save recordings when there are failed tests.
+     * Other options: SKIP or RECORD_ALL.
+     *
+     * Historical warning: This container can fail and cause test instability - for example if retrieving the
+     * recording from the container fails, an exception will be thrown causing the test to fail.
+     *
+     * Although RECORD_FAILING is the default in BrowserWebDriverContainer, the constructor we use resets
+     * it to SKIP. :( So we always explicitly set it.
+     */
+    private static final BrowserWebDriverContainer.VncRecordingMode RECORDING_MODE =
+            BrowserWebDriverContainer.VncRecordingMode.RECORD_FAILING; // RECORD_ALL, RECORD_FAILING, or SKIP
+
+    // Use mp4 since everything supports it
+    private static final VncRecordingContainer.VncRecordingFormat RECORDING_FORMAT =
+            VncRecordingContainer.VncRecordingFormat.MP4;
+
+    private static final File RECORDING_DIRECTORY = new File("target");
+
+    private static final CachingRecordingFileFactory RECORDING_FILE_FACTORY = new CachingRecordingFileFactory();
+
+    public static final WorkaroundBrowserWebDriverContainer firefox =
+            (WorkaroundBrowserWebDriverContainer) new WorkaroundBrowserWebDriverContainer()
             .withCapabilities(getFirefoxOptions())
-            // Don't record. This container can fail and cause test instability - for example if retrieving the
-            // .flv from the container fails, an exception will be thrown causing the test to fail
-            .withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.SKIP, new File("target"))
-            //.withRecordingMode(BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL, new File("target"))
+            .withRecordingMode(RECORDING_MODE, RECORDING_DIRECTORY, RECORDING_FORMAT)
+            .withRecordingFileFactory(RECORDING_FILE_FACTORY)
             .withNetwork(Network.SHARED)
             // Increase the containers shared memory to 2GB to help prevent Firefox from crashing
             .withSharedMemorySize(2147483648L)
@@ -118,6 +150,26 @@ public class OpenNMSSeleniumIT extends AbstractOpenNMSSeleniumHelper {
         driver = firefox.getWebDriver();
     }
 
+    public static Optional<Throwable> failed = Optional.empty();
+
+    /**
+     * @ClassRule does not seem to know if tests failed, but afterTest needs
+     * to know if a screen recording should be saved. Store the latest failure
+     * exception here. The specific failure and exception doesn't matter for
+     * this, we just need to know if there are >0 exceptions for afterTest.
+     */
+    @Rule(order = Integer.MIN_VALUE)
+    public TestWatcher watchman= new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, Description description) {
+            failed = Optional.of(e);
+        }
+
+        @Override
+        protected void succeeded(Description description) {
+        }
+    };
+
     @Override
     public WebDriver getDriver() {
         return driver;
@@ -133,4 +185,81 @@ public class OpenNMSSeleniumIT extends AbstractOpenNMSSeleniumHelper {
         return stack.opennms().getBaseUrlExternal().toString();
     }
 
+    /**
+     * We work around some bugs and shortcomings in org.testcontainers' BrowserWebDriverContainer here until we can
+     * submit issues/PRs upstream and the fixes are released.
+     * First, when a test is complete, nothing is done to tell the VNC recorder Docker image to stop recording.
+     * Transcoding the recording is CPU-intensive and single-threaded and happens while new data is coming in if the
+     * VNC recorder process is still running. Unluckily for us, because of our large screen size, the transcoder can't
+     * keep up with the incoming data from the VNC recorder, so it never gets to EOF on its input file and will run
+     * forever. We work around that by sending a SIGSTOP to the VNC recorder process inside its Docker container (we
+     * can't just stop the container because we need it around and running because that's where the transcoder runs).
+     * Lastly, we provide an alternate RecordingFileFactory that caches the recording file name and we output the
+     * full path as a URI to make it easier to view the recordings.
+     * @param <SELF>
+     */
+    public static class WorkaroundBrowserWebDriverContainer<SELF extends BrowserWebDriverContainer<SELF>>
+            extends BrowserWebDriverContainer<SELF> {
+        @Override
+        public void beforeTest(TestDescription description) {
+            // These are static and reused across all test classes, so reset before every test class run
+            RECORDING_FILE_FACTORY.recordingFile = null;
+            OpenNMSSeleniumIT.failed = Optional.empty();
+
+            super.beforeTest(description);
+        }
+
+        @Override
+        public void afterTest(TestDescription description, Optional<Throwable> throwable) {
+            try {
+                Field f = firefox.getClass().getSuperclass().getDeclaredField("vncRecordingContainer");
+                f.setAccessible(true);
+                VncRecordingContainer vncRecordingContainer = (VncRecordingContainer) f.get(firefox);
+                if (vncRecordingContainer != null && vncRecordingContainer.isRunning()) {
+                    var command = "kill -STOP $(grep -l -F flvrec.py /proc/*/cmdline | sed 's,^/proc/,,;s,/.*,,' "
+                            + "| grep '^[0-9]*$' | grep -v '^1$' | sort -n | head  -1)";
+                    var results = vncRecordingContainer.execInContainer("sh", "-c", command);
+                    if (results.getExitCode() != 0) {
+                        throw new RuntimeException("Got non-zero exit code " + results.getExitCode() + " when attempting to "
+                                + "send a SIGSTOP to the VNC recorder process in the container.\n"
+                                + "Command: " + command + "\n"
+                                + "Stdout: " + results.getStdout() + "\n"
+                                + "Stderr: " + results.getStderr());
+                    }
+                }
+            } catch (NoSuchFieldException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            // This is where the recording is transcoded and extracted to the local system.
+            super.afterTest(description, OpenNMSSeleniumIT.failed);
+
+            // I really wanted a better message with a clickable URI at the end of the test.
+            if (RECORDING_FILE_FACTORY.recordingFile != null &&
+                    RECORDING_FILE_FACTORY.recordingFile.exists()) {
+                LOG.info("Recording: {}", RECORDING_FILE_FACTORY.recordingFile.toURI());
+            }
+        }
+    }
+
+    public static class CachingRecordingFileFactory extends DefaultRecordingFileFactory {
+        public File recordingFile = null;
+
+        @Override
+        public File recordingFileForTest(
+                File vncRecordingDirectory,
+                String prefix,
+                boolean succeeded,
+                VncRecordingContainer.VncRecordingFormat recordingFormat
+        ) {
+            recordingFile = super.recordingFileForTest(vncRecordingDirectory, prefix, succeeded, recordingFormat);
+            return recordingFile;
+        }
+    }
 }
