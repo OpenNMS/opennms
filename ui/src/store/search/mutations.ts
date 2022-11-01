d56150e1f1cd48ddf89086c596c7a28e01e266e8
scthompsonopen <86311553+scthompsonopen@users.noreply.github.com>
2022-11-01 13:15:09 -0300

    NMS-14903: Update Vue Search Styles to Match Classic (#5453)
    * adding search result icons. modifying search results in vue to match styles from results in classic, which has the better user experience. 
    * Created SearchHeader and SearchResult components for better seperation of concerns. 
    
    * Moved some search functionality from component to store in order to tidy up component.
    
    * improving accessibility of the search results and the new menu at the same time

--- ui/src/store/search/mutations.ts
+++ ui/src/store/search/mutations.ts
@@ -1,10 +1,17 @@
-import { SearchResultResponse } from '@/types'
+import { SearchResultResponse, SearchResultsByContext } from '@/types'
 import { State } from './state'
 
-const SAVE_SEARCH_RESULTS = (state: State, results: SearchResultResponse[]) => {
-  state.searchResults = results
+const SAVE_SEARCH_RESULTS = (state: State, items:{responses: SearchResultResponse[], responsesByLabel: SearchResultsByContext}) => {
+  state.searchResults = items.responses
+  state.searchResultsByContext = [...items.responsesByLabel]
 }
 
+const SET_LOADING = (state: State, loading: boolean) => {
+  state.loading = loading
+}
+
+
 export default {
-  SAVE_SEARCH_RESULTS
+  SAVE_SEARCH_RESULTS,
+  SET_LOADING
 }
