c87b56e08fd8b05267f4594c3f45798f6458c74c
Alexander May <amay@remmamay3.attlocal.net>
2023-01-11 09:13:59 -0500

    NMS-15040: Remove wildcard imports

--- opennms-webapp-rest/src/main/java/org/opennms/web/rest/v2/NodeRestService.java
+++ opennms-webapp-rest/src/main/java/org/opennms/web/rest/v2/NodeRestService.java
@@ -34,7 +34,13 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import javax.ws.rs.*;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.container.ResourceContext;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
