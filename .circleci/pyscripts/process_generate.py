8c5bc0830d26a4540c4584dd47fbabb61fed7f42
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2023-01-23 17:11:59 -0500

    put trivy scan into their own workflow

--- .circleci/pyscripts/process_generate.py
+++ .circleci/pyscripts/process_generate.py
@@ -106,6 +106,8 @@ for change in changed_files:
         add_to_build_list("Integration_tests")
     elif "src/test/" in change and "smoke-test/" in change:
         add_to_build_list("smoke_tests")
+    elif "trivy-config/trivyignore" in change:
+        add_to_build_list("trivy-scan")
     elif "opennms-container" in change:
         add_to_build_list("oci")
     elif ".circleci" in change and ".circleci/epoch" not in change:
@@ -176,6 +178,7 @@ else:
         "debs": False,
         "oci": False,
         "build-publish": False,
+        "trivy-scan": False,
         "experimental": False,
     }
 
@@ -306,6 +309,9 @@ for keyword in git_keywords:
             build_mappings["oci"] = True
         if "build-publish" in keyword:
             build_mappings["build-publish"] = True
+        if "trivy-scan" in keyword:
+            build_mappings["trivy-scan"] = True
+
 
 
 if "smoke" in git_keywords or "smoke_tests" in What_to_build:
@@ -319,6 +325,9 @@ if "oci" in git_keywords:
 if "rpms" in git_keywords:
     build_mappings["rpms"] = True
 
+if "trivy-scan" in git_keywords:
+    build_mappings["trivy-scan"] = True
+    
 if "debs" in git_keywords:
     build_mappings["debs"] = True
 
