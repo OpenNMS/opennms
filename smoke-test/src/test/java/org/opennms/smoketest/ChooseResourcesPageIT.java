a47e193f820c222b309763b508009e54b2f0e7f5
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2023-01-30 14:06:12 +0100

    NMS-15292: Escaping parameters in graph results page

--- smoke-test/src/test/java/org/opennms/smoketest/ChooseResourcesPageIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/ChooseResourcesPageIT.java
@@ -34,6 +34,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -71,6 +72,17 @@ public class ChooseResourcesPageIT extends OpenNMSSeleniumIT {
         } finally {
             // CLEANUP
             deleteTestRequisition();
+        }
+    }
+
+    @Test
+    public void testNMS15292() {
+        final String[] parameters = {"zoom=true", "generatedId=4ab5197d-7435-30d5-85ea-437432ff8e44", "nodeCriteria=42", "reports=all"};
+        final String scriptTag = "%22%3E%3Cscript%3Ealert(%27Failed!%27)%3B%3C%2Fscript%3E";
+
+        for (final String parameter : parameters) {
+            getDriver().get(getBaseUrlInternal() + "opennms/graph/results.htm?" + parameter + scriptTag);
+            new WebDriverWait(driver, 5).until(pageContainsText("The OpenNMS Group"));
         }
     }
 
