36affc3c1b09a67d6b0dfcb1fb3b58dd86a48232
Alex May <amay@opennms.com>
2023-02-08 10:56:49 -0500

    NMS-14966: Try to read summary endpoint?

--- smoke-test/src/test/java/org/opennms/smoketest/TimeseriesAPIIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/TimeseriesAPIIT.java
@@ -31,12 +31,18 @@ package org.opennms.smoketest;
 import static org.junit.Assert.assertTrue;
 import static com.jayway.awaitility.Awaitility.await;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Base64;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.opennms.smoketest.containers.OpenNMSContainer;
 import org.opennms.smoketest.stacks.OpenNMSStack;
 import org.opennms.smoketest.utils.KarafShell;
 import org.opennms.smoketest.utils.KarafShellUtils;
@@ -48,7 +54,7 @@ public class TimeseriesAPIIT {
     private static final Logger LOG = LoggerFactory.getLogger(TimeseriesAPIIT.class);
 
     @ClassRule
-    public static OpenNMSStack stack = OpenNMSStack.MINIMAL;
+    public static OpenNMSStack stack = OpenNMSStack.MINIMAL_WITH_DEFAULT_LOCALHOST;
 
     private KarafShell karafShell = new KarafShell(stack.opennms().getSshAddress());
 
@@ -70,8 +76,59 @@ public class TimeseriesAPIIT {
         assertTrue(karafShell.runCommandOnce("feature:install opennms-timeseries-api", output -> !output.toLowerCase().contains("error"), false));
         assertTrue(karafShell.runCommandOnce("feature:install inmemory-timeseries-plugin", output -> !output.toLowerCase().contains("error"), false));
         KarafShellUtils.testHealthCheckSucceeded(stack.opennms().getSshAddress());
-        await().atMost(5, TimeUnit.MINUTES)
+        printSummary();
+        long start = System.currentTimeMillis();
+        await().atMost(10, TimeUnit.MINUTES)
                 .pollInterval(5, TimeUnit.SECONDS)
-                .until(() -> karafShell.runCommandOnce("opennms:get-tss-plugin-metrics", output -> !output.isEmpty(), false));
+                .until(() -> karafShell.runCommandOnce("opennms:get-tss-plugin-metrics", output -> output.contains("data points"), false));
+        String summaryUrl = String.format(
+                "http://0.0.0.0:%d/opennms/summary/results.htm?filterRule=ipaddr+iplike+*.*.*.*&startTime=%d&endTime=%d&attributeSieve=.*",
+                stack.opennms().getWebPort(),
+                0,
+                System.currentTimeMillis());
+        Thread.sleep(120000);
+        System.out.println("<KARAF OUT>");
+        karafShell.runCommandOnce("opennms:get-tss-plugin-metrics", output -> {
+            System.out.println(output);
+            return !output.isEmpty();
+        }
+        , false);
+        System.out.println("</KARAF OUT>");
+        System.out.println("SUMMARY URL: " + summaryUrl);
+        URL summary = new URL(summaryUrl);
+        URLConnection connection = summary.openConnection();
+        String userpass = String.format("%s:%s", OpenNMSContainer.ADMIN_USER, OpenNMSContainer.ADMIN_PASSWORD);
+        String basicAuth = new String(Base64.getEncoder().encode(userpass.getBytes()));
+        connection.setRequestProperty("Authorization", "Basic " + basicAuth);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String input;
+        System.out.println("<SUMMARY CONTENTS>");
+        while ((input = reader.readLine()) != null) {
+            System.out.println(input);
+        }
+        System.out.println("</SUMMARY CONTENTS>");
     }
+
+    private void printSummary() throws Exception {
+
+        String summaryUrl = String.format(
+                "http://0.0.0.0:%d/opennms/summary/results.htm?filterRule=ipaddr+iplike+*.*.*.*&startTime=%d&endTime=%d&attributeSieve=.*",
+                stack.opennms().getWebPort(),
+                0,
+                System.currentTimeMillis());
+        URL summary = new URL(summaryUrl);
+        URLConnection connection = summary.openConnection();
+        String userpass = String.format("%s:%s", OpenNMSContainer.ADMIN_USER, OpenNMSContainer.ADMIN_PASSWORD);
+        String basicAuth = new String(Base64.getEncoder().encode(userpass.getBytes()));
+        connection.setRequestProperty("Authorization", "Basic " + basicAuth);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String input;
+        System.out.println("<SUMMARY CONTENTS>");
+        while ((input = reader.readLine()) != null) {
+            System.out.println(input);
+        }
+        System.out.println("</SUMMARY CONTENTS>");
+
+    }
+
 }
