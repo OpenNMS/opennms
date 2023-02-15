17699d1e27d0cb51c15dda4bced3b5331f09750b
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2023-02-15 15:41:57 -0500

    add plugins to Sentinel

--- opennms-container/core/plugins.sh
+++ opennms-container/core/plugins.sh
@@ -17,6 +17,9 @@ done
 dpkg-deb -R *-plugin-cloud_*_all.deb ./
 find . -name '*.kar' -exec mv {} /usr/share/opennms/deploy \;
 
+cd ..
+rm -r test
+
 cd /usr/share/opennms/deploy || exit 
 urls=$(curl --silent https://api.github.com/repos/OpenNMS/opennms-cortex-tss-plugin/releases | jq -r '.[0].assets[0].browser_download_url')
 for url in $urls; do
---
+++ opennms-container/sentinel/plugins.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir ~/test
+cd ~/test || exit
+urls=$(cloudsmith list packages --query="sentinel-alec-plugin tag:latest" opennms/common -F json  | jq -r '.data[].cdn_url')
+for url in $urls; do 
+ wget "$url"
+done
+dpkg-deb -R *-alec-plugin_*_all.deb ./
+find . -name '*.kar' -exec mv {} /usr/share/opennms/deploy \;
+
+
+urls=$(cloudsmith list packages --query="sentinel-plugin-cloud tag:latest" opennms/common -F json  | jq -r '.data[].cdn_url')
+for url in $urls; do
+    wget "$url"
+done
+dpkg-deb -R *-plugin-cloud_*_all.deb ./
+find . -name '*.kar' -exec mv {} /usr/share/opennms/deploy \;
+
+cd ..
+rm -r test
