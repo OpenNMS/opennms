1b04bd8e0105b72fb624955c563e31c53cc26911
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2023-02-21 15:13:40 -0500

    Move extra steps from dockerfile to the script file

--- opennms-container/core/plugins.sh
+++ opennms-container/core/plugins.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+export ALEC_VERSION="latest"
+export CLOUD_VERSION="latest"
+export CORTEX_VERSION="latest"
+
+apt-get update
+apt-get install -y python3-pip wget curl jq
+pip3 install --upgrade cloudsmith-cli 
+
 mkdir ~/test
 cd ~/test || exit
-urls=$(cloudsmith list packages --query="opennms-alec-plugin tag:latest" opennms/common -F json  | jq -r '.data[].cdn_url')
+urls=$(cloudsmith list packages --query="opennms-alec-plugin version:$ALEC_VERSION format:deb" opennms/common -F json  | jq -r '.data[].cdn_url')
 for url in $urls; do 
  wget "$url"
 done
@@ -10,7 +18,7 @@ dpkg-deb -R *-alec-plugin_*_all.deb ./
 find . -name '*.kar' -exec mv {} /usr/share/opennms/deploy \;
 
 
-urls=$(cloudsmith list packages --query="opennms-plugin-cloud tag:latest" opennms/common -F json  | jq -r '.data[].cdn_url')
+urls=$(cloudsmith list packages --query="opennms-plugin-cloud version:$CLOUD_VERSION format:deb" opennms/common -F json  | jq -r '.data[].cdn_url')
 for url in $urls; do
     wget "$url"
 done
@@ -21,7 +29,17 @@ cd ..
 rm -r test
 
 cd /usr/share/opennms/deploy || exit 
-urls=$(curl --silent https://api.github.com/repos/OpenNMS/opennms-cortex-tss-plugin/releases | jq -r '.[0].assets[0].browser_download_url')
+if [ $CORTEX_VERSION == "latest" ]
+then
+ urls=$(curl --silent https://api.github.com/repos/OpenNMS/opennms-cortex-tss-plugin/releases | jq -r '.[0].assets[0].browser_download_url')
+else
+ urls=$(curl --silent https://api.github.com/repos/OpenNMS/opennms-cortex-tss-plugin/releases | jq -r '.[] | select(.tag_name=="$CORTEX_TAG") | .assets[0].browser_download_url')
+fi
 for url in $urls; do
     wget "$url"
 done
+
+apt-get remove -y python3-pip wget curl jq 
+apt-get clean
+rm -rf /var/cache/apt /var/lib/apt/lists/* /tmp/security.sources.list 
+rm -rf ~/.cache/pip
--- opennms-container/sentinel/plugins.sh
+++ opennms-container/sentinel/plugins.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+export ALEC_VERSION="latest"
+export CLOUD_VERSION="latest"
+
+apt-get update
+apt-get install -y python3-pip wget curl jq
+pip3 install --upgrade cloudsmith-cli 
+
 mkdir ~/test
 cd ~/test || exit
-urls=$(cloudsmith list packages --query="sentinel-alec-plugin tag:latest" opennms/common -F json  | jq -r '.data[].cdn_url')
+urls=$(cloudsmith list packages --query="sentinel-alec-plugin version:$ALEC_VERSION format:deb" opennms/common -F json  | jq -r '.data[].cdn_url')
 for url in $urls; do 
  wget "$url"
 done
@@ -10,7 +17,7 @@ dpkg-deb -R *-alec-plugin_*_all.deb ./
 find . -name '*.kar' -exec mv {} /usr/share/opennms/deploy \;
 
 
-urls=$(cloudsmith list packages --query="sentinel-plugin-cloud tag:latest" opennms/common -F json  | jq -r '.data[].cdn_url')
+urls=$(cloudsmith list packages --query="sentinel-plugin-cloud version:$CLOUD_VERSION format:deb" opennms/common -F json  | jq -r '.data[].cdn_url')
 for url in $urls; do
     wget "$url"
 done
@@ -19,3 +26,8 @@ find . -name '*.kar' -exec mv {} /usr/share/opennms/deploy \;
 
 cd ..
 rm -r test
+
+apt-get remove -y python3-pip wget curl jq 
+apt-get clean
+rm -rf /var/cache/apt /var/lib/apt/lists/* /tmp/security.sources.list 
+rm -rf ~/.cache/pip
