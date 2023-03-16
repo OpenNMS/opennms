244ddcb752909080e2b6fadc9ba3dcd7cc321c5d
Bonrob2 <59453630+Bonrob2@users.noreply.github.com>
2023-03-16 10:39:40 -0400

    Update opennms-web-api/src/main/java/org/opennms/web/servlet/AddStrictTransportSecurityHeaderFilter.java

--- opennms-web-api/src/main/java/org/opennms/web/servlet/AddStrictTransportSecurityHeaderFilter.java
+++ opennms-web-api/src/main/java/org/opennms/web/servlet/AddStrictTransportSecurityHeaderFilter.java
@@ -8,7 +8,7 @@
  *
  * OpenNMS(R) is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License,
+ * by the Free Software Foundation, either version 3 of the license,
  * or (at your option) any later version.
  *
  * OpenNMS(R) is distributed in the hope that it will be useful,
