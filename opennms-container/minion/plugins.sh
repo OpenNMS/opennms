ab4b21f85c4b80e90a41eeb18eee73af5eb1d9f6
Benjamin Reed <ranger@opennms.org>
2023-06-29 12:01:09 -0400

    -F does not work with -O, also some other cleanup

--- opennms-container/minion/plugins.sh
+++ opennms-container/minion/plugins.sh
@@ -7,19 +7,19 @@ IFS=$'\n\t'
 export VELOCLOUD_VERSION="latest"
 export DEPLOY_FOLDER="/opt/usr-plugins"
 
-mkdir $DEPLOY_FOLDER 
+mkdir -p "$DEPLOY_FOLDER"
 
 microdnf -y install jq
 
 cd $DEPLOY_FOLDER || exit 
 if [ $VELOCLOUD_VERSION == "latest" ]
 then
- artifact_urls=$(curl -sSF https://api.github.com/repos/OpenNMS/opennms-velocloud-plugin/releases | jq -r '.[0].assets[0].browser_download_url')
+ artifact_urls=$(curl -sS https://api.github.com/repos/OpenNMS/opennms-velocloud-plugin/releases | jq -r '.[0].assets[0].browser_download_url')
 else
- artifact_urls=$(curl -sSF https://api.github.com/repos/OpenNMS/opennms-velocloud-plugin/releases | jq -r '.[] | select(.tag_name=="$VELOCLOUD_VERSION") | .assets[0].browser_download_url')
+ artifact_urls=$(curl -sS https://api.github.com/repos/OpenNMS/opennms-velocloud-plugin/releases | jq -r '.[] | select(.tag_name=="$VELOCLOUD_VERSION") | .assets[0].browser_download_url')
 fi
 if [ -n "$artifact_urls" ]; then
  for url in $artifact_urls; do
-    curl -sSF -L -O "$url"
+    curl -sS -L -O "$url"
  done
 fi
--- opennms-container/sentinel/plugins.sh
+++ opennms-container/sentinel/plugins.sh
@@ -5,6 +5,8 @@ export CLOUD_VERSION="latest"
 #export DEPLOY_FOLDER="/usr/share/opennms/deploy" 
 export DEPLOY_FOLDER="/opt/usr-plugins"
 
+mkdir -p "$DEPLOY_FOLDER"
+
 microdnf -y install cpio python3-pip jq
 pip3 install --upgrade cloudsmith-cli 
 
@@ -12,18 +14,18 @@ mkdir ~/test
 cd ~/test || exit
 urls=$(cloudsmith list packages --query="sentinel-alec-plugin version:$ALEC_VERSION format:rpm" opennms/common -F json  | jq -r '.data[].cdn_url')
 for url in $urls; do 
- curl -sSF -L -O "$url"
+ curl -sS -L -O "$url"
 done
 rpm2cpio *-alec-plugin*.rpm | cpio -id
-find . -name '*.kar' -exec mv {} $DEPLOY_FOLDER \;
+find . -name '*.kar' -exec mv '{}' "$DEPLOY_FOLDER" \;
 
 
 urls=$(cloudsmith list packages --query="sentinel-plugin-cloud version:$CLOUD_VERSION format:rpm" opennms/common -F json  | jq -r '.data[].cdn_url')
 for url in $urls; do
-    curl -sSF -L -O "$url"
+    curl -sS -L -O "$url"
 done
 rpm2cpio *-plugin-cloud*.rpm | cpio -id
-find . -name '*.kar' -exec mv {} $DEPLOY_FOLDER \;
+find . -name '*.kar' -exec mv '{}' "$DEPLOY_FOLDER" \;
 
 cd ..
 rm -r test
