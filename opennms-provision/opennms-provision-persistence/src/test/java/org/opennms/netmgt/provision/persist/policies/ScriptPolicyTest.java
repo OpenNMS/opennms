37de051b2e2b94470da7bc92acefc024740fe3b2
Benjamin Reed <ranger@opennms.org>
2023-07-05 11:09:18 -0400

    fix some missing changes not kept during merge

--- opennms-provision/opennms-provision-persistence/src/test/java/org/opennms/netmgt/provision/persist/policies/ScriptPolicyTest.java
+++ opennms-provision/opennms-provision-persistence/src/test/java/org/opennms/netmgt/provision/persist/policies/ScriptPolicyTest.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2018 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2018 The OpenNMS Group, Inc.
+ * Copyright (C) 2018-2023 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2023 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -42,6 +42,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -53,6 +54,7 @@ import org.opennms.netmgt.model.PrimaryType;
 import org.opennms.netmgt.model.monitoringLocations.OnmsMonitoringLocation;
 import org.opennms.netmgt.provision.persist.JSR223ScriptCache;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class ScriptPolicyTest {
 
     private final JSR223ScriptCache scriptCache = new JSR223ScriptCache();
