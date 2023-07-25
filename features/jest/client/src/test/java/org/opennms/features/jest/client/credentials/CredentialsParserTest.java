0aedd2168df94a48f43b5f9b7526cb04fff4e137
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2023-07-25 10:37:30 +0200

    NMS-15752: Allow Elasticsearch credentials to be stored in SCV

--- features/jest/client/src/test/java/org/opennms/features/jest/client/credentials/CredentialsParserTest.java
+++ features/jest/client/src/test/java/org/opennms/features/jest/client/credentials/CredentialsParserTest.java
@@ -30,7 +30,10 @@ package org.opennms.features.jest.client.credentials;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.http.HttpHost;
@@ -39,6 +42,10 @@ import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.opennms.core.mate.api.ContextKey;
+import org.opennms.core.mate.api.EntityScopeProvider;
+import org.opennms.core.mate.api.MapScope;
+import org.opennms.core.mate.api.Scope;
 
 public class CredentialsParserTest {
     @Test
@@ -47,19 +54,26 @@ public class CredentialsParserTest {
 
         // Valid
         configuration.withCredentials(new CredentialsScope("192.168.0.1:9200", "ulf", "ulf"));
-        configuration.withCredentials(new CredentialsScope("http://192.168.0.2:9200", "ulf", "ulf2"));
+        configuration.withCredentials(new CredentialsScope("http://192.168.0.2:9200", "${scv:elastic:username}", "${scv:elastic:password}"));
         configuration.withCredentials(new CredentialsScope("https://192.168.0.3:9300", "ulf", "ulf3"));
 
         // Invalid
         configuration.withCredentials(new CredentialsScope("http://192.168.0.1:x", "ulf", "ulf"));
         configuration.withCredentials(new CredentialsScope("192.168.0.1", "ulf", null));
 
-        final Map<AuthScope, Credentials> credentials = new CredentialsParser().parse(configuration.getCredentialsScopes());
+        final Map<ContextKey, String> scopeMap = new HashMap<>();
+        scopeMap.put(new ContextKey("scv","elastic:username"),"foo");
+        scopeMap.put(new ContextKey("scv","elastic:password"),"bar");
+
+        final EntityScopeProvider entityScopeProvider = mock(EntityScopeProvider.class);
+        when(entityScopeProvider.getScopeForScv()).thenReturn(new MapScope(Scope.ScopeName.DEFAULT, scopeMap));
+
+        final Map<AuthScope, Credentials> credentials = new CredentialsParser().parse(configuration.getCredentialsScopes(), entityScopeProvider);
 
         // Verify
         assertThat(credentials.size(), is(3));
         assertThat(credentials, Matchers.hasEntry(new AuthScope(new HttpHost("192.168.0.1", 9200, "http")), new UsernamePasswordCredentials("ulf", "ulf")));
-        assertThat(credentials, Matchers.hasEntry(new AuthScope(new HttpHost("192.168.0.2", 9200,"http")), new UsernamePasswordCredentials("ulf", "ulf2")));
+        assertThat(credentials, Matchers.hasEntry(new AuthScope(new HttpHost("192.168.0.2", 9200,"http")), new UsernamePasswordCredentials("foo", "bar")));
         assertThat(credentials, Matchers.hasEntry(new AuthScope(new HttpHost("192.168.0.3", 9300, "https")), new UsernamePasswordCredentials("ulf", "ulf3")));
     }
 }
