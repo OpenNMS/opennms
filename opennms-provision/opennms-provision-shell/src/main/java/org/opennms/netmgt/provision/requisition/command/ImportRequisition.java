f35044594dd0f157a9637968f4af0fb9066c8021
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2024-02-21 13:05:55 +0100

    NMS-16343: Added parameter to wait for events

--- opennms-provision/opennms-provision-shell/src/main/java/org/opennms/netmgt/provision/requisition/command/ImportRequisition.java
+++ opennms-provision/opennms-provision-shell/src/main/java/org/opennms/netmgt/provision/requisition/command/ImportRequisition.java
@@ -25,6 +25,12 @@ import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.Future;
+import java.util.function.BooleanSupplier;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.karaf.shell.api.action.Action;
@@ -36,9 +42,13 @@ import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
 import org.opennms.netmgt.events.api.EventConstants;
 import org.opennms.netmgt.events.api.EventForwarder;
+import org.opennms.netmgt.events.api.EventListener;
+import org.opennms.netmgt.events.api.EventSubscriptionService;
+import org.opennms.netmgt.events.api.model.IEvent;
 import org.opennms.netmgt.model.events.EventBuilder;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 
 @Command(scope = "opennms", name = "import-requisition", description = "Sends an 'uei.opennms.org/internal/importer/reloadImport' event to import the requisition from a given url parameter")
 @Service
@@ -50,6 +60,12 @@ public class ImportRequisition implements Action {
     @Reference
     private EventForwarder eventForwarder;
 
+    @Reference
+    private EventSubscriptionService eventSubscriptionService;
+
+    @Option(name = "-w", aliases = "--wait", description = "Wait for completion event")
+    private Boolean wait = false;
+
     @Option(name = "-r", aliases = "--rescan", description = "Specify rescanExisting value, valid values : 'true', 'false', 'dbonly'")
     private String rescanExisting;
 
@@ -60,12 +76,55 @@ public class ImportRequisition implements Action {
     @Argument(index = 1, name = "parameters", description = "Provide parameters in key=value form", multiValued = true)
     private List<String> parameters = new LinkedList<>();
 
-    @Override
-    public Object execute() throws Exception {
-        return sendImportRequisitionEvent(eventForwarder, type, parameters, rescanExisting);
+    static class ImportEventListener implements EventListener {
+        public final static List<String> UEIS = Lists.newArrayList(EventConstants.IMPORT_SUCCESSFUL_UEI, EventConstants.IMPORT_FAILED_UEI);
+        private String importResource;
+        private String receivedUei;
+
+        ImportEventListener(final String importResource) {
+            this.importResource = stripCredentials(importResource);
+        }
+
+        static String stripCredentials(final String string) {
+            if (string == null) {
+                return null;
+            } else {
+                return string.replaceAll("(username=)[^;&]*(;&)?", "$1***$2")
+                             .replaceAll("(password=)[^;&]*(;&)?", "$1***$2");
+            }
+        }
+
+        @Override
+        public String getName() {
+            return "ImportRequisition-eventListener";
+        }
+
+        String getReceivedUei() {
+            return this.receivedUei;
+        }
+
+        @Override
+        public void onEvent(final IEvent e) {
+            if (!e.getParm("importResource").getValue().getContent().contains(this.importResource)) {
+                return;
+            }
+
+            if (e.getUei().equals(EventConstants.IMPORT_SUCCESSFUL_UEI) || e.getUei().equals(EventConstants.IMPORT_FAILED_UEI)) {
+                this.receivedUei = e.getUei();
+            }
+        }
+
+        boolean isDone() {
+            return this.receivedUei != null;
+        }
     }
 
-    public static Object sendImportRequisitionEvent(EventForwarder eventForwarder, String type, List<String> parameters, String rescanExisting) throws URISyntaxException {
+    @Override
+    public Object execute() throws Exception {
+        return sendImportRequisitionEvent(eventForwarder, type, parameters, rescanExisting, wait, eventSubscriptionService);
+    }
+
+    public static Object sendImportRequisitionEvent(EventForwarder eventForwarder, String type, List<String> parameters, String rescanExisting, boolean wait, EventSubscriptionService eventSubscriptionService) throws URISyntaxException {
         EventBuilder eventBuilder = new EventBuilder(EventConstants.RELOAD_IMPORT_UEI, EVENT_SOURCE);
         URIBuilder builder = new URIBuilder().setScheme(URI_SCHEME).setHost(type);
         parse(parameters, builder);
@@ -80,8 +139,36 @@ public class ImportRequisition implements Action {
                 return null;
             }
         }
+
+        final ImportEventListener importEventListener = new ImportEventListener(url);
+        if (wait) {
+            eventSubscriptionService.addEventListener(importEventListener, ImportEventListener.UEIS);
+        }
+
         eventForwarder.sendNow(eventBuilder.getEvent());
         System.out.printf("Requisition import triggered asynchronously for URL:\n\t%s\n", url);
+
+        if (wait) {
+            try {
+                while (!importEventListener.isDone()) {
+                    try {
+                        Thread.sleep(1000);
+                        System.out.print(".");
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                if (EventConstants.IMPORT_SUCCESSFUL_UEI.equals(importEventListener.getReceivedUei())) {
+                    System.out.printf("\nImport succeeded.\n");
+                } else {
+                    System.out.printf("\nImport failed.\n");
+                }
+            } finally {
+                eventSubscriptionService.removeEventListener(importEventListener);
+            }
+        }
+
         return null;
     }
 
@@ -99,5 +186,4 @@ public class ImportRequisition implements Action {
             }
         }
     }
-
 }
