0a72ba012194059b99645e4b25bf3680d7ed59de
Benjamin Reed <ranger@opennms.org>
2024-04-11 09:23:25 -0400

    bump the Java memory on all containers

--- smoke-test/src/main/java/org/opennms/smoketest/containers/OpenNMSContainer.java
+++ smoke-test/src/main/java/org/opennms/smoketest/containers/OpenNMSContainer.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
  * This file is part of OpenNMS(R).
  *
- * Copyright (C) 2019-2023 The OpenNMS Group, Inc.
- * OpenNMS(R) is Copyright (C) 1999-2023 The OpenNMS Group, Inc.
+ * Copyright (C) 2019-2024 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2024 The OpenNMS Group, Inc.
  *
  * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
  *
@@ -167,7 +167,7 @@ public class OpenNMSContainer extends GenericContainer<OpenNMSContainer> impleme
                 .mapToInt(Map.Entry::getValue)
                 .toArray();
 
-        String javaOpts = "-Xms2048m -Xmx2048m -Djava.security.egd=file:/dev/./urandom";
+        String javaOpts = "-Xms4g -Xmx4g -Djava.security.egd=file:/dev/./urandom";
         if (COLLECT_COVERAGE) {
             javaOpts += " -javaagent:/opt/opennms/agent/jacoco-agent.jar=output=none,jmx=true,excludes=org.drools.*";
         }
