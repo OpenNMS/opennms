2689df193d7b8593a352cf50a339fce5feca76df
Markus von Rüden <m.v.rueden@open-email.de>
2024-06-11 20:10:47 +0200

    chg: remove new ui

--- ui/src/.d.ts
+++
@@ -1,1 +0,0 @@
-declare module '@vue-leaflet/vue-leaflet'
--- ui/src/types/scv.d.ts
+++
@@ -1,6 +0,0 @@
-export interface SCVCredentials {
-  alias: string
-  username: string
-  password: string
-  attributes: Record<string, string>
-}
