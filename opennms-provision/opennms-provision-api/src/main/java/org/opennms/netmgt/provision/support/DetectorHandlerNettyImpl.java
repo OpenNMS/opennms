d7d9d16e415685e847e9fad68e90c40b8807c427
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2024-06-26 08:04:56 +0200

    NMS-16184: Upgrade to netty4

--- opennms-provision/opennms-provision-api/src/main/java/org/opennms/netmgt/provision/support/DetectorHandlerNettyImpl.java
+++ opennms-provision/opennms-provision-api/src/main/java/org/opennms/netmgt/provision/support/DetectorHandlerNettyImpl.java
@@ -21,13 +21,11 @@
  */
 package org.opennms.netmgt.provision.support;
 
-import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.channel.ChannelStateEvent;
-import org.jboss.netty.channel.Channels;
-import org.jboss.netty.channel.ExceptionEvent;
-import org.jboss.netty.channel.MessageEvent;
-import org.jboss.netty.channel.SimpleChannelHandler;
-//import org.opennms.netmgt.provision.DetectFuture;
+import java.nio.channels.Channels;
+
+import io.netty.channel.ChannelHandlerContext;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import org.opennms.netmgt.provision.support.DetectFutureNettyImpl.ServiceDetectionFailedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,115 +37,72 @@ import org.slf4j.LoggerFactory;
  *
  * @author Seth
  */
-public class DetectorHandlerNettyImpl<Request, Response> extends SimpleChannelHandler {
+public class DetectorHandlerNettyImpl<Request, Response> extends ChannelInboundHandlerAdapter {
     
     private static final Logger LOG = LoggerFactory.getLogger(DetectorHandlerNettyImpl.class);
     
-    //private DetectFuture m_future;
     private AsyncClientConversation<Request, Response> m_conversation;
 
-    /**
-     * <p>setFuture</p>
-     *
-     * @param future a {@link org.opennms.netmgt.provision.DetectFuture} object.
-     * @param <Request> a Request object.
-     * @param <Response> a Response object.
-     */
-    /*
-    public void setFuture(DetectFuture future) {
-        m_future = future;
-    }
-
-    /**
-     * <p>getFuture</p>
-     *
-     * @return a {@link org.opennms.netmgt.provision.DetectFuture} object.
-     * /
-    public DetectFuture getFuture() {
-        return m_future;
-    }
-    */
-
     /** {@inheritDoc} */
     @Override
-    public void channelOpen(ChannelHandlerContext ctx, ChannelStateEvent event) throws Exception {
-        LOG.debug("channelOpen()");
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        LOG.debug("channelActive()");
         if(!getConversation().hasBanner() && getConversation().getRequest() != null) {
             Object request = getConversation().getRequest();
-            ctx.getChannel().write(request);
+            ctx.channel().write(request);
         }
     }
 
     /** {@inheritDoc} */
     @Override
-    public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent event) throws Exception {
-        LOG.debug("channelClosed()");
-        /*
-        if(!getFuture().isDone()) {
-            getFuture().setServiceDetected(false);
-        }
-        */
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        LOG.debug("channelInactive()");
     }
-
-    /*
-    I'm not sure how to create a Netty equivalent to this...
-
-    @Override
-    public void sessionIdle(IoSession session, IdleStatus status) throws Exception {
-        if(getConversation().hasBanner() && status == IdleStatus.READER_IDLE) {
-            getFuture().setServiceDetected(false);
-            session.close(true);
-        }
-    }
-     */
 
     /** {@inheritDoc} */
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent event) throws Exception {
-        LOG.debug("Caught a Throwable in {}", this.getClass().getName(), event.getCause());
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        LOG.debug("Caught a Throwable in {}", this.getClass().getName(), cause);
         //getFuture().setException(event.getCause());
         // Make sure that the channel is closed
-        ctx.getChannel().close();
+        ctx.channel().close();
         // P
-        ctx.sendUpstream(event);
+        ctx.fireChannelRead(cause);
     }
 
     /** {@inheritDoc} */
     @SuppressWarnings("unchecked")
     @Override
-    public void messageReceived(ChannelHandlerContext ctx, MessageEvent message) {
+    public void channelRead(ChannelHandlerContext ctx, Object message) {
         try {
             final AsyncClientConversation<Request, Response> conversation = getConversation();
-            LOG.debug("Client Receiving: {}", message.getMessage().toString().trim());
+            LOG.debug("Client Receiving: {}", message.toString().trim());
             LOG.debug("Conversation: {}", conversation);
 
-            if(conversation.hasExchanges() && conversation.validate((Response)message.getMessage())) {
+            if(conversation.hasExchanges() && conversation.validate((Response)message)) {
 
                 Object request = conversation.getRequest();
 
                 if (request != null) {
                     LOG.debug("Writing request: {}", request);
-                    ctx.getChannel().write(request);
+                    ctx.channel().write(request);
                 } else if (request == null && conversation.isComplete()) {
                     LOG.debug("Closing channel: {}", conversation);
-                    //getFuture().setServiceDetected(true);
-                    ctx.getChannel().close();
+                    ctx.channel().close();
                 } else {
-                    //getFuture().setServiceDetected(false);
                     LOG.debug("Closing channel, detection failed: {}", conversation);
-                    ctx.getChannel().close();
-                    Channels.fireExceptionCaught(ctx, new ServiceDetectionFailedException());
+                    ctx.channel().close();
+                    ctx.fireExceptionCaught(new ServiceDetectionFailedException());
                 }
             } else {
-                LOG.debug("Invalid response: {}", message.getMessage().toString().trim());
-                //getFuture().setServiceDetected(false);
-                ctx.getChannel().close();
-                Channels.fireExceptionCaught(ctx, new ServiceDetectionFailedException());
+                LOG.debug("Invalid response: {}", message.toString().trim());
+                ctx.channel().close();
+                ctx.fireExceptionCaught(new ServiceDetectionFailedException());
             }
         } catch(Throwable e) {
             LOG.debug("Exception caught!", e);
-            ctx.getChannel().close();
-            Channels.fireExceptionCaught(ctx, e);
+            ctx.channel().close();
+            ctx.fireExceptionCaught(e);
         }
     }
 
