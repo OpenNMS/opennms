ff5192c42234c66e090b3a31c55af80c3b68ffe8
Dino <dino2gnt@gmail.com>
2024-10-08 17:30:20 -0500

    Add new Karaf commands for displaying Minions, Locations, and Requisitions

---
+++ core/commands/src/main/java/org/opennms/core/commands/ShowLocations.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2024 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2023 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.core.commands;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.support.table.ShellTable;
+import org.opennms.netmgt.dao.api.MonitoringLocationDao;
+import org.opennms.netmgt.model.monitoringLocations.OnmsMonitoringLocation;
+
+@Command(scope = "opennms", name = "show-locations", description = "Returns a list of defined Minion Locations.")
+@Service
+public class ShowLocations implements Action {
+    @Reference
+    private MonitoringLocationDao monitoringLocationDao;
+
+    @Override
+    public Object execute() {
+        final List<OnmsMonitoringLocation> onmsMonitoringLocationList = monitoringLocationDao.findAll();
+        Collections.sort(onmsMonitoringLocationList, Comparator.comparing(OnmsMonitoringLocation::getLocationName));
+
+        try {
+            ShellTable table = new ShellTable();
+            table.column("Location Name");
+            table.column("Monitoring Area");
+            table.column("Geolocation");
+            table.column("Latitude");
+            table.column("Longitude");
+            table.column("Priority");
+            for (OnmsMonitoringLocation location : onmsMonitoringLocationList) {
+                table.addRow().addContent(location.getLocationName(), location.getMonitoringArea(), location.getGeolocation(), location.getLatitude(), location.getLongitude(), location.getPriority());
+            }
+            table.print(System.out);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace(System.out);
+        }
+
+        return null;
+    }
+}
---
+++ core/commands/src/main/java/org/opennms/core/commands/ShowMinions.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2024 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2023 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.core.commands;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.support.table.ShellTable;
+import org.opennms.netmgt.dao.api.MinionDao;
+import org.opennms.netmgt.model.OnmsMinionCollection;
+import org.opennms.netmgt.model.minion.OnmsMinion;
+
+@Command(scope = "opennms", name = "show-minions", description = "Returns a list of defined Minions.")
+@Service
+public class ShowMinions implements Action {
+    @Reference
+    private MinionDao minionDao;
+
+    @Override
+    public Object execute() {
+        final OnmsMinionCollection coll = new OnmsMinionCollection(minionDao.findAll());
+
+        try {
+            ShellTable table = new ShellTable();
+            table.column("Minion ID");
+            table.column("Location");
+            table.column("Version");
+            table.column("Status");
+            table.column("Last Updated Time");
+            for (OnmsMinion minion : coll) {
+                table.addRow().addContent(minion.getId(), minion.getLocation(), minion.getVersion(), minion.getStatus(), minion.getLastUpdated());
+            }
+            table.print(System.out);
+        } catch (Exception e) {
+             System.out.println(e.getMessage());
+             e.printStackTrace(System.out);
+        }
+        return null;
+    }
+}
