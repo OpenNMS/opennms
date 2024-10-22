2feff67126c3e4359fe04cacfecce6ec833c0511
Umairk95 <160595109+Umairk95@users.noreply.github.com>
2024-10-22 16:19:05 +0500

    Improve python script in Circle CI

--- .circleci/pyscripts/process_generate.py
+++ .circleci/pyscripts/process_generate.py
@@ -217,9 +217,9 @@ if "trigger-build" in mappings:
         or "foundation-" in branch_name
     ) and "merge-foundation/" not in branch_name:
          for item in check_build:
-            if (("docs" in item or "ui" in item or "circleci_configuration" in item) and len(check_build) == 1) or (("docs" in combine_build_element  and "ui" in combine_build_element and len(check_build) == 2) or \
-                ("docs" in combine_build_element  and "circleci_configuration" in combine_build_element and len(check_build) == 2) or ("circleci_configuration" in combine_build_element  and "ui" in combine_build_element and len(check_build) == 2 ))  or \
-                ("docs" in combine_build_element  and "ui" in combine_build_element and "circleci_configuration" in combine_build_element  and len(check_build) == 3):
+            if ((item == "docs" or item == "ui" or item == "circleci_configuration") and len(check_build) == 1) or (("docs" in combine_build_element  and "ui," in combine_build_element and len(check_build) == 2) or \
+                ("docs" in combine_build_element  and "circleci_configuration" in combine_build_element and len(check_build) == 2) or ("circleci_configuration" in combine_build_element  and "ui," in combine_build_element and len(check_build) == 2 ))  or \
+                ("docs" in combine_build_element  and "ui," in combine_build_element and "circleci_configuration" in combine_build_element  and len(check_build) == 3):
               del mappings["trigger-build"]
               check_build.clear()
               del combine_build_element
@@ -252,9 +252,9 @@ if "trigger-build" in mappings:
         build_mappings["master-branch"] = True
     elif not build_trigger_override_found and "merge-foundation/" not in branch_name:
         for item in check_build:
-            if (("docs" in item or "ui" in item or "circleci_configuration" in item) and len(check_build) == 1) or (("docs" in combine_build_element  and "ui" in combine_build_element and len(check_build) == 2) or \
-                ("docs" in combine_build_element  and "circleci_configuration" in combine_build_element and len(check_build) == 2) or ("circleci_configuration" in combine_build_element  and "ui" in combine_build_element and len(check_build) == 2 ))  or \
-                ("docs" in combine_build_element  and "ui" in combine_build_element and "circleci_configuration" in combine_build_element  and len(check_build) == 3):
+            if ((item == "docs" or item == "ui" or item == "circleci_configuration") and len(check_build) == 1) or (("docs" in combine_build_element  and "ui," in combine_build_element and len(check_build) == 2) or \
+                ("docs" in combine_build_element  and "circleci_configuration" in combine_build_element and len(check_build) == 2) or ("circleci_configuration" in combine_build_element  and "ui," in combine_build_element and len(check_build) == 2 ))  or \
+                ("docs" in combine_build_element  and "ui," in combine_build_element and "circleci_configuration" in combine_build_element  and len(check_build) == 3):
               del mappings["trigger-build"]
               check_build.clear()
               del combine_build_element
