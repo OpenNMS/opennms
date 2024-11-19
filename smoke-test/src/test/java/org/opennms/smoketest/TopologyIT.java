376d5617c558a54b64022762f968f68d7e2121c0
musaidali <musaid.ali@dataqhealth.com>
2024-11-19 22:51:53 +0500

    NMS-16460 : Wait for visible before click.

--- smoke-test/src/test/java/org/opennms/smoketest/TopologyIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/TopologyIT.java
@@ -412,6 +412,7 @@ public class TopologyIT extends OpenNMSSeleniumIT {
             testCase.getDriver().get(topologyUiUrl);
             // Wait for the "View" menu to be clickable and the loading indicator to be gone before
             // returning control to the test in order to make sure that the page is fully loaded
+            testCase.wait.until(ExpectedConditions.visibilityOfElementLocated(getCriteriaForMenubarElement("View")));
             testCase.wait.until(ExpectedConditions.elementToBeClickable(getCriteriaForMenubarElement("View")));
             testCase.wait.until(ExpectedConditions.invisibilityOfElementLocated(By.className("v-loading-indicator")));
             return this;
