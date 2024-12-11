0500fe6d0b9e24b915d91a07a767fb7586fd3301
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2024-12-11 15:35:09 -0500

    Update the docker image version

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
@@ -52,7 +52,7 @@ public class OnmsKarafTestCase extends KarafTestCase {
 				.groupId("org.opennms.container")
 				.artifactId("org.opennms.container.karaf")
 				.type("tar.gz")
-				.version("33.1.1");
+				.version("33.1.2-SNAPSHOT");
 	}
 
 	/**
