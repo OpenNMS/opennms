784b30f0d313a51a257a473d805c28448c8e695b
Christian Pape <Christian.Pape@informatik.hs-fulda.de>
2024-12-18 15:55:27 +0100

    NMS-16978: Removed a line

--- features/events/daemon/src/test/java/org/opennms/netmgt/eventd/nms16978/HibernateEventWriterIT.java
+++ features/events/daemon/src/test/java/org/opennms/netmgt/eventd/nms16978/HibernateEventWriterIT.java
@@ -2,10 +2,8 @@ package org.opennms.netmgt.eventd.nms16978;
 
 import org.junit.Before;
 import org.opennms.netmgt.dao.hibernate.EventDaoHibernate;
-import org.opennms.netmgt.eventd.processor.HibernateEventWriter;
 
 public class HibernateEventWriterIT extends org.opennms.netmgt.eventd.processor.HibernateEventWriterIT {
-    //@Override
     @Before
     public void setUp() throws Exception{
         ((EventDaoHibernate)getHibernateEventWriter().getEventDao())
