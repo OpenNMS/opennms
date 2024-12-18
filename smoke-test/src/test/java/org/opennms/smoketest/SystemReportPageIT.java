e18c4d82de3f98a3c7a19dbc84f6c2edd56e7a99
musaidali <musaid.ali@dataqhealth.com>
2024-12-18 21:24:13 +0500

    NMS-16983: File name Fix.

---
+++ smoke-test/src/test/java/org/opennms/smoketest/SystemReportPageIT.java
@@ -0,0 +1,55 @@
+package org.opennms.smoketest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Objects;
+
+
+public class SystemReportPageIT extends UiPageTest  {
+
+
+    private Page uiPage;
+
+    @Before
+    public void setUp() throws Exception {
+        uiPage = new SystemReportPageIT.Page(getBaseUrlInternal());
+        uiPage.open();
+    }
+
+    @Test
+    public void systemReport() throws IOException, InterruptedException {
+
+
+        WebElement filenameInput = driver.findElement(By.id("filename"));
+        filenameInput.sendKeys("abc.txt");
+        WebElement generateButton = driver.findElement(By.xpath("//input[@class='btn btn-secondary' and @value='Generate System Report']"));
+        generateButton.click();
+        execute(() -> {
+            pageContainsText("abc.txt");
+            return null;
+        });
+
+    }
+
+    private class Page {
+        private final String url;
+
+        public Page(String baseUrl) {
+            this.url = Objects.requireNonNull(baseUrl) + "opennms/admin/support/systemReport.htm";
+        }
+        public Page open() {
+            driver.get(url);
+            System.out.println(url);
+            new WebDriverWait(driver, Duration.ofSeconds(10)).until(pageContainsText("Choose which plugins to enable:"));
+            return this;
+        }
+
+
+
+    }
+}
