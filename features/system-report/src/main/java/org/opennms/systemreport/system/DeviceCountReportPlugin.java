a3150ff697497c000ccc90e64ed4834fe29f6a17
mwajahatabbasi <mabbasi@nanthealth.com>
2025-02-05 16:43:50 +0500

    NMS-17076 : System Report Plugin added for Device Count.

---
+++ features/system-report/src/main/java/org/opennms/systemreport/system/DeviceCountReportPlugin.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to The OpenNMS Group, Inc (TOG) under one or more
+ * contributor license agreements.  See the LICENSE.md file
+ * distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * TOG licenses this file to You under the GNU Affero General
+ * Public License Version 3 (the "License") or (at your option)
+ * any later version.  You may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at:
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.opennms.systemreport.system;
+
+import org.opennms.netmgt.dao.api.NodeDao;
+import org.opennms.systemreport.AbstractSystemReportPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class DeviceCountReportPlugin extends AbstractSystemReportPlugin {
+    private static final Logger LOG = LoggerFactory.getLogger(DeviceCountReportPlugin.class);
+
+
+    @Autowired
+    NodeDao m_nodeDao;
+
+    @Override
+    public String getName() {
+        return "Device Count";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Information of Device Count Per SysoID";
+    }
+
+    @Override
+    public int getPriority() {
+        return 6;
+    }
+
+    @Override
+    public boolean isVisible() { return true; }
+
+    @Override
+    public Map<String, Resource> getEntries() {
+        final TreeMap<String,Resource> map = new TreeMap<String,Resource>();
+
+        Map<String, Long> nodeCountMap = m_nodeDao.getNumberOfNodesBySysOid();
+
+        nodeCountMap.forEach((K,V)->{
+            map.put(K,getResource(String.valueOf(V)));
+        });
+
+        return map;
+    }
+
+}
