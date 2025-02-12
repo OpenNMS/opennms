8d99f84a66e06765b02e901a0cdca8e64ffdad02
Mohammad-Junid <mjunaid@nanthealth.com>
2025-02-12 20:05:39 +0500

    NMS-16359  [skip ci] add support for Eddsa provider for java 17

---
+++ features/device-config/ssh-scripting/src/main/java/org/opennms/features/deviceconfig/sshscripting/impl/EdDSAActivator.java
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2017-2017 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *     http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.features.deviceconfig.sshscripting.impl;
+
+import net.i2p.crypto.eddsa.EdDSASecurityProvider;
+import org.apache.sshd.common.util.security.SecurityUtils;
+import org.apache.sshd.common.util.security.eddsa.EdDSASecurityProviderRegistrar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.security.Security;
+
+/**
+ * Custom BundleActivator for EdDSA.
+ */
+public class EdDSAActivator  {
+    private static final Logger logger = LoggerFactory.getLogger(EdDSAActivator.class);
+
+    public void start() throws Exception {
+        System.setProperty("org.apache.sshd.eddsaSupport", "true");
+        logger.info("EdDSAActivator starting...");
+        logger.error("EdDSAActivator starting...");
+        logger.debug("EdDSAActivator starting...");
+        // Insert the provider
+        Security.removeProvider("EdDSA");
+        Security.insertProviderAt(new EdDSASecurityProvider(), 1);
+        logger.info("EdDSA provider inserted at priority 1.");
+        SecurityUtils.registerSecurityProvider(new EdDSASecurityProviderRegistrar());
+        // Optionally, print out all registered providers for verification:
+        for (var provider : Security.getProviders()) {
+            logger.info(provider.getName() + " - " + provider.getInfo());
+        }
+    }
+
+}
