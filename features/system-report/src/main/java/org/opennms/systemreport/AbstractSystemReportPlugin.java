ac795da70f4b569448e77449f69bc3d472fdb49a
mwajahatabbasi <mabbasi@nanthealth.com>
2025-02-14 17:45:14 +0500

    NMS-17079 : Add User Logins reports to the system report bundle.

--- features/system-report/src/main/java/org/opennms/systemreport/AbstractSystemReportPlugin.java
+++ features/system-report/src/main/java/org/opennms/systemreport/AbstractSystemReportPlugin.java
@@ -25,7 +25,6 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
