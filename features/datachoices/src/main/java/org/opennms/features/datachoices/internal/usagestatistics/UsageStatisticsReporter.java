8f405db6b9c6349152a04c03abbeb7ff0d2b238c
mwajahatabbasi <mabbasi@nanthealth.com>
2025-03-04 13:16:03 +0500

    NMS-17082 : Add "Number of Flows per Second (Last 24 Hours)" to Usage Stats

--- features/datachoices/src/main/java/org/opennms/features/datachoices/internal/usagestatistics/UsageStatisticsReporter.java
+++ features/datachoices/src/main/java/org/opennms/features/datachoices/internal/usagestatistics/UsageStatisticsReporter.java
@@ -28,7 +28,14 @@ import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.time.Duration;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
