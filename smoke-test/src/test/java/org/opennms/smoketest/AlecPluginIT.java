a89aa0691eacbec3ba4e910abba1e97b9c80ee8f
jahanzebdataq <jahanzeb.akhtar@dataqhealth.com>
2025-04-11 20:56:22 +0500

    NMS-17868 Added a new test case for ALEC plugin

---
+++ smoke-test/src/test/java/org/opennms/smoketest/AlecPluginIT.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to The OpenNMS Group, Inc (TOG) under one or more
+ * contributor license agreements.  See the LICENSE.md file
+ * distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * TOG licenses this file to You under the GNU Affero General
+ * Public License Version 3 (the "License") or (at your option)
+ * any later version.  You may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at:
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.opennms.smoketest;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.opennms.smoketest.stacks.OpenNMSStack;
+import org.opennms.smoketest.utils.KarafShell;
+import org.opennms.smoketest.utils.KarafShellUtils;
+
+@org.junit.experimental.categories.Category(org.opennms.smoketest.junit.FlakyTests.class)
+public class AlecPluginIT {
+    @ClassRule
+    public static OpenNMSStack stack = OpenNMSStack.minimal(
+            b -> b.withInstallFeature("opennms-alarm-history-elastic")
+            b -> b.withInstallFeature("alec-opennms-standalone", "opennms-alec-plugin")
+    );
+
+    protected KarafShell karafShell = new KarafShell(stack.opennms().getSshAddress());
+
+    @Before
+    public void setUp() throws IOException, InterruptedException {
+        // Make sure the Karaf shell is healthy before we start
+        KarafShellUtils.awaitHealthCheckSucceeded(stack.opennms());
+    }
+
+    @Test
+    public void everythingHappy() throws Exception {
+        karafShell.runCommand("config:edit org.opennms.features.flows.persistence.elastic\n" +
+                "config:property-set elasticUrl http://localhost:9200\n" +
+                "config:update");
+        karafShell.runCommand("config:edit org.opennms.features.alarms.history.elastic\n" +
+                "config:property-set elasticUrl http://localhost:9200\n" +
+                "config:update");
+        karafShell.checkFeature("alec-opennms-standalone", "Started", Duration.ofSeconds(30));
+    }
+}
