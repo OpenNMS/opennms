fdc522c93863c3c1bc8977d4830d81f923d3b1e9
mwajahatabbasi <mabbasi@nanthealth.com>
2025-04-15 18:25:53 +0500

    NMS-17869 : Handle PR Review Changes.

--- smoke-test/src/test/java/org/opennms/smoketest/GrpcExporterPluginIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/GrpcExporterPluginIT.java
@@ -1,3 +1,24 @@
+/*
+ * Licensed to The OpenNMS Group, Inc (TOG) under one or more
+ * contributor license agreements.  See the LICENSE.md file
+ * distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * TOG licenses this file to You under the GNU Affero General
+ * Public License Version 3 (the "License") or (at your option)
+ * any later version.  You may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at:
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
 package org.opennms.smoketest;
 
 import java.io.IOException;
@@ -14,7 +35,7 @@ import org.opennms.smoketest.utils.KarafShellUtils;
 public class GrpcExporterPluginIT {
     @ClassRule
     public static OpenNMSStack stack = OpenNMSStack.MINIMAL;
-
+    private static final String FEATURE_GRPC_EXPORTER="opennms-grpc-exporter";
     protected KarafShell karafShell = new KarafShell(stack.opennms().getSshAddress());
 
     @Before
@@ -25,8 +46,8 @@ public class GrpcExporterPluginIT {
 
     @Test
     public void everythingHappy() throws Exception {
-        karafShell.runCommandOnce("feature:install opennms-grpc-exporter" ,
-                output -> !output.toLowerCase().contains("error"), false);
-        karafShell.checkFeature("opennms-grpc-exporter", "Started", Duration.ofSeconds(30));
+        karafShell.runCommandOnce("feature:install "+FEATURE_GRPC_EXPORTER ,
+               output -> !output.toLowerCase().contains("error"), false);
+        karafShell.checkFeature(FEATURE_GRPC_EXPORTER, "Started", Duration.ofSeconds(30));
     }
 }
