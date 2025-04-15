a8d4de2fd21c70b5d41eaed7542c34615176ed8f
Ronny Trommer <ronny@no42.org>
2025-04-15 23:42:10 +0200

    chore: Use minimal PostgreSQL 13 instead of 11 in tests
    Signed-off-by: Ronny Trommer <ronny@no42.org>

--- smoke-test/src/main/java/org/opennms/smoketest/containers/PostgreSQLContainer.java
+++ smoke-test/src/main/java/org/opennms/smoketest/containers/PostgreSQLContainer.java
@@ -59,7 +59,7 @@ public class PostgreSQLContainer extends org.testcontainers.containers.PostgreSQ
     private HibernateDaoFactory daoFactory;
 
     public PostgreSQLContainer() {
-        super("postgres:10.7-alpine");
+        super("postgres:13-alpine");
         withNetwork(Network.SHARED)
                 .withNetworkAliases(OpenNMSContainer.DB_ALIAS)
                 .withCreateContainerCmdModifier(TestContainerUtils::setGlobalMemAndCpuLimits);
