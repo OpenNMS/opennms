0275e51150e4cf261eac690aeff33fa35c6600e6
jahanzebdataq <jahanzeb.akhtar@dataqhealth.com>
2025-04-16 17:29:30 +0500

    NMS-17886 Modified the regex and added new testcases to check

--- ui/src/components/Configuration/ConfigurationHelper.ts
+++ ui/src/components/Configuration/ConfigurationHelper.ts
@@ -790,7 +790,7 @@ const validateCronTab = (item: LocalConfiguration, oldErrors: LocalErrors) => {
  */
 const validateHost = (host: string) => {
   // no spaces, may starts and ends with brackets, may contain (but not start with) hyphen or dot or colon, cannot be over 49 chars (e.g. IPv6 - vmware://[2001:db8:0:8d3:0:8a2e:70:7344])
-  const customHostnameRegex = /^(?:(\$\{[^}]+\}|\w+):(\$\{[^}]+\}|\S+)@)?(?!.*\.\.)(?!.*[.-]$)(?!.*^[-.])([a-zA-Z\d][a-zA-Z\d.-]*[a-zA-Z\d]?|\[[a-fA-F\d:]+\])(?::([0-9]{1,5}))?$/
+  const customHostnameRegex = /^(?:(\$\{[^}]+\}|\w+):(\$\{[^}]+\}|\S+)@)?(?!.*\.\.)(?!.*[.-]$)(?!.*^[-.])([a-zA-Z\d][a-zA-Z\d.-]*[a-zA-Z\d]?|\[[a-fA-F\d:]+\])(?::(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{0,4}|[1-9][0-9]{0,4}|0))?$/
 
   // Either IPv4, IPv6, a valid domain name, or passes custom regex
   const isHostValid = 
