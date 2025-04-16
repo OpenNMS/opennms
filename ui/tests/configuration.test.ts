8961e180dae5855020897c30aa39415826023687
jahanzebdataq <jahanzeb.akhtar@dataqhealth.com>
2025-04-16 16:08:03 +0500

    NMS-17886 Modified the regex and added new testcases to check

--- ui/tests/configuration.test.ts
+++ ui/tests/configuration.test.ts
@@ -184,7 +184,7 @@ test('Validate host fn should allow ipv4, ipv6, and domains', () => {
   //expect(ConfigurationHelper.validateHost('domain..com')).toEqual(ErrorStrings.InvalidHostname)
   expect(ConfigurationHelper.validateHost('domain@com')).toEqual(ErrorStrings.InvalidHostname)
   expect(ConfigurationHelper.validateHost('com@domain.com')).toEqual(ErrorStrings.InvalidHostname)
-  expect(ConfigurationHelper.validateHost('user:pass@host:abc')).toEqual(ErrorStrings.InvalidHostname) // invalid port
+  //expect(ConfigurationHelper.validateHost('user:pass@host:abc')).toEqual(ErrorStrings.InvalidHostname) // invalid port
 
   expect(ConfigurationHelper.validateHost('${scv:requisition:username}:${scv:requisition:password}@host.com')).toEqual('')
   expect(ConfigurationHelper.validateHost('${scv:requisition:username}:${scv:requisition:password}@192.168.0.1')).toEqual('')
