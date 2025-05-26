cf97e644a0b3ab1fcd1155ddac302551dd9b6d38
mwajahatabbasi <mabbasi@nanthealth.com>
2025-05-26 14:19:50 +0500

    NMS-1781 : Handle PR Review changes

--- features/scv/api/src/main/java/org/opennms/features/scv/api/SecureCredentialsVault.java
+++ features/scv/api/src/main/java/org/opennms/features/scv/api/SecureCredentialsVault.java
@@ -21,18 +21,7 @@
  */
 package org.opennms.features.scv.api;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.Properties;
 import java.util.Set;
-import java.util.stream.Stream;
 
 public interface SecureCredentialsVault {
 
