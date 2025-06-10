135c6c98778d84d052b09d5349d40394b3ed1385
MuhammadTalhaDataQHealth <mtalha@nanthealth.com>
2025-06-10 20:40:28 +0500

    OpenNMS Horizon 33.1.7

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
@@ -52,7 +52,7 @@ public class OnmsKarafTestCase extends KarafTestCase {
 				.groupId("org.opennms.container")
 				.artifactId("org.opennms.container.karaf")
 				.type("tar.gz")
-				.version("33.1.7-SNAPSHOT");
+				.version("33.1.7");
 	}
 
 	/**
