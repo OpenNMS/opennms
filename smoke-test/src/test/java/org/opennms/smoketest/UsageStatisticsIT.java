d761a385db2b554edcd26dc26545d12383bc10c1
Chandra Gorantla <chandra@opennms.com>
2025-07-31 09:38:09 -0400

    NMS-18169: Fix smoke test

--- smoke-test/src/test/java/org/opennms/smoketest/UsageStatisticsIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/UsageStatisticsIT.java
@@ -86,8 +86,8 @@ public class UsageStatisticsIT {
         final Map<String, Boolean> services = (Map<String, Boolean>) usageReport.get("services");
 
         assertEquals(29, services.size());
-        assertEquals(25, services.entrySet().stream().filter(Map.Entry::getValue).count());
-        assertEquals(4, services.entrySet().stream().filter(e -> !e.getValue()).count());
+        assertEquals(27, services.entrySet().stream().filter(Map.Entry::getValue).count());
+        assertEquals(2, services.entrySet().stream().filter(e -> !e.getValue()).count());
 
         assertThat((String) usageReport.get("systemId"), matchesPattern("^\\S+-\\S+-\\S+-\\S+-\\S+$"));
 
