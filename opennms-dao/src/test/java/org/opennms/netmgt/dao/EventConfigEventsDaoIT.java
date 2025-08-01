e66c1f1a80f79d77e9b4c44a37ee6e97a5b5b28a
junaidkhan <junaid.khan@dataqhealth.com>
2025-08-01 19:33:44 +0500

    NMS-18150 Implement JPA entities and repositories (remove duplicate code).

--- opennms-dao/src/test/java/org/opennms/netmgt/dao/EventConfigEventsDaoIT.java
+++ opennms-dao/src/test/java/org/opennms/netmgt/dao/EventConfigEventsDaoIT.java
@@ -49,10 +49,7 @@ import static org.junit.Assert.assertEquals;
         "classpath:/META-INF/opennms/applicationContext-soa.xml",
         "classpath:/META-INF/opennms/applicationContext-dao.xml",
         "classpath:/META-INF/opennms/applicationContext-mockConfigManager.xml",
-        "classpath:/META-INF/opennms/applicationContext-databasePopulator.xml",
-        "classpath*:/META-INF/opennms/component-dao.xml",
         "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
-        "classpath:/META-INF/opennms/applicationContext-minimal-conf.xml"
 })
 @JUnitConfigurationEnvironment
 @JUnitTemporaryDatabase
