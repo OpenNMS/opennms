ba4034c2a5fc96826a12e7485785fbe48f771a4a
mwajahatabbasi <mabbasi@nanthealth.com>
2025-08-14 23:15:37 +0500

    NMS-18154 : Implement Rest API to filter event conf by uei, vendor and source name.

--- opennms-webapp-rest/src/test/java/org/opennms/web/rest/v2/EventConfRestServiceIT.java
+++ opennms-webapp-rest/src/test/java/org/opennms/web/rest/v2/EventConfRestServiceIT.java
@@ -45,7 +45,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
