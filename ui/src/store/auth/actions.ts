3acc83656428a042c2d251ea44091a86b47f9b5e
Scott Theleman <synqotik@users.noreply.github.com>
2025-09-23 15:11:45 -0400

    NMS-18535: Fix SCV security and UI role routing guards (#7974)

--- ui/src/store/auth/actions.ts
+++ ui/src/store/auth/actions.ts
@@ -3,7 +3,10 @@ import { VuexContext } from '@/types'
 
 const getWhoAmI = async (context: VuexContext) => {
   const whoAmI = await API.getWhoAmI()
-  context.commit('SAVE_WHO_AM_I_TO_STATE', whoAmI)
+
+  if (whoAmI) {
+    context.commit('SAVE_WHO_AM_I_TO_STATE', whoAmI)
+  }
 }
 
 export default {
