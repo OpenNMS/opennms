280bb4ce14658500669155efe17d10a58b42fd74
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2025-09-24 10:40:34 -0400

    Revert "NMS-18535: Fix SCV security and UI role routing guards (#7974)"
    This reverts commit 3acc83656428a042c2d251ea44091a86b47f9b5e.

--- ui/src/services/whoAmIService.ts
+++ ui/src/services/whoAmIService.ts
@@ -25,12 +25,14 @@ import { WhoAmIResponse } from '@/types'
 
 const endpoint = '/whoami'
 
-const getWhoAmI = async (): Promise<WhoAmIResponse | false> => {
+const getWhoAmI = async (): Promise<WhoAmIResponse> => {
   try {
     const resp = await rest.get(endpoint)
     return resp.data
   } catch (err) {
-    return false
+    return {
+      roles: [] as string[]
+    } as WhoAmIResponse
   }
 }
 
