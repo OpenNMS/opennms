e08f3be48bef9b059a866786ea10f57ad51b36bd
Scott Theleman <stheleman@opennms.com>
2025-10-01 18:50:19 -0400

    NMS-18767: Make app logo configurable per product (Horizon)

--- ui/src/components/Menu/Menubar.vue
+++ ui/src/components/Menu/Menubar.vue
@@ -40,7 +40,7 @@ import { useOutsideClick } from '@featherds/composables/events/OutsideClick'
 import { FeatherAppBar, FeatherAppBarLink } from '@featherds/app-bar'
 import { FeatherButton } from '@featherds/button'
 
-import IconLogo from '@/assets/LogoHorizon.vue'
+import IconLogo from './src/assets/ProductLogo.vue'
 import { useAppStore } from '@/stores/appStore'
 import { useMenuStore } from '@/stores/menuStore'
 import { MainMenu } from '@/types/mainMenu'
