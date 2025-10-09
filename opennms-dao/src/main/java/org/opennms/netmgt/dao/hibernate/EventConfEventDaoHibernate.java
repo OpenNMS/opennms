9aea71acf2d077094f6e4eed04ba24490ba74c79
mwajahatabbasi <mabbasi@nanthealth.com>
2025-10-09 16:11:03 +0500

    NMS-18552 : Handle PR review changes.

--- opennms-dao/src/main/java/org/opennms/netmgt/dao/hibernate/EventConfEventDaoHibernate.java
+++ opennms-dao/src/main/java/org/opennms/netmgt/dao/hibernate/EventConfEventDaoHibernate.java
@@ -26,7 +26,12 @@ import org.opennms.netmgt.model.EventConfEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class EventConfEventDaoHibernate
         extends AbstractDaoHibernate<EventConfEvent, Long>
