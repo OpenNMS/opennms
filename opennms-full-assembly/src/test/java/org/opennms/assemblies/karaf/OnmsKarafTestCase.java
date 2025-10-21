4a82c0ac09efdc933c369e143bc19080866d38ff
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2025-10-21 09:06:07 -0400

    bump up version due to changes in NMS-18201

--- opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
+++ opennms-full-assembly/src/test/java/org/opennms/assemblies/karaf/OnmsKarafTestCase.java
@@ -52,7 +52,7 @@ public class OnmsKarafTestCase extends KarafTestCase {
 				.groupId("org.opennms.container")
 				.artifactId("org.opennms.container.karaf")
 				.type("tar.gz")
-				.version("34.0.3-SNAPSHOT");
+				.version("34.1.0-SNAPSHOT");
 	}
 
 	/**
