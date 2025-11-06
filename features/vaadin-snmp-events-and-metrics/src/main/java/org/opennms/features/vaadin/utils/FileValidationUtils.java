81a7c5f828c9a1bef708d8a291d3d9fd6e68b3f9
mwajahatabbasi <mabbasi@nanthealth.com>
2025-11-06 20:12:14 +0500

    NMS-19045: Handle PR Review Changes.

--- features/vaadin-snmp-events-and-metrics/src/main/java/org/opennms/features/vaadin/utils/FileValidationUtils.java
+++ features/vaadin-snmp-events-and-metrics/src/main/java/org/opennms/features/vaadin/utils/FileValidationUtils.java
@@ -1,7 +1,27 @@
 package org.opennms.features.vaadin.utils;
 
 import java.io.File;
-
+/*
+ * Licensed to The OpenNMS Group, Inc (TOG) under one or more
+ * contributor license agreements.  See the LICENSE.md file
+ * distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * TOG licenses this file to You under the GNU Affero General
+ * Public License Version 3 (the "License") or (at your option)
+ * any later version.  You may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at:
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
 public final class FileValidationUtils {
     public static  boolean isValidFileName(String fileName) {
         if (fileName == null || fileName.isEmpty()) {
