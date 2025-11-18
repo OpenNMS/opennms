30b20481c79401edc6ef418e715230252a01b5ee
aurangzeb <azeb@nanthealth.com>
2025-11-18 20:51:32 +0500

    NMS-19044 Remove sysout statement, and add copyright header.

--- smoke-test/src/test/java/org/opennms/smoketest/EventConfRestIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/EventConfRestIT.java
@@ -1,3 +1,24 @@
+/*
+ * Licensed to The OpenNMS Group, Inc (TOG) under one or more
+ * contributor license agreements.  See the LICENSE.md file
+ * distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * TOG licenses this file to You under the GNU Affero General
+ * Public License Version 3 (the "License") or (at your option)
+ * any later version.  You may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at:
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
 package org.opennms.smoketest;
 
 import io.restassured.RestAssured;
@@ -59,7 +80,6 @@ public class EventConfRestIT {
 
         List<Map<String, Object>> successList = (List<Map<String, Object>>) responseMap.get("success");
         int successCount = successList != null ? successList.size() : 0;
-        System.out.println("Uploaded document success count:" + successCount);
 
         assertEquals("Mismatch in successfully uploaded file count!", eventFiles.length, successCount);
     }
