623d8de9b94b56dae383030ddd94bf7d85d6f4c4
David Schlenk <dschlenk@onec1.com>
2025-12-09 17:06:38 -0600

    NMS-19223: add node message to ResponseTimeResource messages
    Signed-off-by: David Schlenk <dschlenk@onec1.com>

--- features/kafka/producer/src/test/java/org/opennms/features/kafka/producer/CollectionSetMapperTest.java
+++ features/kafka/producer/src/test/java/org/opennms/features/kafka/producer/CollectionSetMapperTest.java
@@ -23,11 +23,14 @@ package org.opennms.features.kafka.producer;
 
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Collections;
 import java.util.Date;
+import java.util.function.Supplier;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
@@ -41,10 +44,12 @@ import org.opennms.netmgt.collection.api.CollectionSet;
 import org.opennms.netmgt.collection.api.ServiceParameters;
 import org.opennms.netmgt.collection.support.builder.CollectionSetBuilder;
 import org.opennms.netmgt.collection.support.builder.InterfaceLevelResource;
+import org.opennms.netmgt.collection.support.builder.LatencyTypeResource;
 import org.opennms.netmgt.collection.support.builder.NodeLevelResource;
 import org.opennms.netmgt.dao.api.NodeDao;
 import org.opennms.netmgt.dao.api.ResourceDao;
 import org.opennms.netmgt.dao.api.SessionUtils;
+import org.opennms.netmgt.model.OnmsNode;
 
 public class CollectionSetMapperTest {
 
@@ -78,5 +83,56 @@ public class CollectionSetMapperTest {
         assertTrue(collectionSetResource.hasInterface());
         assertThat(collectionSetResource.getInterface().getIfIndex(), Matchers.is(25));
         assertThat(collectionSetResource.getInterface().getInstance(), Matchers.is("25"));
+    }
+
+    @Test
+    public void testCollectionSetForResponseResource() throws UnknownHostException {
+        NodeDao nodeDao = Mockito.mock(NodeDao.class);
+        OnmsNode mockNode = new OnmsNode();
+        mockNode.setId(1);
+        mockNode.setLabel("TestNode");
+        OnmsNode snmpNode = new OnmsNode();
+        snmpNode.setId(14);
+        snmpNode.setLabel("SnmpNode");
+        when(nodeDao.get("1")).thenReturn(mockNode);
+        when(nodeDao.get("14")).thenReturn(snmpNode);
+
+        SessionUtils sessionUtils = Mockito.mock(SessionUtils.class);
+        when(sessionUtils.withReadOnlyTransaction(any(Supplier.class)))
+            .thenAnswer(invocation -> {
+                Supplier<?> supplier = invocation.getArgument(0);
+                return supplier.get();
+            });
+        ServiceParameters EMPTY_PARAMS = new ServiceParameters(Collections.emptyMap());
+
+        CollectionSetMapper collectionSetMapper = new CollectionSetMapper(nodeDao, sessionUtils, Mockito.mock(ResourceDao.class));
+
+        CollectionAgent agent = new MockCollectionAgent(1, "TestNode", InetAddress.getLocalHost());
+
+        LatencyTypeResource latencyTypeResource = new LatencyTypeResource("ICMP", InetAddress.getLocalHost().getHostAddress(), "Default");
+        latencyTypeResource.addTag("node_id", "1");
+        latencyTypeResource.addTag("location", "Default");
+
+        CollectionSet collectionSet = new CollectionSetBuilder(agent).withTimestamp(new Date(2))
+                .withGauge(latencyTypeResource, "icmp", "icmp", 1.0)
+                .build();
+        CollectionSetProtos.CollectionSet collectionSetProto = collectionSetMapper.buildCollectionSetProtos(collectionSet, EMPTY_PARAMS);
+        assertThat(collectionSetProto.getResourceList(), Matchers.hasSize(1));
+        CollectionSetProtos.CollectionSetResource collectionSetResource = collectionSetProto.getResource(0);
+        assertTrue(collectionSetResource.hasResponse());
+        assertThat(collectionSetResource.getResponse().getNode().getNodeId(), Matchers.is(1L));
+        assertThat(collectionSetResource.getResponse().getNode().getNodeLabel(), Matchers.is("TestNode"));
+
+        agent = new MockCollectionAgent(14, "SnmpNode", InetAddress.getByName("10.11.12.13"));
+        latencyTypeResource = new LatencyTypeResource("SNMP", "10.11.12.13", "Default");
+        collectionSet = new CollectionSetBuilder(agent).withTimestamp(new Date(2))
+          .withGauge(latencyTypeResource, "snmp", "snmp", 1.0)
+          .build();
+        collectionSetProto = collectionSetMapper.buildCollectionSetProtos(collectionSet, EMPTY_PARAMS);
+        assertThat(collectionSetProto.getResourceList(), Matchers.hasSize(1));
+        collectionSetResource = collectionSetProto.getResource(0);
+        assertTrue(collectionSetResource.hasResponse());
+        assertThat(collectionSetResource.getResponse().getNode().getNodeId(), Matchers.is(14L));
+        assertThat(collectionSetResource.getResponse().getNode().getNodeLabel(), Matchers.is("SnmpNode"));
     }
 }
