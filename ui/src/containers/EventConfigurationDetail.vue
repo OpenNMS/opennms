a2247837205e74bfbf8fd426a255e951d9722d67
Shahbaz <rmasood@nanthealth.com>
2025-12-17 18:25:33 +0500

    json implementation on FE

--- ui/src/containers/EventConfigurationDetail.vue
+++ ui/src/containers/EventConfigurationDetail.vue
@@ -130,10 +130,9 @@ const onAddEventClick = (source: EventConfigSource) => {
 onMounted(async () => {
   if (route.params.id) {
     await store.fetchSourceById(route.params.id as string)
-    store.refreshEventConfigEvents()
-    if (store.selectedSource) {
-      await store.fetchEventsBySourceId()
-    }
+
+    console.log(store.events)
+    
   }
 })
 </script>
