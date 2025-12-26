b5fb3723c38ef696215ca0c0dec32df459880e10
Mohammad-Junid <mjunaid@nanthealth.com>
2025-12-26 23:54:37 +0500

    NMS-19117  minor issue

--- core/ipc/sink/kafka/client/src/main/java/org/opennms/core/ipc/sink/kafka/client/KafkaRemoteMessageDispatcherFactory.java
+++ core/ipc/sink/kafka/client/src/main/java/org/opennms/core/ipc/sink/kafka/client/KafkaRemoteMessageDispatcherFactory.java
@@ -33,6 +33,7 @@ import java.util.Set;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.UUID;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
