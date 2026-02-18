6c8905f3e1e4345313bda9f86acb2a9d247a308e
Dino <dino2gnt@gmail.com>
2026-02-18 11:04:14 -0600

    Fix test?

--- smoke-test/src/test/java/org/opennms/smoketest/MenuHeaderIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/MenuHeaderIT.java
@@ -127,7 +127,7 @@ public class MenuHeaderIT extends OpenNMSSeleniumIT {
 
         // Metrics Menu
         clickMenuItem("metricsMenu", "Resource Graphs");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='router-link-active router-link-exact-active']", 'Resource Graphs')));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='router-link-active router-link-exact-active']/[contains(text()[normalize-space()], 'Resource Graphs')]")));
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//li[@class='feather-list-header']/[contains(text()[normalize-space()], 'Resources')]")));
 
         // Distributed Monitoring
