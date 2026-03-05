0ef1c0475e89f21fa8e513eaa24f29df6f53467d
David Hustace <david@beaconstrategists.com>
2026-03-05 08:59:11 -0800

    feat: create core/event-forwarder-kafka Maven module skeleton

---
+++ core/event-forwarder-kafka/src/main/java/org/opennms/core/event/forwarder/kafka/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * EventForwarder implementation that enriches events locally and publishes
+ * to Kafka (FAULT events) or JMS MessageBus (IPC events).
+ */
+package org.opennms.core.event.forwarder.kafka;
