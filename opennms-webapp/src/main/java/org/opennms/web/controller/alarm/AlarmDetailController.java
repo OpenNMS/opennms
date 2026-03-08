69b10d523c4da38c82c5121543cc544a59d2e248
David Hustace <david@beaconstrategists.com>
2026-03-08 08:40:38 -0700

    fix: remove webEventRepository dependency from alarm and notification controllers
    AlarmDetailController and NotificationFilterController referenced
    webEventRepository via Spring XML injection in dispatcher-servlet.xml.
    With events flowing through Kafka instead of PostgreSQL, the
    webEventRepository bean no longer exists. Remove the dependency and
    return empty results for related-events lookups.

--- opennms-webapp/src/main/java/org/opennms/web/controller/alarm/AlarmDetailController.java
+++ opennms-webapp/src/main/java/org/opennms/web/controller/alarm/AlarmDetailController.java
@@ -21,28 +21,18 @@
  */
 package org.opennms.web.controller.alarm;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.opennms.core.utils.InetAddressUtils;
-import org.opennms.core.utils.WebSecurityUtils;
 import org.opennms.netmgt.dao.api.AlarmRepository;
 import org.opennms.netmgt.model.OnmsAcknowledgment;
 import org.opennms.netmgt.model.OnmsAlarm;
 import org.opennms.web.alarm.AlarmIdNotFoundException;
-import org.opennms.web.event.Event;
-import org.opennms.web.event.SortStyle;
-import org.opennms.web.event.WebEventRepository;
-import org.opennms.web.event.filter.AlarmIDFilter;
-import org.opennms.web.event.filter.EventCriteria;
-import org.opennms.web.filter.Filter;
 import org.opennms.web.servlet.XssRequestWrapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -58,39 +48,17 @@ import org.springframework.web.servlet.view.RedirectView;
  * @author Ronny Trommer <ronny@opennms.org>
  */
 public class AlarmDetailController extends MultiActionController {
-    private static final int DEFAULT_SHORT_LIMIT = 20;
-    private static final int DEFAULT_MULTIPLE = 0;
-
     private static final Logger logger = LoggerFactory.getLogger(AlarmDetailController.class);
 
-    /**
-     * OpenNMS alarm repository
-     */
     private AlarmRepository m_webAlarmRepository;
-
-    /**
-     * OpenNMS event repository
-     */
-    private WebEventRepository m_webEventRepository;
-
-    private Integer m_defaultShortLimit = DEFAULT_SHORT_LIMIT;
 
     public void setAlarmRepository(AlarmRepository repository) {
         m_webAlarmRepository = repository;
     }
-    public void setWebEventRepository(final WebEventRepository repository) {
-        m_webEventRepository = repository;
-    }
-    public void setDefaultShortLimit(final Integer limit) { m_defaultShortLimit = limit; }
+    public void setDefaultShortLimit(final Integer limit) { /* no-op: events table removed */ }
 
-    /**
-     * <p>afterPropertiesSet</p>
-     *
-     * @throws java.lang.Exception if any.
-     */
     public void afterPropertiesSet() throws Exception {
         Assert.notNull(m_webAlarmRepository, "webAlarmRepository must be set");
-        Assert.notNull(m_webEventRepository, "webEventRepository must be set");
     }
 
     /**
@@ -229,53 +197,7 @@ public class AlarmDetailController extends MultiActionController {
     }
 
     private List<RelatedEvent> getRelatedEvents(final OnmsAlarm alarm, final HttpServletRequest request) {
-        Assert.notNull(alarm);
- 
-        final List<RelatedEvent> relatedEvents = new ArrayList<>();
-
-        final List<Filter> filters = new ArrayList<>(List.of(new AlarmIDFilter(alarm.getId())));
-
-        SortStyle sortStyle = SortStyle.ID;
-        final String sortStyleString = request.getParameter("sortby");
-        if (sortStyleString != null) {
-            try {
-                sortStyle = SortStyle.getSortStyle(sortStyleString);
-            } catch (final IllegalArgumentException e) {
-                logger.error("Unable to determine SortStyle for '{}'", sortStyleString, e);
-            }
-        }
-
-        int limit = m_defaultShortLimit;
-        final String limitString = request.getParameter("limit");
-        if (limitString != null) {
-            try {
-                int newLimit = WebSecurityUtils.safeParseInt(limitString);
-                if (newLimit > 0) {
-                    limit = newLimit;
-                }
-            } catch (final NumberFormatException e) {
-                logger.error("Unable to parse query limit '{}'", limitString, e);
-            }
-        }
-
-        int multiple = DEFAULT_MULTIPLE;
-        final String multipleString = request.getParameter("multiple");
-        if (multipleString != null) {
-            try {
-                multiple = Math.max(0, WebSecurityUtils.safeParseInt(multipleString));
-            } catch (final NumberFormatException e) {
-                logger.error("Unable to parse query multiple '{}'", multipleString, e);
-            }
-        }
-
-        final EventCriteria queryCriteria = new EventCriteria(filters, sortStyle, null, limit, limit * multiple);
-        try {
-            for (final Event event : m_webEventRepository.getMatchingEvents(queryCriteria)) {
-                relatedEvents.add(new RelatedEvent(event.getId(), event.getAlarmId(), event.getCreateTime(), event.getSeverity(), event.getUei(), event.getLogMessage()));
-            }
-        } catch (final Exception e) {
-            logger.error("Could not retrieve events for queryCriteria '{}'.", queryCriteria);
-        }
-        return relatedEvents;
+        // Events table no longer exists — events flow through Kafka.
+        return Collections.emptyList();
     }
 }
--- opennms-webapp/src/main/java/org/opennms/web/controller/notification/NotificationFilterController.java
+++ opennms-webapp/src/main/java/org/opennms/web/controller/notification/NotificationFilterController.java
@@ -22,11 +22,10 @@
 package org.opennms.web.controller.notification;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -34,10 +33,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.opennms.core.utils.WebSecurityUtils;
 import org.opennms.netmgt.dao.api.NodeDao;
 import org.opennms.netmgt.model.OnmsNode;
-import org.opennms.web.event.Event;
-import org.opennms.web.event.WebEventRepository;
-import org.opennms.web.event.filter.EventCriteria;
-import org.opennms.web.event.filter.EventIdListFilter;
 import org.opennms.web.filter.Filter;
 import org.opennms.web.notification.AcknowledgeType;
 import org.opennms.web.notification.NoticeQueryParms;
@@ -69,7 +64,6 @@ public class NotificationFilterController extends AbstractController implements 
     private SortStyle m_defaultSortStyle = SortStyle.ID;
     private AcknowledgeType m_defaultAckType = AcknowledgeType.UNACKNOWLEDGED;
 
-    private WebEventRepository m_webEventRepository;
     private WebNotificationRepository m_webNotificationRepository;
     private NodeDao m_nodeDao;
 
@@ -168,13 +162,8 @@ public class NotificationFilterController extends AbstractController implements 
         int noticeCount = m_webNotificationRepository.countMatchingNotifications(countCriteria);
         final Map<Integer,String[]> nodeLabels = new HashMap<Integer,String[]>();
         final Map<Integer,String[]> nodeLocations = new HashMap<Integer,String[]>();
-        Set<Long> eventIds = new TreeSet<>();
 
-        // really inefficient, is there a better way to do this?
         for (Notification notice : notices) {
-            if (notice.getEventId() > 0) {
-                eventIds.add(notice.getEventId());
-            }
             if (notice.getNodeId() > 0) {
                 if (!nodeLabels.containsKey(notice.getNodeId())) {
                     String[] labels = null;
@@ -213,19 +202,12 @@ public class NotificationFilterController extends AbstractController implements 
             }
         }
 
-        Map<Long, Event> events = new HashMap<Long, Event>();
-        if (eventIds.size() > 0) {
-            for (Event e : m_webEventRepository.getMatchingEvents(new EventCriteria(new EventIdListFilter(eventIds)))) {
-                events.put(e.getId(), e);
-            }
-        }
-
         ModelAndView modelAndView = new ModelAndView(m_successView);
         modelAndView.addObject("notices", notices);
         modelAndView.addObject("noticeCount", noticeCount);
         modelAndView.addObject("nodeLabels", nodeLabels);
         modelAndView.addObject("nodeLocations", nodeLocations);
-        modelAndView.addObject("events", events);
+        modelAndView.addObject("events", Collections.emptyMap());
         modelAndView.addObject("parms", parms);
         return modelAndView;
     }
@@ -267,15 +249,6 @@ public class NotificationFilterController extends AbstractController implements 
     }
 
     /**
-     * <p>setWebEventRepository</p>
-     *
-     * @param webEventRepository a {@link org.opennms.web.event.WebEventRepository} object.
-     */
-    public void setWebEventRepository(WebEventRepository webEventRepository) {
-        m_webEventRepository = webEventRepository;
-    }
-
-    /**
      * <p>setWebNotificationRepository</p>
      *
      * @param webNotificationRepository a {@link org.opennms.web.notification.WebNotificationRepository} object.
@@ -304,7 +277,6 @@ public class NotificationFilterController extends AbstractController implements 
         Assert.isTrue(m_defaultLongLimit > 0, "property defaultLongLimit must be set to a value greater than 0");
         Assert.notNull(m_successView, "property successView must be set");
         Assert.notNull(m_webNotificationRepository, "webNotificationRepository must be set");
-        Assert.notNull(m_webEventRepository, "webEventRepository must be set");
         Assert.notNull(m_nodeDao, "nodeDao must be set");
     }
 
