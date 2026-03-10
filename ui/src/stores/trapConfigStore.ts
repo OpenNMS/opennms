57c574fecfccacc48e9b3186bab182350d0bc65f
Shahbaz <rmasood@nanthealth.com>
2026-03-10 23:53:41 +0500

    Changes

--- ui/src/stores/trapConfigStore.ts
+++ ui/src/stores/trapConfigStore.ts
@@ -6,30 +6,6 @@ export const useTrapConfigStore = defineStore('useTrapConfigStore', {
     isLoading: false,
     activeTab: 0
   }),
-  actions: {
-    // async fetchAllTrapConfigsNames() {
-    //   this.isLoading = true
-    //   try {
-    //     const response = await getAllTrapConfigNames()
-    //     this.uploadedTrapConfigs = response
-    //     this.isLoading = false
-    //   } catch (error) {
-    //     console.error('Error fetching all trap configuration names:', error)
-    //     this.isLoading = false
-    //   }
-    // },
-    // async fetchTrapConfigs() {
-    //   this.isLoading = true
-    //   try {
-    //     const response = await getTrapConfigs()
-    //     await this.fetchAllTrapConfigsNames()
-    //     this.trapConfigs = response
-    //     this.isLoading = false
-    //   } catch (error) {
-    //     console.error('Error fetching trap configurations:', error)
-    //     this.isLoading = false
-    //   }
-    // }
-  }
+  actions: {}
 })
 
