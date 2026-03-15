3979ff935703bdf1e57c2849e808ea2b0527f6c8
David Hustace <david@beaconstrategists.com>
2026-03-14 17:35:51 -0700

    feat: move PassiveServiceMonitor to poller-api module
    Reads from PassiveStatusHolder instead of PassiveStatusKeeper singleton.
    Available on both Pollerd and Minion via the poller-api OSGi bundle.

---
+++ features/poller/api/src/main/java/org/opennms/netmgt/poller/monitors/PassiveServiceMonitor.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to The OpenNMS Group, Inc (TOG) under one or more
+ * contributor license agreements.  See the LICENSE.md file
+ * distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * TOG licenses this file to You under the GNU Affero General
+ * Public License Version 3 (the "License") or (at your option)
+ * any later version.  You may not use this file except in
+ * compliance with the License.  You may obtain a copy of the
+ * License at:
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied.  See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ */
+package org.opennms.netmgt.poller.monitors;
+
+import java.util.Map;
+
+import org.opennms.netmgt.poller.MonitoredService;
+import org.opennms.netmgt.poller.PollStatus;
+import org.opennms.netmgt.poller.passive.PassiveStatusHolder;
+import org.opennms.netmgt.poller.support.AbstractServiceMonitor;
+
+/**
+ * Monitors service status using externally-reported passive status values.
+ *
+ * <p>Reads from {@link PassiveStatusHolder}, a shared static singleton populated by
+ * {@code PassiveStatusKeeper} (on Pollerd) or {@code PassiveStatusTwinSubscriber}
+ * (on Minion via Twin API).</p>
+ */
+public class PassiveServiceMonitor extends AbstractServiceMonitor {
+
+    @Override
+    public PollStatus poll(MonitoredService svc, Map<String, Object> parameters) {
+        return PassiveStatusHolder.getStatus(svc.getNodeLabel(), svc.getIpAddr(), svc.getSvcName());
+    }
+}
