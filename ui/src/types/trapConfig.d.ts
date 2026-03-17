f7b17b2b8adc55e495f3a68c906e50e7ad92855e
Shahbaz Masood <159028986+Shahbaz-dataq@users.noreply.github.com>
2026-03-17 18:09:56 +0500

    NMS-19597: Create form for SNMP v3 User creation and edit (#8351)
    * create user
    
    * fixes
    
    * fixes
    
    * revert changes
    
    * changes
    
    * new changes
    
    * changes

--- ui/src/types/trapConfig.d.ts
+++ ui/src/types/trapConfig.d.ts
@@ -1,4 +1,14 @@
+import { CreateEditMode } from '.'
+
 export interface TrapConfigStoreState {
   isLoading: boolean
   activeTab: number
+  credentialDrawerState: {
+    visible: boolean
+  }
+  createUserDrawerState: {
+    visible: boolean
+    mode: CreateEditMode
+  }
 }
+
