c1b73e4b6f83450287e9f75e57d3d87b9701c0b0
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2026-04-17 14:19:15 -0400

    revert some of the changes

--- ui/src/containers/Home.vue
+++ ui/src/containers/Home.vue
@@ -1,6 +1,14 @@
 <template>
-  <div />
+  <div class="feather-row">
+    <div class="feather-col-12">
+      <span>Please choose an option from the main menu.</span>
+    </div>
+  </div>
 </template>
-
+  
 <script setup lang="ts">
 </script>
+  
+<style scoped>
+</style>
+ 
