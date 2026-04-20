7c4889677ecc2a4d241fad80efef7ad59b1f18a2
Scott Theleman <synqotik@users.noreply.github.com>
2026-04-20 12:02:26 -0400

    fix smoke tests

--- smoke-test/src/test/java/org/opennms/smoketest/MenuHeaderIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/MenuHeaderIT.java
@@ -94,7 +94,7 @@ public class MenuHeaderIT extends OpenNMSSeleniumIT {
         // Inventory Menu
         // Note, some items are below under Vue UI checks
         clickMenuItem("inventoryMenu", "Nodes");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='btn-toolbar']/span[text()='Nodes' or text()='Availability']")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'btn-toolbar')]/span[text()='Nodes' or text()='Availability']")));
 
         clickMenuItem("inventoryMenu", "Assets");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='content']//div[@class='card-header']/span[text()='Search Asset Information']")));
