4d17a83d164ada781fce0a20f98f8474594ed7dd
Chandra Gorantla <chandra@opennms.com>
2026-07-02 22:47:19 -0400

    NMS-19620: fix nodelist smoke test

--- smoke-test/src/test/java/org/opennms/smoketest/ui/NodeListIT.java
+++ smoke-test/src/test/java/org/opennms/smoketest/ui/NodeListIT.java
@@ -82,14 +82,18 @@ public class NodeListIT extends OpenNMSSeleniumIT {
         wait.until(pageContainsText("Nodes"));
         wait.until(pageContainsText("Test_Node1"));
 
+        // The nodes table is a PrimeVue DataTable: the 'node-table' class is merged with
+        // PrimeVue's own classes and the <table> is nested inside a table-container div
+        final String tableXpath = "//div[contains(@class, 'node-table')]//table/tbody/tr/td";
+
         // find the Actions menu for this node
-        findElementByXpath("//div[@class='node-table']/table/tbody/tr/td//button[@title='Node Actions']");
+        findElementByXpath(tableXpath + "//button[@title='Node Actions']");
 
         // find the node row in the table
-        findElementByXpath("//div[@class='node-table']/table/tbody/tr/td/a[text()='Test_Node1']");
+        findElementByXpath(tableXpath + "//a[text()='Test_Node1']");
 
         // find the node link
-        String nodeLinkXpath = String.format("//div[@class='node-table']/table/tbody/tr/td/a[contains(@href, 'element/node.jsp?node=%d')]", savedNodeId);
+        String nodeLinkXpath = String.format(tableXpath + "//a[contains(@href, 'element/node.jsp?node=%d')]", savedNodeId);
 
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(nodeLinkXpath)));
 
