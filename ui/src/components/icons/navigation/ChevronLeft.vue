b9c52fcd849d7cbbd0d5bb614db05c8de10a471a
Scott Theleman <synqotik@users.noreply.github.com>
2026-07-10 13:33:33 -0400

    NMS-19981: Vendor all FeatherDS icons as local OnmsIcon components
    Generate local template-only SFCs for all 262 @featherds/icon glyphs into
    src/components/icons/<category>/<Name>.vue (SVG geometry copied verbatim via a
    one-time render-to-string generator, scripts/generate-icons.mjs), plus a
    self-contained OnmsIcon wrapper (depends only on vue) replacing FeatherIcon
    with the same :icon/flex/title API. No app code wired to these yet.

---
+++ ui/src/components/icons/navigation/ChevronLeft.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M14.71,6.7a1,1,0,0,0-1.41,0L8,12l5.3,5.3a1,1,0,0,0,1.41-1.41L10.83,12l3.88-3.89A1,1,0,0,0,14.71,6.7Z"></path></svg>
+</template>
---
+++ ui/src/components/icons/network/Appliances.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M22,12h-4c-1.1,0-2,.9-2,2v8c0,1.1.9,2,2,2h4c1.1,0,2-.9,2-2v-8c0-1.1-.9-2-2-2ZM22,14v3h-4v-3h4ZM18,22v-3h4v3h-4ZM2,12h12v2h-3v3h2c.55,0,1,.45,1,1v1H4v-1c0-.55.45-1,1-1h2v-3H2c-1.1,0-2-.9-2-2V3C0,1.9.9,1,2,1h14c1.1,0,2,.9,2,2v7h-2V3H2v9Z"></path></svg>
+</template>
