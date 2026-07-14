43037a2c56e4dd4156b91a5f690b13e8f6938dbb
Scott Theleman <synqotik@users.noreply.github.com>
2026-07-14 09:39:04 -0400

    !smoke lint fix

--- ui/src/components/Menu/SideMenu.vue
+++ ui/src/components/Menu/SideMenu.vue
@@ -80,7 +80,6 @@ const isPinned = ref<boolean>(menuStore.sideMenuExpanded() ?? false)
 const isHovering = ref<boolean>(false)
 const isOpen = computed<boolean>(() => isPinned.value || isHovering.value)
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const tieredMenuRef = ref<any>(null)
 
 const onRailEnter = () => {
