344e9fb6b063a5e247196d945d7fc9b20124da42
Chandra Gorantla <chandra@opennms.com>
2026-07-23 14:13:03 -0400

    NMS-20030: Fix v2 alarm rest api access issue

--- opennms-webapp-rest/src/test/java/org/opennms/web/rest/v2/AlarmRestServiceIT.java
+++ opennms-webapp-rest/src/test/java/org/opennms/web/rest/v2/AlarmRestServiceIT.java
@@ -687,6 +687,30 @@ public class AlarmRestServiceIT extends AbstractSpringJerseyRestTestCase {
         executeQueryAndVerify("_s=alarm.severity==CLEARED", 1);
     }
 
+    @Test
+    @JUnitTemporaryDatabase
+    public void testReadOnlyUserCannotModifyAlarm() throws Exception {
+        // NMS-20051: a ROLE_REST user who is also ROLE_READONLY must not be able to modify alarms.
+        setUser("ranger", new String[]{ "ROLE_REST", "ROLE_READONLY" });
+        sendRequest(PUT, "/alarms/" + alarm1.getId(), parseParamData("ack=true"), 403);
+    }
+
+    @Test
+    @JUnitTemporaryDatabase
+    public void testCannotAcknowledgeAsAnotherUser() throws Exception {
+        // NMS-20051: a ROLE_REST user without ROLE_DELEGATE must not acknowledge as another username.
+        setUser("foo", new String[]{ "ROLE_REST" });
+        sendRequest(PUT, "/alarms/" + alarm1.getId(), parseParamData("ack=true&ackUser=bar"), 403);
+    }
+
+    @Test
+    @JUnitTemporaryDatabase
+    public void testCanAcknowledgeAsSelf() throws Exception {
+        // NMS-20051: the fix must still allow a ROLE_REST user to acknowledge as themselves.
+        setUser("foo", new String[]{ "ROLE_REST" });
+        sendRequest(PUT, "/alarms/" + alarm1.getId(), parseParamData("ack=true"), 204);
+    }
+
     private void anticipateEvent(EventBuilder eventBuilder) {
         m_eventMgr.getEventAnticipator().anticipateEvent(eventBuilder.getEvent());
     }
