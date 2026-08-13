5037c2a6e7fbac30306217c1e1313ba1943146a1
Dino <dino2gnt@gmail.com>
2026-08-13 13:34:41 -0500

    Fix ITs

--- opennms-services/src/test/java/org/opennms/netmgt/notifd/BSFNotificationStrategyIT.java
+++ opennms-services/src/test/java/org/opennms/netmgt/notifd/BSFNotificationStrategyIT.java
@@ -103,7 +103,7 @@ public class BSFNotificationStrategyIT {
     public void canUseNodeInScript() throws IOException {
         // Create a simple Groovy script that verifies the node bean
         File notifyScript = tempFolder.newFile("notify.groovy");
-        FileUtils.write(notifyScript, "results.put(\"status\", node.id == 1 ? \"OK\" : \"NOT_OK\")");
+        FileUtils.write(notifyScript, "results.put(\"status\", node.id == " + node1Id() + " ? \"OK\" : \"NOT_OK\")");
 
         List<Argument> arguments = new ArrayList<>();
         // Point to our script
@@ -116,17 +116,47 @@ public class BSFNotificationStrategyIT {
     }
 
     @Test
-    public void canEvalBeanshellScript() throws IOException {
+    public void canEvalScript() throws IOException {
         // Under run-type=eval the script's last expression becomes the status
-        File notifyBsh = tempFolder.newFile("notify-eval.bsh");
-        FileUtils.write(notifyBsh, "node.id == " + node1Id() + " ? \"OK\" : \"NOT_OK\";");
+        File notifyScript = tempFolder.newFile("notify-eval.groovy");
+        FileUtils.write(notifyScript, "node.id == " + node1Id() + " ? \"OK\" : \"NOT_OK\"");
 
         List<Argument> arguments = new ArrayList<>();
-        arguments.add(new Argument("file-name", null, notifyBsh.getAbsolutePath(), false));
+        arguments.add(new Argument("file-name", null, notifyScript.getAbsolutePath(), false));
         arguments.add(new Argument("run-type", null, "eval", false));
         arguments.add(new Argument(NotificationManager.PARAM_NODE, null, node1Id(), false));
 
         assertEquals(0, bsfNotificationStrategy.send(arguments));
+    }
+
+    @Test
+    public void beanshellScriptIsNoLongerSupported() throws IOException {
+        // BeanShell was removed along with BSF; a .bsh script must now fail
+        // cleanly with a diagnosable message rather than silently doing nothing
+        File notifyBsh = tempFolder.newFile("notify-legacy.bsh");
+        FileUtils.write(notifyBsh, "results.put(\"status\", \"OK\");");
+
+        List<Argument> arguments = new ArrayList<>();
+        arguments.add(new Argument("file-name", null, notifyBsh.getAbsolutePath(), false));
+
+        assertEquals(-1, bsfNotificationStrategy.send(arguments));
+        MockLogAppender.assertLogMatched(Level.ERROR, "No JSR-223 script engine found");
+        MockLogAppender.resetState();
+    }
+
+    @Test
+    public void beanshellLangClassIsNoLongerSupported() throws IOException {
+        // likewise for an explicit lang-class left over from a BSF-era config
+        File notifyScript = tempFolder.newFile("notify-legacy-lang.groovy");
+        FileUtils.write(notifyScript, "results.put(\"status\", \"OK\")");
+
+        List<Argument> arguments = new ArrayList<>();
+        arguments.add(new Argument("file-name", null, notifyScript.getAbsolutePath(), false));
+        arguments.add(new Argument("lang-class", null, "beanshell", false));
+
+        assertEquals(-1, bsfNotificationStrategy.send(arguments));
+        MockLogAppender.assertLogMatched(Level.ERROR, "No JSR-223 script engine found");
+        MockLogAppender.resetState();
     }
 
     @Test
@@ -158,25 +188,26 @@ public class BSFNotificationStrategyIT {
     public void canResolveEngineViaLangClass() throws IOException {
         // No usable extension; lang-class selects the engine by JSR-223 name
         File notifyTxt = tempFolder.newFile("notify.txt");
-        FileUtils.write(notifyTxt, "results.put(\"status\", node.id == " + node1Id() + " ? \"OK\" : \"NOT_OK\");");
+        FileUtils.write(notifyTxt, "results.put(\"status\", node.id == " + node1Id() + " ? \"OK\" : \"NOT_OK\")");
 
         List<Argument> arguments = new ArrayList<>();
         arguments.add(new Argument("file-name", null, notifyTxt.getAbsolutePath(), false));
-        arguments.add(new Argument("lang-class", null, "beanshell", false));
+        arguments.add(new Argument("lang-class", null, "groovy", false));
         arguments.add(new Argument(NotificationManager.PARAM_NODE, null, node1Id(), false));
 
         assertEquals(0, bsfNotificationStrategy.send(arguments));
     }
 
     @Test
-    public void nullVariablesStayDefinedInBeanshell() throws IOException {
-        // no -nodeid argument: node, node_label, foreign_source etc. are null;
-        // BSF defined them as null and scripts test them against null
-        File notifyBsh = tempFolder.newFile("notify-null.bsh");
-        FileUtils.write(notifyBsh, "results.put(\"status\", (node == null && foreign_source == null) ? \"OK\" : \"NOT_OK\");");
+    public void nullVariablesStayDefined() throws IOException {
+        // no -nodeid argument: node, node_label, foreign_source etc. are null.
+        // They must still be *defined* so scripts can test them against null,
+        // rather than failing with a missing-property error.
+        File notifyScript = tempFolder.newFile("notify-null.groovy");
+        FileUtils.write(notifyScript, "results.put(\"status\", (node == null && foreign_source == null) ? \"OK\" : \"NOT_OK\")");
 
         List<Argument> arguments = new ArrayList<>();
-        arguments.add(new Argument("file-name", null, notifyBsh.getAbsolutePath(), false));
+        arguments.add(new Argument("file-name", null, notifyScript.getAbsolutePath(), false));
 
         assertEquals(0, bsfNotificationStrategy.send(arguments));
     }
@@ -185,11 +216,11 @@ public class BSFNotificationStrategyIT {
     public void fileNameFromSubstitutionWorks() throws IOException {
         // notifd passes "" as the value for switches without a notification
         // parameter; the command's <substitution> must be honored then
-        File notifyBsh = tempFolder.newFile("notify-subst.bsh");
-        FileUtils.write(notifyBsh, "results.put(\"status\", node.id == " + node1Id() + " ? \"OK\" : \"NOT_OK\");");
+        File notifyScript = tempFolder.newFile("notify-subst.groovy");
+        FileUtils.write(notifyScript, "results.put(\"status\", node.id == " + node1Id() + " ? \"OK\" : \"NOT_OK\")");
 
         List<Argument> arguments = new ArrayList<>();
-        arguments.add(new Argument("file-name", notifyBsh.getAbsolutePath(), "", false));
+        arguments.add(new Argument("file-name", notifyScript.getAbsolutePath(), "", false));
         arguments.add(new Argument(NotificationManager.PARAM_NODE, null, node1Id(), false));
 
         assertEquals(0, bsfNotificationStrategy.send(arguments));
@@ -230,7 +261,7 @@ public class BSFNotificationStrategyIT {
     @Test
     public void missingScriptFileReturnsFailure() {
         List<Argument> arguments = new ArrayList<>();
-        arguments.add(new Argument("file-name", null, new File(tempFolder.getRoot(), "no-such-script.bsh").getAbsolutePath(), false));
+        arguments.add(new Argument("file-name", null, new File(tempFolder.getRoot(), "no-such-script.groovy").getAbsolutePath(), false));
 
         assertEquals(-1, bsfNotificationStrategy.send(arguments));
         MockLogAppender.assertLogMatched(Level.WARN, "Cannot locate or read script file");
@@ -239,11 +270,11 @@ public class BSFNotificationStrategyIT {
 
     @Test
     public void nonOkStatusReturnsFailure() throws IOException {
-        File notifyBsh = tempFolder.newFile("notify-nok.bsh");
-        FileUtils.write(notifyBsh, "results.put(\"status\", \"NOT_OK\");");
+        File notifyScript = tempFolder.newFile("notify-nok.groovy");
+        FileUtils.write(notifyScript, "results.put(\"status\", \"NOT_OK\")");
 
         List<Argument> arguments = new ArrayList<>();
-        arguments.add(new Argument("file-name", null, notifyBsh.getAbsolutePath(), false));
+        arguments.add(new Argument("file-name", null, notifyScript.getAbsolutePath(), false));
 
         assertEquals(-1, bsfNotificationStrategy.send(arguments));
         MockLogAppender.assertLogMatched(Level.WARN, "did not indicate successful notification");
@@ -252,11 +283,11 @@ public class BSFNotificationStrategyIT {
 
     @Test
     public void invalidRunTypeReturnsFailure() throws IOException {
-        File notifyBsh = tempFolder.newFile("notify-bogus.bsh");
-        FileUtils.write(notifyBsh, "results.put(\"status\", \"OK\");");
+        File notifyScript = tempFolder.newFile("notify-bogus.groovy");
+        FileUtils.write(notifyScript, "results.put(\"status\", \"OK\")");
 
         List<Argument> arguments = new ArrayList<>();
-        arguments.add(new Argument("file-name", null, notifyBsh.getAbsolutePath(), false));
+        arguments.add(new Argument("file-name", null, notifyScript.getAbsolutePath(), false));
         arguments.add(new Argument("run-type", null, "bogus", false));
 
         assertEquals(-1, bsfNotificationStrategy.send(arguments));
@@ -266,12 +297,13 @@ public class BSFNotificationStrategyIT {
 
     @Test
     public void deprecatedBsfEngineSwitchWarnsButStillWorks() throws IOException {
-        File notifyBsh = tempFolder.newFile("notify-deprecated.bsh");
-        FileUtils.write(notifyBsh, "results.put(\"status\", node.id == " + node1Id() + " ? \"OK\" : \"NOT_OK\");");
+        File notifyScript = tempFolder.newFile("notify-deprecated.groovy");
+        FileUtils.write(notifyScript, "results.put(\"status\", node.id == " + node1Id() + " ? \"OK\" : \"NOT_OK\")");
 
         List<Argument> arguments = new ArrayList<>();
-        arguments.add(new Argument("file-name", null, notifyBsh.getAbsolutePath(), false));
-        arguments.add(new Argument("lang-class", null, "beanshell", false));
+        arguments.add(new Argument("file-name", null, notifyScript.getAbsolutePath(), false));
+        arguments.add(new Argument("lang-class", null, "groovy", false));
+        // a genuine BSF-era value: it must be ignored, not acted on
         arguments.add(new Argument("bsf-engine", null, "bsh.util.BeanShellBSFEngine", false));
         arguments.add(new Argument("file-extensions", null, "bsh", false));
         arguments.add(new Argument(NotificationManager.PARAM_NODE, null, node1Id(), false));
