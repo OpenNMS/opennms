33c974662fa793872a81c03b6a3b24c58e48f5a4
Morteza E <105240903+mershad-manesh@users.noreply.github.com>
2026-09-21 14:57:43 -0400

    enable healthcheck

--- opennms-container/minion/container-fs/health.sh
+++ opennms-container/minion/container-fs/health.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if curl http://localhost:8181/minion/rest/health/probe | grep --quiet "Everything is awesome"; then 
+if curl -sSf http://localhost:8181/minion/rest/health/probe | grep --quiet "Everything is awesome"; then
   exit 0;
 else
   exit 1;
